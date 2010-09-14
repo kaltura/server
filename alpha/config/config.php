d1c1d2970a46c882928d25d7d512e69a6bcfc0e9
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 11:45:00 +0000

    Directory tree refactoring #41 - Merging onprem into the core
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@50038 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/config/config.php
@@ -0,0 +1,6 @@
+<?php
+require_once ( "kConf.php" ); // kaltura configuration
+// symfony directories
+
+$sf_symfony_lib_dir = realpath(dirname(__FILE__).'/../../symfony');
+$sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../symfony-data');
