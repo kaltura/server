ac477c49831afc8e1c020d06101ac7caf2d9d25c
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 10:04:04 +0000

    Directory tree refactoring step #27 - Moved partner specific to SaaS
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@49945 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/partner_specific/clear_partners_archive_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# delete archive content of partner 34149
-MAX_DATE=$(date -d "yesterday" +%Y-%m-%d)
-/opt/kaltura/app/alpha/crond/kaltura/partner_specific/remove_archive_for_partner.sh 34149 $MAX_DATE >> /var/log/`hostname`-removePartenrsArchiveContent.log
-
-# delete archive content of partner 27121
-#/opt/kaltura/app/alpha/crond/kaltura/partner_specific/remove_archive_for_partner.sh 27121 $MAX_DATE >> /var/log/`hostname`-removePartenrsArchiveContent.log
