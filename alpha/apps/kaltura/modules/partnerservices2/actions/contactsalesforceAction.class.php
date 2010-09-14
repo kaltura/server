1682f142be03056507a5ec2601745ea940fd6787
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 11:27:23 +0000

    Directory tree refactoring #41 - Merging onprem into the core
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@50005 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/apps/kaltura/modules/partnerservices2/actions/contactsalesforceAction.class.php
@@ -0,0 +1,22 @@
+<?php
+require_once ( "defPartnerservices2Action.class.php");
+require_once ( "myPartnerUtils.class.php");
+
+class contactsalesforceAction extends defPartnerservices2Action
+{
+	public function describe()
+	{
+		return false;
+	}
+	
+	public function executeImpl ( $partner_id , $subp_id , $puser_id , $partner_prefix , $puser_kuser )
+	{
+		return false;
+	}
+	
+	private function sendLeadToSalesForce($lead_array)
+	{
+		return false;
+	}
+}
+?>
