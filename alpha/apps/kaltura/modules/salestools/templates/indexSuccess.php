d6ff6b1898b6f34a791f6e7a52aa91d83b2b0cfe
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 10:10:09 +0000

    Directory tree refactoring step #36 - Moved SaaS specific files
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@49956 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/salestools/templates/indexSuccess.php
+++
@@ -1,2 +0,0 @@
-<a href="<?php echo substr($_SERVER['REQUEST_URI'],0, strpos($_SERVER['REQUEST_URI'], '?')); ?>?logout=1">logout</a><br />
-<a href="/index.php/salestools/partnersettings">partner settings</a>
