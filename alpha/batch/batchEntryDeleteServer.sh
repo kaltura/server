01290f06e6b59225b6e656b45e29abb187c4316d
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 10:24:11 +0000

    Directory tree refactoring #40 - Extracting SaaS specific from the server
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@49980 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/batch/batchEntryDeleteServer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /web/kaltura/alpha/batch
-
-LOGFILE="/web/logs/$HOSTNAME-batchEntryDeleteServer.log"
-PHP="./batchEntryDeleteServer.php"
-BATCH_LOGFILE="/web/logs/$HOSTNAME-batchRunner.log"
-php batchRunner.php "php $PHP" $LOGFILE >> $BATCH_LOGFILE &
--- alpha/batch/batchImportServer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /web/kaltura/alpha/batch
-
-LOGFILE="/web/logs/$HOSTNAME-batchImportServer.log"
-PHP="./batchImportServer.php"
-BATCH_LOGFILE="/web/logs/$HOSTNAME-batchRunner.log"
-php batchRunner.php "php $PHP" $LOGFILE >> $BATCH_LOGFILE &
