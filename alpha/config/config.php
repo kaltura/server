23016a1361783a51bd0c67ec78e0670a603a9f7b
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 13:14:49 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@50069 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/config.php
+++ alpha/config/config.php
@@ -2,5 +2,5 @@
 require_once ( "kConf.php" ); // kaltura configuration
 // symfony directories
 
-$sf_symfony_lib_dir = realpath(dirname(__FILE__).'/../../symfony');
-$sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../symfony-data');
+$sf_symfony_lib_dir = realpath(dirname(__FILE__).'/../../../symfony');
+$sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../../symfony-data');
