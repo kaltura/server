c06d3a4eb92551cd0933c6f81925958833294863
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 10:01:30 +0000

    Directory tree refactoring step #25 - Moved partner specific to SaaS
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@49943 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/limelight/get_ll_log.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#WGET_RC=1
-#while [ $WGET_RC -ne 0 ]; do
-wget -c "ftp://kaltura:n9iub4@kaltura.logs.llnw.net/ht/kaltura.ht.$1.log.gz"
-#	WGET_RC=$?
-#done
--- alpha/crond/limelight/ll_insert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-zcat kaltura.ht.$1.log.gz | php /opt/kaltura/app/alpha/scripts/billing_summary_limelight.php  >ll_res
-php /opt/kaltura/app/alpha/scripts/billing_summary_insert.php ll $1 ll_res >ll_res.sql
-mysql -hpa-db kaltura -uroot -proot < ll_res.sql
-
-rm -f ll_res
-rm -f ll_res.sql
