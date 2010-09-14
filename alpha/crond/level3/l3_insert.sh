7ff0441713831aec63495bd522b5d604d36cd5a5
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 10:01:16 +0000

    Directory tree refactoring step #24 - Moved partner specific to SaaS
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@49942 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/level3/l3_insert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-zcat l3_logs_$1_.log.gz | php /opt/kaltura/app/alpha/scripts/billing_summary_level3.php  >l3_res
-php /opt/kaltura/app/alpha/scripts/billing_summary_insert.php l3 $1 l3_res >l3_res.sql
-mysql -hpa-db kaltura -uroot -proot < l3_res.sql
-
-rm -f l3_res
-rm -f l3_res.sql
