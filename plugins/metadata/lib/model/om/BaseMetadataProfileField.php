9162f3ee9582db9038313b59a47c9a8fe4daccb3
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-14 17:23:12 +0000

    Auto generated
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@50124 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/lib/model/om/BaseMetadataProfileField.php
+++ plugins/metadata/lib/model/om/BaseMetadataProfileField.php
@@ -846,6 +846,15 @@ abstract class BaseMetadataProfileField extends BaseObject  implements Persisten
 	{
 		return parent::preSave($con);
 	}
+
+	/**
+	 * Code to be run after persisting the object
+	 * @param PropelPDO $con
+	 */
+	public function postSave(PropelPDO $con = null) 
+	{
+		$this->oldColumnsValues = array(); 
+	}
 	
 	/**
 	 * Code to be run before inserting to database
