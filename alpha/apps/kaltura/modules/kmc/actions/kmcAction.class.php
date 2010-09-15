36831c613489dfdc2a0bd34ad0e9cc00e03da664
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-15 09:20:27 +0000

    do not cause php notices
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@50155 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/actions/kmcAction.class.php
+++ alpha/apps/kaltura/modules/kmc/actions/kmcAction.class.php
@@ -9,13 +9,15 @@ class kmcAction extends kalturaAction
 		$this->beta = $this->getRequestParameter( "beta" );
 		$this->setPassHashKey = $this->getRequestParameter( "setpasshashkey" );
 		$this->hashKeyErrorCode = null;
-		try {
-			if (adminKuserPeer::isHashKeyValid()) {
-				$this->hashKeyErrorCode = kAdminKuserException::NEW_PASSWORD_HASH_KEY_INVALID;
+		if ($this->setPassHashKey) {
+			try {
+				if (adminKuserPeer::isHashKeyValid($this->setPassHashKey)) {
+					$this->hashKeyErrorCode = kAdminKuserException::NEW_PASSWORD_HASH_KEY_INVALID;
+				}
 			}
-		}
-		catch (kCoreException $e) {
-			$this->hashKeyErrorCode = $e->getCode();
+			catch (kCoreException $e) {
+				$this->hashKeyErrorCode = $e->getCode();
+			}
 		}
 		sfView::SUCCESS;
 	}
