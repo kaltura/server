d189834ddc7de05819d5f100875c230ab54bc2a7
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-21 16:07:44 +0000

    Moved the populate sphinx entires script
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@50565 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/base/scripts/populateSphinxEntries.php
@@ -0,0 +1,74 @@
+<?php
+
+set_time_limit(0);
+
+ini_set("memory_limit","700M");
+
+chdir(dirname(__FILE__));
+
+define('ROOT_DIR', realpath(dirname(__FILE__) . '/../../../'));
+require_once(ROOT_DIR . '/infra/bootstrap_base.php');
+require_once(ROOT_DIR . '/infra/KAutoloader.php');
+
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "metadata", "*"));
+KAutoloader::setClassMapFilePath('../../cache/classMap.cache');
+KAutoloader::register();
+
+error_reporting(E_ALL);
+
+KalturaLog::setLogger(new KalturaStdoutLogger());
+
+$dbConf = kConf::getDB();
+DbManager::setConfig($dbConf);
+DbManager::initialize();
+
+$c = new Criteria();
+
+if($argc > 1 && is_numeric($argv[1]))
+	$c->add(entryPeer::INT_ID, $argv[1], Criteria::GREATER_EQUAL);
+
+$c->addAscendingOrderByColumn(entryPeer::INT_ID);
+$c->setLimit(10000);
+
+$insertsFile = fopen('inserts.sql', 'a');
+$errorsFile = fopen('errors.sql', 'a');
+
+if(!$insertsFile)
+{
+	echo "upable to open sql file [" . realpath($insertsFile) . "]";
+	exit;
+}
+
+$con = myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_PROPEL2);
+//$sphinxCon = DbManager::getSphinxConnection();
+
+$entries = entryPeer::doSelect($c, $con);
+while(count($entries))
+{
+	foreach($entries as $entry)
+	{
+		KalturaLog::log('entry id ' . $entry->getId() . ' int id[' . $entry->getIntId() . '] crc id[' . $entry->getSphinxId() . ']');
+		
+		try{
+			$sql = $entry->saveToSphinx(true);
+//			$sphinxCon->exec($sql);
+//			fputs($insertsFile, "$sql;\n");
+		}
+		catch(Exception $e){
+			fputs($errorsFile, $e->getMessage() . "\n");
+		}
+	}
+	
+	$c->setOffset($c->getOffset() + count($entries));
+	entryPeer::clearInstancePool();
+	MetadataPeer::clearInstancePool();
+	MetadataProfilePeer::clearInstancePool();
+	MetadataProfileFieldPeer::clearInstancePool();
+	$entries = entryPeer::doSelect($c, $con);
+}
+
+fclose($insertsFile);
+fclose($errorsFile);
+
+KalturaLog::log('Done');
