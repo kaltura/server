d9daf56190b17a5217824d9a80aa371dc1a7c7d8
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-05 10:18:34 +0000

    Fixed sphinx entries script after Eran's sphinx refactoring
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@51020 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/base/scripts/populateSphinxEntries.php
+++ deployment/base/scripts/populateSphinxEntries.php
@@ -12,6 +12,7 @@ require_once(ROOT_DIR . '/infra/KAutoloader.php');
 
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "metadata", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "sphinx_search", "*"));
 KAutoloader::setClassMapFilePath('../../cache/classMap.cache');
 KAutoloader::register();
 
@@ -44,16 +45,15 @@ $con = myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_PROPEL2);
 //$sphinxCon = DbManager::getSphinxConnection();
 
 $entries = entryPeer::doSelect($c, $con);
+$sphinx = new kSphinxSearchManager();
 while(count($entries))
 {
 	foreach($entries as $entry)
 	{
-		KalturaLog::log('entry id ' . $entry->getId() . ' int id[' . $entry->getIntId() . '] crc id[' . $entry->getSphinxId() . ']');
+		KalturaLog::log('entry id ' . $entry->getId() . ' int id[' . $entry->getIntId() . '] crc id[' . $sphinx->getSphinxId($entry) . ']');
 		
-		try{
-			$sql = $entry->saveToSphinx(true);
-//			$sphinxCon->exec($sql);
-//			fputs($insertsFile, "$sql;\n");
+		try {
+			$sphinx->saveToSphinx($entry, true);
 		}
 		catch(Exception $e){
 			fputs($errorsFile, $e->getMessage() . "\n");
