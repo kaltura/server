daa6f4603596cbd3ef91d5ad6fab3e9554de9b16
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-11 13:28:40 +0000

    new - EXPRESSION_ENCODER support
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@51218 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/batches/Convert/KAsyncConvert.class.php
+++ batch/batches/Convert/KAsyncConvert.class.php
@@ -318,6 +318,7 @@ class KAsyncConvert extends KBatchBase
 		if  ( $this->taskConfig->params->KalturaCom ) $supported_engines_arr[] = "[" . KalturaConversionEngineType::KALTURA_COM . "] kaltura";
 		if  ( $this->taskConfig->params->useFFMpegAux ) $supported_engines_arr[] = "[" . KalturaConversionEngineType::FFMPEG_AUX . "] ffmpeg_aux";
 		if  ( $this->taskConfig->params->useFFMpegVp8 ) $supported_engines_arr[] = "[" . KalturaConversionEngineType::FFMPEG_VP8 . "] ffmpeg_vp8";
+		if  ( $this->taskConfig->params->useExpressionEncoder ) $supported_engines_arr[] = "[" . KalturaConversionEngineType::EXPRESSION_ENCODER . "] expression_encoder";
 		if  ( $this->taskConfig->params->QuickTimePlayerTools ) $supported_engines_arr[] = "[" . KalturaConversionEngineType::QUICK_TIME_PLAYER_TOOLS . "] quicktimeplayer_tools";
 		if  ( $this->taskConfig->params->usePdfCreator ) $supported_engines_arr[] = "[" . KalturaConversionEngineType::PDF_CREATOR . "] pdf creator";
 		if  ( $this->taskConfig->params->usePdf2Swf ) $supported_engines_arr[] = "[" . KalturaConversionEngineType::PDF2SWF . "] pdf2swf";
@@ -346,6 +347,7 @@ class KAsyncConvert extends KBatchBase
 		if  ( $this->taskConfig->params->KalturaCom ) $supported_engines_arr[] = KalturaConversionEngineType::KALTURA_COM;
 		if  ( $this->taskConfig->params->useFFMpegAux ) $supported_engines_arr[] = KalturaConversionEngineType::FFMPEG_AUX;
 		if  ( $this->taskConfig->params->useFFMpegVp8 ) $supported_engines_arr[] = KalturaConversionEngineType::FFMPEG_VP8;
+		if  ( $this->taskConfig->params->useExpressionEncoder ) $supported_engines_arr[] = KalturaConversionEngineType::EXPRESSION_ENCODER;
 		if  ( $this->taskConfig->params->useQtTools ) $supported_engines_arr[] = KalturaConversionEngineType::QUICK_TIME_PLAYER_TOOLS;
 		if  ( $this->taskConfig->params->useFastStart ) $supported_engines_arr[] = KalturaConversionEngineType::FAST_START;
 		if  ( $this->taskConfig->params->usePdfCreator ) $supported_engines_arr[] = KalturaConversionEngineType::PDF_CREATOR;
