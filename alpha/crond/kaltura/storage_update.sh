397aa72685c06cd48fc57d354954f83e0235cc3b
gonen <gonen@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-20 10:14:03 +0000

    disabled findMediaStats.php script and changed logging configuration
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@51605 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/storage_update.sh
+++ alpha/crond/kaltura/storage_update.sh
@@ -4,8 +4,9 @@ cd /opt/kaltura/logs
 
 WHEN=$(date -d "yesterday" +%Y-%m-%d)
 
-php /opt/kaltura/app/support_prod/test/dummy/findEntriesSizes.php $WHEN >> /var/log/`hostname`-findEntriesSizes.log
-php /opt/kaltura/app/support_prod/test/dummy/findMediaStats.php $WHEN >> /var/log/`hostname`-findMediaStats.log
+php /opt/kaltura/app/support_prod/test/dummy/findEntriesSizes.php $WHEN >> /var/log/`hostname`-findEntriesSizes.log 2>&1
+#not in use any more
+##php /opt/kaltura/app/support_prod/test/dummy/findMediaStats.php $WHEN >> /var/log/`hostname`-findMediaStats.log 2>&1
 #not ready yet
 #php /web/kaltura/alpha/test/dummy/findUserStats.php $WHEN
 #php /web/kaltura/alpha/batch/batchPartnerUsage.php >> /web/logs/BATCH1-batchPartnerUsage.log
@@ -13,4 +14,4 @@ TODAY=$(date +%F)
 #php /web/kaltura/alpha/batch/newBatchPartnerUsage.php daily_storage $TODAY >> /web/logs/BATCH1-newBatchPartnerUsage.log
 #php /web/kaltura/alpha/batch/newBatchPartnerUsage.php monthly_agg $TODAY >> /web/logs/BATCH1-newBatchPartnerUsageMonthly.log
 # monitor partner usage:
-php /opt/kaltura/app/alpha/batch/batchPartnerUsage.php >> /var/log/`hostname`-BatchPartnerUsage_upgradeProcess.log
+php /opt/kaltura/app/alpha/batch/batchPartnerUsage.php >> /var/log/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
