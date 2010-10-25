8ac0634aa879d2407ef991b4b1461c5d509408ef
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-25 16:09:10 +0000

    new service entryMetadata
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@51882 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/metadata/services/EntryMetadataService.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Metadata service
+ *
+ * @service entryMetadata
+ */
+class EntryMetadataService extends KalturaEntryService
+{
+	public function initService($partnerId, $puserId, $ksStr, $serviceName, $action)
+	{
+		parent::initService($partnerId, $puserId, $ksStr, $serviceName, $action);
+
+		myPartnerUtils::addPartnerToCriteria(new MetadataPeer(), $this->getPartnerId(), $this->private_partner_data, $this->partnerGroup());
+		
+		if(!MetadataPlugin::isAllowedPartner(kCurrentContext::$ks_partner_id))
+			throw new KalturaAPIException(KalturaErrors::SERVICE_FORBIDDEN);
+	}
+	
+	/**
+	 * List base entries with their metadata by filter with paging support.
+	 * 
+	 * @action list
+     * @param KalturaBaseEntryFilter $filter Entry filter
+	 * @param KalturaFilterPager $pager Pager
+	 * @return KalturaEntryMetadataListResponse Wrapper for array of base entries and total count
+	 */
+	function listAction(KalturaBaseEntryFilter $filter = null, KalturaFilterPager $pager = null)
+	{
+	    myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_PROPEL3;
+		
+	    list($entries, $totalCount) = parent::listEntriesByFilter($filter, $pager);
+	    $entryIds = array();
+	    foreach($entries as $entry)
+	    	$entryIds[] = $entry->getId();
+	    	
+	    $metadatas = MetadataPeer::retrieveAllByObjectIds(Metadata::TYPE_ENTRY, $entryIds);
+	    
+		$ks = $this->getKs();
+		$isAdmin = false;
+		if($ks)
+			$isAdmin = $ks->isAdmin();
+			
+	    $newList = KalturaEntryMetadataArray::fromEntryAndMetadataArray($entries, $isAdmin);
+		$response = new KalturaEntryMetadataListResponse();
+		$response->objects = $newList;
+		$response->totalCount = $totalCount;
+		return $response;
+	}
+    
+	/**
+	 * Get base entry with its metadata objects by entry id.
+	 * 
+	 * @action get
+	 * @param string $entryId Entry id
+	 * @return KalturaEntryMetadata The requested entry with its metadata objects
+	 */
+	function getAction($entryId, $version = -1)
+	{
+	    myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_PROPEL3;
+		
+		$entryMetadata = new KalturaEntryMetadata();
+		$entryMetadata->entry = $this->getEntry($entryId, $version);
+		$entryMetadata->metadatas = new KalturaMetadataArray();
+		
+		$metadatas = MetadataPeer::retrieveAllByObject(Metadata::TYPE_ENTRY, $entryId);
+	
+		foreach ($metadatas as $obj)
+		{
+    		$metadata = new KalturaMetadata();
+			$metadata->fromObject($obj);
+			
+			if($metadata->metadataObjectType != KalturaMetadataObjectType::ENTRY)
+				continue;
+				
+			$entryMetadata->metadatas[] = $metadata;
+		}
+		return $entryMetadata;
+	}
+}
