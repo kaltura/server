8389b8d593600f23d554e0b17597440be9b76d1c
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-26 14:53:20 +0000

    Support more per partner modules
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@51929 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/system_partner/lib/KalturaSystemPartnerConfiguration.php
+++ plugins/system_partner/lib/KalturaSystemPartnerConfiguration.php
@@ -106,4 +106,28 @@ class KalturaSystemPartnerConfiguration extends KalturaObject
 	{
 		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
 	}
+	
+	public function fromObject ( $source_object  )
+	{
+		parent::fromObject($source_object);
+		
+		if(class_exists('MetadataPlugin'))
+			$this->enableMetadata = $source_object->getPluginEnabled(MetadataPlugin::getPluginName());
+			
+		if(class_exists('AuditPlugin'))
+			$this->enableAuditTrail = $source_object->getPluginEnabled(AuditPlugin::getPluginName());
+	}
+	
+	public function toObject ( $object_to_fill = null , $props_to_skip = array() )
+	{
+		$object_to_fill = parent::toObject($object_to_fill, $props_to_skip);
+		
+		if(class_exists('MetadataPlugin'))
+			$object_to_fill->setPluginEnabled(MetadataPlugin::getPluginName(), $this->enableMetadata);
+			
+		if(class_exists('AuditPlugin'))
+			$object_to_fill->setPluginEnabled(AuditPlugin::getPluginName(), $this->enableAuditTrail);
+			
+		return $object_to_fill;
+	}
 }
