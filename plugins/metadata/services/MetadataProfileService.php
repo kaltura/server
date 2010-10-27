8d63d243aa5bb167d2379671ea80c8a2d238a878
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-27 10:07:15 +0000

    Set partner in default criteria for metadata, metadata profile, entry and file sync
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@51951 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/services/MetadataProfileService.php
+++ plugins/metadata/services/MetadataProfileService.php
@@ -11,6 +11,9 @@ class MetadataProfileService extends KalturaBaseService
 		parent::initService($partnerId, $puserId, $ksStr, $serviceName, $action);
 
 		myPartnerUtils::addPartnerToCriteria(new MetadataProfilePeer(), $this->getPartnerId(), $this->private_partner_data, $this->partnerGroup());
+		myPartnerUtils::addPartnerToCriteria(new MetadataPeer(), $this->getPartnerId(), $this->private_partner_data, $this->partnerGroup());
+		myPartnerUtils::addPartnerToCriteria(new entryPeer(), $this->getPartnerId(), $this->private_partner_data, $this->partnerGroup());
+		myPartnerUtils::addPartnerToCriteria(new FileSyncPeer(), $this->getPartnerId(), $this->private_partner_data, $this->partnerGroup());
 		
 		if(!MetadataPlugin::isAllowedPartner(kCurrentContext::$ks_partner_id))
 			throw new KalturaAPIException(KalturaErrors::SERVICE_FORBIDDEN);
