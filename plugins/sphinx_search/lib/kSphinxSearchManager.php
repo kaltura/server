045f12cb025e89288dbea2b19f18c71f0416a250
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-28 10:40:28 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52036 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/lib/kSphinxSearchManager.php
+++ plugins/sphinx_search/lib/kSphinxSearchManager.php
@@ -25,6 +25,7 @@ class kSphinxSearchManager implements
 	 */
 	public function objectChanged(BaseObject $object, array $modifiedColumns) 
 	{
+		KalturaLog::debug(get_class($object) . ' consumed');
 		if(!($object instanceof entry))
 			return;
 
