5d0d383a475ffc652ba8c112dc9e7f930fbb3755
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-28 10:19:25 +0000

    Sphinx moved to plugin
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52033 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/updatePartnerEntries2Sphinx.php
+++ scripts/utils/updatePartnerEntries2Sphinx.php
@@ -18,6 +18,7 @@ require_once(ROOT_DIR . '/infra/KAutoloader.php');
 
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "metadata", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "sphinx_search", "*"));
 KAutoloader::setClassMapFilePath('../cache/classMap.cache');
 KAutoloader::register();
 
@@ -28,6 +29,7 @@ $dbConf = kConf::getDB();
 DbManager::setConfig($dbConf);
 DbManager::initialize();
 
+$sphinx = new kSphinxSearchManager();
 
 $c = new Criteria();
 $c->add(entryPeer::PARTNER_ID, $partnerId);
@@ -36,7 +38,7 @@ $entries = entryPeer::doSelect($c);
 foreach($entries as $entry)
 {
 	usleep(100);
-	$entry->saveToSphinx(false, true);
+	$sphinx->saveToSphinx($entry, false, true);
 	echo $entry->getId() . "Saved\n";
 }
 echo "Done\n";
