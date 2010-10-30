b0a38cb7e64bcd1965a4e6cc43ce94c621c37d49
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-30 09:48:04 +0000

    Added commercial_use filter
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52072 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/model/objectfilters/partnerFilter.class.php
+++ alpha/apps/kaltura/lib/model/objectfilters/partnerFilter.class.php
@@ -18,6 +18,7 @@ class partnerFilter extends baseObjectFilter
 			"_gte_created_at",
 			"_lte_created_at",
 			"_like_partner_name-description-website-admin_name-admin_email",
+			"_eq_commercial_use",
 			) , NULL );
 
 		$this->allowed_order_fields = array ( "created_at" , "updated_at", "id", "name", "website", "admin_name", "admin_email", "status");
