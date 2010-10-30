42d2e4b7a782388b3936d274c68414776a90cc16
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-30 09:58:22 +0000

    Use getters instead of public attrs
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52075 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/plugins/KalturaAdminConsolePlugin.php
+++ infra/plugins/KalturaAdminConsolePlugin.php
@@ -5,17 +5,22 @@ abstract class KalturaAdminConsolePlugin
 	/**
 	 * @var string - keep null for top level
 	 */
-	public $rootLabel = null;
+	protected $rootLabel = null;
 	
 	/**
 	 * @var string - the action name
 	 */
-	public $action;
+	protected $action = null;
 	
 	/**
 	 * @var string - menu label
 	 */
-	public $label;
+	protected $label = null;
+	
+	/**
+	 * @var Zend_Controller_Action - the executed action
+	 */
+	private $currentAction = null;
 	
 	/**
 	 * @return string - absolute file path of the phtml template
@@ -52,9 +57,45 @@ abstract class KalturaAdminConsolePlugin
 		return $legalAccess;
 	}
 	
+	/**
+	 * @return string - keep null for top level
+	 */
+	public function getNavigationRootLabel()
+	{
+		return $this->rootLabel;
+	}
+	
+	/**
+	 * @return string - the action name
+	 */
+	public function getNavigationActionName()
+	{
+		return $this->action;
+	}
+	
+	/**
+	 * Return null to exclude from navigation
+	 * @return string - menu label
+	 */
+	public function getNavigationActionLabel()
+	{
+		return $this->label;
+	}
+	
 	public function action(Zend_Controller_Action $action)
 	{
+		$this->currentAction = $action;
 		$action->view->addBasePath($this->getTemplatePath());
 		$this->doAction($action);
 	}
+	
+    protected function _getParam($paramName, $default = null)
+    {
+        $value = $this->currentAction->getRequest()->getParam($paramName);
+        if ((null == $value) && (null !== $default)) {
+            $value = $default;
+        }
+
+        return $value;
+    }
 }
