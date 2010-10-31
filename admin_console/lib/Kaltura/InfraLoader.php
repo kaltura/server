9897256206365450d6c6a9f7f07a2c09265ed45f
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-31 09:29:48 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52086 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/lib/Kaltura/InfraLoader.php
+++ admin_console/lib/Kaltura/InfraLoader.php
@@ -7,6 +7,7 @@ class Kaltura_InfraLoader implements Zend_Loader_Autoloader_Interface
 		$infaDir = realpath('../../infra/');
 		$pluginsDir = realpath('../../plugins/');
 		
+		require_once($infaDir . DIRECTORY_SEPARATOR . 'bootstrap_base.php');
 		require_once($infaDir . DIRECTORY_SEPARATOR . 'KAutoloader.php');
 		KAutoloader::setClassPath(array($infaDir . DIRECTORY_SEPARATOR . '*'));
 		KAutoloader::addClassPath(KAutoloader::buildPath($pluginsDir, '*'));
