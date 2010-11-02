5a62dfbe2d4a7176443afee4ab586123427d4437
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-02 09:28:01 +0000

    Impplements getInterface
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52162 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/plugins/KalturaPlugin.php
+++ infra/plugins/KalturaPlugin.php
@@ -1,9 +1,9 @@
 <?php
 abstract class KalturaPlugin implements IKalturaPlugin
 {
-	public function getInstance($interface)
+	public function getInstance($intrface)
 	{
-		if($this instanceof $interface)
+		if($this instanceof $intrface)
 			return $this;
 			
 		return null;
