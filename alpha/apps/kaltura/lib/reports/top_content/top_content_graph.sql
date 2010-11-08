a82c49288cf061a43e519dab9ee80405592ed98b
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-08 11:11:37 +0000

    Support graph by user timezone
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52334 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/top_content/top_content_graph.sql
+++ alpha/apps/kaltura/lib/reports/top_content/top_content_graph.sql
@@ -1,24 +1,28 @@
 SELECT
-	date_id,
+	calc_time_shift(date_id, hour_id, time_shift) date_id, # time shifted date
 	SUM(count_plays) count_plays,
 #	AVG(distinct_plays) distinct_plays, /* Because we don't know the real number, we use avarage instead*/
 	SUM(sum_time_viewed) sum_time_viewed,
 	SUM(sum_time_viewed)/SUM(count_plays) avg_time_viewed,
 	SUM(count_loads) count_loads
 FROM 
-	dwh_aggr_events_entry ev , kalturadw.dwh_dim_entries en
+	dwh_hourly_events_entry ev , kalturadw.dwh_dim_entries en,
+    (SELECT {TIME_SHIFT} time_shift, # time shift in hours
+		{FROM_DATE_ID} start_date, # from date
+		{TO_DATE_ID} end_date # to date
+	) p
 WHERE 	
 	en.entry_id=ev.entry_id
 	AND {OBJ_ID_CLAUSE}
 	AND {SEARCH_TEXT_MATCH}
 	AND {CATEGORIES_MATCH}
 	AND ev.partner_id =  {PARTNER_ID} # PARTNER_ID
-	AND date_id BETWEEN {FROM_DATE_ID} #FROM_TIME
-			AND {TO_DATE_ID} #TO_TIME
+	AND date_id BETWEEN calc_time_shift(start_date, 0, time_shift) AND calc_time_shift(end_date, 23, time_shift)
+	AND calc_time_shift(date_id, hour_id, time_shift) between p.start_date AND p.end_date
 	AND 
 		( count_time_viewed > 0 OR
 		  count_plays > 0 OR
 		  count_loads > 0 )
-GROUP BY date_id
-ORDER BY date_id
+GROUP BY calc_time_shift(date_id, hour_id, time_shift)
+ORDER BY calc_time_shift(date_id, hour_id, time_shift)
 LIMIT 0,365  /* pagination  */
