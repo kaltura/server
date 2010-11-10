dd4f1d94861285ff184aa02061f570ebb0727d3a
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-10 07:16:46 +0000

    Added VIRUS_SCAN job type
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52426 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/enums/KalturaBatchJobType.php
+++ api_v3/lib/types/enums/KalturaBatchJobType.php
@@ -18,26 +18,22 @@ class KalturaBatchJobType extends KalturaEnum
 	const PULL = 12;
 	const REMOTE_CONVERT = 13;
 	const EXTRACT_MEDIA = 14;
-	
 	const MAIL = 15;
 	const NOTIFICATION = 16;
-	
 	const CLEANUP = 17;
 	const SCHEDULER_HELPER = 18;
-	
 	const BULKDOWNLOAD = 19;
 	const DB_CLEANUP = 20;
-	
 	const PROVISION_PROVIDE = 21;
 	const CONVERT_COLLECTION = 22;
 	const STORAGE_EXPORT = 23;
 	const PROVISION_DELETE = 24;
 	const STORAGE_DELETE = 25;
 	const EMAIL_INGESTION = 26;
-	
 	const METADATA_IMPORT = 27;
 	const METADATA_TRANSFORM = 28;
 	const FILESYNC_IMPORT = 29;
+	const VIRUS_SCAN = 30;
 	
 	const PROJECT = 1000;
 }
