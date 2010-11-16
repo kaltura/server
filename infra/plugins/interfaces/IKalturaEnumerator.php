95f276f68dfe52a05ee98778d2470b18f981cf72
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-16 18:14:00 +0000

    Functionality moved from plugin classes to dynamic enum class
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52726 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/plugins/interfaces/IKalturaEnumerator.php
+++ infra/plugins/interfaces/IKalturaEnumerator.php
@@ -1,6 +1,8 @@
 <?php
 interface IKalturaEnumerator extends IKalturaBase
 {
+	const PLUGIN_VALUE_DELIMITER = '.';
+	
 	/**
 	 * @return array<string> list of enum classes names that extend the base enum name
 	 */
