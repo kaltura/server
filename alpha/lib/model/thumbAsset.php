c716f2847b4abf7c7b94c86870f04fa71d66939b
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-16 08:00:58 +0000

    new
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52637 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/model/thumbAsset.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Subclass for representing a row from the 'flavor_asset' table, used for thumb_assets
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class thumbAsset extends flavorAsset 
+{
+	
+}
