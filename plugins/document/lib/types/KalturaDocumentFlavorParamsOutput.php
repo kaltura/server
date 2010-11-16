99b70bcd6e23b975c25ea57331be3b286047dfb3
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-16 18:16:30 +0000

    Support multiple flavor types
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52731 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/document/lib/types/KalturaDocumentFlavorParamsOutput.php
@@ -0,0 +1,12 @@
+<?php
+class KalturaDocumentFlavorParamsOutput extends KalturaFlavorParamsOutput 
+{
+	private static $map_between_objects = array
+	(
+	);
+	
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
+	}
+}
