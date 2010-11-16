c5efb8dd8e1b02be0a1833f35eba1d8ede262e17
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-16 18:12:10 +0000

    Functionality moved from plugin classes to dynamic enum class
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52722 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/IKalturaDynamicEnum.php
+++ api_v3/lib/types/IKalturaDynamicEnum.php
@@ -5,5 +5,18 @@
  */
 interface IKalturaDynamicEnum 
 {
+	/**
+	 * @return array
+	 */
 	public static function getEnumClass();
+	
+	/**
+	 * @return array
+	 */
+	public static function getAdditionalValues();
+	
+	/**
+	 * @return KalturaDynamicEnum
+	 */
+	public static function get();
 }
--- api_v3/lib/types/KalturaDynamicEnum.php
+++ api_v3/lib/types/KalturaDynamicEnum.php
@@ -5,4 +5,73 @@
  */
 abstract class KalturaDynamicEnum extends KalturaStringEnum implements IKalturaDynamicEnum
 {
+	/**
+	 * @return array
+	 */
+	public static function getAdditionalValues()
+	{
+		return array();
+	}
+	
+	/**
+	 * @return KalturaDynamicEnum
+	 */
+	public static function get()
+	{
+		return null;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getPluginName()
+	{
+		return null;
+	}
+	
+	/**
+	 * @param string $const
+	 * @return int
+	 */
+	public function coreValue($const)
+	{
+		$enumName = $this->getEnumClass();
+		$pluginName = $this->getPluginName();
+		$dynamicEnum = DynamicEnumPeer::retrieveByPluginConstant($enumName, $const, $pluginName);
+		
+		if(!$dynamicEnum)
+		{
+			$dynamicEnum = new DynamicEnum();
+			$dynamicEnum->setEnumName($enumName);
+			$dynamicEnum->setValueName($const);
+			$dynamicEnum->setPluginName($pluginName);
+			$dynamicEnum->save();
+		}
+		
+		return $dynamicEnum->getId();
+	}
+
+	/**
+	 * @param int $id
+	 * @return string
+	 */
+	public function coreToApi($id)
+	{
+		$enumName = $this->getEnumClass();
+		$pluginName = $this->getPluginName();
+		$dynamicEnum = DynamicEnumPeer::retrieveByPluginValue($enumName, $id, $pluginName);
+		if($dynamicEnum)
+			return $this->apiValue($dynamicEnum->getValueName());
+			
+		return null;
+	}
+
+	/**
+	 * @param string $const
+	 * @return string
+	 */
+	public function apiValue($const)
+	{
+		return $this->getPluginName() . IKalturaEnumerator::PLUGIN_VALUE_DELIMITER . $const; 
+	}
 }
