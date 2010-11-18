44db148821bc969fb3758921d13417aa209333b2
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-18 10:22:49 +0000

    Using object added instead of object created
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52897 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/multi_centers/lib/kMultiCentersSynchronizer.php
+++ plugins/multi_centers/lib/kMultiCentersSynchronizer.php
@@ -1,5 +1,5 @@
 <?php
-class kMultiCentersSynchronizer implements kObjectCreatedEventConsumer
+class kMultiCentersSynchronizer implements kObjectAddedEventConsumer
 {
 	public function getEntryId(FileSync $fileSync)
 	{
@@ -27,9 +27,17 @@ class kMultiCentersSynchronizer implements kObjectCreatedEventConsumer
 	 * @param BaseObject $object
 	 * @return bool true if should continue to the next consumer
 	 */
-	public function objectCreated(BaseObject $object)
+	public function objectAdded(BaseObject $object)
 	{
-		if(!($object instanceof FileSync) || $object->getStatus() != FileSync::FILE_SYNC_STATUS_PENDING || $object->getFileType() != FileSync::FILE_SYNC_FILE_TYPE_FILE)
+		if(
+			!($object instanceof FileSync) 
+			|| 
+			$object->getStatus() != FileSync::FILE_SYNC_STATUS_PENDING 
+			|| 
+			$object->getFileType() != FileSync::FILE_SYNC_FILE_TYPE_FILE 
+			|| 
+			$object->getDc() == kDataCenterMgr::getCurrentDcId()
+		)
 			return true;
 
 		$c = new Criteria();
