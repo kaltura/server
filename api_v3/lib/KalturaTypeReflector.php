c4a79ff0503302ae466d14cc5191882f4ac5aeab
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-18 09:03:55 +0000

    Support pluginable enums
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52889 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/KalturaTypeReflector.php
+++ api_v3/lib/KalturaTypeReflector.php
@@ -456,6 +456,32 @@ class KalturaTypeReflector
 				return true;
 			}
 		}
+		
+		if($this->isDynamicEnum())
+		{
+			$split = explode(IKalturaEnumerator::PLUGIN_VALUE_DELIMITER, $value, 2);
+			if(count($split) == 1)
+				return false;
+				
+			list($pluginName, $valueName) = $split;
+			
+			// TODO remove call_user_func after moving to php 5.3
+			$baseEnumName = call_user_func("$type::getEnumClass");
+	//		$baseEnumName = $type::getEnumClass();
+	 
+			$pluginInstance = KalturaPluginManager::getPluginInstance($pluginName);
+			$enums = $pluginInstance->getEnums($baseEnumName);
+			
+			foreach($enums as $enum)
+			{		
+				// TODO remove call_user_func after moving to php 5.3
+				$enumConstans = call_user_func("$enum::getAdditionalValues");
+	//			$enumConstans = $enum::getAdditionalValues();
+				if(in_array($valueName, $enumConstans))
+					return true;
+			}
+		}
+		
 		return false;
 	}
 	
