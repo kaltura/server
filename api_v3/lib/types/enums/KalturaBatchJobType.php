3118f4209e005464ae6a3399cc0c1c006a8827aa
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-19 06:24:48 +0000

    Batch job type changed to pluginable enum
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@52994 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/enums/KalturaBatchJobType.php
+++ api_v3/lib/types/enums/KalturaBatchJobType.php
@@ -5,8 +5,21 @@
  */
 class KalturaBatchJobType extends KalturaDynamicEnum implements BatchJobType
 {
+	/**
+	 * @return string
+	 */
 	public static function getEnumClass()
 	{
 		return 'BatchJobType';
+	}
+	
+	/**
+	 * @param string $const
+	 * @param string $type
+	 * @return int
+	 */
+	public static function getCoreValue($valueName, $type = __CLASS__)
+	{
+		return parent::getCoreValue($valueName, $type);
 	}
 }
