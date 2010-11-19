b6aeeff679c844fd6eed0fd81cdec84ec0dc9ae3
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-19 13:55:24 +0000

    Translate pluginable job types
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53010 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/batch/KalturaBatchJobFilterExt.php
+++ api_v3/lib/types/batch/KalturaBatchJobFilterExt.php
@@ -24,6 +24,28 @@ class KalturaBatchJobFilterExt extends KalturaBatchJobFilter
 	 * @var string
 	 */
 	public $jobTypeAndSubTypeIn; 
+	
+	public function toObject($object_to_fill = null, $props_to_skip = array())
+	{
+		$dbFilter = parent::toObject($object_to_fill, $props_to_skip);
+		
+		$jobTypeAndSubTypeIn = $this->jobTypeAndSubTypeIn;
+		if (is_null($this->jobTypeAndSubTypeIn))
+			return $dbFilter;
+			
+		$finalTypesAndSubTypes = array();
+		$arr = explode(BatchJobFilter::JOB_TYPE_AND_SUB_TYPE_MAIN_DELIMITER, $this->jobTypeAndSubTypeIn);
+		foreach($arr as $jobTypeIn)
+		{
+			list($jobType, $jobSubTypes) = explode(BatchJobFilter::JOB_TYPE_AND_SUB_TYPE_TYPE_DELIMITER, $jobTypeIn);
+			$jobType = KalturaBatchJobType::getCoreValue($jobType);
+			
+			$finalTypesAndSubTypes[] = $jobType . BatchJobFilter::JOB_TYPE_AND_SUB_TYPE_TYPE_DELIMITER . $jobSubTypes;
+		}
+		$jobTypeAndSubTypeIn = implode(BatchJobFilter::JOB_TYPE_AND_SUB_TYPE_MAIN_DELIMITER, $finalTypesAndSubTypes);
+		$dbFilter->set('_in_job_type_and_sub_type', $jobTypeAndSubTypeIn);
+			
+		return $dbFilter;
+	}
 }
 
-?>
