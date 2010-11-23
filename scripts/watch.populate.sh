05a9422b19ddcda0ec37e61a62c8c2b5a80f0849
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-23 14:35:18 +0000

    1. populateFromLog script changed its place when sphinx moved to plugin. 2. using @tokens@ where necessary
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53155 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/watch.populate.sh
+++ scripts/watch.populate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 SERVER=$1
-MAILTO="alex.bandel@kaltura.com"
+MAILTO="@ADMIN_CONSOLE_ADMIN_MAIL@"
 KP=$(pgrep -f populateFromLog.php)
 MAINT=/opt/kaltura/maintenance
 if [[ "X$KP" = "X" && ! -f $MAINT ]]
       then
           echo "populateFromLog.php `hostname` was restarted" | mail -s "populateFromLog.php script not found on `hostname`" $MAILTO
-	  cd /opt/kaltura/app/scripts/sphinx
-	  php populateFromLog.php ${SERVER} >> /var/log/kaltura_sphinx_populate.log 2>&1 &
+	  cd @APP_DIR@/plugins/sphinx_search/scripts
+	  php populateFromLog.php ${SERVER} >> @LOG_DIR@/kaltura_sphinx_populate.log 2>&1 &
       fi
 
