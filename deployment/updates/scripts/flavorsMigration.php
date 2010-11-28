494fc5b23fdb1d2c1e406125ef52bf9c4eb20848
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-28 15:31:23 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53334 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/updates/scripts/flavorsMigration.php
@@ -0,0 +1,162 @@
+<?php
+
+$dryRun = true; //TODO: change for real run
+$stopFile = dirname(__FILE__).'/stop_live_migration'; // creating this file will stop the script
+$entryLimitEachLoop = 500;
+
+//------------------------------------------------------
+
+set_time_limit(0);
+
+ini_set("memory_limit","700M");
+
+chdir(dirname(__FILE__));
+
+define('ROOT_DIR', realpath(dirname(__FILE__) . '/../../../'));
+require_once(ROOT_DIR . '/infra/bootstrap_base.php');
+require_once(ROOT_DIR . '/infra/KAutoloader.php');
+
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "*"));
+KAutoloader::setClassMapFilePath('../../cache/classMap.cache');
+KAutoloader::register();
+
+date_default_timezone_set(kConf::get("date_default_timezone")); // America/New_York
+
+KalturaLog::setLogger(new KalturaStdoutLogger());
+
+DbManager::setConfig(kConf::getDB());
+DbManager::initialize();
+
+$con = myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_PROPEL2);
+	
+$c = new Criteria();
+$c->add(assetPeer::TYPE, 0);
+$c->setLimit($entryLimitEachLoop);
+
+$assets = assetPeer::doSelect($c, $con);
+while(count($assets))
+{
+	foreach($assets as $asset)
+	{
+		switch($asset->getFormat())
+		{
+			case assetParams::CONTAINER_FORMAT_PDF:
+				$asset->setType(DocumentAssetType::get()->coreValue(DocumentAssetType::PDF));
+				break;
+				
+			case assetParams::CONTAINER_FORMAT_SWF:
+				$asset->setType(DocumentAssetType::get()->coreValue(DocumentAssetType::SWF));
+				break;
+				
+			case thumbParams::CONTAINER_FORMAT_JPG:
+				$asset->setType(assetType::THUMBNAIL);
+				break;
+				
+			default:
+				$asset->setType(assetType::FLAVOR);
+				
+				$asset->putInCustomData(flavorAsset::CUSTOM_DATA_FIELD_BITRATE, $asset->getBitrate());
+				$asset->putInCustomData(flavorAsset::CUSTOM_DATA_FIELD_FRAME_RATE, $asset->getFrameRate());
+				$asset->putInCustomData(flavorAsset::CUSTOM_DATA_FIELD_VIDEO_CODEC_ID, $asset->getVideoCodecId());
+		}
+		$asset->save();
+	}
+	assetPeer::clearInstancePool();
+	$assets = assetPeer::doSelect($c, $con);
+}
+
+$c = new Criteria();
+$c->add(assetParamsPeer::TYPE, 0);
+$c->setLimit($entryLimitEachLoop);
+
+$assetParams = assetParamsPeer::doSelect($c, $con);
+while(count($assetParams))
+{
+	foreach($assetParams as $assetParam)
+	{
+		switch($assetParam->getFormat())
+		{
+			case assetParams::CONTAINER_FORMAT_PDF:
+				$assetParam->setType(DocumentAssetType::get()->coreValue(DocumentAssetType::PDF));
+				break;
+				
+			case assetParams::CONTAINER_FORMAT_SWF:
+				$assetParam->setType(DocumentAssetType::get()->coreValue(DocumentAssetType::SWF));
+				break;
+				
+			case thumbParams::CONTAINER_FORMAT_JPG:
+				$assetParam->setType(assetType::THUMBNAIL);
+				break;
+				
+			default:
+				$assetParam->setType(assetType::FLAVOR);
+				
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_VIDEO_CODEC, $assetParam->getVideoCodec());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_VIDEO_BITRATE, $assetParam->getVideoBitrate());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_CODEC, $assetParam->getAudioCodec());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_BITRATE, $assetParam->getAudioBitrate());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_CHANNELS, $assetParam->getAudioChannels());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_SAMPLE_RATE, $assetParam->getAudioSampleRate());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_RESOLUTION, $assetParam->getAudioResolution());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_FRAME_RATE, $assetParam->getFrameRate());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_GOP_SIZE, $assetParam->getGopSize());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_TWO_PASS, $assetParam->getTwoPass());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_DEINTERLICE, $assetParam->getDeinterlice());
+				$assetParam->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_ROTATE, $assetParam->getRotate());
+		}
+		$assetParam->save();
+	}
+	assetParamsPeer::clearInstancePool();
+	$assetParams = assetParamsPeer::doSelect($c, $con);
+}
+
+
+$c = new Criteria();
+$c->add(assetParamsOutputPeer::TYPE, 0);
+$c->setLimit($entryLimitEachLoop);
+
+$assetParamsOutputs = assetParamsOutputPeer::doSelect($c, $con);
+while(count($assetParamsOutputs))
+{
+	foreach($assetParamsOutputs as $assetParamsOutput)
+	{
+		switch($assetParamsOutput->getFormat())
+		{
+			case assetParams::CONTAINER_FORMAT_PDF:
+				$assetParamsOutput->setType(DocumentAssetType::get()->coreValue(DocumentAssetType::PDF));
+				break;
+				
+			case assetParams::CONTAINER_FORMAT_SWF:
+				$assetParamsOutput->setType(DocumentAssetType::get()->coreValue(DocumentAssetType::SWF));
+				break;
+				
+			case thumbParams::CONTAINER_FORMAT_JPG:
+				$assetParamsOutput->setType(assetType::THUMBNAIL);
+				break;
+				
+			default:
+				$assetParamsOutput->setType(assetType::FLAVOR);
+				
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_VIDEO_CODEC, $assetParamsOutput->getVideoCodec());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_VIDEO_BITRATE, $assetParamsOutput->getVideoBitrate());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_CODEC, $assetParamsOutput->getAudioCodec());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_BITRATE, $assetParamsOutput->getAudioBitrate());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_CHANNELS, $assetParamsOutput->getAudioChannels());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_SAMPLE_RATE, $assetParamsOutput->getAudioSampleRate());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_AUDIO_RESOLUTION, $assetParamsOutput->getAudioResolution());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_FRAME_RATE, $assetParamsOutput->getFrameRate());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_GOP_SIZE, $assetParamsOutput->getGopSize());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_TWO_PASS, $assetParamsOutput->getTwoPass());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_DEINTERLICE, $assetParamsOutput->getDeinterlice());
+				$assetParamsOutput->putInCustomData(flavorParams::CUSTOM_DATA_FIELD_ROTATE, $assetParamsOutput->getRotate());
+		}
+		$assetParamsOutput->save();
+	}
+	assetParamsOutputPeer::clearInstancePool();
+	$assetParamsOutputs = assetParamsOutputPeer::doSelect($c, $con);
+}
+
+KalturaLog::log('Done');
+
+
