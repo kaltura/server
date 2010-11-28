c34f4ca562caa5d74d0ce7ec43f13971673ab1ac
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-28 14:12:04 +0000

    default asset type should be undefined
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53328 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2010-11-16_flavor_types.sql
+++ deployment/updates/sql/2010-11-16_flavor_types.sql
@@ -1,5 +1,5 @@
-ALTER TABLE  `flavor_asset` ADD  `type` INT NOT NULL DEFAULT  '1',
+ALTER TABLE  `flavor_asset` ADD  `type` INT NOT NULL DEFAULT  '0',
 ADD  `custom_data` TEXT NULL;
 
-ALTER TABLE  `flavor_params` ADD  `type` INT NOT NULL DEFAULT  '1';
-ALTER TABLE  `flavor_params_output` ADD  `type` INT NOT NULL DEFAULT  '1';
+ALTER TABLE  `flavor_params` ADD  `type` INT NOT NULL DEFAULT  '0';
+ALTER TABLE  `flavor_params_output` ADD  `type` INT NOT NULL DEFAULT  '0';
