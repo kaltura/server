d6c2812f2db513808d8714b0da6db67cf6dbe8f4
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-28 15:31:42 +0000

    Setters add the data to custom data object
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53335 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/flavorAsset.php
+++ alpha/lib/model/flavorAsset.php
@@ -21,6 +21,19 @@ class flavorAsset extends asset
 		$this->type = assetType::FLAVOR;
 	}
 	
+	const CUSTOM_DATA_FIELD_BITRATE = "FlavorBitrate";
+	const CUSTOM_DATA_FIELD_FRAME_RATE = "FlavorFrameRate";
+	const CUSTOM_DATA_FIELD_VIDEO_CODEC_ID = "FlavorVideoCodecId";
+	
+//	Should be uncommented after migration script executed
+//	public function getBitrate()			{return $this->getFromCustomData(flavorAsset::CUSTOM_DATA_FIELD_BITRATE);}
+//	public function getFrameRate()			{return $this->getFromCustomData(flavorAsset::CUSTOM_DATA_FIELD_FRAME_RATEF);}
+//	public function getVideoCodecId()		{return $this->getFromCustomData(flavorAsset::CUSTOM_DATA_FIELD_VIDEO_CODEC_ID);}
+	
+	public function setBitrate($v)			{$this->putInCustomData(flavorAsset::CUSTOM_DATA_FIELD_BITRATE, $v); return parent::setBitrate($v);}
+	public function setFrameRate($v)		{$this->putInCustomData(flavorAsset::CUSTOM_DATA_FIELD_FRAME_RATE, $v); return parent::setFrameRate($v);}
+	public function setVideoCodecId($v)		{$this->putInCustomData(flavorAsset::CUSTOM_DATA_FIELD_VIDEO_CODEC_ID, $v); return parent::setVideoCodecId($v);}
+	
 	public function getIsWeb()
 	{
 		return $this->hasTag(flavorParams::TAG_WEB);
