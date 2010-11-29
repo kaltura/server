158d55c57443c338c07a94890a4907df71e304aa
orip <orip@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-29 15:29:41 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53384 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ tests/thumbnailTests/convertImageProductionTest.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'convertImageTester.php';
+require_once 'bootstrap.php';
+
+/**
+ * this class tests the compatibility of a new code with the code  residinf in producion.
+ * the myconverter::convertImage function will run on a known set of image files and convert them
+ * using some parameters. the same will be done by calling to URL request (production side). after that
+ * the two outputs will be compared.
+ * @author ori
+ */
+class convertImageProductionTest
+{
+	
+	/**
+	 * arrays of input data for tests.
+	 * every element is a string file name (including or not including full path)
+	 * $sourceFiles[$i] output will be tested with respect to $outputReferenceFiles[$i]
+	 */
+	private $sourceFiles = array();				// array of different sorce files
+	private $outputReferenceFiles = array();	// array of different url requests
+	
+	/**
+	 * set up all needed parameters for the test
+	 * @param unknown_type $testsFile - test file path. the file contains the source file and
+	 * the output reference file for a specefic test
+	 * @param unknown_type $imagesDir - the directory of all the images to be tested
+	 */
+	public function setUp($testsFile, $imagesDir)
+	{
+		$fileHundler = null;
+		if (($fileHundler = fopen($testsFile, "r")) === false)
+			die ('unable to read tests file [' . $testsFile . ']');
+		fgets($fileHundler);	// discard form header line
+		while (!feof($fileHundler)) {
+			$line = fgets($fileHundler);
+			$line = explode("\t", $line);
+			$this->sourceFiles[] = $imagesDir . '/' . trim($line[0]);
+			$this->outputReferenceFiles[] = trim($line[1]);
+		}
+	}
+	
+	/**
+	 * test convertImage function (myFileConverter::convertImage)
+	 * the test is done by executing a number of different tests on different files and comparing result
+	 * to the produvtion servert results
+	 */
+	public function testConvertImage()
+	{
+		$status = null;
+		$tester = null;
+		// test all source files and compare result to output reference file
+		for ($i = 0; $i < count($this->sourceFiles); $i++) {			
+			$tester = new convertImageTester($this->sourceFiles[$i], $this->outputReferenceFiles[$i]);
+
+			// extract convertion parameters from $outputReferenceFile and update $tester for those parameters
+			$params = array();
+			$tmp = array();
+			$tmp = explode("/", $this->outputReferenceFiles[$i]);
+			// get rid of source file name and extension of file
+			for ($j = 0; $j < 8 ; $j++)
+				array_shift($tmp);
+			array_pop($tmp);				
+			$j = 0;
+			while($j < count($tmp)) {
+				$params["$tmp[$j]"] = $tmp[$j + 1];
+				$j += 2;
+			}
+			array_key_exists('width', $params) ? $tester->setWidth($params['width']) :  $tester->setWidth();
+			array_key_exists('height', $params) ? $tester->setHeight($params['height']) : $tester->setHeight();
+			array_key_exists('type', $params) ? $tester->setCropType($params['type']) : $tester->setCropType();
+			array_key_exists('bgcolor', $params) ? $tester->setBGColor($params['bgcolor']) : $tester->setBGColor();
+			array_key_exists('quality', $params) ? $tester->setQuality($params['quality']) : $tester->setQuality();
+			array_key_exists('src_x', $params) ? $tester->setSrcX($params['src_x']) : $tester->setSrcX();
+			array_key_exists('src_y', $params) ? $tester->setSrcY($params['src_y']) : $tester->setSrcY();
+			array_key_exists('src_w', $params) ? $tester->setSrcW($params['src_w']) : $tester->setSrcW();
+			array_key_exists('src_h', $params) ? $tester->setSrcH($params['src_h']) : $tester->setSrcH();	
+			
+			// excute test and assert
+			$status = $tester->execute();
+			if ($status === false)
+				echo 'unable to convert [' . $tester->getSourceFile() . '] with parameterrs: ' .
+					print_r($tester->getParams()) . PHP_EOL;
+		//	assert(true === $status);
+			
+			// download from production the converted image (thumbnail) and
+			// check if output is identical to reference output
+			$tester->downloadUrlFile();
+			$status = $tester->compareTargetReference();
+			$tester->deleteDownloadFile();
+				if ($status === false)
+					echo 'images files: ['  . $tester->getOutputReferenceFile() . '], [' .
+						$tester->getTargetFile(). '] are not identical' . PHP_EOL;
+		//	assert(true === $status);
+			echo 'convertImage test completed on file [' . $tester->getOutputReferenceFile() . ']' . PHP_EOL;
+			unset($tester);
+		}
+	}
+	
+}
+
+
+$test = new convertImageProductionTest();
+$test->setUp("convertImageProductionTests.txt", dirname(__FILE__) . '/images');
+$test->testConvertImage();
+
+?>
---
+++ tests/thumbnailTests/convertImageTest.php
@@ -0,0 +1,106 @@
+<?php
+
+require_once 'convertImageTester.php';
+require_once 'bootstrap.php';
+
+/**
+ * this class tests myFileConverter::convertImage function by running the convertImage function on a number
+ * of known image files. the output (converted image file) is then compared to the output reference file.
+ * @author ori
+ *
+ */
+class convertImageTest
+{
+	/**
+	 * arrays of input data for tests.
+	 * every element is a string file name (including or not including full path)
+	 * $sourceFiles[$i] will be tested with respect to $targetFiles[$i] and $outputReferenceFiles[$i]
+	 */
+	private $sourceFiles = array();				// array of different sorce files
+	private $outputReferenceFiles = array();	// array of different output reference files
+
+	/**
+	 * set up all needed parameters for the test
+	 * @param unknown_type $testsFile - test file path. the file contains the source file and
+	 * the output reference file for a specefic test
+	 * @param unknown_type $imagesDir - the directory of all the images to be tested
+	 */
+	public function setUp($testsFile, $imagesDir)
+	{
+		$fileHundler = null;
+		if (($fileHundler = fopen($testsFile, "r")) === false)
+			die ('unable to read tests file [' . $testsFile . ']');
+		fgets($fileHundler);	// discard form header line
+		while (!feof($fileHundler)) {
+			$line = fgets($fileHundler);
+			$line = explode("\t", $line);
+			$this->sourceFiles[] = $imagesDir . '/' . trim($line[0]);
+			if (count($line)> 1)
+				$this->outputReferenceFiles[] = $imagesDir . '/' . trim($line[1]);
+			else $this->outputReferenceFiles[] = null;			
+		}
+	}
+
+	/**
+	 * test convertImage function (myFileConverter::convertImage)
+	 * the test is done by executing a number of different tests on different files
+	 */
+	public function testConvertImage()
+	{
+		$status = null;
+		$tester = null;
+		// test all source files and compare result to output reference file
+		for ($i = 0; $i < count($this->sourceFiles); $i++) {			
+			$tester = new convertImageTester($this->sourceFiles[$i], $this->outputReferenceFiles[$i]);
+
+			// extract convertion parameters from $outputReferenceFile and update $tester for those parameters
+			if ($this->outputReferenceFiles[$i]) 
+			{
+				$params = array();
+				$tmp = array();
+				$tmp = explode("_", basename($this->outputReferenceFiles[$i]));
+				// get rid of source file name and extension of file
+				array_pop($tmp);
+				array_shift($tmp);		
+				$j = 0;
+				while($j < count($tmp)) {
+					$params["$tmp[$j]"] = $tmp[$j + 1];
+					$j += 2;
+				}
+				array_key_exists('width', $params) ? $tester->setWidth($params['width']) :  $tester->setWidth();
+				array_key_exists('height', $params) ? $tester->setHeight($params['height']) : $tester->setHeight();
+				array_key_exists('cropType', $params) ? $tester->setCropType($params['cropType']) : $tester->setCropType();
+				array_key_exists('bGColor', $params) ? $tester->setBGColor($params['bGColor']) : $tester->setBGColor();
+				array_key_exists('forceJpeg', $params) ? $tester->setForceJpeg($params['forceJpeg']) : $tester->setForceJpeg();
+				array_key_exists('quality', $params) ? $tester->setQuality($params['quality']) : $tester->setQuality();
+				array_key_exists('srcX', $params) ? $tester->setSrcX($params['srcX']) : $tester->setSrcX();
+				array_key_exists('srcY', $params) ? $tester->setSrcY($params['srcY']) : $tester->setSrcY();
+				array_key_exists('srcW', $params) ? $tester->setSrcW($params['srcW']) : $tester->setSrcW();
+				array_key_exists('srcH', $params) ? $tester->setSrcH($params['srcH']) : $tester->setSrcH();	
+			}	
+			
+			// excute test and assert 
+			$status = $tester->execute();
+			if ($status === false)
+				echo 'unable to convert [' . $tester->getSourceFile() . '] with parameterrs: ' .
+					print_r($tester->getParams());
+			assert(true === $status);
+			
+			// check if output is identical to reference output
+			$status = $tester->compareTargetReference();
+				if ($status === false)
+					echo 'images files: ['  . $tester->getOutputReferenceFile() . '], [' .
+						$tester->getTargetFile(). '] are not identical';
+			assert(true === $status);
+			echo 'convertImage test was successfull on file [' . $tester->getSourceFile() . ']' . PHP_EOL;
+			unset($tester);
+		}
+	}
+}
+
+$test = new convertImageTest();
+$test->setUp("convertImageTestsFiles.txt", dirname(__FILE__) . '/images');
+$test->testConvertImage();
+
+
+	
