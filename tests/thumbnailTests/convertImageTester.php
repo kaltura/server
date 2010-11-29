502370015906f4cf5020f4944675fc5cb0bdbbb0
orip <orip@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-11-29 15:43:08 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53388 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/thumbnailTests/convertImageTester.php
+++ tests/thumbnailTests/convertImageTester.php
@@ -170,6 +170,7 @@ class convertImageTester {
 		$retValue = null;
 		$output = null;
 		$output = system($cmd, $retValue);
+		echo "aa " . $cmd . " aa"; 
 		$compareResult = file_get_contents('resultLog.txt');
 		echo 'score is: ' .$compareResult;
 		unlink($tmpFile);			// delete tmp comparing file (used to copmpare the two image files)
