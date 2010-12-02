4f434ba924db148ae647807a8f116c8991225d9e
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-02 03:55:37 +0000

    add - getflavorParams & getflavorParamsOutputs
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53548 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/flavorAsset.php
+++ alpha/lib/model/flavorAsset.php
@@ -20,7 +20,82 @@ class flavorAsset extends asset
 		parent::applyDefaultValues();
 		$this->setType(assetType::FLAVOR);
 	}
-	
+
+	/**
+	 * Gets an array of assetParamsOutput objects which contain a foreign key that references this object.
+	 *
+	 * If this collection has already been initialized with an identical Criteria, it returns the collection.
+	 * Otherwise if this asset has previously been saved, it will retrieve
+	 * related assetParamsOutputs from storage. If this asset is new, it will return
+	 * an empty collection or the current collection, the criteria is ignored on a new object.
+	 *
+	 * @param      PropelPDO $con
+	 * @param      Criteria $criteria
+	 * @return     array flavorParamsOutput[]
+	 * @throws     PropelException
+	 */
+	public function getflavorParamsOutputs($criteria = null, PropelPDO $con = null)
+	{
+		if ($criteria === null) {
+			$criteria = new Criteria(assetPeer::DATABASE_NAME);
+		}
+		elseif ($criteria instanceof Criteria)
+		{
+			$criteria = clone $criteria;
+		}
+
+		if ($this->collassetParamsOutputs === null) {
+			if ($this->isNew()) {
+			   $this->collassetParamsOutputs = array();
+			} else {
+
+				$criteria->add(flavorParamsOutputPeer::FLAVOR_ASSET_ID, $this->id);
+
+				flavorParamsOutputPeer::addSelectColumns($criteria);
+				$this->collassetParamsOutputs = flavorParamsOutputPeer::doSelect($criteria, $con);
+			}
+		} else {
+			// criteria has no effect for a new object
+			if (!$this->isNew()) {
+				// the following code is to determine if a new query is
+				// called for.  If the criteria is the same as the last
+				// one, just return the collection.
+
+
+				$criteria->add(flavorParamsOutputPeer::FLAVOR_ASSET_ID, $this->id);
+
+				flavorParamsOutputPeer::addSelectColumns($criteria);
+				if (!isset($this->lastassetParamsOutputCriteria) || !$this->lastassetParamsOutputCriteria->equals($criteria)) {
+					$this->collassetParamsOutputs = flavorParamsOutputPeer::doSelect($criteria, $con);
+				}
+			}
+		}
+		$this->lastassetParamsOutputCriteria = $criteria;
+		return $this->collassetParamsOutputs;
+	}
+
+	/**
+	 * Get the associated assetParams object
+	 *
+	 * @param      PropelPDO Optional Connection object.
+	 * @return     assetParams The associated assetParams object.
+	 * @throws     PropelException
+	 */
+	public function getFlavorParams(PropelPDO $con = null)
+	{
+		if ($this->aassetParams === null && ($this->flavor_params_id !== null)) {
+			$this->aassetParams = flavorParamsPeer::retrieveByPk($this->flavor_params_id);
+			/* The following can be used additionally to
+			   guarantee the related object contains a reference
+			   to this object.  This level of coupling may, however, be
+			   undesirable since it could result in an only partially populated collection
+			   in the referenced object.
+			   $this->aassetParams->addassets($this);
+			 */
+		}
+		return $this->aassetParams;
+	}
+
 	const CUSTOM_DATA_FIELD_BITRATE = "FlavorBitrate";
 	const CUSTOM_DATA_FIELD_FRAME_RATE = "FlavorFrameRate";
 	const CUSTOM_DATA_FIELD_VIDEO_CODEC_ID = "FlavorVideoCodecId";
