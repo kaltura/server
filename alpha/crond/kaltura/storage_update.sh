33158a80ec5ed76183c8c676efd021c203a0802d
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-02 12:25:38 +0000

    storage_update merged in alpha/crond/kaltura/storage_update.sh unused lines were removed
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53579 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/storage_update.sh
+++ alpha/crond/kaltura/storage_update.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-cd /opt/kaltura/logs
+cd @LOG_DIR@
 
 WHEN=$(date -d "yesterday" +%Y-%m-%d)
 
-php /opt/kaltura/app/support_prod/test/dummy/findEntriesSizes.php $WHEN >> /var/log/`hostname`-findEntriesSizes.log 2>&1
-#not in use any more
-##php /opt/kaltura/app/support_prod/test/dummy/findMediaStats.php $WHEN >> /var/log/`hostname`-findMediaStats.log 2>&1
-#not ready yet
-#php /web/kaltura/alpha/test/dummy/findUserStats.php $WHEN
-#php /web/kaltura/alpha/batch/batchPartnerUsage.php >> /web/logs/BATCH1-batchPartnerUsage.log
-TODAY=$(date +%F)
-#php /web/kaltura/alpha/batch/newBatchPartnerUsage.php daily_storage $TODAY >> /web/logs/BATCH1-newBatchPartnerUsage.log
-#php /web/kaltura/alpha/batch/newBatchPartnerUsage.php monthly_agg $TODAY >> /web/logs/BATCH1-newBatchPartnerUsageMonthly.log
-# monitor partner usage:
-php /opt/kaltura/app/alpha/batch/batchPartnerUsage.php >> /var/log/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
+php @APP_DIR@/scripts/findEntriesSizes.php $WHEN >> @LOG_DIR@/`hostname`-findEntriesSizes.log
+php @APP_DIR@/alpha/batch/batchPartnerUsage.php >> @LOG_DIR@/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
--- scripts/storage_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd @LOG_DIR@
-
-WHEN=$(date -d "yesterday" +%Y-%m-%d)
-
-php @APP_DIR@/scripts/findEntriesSizes.php $WHEN >> @LOG_DIR@/`hostname`-findEntriesSizes.log
-
