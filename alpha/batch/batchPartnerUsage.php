a69b89b851c671579be7579ecb42196466796cdb
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-06 15:17:10 +0000

    moved from saas to core
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53699 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/batch/batchPartnerUsage.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+<?php
+/*
+ * Created on Apr 18, 2008
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+require_once(realpath(dirname(__FILE__)).'/../config/sfrootdir.php');
+define('SF_APP',         'kaltura');
+define('SF_ENVIRONMENT', 'batch');
+define('SF_DEBUG',       true);
+
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'lib/batch/myBatchPartnerUsage.class.php');
+
+ini_set('memory_limit', '1024M');
+kCurrentContext::$ps_vesion == 'ps2';
+$batchClient = new myBatchPartnerUsage();
+
+?>
