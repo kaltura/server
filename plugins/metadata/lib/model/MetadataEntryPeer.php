337f83544bd9ee2f53653daab04016c1699c5a35
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-06 14:50:53 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53690 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/lib/model/MetadataEntryPeer.php
+++ plugins/metadata/lib/model/MetadataEntryPeer.php
@@ -1,45 +1,4 @@
 <?php
 class MetadataEntryPeer extends entryPeer implements iMetadataPeer
 {
-	/*
-	public static function saveToSphinx($objectId, array $data)
-	{
-		$entry = self::retrieveByPK($objectId);
-		if(!$entry)
-		{
-			KalturaLog::err("Entry [$objectId] not found");
-			return;
-		}
-			
-		// TODO - remove after solving the replace bug that removes all fields
-		$entry->saveToSphinx(false, true);
-		return;
-		
-		$pluginsData = $entry->getPluginData();
-		$sphinxPluginsData = array();
-		if($pluginsData && is_array($pluginsData))
-		{
-			foreach($pluginsData as $pluginName => $pluginData)
-			{
-				$sphinxPluginData = $entry->array2sphinxData($pluginsData, $pluginName);
-				if($sphinxPluginData)
-					$sphinxPluginsData[] = $sphinxPluginData;
-			}
-		}
-		
-		$sphinxPluginData = $entry->array2sphinxData($data, MetadataPlugin::PLUGIN_NAME);
-		if($sphinxPluginData)
-			$sphinxPluginsData[] = $sphinxPluginData;
-				
-		if(count($sphinxPluginsData))
-		{
-			$value = implode(',', $sphinxPluginsData);
-			$entry->updateSphinx('plugins_data', $value);
-		}
-		else
-		{
-			KalturaLog::debug("No plugins data should be saved to the entry [$objectId]");
-		}
-	}
-	*/
 }
