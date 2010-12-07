4e5e4e52a8fcb66454b5fc05bfb04f50c440bafc
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-07 16:37:36 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53765 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/interfaces/IIndexable.php
@@ -0,0 +1,45 @@
+<?php
+interface IIndexable
+{
+	const FIELD_TYPE_STRING = 'string';
+	const FIELD_TYPE_INTEGER = 'int';
+	const FIELD_TYPE_DATETIME = 'datetime';
+	
+	/**
+	 * Is the id as used and know by Kaltura
+	 * @return string
+	 */
+	public function getId();
+	
+	/**
+	 * Is the id as used and know by the indexing server
+	 * @return int
+	 */
+	public function getIntId();
+	
+	/**
+	 * @return int
+	 */
+	public function getPartnerId();
+	
+	/**
+	 * @return string
+	 */
+	public function getEntryId();
+	
+	/**
+	 * @return string object propel name
+	 */
+	public function getObjectIndexName();
+	
+	/**
+	 * @return array while the key is the index attribute name and the value is the getter name
+	 * For example array('name' => 'title') if name should be retrieved by getTitle() getter
+	 */
+	public function getIndexFieldsMap();
+	
+	/**
+	 * @return string field type, string, int or timestamp
+	 */
+	public function getIndexFieldType($field);
+}
