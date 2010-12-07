d8a64b24bc1a9c318c66a743aa918b95339eeebd
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-07 16:28:31 +0000

    Uses many criteria plugins
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53763 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/kConf.php
+++ alpha/config/kConf.php
@@ -127,8 +127,6 @@ class kConf extends kConfLocal
 					"StorageProfilePlugin", // Should always be enabled
 				),
 				
-                "search_plugin"	=> "sphinxSearch",
-											
 				"event_consumers" => array(
                 	"kFlowManager",
                 	"kStorageExporter",
