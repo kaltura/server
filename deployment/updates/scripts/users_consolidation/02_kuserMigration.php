836ba6dffada0419b08f7b345491792ac15bbdc9
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-09 23:45:25 +0000

    case sesitive path names
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53886 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/users_consolidation/02_kuserMigration.php
+++ deployment/updates/scripts/users_consolidation/02_kuserMigration.php
@@ -18,7 +18,7 @@ require_once(ROOT_DIR . '/infra/KAutoloader.php');
 
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "*"));
-KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "admin_console", "lib", "kaltura"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "admin_console", "lib", "Kaltura"));
 KAutoloader::setClassMapFilePath('../../../cache/classMap.cache');
 KAutoloader::register();
 
