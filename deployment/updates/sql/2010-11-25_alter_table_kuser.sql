d8c371c39ca56328f8fb1536b0156c9175ccb469
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-12 13:18:56 +0000

    added is_admin index to kuser table
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53937 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2010-11-25_alter_table_kuser.sql
+++ deployment/updates/sql/2010-11-25_alter_table_kuser.sql
@@ -4,4 +4,5 @@ ADD `first_name` VARCHAR(40) AFTER `screen_name`,
 ADD `last_name` VARCHAR(40) AFTER `first_name`,
 ADD `is_admin` TINYINT AFTER `login_data_id`,
 ADD `custom_data` TEXT,
-ADD KEY `login_data_id_index`(`login_data_id`);
+ADD KEY `login_data_id_index`(`login_data_id`),
+ADD KEY `is_admin_index`(`is_admin`);
