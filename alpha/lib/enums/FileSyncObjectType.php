61b0e070845f306a7fe57ca9cd4f028ceceeb6b1
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-12 09:02:10 +0000

    File sync object type is pluginable enum
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@53906 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/enums/FileSyncObjectType.php
@@ -0,0 +1,10 @@
+<?php
+interface FileSyncObjectType extends BaseEnum
+{
+	const ENTRY = 1;
+	const UICONF = 2;
+	const BATCHJOB = 3;
+	const FLAVOR_ASSET = 4;
+	const METADATA = 5;
+	const METADATA_PROFILE = 6;
+}
