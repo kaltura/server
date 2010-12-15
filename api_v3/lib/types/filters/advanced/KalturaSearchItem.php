2731b66abff551ad1aeced278a8a6495b9e8beff
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-15 10:30:27 +0000

    Return null if no object supplied
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54077 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/filters/advanced/KalturaSearchItem.php
+++ api_v3/lib/types/filters/advanced/KalturaSearchItem.php
@@ -8,6 +8,9 @@ class KalturaSearchItem extends KalturaObject
 {
 	public function toObject ( $object_to_fill = null , $props_to_skip = array() )
 	{
+		if(is_null($object_to_fill))
+			return null;
+			
 		$object_to_fill = parent::toObject($object_to_fill, $props_to_skip);
 		$object_to_fill->setKalturaClass(get_class($this));
 		
