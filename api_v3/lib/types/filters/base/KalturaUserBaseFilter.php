413804f57bdfe4005c96e614a128de3c3887ca7e
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-16 17:37:56 +0000

    Auto generated
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54178 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/filters/base/KalturaUserBaseFilter.php
+++ api_v3/lib/types/filters/base/KalturaUserBaseFilter.php
@@ -8,8 +8,6 @@ class KalturaUserBaseFilter extends KalturaFilter
 {
 	private $map_between_objects = array
 	(
-		"idEqual" => "_eq_id",
-		"idIn" => "_in_id",
 		"partnerIdEqual" => "_eq_partner_id",
 		"screenNameLike" => "_like_screen_name",
 		"screenNameStartsWith" => "_likex_screen_name",
@@ -17,8 +15,6 @@ class KalturaUserBaseFilter extends KalturaFilter
 		"emailStartsWith" => "_likex_email",
 		"tagsMultiLikeOr" => "_mlikeor_tags",
 		"tagsMultiLikeAnd" => "_mlikeand_tags",
-		"statusEqual" => "_eq_status",
-		"statusIn" => "_in_status",
 		"createdAtGreaterThanOrEqual" => "_gte_created_at",
 		"createdAtLessThanOrEqual" => "_lte_created_at",
 		"isAdminEqual" => "_eq_is_admin",
@@ -39,20 +35,6 @@ class KalturaUserBaseFilter extends KalturaFilter
 	{
 		return array_merge(parent::getOrderByMap(), $this->order_by_map);
 	}
-
-	/**
-	 * 
-	 * 
-	 * @var string
-	 */
-	public $idEqual;
-
-	/**
-	 * 
-	 * 
-	 * @var string
-	 */
-	public $idIn;
 
 	/**
 	 * 
@@ -102,20 +84,6 @@ class KalturaUserBaseFilter extends KalturaFilter
 	 * @var string
 	 */
 	public $tagsMultiLikeAnd;
-
-	/**
-	 * 
-	 * 
-	 * @var KalturaUserStatus
-	 */
-	public $statusEqual;
-
-	/**
-	 * 
-	 * 
-	 * @var string
-	 */
-	public $statusIn;
 
 	/**
 	 * 
