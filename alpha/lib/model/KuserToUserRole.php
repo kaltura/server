01ac9c159e0ac19f2062ed3b63ba348a21f42442
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-16 17:46:06 +0000

    Auto generated
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54182 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/model/KuserToUserRole.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Skeleton subclass for representing a row from the 'kuser_to_user_role' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    lib.model
+ */
+class KuserToUserRole extends BaseKuserToUserRole {
+
+} // KuserToUserRole
---
+++ alpha/lib/model/PermissionToPermissionItemPeer.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'permission_to_permission_item' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    lib.model
+ */
+class PermissionToPermissionItemPeer extends BasePermissionToPermissionItemPeer {
+
+} // PermissionToPermissionItemPeer
