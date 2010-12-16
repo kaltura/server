33d0951d8303bf4c4361a50d682e01e0a5047282
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-16 10:02:53 +0000

    Updated schema with R&P changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54145 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/data/sql/schema.sql
+++ alpha/data/sql/schema.sql
@@ -1864,5 +1864,123 @@ CREATE TABLE `user_login_data`
 	KEY `login_email_index`(`login_email`)
 )Type=MyISAM;
 
+#-----------------------------------------------------------------------------
+#-- permission
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `permission`;
+
+
+CREATE TABLE `permission`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`type` INTEGER  NOT NULL,
+	`name` VARCHAR(100)  NOT NULL,
+	`friendly_name` VARCHAR(100),
+	`description` TEXT,
+	`partner_id` INTEGER  NOT NULL,
+	`status` INTEGER  NOT NULL,
+	`depends_on_permission_names` TEXT,
+	`tags` TEXT,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	`custom_data` TEXT,
+	PRIMARY KEY (`name`),
+	KEY `partner_id_index`(`partner_id`),
+	KEY `id_index`(`id`)
+)Type=MyISAM;
+
+#-----------------------------------------------------------------------------
+#-- user_role
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_role`;
+
+
+CREATE TABLE `user_role`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100)  NOT NULL,
+	`description` TEXT,
+	`partner_id` INTEGER  NOT NULL,
+	`status` INTEGER  NOT NULL,
+	`permission_names` TEXT,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	`custom_data` TEXT,
+	PRIMARY KEY (`id`),
+	KEY `partner_id_index`(`partner_id`)
+)Type=MyISAM;
+
+#-----------------------------------------------------------------------------
+#-- permission_item
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `permission_item`;
+
+
+CREATE TABLE `permission_item`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`type` VARCHAR(100)  NOT NULL,
+	`value` TEXT  NOT NULL,
+	`tags` TEXT,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	`custom_data` TEXT,
+	PRIMARY KEY (`type`),
+	INDEX `I_referenced_permission_to_permission_item_FK_2_1` (`id`)
+)Type=MyISAM;
+
+#-----------------------------------------------------------------------------
+#-- permission_to_permission_item
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `permission_to_permission_item`;
+
+
+CREATE TABLE `permission_to_permission_item`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`permission_name` VARCHAR(100)  NOT NULL,
+	`permission_item_id` INTEGER  NOT NULL,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	PRIMARY KEY (`id`),
+	INDEX `permission_to_permission_item_FI_1` (`permission_name`),
+	CONSTRAINT `permission_to_permission_item_FK_1`
+		FOREIGN KEY (`permission_name`)
+		REFERENCES `permission` (`name`),
+	INDEX `permission_to_permission_item_FI_2` (`permission_item_id`),
+	CONSTRAINT `permission_to_permission_item_FK_2`
+		FOREIGN KEY (`permission_item_id`)
+		REFERENCES `permission_item` (`id`)
+)Type=MyISAM;
+
+#-----------------------------------------------------------------------------
+#-- kuser_to_user_role
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `kuser_to_user_role`;
+
+
+CREATE TABLE `kuser_to_user_role`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`kuser_id` INTEGER  NOT NULL,
+	`user_role_id` INTEGER  NOT NULL,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	PRIMARY KEY (`id`),
+	INDEX `kuser_to_user_role_FI_1` (`kuser_id`),
+	CONSTRAINT `kuser_to_user_role_FK_1`
+		FOREIGN KEY (`kuser_id`)
+		REFERENCES `kuser` (`id`),
+	INDEX `kuser_to_user_role_FI_2` (`user_role_id`),
+	CONSTRAINT `kuser_to_user_role_FK_2`
+		FOREIGN KEY (`user_role_id`)
+		REFERENCES `user_role` (`id`)
+)Type=MyISAM;
+
 # This restores the fkey checks, after having unset them earlier
 SET FOREIGN_KEY_CHECKS = 1;
