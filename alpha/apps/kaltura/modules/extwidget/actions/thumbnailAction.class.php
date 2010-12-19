7d20bd271a6a163f769e49e446da2d595213dab6
orip <orip@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-19 10:51:00 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54238 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
@@ -283,8 +283,8 @@ class thumbnailAction extends sfAction
 		{
 			try
 			{
-			$tempThumbPath = myEntryUtils::resizeEntryImage( $entry, $version , $width , $height , $type , $bgcolor , $crop_provider, $quality,
-			$src_x, $src_y, $src_w, $src_h, $vid_sec, $vid_slice, $vid_slices  );
+				$tempThumbPath = myEntryUtils::resizeEntryImage( $entry, $version , $width , $height , $type , $bgcolor , $crop_provider, $quality,
+				$src_x, $src_y, $src_w, $src_h, $vid_sec, $vid_slice, $vid_slices  );
 			}
 			catch(Exception $ex)
 			{
