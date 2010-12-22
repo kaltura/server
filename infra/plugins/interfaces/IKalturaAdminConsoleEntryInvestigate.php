1723361f44dde13302add8ec631a987184861bdc
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-22 18:39:37 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54541 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ infra/plugins/interfaces/IKalturaAdminConsoleEntryInvestigate.php
@@ -0,0 +1,7 @@
+<?php
+interface IKalturaAdminConsoleEntryInvestigate extends IKalturaBase
+{
+	// TODO	
+	// called by the admin console to add data to the investigate screen
+	// should add metadata, content distribution and track entry
+}
