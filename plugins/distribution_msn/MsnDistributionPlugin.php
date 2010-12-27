295ffcc76516df64651b70c55b96cc625c1b942a
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-27 08:23:51 +0000

    Load additional distributor MRSS data
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54785 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/distribution_msn/MsnDistributionPlugin.php
+++ plugins/distribution_msn/MsnDistributionPlugin.php
@@ -204,7 +204,18 @@ class MsnDistributionPlugin extends KalturaPlugin implements IKalturaPermissions
 	 */
 	public static function contibuteMRSS(EntryDistribution $entryDistribution, SimpleXMLElement $mrss)
 	{
+		// append MSN specific report statistics
+		$status = $mrss->addChild('status');
+		// TODO add attribute with date of last report
 		
+		$status->addChild('emailed', $entryDistribution->getFromCustomData('emailed'));
+		$status->addChild('rated', $entryDistribution->getFromCustomData('rated'));
+		$status->addChild('blogged', $entryDistribution->getFromCustomData('blogged'));
+		$status->addChild('reviewed', $entryDistribution->getFromCustomData('reviewed'));
+		$status->addChild('bookmarked', $entryDistribution->getFromCustomData('bookmarked'));
+		$status->addChild('playbackFailed', $entryDistribution->getFromCustomData('playbackFailed'));
+		$status->addChild('timeSpent', $entryDistribution->getFromCustomData('timeSpent'));
+		$status->addChild('recommended', $entryDistribution->getFromCustomData('recommended'));
 	}
 
 	/**
