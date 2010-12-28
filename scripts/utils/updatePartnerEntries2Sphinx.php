ebc16ac3b68b5d1fb8daa8236694ef124f5fd156
orip <orip@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-12-28 08:06:11 +0000

    the script was modified a bit.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@54860 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/updatePartnerEntries2Sphinx.php
+++ scripts/utils/updatePartnerEntries2Sphinx.php
@@ -1,29 +1,36 @@
 <?php
+ini_set("memory_limit","256M");
 
-if($argc != 2)
-{
-	echo "Arguments missing.\n\n";
-	echo "Usage: php updatePartnerEntries2Sphinx.php {partner id}\n";
-	exit;
-} 
-$partnerId = $argv[1];
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/../../alpha/'));
+define('SF_APP',         'kaltura');
+define('SF_ENVIRONMENT', 'batch');
+define('SF_DEBUG',       true);
 
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+require_once(SF_ROOT_DIR.'/../infra/bootstrap_base.php');
+require_once(KALTURA_INFRA_PATH.DIRECTORY_SEPARATOR."KAutoloader.php");
 
-
-ini_set("memory_limit","1024M");
-
-define('ROOT_DIR', realpath(dirname(__FILE__) . '/../../'));
-require_once(ROOT_DIR . '/infra/bootstrap_base.php');
-require_once(ROOT_DIR . '/infra/KAutoloader.php');
-
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "api_v3", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "batch", "mediaInfoParser", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "metadata", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "sphinx_search", "*"));
-KAutoloader::setClassMapFilePath('../cache/classMap.cache');
+KAutoloader::setClassMapFilePath('./logs/classMap.cache');
 KAutoloader::register();
 
-error_reporting(E_ALL);
-KalturaLog::setLogger(new KalturaStdoutLogger());
+error_reporting ( E_ALL );
+
+$dbConf = kConf::getDB ();
+DbManager::setConfig ( $dbConf );
+DbManager::initialize ();
+
+if ($argc !== 2)
+{
+	die('pleas provide partner id as input' . PHP_EOL . 
+		'to run script: ' . basename(__FILE__) . ' X' . PHP_EOL . 
+		'whereas X is partner id' . PHP_EOL);
+}
+$partnerId = @$argv[1];
 
 $dbConf = kConf::getDB();
 DbManager::setConfig($dbConf);
