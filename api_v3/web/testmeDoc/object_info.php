4f02f1bf3f1c2c5e37aae725cc62a52a93116cab
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-04 11:33:28 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55202 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/web/testmeDoc/object_info.php
+++ api_v3/web/testmeDoc/object_info.php
@@ -38,87 +38,54 @@ else
 		<td class="description" colspan="3"><?php echo formatDescription($typeReflector->getDescription()); ?></td>
 	</tr>
 	<?php if ($typeReflector->isArray()): ?>
-	<tr>
-		<td colspan="4">Array of type <a href="?object=<?php echo $typeReflector->getArrayType(); ?>"><?php echo $typeReflector->getArrayType(); ?></a></td>
-	</tr>
+		<tr>
+			<td colspan="4">Array of type <a href="?object=<?php echo $typeReflector->getArrayType(); ?>"><?php echo $typeReflector->getArrayType(); ?></a></td>
+		</tr>
 	<?php else: ?>
-	<tr>
-		<td colspan="4" class="title">
+		<tr>
+			<td colspan="4" class="title">
+				<?php if ($typeReflector->isEnum() || $typeReflector->isStringEnum()): ?>
+					Enumerations
+				<?php else: ?>
+					Properties
+				<?php endif; ?>
+			</td>
+		</tr>
+		<tr>
+			<th class="subtitle">Name</th>
+			<th class="subtitle">Type</th>
 			<?php if ($typeReflector->isEnum() || $typeReflector->isStringEnum()): ?>
-				Enumerations
-			<?php else: ?>
-				Properties
+			<th class="subtitle">Value</th>
 			<?php endif; ?>
-		</td>
-	</tr>
-	<tr>
-		<th class="subtitle">Name</th>
-		<th class="subtitle">Type</th>
-		<?php if ($typeReflector->isEnum() || $typeReflector->isStringEnum()): ?>
-		<th class="subtitle">Value</th>
-		<?php endif; ?>
-		<th class="subtitle">Description</th>
-	</tr>
-	<?php $odd = true; ?>
-	<?php foreach($properties as $property): ?>
-	<?php $odd = !$odd;?>
-	<tr<?php echo ($odd) ? " class=\"odd\"" : ""; ?>>
-		<td><?php echo $property->getName(); ?></td>
-		<td>
-			<?php if ($property->isComplexType()): ?>
-				<a href="?object=<?php echo $property->getType(); ?>"><?php echo $property->getType();?></a>
-			<?php else: ?>
-				<?php echo $property->getType(); ?>
+			<th class="subtitle">Description</th>
+		</tr>
+		<?php $odd = true; ?>
+		<?php foreach($properties as $property): ?>
+		<?php $odd = !$odd;?>
+		<tr<?php echo ($odd) ? " class=\"odd\"" : ""; ?>>
+			<td><?php echo $property->getName(); ?></td>
+			<td>
+				<?php if ($property->isComplexType()): ?>
+					<a href="?object=<?php echo $property->getType(); ?>"><?php echo $property->getType();?></a>
+				<?php else: ?>
+					<?php echo $property->getType(); ?>
+				<?php endif; ?>
+			</td>
+			<?php if ($typeReflector->isEnum() || $typeReflector->isStringEnum()): ?>
+			<td>
+				<?php echo $property->getDefaultValue(); ?>
+			</td>
 			<?php endif; ?>
-		</td>
-		<?php if ($typeReflector->isEnum() || $typeReflector->isStringEnum()): ?>
-		<td>
-			<?php echo $property->getDefaultValue(); ?>
-		</td>
-		<?php endif; ?>
-		<td>
-			<?php if ($property->getName() == "orderBy"): /* hack for filters order by */ ?>
-				<?php $filterEnumOrder = str_replace("Filter", "OrderBy", $object); ?>
-				This parameter sets the order criteria by which objects will be retrieved. This parameter should by set according to the following enumeration: <a href="?object=<?php echo $filterEnumOrder; ?>"><?php echo $filterEnumOrder; ?></a>.
-			<?php else: ?>
-				
-				<?php echo formatDescription($property->getDescription()); ?>
-			<?php endif; ?>
-		</td>
-	</tr>
-	<?php endforeach; ?>
-	<?php 
-		if ($typeReflector->isDynamicEnum())
-		{
-			$type = $typeReflector->getType();
-			// TODO remove call_user_func after moving to php 5.3
-			$baseEnumName = call_user_func("$type::getEnumClass");
-//			$baseEnumName = $type::getEnumClass();
-			$pluginInstances = KalturaPluginManager::getPluginInstances('IKalturaEnumerator');
-			foreach($pluginInstances as $pluginInstance)
-			{
-				$enums = $pluginInstance->getEnums($baseEnumName);
-				foreach($enums as $enum)
-				{
-					// TODO remove call_user_func after moving to php 5.3
-					$enumConstans = call_user_func("$enum::getAdditionalValues");
-//					$enumConstans = $enum::getAdditionalValues();
-					foreach($enumConstans as $name => $value)
-					{
-						$odd = !$odd;
-						$value = $pluginInstance->getApiValue($value);
-						?>
-							<tr<?php echo ($odd) ? " class=\"odd\"" : ""; ?>>
-								<td><?php echo $name; ?></td>
-								<td>string</td>
-								<td><?php echo $value; ?></td>
-								<td></td>
-							</tr>
-						<?php
-					}
-				}
-			}
-		}
-	?>
+			<td>
+				<?php if ($property->getName() == "orderBy"): /* hack for filters order by */ ?>
+					<?php $filterEnumOrder = str_replace("Filter", "OrderBy", $object); ?>
+					This parameter sets the order criteria by which objects will be retrieved. This parameter should by set according to the following enumeration: <a href="?object=<?php echo $filterEnumOrder; ?>"><?php echo $filterEnumOrder; ?></a>.
+				<?php else: ?>
+					
+					<?php echo formatDescription($property->getDescription()); ?>
+				<?php endif; ?>
+			</td>
+		</tr>
+		<?php endforeach; ?>
 	<?php endif; ?>
 </table>
