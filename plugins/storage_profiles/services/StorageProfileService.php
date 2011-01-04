7c8a3f9bcf6934cf0b308d607755ad0bbd4a3292
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-04 13:16:25 +0000

    Should use the KSpartner id
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55214 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/storage_profiles/services/StorageProfileService.php
+++ plugins/storage_profiles/services/StorageProfileService.php
@@ -12,7 +12,7 @@ class StorageProfileService extends KalturaBaseService
 
 		// since plugin might be using KS impersonation, we need to validate the requesting
 		// partnerId from the KS and not with the $_POST one
-		if(!StorageProfilePlugin::isAllowedPartner($this->getPartnerId()))
+		if(!StorageProfilePlugin::isAllowedPartner($partnerId))
 			throw new KalturaAPIException(KalturaErrors::SERVICE_FORBIDDEN);
 	}
 	
