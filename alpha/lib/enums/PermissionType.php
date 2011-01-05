97eafa22e0c68bf759df34cc666a0c8dc98bb61f
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-05 09:46:12 +0000

    added partner_group permission type
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55285 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/enums/PermissionType.php
+++ alpha/lib/enums/PermissionType.php
@@ -4,5 +4,6 @@ interface PermissionType extends BaseEnum
 	const API_ACCESS       = '1';
 	const SPECIAL_FEATURE  = '2';
 	const PLUGIN           = '3';
+	const PARTNER_GROUP    = '4';
 	const EXTERNAL         = '99';
 }
