f209401422314ba9e5d9a341b8bf8af0b607f9a8
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-05 12:07:54 +0000

    added new preview page for html5 player
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55307 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/apps/kaltura/modules/kmc/actions/previewAction.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once ( "kalturaAction.class.php" );
+
+class previewAction extends kalturaAction
+{
+	public function execute ( ) 
+	{
+		//sfView::SUCCESS;
+	}
+}
+?>
---
+++ alpha/apps/kaltura/modules/kmc/templates/previewSuccess.php
@@ -0,0 +1,64 @@
+﻿<?php 
+if( ! $sf_params->has('p') || $sf_params->has['e'] ) { 
+	die('Missing entry id or partner id');	
+}
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kaltura HTML5 Player</title>
+</head>
+<body>
+	<div id="main">
+
+		<div class="content">
+			<div class="title">
+				<h2>Kaltura HTML5 Player</h2>
+			</div>
+			<div class="contwrap">
+			
+			<div style="width:300px;">
+				<span style="font-size: 16px;">Entry id:</span> <input style="width: 100px;" id="kentryid" value="<?php echo $sf_params->get('e');?>" /> 
+				<input style="width: 100px; font-size: 12px;" id="showVideo" type="button" value="Change entry" disabled="true">
+			</div><br />
+
+			<div id="videoContainer" style="width:400px">
+				
+			</div>
+
+			</div><!-- end contwrap -->
+		</div><!-- end content -->
+	</div><!-- end #main -->
+
+<!-- <script type="text/javascript" src="http://html5.kaltura.org/js"></script> -->
+<script src="/lib/js/html5lib/mwEmbedLoader.js"></script> 
+<script type="text/javascript">
+var partnerId = <?php echo $sf_params->get('p'); ?>
+
+mw.ready( function(){
+
+	mw.setConfig( 'EmbedPlayer.OverlayControls', false );
+	
+	$j('#showVideo').attr('disabled', null);
+	
+	$j('#showVideo').click( function(){		
+		var entryId =  $j( '#kentryid' ).val();
+		kalturaIframeEmbed('videoContainer', { 
+			entry_id: entryId, 
+			wid: '_' + partnerId,
+			p: partnerId 
+		}, {
+			hash: mw.getKalturaIframeHash(), 
+			width: 400,
+			height: 300 
+		} );
+	});
+
+	$j('#showVideo').trigger('click');	
+} );
+</script>
+</body>
+</html>
