a2fb91e3841d0f090bfbfe8dfe6012f7052b7250
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-05 11:49:44 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55303 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/permission/filters/base/KalturaUserRoleBaseFilter.php
+++ api_v3/lib/types/permission/filters/base/KalturaUserRoleBaseFilter.php
@@ -17,6 +17,8 @@ class KalturaUserRoleBaseFilter extends KalturaFilter
 		"statusIn" => "_in_status",
 		"partnerIdEqual" => "_eq_partner_id",
 		"partnerIdIn" => "_in_partner_id",
+		"tagsMultiLikeOr" => "_mlikeor_tags",
+		"tagsMultiLikeAnd" => "_mlikeand_tags",
 		"createdAtGreaterThanOrEqual" => "_gte_created_at",
 		"createdAtLessThanOrEqual" => "_lte_created_at",
 		"updatedAtGreaterThanOrEqual" => "_gte_updated_at",
@@ -107,6 +109,20 @@ class KalturaUserRoleBaseFilter extends KalturaFilter
 	 * @var string
 	 */
 	public $partnerIdIn;
+
+	/**
+	 * 
+	 * 
+	 * @var string
+	 */
+	public $tagsMultiLikeOr;
+
+	/**
+	 * 
+	 * 
+	 * @var string
+	 */
+	public $tagsMultiLikeAnd;
 
 	/**
 	 * 
