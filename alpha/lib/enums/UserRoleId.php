b8f0a202bdd99ec59854d63c008c3fff977402b5
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-05 17:32:05 +0000

    no need for NO_KS_ROLE - removed
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55349 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/enums/UserRoleId.php
+++ alpha/lib/enums/UserRoleId.php
@@ -3,5 +3,4 @@ interface UserRoleId extends BaseEnum
 {
 	const PARTNER_ADMIN_ROLE     = 'PARTNER_ADMIN_ROLE';
 	const BASE_USER_SESSION_ROLE = 'BASE_USER_SESSION_ROLE';
-	const NO_SESSION_ROLE        = 'NO_SESSION_ROLE';
 }
