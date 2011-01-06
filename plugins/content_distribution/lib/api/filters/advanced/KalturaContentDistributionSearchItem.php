26616c37f7cb6362ce3c5956017e4694be81d508
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-06 13:37:38 +0000

    Added search rules
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55406 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/lib/api/filters/advanced/KalturaContentDistributionSearchItem.php
+++ plugins/content_distribution/lib/api/filters/advanced/KalturaContentDistributionSearchItem.php
@@ -2,6 +2,11 @@
 class KalturaContentDistributionSearchItem extends KalturaSearchItem
 {
 	/**
+	 * @var bool
+	 */
+	public $noDistributionProfiles;
+	
+	/**
 	 * @var int
 	 */
 	public $distributionProfileId;
@@ -20,13 +25,27 @@ class KalturaContentDistributionSearchItem extends KalturaSearchItem
 	 * @var KalturaEntryDistributionStatus
 	 */
 	public $entryDistributionStatus;
+	
+	/**
+	 * @var bool
+	 */
+	public $hasEntryDistributionValidationErrors;
+	
+	/**
+	 * Comma seperated validation error types
+	 * @dynamicType KalturaDistributionErrorType
+	 * @var string
+	 */
+	public $entryDistributionValidationErrors;
 
 	private static $map_between_objects = array
 	(
+		'noDistributionProfiles',
 		'distributionProfileId',
 		'distributionSunStatus',
 		'entryDistributionFlag',
 		'entryDistributionStatus',
+		'hasEntryDistributionValidationErrors',
 	);
 
 	public function getMapBetweenObjects ( )
@@ -39,6 +58,18 @@ class KalturaContentDistributionSearchItem extends KalturaSearchItem
 		if(!$object_to_fill)
 			$object_to_fill = new ContentDistributionSearchFilter();
 			
-		return parent::toObject($object_to_fill, $props_to_skip);
+		$object = parent::toObject($object_to_fill, $props_to_skip);
+		if($this->entryDistributionValidationErrors)
+			$object->setEntryDistributionValidationErrors(explode(',', $this->entryDistributionValidationErrors));
+			
+		return $object;
+	}
+
+	public function fromObject ( $source_object  )
+	{
+		parent::fromObject($source_object);
+		$entryDistributionValidationErrors = $source_object->getEntryDistributionValidationErrors();
+		if(count($entryDistributionValidationErrors))
+			$this->entryDistributionValidationErrors = implode(',', $entryDistributionValidationErrors);
 	}
 }
