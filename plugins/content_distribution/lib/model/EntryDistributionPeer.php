2123a25bed19fbe7e9cd0d372dd4167e868b9a9a
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-09 15:04:15 +0000

    Added sun status to the sphinx
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55584 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/lib/model/EntryDistributionPeer.php
+++ plugins/content_distribution/lib/model/EntryDistributionPeer.php
@@ -16,6 +16,7 @@ class EntryDistributionPeer extends BaseEntryDistributionPeer
 {
 	/** the search index column name for the NEXT_REPORT field */
 	const NEXT_REPORT = 'entry_distribution.NEXT_REPORT';
+	const SUN_STATUS = 'entry_distribution.SUN_STATUS';
 	
 	/**
 	 * Creates default criteria filter
