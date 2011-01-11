7520eb80b41cd00ddd27d3373b73e51b79c2bc59
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-11 12:59:25 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55741 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/virus_scan/tests/services/virusScanProfile/VirusScanProfileServiceBaseTest.php
+++ plugins/virus_scan/tests/services/virusScanProfile/VirusScanProfileServiceBaseTest.php
@@ -7,59 +7,111 @@ abstract class VirusScanProfileServiceBaseTest extends KalturaApiUnitTestCase
 {
 	/**
 	 * Tests virusScanProfile->list action
-	 * @param KalturaVirusScanProfileFilter $filter
-	 * @param KalturaFilterPager $pager
+	 * @param KalturaVirusScanProfileFilter $filter 
+	 * @param KalturaFilterPager $pager 
+	 * @param KalturaVirusScanProfileListResponse $reference 
 	 * @dataProvider provideData
 	 */
-	public function testList(KalturaVirusScanProfileFilter $filter = null, KalturaFilterPager $pager = null)
+	public function testList(KalturaVirusScanProfileFilter $filter = null, KalturaFilterPager $pager = null, KalturaVirusScanProfileListResponse $reference)
 	{
-		$resultObject = $this->client->virusScanProfile->listAction($filter, $pager);
+		$resultObject = $this->client->virusScanProfile->list($filter, $pager);
 		$this->assertType('KalturaVirusScanProfileListResponse', $resultObject);
-		$this->assertNotEquals($resultObject->totalCount, 0);
+		$this->validateList($filter, $pager, $reference);
+	}
+
+	/**
+	 * Validates testList results
+	 */
+	protected function validateList(KalturaVirusScanProfileFilter $filter = null, KalturaFilterPager $pager = null, KalturaVirusScanProfileListResponse $reference)
+	{
 	}
 
 	/**
 	 * Tests virusScanProfile->add action
-	 * @param KalturaVirusScanProfile $virusScanProfile
+	 * @param KalturaVirusScanProfile $virusScanProfile 
+	 * @param KalturaVirusScanProfile $reference 
 	 * @return int
 	 * @dataProvider provideData
 	 */
-	public function testAdd(KalturaVirusScanProfile $virusScanProfile)
+	public function testAdd(KalturaVirusScanProfile $virusScanProfile, KalturaVirusScanProfile $reference)
 	{
 		$resultObject = $this->client->virusScanProfile->add($virusScanProfile);
 		$this->assertType('KalturaVirusScanProfile', $resultObject);
 		$this->assertNotNull($resultObject->id);
+		$this->validateAdd($virusScanProfile, $reference);
 		return $resultObject->id;
+	}
+
+	/**
+	 * Validates testAdd results
+	 */
+	protected function validateAdd(KalturaVirusScanProfile $virusScanProfile, KalturaVirusScanProfile $reference)
+	{
 	}
 
 	/**
 	 * Tests virusScanProfile->get action
-	 * @param int $virusScanProfileId
+	 * @param int $virusScanProfileId 
+	 * @param KalturaVirusScanProfile $reference 
 	 * @return int
-	 * @depends testAdd with data set #0
+	 * @dataProvider provideData
 	 */
-	public function testGet($virusScanProfileId)
+	public function testGet($virusScanProfileId, KalturaVirusScanProfile $reference)
 	{
 		$resultObject = $this->client->virusScanProfile->get($virusScanProfileId);
 		$this->assertType('KalturaVirusScanProfile', $resultObject);
 		$this->assertNotNull($resultObject->id);
+		$this->validateGet($virusScanProfileId, $reference);
 		return $resultObject->id;
 	}
 
 	/**
+	 * Validates testGet results
+	 */
+	protected function validateGet($virusScanProfileId, KalturaVirusScanProfile $reference)
+	{
+	}
+
+	/**
 	 * Tests virusScanProfile->update action
-	 * @param int $virusScanProfileId
-	 * @param KalturaVirusScanProfile $virusScanProfile
+	 * @param int $virusScanProfileId 
+	 * @param KalturaVirusScanProfile $virusScanProfile Id
+	 * @param KalturaVirusScanProfile $reference 
 	 * @return int
-	 * @depends testAdd with data set #0
 	 * @dataProvider provideData
 	 */
-	public function testUpdate($virusScanProfileId, KalturaVirusScanProfile $virusScanProfile)
+	public function testUpdate($virusScanProfileId, KalturaVirusScanProfile $virusScanProfile, KalturaVirusScanProfile $reference)
 	{
 		$resultObject = $this->client->virusScanProfile->update($virusScanProfileId, $virusScanProfile);
 		$this->assertType('KalturaVirusScanProfile', $resultObject);
 		$this->assertNotNull($resultObject->id);
+		$this->validateUpdate($virusScanProfileId, $virusScanProfile, $reference);
 		return $resultObject->id;
+	}
+
+	/**
+	 * Validates testUpdate results
+	 */
+	protected function validateUpdate($virusScanProfileId, KalturaVirusScanProfile $virusScanProfile, KalturaVirusScanProfile $reference)
+	{
+	}
+
+	/**
+	 * Tests virusScanProfile->delete action
+	 * @param int $virusScanProfileId 
+	 * @dataProvider provideData
+	 */
+	public function testDelete($virusScanProfileId)
+	{
+		$resultObject = $this->client->virusScanProfile->delete($virusScanProfileId);
+		$this->validateDelete($virusScanProfileId);
+	}
+
+	/**
+	 * Validates testDelete results
+	 */
+	protected function validateDelete($virusScanProfileId)
+	{
 	}
 
 	/**
@@ -68,16 +120,5 @@ abstract class VirusScanProfileServiceBaseTest extends KalturaApiUnitTestCase
 	 * @return int
 	 */
 	abstract public function testFinished($id);
-
-	/**
-	 * Tests virusScanProfile->delete action
-	 * @param int $virusScanProfileId
-	 * @return int
-	 * @depends testFinished
-	 */
-	public function testDelete($virusScanProfileId)
-	{
-		$resultObject = $this->client->virusScanProfile->delete($virusScanProfileId);
-	}
 
 }
--- plugins/virus_scan/tests/services/virusScanProfile/VirusScanProfileServiceTest.php
+++ plugins/virus_scan/tests/services/virusScanProfile/VirusScanProfileServiceTest.php
@@ -9,26 +9,73 @@ require_once(dirname(__FILE__) . '/VirusScanProfileServiceBaseTest.php');
 class VirusScanProfileServiceTest extends VirusScanProfileServiceBaseTest
 {
 	/**
-	 * Called when all tests are done
-	 * @param int $id
-	 * @return int
-	 * @depends testFunction - TODO: replace testFunction with last test function that uses that id
+	 * Validates testList results
 	 */
-	public function testFinished($id)
+	protected function validateList(KalturaVirusScanProfileFilter $filter = null, KalturaFilterPager $pager = null, KalturaVirusScanProfileListResponse $reference)
 	{
-		return $id;
+		parent::validateList($filter, $pager, $reference);
+		// TODO - add your own validations here
+	}
+
+	/**
+	 * Validates testAdd results
+	 */
+	protected function validateAdd(KalturaVirusScanProfile $virusScanProfile, KalturaVirusScanProfile $reference)
+	{
+		parent::validateAdd($virusScanProfile, $reference);
+		// TODO - add your own validations here
+	}
+
+	/**
+	 * Validates testGet results
+	 */
+	protected function validateGet($virusScanProfileId, KalturaVirusScanProfile $reference)
+	{
+		parent::validateGet($virusScanProfileId, $reference);
+		// TODO - add your own validations here
+	}
+
+	/**
+	 * Validates testUpdate results
+	 */
+	protected function validateUpdate($virusScanProfileId, KalturaVirusScanProfile $virusScanProfile, KalturaVirusScanProfile $reference)
+	{
+		parent::validateUpdate($virusScanProfileId, $virusScanProfile, $reference);
+		// TODO - add your own validations here
+	}
+
+	/**
+	 * Validates testDelete results
+	 */
+	protected function validateDelete($virusScanProfileId)
+	{
+		parent::validateDelete($virusScanProfileId);
+		// TODO - add your own validations here
 	}
 
 	/**
 	 * Tests virusScanProfile->scan action
 	 * @param string $flavorAssetId
 	 * @param int $virusScanProfileId
+	 * @param int $reference
 	 * @dataProvider provideData
 	 */
-	public function testScan($flavorAssetId, $virusScanProfileId = null)
+	public function testScan($flavorAssetId, $virusScanProfileId = null, $reference)
 	{
-		$resultObject = $this->client->virusScanProfile->scan($flavorAssetId, $virusScanProfileId);
+		$resultObject = $this->client->virusScanProfile->scan($flavorAssetId, $virusScanProfileId, $reference);
 		$this->assertType('int', $resultObject);
+		// TODO - add here your own validations
+	}
+
+	/**
+	 * Called when all tests are done
+	 * @param int $id
+	 * @return int
+	 * @depends testUpdate - TODO: replace testUpdate with last test function that uses that id
+	 */
+	public function testFinished($id)
+	{
+		return $id;
 	}
 
 }
