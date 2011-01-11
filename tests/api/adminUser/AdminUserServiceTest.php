b7a6471f2e8ff457fb88c69fd08c9084b8c4d79c
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-11 12:58:11 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55739 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/api/adminUser/AdminUserServiceTest.php
+++ tests/api/adminUser/AdminUserServiceTest.php
@@ -14,12 +14,14 @@ class AdminUserServiceTest extends AdminUserServiceBaseTest
 	 * @param string $password
 	 * @param string $newEmail
 	 * @param string $newPassword
+	 * @param KalturaAdminUser $reference
 	 * @dataProvider provideData
 	 */
-	public function testUpdatePassword($email, $password, $newEmail = null, $newPassword = null)
+	public function testUpdatePassword($email, $password, $newEmail = null, $newPassword = null, KalturaAdminUser $reference)
 	{
-		$resultObject = $this->client->adminUser->updatePassword($email, $password, $newEmail, $newPassword);
+		$resultObject = $this->client->adminUser->updatePassword($email, $password, $newEmail, $newPassword, $reference);
 		$this->assertType('KalturaAdminUser', $resultObject);
+		// TODO - add here your own validations
 	}
 
 	/**
@@ -30,6 +32,7 @@ class AdminUserServiceTest extends AdminUserServiceBaseTest
 	public function testResetPassword($email)
 	{
 		$resultObject = $this->client->adminUser->resetPassword($email);
+		// TODO - add here your own validations
 	}
 
 	/**
@@ -37,12 +40,14 @@ class AdminUserServiceTest extends AdminUserServiceBaseTest
 	 * @param string $email
 	 * @param string $password
 	 * @param int $partnerId
+	 * @param string $reference
 	 * @dataProvider provideData
 	 */
-	public function testLogin($email, $password, $partnerId = null)
+	public function testLogin($email, $password, $partnerId = null, $reference)
 	{
-		$resultObject = $this->client->adminUser->login($email, $password, $partnerId);
+		$resultObject = $this->client->adminUser->login($email, $password, $partnerId, $reference);
 		$this->assertType('string', $resultObject);
+		// TODO - add here your own validations
 	}
 
 	/**
@@ -54,6 +59,18 @@ class AdminUserServiceTest extends AdminUserServiceBaseTest
 	public function testSetInitialPassword($hashKey, $newPassword)
 	{
 		$resultObject = $this->client->adminUser->setInitialPassword($hashKey, $newPassword);
+		// TODO - add here your own validations
+	}
+
+	/**
+	 * Called when all tests are done
+	 * @param int $id
+	 * @return int
+	 * @depends testUpdate - TODO: replace testUpdate with last test function that uses that id
+	 */
+	public function testFinished($id)
+	{
+		return $id;
 	}
 
 }
