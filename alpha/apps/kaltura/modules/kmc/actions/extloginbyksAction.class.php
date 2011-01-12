b5cabb27c86493a050c1b595879a4670969f641f
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-12 08:44:08 +0000

    Added extloginbyks action - same as external login but using a valid KS + a requested partner id to log into
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55801 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/apps/kaltura/modules/kmc/actions/extloginbyksAction.class.php
@@ -0,0 +1,123 @@
+<?php
+class extloginbyksAction extends kalturaAction
+{
+	
+	protected function addError ( $error_code )
+	{
+		if ( is_array ( $error_code ) )
+		{
+			$args = $error_code;
+			$error_code = $error_code[0];
+		}
+		else
+		{
+			$args = func_get_args();
+		}
+		array_shift($args);
+		
+		$error = explode(",", $error_code, 2);
+		
+		$error_code = $error[0];
+		$error_message = $error[1];
+
+		$formated_desc = @call_user_func_array('sprintf', array_merge((array)$error_message, $args)); 
+		
+		$this->error[] = array("code" => $error_code, "desc" => $formated_desc);
+	}
+	
+	private function dieOnError($error_code)
+	{
+		if ( is_array ( $error_code ) )
+		{
+			$args = $error_code;
+			$error_code = $error_code[0];
+		}
+		else
+		{
+			$args = func_get_args();
+		}
+		array_shift($args);
+		
+		$error = explode(",", $error_code, 2);
+		
+		$error_code = $error[0];
+		$error_message = $error[1];
+
+		$formated_desc = @call_user_func_array('sprintf', array_merge((array)$error_message, $args)); 
+		
+		header("X-Kaltura:error-$error_code");
+		header('X-Kaltura-App: exiting on error '.$error_code.' - '.$formated_desc);
+		
+		die();
+	}
+	
+	public function execute()
+	{
+		$ks = $this->getP ( "ks" );
+		$requestedPartnerId = $this->getP ( "partner_id" );
+
+		try {
+			$adminKuser = UserLoginDataPeer::userLoginByKs($ks, $requestedPartnerId);
+		}
+		catch (kUserException $e) {
+			$code = $e->getCode();
+			if ($code == kUserException::USER_NOT_FOUND) {
+				$this->dieOnError  ( APIErrors::ADMIN_KUSER_NOT_FOUND );
+			}
+			if ($code == kUserException::LOGIN_DATA_NOT_FOUND) {
+				$this->dieOnError  ( APIErrors::ADMIN_KUSER_NOT_FOUND );
+			}
+			else if ($code == kUserException::LOGIN_RETRIES_EXCEEDED) {
+				$this->dieOnError  ( APIErrors::LOGIN_RETRIES_EXCEEDED );
+			}
+			else if ($code == kUserException::LOGIN_BLOCKED) {
+				$this->dieOnError  ( APIErrors::LOGIN_BLOCKED );
+			}
+			else if ($code == kUserException::PASSWORD_EXPIRED) {
+				$this->dieOnError  ( APIErrors::PASSWORD_EXPIRED );
+			}
+			else if ($code == kUserException::WRONG_PASSWORD) {
+				$this->dieOnError  (APIErrors::ADMIN_KUSER_NOT_FOUND);
+			}
+			else if ($code == kUserException::USER_IS_BLOCKED) {
+				$this->dieOnError  (APIErrors::USER_IS_BLOCKED);
+			}
+			$this->dieOnError  ( APIErrors::INTERNAL_SERVERL_ERROR );
+		}
+		if (!$adminKuser || !$adminKuser->getIsAdmin()) {
+			$this->dieOnError  ( APIErrors::ADMIN_KUSER_NOT_FOUND );
+		}
+		
+		
+		if ($requestedPartnerId != $adminKuser->getPartnerId()) {
+			$this->dieOnError  ( APIErrors::UNKNOWN_PARTNER_ID );
+		}
+		
+		$partner = PartnerPeer::retrieveByPK( $adminKuser->getPartnerId() );
+		
+		if (!$partner)
+		{
+			$this->dieOnError  ( APIErrors::UNKNOWN_PARTNER_ID );	
+		}
+		
+		$partner_id = $partner->getId();
+		$subp_id = $partner->getSubpId() ;
+		$admin_puser_id = $adminKuser->getPuserId();
+		$screen_name = $adminKuser->getScreenName();
+		
+		$ks = null;
+		
+		kSessionUtils::createKSessionNoValidations ( $partner_id ,  $admin_puser_id , $ks , 30 * 86400 , 2 , "" , "*" );
+				
+		$exp = 0;
+		$path = "/";
+		
+		$this->getResponse()->setCookie("pid", $partner_id, $exp, $path);
+		$this->getResponse()->setCookie("subpid", $subp_id, $exp, $path);
+		$this->getResponse()->setCookie("uid", $admin_puser_id, $exp, $path);
+		$this->getResponse()->setCookie("kmcks", $ks, $exp, $path);
+		$this->getResponse()->setCookie("screen_name", $screen_name, $exp, $path);
+		$this->redirect('kmc/kmc2');
+	}
+}
+?>
