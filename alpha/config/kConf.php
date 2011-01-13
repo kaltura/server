59d55600569ec407b19349ef44ac5acf99ee16c0
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-13 15:31:33 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55986 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/kConf.php
+++ alpha/config/kConf.php
@@ -172,7 +172,7 @@ class kConf extends kConfLocal
 				
 				'ps2_actions_not_blocked_by_permissions' => array(
 					// should list action class names lowercase!
-					'mymultieequest',
+					'mymultirequest',
 					'adminloginaction',
 					'resetadminpasswordaction',
 					'executeplaylistaction',
