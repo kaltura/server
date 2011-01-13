4d309c84a280af93a67159df835fbbf48b3ef4fd
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-13 13:44:58 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@55946 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/web/lib/js/kmc4.js
+++ alpha/web/lib/js/kmc4.js
@@ -415,12 +415,13 @@ $(window).load(function(){
 			
 			var partners_count = kmc.vars.allowed_partners.length;
 			console.log(kmc.vars.allowed_partners);
-			/*
+			
 			for( var i=0; i <= partners_count; i++ ) {
 				console.log(kmc.vars.allowed_partners[i]);
 				content += kmc.vars.allowed_partners[i].name + '<br />';
 			}
 			content += '<input type="button" value=" Go " />';
+			/*
 			
 			//window.location.href = '/index.php/kmc/extloginbyks?ks=' + kmc.vars.ks + '&partner_id=' + this.value;
 			
