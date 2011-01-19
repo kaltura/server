e02e866d56ede14d3429e6a267791b27af569ad7
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-19 16:48:35 +0000

    Implement getCondiftion and XML marshaling
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56240 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/model/objectfilters/AdvancedSearchFilterOperator.class.php
+++ alpha/apps/kaltura/lib/model/objectfilters/AdvancedSearchFilterOperator.class.php
@@ -42,4 +42,84 @@ abstract class AdvancedSearchFilterOperator extends AdvancedSearchFilterItem
 	public function setItems(array $items) {
 		$this->items = $items;
 	}
+
+	/**
+	 * @var string
+	 */
+	protected $condition = null;
+	
+	public function getCondition()
+	{
+		if($this->condition)
+			return $this->condition;
+			
+		$conditions = array();
+		
+		if(count($this->items))
+		{
+			foreach($this->items as $item)
+			{
+				if($item instanceof AdvancedSearchFilterOperator)
+				{
+					$condition = $item->getCondition();
+					if($condition)
+						$conditions[] = "($condition)";
+				}
+			}
+		}
+
+		if(!count($conditions))
+			return null;
+			
+		$glue = ($this->type == MetadataSearchFilter::SEARCH_AND ? ' & ' : ' | ');
+		$this->condition = implode($glue, $conditions);
+		
+		return $this->condition;
+	}
+	
+	public function apply(baseObjectFilter $filter, Criteria &$criteria, array &$matchClause, array &$whereClause)
+	{
+		KalturaLog::debug("apply from [" . get_class($filter) . "]");
+		
+		$condition = $this->getCondition();
+		if($condition && strlen($condition))
+			$matchClause[] = $condition;
+	}
+	
+	public function addToXml(SimpleXMLElement &$xmlElement)
+	{
+		parent::addToXml($xmlElement);
+		
+		$xmlElement->addAttribute('operatorType', $this->type);
+		
+		foreach($this->items as $item)
+		{
+			$itemXmlElement = $xmlElement->addChild('item');
+			$itemXmlElement->addAttribute('type', get_class($item));
+			
+			$item->addToXml($itemXmlElement);
+		}
+	}
+	
+	public function fillObjectFromXml(SimpleXMLElement $xmlElement)
+	{
+		parent::fillObjectFromXml($xmlElement);
+		
+		$attr = $xmlElement->attributes();
+			
+		if(isset($attr['operatorType']))
+			$this->type = (int) $attr['operatorType'];
+			
+		foreach($xmlElement->item as $child)
+		{
+			$attr = $child->attributes();
+			if(!isset($attr['type']))
+				continue;
+				
+			$type = (string) $attr['type'];
+			$item = new $type();
+			$item->fillObjectFromXml($child);
+			$this->items[] = $item;
+		}
+	}
 }
