955d0d0ad9dd85b583ff479ef0715b9b195751c1
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-20 14:44:43 +0000

    ticket 8519: add serve action to dataService, and identifier if to return the contentdata in the get / list action
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56289 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/entry/KalturaDataEntry.php
+++ api_v3/lib/types/entry/KalturaDataEntry.php
@@ -9,8 +9,16 @@ class KalturaDataEntry extends KalturaBaseEntry
 	 */
 	public $dataContent;
 	
+	/**
+	 * indicator whether to return the object for get action with the dataContent field.
+	 * @insertonly
+	 * @var bool
+	 */
+	public $retrieveDataContentByGet;
+	
 	private static $map_between_objects = array(
 		"dataContent",
+		"retrieveDataContentByGet",
 	);
 	
 	public function __construct()
@@ -21,5 +29,30 @@ class KalturaDataEntry extends KalturaBaseEntry
 	public function getMapBetweenObjects()
 	{
 		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
+	}
+	
+
+	public function toObject($dbDataEntry = null, $propsToSkip = array())
+	{
+		if(is_null($dbDataEntry))
+			$dbDataEntry = new KalturaDataEntry();
+			
+		if ($this->retrieveDataContentByGet === null)
+			$this->retrieveDataContentByGet = 1;
+			
+		$dbDataEntry->putInCustomData('retrieveDataContentByGet',$this->retrieveDataContentByGet);
+			
+		return parent::toObject($dbDataEntry, $propsToSkip);
+	}
+	
+	public function fromObject($dbDataEntry)
+	{
+		parent::fromObject($dbDataEntry);
+		$retrieveDataContentByGet = $dbDataEntry->getFromCustomData('retrieveDataContentByGet');
+		$this->retrieveDataContentByGet = $retrieveDataContentByGet;
+		
+		if($retrieveDataContentByGet != true)
+			$this->dataContent = '';
+				
 	}
 }
