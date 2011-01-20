d3bbb8f1d5f15e706dcc2ce4588c54a9c1f9ee3c
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-20 18:15:09 +0000

    Added facility methods
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56312 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/batch/Distribute/engines/DistributionEngine.php
+++ plugins/content_distribution/batch/Distribute/engines/DistributionEngine.php
@@ -44,7 +44,7 @@ abstract class DistributionEngine implements IDistributionEngine
 
 	/**
 	 * @param string $entryId
-	 * @return KalturaBaseEntry
+	 * @return KalturaMediaEntry
 	 */
 	protected function getEntry($entryId)
 	{
@@ -52,17 +52,92 @@ abstract class DistributionEngine implements IDistributionEngine
 	}
 
 	/**
-	 * @param string $entryId
-	 * @return KalturaMetadata
+	 * @param string $flavorAssetIds comma seperated
+	 * @return array<KalturaFlavorAsset>
 	 */
-	protected function getEntryMetadata($entryId)
+	protected function getFlavorAssets($flavorAssetIds)
+	{
+		$filter = new KalturaAssetFilter();
+		$filter->idIn = $flavorAssetIds;
+		$flavorAssetsList = $this->kalturaClient->flavorAsset->listAction($filter);
+		return $flavorAssetsList->objects;
+	}
+
+	/**
+	 * @param string $thumbAssetIds comma seperated
+	 * @return array<KalturaThumbAsset>
+	 */
+	protected function getThumbAssets($thumbAssetIds)
+	{
+		$filter = new KalturaAssetFilter();
+		$filter->idIn = $thumbAssetIds;
+		$thumbAssetsList = $this->kalturaClient->thumbAsset->listAction($filter);
+		return $thumbAssetsList->objects;
+	}
+
+	/**
+	 * @param string $thumbAssetId
+	 * @return string url
+	 */
+	protected function getThumbAssetUrl($thumbAssetId)
+	{
+		$domain = $this->kalturaClient->getConfig()->serviceUrl;
+		return "$domain/service/thumbAsset/action/serve/thumbAssetId/$thumbAssetId";
+	}
+
+	/**
+	 * @param string $flavorAssetId
+	 * @return string url
+	 */
+	protected function getFlavorAssetUrl($flavorAssetId)
+	{
+//		TODO
+//		$domain = $this->kalturaClient->getConfig()->serviceUrl;
+//		return "$domain/service/flavorAsset/action/serve/flavorAssetId/$flavorAssetId";
+	}
+
+	/**
+	 * @param array<KalturaMetadata> $metadataObjects
+	 * @param string $field
+	 * @return array|string
+	 */
+	protected function findMetadataValue(array $metadataObjects, $field, $asArray = false)
+	{
+		$results = array();
+		foreach($metadataObjects as $metadata)
+		{
+			$xml = new DOMDocument();
+			$xml->loadXML($metadata->xml);
+			$nodes = $xml->getElementsByTagName($field);
+			foreach($nodes as $node)
+				$results = $node->textContent;
+		}
+		
+		if(!$asArray)
+		{
+			if(!count($results))
+				return null;
+				
+			if(count($results) == 1)
+				return reset($results);
+		}
+			
+		return $results;
+	}
+
+	/**
+	 * @param string $objectId
+	 * @param KalturaMetadataObjectType $objectType
+	 * @return array<KalturaMetadata>
+	 */
+	protected function getMetadataObjects($objectId, $objectType = KalturaMetadataObjectType::ENTRY)
 	{
 		if(!class_exists('KalturaMetadata'))
 			return null;
 			
 		$metadataFilter = new KalturaMetadataFilter();
-		$metadataFilter->objectIdEqual = $entryId;
-		$metadataFilter->metadataObjectTypeEqual = KalturaMetadataObjectType::ENTRY;
+		$metadataFilter->objectIdEqual = $objectId;
+		$metadataFilter->metadataObjectTypeEqual = $objectType;
 		$metadataFilter->orderBy = KalturaMetadataOrderBy::CREATED_AT_DESC;
 		$metadataPager = new KalturaFilterPager();
 		$metadataPager->pageSize = 1;
@@ -70,6 +145,6 @@ abstract class DistributionEngine implements IDistributionEngine
 		if(!$metadataListResponse->totalCount)
 			throw new Exception("No metadata objects found");
 
-		return reset($metadataListResponse->objects);
+		return $metadataListResponse->objects;
 	}
 }
