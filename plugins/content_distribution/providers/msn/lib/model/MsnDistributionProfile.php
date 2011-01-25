d47d651e93c80dbc95bd3882a271081375987502
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-25 17:30:35 +0000

    Validates metadata fields
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56470 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/providers/msn/lib/model/MsnDistributionProfile.php
+++ plugins/content_distribution/providers/msn/lib/model/MsnDistributionProfile.php
@@ -11,12 +11,79 @@ class MsnDistributionProfile extends DistributionProfile
 	const CUSTOM_DATA_FLV_FLAVOR_PARAMS_ID = 'flvFlavorParamsId';
 	const CUSTOM_DATA_WMV_FLAVOR_PARAMS_ID = 'wmvFlavorParamsId';
 
+	const METADATA_FIELD_VIDEO_CAT = 'MSNVideoCat';
+	const METADATA_FIELD_VIDEO_TOP = 'MSNVideoTop';
+	const METADATA_FIELD_VIDEO_TOP_CAT = 'MSNVideoTopCat';
+	const METADATA_FIELD_PUBLIC = 'MSNPublic';
+	
 	/* (non-PHPdoc)
 	 * @see DistributionProfile::getProvider()
 	 */
 	public function getProvider()
 	{
 		return MsnDistributionPlugin::getProvider();
+	}
+			
+	/* (non-PHPdoc)
+	 * @see DistributionProfile::validateForSubmission()
+	 */
+	public function validateForSubmission(EntryDistribution $entryDistribution, $action)
+	{
+		$validationErrors = parent::validateForSubmission($entryDistribution, $action);
+		
+		if(!class_exists('MetadataProfile'))
+			return $validationErrors;
+			
+		$requiredFields = array(
+			self::METADATA_FIELD_VIDEO_CAT,
+			self::METADATA_FIELD_VIDEO_TOP,
+			self::METADATA_FIELD_VIDEO_TOP_CAT,
+			self::METADATA_FIELD_PUBLIC,
+		);
+		
+		$metadataProfileId = $this->getMetadataProfileId();
+		if(!$metadataProfileId)
+		{
+			foreach($requiredFields as $field)
+				$validationErrors[] = $this->createValidationError($action, DistributionErrorType::MISSING_METADATA, $field);
+			return $validationErrors;
+		}
+	
+		$metadatas = MetadataPeer::retrieveAllByObject(Metadata::TYPE_ENTRY, $entryDistribution->getEntryId());
+		if(!count($metadatas))
+		{
+			foreach($requiredFields as $field)
+				$validationErrors[] = $this->createValidationError($action, DistributionErrorType::MISSING_METADATA, $field);
+			return $validationErrors;
+		}
+		
+		foreach($requiredFields as $field)
+		{
+			$metadataProfileCategoryField = MetadataProfileFieldPeer::retrieveByMetadataProfileAndKey($metadataProfileId, $field);
+			if(!$metadataProfileCategoryField)
+			{
+				$validationErrors[] = $this->createValidationError($action, DistributionErrorType::MISSING_METADATA, $field);
+				continue;
+			}
+		
+			$values = $this->findMetadataValue($metadatas, $field);
+			if(!count($values))
+			{
+				$validationErrors[] = $this->createValidationError($action, DistributionErrorType::MISSING_METADATA, $field);
+				continue;
+			}
+				
+			foreach($values as $value)
+			{
+				if(!strlen($value))
+				{
+					$validationErrors[] = $this->createValidationError($action, DistributionErrorType::INVALID_DATA, $field);
+					return $validationErrors;
+				}
+			}
+		}
+		
+		return $validationErrors;
 	}
 
 	public function getUsername()				{return $this->getFromCustomData(self::CUSTOM_DATA_USERNAME);}
