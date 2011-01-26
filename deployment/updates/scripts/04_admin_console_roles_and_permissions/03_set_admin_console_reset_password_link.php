d79efdd1691a86a763fd72fd6c14fb40913c63d8
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-26 14:00:33 +0000

    migration updates
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56497 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/updates/scripts/04_admin_console_roles_and_permissions/03_set_admin_console_reset_password_link.php
@@ -0,0 +1,29 @@
+<?php
+
+$dryRun = true; //TODO: change for real run
+if($argc > 1 && $argv[1] == 'realrun')
+	$dryRun = false;
+	
+//------------------------------------------------------
+
+require_once(dirname(__FILE__).'/../../../bootstrap.php');
+define('ADMIN_CONSOLE_PARTNER_ID', -2);
+
+$partner = PartnerPeer::retrieveByPK(ADMIN_CONSOLE_PARTNER_ID);
+$partner->setPassResetUrlPrefixName('admin_console');
+
+if ($dryRun)
+{
+	KalturaLog::log('DRY RUN! - Setting partner ['.$partner->getId().'] with password reset url prefix name of ['.$partner->getPassResetUrlPrefixName().']');
+}
+else
+{
+	KalturaLog::log('Setting partner ['.$partner->getId().'] with password reset url prefix name of ['.$partner->getPassResetUrlPrefixName().']');
+	$partner->save();
+}
+
+//------------------------------------------------------
+
+$msg = 'Done - ' . ($dryRun ? 'DRY RUN!' : 'REAL RUN!');
+KalturaLog::log($msg);
+echo $msg;
