8c649ab0494ffb8f07e741221a8a6cd7ad705628
orip <orip@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-27 07:13:03 +0000

    added comment
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56556 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/log/KalturaLog.php
+++ infra/log/KalturaLog.php
@@ -117,7 +117,9 @@ class UniqueId
 		{
 			self::$_uniqueId = (string)rand();
 			// add a the unique id to Apache's internal variable so we can later log it using the %{KalturaLog_UniqueId}n placeholder
-			// within the LogFormat apache directive. This way each access_log record can be matched with its kaltura log lines
+			// within the LogFormat apache directive. This way each access_log record can be matched with its kaltura log lines.
+			// before setting the apache note name and value, a condition checks if function exists,
+			// due to fact that running from command line will not define this function
 			if (function_exists('apache_note'))
 				apache_note("KalturaLog_UniqueId", self::$_uniqueId);
 		}
