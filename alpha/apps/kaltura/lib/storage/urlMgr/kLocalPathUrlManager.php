8156477c6c5878917fe14f316ef3be454ad57a1c
etam <etam@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-27 09:14:29 +0000

    override getFlavorAssetUrl in order to use a local streaming server
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56562 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/storage/urlMgr/kLocalPathUrlManager.php
+++ alpha/apps/kaltura/lib/storage/urlMgr/kLocalPathUrlManager.php
@@ -19,4 +19,28 @@ class kLocalPathUrlManager extends kUrlManager
 		
 		return $url;
 	}
+	
+	/**
+	 * Returns the url for the given flavor. in case of rtmp, return the file path in order ot use a local streaming server
+	 * 
+	 * @param flavorAsset $flavorAsset
+	 * @return string
+	 */
+	public function getFlavorAssetUrl(flavorAsset $flavorAsset)
+	{
+		if($this->protocol != StorageProfile::PLAY_FORMAT_RTMP)
+			return parent::getFlavorAssetUrl($flavorAsset);
+                	
+		$syncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
+		$fileSync = kFileSyncUtils::getReadyInternalFileSyncForKey($syncKey);
+		$url = $this->getFileSyncUrl($fileSync);
+		if ($this->extention && strtolower($this->extention) != 'flv' ||
+			$this->containerFormat && strtolower($this->containerFormat) != 'flash video')
+		{
+			$url = "mp4:$url";
+		}
+
+		$url = str_replace('\\', '/', $url);
+		return $url;
+	}
 }
