b79c0882a37e64f0c43737f7c941d3867d69e437
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-27 12:49:28 +0000

    kmc version updated to v4.0.4
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56608 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/kConf.php
+++ alpha/config/kConf.php
@@ -100,7 +100,7 @@ class kConf extends kConfLocal
 				"kdp3_wrapper_version" => 'v32.0',
 				"kmc_secured_login" => false,
 				
-				"kmc_version" => 'v4.0.3',
+				"kmc_version" => 'v4.0.4',
 				
 				"paypal_data" => array (),
 				
