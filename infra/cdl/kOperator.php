4a8a31bca3ce137736f115fb2d6324a443a7886e
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-29 11:37:59 +0000

    additional operator params
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56668 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/cdl/kOperator.php
+++ infra/cdl/kOperator.php
@@ -1,6 +1,19 @@
 <?php
 class kOperator
 {
+	public function __construct($opr=null)
+	{
+		if($opr==null){
+//			KalturaLog::info("no input");
+			return;
+		}
+//		KalturaLog::info(print_r($opr,true));
+		$this->id = $opr->id;
+		$this->extra = isset($opr->extra) ? $opr->extra : null;
+		$this->command = isset($opr->command) ? $opr->command : null;
+		$this->config = isset($opr->config) ? $opr->config : null;
+	}
+	
 	/**
 	 * @var int
 	 */
@@ -20,4 +33,16 @@ class kOperator
 	 * @var string
 	 */
 	public $config;
+	
+	/**
+	 * @var int
+	 */
+	public $extracMediaEnabled=1;
+	
+	/**
+	 * @var int
+	 */
+	public $thumbEnabled=1;
+	
+	
 }
