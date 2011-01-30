7d8fb9e9d830049f62be447b5b65bb482aeb1139
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-30 16:23:25 +0000

    Short link table
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56824 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/updates/sql/2011-01-30_short_link_create.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE `short_link`
+(
+	`id` VARCHAR(20)  NOT NULL,
+	`int_id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	`expires_at` DATETIME,
+	`partner_id` INTEGER,
+	`kuser_id` INTEGER,
+	`name` VARCHAR(63),
+	`system_name` VARCHAR(63),
+	`full_url` VARCHAR(255),
+	`status` INTEGER,
+	PRIMARY KEY (`id`),
+	KEY `int_id`(`int_id`),
+	KEY `partner_id`(`partner_id`),
+	KEY `kuser_partner_name`(`partner_id`, `kuser_id`, `system_name`)
+)Type=MyISAM;
