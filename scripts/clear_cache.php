954e275e050ea1187a6c349258946628a46878f8
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-01-30 15:43:43 +0000

    update
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@56795 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/clear_cache.php
+++ scripts/clear_cache.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once(dirname(__FILE__).'/bootstrap.php');
+error_reporting(E_ALL);
+
+require_once(dirname(__FILE__).'/../alpha/config/kConf.php');
 
 // clear kConf defined cache directories
 system('rm -rf '.kConf::get('cache_root_path').'/*');
@@ -8,8 +10,8 @@ system('rm -rf '.kConf::get('general_cache_dir').'/*');
 system('rm -rf '.kConf::get('response_cache_dir').'/*');
 
 // clear symfony (alpha) cache
-system(SF_ROOT_DIR.'/symfony cc');
-system(SF_ROOT_DIR.'/symfony cc');
+system(kConf::get('sf_root_dir').'/symfony cc');
+system(kConf::get('sf_root_dir').'/symfony cc');
 
 // clear APC cache
 if (function_exists('apc_clear_cache'))
