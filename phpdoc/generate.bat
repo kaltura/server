5d887cd7f1a159538207dbf48fcd61096bd907ed
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-02 22:19:01 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57109 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- phpdoc/generate.bat
+++ phpdoc/generate.bat
@@ -1,8 +1,8 @@
 cd PhpDocumentor
-#phpdoc -o HTML:Smarty:default -t core -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../alpha,../api_v3,../infra,../plugins
-phpdoc -o HTML:frames:phpedit -t ..\batch -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../../batch,../../infra,../../plugins -po General,api,Core,plugins*
-#phpdoc -o HTML:Smarty:default -t admin -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../,../admin_console,../infra,../plugins
-#phpdoc -o HTML:Smarty:default -t docs -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../alpha,../api_v3,../batch,../,../admin_console,../generator,../deployment,../infra,../plugins,../scripts,../tests
+#phpdoc -o HTML:frames/Extjs:default -t ..\core -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../../alpha,../../api_v3,../../infra,../../plugins
+phpdoc -o HTML:frames/Extjs:default -t ..\batch -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../../batch,../../infra,../../plugins
+#phpdoc -o HTML:Smarty:default -t ..\admin -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../../,../../admin_console,../../infra,../../plugins
+#phpdoc -o HTML:Smarty:default -t ..\docs -ti "Kaltura Server" -dn General -dc Global -ct readonly,insertonly,writeonly,service,action,clientgenerator,filter,serverOnly,dynamicType,requiresPermission -d ../../alpha,../../api_v3,../../batch,../../,../../admin_console,../../generator,../../deployment,../../infra,../../plugins,../../scripts,../../tests
 
 #HTML:frames:phphtmllib
 #HTML:frames:earthli
