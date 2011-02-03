b741e7a798aa0efaddd2399aa67b621a4a864498
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-03 13:46:50 +0000

    Using serviceId instead of serviceName in permission_item
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57156 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/services/UserRoleService.php
+++ api_v3/services/UserRoleService.php
@@ -8,9 +8,9 @@
  */
 class UserRoleService extends KalturaBaseService
 {
-	public function initService($serviceName, $actionName)
+	public function initService($serviceId, $serviceName, $actionName)
 	{
-		parent::initService($serviceName, $actionName);
+		parent::initService($serviceId, $serviceName, $actionName);
 		
 		myPartnerUtils::addPartnerToCriteria(new UserRolePeer(), $this->getPartnerId(), $this->private_partner_data, $this->partnerGroup());
 		myPartnerUtils::addPartnerToCriteria(new PermissionPeer(), $this->getPartnerId(), $this->private_partner_data, $this->partnerGroup());
