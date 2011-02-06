bf4861e6171c2dd3b3c40b90b21272024eaae5ed
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-06 12:40:23 +0000

    Script update
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57231 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/clear_cache.php
+++ scripts/clear_cache.php
@@ -5,9 +5,11 @@ error_reporting(E_ALL);
 require_once(dirname(__FILE__).'/../alpha/config/kConf.php');
 
 // clear kConf defined cache directories
-system('rm '.realpath(kConf::get('cache_root_path')).DIRECTORY_SEPARATOR.'*');
-system('rm '.realpath(kConf::get('general_cache_dir')).DIRECTORY_SEPARATOR.'*');
-system('rm '.realpath(kConf::get('response_cache_dir')).DIRECTORY_SEPARATOR.'*');
+$path = realpath(kConf::get('cache_root_path'));
+
+askToDelete(realpath(kConf::get('general_cache_dir')));
+askToDelete(realpath(kConf::get('response_cache_dir')));
+askToDelete(realpath(kConf::get('cache_root_path')));
 
 // clear symfony (alpha) cache
 system('php '.realpath(kConf::get('sf_root_dir')).DIRECTORY_SEPARATOR.'symfony cc');
@@ -17,8 +19,35 @@ system('php '.realpath(kConf::get('sf_root_dir')).DIRECTORY_SEPARATOR.'symfony c
 if (function_exists('apc_clear_cache'))
 {
 	// clear apc system cache
-	apc_clear_cache(); 
+	if (!apc_clear_cache())
+	{
+		echo 'Unable to clear APC SYSTEM cache!'.PHP_EOL;
+	} 
 	
 	// clear apc user cache
-	apc_clear_cache('user');
+	if (!apc_clear_cache('user'))
+	{
+		echo 'Unable to clear APC USER cache!'.PHP_EOL;
+	}
+}
+
+function askToDelete($path)
+{
+	$baseKalturaPath = realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'..');
+	if (strpos($path, $baseKalturaPath) === 0)
+	{
+		echo 'Are you sure you want to delete all contents of ['.$path.'] (y/n) ?  ';
+		$input = trim(fgets(STDIN));
+		if ($input === 'y') {
+			echo 'rm -rf '.$path.DIRECTORY_SEPARATOR.'*'.PHP_EOL;
+			system('rm -rf '.$path.DIRECTORY_SEPARATOR.'*');
+		}
+		else {
+			echo 'Skipping...'.PHP_EOL;
+		}
+	}
+	else
+	{
+		echo 'Path ['.$path.'] does not belong to the kaltura server. Skipping.'.PHP_EOL;
+	}	
 }
