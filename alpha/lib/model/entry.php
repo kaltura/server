256a1310effcb8afe1383035e189d57cbeb5185c
gonen <gonen@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-06 14:03:00 +0000

    added retrieveDataContentByGet to customData
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57243 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/entry.php
+++ alpha/lib/model/entry.php
@@ -1828,6 +1828,13 @@ class entry extends Baseentry implements ISyncableFile, IIndexable
 		return "";
 	}
 	 
+	 
+	// -- retrieveDataContentByGet --
+	// disable/enable sending data content for data entries in API v3 (for binary-data-entry use-case)
+	public function setRetrieveDataContentByGet( $v ) { $this->putInCustomData("retrieveDataContentByGet", $v); }
+	public function getRetrieveDataContentByGet() { return $this->getFromCustomData("retrieveDataContentByGet", "", 1); }
+	
+	
 	// ------------------------------------------------------------
 	// setters & gettes for entry when callnig addentry
 	private $m_url;
