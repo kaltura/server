b25c0f7d923b4850a34e60ff811cfff890741812
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-11 13:09:30 +0000

    Updated preview page
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57583 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/actions/previewAction.class.php
+++ alpha/apps/kaltura/modules/kmc/actions/previewAction.class.php
@@ -6,7 +6,26 @@ class previewAction extends kalturaAction
 {
 	public function execute ( ) 
 	{
-		//sfView::SUCCESS;
+		$html5_version = kConf::get('html5_version');
+
+		$uiconf_id = $this->getRequestParameter('uiconf_id');
+		if(!$uiconf_id)
+			KExternalErrors::dieError(KExternalErrors::MISSING_PARAMETER, 'uiconf_id');
+
+		$uiConf = uiConfPeer::retrieveByPK($uiconf_id);
+		if(!$uiConf)
+			KExternalErrors::dieError(KExternalErrors::UI_CONF_NOT_FOUND);
+
+		$partner_id = $this->getRequestParameter('partner_id', $uiConf->getPartnerId());
+		if(!$partner_id)
+			KExternalErrors::dieError(KExternalErrors::MISSING_PARAMETER, 'partner_id');
+
+		$entry_id = $this->getRequestParameter('entry_id');
+		if(!$entry_id)
+			KExternalErrors::dieError(KExternalErrors::MISSING_PARAMETER, 'entry_id');
+
+		$partner_host = myPartnerUtils::getHost($partner_id);
+		$partner_cdnHost = myPartnerUtils::getCdnHost($partner_id);
 	}
 }
 ?>
--- alpha/apps/kaltura/modules/kmc/templates/previewSuccess.php
+++ alpha/apps/kaltura/modules/kmc/templates/previewSuccess.php
@@ -1,7 +1,22 @@
 ﻿<?php 
-if( ! $sf_params->has('p') || $sf_params->has['e'] ) { 
+if( ! $partner_id || $entry_id ) {
 	die('Missing entry id or partner id');	
 }
+
+if( $uiConf ) {
+    echo '<pre>';
+    print_r($uiConf);
+    exit();
+}
+
+// Create swf url
+$swfUrl = "http://". $partner_host ."/index.php/kwidget";
+$swfUrl .= "/cache_st/" . time()+(60*15);
+$swfUrl .= "/wid/_" . $partner_id;
+$swfUrl .= "/uiconf_id/" . $uiconf_id;
+$swfUrl .= "/entry_id/" . $entry_id;
+
+$thumbnailUrl = "http://". $partner_cdnHost ."/p/". $partner_id ."/sp/". $partner_id ."00/thumbnail". $entry_id ."/width/120/height/90/bgcolor/000000/type/2";
 ?>
 <!doctype html>
 <html>
@@ -10,7 +25,7 @@ if( ! $sf_params->has('p') || $sf_params->has['e'] ) {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Kaltura Player</title>
-	<script src="/html5/html5lib/v1.2/mwEmbedLoader.php"></script>
+	<script src="/html5/html5lib/v<?php echo $html5_version; ?>/mwEmbedLoader.php"></script>
 </head>
 <body>
 	<div id="main">
@@ -26,13 +41,38 @@ if( ! $sf_params->has('p') || $sf_params->has['e'] ) {
 				<input style="width: 100px; font-size: 12px;" id="showVideo" type="button" value="Change entry" disabled="true">
 			</div><br />
 */?>
-			<div id="videoContainer" style="width:400px"></div>
-			<iframe src="/html5/html5lib/v1.2/mwEmbedFrame.php/entry_id/<?php echo $sf_params->get('e');?>/wid/_<?php echo $sf_params->get('p'); ?>/uiconf_id/<?php echo $sf_params->get('u'); ?>/p/<?php echo $sf_params->get('p'); ?>" width="400" height="300" frameborder="0"></iframe>
+			<div id="videoContainer" style="width:400px">
+			    <object id="kaltura_player" name="kaltura_player" type="application/x-shockwave-flash" 
+				    allowFullScreen="true" allowNetworking="all" allowScriptAccess="always" height="333" width="400" 
+				    xmlns:dc="http://purl.org/dc/terms/" 
+				    xmlns:media="http://search.yahoo.com/searchmonkey/media/" 
+				    rel="media:video" 
+				    resource="<?php echo $swfUrl; ?>" 
+				    data="<?php echo $swfUrl; ?>">
+				<param name="allowFullScreen" value="true" />
+				<param name="allowNetworking" value="all" />
+				<param name="allowScriptAccess" value="always" />
+				<param name="bgcolor" value="#000000" />
+				<param name="flashVars" value="&" />
+				<param name="movie" value="<?php echo $swfUrl; ?>" />
+				<a href="http://corp.kaltura.com">video platform</a> 
+				<a href="http://corp.kaltura.com/video_platform/video_management">video management</a> 
+				<a href="http://corp.kaltura.com/solutions/video_solution">video solutions</a> 
+				<a href="http://corp.kaltura.com/video_platform/video_publishing">video player</a> 
+				<a rel="media:thumbnail" href="<?php echo $thumbnailUrl; ?>"></a>
+				<span property="dc:description" content=""></span>
+				<span property="media:title" content="Kaltura Video"></span>
+				<span property="media:width" content="400"></span>
+				<span property="media:height" content="333"></span> 
+				<span property="media:type" content="application/x-shockwave-flash"></span> 
+			   </object>
+			</div>
 
 			</div><!-- end contwrap -->
 		</div><!-- end content -->
 	</div><!-- end #main -->
 <?php /*
+			<iframe src="/html5/html5lib/v1.2/mwEmbedFrame.php/entry_id//wid/_<?php echo $sf_params->get('partner_id'); ?>/uiconf_id/<?php echo $sf_params->get('u'); ?>/p/<?php echo $sf_params->get('p'); ?>" width="400" height="300" frameborder="0"></iframe>
 <script type="text/javascript">
 var partnerId = <?php echo $sf_params->get('p'); ?>;
 var uiConf = <?php echo $sf_params->get('u'); ?>;
