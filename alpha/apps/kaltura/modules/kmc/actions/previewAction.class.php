09c3f34b8ff6e71cb97e4bfa317a6dbdf8fb2b23
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-11 13:43:27 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57586 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/actions/previewAction.class.php
+++ alpha/apps/kaltura/modules/kmc/actions/previewAction.class.php
@@ -6,26 +6,26 @@ class previewAction extends kalturaAction
 {
 	public function execute ( ) 
 	{
-		$html5_version = kConf::get('html5_version');
+		$this->html5_version = kConf::get('html5_version');
 
-		$uiconf_id = $this->getRequestParameter('uiconf_id');
-		if(!$uiconf_id)
+		$this->uiconf_id = $this->getRequestParameter('uiconf_id');
+		if(!$this->uiconf_id)
 			KExternalErrors::dieError(KExternalErrors::MISSING_PARAMETER, 'uiconf_id');
 
-		$uiConf = uiConfPeer::retrieveByPK($uiconf_id);
-		if(!$uiConf)
+		$this->uiConf = uiConfPeer::retrieveByPK($uiconf_id);
+		if(!$this->uiConf)
 			KExternalErrors::dieError(KExternalErrors::UI_CONF_NOT_FOUND);
 
-		$partner_id = $this->getRequestParameter('partner_id', $uiConf->getPartnerId());
-		if(!$partner_id)
+		$this->partner_id = $this->getRequestParameter('partner_id', $this->uiConf->getPartnerId());
+		if(!$this->partner_id)
 			KExternalErrors::dieError(KExternalErrors::MISSING_PARAMETER, 'partner_id');
 
-		$entry_id = $this->getRequestParameter('entry_id');
-		if(!$entry_id)
+		$this->entry_id = $this->getRequestParameter('entry_id');
+		if(!$this->entry_id)
 			KExternalErrors::dieError(KExternalErrors::MISSING_PARAMETER, 'entry_id');
 
-		$partner_host = myPartnerUtils::getHost($partner_id);
-		$partner_cdnHost = myPartnerUtils::getCdnHost($partner_id);
+		$this->partner_host = myPartnerUtils::getHost($partner_id);
+		$this->partner_cdnHost = myPartnerUtils::getCdnHost($partner_id);
 	}
 }
 ?>
