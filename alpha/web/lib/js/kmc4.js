a63e4d95c815001f60349ea11974adf337802480
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-13 15:18:27 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57675 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/web/lib/js/kmc4.js
+++ alpha/web/lib/js/kmc4.js
@@ -384,6 +384,7 @@ kmc.utils = {
         XD.receiveMessage(function(message){
             if(message.data == "reload") {
                 kmc.utils.closeModal();
+		alert(kmc.vars.service_url + "/index.php/kmc/#account|user");
                 window.location.href = kmc.vars.service_url + "/index.php/kmc/#account|user";
             } else {
                 kmc.utils.closeModal();
