3d15724288595c5cf81b307ef62041cca49dc9fe
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-17 17:28:02 +0000

    merge from cassiopeia tag
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57955 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/Partner.php
+++ alpha/lib/model/Partner.php
@@ -36,7 +36,7 @@ class Partner extends BasePartner
 	
 	const MAX_ACCESS_CONTROLS = 24;
 	
-	const MAX_NUMBER_OF_CATEGORIES = 200;
+	const MAX_NUMBER_OF_CATEGORIES = 1000;
 	
 	const CATEGORIES_LOCK_TIMEOUT = 300; // in seconds
 	
