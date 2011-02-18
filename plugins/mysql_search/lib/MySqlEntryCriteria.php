4a237798757538372af760b940466dd4ae36ab5d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-18 06:44:59 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@57965 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/mysql_search/lib/MySqlEntryCriteria.php
+++ plugins/mysql_search/lib/MySqlEntryCriteria.php
@@ -2,20 +2,27 @@
 
 class MySqlEntryCriteria extends MySqlCriteria
 {
+	/**
+	 * @var SearchEntryPeer
+	 */
+	private $peer = null;
+	
 	public static $mySqlFields = array(
 		'plugins_data',
 		'entry.DURATION_TYPE' => 'duration_type',
 		'entry.SEARCH_TEXT' => 'search_text',
 	);
-	
-	public static $mySqlOrderFields = array(
-	);
-	
-	public static $mySqlTypes = array(
-		'metadata' => 'string',
-		'duration_type' => 'string',
-		'search_text' => 'string',
-	);
+
+	/* (non-PHPdoc)
+	 * @see MySqlCriteria::getSearchPeer()
+	 */
+	protected function getSearchPeer()
+	{
+		if(!$this->peer)
+			$this->peer = new SearchEntryPeer();
+			
+		return $this->peer;
+	}	
 	
 	/**
 	 * @return criteriaFilter
@@ -23,87 +30,6 @@ class MySqlEntryCriteria extends MySqlCriteria
 	protected function getDefaultCriteriaFilter()
 	{
 		return entryPeer::getCriteriaFilter();
-	}
-	
-	/**
-	 * @return string
-	 */
-	protected function getMySqlIndexName()
-	{
-		return kMySqlSearchManager::getMySqlIndexName(entryPeer::TABLE_NAME);;
-	}
-	
-	protected function executeMySql($index, $wheres, $orderBy, $limit, $setLimit)
-	{
-		$sql = "SELECT entry_id FROM $index $wheres $orderBy LIMIT $limit";
-		
-		// TODO - add DB config plugin
-		$pdo = DbManager::getConnection(MySqlSearchPlugin::getDbConnectionName());
-		$stmt = $pdo->query($sql);
-		if(!$stmt)
-		{
-			KalturaLog::err("Invalid mySql query [$sql]");
-			return;
-		}
-		
-		$ids = $stmt->fetchAll(PDO::FETCH_COLUMN, 2);
-		
-		if(count($this->entryIds))
-		{
-			foreach($this->entryIds as $comparison => $entryIds)
-			{
-				// keeps only ids that appears in both arrays
-				if($comparison == Criteria::IN)
-				{
-					$ids = array_intersect($ids, array_keys($entryIds));
-				}
-				
-				// removes ids that appears in the comparison array
-				if($comparison == Criteria::NOT_IN)
-				{
-					$ids = array_diff($ids, array_keys($entryIds));
-				}
-			}
-		}
-		KalturaLog::debug("Found " . count($ids) . " ids");
-		
-		foreach($this->keyToRemove as $key)
-		{
-			KalturaLog::debug("Removing key [$key] from criteria");
-			$this->remove($key);
-		}
-		
-		$this->addAnd(entryPeer::ID, $ids, Criteria::IN);
-		
-		$this->recordsCount = 0;
-		
-		if(!$this->doCount)
-			return;
-			
-		if($setLimit)
-		{
-			$this->setOffset(0);
-			
-			$sql = "show meta";
-			$stmt = $pdo->query($sql);
-			$meta = $stmt->fetchAll(PDO::FETCH_NAMED);
-			if(count($meta))
-			{
-				foreach($meta as $metaItem)
-				{
-					KalturaLog::debug("MySql query " . $metaItem['Variable_name'] . ': ' . $metaItem['Value']);
-					if($metaItem['Variable_name'] == 'total_found')
-						$this->recordsCount = (int)$metaItem['Value'];
-				}
-			}
-		}
-		else
-		{
-			$c = clone $this;
-			$c->setLimit(null);
-			$c->setOffset(null);
-			$this->recordsCount = entryPeer::doCount($c);
-		}
 	}
 	
 	/**
@@ -230,101 +156,16 @@ class MySqlEntryCriteria extends MySqlCriteria
 		parent::applyFilter($filter);
 	}
 
-	/* (non-PHPdoc)
-	 * @see propel/util/Criteria#getNewCriterion()
-	 */
-	public function getNewCriterion($column, $value, $comparison = null)
+	public function getSearchFieldName($fieldName)
 	{
-		return new MySqlCriterion('MySqlEntryCriteria', $this, $column, $value, $comparison);
-	}
-
-	/* (non-PHPdoc)
-	 * @see propel/util/Criteria#add()
-	 */
-	public function add($p1, $value = null, $comparison = null)
-	{
-		if ($p1 instanceof Criterion)
-		{
-			$oc = $this->getCriterion($p1->getColumn());
-			if(!is_null($oc) && $oc->getValue() == $p1->getValue() && $oc->getComparison() != $p1->getComparison())
-				return $this;
-				
-			return parent::add($p1);
-		}
-		
-		$nc = new MySqlCriterion('MySqlEntryCriteria', $this, $p1, $value, $comparison);
-		return parent::add($nc);
-	}
-
-	/* (non-PHPdoc)
-	 * @see propel/util/Criteria#addAnd()
-	 */
-	public function addAnd($p1, $p2 = null, $p3 = null)
-	{
-		if (is_null($p3)) 
-			return parent::addAnd($p1, $p2, $p3);
-			
-		// addAnd(column, value, comparison)
-		$nc = new MySqlCriterion('MySqlEntryCriteria', $this, $p1, $p2, $p3);
-		$oc = $this->getCriterion($p1);
-		
-		if ( !is_null($oc) )
-		{
-			// no need to add again
-			if($oc->getValue() != $p2 || $oc->getComparison() != $p3)
-				$oc->addAnd($nc);
-				
-			return $this;
-		}
-			
-		return $this->add($nc);
-	}
-	
-	public function setEntryIds($comparison, $entryIds)
-	{
-		$this->entryIds[$comparison] = $entryIds;
-	}
-	
-	public function hasMySqlFieldName($fieldName)
-	{
-		if(strpos($fieldName, '.') === false)
-		{
-			$fieldName = strtoupper($fieldName);
-			$fieldName = "entry.$fieldName";
-		}
-			
-		return isset(self::$mySqlFields[$fieldName]);
-	}
-	
-	public function getMySqlOrderFields()
-	{
-		return self::$mySqlOrderFields;
-	}
-	
-	public function getMySqlFieldName($fieldName)
-	{
-		if(strpos($fieldName, '.') === false)
-		{
-			$fieldName = strtoupper($fieldName);
-			$fieldName = "entry.$fieldName";
-		}
-			
 		if(!isset(self::$mySqlFields[$fieldName]))
 			return $fieldName;
 			
 		return self::$mySqlFields[$fieldName];
 	}
 	
-	public function getMySqlFieldType($fieldName)
-	{
-		if(!isset(self::$mySqlTypes[$fieldName]))
-			return null;
-			
-		return self::$mySqlTypes[$fieldName];
-	}
-	
 	public function hasMatchableField ( $field_name )
 	{
-		return in_array($field_name, array("name", "description", "tags", "admin_tags", "categories_ids", "flavor_params_ids", "duration_type", "search_text"));
+		return in_array($field_name, array("name", "description", "tags", "admin_tags", "categories_ids", "flavor_params_ids", "duration_type"));
 	}
 }
