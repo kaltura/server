5edf42845e4160987afaf713c6f954a5c4d41ff8
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-21 09:45:59 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58055 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/templates/kmc4Success.php
+++ alpha/apps/kaltura/modules/kmc/templates/kmc4Success.php
@@ -149,7 +149,7 @@ $currentAccount = '';
 if( count($allowedPartners) > 1 ) {
     foreach( $allowedPartners as $p ) {
 	if($p['id'] == $partner_id) {
-	    $currentAccount = ' &nbsp;|&nbsp; Account: '.  $p['name'] .' ( <a id="ChangePartner" href="#change_partner">Change Account</a> ) &nbsp;';
+	    $currentAccount = ' &nbsp;|&nbsp; Account: '.  $p['name'] .' &nbsp;( <a id="ChangePartner" href="#change_partner">Change Account</a> ) &nbsp;';
 	    break;
 	}
     }
