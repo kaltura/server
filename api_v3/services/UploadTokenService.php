c7190aa6399344fafd348717216c12d802eea72c
etam <etam@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-02-26 22:46:49 +0000

    upload - if the token was generated on another datacenter, proxy the upload action there
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58265 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/services/UploadTokenService.php
+++ api_v3/services/UploadTokenService.php
@@ -82,7 +82,14 @@ class UploadTokenService extends KalturaBaseService
 		$uploadTokenDb = UploadTokenPeer::retrieveByPK($uploadTokenId);
 		if (is_null($uploadTokenDb))
 			throw new KalturaAPIException(KalturaErrors::UPLOAD_TOKEN_NOT_FOUND);
-			
+
+		// if the token was generated on another datacenter, proxy the upload action there
+		$remoteDCHost = kUploadTokenMgr::getRemoteHostForUploadToken($uploadTokenId, kDataCenterMgr::getCurrentDcId());
+		if($remoteDCHost)
+		{
+			kFile::dumpApiRequest($remoteDCHost);
+		}
+
 		$uploadTokenMgr = new kUploadTokenMgr($uploadTokenDb);
 		try
 		{
