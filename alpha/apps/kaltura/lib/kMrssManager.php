1f8bd1c254279030b35dd2f7cf66485bcd824769
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-05 13:40:54 +0000

    added fields to entry item
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58470 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/kMrssManager.php
+++ alpha/apps/kaltura/lib/kMrssManager.php
@@ -200,6 +200,7 @@ class kMrssManager
 		$mrss->addChild('userId', $entry->getPuserId(true));
 		$mrss->addChild('name', self::stringToSafeXml($entry->getName()));
 		$mrss->addChild('description', self::stringToSafeXml($entry->getDescription()));
+		$mrss->addChild('thumbnailUrl', $entry->getThumbnailUrl());
 		$tags = $mrss->addChild('tags');
 		foreach(explode(',', $entry->getTags()) as $tag)
 			$tags->addChild('tag', self::stringToSafeXml($tag));
