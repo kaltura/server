27408597cc3e5e29ed4072d2e2283e9914f15231
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-06 11:44:25 +0000

    added default value 1
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58520 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2011-03-01_alter_syndication_feed.sql
+++ deployment/updates/sql/2011-03-01_alter_syndication_feed.sql
@@ -1,3 +1,3 @@
 ALTER TABLE  `syndication_feed`
 ADD `custom_data` TEXT AFTER `created_at`,
-ADD `display_in_search` TINYINT after `custom_data`;
+ADD `display_in_search` TINYINT after `custom_data` default 1;
