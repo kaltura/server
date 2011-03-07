61c428a754368be4e758e68a495fb0437458da11
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-07 09:56:10 +0000

    changed class name
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58572 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/syndicationFeed/KalturaGenericXsltSyndicationFeed.php
+++ api_v3/lib/types/syndicationFeed/KalturaGenericXsltSyndicationFeed.php
@@ -3,7 +3,7 @@
  * @package api
  * @subpackage objects
  */
-class KalturaXsltSyndicationFeed extends KalturaGenericSyndicationFeed
+class KalturaGenericXsltSyndicationFeed extends KalturaGenericSyndicationFeed
 {
 	/**
 	*
