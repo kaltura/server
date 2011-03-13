d48846f2905c97d4fc304d90d1928a9df2cf3927
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-13 14:48:55 +0000

    fix - check for _swf existence before querying its attributes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58771 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/document/lib/kdl_transcoders/KDLTranscoderPdf2Swf.php
+++ plugins/document/lib/kdl_transcoders/KDLTranscoderPdf2Swf.php
@@ -15,34 +15,35 @@ class KDLTranscoderPdf2Swf extends KDLOperatorBase
 		/* PDF to SWF - will use SWFTOOLS */
 		
 		$cmdStr = '';
-		if ($target->_swf->_flashVersion) {
-			$cmdStr .= '--set flashversion='.$target->_swf->_flashVersion.' ';
+		if($target->_swf) {
+			if ($target->_swf->_flashVersion) {
+				$cmdStr .= '--set flashversion='.$target->_swf->_flashVersion.' ';
+			}
+			if ($target->_swf->_zoom) {
+				$cmdStr .= '--set zoom='.$target->_swf->_zoom.' ';
+			}
+			if ($target->_swf->_zlib) {
+				$cmdStr .= '--zlib ';
+			}
+			if ($target->_swf->_jpegQuality) {
+				$cmdStr .= '--jpegquality '.$target->_swf->_jpegQuality.' ';
+			}
+			if ($target->_swf->_sameWindow) {
+				$cmdStr .= '--samewindow ';
+			}
+			if ($target->_swf->_insertStop) {
+				$cmdStr .= '--stop ';
+			}
+			if ($target->_swf->_useShapes) {
+				$cmdStr .= '--shapes ';
+			}
+			if ($target->_swf->_storeFonts) {
+				$cmdStr .= '--fonts ';
+			}
+			if ($target->_swf->_flatten) {
+				$cmdStr .= '--flatten ';
+			}
 		}
-		if ($target->_swf->_zoom) {
-			$cmdStr .= '--set zoom='.$target->_swf->_zoom.' ';
-		}
-		if ($target->_swf->_zlib) {
-			$cmdStr .= '--zlib ';
-		}
-		if ($target->_swf->_jpegQuality) {
-			$cmdStr .= '--jpegquality '.$target->_swf->_jpegQuality.' ';
-		}
-		if ($target->_swf->_sameWindow) {
-			$cmdStr .= '--samewindow ';
-		}
-		if ($target->_swf->_insertStop) {
-			$cmdStr .= '--stop ';
-		}
-		if ($target->_swf->_useShapes) {
-			$cmdStr .= '--shapes ';
-		}
-		if ($target->_swf->_storeFonts) {
-			$cmdStr .= '--fonts ';
-		}
-		if ($target->_swf->_flatten) {
-			$cmdStr .= '--flatten ';
-		}
-		
 		$cmdStr .= KDLCmdlinePlaceholders::InFileName.
 		           ' -o '.KDLCmdlinePlaceholders::OutFileName;
 		
