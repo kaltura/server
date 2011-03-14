562c996a64b1a859797b50f761109d803d2b0122
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-14 10:44:33 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58792 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/content_distribution/providers/example/lib/admin/ExampleProfileConfiguration.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * @package plugins.exampleDistribution
+ * @subpackage admin
+ */
+class Form_ExampleProfileConfiguration extends Form_ProviderProfileConfiguration
+{
+	protected function addProviderElements()
+	{
+		$element = new Zend_Form_Element_Hidden('providerElements');
+		$element->setLabel('Example Specific Configuration');
+		$element->setDecorators(array('ViewHelper', array('Label', array('placement' => 'append')), array('HtmlTag',  array('tag' => 'b'))));
+		$this->addElements(array($element));
+		
+		$this->addElement('text', 'username', array(
+			'label'			=> 'Username:',
+			'filters'		=> array('StringTrim'),
+		));
+	
+		$this->addElement('text', 'password', array(
+			'label'			=> 'Password:',
+			'filters'		=> array('StringTrim'),
+		));
+	
+		$this->addElement('text', 'accountId', array(
+			'label'			=> 'Account:',
+			'filters'		=> array('StringTrim'),
+		));
+	}
+}
