fe29f1b91028166206643a0b7cf0cf4ea3d93fab
dorp <dorp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-14 14:37:35 +0000

    Moved the domain referrer queries to use the aggregation and not the fact.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58813 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/top_syndication/top_syndication_count_for_objects.sql
+++ alpha/apps/kaltura/lib/reports/top_syndication/top_syndication_count_for_objects.sql
@@ -1,10 +1,9 @@
-SELECT 
-	COUNT( DISTINCT ifnull(ev.referrer,"-")) count_all
-FROM kalturadw.dwh_fact_events  ev 
-WHERE
-{OBJ_ID_CLAUSE}
-AND  ev.partner_id = {PARTNER_ID} /* PARTNER_ID*/
-AND event_time BETWEEN '{FROM_TIME}' /*FROM_TIME*/ 
-		AND '{TO_TIME}' /*TO_TIME*/
-	AND event_type_id IN (2,3,4,5,6,7) /*event types %*/
-	AND entry_media_type_id IN (1,5,6)  /* allow only video & audio & mix */
+SELECT COUNT( DISTINCT IFNULL(ev.referrer_id,"-")) count_all
+FROM kalturadw.dwh_hourly_events_domain_referrer  ev 
+WHERE {OBJ_ID_CLAUSE} AND 
+ev.partner_id = {PARTNER_ID} /*PARTNER_ID*/
+AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
+		AND IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
+		AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 0)
+		AND hour_id < IF (date_id = IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 24)
+AND (count_time_viewed > 0 OR count_plays > 0 OR count_loads > 0)
--- alpha/apps/kaltura/lib/reports/top_syndication/top_syndication_detail_for_objects.sql
+++ alpha/apps/kaltura/lib/reports/top_syndication/top_syndication_detail_for_objects.sql
@@ -1,29 +1,18 @@
-SELECT 
-	a.referrer referrer,
+SELECT 	r.referrer,
 	SUM(count_plays) count_plays,
-	SUM(time_viewed) sum_time_viewed,
-	AVG(time_viewed) avg_time_viewed,
+	SUM(sum_time_viewed) sum_time_viewed,
+	SUM(sum_time_viewed)/SUM(count_plays) avg_time_viewed,
 	SUM(count_loads) count_loads,
 	( SUM(count_plays) / SUM(count_loads) ) load_play_ratio
-FROM (
-	SELECT 
-		ev.referrer,
-		session_id,
-		IF(event_type_id = 3, uid,NULL) uid,
-		MAX(IF(event_type_id IN(4,5,6,7),current_point,NULL))/60000  time_viewed,
-		COUNT(IF(event_type_id = 3, 1,NULL)) count_plays,
-		COUNT(IF(event_type_id = 2, 1,NULL)) count_loads
-	FROM kalturadw.dwh_fact_events  ev 
-	WHERE
-	{OBJ_ID_CLAUSE}
-	AND ev.partner_id = {PARTNER_ID} /* PARTNER_ID*/
-	AND event_time BETWEEN '{FROM_TIME}' /*FROM_TIME*/ 
-			AND '{TO_TIME}' /*TO_TIME*/
-		AND event_type_id IN (2,3,4,5,6,7) /*event types %*/
-		AND entry_media_type_id IN (1,5,6)  /* allow only video & audio & mix */
-	GROUP BY ev.referrer,session_id,uid
-) AS a
-GROUP BY a.referrer 
+FROM kalturadw.dwh_hourly_events_domain_referrer  ev, kalturadw.dwh_dim_referrer r
+WHERE {OBJ_ID_CLAUSE} 
+AND ev.referrer_id = r.referrer_id 
+AND ev.partner_id = {PARTNER_ID} /* PARTNER_ID*/
+AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
+		AND     IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
+	AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 0)
+	AND hour_id < IF (date_id = IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 24)
+AND (count_time_viewed > 0 OR count_plays > 0 OR count_loads > 0)
+GROUP BY r.referrer_id
 ORDER BY {SORT_FIELD}
 LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */
-
