342791c0de6037f8ad2527135328b832a155bbe1
dorp <dorp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-14 11:57:32 +0000

    Fixed the query to display the true value for the number of entries and made the query more efficient.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58805 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/admin_console/admin_console_detail.sql
+++ alpha/apps/kaltura/lib/reports/admin_console/admin_console_detail.sql
@@ -1,5 +1,5 @@
 SELECT
-	aggr_p.status status,
+	aggr_p.STATUS STATUS,
 	aggr_p.partner_id id,
 	aggr_p.partner_name "partner name",
 	aggr_p.created_at "created at",
@@ -13,54 +13,41 @@ SELECT
 	aggr_p.count_audio "count audio",
 	aggr_p.count_mix "count mix",
 	FLOOR(aggr_p.count_bandwidth / 1024) "count bandwidth mb",
-#	FLOOR(all_time_aggr.count_bandwidth_all_time / 1024) "bandwidth all time",
 	aggr_p.count_storage "count storage mb",
 	all_time_aggr.count_storage_all_time "storage all time mb"
 FROM
 (
-	SELECT
-		dim_partner.partner_status_id status,
-    	dim_partner.partner_name,
-    	dim_partner.created_at,
-    	dim_partner.partner_package,
-    	dim_partner.partner_id partner_id,	
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_loads , NULL ) ) count_loads,
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_plays , NULL ) ) count_plays,
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_video , NULL ) ) count_video,
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_audio , NULL ) ) count_audio,
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_mix , NULL ) ) count_mix,
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_image , NULL ) ) count_image,
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_bandwidth , NULL ) ) count_bandwidth,
-		SUM(IF(date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}, aggr_partner.count_storage , NULL ) ) count_storage
-	FROM 
-		kalturadw.dwh_hourly_partner aggr_partner RIGHT JOIN kalturadw.dwh_dim_partners dim_partner
-		ON ( aggr_partner.partner_id = dim_partner.partner_id 
-			AND	aggr_partner.date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID})
-	WHERE 
-		{OBJ_ID_CLAUSE}
-		AND dim_partner.created_date_id BETWEEN 0 and  {TO_DATE_ID}
-	GROUP BY 
-		dim_partner.partner_id
-	ORDER BY 
-		dim_partner.partner_id
-	LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */
-) aggr_p ,
+	SELECT	partner_status_id STATUS,
+    	partner_name,
+    	created_at,
+    	partner_package,
+    	dwh_dim_partners.partner_id partner_id,	
+	SUM(count_loads) count_loads,
+	SUM(count_plays) count_plays,
+	SUM(count_video) count_video,
+	SUM(count_audio) count_audio,
+	SUM(count_mix) count_mix,
+	SUM(count_image) count_image,
+	SUM(count_bandwidth) count_bandwidth,
+	SUM(count_storage) count_storage
+	FROM kalturadw.dwh_hourly_partner RIGHT JOIN kalturadw.dwh_dim_partners
+	ON (dwh_hourly_partner.partner_id = dwh_dim_partners.partner_id AND dwh_hourly_partner.date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID})
+	WHERE  {OBJ_ID_CLAUSE} AND
+	dwh_dim_partners.created_date_id <= {TO_DATE_ID}
+	GROUP BY dwh_dim_partners.partner_id
+	ORDER BY dwh_dim_partners.partner_id
+	LIMIT LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */
+) aggr_p,
 (
-	SELECT
-		aggr_partner.partner_id, 
-		(SUM(aggr_partner.count_video ) + SUM(aggr_partner.count_audio ) + SUM(aggr_partner.count_mix )+ SUM(aggr_partner.count_image )) count_media_all_time,
-		SUM(aggr_partner.count_bandwidth ) count_bandwidth_all_time,
-		SUM(aggr_partner.count_storage ) count_storage_all_time
-	FROM 
-		kalturadw.dwh_hourly_partner aggr_partner RIGHT JOIN kalturadw.dwh_dim_partners dim_partner
-		ON ( aggr_partner.partner_id = dim_partner.partner_id )
-	WHERE 
-		{OBJ_ID_CLAUSE}
-		AND dim_partner.created_date_id BETWEEN 0 and  {TO_DATE_ID}
-	GROUP BY 
-		dim_partner.partner_id
-	ORDER BY 
-		dim_partner.partner_id
-	LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */
+	SELECT	partner_id, 
+		SUM(count_video + count_audio + count_image) count_media_all_time,
+		SUM(count_bandwidth ) count_bandwidth_all_time,
+		SUM(count_storage ) count_storage_all_time
+	FROM kalturadw.dwh_aggr_partner 
+	WHERE {OBJ_ID_CLAUSE} AND
+	date_id <= {TO_DATE_ID}
+	GROUP BY partner_id
+	ORDER BY partner_id
+	LIMIT LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */
 ) all_time_aggr
 WHERE aggr_p.partner_id = all_time_aggr.partner_id
