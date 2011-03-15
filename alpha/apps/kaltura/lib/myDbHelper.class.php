1eb5164f9001e9339352a24a1bc9ca501f2925e8
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-15 14:22:32 +0000

    Added DB_HELPER_CONN_DWH connection const
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58866 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/myDbHelper.class.php
+++ alpha/apps/kaltura/lib/myDbHelper.class.php
@@ -10,6 +10,7 @@ class myDbHelper
 	const DB_HELPER_CONN_SPHINX_LOG = "sphinx_log";
 	const DB_HELPER_CONN_SPHINX_LOG_READ = "sphinx_log_read";
 	const DB_HELPER_CONN_THUMB = "thumbs_sql";
+	const DB_HELPER_CONN_DWH = "dwh";
 	
 	public static $use_alternative_con = null;
 	
