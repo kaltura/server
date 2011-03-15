4f33955581a9252de7182a5d644a74a8cf28d959
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-15 16:59:45 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@58891 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/web/lib/js/kmc4.js
+++ alpha/web/lib/js/kmc4.js
@@ -27,49 +27,7 @@
 //			* Flavors preview to display based on flavor size with logic for not exceeding available screen area
  
 /* WResize: plugin for fixing the IE window resize bug (http://noteslog.com/) */
-(function($){
-	$.fn.wresize=function(f){
-		version='1.1';
-		wresize={
-			fired:false,
-			width:0
-		};
-
-		function resizeOnce(){
-			if($.browser.msie){
-				if(!wresize.fired){
-					wresize.fired=true
-					}else{
-					var version=parseInt($.browser.version,10);
-					wresize.fired=false;
-					if(version<7){
-						return false
-						}else if(version==7){
-						var width=$(window).width();
-						if(width!=wresize.width){
-							wresize.width=width;
-							return false
-							}
-						}
-				}
-		}
-	return true
-}
-function handleWResize(e){
-	if(resizeOnce()){
-		return f.apply(this,[e])
-		}
-	}
-this.each(function(){
-	if(this==window){
-		$(this).resize(handleWResize)
-		}else{
-		$(this).resize(f)
-		}
-	});
-return this
-}
-})(jQuery);
+(function($){$.fn.wresize=function(f){version='1.1';wresize={fired:false,width:0};function resizeOnce(){if($.browser.msie){if(!wresize.fired){wresize.fired=true}else{var version=parseInt($.browser.version,10);wresize.fired=false;if(version<7){return false}else if(version==7){var width=$(window).width();if(width!=wresize.width){wresize.width=width;return false}}}}return true}function handleWResize(e){if(resizeOnce()){return f.apply(this,[e])}}this.each(function(){if(this==window){$(this).resize(handleWResize)}else{$(this).resize(f)}});return this}})(jQuery);
 
 $(window).load(function(){
 	$(window).wresize(kmc.utils.resize);
