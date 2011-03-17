98d01cba2f60313a03ef5e38258b98c423cd2c51
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-17 20:04:37 +0000

    added feed description
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59018 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/syndicationFeed/KalturaGenericSyndicationFeed.php
+++ api_v3/lib/types/syndicationFeed/KalturaGenericSyndicationFeed.php
@@ -5,8 +5,25 @@
  */
 class KalturaGenericSyndicationFeed extends KalturaBaseSyndicationFeed
 {
+    /**
+    * feed description
+    * 
+    * @var string
+    */
+    public $feedDescription;
+        
     function __construct()
 	{
 		$this->type = KalturaSyndicationFeedType::KALTURA;
+	}
+	
+	private static $mapBetweenObjects = array
+	(
+		"feedDescription",
+	);
+	
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), self::$mapBetweenObjects);
 	}
 }
