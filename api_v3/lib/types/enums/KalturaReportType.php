2f5333b42daea17eeb2e78a52f7b70a516780b70
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-24 16:07:18 +0000

    Removed ADMIN_CONSOLE
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59298 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/enums/KalturaReportType.php
+++ api_v3/lib/types/enums/KalturaReportType.php
@@ -16,6 +16,6 @@ class KalturaReportType extends KalturaEnum
 	const TOP_CONTRIBUTORS = 5;
 	const TOP_SYNDICATION = 6;
 	const CONTENT_CONTRIBUTIONS = 7;
-//	const ADMIN_CONSOLE = 10;	
+//	const ADMIN_CONSOLE = 10;		// shouldn't be accessable to users through the API
 }
 ?>
