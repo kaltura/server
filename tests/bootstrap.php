1f737d7e074331f0c41eaad791c0e3a65c3f2ac5
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-28 14:46:09 +0000

    Loads tests classes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59430 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,49 @@
 <?php
+require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."infra".DIRECTORY_SEPARATOR."bootstrap_base.php");
+define("KALTURA_API_PATH", KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."api_v3");
+define("KALTURA_TESTS_PATH", KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."tests");
 
-// load all required classes
-require_once(dirname(__FILE__).'/../alpha/config/sfrootdir.php');
-require_once(dirname(__FILE__).'/../api_v3/bootstrap.php');
+require_once (KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR.'alpha'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'kConf.php');
 
-// tests can include the following path if they need to use a Kalturaclient
-DEFINE('KALTURA_CLIENT_PATH', dirname(__FILE__).'/../generator/output/php5full/KalturaClient.php');
+
+// Autoloader
+require_once(KALTURA_INFRA_PATH.DIRECTORY_SEPARATOR."KAutoloader.php");
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "infra", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_TESTS_PATH, "base", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_TESTS_PATH, "lib", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_TESTS_PATH, "api", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_API_PATH, "lib", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_API_PATH, "services", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "alpha", "plugins", "*")); // needed for testmeDoc
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "generator")); // needed for testmeDoc
+KAutoloader::setClassMapFilePath(kConf::get("cache_root_path") . '/tests/classMap.cache');
+//KAutoloader::dumpExtra();
+KAutoloader::register();
+
+
+// Timezone
+date_default_timezone_set(kConf::get("date_default_timezone")); // America/New_York
+
+
+// Logger
+$loggerConfigPath = KALTURA_TESTS_PATH.DIRECTORY_SEPARATOR."base".DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."logger.ini";
+
+try // we don't want to fail when logger is not configured right
+{
+	$config = new Zend_Config_Ini($loggerConfigPath);
+}
+catch(Zend_Config_Exception $ex)
+{
+	$config = null;
+}
+
+KalturaLog::initLog($config);
+
+// for places where kLog is used
+kLog::setLogger(KalturaLog::getInstance());
+
 
 // set DB
 DbManager::setConfig(kConf::getDB());
