d066db68c2c73fb8674b1469cbbf584250fe9603
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-03-28 20:31:45 +0000

    missing ;
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59455 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2011-03-27_flavor_params_iphone_ipad_data_update.sql
+++ deployment/updates/sql/2011-03-27_flavor_params_iphone_ipad_data_update.sql
@@ -8,7 +8,7 @@ update flavor_params
     conversion_engines_extra_params='-flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 400k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2  -vsync 2 | -flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 400k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2 -vsync 2'
   where isnull(deleted_at) and partner_id!=-1 and tags not like '%iphonenew%' and tags like '%iphone%'  
     and name = 'Mobile (H264)'
-    and frame_rate=25
+    and frame_rate=25;
 
 # for 23 records
 update flavor_params
@@ -20,7 +20,7 @@ update flavor_params
     conversion_engines_extra_params='-flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 400k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2  -vsync 2 | -flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 400k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2 -vsync 2'
   where isnull(deleted_at) and partner_id!=-1 and tags not like '%iphonenew%' and tags like '%iphone%'  
     and frame_rate=25
-    and (tags='iphone,web' or tags='iphone' or tags='web,iphone,android')
+    and (tags='iphone,web' or tags='iphone' or tags='web,iphone,android');
 
 # for 13 records - test accounts?
 update flavor_params
@@ -32,7 +32,7 @@ update flavor_params
     conversion_engines_extra_params='-flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 400k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2  -vsync 2 | -flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 400k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2 -vsync 2'
   where isnull(deleted_at) and partner_id!=-1 and tags not like '%iphonenew%' and tags like '%iphone%'  
     and frame_rate=25
-  and partner_id in(308862,336081,225802)
+  and partner_id in(308862,336081,225802);
 
 #ipad - 310
 update flavor_params
@@ -43,4 +43,4 @@ update flavor_params
     conversion_engines_extra_params='-flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 800k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2  -vsync 2 | -flags +loop+mv4 -cmp 256 -partitions +parti4x4+partp8x8+partb8x8 -trellis 1 -refs 1 -me_range 16 -keyint_min 20 -sc_threshold 40 -i_qfactor 0.71 -bt 800k -maxrate 1200k -bufsize 1200k -rc_eq ''blurCplx^(1-qComp)'' -level 30 -async 2 -vsync 2'
   where isnull(deleted_at) and partner_id!=-1 and tags not like '%ipadnew%' and tags like '%ipad%'  
     and frame_rate in (0,25)
-    and name='iPad'
+    and name='iPad';
