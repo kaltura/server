c3806df46aaf92f96e2c35708480bc19d4b3cba8
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-01 00:57:31 +0000

    Added DropFolder custom data parameters
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59657 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/DropFolder.php
+++ alpha/lib/model/DropFolder.php
@@ -13,6 +13,87 @@
  * @package Core
  * @subpackage model
  */
-class DropFolder extends BaseDropFolder {
-
+class DropFolder extends BaseDropFolder
+{
+	
+	// ------------------------------------------
+	// -- Custom data columns -------------------
+	// ------------------------------------------
+	
+	const FILE_SIZE_CHECK_INTERVAL_COLUMN = 'file_size_check_interval';
+	const FILE_HANDLERS_CONFIG_COLUMN     = 'file_handlers_config';
+	const UNMATCHED_FILE_POLICY_COLUMN    = 'unmatched_file_policy';
+	const FILE_DELETE_POLICY_COLUMN       = 'file_delete_policy';
+	const AUTO_FILE_DELETE_POLICY_COLUMN  = 'auto_file_delete_days';
+	
+	
+	// File size check interval - value in seconds
+	
+	public function getFileSizeCheckInterval()
+	{
+		return $this->getFromCustomData(self::FILE_SIZE_CHECK_INTERVAL_COLUMN);
+	}
+	
+	public function setFileSizeCheckInterval($interval)
+	{
+		$this->putInCustomData(self::FILE_SIZE_CHECK_INTERVAL_COLUMN, $interval);
+	}
+	
+	
+	// File handlers configuration
+	// The configuration is a key-value array where the key is from IngestionFileHandlerType and the value is the file handler configuration array
+	
+	public function getFileHandlersConfig()
+	{
+		$serializedConfig = $this->getFromCustomData(self::FILE_HANDLERS_CONFIG_COLUMN);
+		$configArray = unserialize($serializedConfig);
+		return $configArray;
+	}
+	
+	public function setFileHandlersConfig($fileHandlersConfig)
+	{
+		$serializedConfig = serialize($fileHandlersConfig);
+		$this->putInCustomData(self::FILE_HANDLERS_CONFIG_COLUMN, $serializedConfig);
+	}
+	
+	
+	// Unmatched file policy
+	
+	public function getUnmatchedFilePolicy()
+	{
+		return $this->getFromCustomData(self::UNMATCHED_FILE_POLICY_COLUMN);
+	}
+	
+	public function setUnmatchedFilePolicy($policy)
+	{
+		$this->putInCustomData(self::UNMATCHED_FILE_POLICY_COLUMN, $policy);
+	}
+	
+	
+	// File delete policy
+	
+	public function getFileDeletePolicy()
+	{
+		return $this->getFromCustomData(self::FILE_DELETE_POLICY_COLUMN);
+	}
+	
+	public function setFileDeletePolicy($policy)
+	{
+		$this->putInCustomData(self::FILE_DELETE_POLICY_COLUMN, $policy);
+	}
+	
+	
+	// Automatic file delete days
+		
+	public function getAutoFileDeleteDays()
+	{
+		return $this->getFromCustomData(self::AUTO_FILE_DELETE_POLICY_COLUMN);
+	}
+	
+	public function setAutoFileDeleteDays($days)
+	{
+		$this->putInCustomData(self::AUTO_FILE_DELETE_POLICY_COLUMN, $days);
+	}
+	
+	
 } // DropFolder
