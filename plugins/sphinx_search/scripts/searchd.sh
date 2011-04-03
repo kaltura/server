1bbd41e08f59aadfa0df9b7e52871e5aafa5ef0a
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-03 08:44:06 +0000

    some cosmetic changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59673 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/scripts/searchd.sh
+++ plugins/sphinx_search/scripts/searchd.sh
@@ -9,9 +9,12 @@
 # by Vladimir Fedorkov Mar 1, 2006, info@astellar.com 
 # Slightly modified by Kaltura
 #
+# Source function library.
+#. /etc/rc.d/init.d/functions
+
 # public domain
 
-BASE_PATH=@BIN_DIR@
+BASE_PATH=@SPHINX_BIN_DIR@
 PID_FILE=@BASE_DIR@/sphinx/searchd.pid
 CONFIG_FILE=@APP_DIR@/configurations/sphinx/kaltura.conf
 
@@ -23,16 +26,45 @@ prog="searchd"
 
 export prog RETVAL EXEC_PATH SUDO_USER CONFIG_FILE
 
+echo_success() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "["
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
+  echo -n $"  OK  "
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 0
+}
+
+echo_failure() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "["
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
+  echo -n $"FAILED"
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 1
+}
+
 do_start() {
-	echo "Starting $prog"
+	echo -n $"Starting:"
 	$EXEC_PATH/$prog --config $CONFIG_FILE
 	RETVAL=$?
 	echo
+	if [ "$RETVAL" -eq 0 ]; then
+		echo_success
+        echo
+	else
+		echo_failure
+        echo
+	fi
 	return $RETVAL
 }
 
 do_stop() {
-	echo "Stopping $prog"
+	echo -n $"Shutting down:"
 	if [ -e $PID_FILE ] ; then
 		kill -15 `cat $PID_FILE`
 		sleep 5
@@ -41,7 +73,13 @@ do_stop() {
 		fi
 	fi
 	RETVAL=$?
-	echo
+	if [ "$RETVAL" -eq 0 ]; then
+		echo_success
+        echo
+	else
+		echo_failure
+        echo
+	fi
 	return $RETVAL
 }
 
@@ -51,6 +89,17 @@ do_stopwait() {
         RETVAL=$?
         echo
         return $RETVAL
+}
+
+show_status() {
+      KP=$(pgrep -P 1 -f searchd) 
+      if [ "X$KP" != "X"  ]; then
+	  echo "searchd is running as $KP ..."
+	  return 0
+      else
+          echo "Service searchd isn't running"
+          return 1
+      fi
 }
 
 case $* in
@@ -63,13 +112,15 @@ start)
 stop)
 	do_stop
 	;;
-
+status)
+    show_status
+    ;;
 stopwait)
         do_stopwait
         ;;
 
 *)
-	echo "usage: $0 {start|stop|stopwait}" >&2
+	echo "usage: $0 {start|stop|stopwait|status}" >&2
 
 	exit 1
 	;;
