9a2f296502ea7c378201fe92a43adb4834b51ceb
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-04 17:55:30 +0000

    Moving drop folder files to plugin
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59882 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/enums/KalturaDropFolderFileStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package api
- * @subpackage enum
- */
-class KalturaDropFolderFileStatus extends KalturaEnum implements DropFolderFileStatus
-{
-	// see DropFolderFileStatus interface
-}
