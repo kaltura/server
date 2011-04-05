ff83aab794c59d7a685700878ea6f2909e4819cf
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-05 14:06:10 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@59932 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/enums/entryReplacementStatus.php
+++ alpha/lib/enums/entryReplacementStatus.php
@@ -5,6 +5,7 @@
  */ 
 interface entryReplacementStatus extends BaseEnum
 {
+	const NONE = 0;
 	const APPROVED_BUT_NOT_READY = 1;
 	const READY_BUT_NOT_APPROVED = 2;
 	const NOT_READY_AND_NOT_APPROVED = 3;
