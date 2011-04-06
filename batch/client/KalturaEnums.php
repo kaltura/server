73911ea0cf73c4ef44ea25191fec3ce734c772a5
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-06 12:31:35 +0000

    Auto generated
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60000 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/client/KalturaEnums.php
+++ batch/client/KalturaEnums.php
@@ -183,6 +183,26 @@ class KalturaBaseSyndicationFeedOrderBy
  * @package Scheduler
  * @subpackage Client
  */
+class KalturaBatchJobAppErrors
+{
+	const OUTPUT_FILE_DOESNT_EXIST = 11;
+	const OUTPUT_FILE_WRONG_SIZE = 12;
+	const CANNOT_CREATE_DIRECTORY = 13;
+	const NFS_FILE_DOESNT_EXIST = 21;
+	const EXTRACT_MEDIA_FAILED = 31;
+	const CLOSER_TIMEOUT = 41;
+	const ENGINE_NOT_FOUND = 51;
+	const REMOTE_FILE_NOT_FOUND = 61;
+	const REMOTE_DOWNLOAD_FAILED = 62;
+	const CSV_FILE_NOT_FOUND = 71;
+	const CONVERSION_FAILED = 81;
+	const THUMBNAIL_NOT_CREATED = 91;
+}
+
+/**
+ * @package Scheduler
+ * @subpackage Client
+ */
 class KalturaBatchJobErrorTypes
 {
 	const APP = 0;
