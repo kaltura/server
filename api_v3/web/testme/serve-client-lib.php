58e429a0b7b941ab791ab572f80026fe838b7644
etam <etam@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-10 07:28:25 +0000

    no need for serve-client-lib since we serve the files from $root/content/clientlibs
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60129 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/web/testme/serve-client-lib.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once("../../bootstrap.php");
-
-$lib = $_GET['lib'];
-header("Content-Disposition: attachment; filename=\"$lib\"");
-$root = myContentStorage::getFSContentRootPath();
-kFile::dumpFile("$root/content/generator/output/$lib");
