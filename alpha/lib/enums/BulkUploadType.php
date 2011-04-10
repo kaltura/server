347547478150675183221bb7b6fda8232b47fad0
ronic <ronic@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-10 06:46:33 +0000

    Added bulk upload type
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60124 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/enums/BulkUploadType.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @package Core
+ * @subpackage model.enum
+ */
+interface BulkUploadType extends BaseEnum
+{
+	const NONE = 0;
+}
---
+++ api_v3/lib/types/batch/KalturaBulkUploadType.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 
+ * Represents the bulk upload type
+ * @author Roni
+ * @package api
+ * @subpackage objects
+ *
+ */
+class KalturaBulkUploadType extends KalturaObject implements BulkUploadType
+{
+
+}
