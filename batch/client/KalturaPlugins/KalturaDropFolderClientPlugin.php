a2229bdf6c5d7a7681bcbd2b1e9a0d02851526dc
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-11 14:35:49 +0000

    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60245 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ batch/client/KalturaPlugins/KalturaDropFolderClientPlugin.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package Scheduler
+ * @subpackage Client
+ */
+require_once(dirname(__FILE__) . "/../KalturaClientBase.php");
+require_once(dirname(__FILE__) . "/../KalturaEnums.php");
+require_once(dirname(__FILE__) . "/../KalturaTypes.php");
+
+/**
+ * @package Scheduler
+ * @subpackage Client
+ */
+class KalturaDropFolderClientPlugin extends KalturaClientPlugin
+{
+	/**
+	 * @var KalturaDropFolderClientPlugin
+	 */
+	protected static $instance;
+
+	protected function __construct(KalturaClient $client)
+	{
+		parent::__construct($client);
+	}
+
+	/**
+	 * @return KalturaDropFolderClientPlugin
+	 */
+	public static function get(KalturaClient $client)
+	{
+		if(!self::$instance)
+			self::$instance = new KalturaDropFolderClientPlugin($client);
+		return self::$instance;
+	}
+
+	/**
+	 * @return array<KalturaServiceBase>
+	 */
+	public function getServices()
+	{
+		$services = array(
+		);
+		return $services;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'dropFolder';
+	}
+}
+
