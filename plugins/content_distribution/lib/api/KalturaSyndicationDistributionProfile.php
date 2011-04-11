fed140da4ca2a91d809adcac091e989c986dbc04
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-11 19:34:32 +0000

    added validation of xslt on toInsertableObject and toUpdatbleObject
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60278 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/lib/api/KalturaSyndicationDistributionProfile.php
+++ plugins/content_distribution/lib/api/KalturaSyndicationDistributionProfile.php
@@ -24,17 +24,47 @@ class KalturaSyndicationDistributionProfile extends KalturaDistributionProfile
 		'xsl',
 		'feedId',
 	);
-		 
+	
 	public function getMapBetweenObjects()
 	{
 		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
+	}
+		
+	/**
+	 * @param SyndicationDistributionProfile $object_to_fill
+	 * @param array $props_to_skip
+	 * @return SyndicationDistributionProfile
+	 */
+	public function toInsertableObject ( $object_to_fill = null , $props_to_skip = array() )
+	{
+		if(is_null($object_to_fill))
+			$object_to_fill = new SyndicationDistributionProfile();
+		
+		kSyndicationFeedManager::validateXsl($this->xsl);	
+		
+		return parent::toInsertableObject($object_to_fill, $props_to_skip);
+	}
+	
+	/**
+	 * @param SyndicationDistributionProfile $object_to_fill
+	 * @param array $props_to_skip
+	 * @return SyndicationDistributionProfile
+	 */
+	public function toUpdatableObject ( $object_to_fill , $props_to_skip = array() )
+	{
+		if(is_null($object_to_fill))
+			$object_to_fill = new SyndicationDistributionProfile();
+		
+		kSyndicationFeedManager::validateXsl($this->xsl);
+		
+		return parent::toUpdatableObject($object_to_fill, $props_to_skip );
 	}
 	
 	public function toObject($object = null, $skip = array())
 	{
 		if(is_null($object))
 			$object = new SyndicationDistributionProfile();
-			
+		
 		return parent::toObject($object, $skip);
 	}
 }
