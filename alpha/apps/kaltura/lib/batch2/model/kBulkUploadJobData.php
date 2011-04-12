cf9231bfaa99cb90d296bc9b83089f9d8d5ea349
ronic <ronic@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-12 10:56:26 +0000

    Added Job data for bulks + code review changes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60319 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/model/kBulkUploadJobData.php
+++ alpha/apps/kaltura/lib/batch2/model/kBulkUploadJobData.php
@@ -5,9 +5,6 @@
  */
 class kBulkUploadJobData extends kBulkUploadBaseJobData
 {
-	const BULK_UPLOAD_CSV_VERSION_V1 = 1; // 5 values in a row
-	const BULK_UPLOAD_CSV_VERSION_V2 = 2; // 12 values in a row
-	
 	/**
 	 * Created by the API
 	 * 
@@ -49,49 +46,4 @@ class kBulkUploadJobData extends kBulkUploadBaseJobData
 	public function setResultsFileUrl($resultsFileUrl) {
 		$this->resultsFileUrl = $resultsFileUrl;
 	}
-
-
-	//TODO: Roni - delete this once csv KBulkUploadCsvJobData is working
-		/**
-	 * Created by the API
-	 * 
-	 * @var string
-	 */
-	private $csvFilePath;
-	
-	/**
-	 * The version of the csv file
-	 * 
-	 * @var int
-	 */
-	private $csvVersion;
-
-	/**
-	 * @return the $csvFilePath
-	 */
-	public function getCsvFilePath() {
-		return $this->csvFilePath;
-	}
-		
-	/**
-	 * @return the $csvVersion
-	 */
-	public function getCsvVersion() {
-		return $this->csvVersion;
-	}
-	
-	/**
-	 * @param string $csvFilePath
-	 */
-	public function setCsvFilePath($csvFilePath) {
-		$this->csvFilePath = $csvFilePath;
-	}
-	
-	/**
-	 * @param int $csvVersion
-	 */
-	public function setCsvVersion($csvVersion) {
-		$this->csvVersion = $csvVersion;
-	}
-	
 }
