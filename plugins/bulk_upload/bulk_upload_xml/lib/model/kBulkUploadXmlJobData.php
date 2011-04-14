d8644bd40e094785cdcf6ec9d9cc68f7a76abbe1
ronic <ronic@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-14 12:52:40 +0000

    removed all from the XML Bulk upload job data
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60578 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/bulk_upload/bulk_upload_xml/lib/model/kBulkUploadXmlJobData.php
+++ plugins/bulk_upload/bulk_upload_xml/lib/model/kBulkUploadXmlJobData.php
@@ -5,25 +5,5 @@
  */
 class kBulkUploadXmlJobData extends kBulkUploadJobData
 {
-	/**
-	 * Created by the API
-	 * 
-	 * @var string
-	 */
-	private $xmlFilePath;
-			
-	/**
-	 * @return the $xmlFilePath
-	 */
-	public function getXmlFilePath() {
-		return $this->xmlFilePath;
-	}
-
 	
-	/**
-	 * @param string $xmlFilePath
-	 */
-	public function setXmlFilePath($xmlFilePath) {
-		$this->xmlFilePath = $xmlFilePath;
-	}
 }
