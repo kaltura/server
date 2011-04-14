c9a702712092c188c1af105eaf1404c8480b76ea
ronic <ronic@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-14 11:24:12 +0000

    Added KalturaBulkUploadTypes for XML and CSV bulk uploads plugins
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60559 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/bulk_upload/bulk_upload_csv/lib/api/KalturaBulkUploadCsvType.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 
+ * Represents the CSV plugin bulk upload type 
+ * @author Roni
+ *
+ */
+class KalturaBulkUploadCsvType extends KalturaBulkUploadCsvType
+{
+	/**
+	 * 
+	 * Bulk upload CSV type
+	 * @var string
+	 */
+	const CSV = "CSV";
+}
---
+++ plugins/bulk_upload/bulk_upload_xml/lib/api/KalturaBulkUploadXmlType.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 
+ * Represents the XML plugin bulk upload type 
+ * @author Roni
+ *
+ */
+class KalturaBulkUploadXmlType extends KalturaBulkUploadXmlType
+{
+	/**
+	 * 
+	 * Bulk upload XML type
+	 * @var string
+	 */
+	const XML = "XML";
+}
