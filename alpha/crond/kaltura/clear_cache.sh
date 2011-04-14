71015da49563c25871fff87f6e638464f9607863
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-14 12:47:28 +0000

    Merge from production tag
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60575 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/clear_cache.sh
+++ alpha/crond/kaltura/clear_cache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nice -n 19 find /tmp -cmin +15 -name "cache*" -delete
+nice -n 19 find /tmp/cache_v3-600 -type f -cmin +15 -name "cache*" -delete
+nice -n 19 find /tmp -type f -cmin +15 -name "cache*" -delete
