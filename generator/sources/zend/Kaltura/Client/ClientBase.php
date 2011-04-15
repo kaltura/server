8c2cf4d1fa87d1caa13226a428e801d4f7bf59f2
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-15 13:04:48 +0000

    Added log
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60623 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/sources/zend/Kaltura/Client/ClientBase.php
+++ generator/sources/zend/Kaltura/Client/ClientBase.php
@@ -211,7 +211,7 @@ class Kaltura_Client_ClientBase
 		$objectType = reset($xml->objectType);
 			
 		$type = Kaltura_Client_TypeMap::getZendType($objectType);
-		$this->log("Instantiating new object  [$type]");
+		$this->log("Instantiating new object type [$type] server type [$objectType]");
 		$ret = new $type();
 	
 		foreach($xml->children() as $attributeName => $attributeValue)
