a4fd2ec8f90356e0306a8ef21a17914bd7238d2e
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-15 13:57:13 +0000

    Operate zend client
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60641 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/lib/Kaltura/Client/Exception.php
+++ admin_console/lib/Kaltura/Client/Exception.php
@@ -5,6 +5,15 @@
  */
 class Kaltura_Client_Exception extends Exception 
 {
+	const ERROR_GENERIC = -1;
+	const ERROR_UNSERIALIZE_FAILED = -2;
+	const ERROR_FORMAT_NOT_SUPPORTED = -3;
+	const ERROR_UPLOAD_NOT_SUPPORTED = -4;
+	const ERROR_CONNECTION_FAILED = -5;
+	const ERROR_READ_FAILED = -6;
+	const ERROR_INVALID_PARTNER_ID = -7;
+	const ERROR_INVALID_OBJECT_TYPE = -8;
+	
     public function __construct($message, $code) 
     {
     	$this->code = $code;
--- admin_console/lib/Kaltura/View/Helper/FailReasonTranslate.php
+++ admin_console/lib/Kaltura/View/Helper/FailReasonTranslate.php
@@ -237,31 +237,31 @@ class Kaltura_View_Helper_FailReasonTranslate extends Zend_View_Helper_Abstract
 	);
 	
 	private static $clientDescriptions = array(
-		KalturaClientException::ERROR_UNSERIALIZE_FAILED => 'Failed to unserialize response',
-		KalturaClientException::ERROR_FORMAT_NOT_SUPPORTED => 'unsupported format',
-		KalturaClientException::ERROR_UPLOAD_NOT_SUPPORTED => 'upload is not supported',
-		KalturaClientException::ERROR_CONNECTION_FAILED => 'HTTP connection failed',
-		KalturaClientException::ERROR_READ_FAILED => 'Response read failed',
-		KalturaClientException::ERROR_INVALID_PARTNER_ID => 'Invalid partner ID',
-		KalturaClientException::ERROR_INVALID_OBJECT_TYPE => 'Invalid object type',
+		Kaltura_Client_Exception::ERROR_UNSERIALIZE_FAILED => 'Failed to unserialize response',
+		Kaltura_Client_Exception::ERROR_FORMAT_NOT_SUPPORTED => 'unsupported format',
+		Kaltura_Client_Exception::ERROR_UPLOAD_NOT_SUPPORTED => 'upload is not supported',
+		Kaltura_Client_Exception::ERROR_CONNECTION_FAILED => 'HTTP connection failed',
+		Kaltura_Client_Exception::ERROR_READ_FAILED => 'Response read failed',
+		Kaltura_Client_Exception::ERROR_INVALID_PARTNER_ID => 'Invalid partner ID',
+		Kaltura_Client_Exception::ERROR_INVALID_OBJECT_TYPE => 'Invalid object type',
 	);
 	
 	public function failReasonTranslate($status, $errType, $errNumber, $message)
 	{
-		if($status != KalturaBatchJobStatus::FAILED)
-			return $this->view->enumTranslate('KalturaBatchJobStatus', $status);
+		if($status != Kaltura_Client_Enum_BatchJobStatus::FAILED)
+			return $this->view->enumTranslate('Kaltura_Client_Enum_BatchJobStatus', $status);
 			
-		$errTypeDesc = $this->view->enumTranslate('KalturaBatchJobErrorTypes', $errType);
+		$errTypeDesc = $this->view->enumTranslate('Kaltura_Client_Enum_BatchJobErrorTypes', $errType);
 		$ret = $errTypeDesc;
 		
 		switch($errType)
 		{
-			case KalturaBatchJobErrorTypes::APP:
-				$title = $this->view->enumTranslate('KalturaBatchJobAppErrors', $errNumber);
+			case Kaltura_Client_Enum_BatchJobErrorTypes::APP:
+				$title = $this->view->enumTranslate('Kaltura_Client_Enum_BatchJobAppErrors', $errNumber);
 				$ret = "<span title=\"$title\">$errTypeDesc ($errNumber)</span>";
 				break;
 				
-			case KalturaBatchJobErrorTypes::HTTP:
+			case Kaltura_Client_Enum_BatchJobErrorTypes::HTTP:
 				$url = 'http://en.wikipedia.org/wiki/List_of_HTTP_status_codes';
 				
 				if($errNumber > 0)
@@ -274,7 +274,7 @@ class Kaltura_View_Helper_FailReasonTranslate extends Zend_View_Helper_Abstract
 				$ret = "<a title=\"$title\" href=\"$url\" target=\"_blank\">$errTypeDesc ($errNumber)</a>";
 				break;
 				
-			case KalturaBatchJobErrorTypes::CURL:
+			case Kaltura_Client_Enum_BatchJobErrorTypes::CURL:
 				$url = 'http://curl.haxx.se/libcurl/c/libcurl-errors.html';
 				if(isset(self::$curlHashes[$errNumber]))
 					$url .= '#' . self::$curlHashes[$errNumber];
@@ -286,15 +286,15 @@ class Kaltura_View_Helper_FailReasonTranslate extends Zend_View_Helper_Abstract
 				$ret = "<a title=\"$title\" href=\"$url\" target=\"_blank\">$errTypeDesc ($errNumber)</a>";
 				break;
 				
-			case KalturaBatchJobErrorTypes::RUNTIME:
+			case Kaltura_Client_Enum_BatchJobErrorTypes::RUNTIME:
 				$ret = "<span title=\"$message\">$errTypeDesc ($errNumber)</span>";
 				break;
 				
-			case KalturaBatchJobErrorTypes::Kaltura_API:
+			case Kaltura_Client_Enum_BatchJobErrorTypes::KALTURA_API:
 				$ret = "<span title=\"$message\">$errTypeDesc ($errNumber)</span>";
 				break;
 				
-			case KalturaBatchJobErrorTypes::Kaltura_CLIENT:
+			case Kaltura_Client_Enum_BatchJobErrorTypes::KALTURA_CLIENT:
 				$title = $message;
 				if(isset(self::$clientDescriptions[$errNumber]))
 					$title = self::$clientDescriptions[$errNumber];
