e78eb09309796f81c492368d0e02f8ffe63a0de9
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-16 10:53:16 +0000

    Reverted status update
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60683 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/batches/BulkUpload/KAsyncBulkUpload.class.php
+++ batch/batches/BulkUpload/KAsyncBulkUpload.class.php
@@ -87,18 +87,12 @@ class KAsyncBulkUpload extends KBatchBase {
 	{
 		KalturaLog::debug ( "startBulkUpload($job->id)" );
 		
-		if($job->status == KalturaBatchJobStatus::PENDING)
-			$job = $this->updateJob($job, 'Downloading file header', KalturaBatchJobStatus::QUEUED, 1);
-		
 		//Gets the right Engine instance 
 		$engine = KBulkUploadEngine::getEngine($job->jobSubType, $this->taskConfig, $this->kClient, $job);
 		if (is_null ( $engine )) {
 			throw new KalturaException ( "Unable to find bulk upload engine", KalturaBatchJobAppErrors::ENGINE_NOT_FOUND );
 		}
 
-		if($job->status == KalturaBatchJobStatus::QUEUED)
-			$this->updateJob($job, 'Handling bulk [' . $engine->getName() . ']', KalturaBatchJobStatus::PROCESSING, 2);
-		
 		$engine->handleBulkUpload();
 		$job = $engine->getJob();
 		$data = $engine->getData();
@@ -108,7 +102,6 @@ class KAsyncBulkUpload extends KBatchBase {
 			
 		if($engine->shouldRetry())
 		{
-			$this->updateJob($job, null, KalturaBatchJobStatus::RETRY);
 			$this->kClient->batch->resetJobExecutionAttempts($job->id, $this->getExclusiveLockKey(), $job->jobType);
 			return $job;
 		}
