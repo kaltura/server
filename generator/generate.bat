701d500d1b037223e194b4a7bb350ac15e2d8fe5
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-16 06:38:41 +0000

    Clean batch cache after generating new client
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@60661 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.bat
+++ generator/generate.bat
@@ -5,3 +5,5 @@ php generate.php
 
 xcopy /Y /S /R C:\web\content\generator\output\php5ZendClientAdminConsole\* ..\admin_console\lib
 xcopy /Y /S /R C:\web\content\generator\output\batchClient\* ..\batch\client
+
+del /F /S /Q ..\cache\batch\*
--- generator/generate.sh
+++ generator/generate.sh
@@ -7,3 +7,5 @@ php generate.php "$@"
 
 rsync -avC /web/content/generator/output/php5ZendClientAdminConsole/ ../admin_console/lib
 rsync -avC /web/content/generator/output/batchClient/ ../batch/client
+
+rm -fr ../cache/batch/*
