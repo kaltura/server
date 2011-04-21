01bfae156a8ea0f20866209f28e6bf131fa2aa91
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-21 11:18:51 +0000

    PartnerPackageHelper moved to ui infra
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@61052 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/lib/Kaltura/View/Helper/PackageNameById.php
+++ admin_console/lib/Kaltura/View/Helper/PackageNameById.php
@@ -3,6 +3,6 @@ class Kaltura_View_Helper_PackageNameById extends Zend_View_Helper_Abstract
 {
 	public function packageNameById($id)
 	{
-		return Kaltura_PartnerPackageHelper::getPackageNameById($id);
+		return Infra_PartnerPackageHelper::getPackageNameById($id);
 	}
 }
