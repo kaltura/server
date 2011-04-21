110351ad24ae63a0f03c1aeafb42ffb533bc7a2b
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-04-21 12:02:33 +0000

    getByEntryId permissions
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@61066 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/updates/scripts/add_permissions/2011_04_21_assets.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage dragonfly.roles_and_permissions
+ * 
+ * Adds conversionProfile->getDefault permission item
+ * 
+ * No need to re-run after server code deploy
+ */
+
+$script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
+$config = realpath(dirname(__FILE__)) . '/configs/assets.ini';
+passthru("php $script $config");
