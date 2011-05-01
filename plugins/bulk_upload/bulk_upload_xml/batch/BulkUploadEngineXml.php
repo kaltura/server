2c9a30daf39b03c28a03fcd79a29badccea8e9e8
ronic <ronic@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-01 12:08:25 +0000

    Bug fix
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@61487 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/bulk_upload/bulk_upload_xml/batch/BulkUploadEngineXml.php
+++ plugins/bulk_upload/bulk_upload_xml/batch/BulkUploadEngineXml.php
@@ -486,14 +486,12 @@ class BulkUploadEngineXml extends KBulkUploadEngine
 		foreach($noParamsFlavorAssets as $index => $flavorAsset) // Adds all the entry flavors
 		{
 			$flavorResource = $noParamsFlavorResources[$index];
-			$flavorAsset->entryId = $newEntryId; 
 			$this->kClient->flavorAsset->add($newEntryId, $flavorAsset, $flavorResource);
 		}
 	
 		foreach($noParamsThumbAssets as $index => $thumbAsset) //Adds the entry thumb assests
 		{
 			$thumbResource = $noParamsThumbResources[$index];
-			$thumbAsset->entryId = $newEntryId;
 			$this->kClient->thumbAsset->add($newEntryId, $thumbAsset, $thumbResource);
 		}
 				
