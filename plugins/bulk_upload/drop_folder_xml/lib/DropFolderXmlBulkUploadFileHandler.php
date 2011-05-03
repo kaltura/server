0e3f329c53c777a1478b2fa862234a3e8e0bd5ca
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-03 02:40:35 +0000

    Drop folder XML file handler - temporary work - not yet implemented
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@61590 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/bulk_upload/drop_folder_xml/lib/DropFolderXmlBulkUploadFileHandler.php
+++ plugins/bulk_upload/drop_folder_xml/lib/DropFolderXmlBulkUploadFileHandler.php
@@ -5,6 +5,12 @@
  */
 class DropFolderXmlBulkUploadFileHandler extends DropFolderFileHandler
 {	
+	const LOCAL_RESOURCE_NODE_NAME = 'localFileContentResource';
+	const LOCAL_RESOURCE_PATH_ATTRIBUTE = 'filePath';
+	const LOCAL_RESOURCE_FILE_SIZE_PARAM = 'fileSize';
+	const LOCAL_RESOURCE_FILE_CHECKSUM_PARAM = 'fileChecksum';
+	
+	
 	public function getType() 
 	{
 		return KalturaDropFolderFileHandlerType::XML;
@@ -12,9 +18,86 @@ class DropFolderXmlBulkUploadFileHandler extends DropFolderFileHandler
 	
 	public function handle()
 	{
-//		TODO
-//		1. Validate that all files arrived and have the correct size
-//		2. Replace all localFileContentResource with dropFolderFileContentResource
-//		3. add bulk upload of type KalturaBulkUploadType::DROP_FOLDER_XML
+		// check prerequisites
+		$checkConfig = $this->checkConfig();
+		if (!$checkConfig) {
+			return false;
+		}	
+		
+		$xmlPath = realpath($this->dropFolder->path.'/'.$this->dropFolderFile->fileName);
+		if (!$xmlPath) {
+			//TODO: error
+			return false;
+		}
+		
+		$xmlDoc = new DOMDocument();
+		$xmlDoc->loadXML($xmlPath);
+
+		if (!$xmlDoc) {
+			//TODO: error
+			return false;
+		}
+		
+		$localResources = $xmlDoc->getElementsByTagName(self::LOCAL_RESOURCE_NODE_NAME);
+		
+		if (!$localResources) {
+			//TODO: error happened
+			return error;
+		}
+		
+		foreach ($localResources as $local)
+		{
+			$dropFolderFileId = $this->checkFileExists($local);
+			if (is_null($dropFolderFileId)) {
+				//TODO: change status to waiting
+			}
+			$localVerified = $this->verifyLocalResource($local);
+			if ($localVerified) {
+				//TODO: error
+			}
+			$this->replaceResource($local, $dropFolderFileId, $xmlDoc);
+		}
+		
+
+		//TODO: create a temporary XML file from the modified $xmlDoc
+		//TODO: add bulk upload of type KalturaBulkUploadType::DROP_FOLDER_XML
+		//TODO: delete the temporary file
+	
 	}
+	
+	private function checkFileExists(DOMElement $localResource)
+	{
+		$filePath = $localResource->getAttribute(self::LOCAL_RESOURCE_PATH_ATTRIBUTE);
+		//TODO: verify filePath exists in drop folder
+		//TODO: return dropFolderFile id
+	}
+	
+	private function verifyLocalResource(DOMElement $localResource)
+	{
+		$fileSize = $localResource->getElementsByTagName(self::LOCAL_RESOURCE_FILE_SIZE_PARAM);
+		$fileSize = ($fileSize->length > 0) ? $fileSize->item(0)->nodeValue : null;
+		//TODO: verify file size
+		
+		$fileChecksum = $localResource->getElementsByTagName(self::LOCAL_RESOURCE_FILE_CHECKSUM_PARAM);
+		$fileChecksum = ($fileChecksum->length > 0) ? $fileChecksum->item(0)->nodeValue : null;
+		//TODO: verify checksum - need to find out what type of checksum to verify
+		//TODO: set error description and code
+	}
+	
+	private function replaceResource(DOMElement $localResource, $dropFolderFileId, DOMDocument $xmlDoc)
+	{
+		// create a new XML drop folder resource with the given id
+		$newDropFolderResource = $xmlDoc->createElement('dropFolderFileContentResource');
+		$newDropFolderResource->appendChild($xmlDoc->createElement('dropFolderFileId', $dropFolderFileId));
+		
+		// replace the local resource with the new drop folder file resource
+		$parent = $localResource->parentNode;
+		$parent-> replaceChild($newDropFolderResource, $localResource);
+	}
+	
+	private function addBulkUploadJob($xmlData)
+	{
+		//TODO: implement
+	}
+	
 }
