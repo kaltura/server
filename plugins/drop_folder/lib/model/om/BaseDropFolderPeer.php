1abcc2c854d2006772ba738ee7aa503f71f1962c
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-03 12:40:40 +0000

    Regenerate OM classes after adding the query cache and the reloadAfterInsert attribute
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@61639 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/drop_folder/lib/model/om/BaseDropFolderPeer.php
+++ plugins/drop_folder/lib/model/om/BaseDropFolderPeer.php
@@ -234,15 +234,37 @@ abstract class BaseDropFolderPeer {
 		$criteria->clearOrderByColumns(); // ORDER BY won't ever affect the count
 		$criteria->setDbName(self::DATABASE_NAME); // Set the correct dbName
 		
-		// BasePeer returns a PDOStatement
-		$stmt = DropFolderPeer::doCountStmt($criteria, $con);
+		DropFolderPeer::attachCriteriaFilter($criteria);
 
+		$cacheKey = null;
+		$cachedResult = kQueryCache::getCachedQueryResults(
+			$criteria, 
+			kQueryCache::QUERY_TYPE_COUNT,
+			'DropFolderPeer', 
+			$cacheKey);
+		if ($cachedResult !== null)
+		{
+			return $cachedResult;
+		}
+		
+		// set the connection to slave server
+		$con = DropFolderPeer::alternativeCon ($con);
+		
+		// BasePeer returns a PDOStatement
+		$stmt = BasePeer::doCount($criteria, $con);
+		
 		if ($row = $stmt->fetch(PDO::FETCH_NUM)) {
 			$count = (int) $row[0];
 		} else {
 			$count = 0; // no rows returned; we infer that means 0 matches.
 		}
 		$stmt->closeCursor();
+		
+		if ($cacheKey !== null)
+		{
+			kQueryCache::cacheQueryResults($cacheKey, $count);
+		}
+		
 		return $count;
 	}
 	/**
@@ -264,6 +286,68 @@ abstract class BaseDropFolderPeer {
 		}
 		return null;
 	}
+	
+	/**
+	 * Override in order to use the query cache.
+	 * Cache invalidation keys are used to determine when cached queries are valid.
+	 * Before returning a query result from the cache, the time of the cached query
+	 * is compared to the time saved in the invalidation key.
+	 * A cached query will only be used if it's newer than the matching invalidation key.
+	 *  
+	 * @param      Criteria $criteria The Criteria object used to build the SELECT statement.
+	 * @param      string $queryType The type of the query: select / count.
+	 * @return     string The invalidation key that should be checked before returning a cached result for this criteria.
+	 *		 if null is returned, the query cache won't be used - the query will be performed on the DB.
+	 */
+	public static function getCacheInvalidationKeys(Criteria $criteria, $queryType)
+	{
+		return array();
+	}
+
+	/**
+	 * Override in order to filter objects returned from doSelect.
+	 *  
+	 * @param      array $selectResults The array of objects to filter.
+	 */
+	public static function filterSelectResults(&$selectResults)
+	{
+	}
+	
+	/**
+	 * Adds the supplied object array to the instance pool, objects already found in the pool
+	 * will be replaced with instance from the pool.
+	 *  
+	 * @param      array $queryResult The array of objects to get / add to pool.
+	 */
+	public static function updateInstancePool(&$queryResult)
+	{
+		foreach ($queryResult as $curIndex => $curObject)
+		{
+			$objFromPool = DropFolderPeer::getInstanceFromPool($curObject->getPrimaryKey());
+			if ($objFromPool === null)
+			{
+				DropFolderPeer::addInstanceToPool($curObject);
+			}
+			else
+			{
+				$queryResult[$curIndex] = $objFromPool;
+			}
+		}
+	}
+	
+	/**
+	 * Adds the supplied object array to the instance pool.
+	 *  
+	 * @param      array $queryResult The array of objects to add to pool.
+	 */
+	public static function addInstancesToPool($queryResult)
+	{
+		foreach ($queryResult as $curResult)
+		{
+			DropFolderPeer::addInstanceToPool($curResult);
+		}
+	}
+	
 	/**
 	 * Method to do selects.
 	 *
@@ -274,8 +358,34 @@ abstract class BaseDropFolderPeer {
 	 *		 rethrown wrapped into a PropelException.
 	 */
 	public static function doSelect(Criteria $criteria, PropelPDO $con = null)
-	{
-		return DropFolderPeer::populateObjects(DropFolderPeer::doSelectStmt($criteria, $con));
+	{		
+		$criteria = DropFolderPeer::prepareCriteriaForSelect($criteria);
+		
+		$cacheKey = null;
+		$cachedResult = kQueryCache::getCachedQueryResults(
+			$criteria, 
+			kQueryCache::QUERY_TYPE_SELECT,
+			'DropFolderPeer', 
+			$cacheKey);
+		if ($cachedResult !== null)
+		{
+			DropFolderPeer::filterSelectResults($cachedResult);
+			DropFolderPeer::updateInstancePool($cachedResult);
+			return $cachedResult;
+		}
+
+		$con = DropFolderPeer::alternativeCon($con);
+		
+		$queryResult = DropFolderPeer::populateObjects(BasePeer::doSelect($criteria, $con));
+		
+		if ($cacheKey !== null)
+		{
+			kQueryCache::cacheQueryResults($cacheKey, $queryResult);
+		}
+		
+		DropFolderPeer::filterSelectResults($queryResult);
+		DropFolderPeer::addInstancesToPool($queryResult);
+		return $queryResult;
 	}
 
 	public static function alternativeCon($con)
@@ -436,24 +546,8 @@ abstract class BaseDropFolderPeer {
 		return BasePeer::doCount($criteria, $con);
 	}
 	
-	
-	/**
-	 * Prepares the Criteria object and uses the parent doSelect() method to execute a PDOStatement.
-	 *
-	 * Use this method directly if you want to work with an executed statement durirectly (for example
-	 * to perform your own object hydration).
-	 *
-	 * @param      Criteria $criteria The Criteria object used to build the SELECT statement.
-	 * @param      PropelPDO $con The connection to use
-	 * @throws     PropelException Any exceptions caught during processing will be
-	 *		 rethrown wrapped into a PropelException.
-	 * @return     PDOStatement The executed PDOStatement object.
-	 * @see        BasePeer::doSelect()
-	 */
-	public static function doSelectStmt(Criteria $criteria, PropelPDO $con = null)
+	public static function prepareCriteriaForSelect(Criteria $criteria)
 	{
-		$con = DropFolderPeer::alternativeCon($con);
-		
 		if ($criteria->hasSelectClause()) 
 		{
 			$asColumns = $criteria->getAsColumns();
@@ -474,6 +568,28 @@ abstract class BaseDropFolderPeer {
 
 		// attach default criteria
 		DropFolderPeer::attachCriteriaFilter($criteria);
+
+		return $criteria;
+	}
+	
+	/**
+	 * Prepares the Criteria object and uses the parent doSelect() method to execute a PDOStatement.
+	 *
+	 * Use this method directly if you want to work with an executed statement durirectly (for example
+	 * to perform your own object hydration).
+	 *
+	 * @param      Criteria $criteria The Criteria object used to build the SELECT statement.
+	 * @param      PropelPDO $con The connection to use
+	 * @throws     PropelException Any exceptions caught during processing will be
+	 *		 rethrown wrapped into a PropelException.
+	 * @return     PDOStatement The executed PDOStatement object.
+	 * @see        BasePeer::doSelect()
+	 */
+	public static function doSelectStmt(Criteria $criteria, PropelPDO $con = null)
+	{
+		$con = DropFolderPeer::alternativeCon($con);
+		
+		$criteria = DropFolderPeer::prepareCriteriaForSelect($criteria);
 		
 		// BasePeer returns a PDOStatement
 		return BasePeer::doSelect($criteria, $con);
@@ -613,7 +729,6 @@ abstract class BaseDropFolderPeer {
 				$obj = new $cls();
 				$obj->hydrate($row);
 				$results[] = $obj;
-				DropFolderPeer::addInstanceToPool($obj, $key);
 			} // if key exists
 		}
 		$stmt->closeCursor();
