6025b7eb45fbcd1080222ed186610c2b585afb8d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-04 09:22:07 +0000

    Remove getObject
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@61713 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/providers/youtube_api/lib/admin/YoutubeApiProfileConfiguration.php
+++ plugins/content_distribution/providers/youtube_api/lib/admin/YoutubeApiProfileConfiguration.php
@@ -5,51 +5,6 @@
  */
 class Form_YoutubeApiProfileConfiguration extends Form_ProviderProfileConfiguration
 {
-	public function getObject($objectType, array $properties, $add_underscore = true, $include_empty_fields = false)
-	{
-		$object = parent::getObject($objectType, $properties, $add_underscore, $include_empty_fields);
-		
-		if($object instanceof Kaltura_Client_YoutubeApiDistribution_Type_YoutubeApiDistributionProfile)
-		{
-			$requiredFlavorParamsIds = explode(',', $object->requiredFlavorParamsIds);
-			$optionalFlavorParamsIds = explode(',', $object->optionalFlavorParamsIds);
-			
-			if($object->movFlavorParamsId)
-			{
-				if(!in_array($object->movFlavorParamsId, $requiredFlavorParamsIds))
-					$requiredFlavorParamsIds[] = $object->movFlavorParamsId;
-					
-				$flavorKey = array_search($object->movFlavorParamsId, $optionalFlavorParamsIds);
-				if($flavorKey !== false)
-					unset($optionalFlavorParamsIds[$flavorKey]);
-			}
-			
-			if($object->flvFlavorParamsId)
-			{
-				if(!in_array($object->flvFlavorParamsId, $requiredFlavorParamsIds))
-					$requiredFlavorParamsIds[] = $object->flvFlavorParamsId;
-					
-				$flavorKey = array_search($object->flvFlavorParamsId, $optionalFlavorParamsIds);
-				if($flavorKey !== false)
-					unset($optionalFlavorParamsIds[$flavorKey]);
-			}
-			
-			if($object->wmvFlavorParamsId)
-			{
-				if(!in_array($object->wmvFlavorParamsId, $requiredFlavorParamsIds))
-					$requiredFlavorParamsIds[] = $object->wmvFlavorParamsId;
-					
-				$flavorKey = array_search($object->wmvFlavorParamsId, $optionalFlavorParamsIds);
-				if($flavorKey !== false)
-					unset($optionalFlavorParamsIds[$flavorKey]);
-			}
-			
-			$object->requiredFlavorParamsIds = implode(',', $requiredFlavorParamsIds);
-			$object->optionalFlavorParamsIds = implode(',', $optionalFlavorParamsIds);
-		}
-		return $object;
-	}
-	
 	protected function addProviderElements()
 	{
 		$element = new Zend_Form_Element_Hidden('providerElements');
