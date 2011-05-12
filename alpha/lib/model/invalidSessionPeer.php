b9ccee2d2c70827d74d16dd2880d7d9a7f925231
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-12 11:49:02 +0000

    tickets 10223 & 10224: add limit number of action for KS and set role on KS
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@62153 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/invalidSessionPeer.php
+++ alpha/lib/model/invalidSessionPeer.php
@@ -24,19 +24,66 @@ class invalidSessionPeer extends BaseinvalidSessionPeer {
 	{
 		$criteria = new Criteria();
 		$criteria->add(invalidSessionPeer::KS, base64_decode($ks));
-
+		$criteria->addAnd(invalidSessionPeer::KS_VALID_UNTIL, null, Criteria::NOT_EQUAL);
 		$cnt = invalidSessionPeer::doCount($criteria, $con);
 		return ($cnt > 0);
+	}
+	
+	/**
+	 * @param      string $ks
+	 * @param      PropelPDO $con the connection to use
+	 * @return     bool
+	 */
+	public static function isValidActionsLimit($ks, PropelPDO $con = null)
+	{
+		$criteria = new Criteria();
+		$criteria->add(invalidSessionPeer::KS, base64_decode($ks));
+		$cnt = invalidSessionPeer::doSelectOne($criteria, $con);
+		
+		if ($cnt){
+			$currentActionLimit = $cnt->getActionsLimit();
+			
+			if (!is_null($currentActionLimit) && $currentActionLimit <= 0)
+				return false;
+			
+			$cnt->setActionsLimit($currentActionLimit - 1);
+			$cnt->save();
+		}
+		
+		return true;
+		
+	}
+	
+	/**
+	 * @param      ks $ks
+	 * @param	   int $limit
+	 * @return     invalidSession
+	 */
+	public static function actionsLimitKs(ks $ks, $limit)
+	{
+		$invalidSession = new invalidSession();
+		$invalidSession->setKs(base64_decode($ks->toSecureString()));
+		$invalidSession->setActionsLimit($limit);
+		$invalidSession->save();
+		
+		return $invalidSession;
 	}
 	
 	/**
 	 * @param      ks $ks
 	 * @return     invalidSession
 	 */
-	public static function invalidateKs(ks $ks)
+	public static function invalidateKs(ks $ks, PropelPDO $con = null)
 	{
-		$invalidSession = new invalidSession();
-		$invalidSession->setKs(base64_decode($ks->getOriginalString()));
+		$criteria = new Criteria();
+		$criteria->add(invalidSessionPeer::KS, base64_decode($ks->toSecureString()));
+		$invalidSession = invalidSessionPeer::doSelectOne($criteria, $con);
+		
+		if(!$invalidSession){
+			$invalidSession = new invalidSession();
+			$invalidSession->setKs(base64_decode($ks->getOriginalString()));
+		}
+				
 		$invalidSession->setKsValidUntil($ks->valid_until);
 		$invalidSession->save();
 		
