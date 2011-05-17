cf55fed92b0d8e674b36b41829d471861070ac19
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-17 10:17:19 +0000

    Added support to enable and disable package
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@62452 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/services/ContentDistributionBatchService.php
+++ plugins/content_distribution/services/ContentDistributionBatchService.php
@@ -165,6 +165,162 @@ class ContentDistributionBatchService extends BatchService
 	
 // --------------------------------- DistributionUpdateJob functions 	--------------------------------- //
 	
+// --------------------------------- DistributionEnableJob functions 	--------------------------------- //
+	
+	/**
+	 * batch getExclusiveDistributionEnableJob action allows to get a BatchJob of type DISTRIBUTION_ENABLE 
+	 * 
+	 * @action getExclusiveDistributionEnableJobs
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism  
+	 * @param int $maxExecutionTime The maximum time in seconds the job reguarly take. Is used for the locking mechanism when determining an unexpected termination of a batch-process.
+	 * @param int $numberOfJobs The maximum number of jobs to return. 
+	 * @param KalturaBatchJobFilter $filter Set of rules to fetch only rartial list of jobs  
+	 * @return KalturaBatchJobArray 
+	 */
+	function getExclusiveDistributionEnableJobsAction(KalturaExclusiveLockKey $lockKey, $maxExecutionTime, $numberOfJobs, KalturaBatchJobFilter $filter = null)
+	{
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_ENABLE);
+		return $this->getExclusiveJobsAction($lockKey, $maxExecutionTime, $numberOfJobs, $filter, $jobType);
+	}
+
+	
+	/**
+	 * batch updateExclusiveDistributionEnableJob action updates a BatchJob of type DISTRIBUTION_ENABLE that was claimed using the getExclusiveDistributionEnableJobs
+	 * 
+	 * @action updateExclusiveDistributionEnableJob
+	 * @param int $id The id of the job to free
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism  
+	 * @param KalturaBatchJob $job
+	 * @return KalturaBatchJob 
+	 */
+	function updateExclusiveDistributionEnableJobAction($id ,KalturaExclusiveLockKey $lockKey, KalturaBatchJob $job)
+	{
+		$dbBatchJob = BatchJobPeer::retrieveByPK($id);
+		
+		// verifies that the job is of the right type
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_ENABLE);
+		if($dbBatchJob->getJobType() != $jobType)
+			throw new KalturaAPIException(APIErrors::UPDATE_EXCLUSIVE_JOB_WRONG_TYPE, $id, serialize($lockKey), serialize($job));
+	
+		$dbBatchJob = kBatchManager::updateExclusiveBatchJob($id, $lockKey->toObject(), $job->toObject($dbBatchJob));
+				
+		$batchJob = new KalturaBatchJob(); // start from blank
+		return $batchJob->fromObject($dbBatchJob);
+	}
+
+	
+	/**
+	 * batch freeExclusiveDistributionEnableJob action frees a BatchJob of type DistributionEnable that was claimed using the getExclusiveDistributionEnableJobs
+	 * 
+	 * @action freeExclusiveDistributionEnableJob
+	 * @param int $id The id of the job to free
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism
+	 * @param bool $resetExecutionAttempts Resets the job execution attampts to zero  
+	 * @return KalturaFreeJobResponse 
+	 */
+	function freeExclusiveDistributionEnableJobAction($id ,KalturaExclusiveLockKey $lockKey, $resetExecutionAttempts = false)
+	{
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_ENABLE);
+		return $this->freeExclusiveJobAction($id ,$lockKey, $jobType, $resetExecutionAttempts);
+	}
+	
+	
+	/**
+	 * batch getExclusiveAlmostDoneDistributionEnableJobs action allows to get a BatchJob of type DISTRIBUTION_ENABLE that wait for remote closure 
+	 * 
+	 * @action getExclusiveAlmostDoneDistributionEnableJobs
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism  
+	 * @param int $maxExecutionTime The maximum time in seconds the job reguarly take. Is used for the locking mechanism when determining an unexpected termination of a batch-process.
+	 * @param int $numberOfJobs The maximum number of jobs to return. 
+	 * @param KalturaBatchJobFilter $filter Set of rules to fetch only rartial list of jobs  
+	 * @return KalturaBatchJobArray 
+	 */
+	function getExclusiveAlmostDoneDistributionEnableJobsAction(KalturaExclusiveLockKey $lockKey, $maxExecutionTime, $numberOfJobs, KalturaBatchJobFilter $filter = null)
+	{
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_ENABLE);
+		return $this->getExclusiveAlmostDoneAction($lockKey, $maxExecutionTime, $numberOfJobs, $filter, $jobType);
+	}
+	
+// --------------------------------- DistributionEnableJob functions 	--------------------------------- //
+	
+// --------------------------------- DistributionDisableJob functions 	--------------------------------- //
+	
+	/**
+	 * batch getExclusiveDistributionDisableJob action allows to get a BatchJob of type DISTRIBUTION_DISABLE 
+	 * 
+	 * @action getExclusiveDistributionDisableJobs
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism  
+	 * @param int $maxExecutionTime The maximum time in seconds the job reguarly take. Is used for the locking mechanism when determining an unexpected termination of a batch-process.
+	 * @param int $numberOfJobs The maximum number of jobs to return. 
+	 * @param KalturaBatchJobFilter $filter Set of rules to fetch only rartial list of jobs  
+	 * @return KalturaBatchJobArray 
+	 */
+	function getExclusiveDistributionDisableJobsAction(KalturaExclusiveLockKey $lockKey, $maxExecutionTime, $numberOfJobs, KalturaBatchJobFilter $filter = null)
+	{
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_DISABLE);
+		return $this->getExclusiveJobsAction($lockKey, $maxExecutionTime, $numberOfJobs, $filter, $jobType);
+	}
+
+	
+	/**
+	 * batch updateExclusiveDistributionDisableJob action updates a BatchJob of type DISTRIBUTION_DISABLE that was claimed using the getExclusiveDistributionDisableJobs
+	 * 
+	 * @action updateExclusiveDistributionDisableJob
+	 * @param int $id The id of the job to free
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism  
+	 * @param KalturaBatchJob $job
+	 * @return KalturaBatchJob 
+	 */
+	function updateExclusiveDistributionDisableJobAction($id ,KalturaExclusiveLockKey $lockKey, KalturaBatchJob $job)
+	{
+		$dbBatchJob = BatchJobPeer::retrieveByPK($id);
+		
+		// verifies that the job is of the right type
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_DISABLE);
+		if($dbBatchJob->getJobType() != $jobType)
+			throw new KalturaAPIException(APIErrors::UPDATE_EXCLUSIVE_JOB_WRONG_TYPE, $id, serialize($lockKey), serialize($job));
+	
+		$dbBatchJob = kBatchManager::updateExclusiveBatchJob($id, $lockKey->toObject(), $job->toObject($dbBatchJob));
+				
+		$batchJob = new KalturaBatchJob(); // start from blank
+		return $batchJob->fromObject($dbBatchJob);
+	}
+
+	
+	/**
+	 * batch freeExclusiveDistributionDisableJob action frees a BatchJob of type DistributionDisable that was claimed using the getExclusiveDistributionDisableJobs
+	 * 
+	 * @action freeExclusiveDistributionDisableJob
+	 * @param int $id The id of the job to free
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism
+	 * @param bool $resetExecutionAttempts Resets the job execution attampts to zero  
+	 * @return KalturaFreeJobResponse 
+	 */
+	function freeExclusiveDistributionDisableJobAction($id ,KalturaExclusiveLockKey $lockKey, $resetExecutionAttempts = false)
+	{
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_DISABLE);
+		return $this->freeExclusiveJobAction($id ,$lockKey, $jobType, $resetExecutionAttempts);
+	}
+	
+	
+	/**
+	 * batch getExclusiveAlmostDoneDistributionDisableJobs action allows to get a BatchJob of type DISTRIBUTION_DISABLE that wait for remote closure 
+	 * 
+	 * @action getExclusiveAlmostDoneDistributionDisableJobs
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism  
+	 * @param int $maxExecutionTime The maximum time in seconds the job reguarly take. Is used for the locking mechanism when determining an unexpected termination of a batch-process.
+	 * @param int $numberOfJobs The maximum number of jobs to return. 
+	 * @param KalturaBatchJobFilter $filter Set of rules to fetch only rartial list of jobs  
+	 * @return KalturaBatchJobArray 
+	 */
+	function getExclusiveAlmostDoneDistributionDisableJobsAction(KalturaExclusiveLockKey $lockKey, $maxExecutionTime, $numberOfJobs, KalturaBatchJobFilter $filter = null)
+	{
+		$jobType = ContentDistributionPlugin::getBatchJobTypeCoreValue(ContentDistributionBatchJobType::DISTRIBUTION_DISABLE);
+		return $this->getExclusiveAlmostDoneAction($lockKey, $maxExecutionTime, $numberOfJobs, $filter, $jobType);
+	}
+	
+// --------------------------------- DistributionDisableJob functions 	--------------------------------- //
+	
 // --------------------------------- DistributionDeleteJob functions 	--------------------------------- //
 	
 	/**
