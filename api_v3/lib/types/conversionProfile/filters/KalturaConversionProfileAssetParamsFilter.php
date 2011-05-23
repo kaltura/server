570e90408f28062e540e17e70ce0048f813ae565
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-23 11:38:23 +0000

    Query the profiles and assets anyway to filter by the partner
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@62952 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/conversionProfile/filters/KalturaConversionProfileAssetParamsFilter.php
+++ api_v3/lib/types/conversionProfile/filters/KalturaConversionProfileAssetParamsFilter.php
@@ -21,68 +21,46 @@ class KalturaConversionProfileAssetParamsFilter extends KalturaConversionProfile
 			$object_to_fill = new assetParamsConversionProfileFilter();
 			
 		$conversionProfileCriteria = new Criteria();
-		$setConversionProfile = false;
 		
 		if($this->conversionProfileIdEqual)
-		{
-			$setConversionProfile = true;
 			$conversionProfileCriteria->add(conversionProfile2Peer::ID, $this->conversionProfileIdEqual);
-		}
 		if($this->conversionProfileIdIn)
-		{
-			$setConversionProfile = true;
 			$conversionProfileCriteria->add(conversionProfile2Peer::ID, explode(',', $this->conversionProfileIdIn), Criteria::IN);
-		}
 		if($this->conversionProfileIdFilter)
 		{
-			$setConversionProfile = true;
 			$conversionProfileIdFilter = new conversionProfile2Filter();
 			$this->conversionProfileIdFilter->toObject($conversionProfileIdFilter);
 			$conversionProfileIdFilter->attachToCriteria($conversionProfileCriteria);
 		}
-		if($setConversionProfile)
-		{
-			$this->conversionProfileIdEqual = null;
-			$this->conversionProfileIdFilter = null;
-			$conversionProfileIdIn = conversionProfile2Peer::getIds($conversionProfileCriteria);
-			if(count($conversionProfileIdIn))
-				$this->conversionProfileIdIn = implode(',', $conversionProfileIdIn);
-			else
-				$this->conversionProfileIdIn = -1; // none existing conversion profile
-		}
+		$this->conversionProfileIdEqual = null;
+		$this->conversionProfileIdFilter = null;
+		$conversionProfileIdIn = conversionProfile2Peer::getIds($conversionProfileCriteria);
+		if(count($conversionProfileIdIn))
+			$this->conversionProfileIdIn = implode(',', $conversionProfileIdIn);
+		else
+			$this->conversionProfileIdIn = -1; // none existing conversion profile
 		
 		
 		$assetParamsCriteria = new Criteria();
-		$setAssetParams = false;
 		
 		if($this->assetParamsIdEqual)
-		{
-			$setAssetParams = true;
 			$assetParamsCriteria->add(assetParamsPeer::ID, $this->assetParamsIdEqual);
-		}
 		if($this->assetParamsIdIn)
-		{
-			$setAssetParams = true;
 			$assetParamsCriteria->add(assetParamsPeer::ID, explode(',', $this->assetParamsIdIn), Criteria::IN);
-		}
 		if($this->assetParamsIdFilter)
 		{
-			$setAssetParams = true;
 			$assetParamsIdFilter = new assetParamsFilter();
 			$this->assetParamsIdFilter->toObject($assetParamsIdFilter);
 			$assetParamsIdFilter->attachToCriteria($assetParamsCriteria);
 		}
-		if($setAssetParams)
-		{
-			$this->assetParamsIdEqual = null;
-			$this->assetParamsIdFilter = null;
-			assetParamsPeer::resetInstanceCriteriaFilter();
-			$assetParamsIdIn = assetParamsPeer::getIds($assetParamsCriteria);
-			if(count($assetParamsIdIn))
-				$this->assetParamsIdIn = implode(',', $assetParamsIdIn);
-			else
-				$this->assetParamsIdIn = -1; // none existing flavor
-		}
+		$this->assetParamsIdEqual = null;
+		$this->assetParamsIdFilter = null;
+		assetParamsPeer::resetInstanceCriteriaFilter();
+		$assetParamsIdIn = assetParamsPeer::getIds($assetParamsCriteria);
+		if(count($assetParamsIdIn))
+			$this->assetParamsIdIn = implode(',', $assetParamsIdIn);
+		else
+			$this->assetParamsIdIn = -1; // none existing flavor
 		
 		return parent::toObject($object_to_fill, $props_to_skip);
 	}
