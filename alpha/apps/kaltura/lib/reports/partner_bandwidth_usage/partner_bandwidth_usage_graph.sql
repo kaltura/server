59b5d063bf0e106afe4fb39707de9d45e5d17988
dorp <dorp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-23 09:06:56 +0000

    Removed time zone referrernce from the bandwidth query.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@62918 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/partner_bandwidth_usage/partner_bandwidth_usage_graph.sql
+++ alpha/apps/kaltura/lib/reports/partner_bandwidth_usage/partner_bandwidth_usage_graph.sql
@@ -8,7 +8,6 @@ FROM (
         WHERE
 		partner_id = {PARTNER_ID}
 		AND
-		date_id BETWEEN  IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}) AND
-				IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
+		date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}
         ) raw_data
 GROUP BY {GROUP_COLUMN}, partner_id;
