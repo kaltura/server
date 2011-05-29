c42d728bfb135f78fa3fe7ec1c265041367beea4
meravb <meravb@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-29 12:39:14 +0000

    External Path - Merav
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@63340 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/storage/kExternalPathManager.php
+++ alpha/apps/kaltura/lib/storage/kExternalPathManager.php
@@ -12,20 +12,29 @@ class kExternalPathManager extends kPathManager
 	public function generateFilePathArr(ISyncableFile $object, $subType, $version = null, $storageProfileId = null)
 	{
 		$storageProfile = kPathManager::getStorageProfile($storageProfileId);
-		$date_format = $storageProfile->getDateFormat();
+		$path_format = $storageProfile->getPathFormat();
 		
-		if (is_null($date_format))
+		if (is_null($path_format))
 		{
-			$dateDir = date('Ymd');
-			$partnerDir = floor($object->getPartnerId() / 1000);
-			$path = "$dateDir/$partnerDir/" . $object->generateFileName($subType, $version);
+			$format = '{defaultDateDir}/{partnerDir}/{filename}';
 		}
 		else
 		{
-			$dateDir = date('Y-m');
-			$day = date ("d");
-			$path = "$dateDir/$day/" . $object->generateFileName($subType, $version);
+			$format = '{pathFormat}/{filename}';
 		}
+		
+		$fileName = $object->generateFileName($subType, $version);
+		$partnerDir = floor($object->getPartnerId() / 1000);
+		$defaultDateDir = date ("Ymd");
+		
+		$pathDir = str_replace ('Y', date ("Y"), $path_format);
+		$pathDir = str_replace('m', date ("m"), $pathDir);
+		$pathDir = str_replace('d', date ("d"), $pathDir);
+		
+		$path = str_replace('{filename}', $fileName, $format);
+		$path = str_replace('{partnerDir}', $partnerDir, $path);
+		$path = str_replace('{defaultDateDir}', $defaultDateDir, $path);
+		$path = str_replace('{pathFormat}', $pathDir, $path);
 		
 		$root = '/';
 		return array($root, $path);
