b8974cea13530bfd2da8a9029b0b9458e4cea908
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-05-31 14:15:00 +0000

    VLC transcoding plugin
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@63540 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/vlc/lib/KOperationEngineVlc.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package plugins.vlc
+ * @subpackage lib
+ */
+class KOperationEngineVlc  extends KSingleOutputOperationEngine
+{
+
+	public function __construct($cmd, $outFilePath)
+	{
+		parent::__construct($cmd,$outFilePath);
+		KalturaLog::info(": cmd($cmd), outFilePath($outFilePath)");
+	}
+
+	protected function getCmdLine()
+	{
+		$exeCmd =  parent::getCmdLine();
+		KalturaLog::info(print_r($this,true));
+		return $exeCmd;
+	}
+
+	public function configure(KSchedularTaskConfig $taskConfig, KalturaConvartableJobData $data)
+	{
+		parent::configure($taskConfig, $data);
+		KalturaLog::info("taskConfig-->".print_r($taskConfig,true)."\ndata->".print_r($data,true));
+	}
+
+	private function parsePlayList($fileIn, $fileOut)
+	{
+		$fdIn = fopen($fileIn, 'r');
+		if($fdIn==false)
+			return false;
+		$fdOut = fopen($fileOut, 'w');
+		if($fdOut==false)
+			return false;
+		$strIn=null;
+		while ($strIn=fgets($fdIn)){
+			if(strstr($strIn,"---")){
+				$i=strrpos($strIn,"/");
+				$strIn = substr($strIn,$i+1);
+			}
+			fputs($fdOut,$strIn);
+			echo $strIn;
+		}
+		fclose($fdOut);
+		fclose($fdIn);
+		return true;
+	}
+}
