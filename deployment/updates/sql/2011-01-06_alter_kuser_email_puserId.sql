180fd48b34859d1cdf04944c143dcce62e6e6ddd
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-06-12 09:42:34 +0000

    remove all sql & php scripts that are handled in dragonfly (base folder) updates folder contains only eagle updates.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@63970 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2011-01-06_alter_kuser_email_puserId.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE  `kuser`
-CHANGE  `email`  `email` VARCHAR( 100 ),
-CHANGE  `puser_id`  `puser_id` VARCHAR( 100 );
--- deployment/updates/sql/2011-01-19_bulk_results_index.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE bulk_upload_result ADD INDEX job_id_line_index (BULK_UPLOAD_JOB_ID,LINE_INDEX);
