0013260d8ab7ef4b856feb780b3c495be2c4af6f
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-06-20 08:37:41 +0000

    KMC version 4.2.1
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@64449 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/kConf.php
+++ alpha/config/kConf.php
@@ -113,7 +113,7 @@ class kConf extends kConfLocal
 				"html5_version" => '1.3a',
 				"kmc_secured_login" => false,
 				
-				"kmc_version" => 'v4.2',
+				"kmc_version" => 'v4.2.1',
 				"new_partner_kmc_version" => 4,
 				
 				"paypal_data" => array (),
