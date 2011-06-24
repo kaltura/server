ce631060b4775a1a55a94276e78c800ec64034fd
nogab <nogab@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-06-24 09:42:36 +0000

    reset password- text fix
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@64724 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/configs/lang/en.php
+++ admin_console/configs/lang/en.php
@@ -961,9 +961,9 @@ return array(
 	'Kaltura_Client_Metadata_Enum_MetadataProfileStatus::TRANSFORMING' => 'Transforming',
 
 	//reset user's password
-	'reset uses password first notification' => "<b> Please Note: </b> The reset password actions should only be used by authorized administrators and when a KMC user has no way to retrieve his current password using the 'Forgot password' option in KMC login page.",
+	'reset users password first notification' => "<b> Please Note: </b> The reset password actions should only be used by authorized administrators and when a KMC user has no way to retrieve his current password using the 'Forgot password' option in KMC login page.",
 	'reset users password second notification' => "This action will override user's existing password, and for security reason will <b>NOT</b> send the new password to the KMC user via email.",
-	'reset users password third notofication' => "<b>You will not be able to view the password outside of this window, so please make sure to copy it before closing the window.</b> You shoule pass the new password to the KMC user in a secured way.",
+	'reset users password third notification' => "<b>You will not be able to view the password outside of this window, so please make sure to copy it before closing the window.</b> You should pass the new password to the KMC user in a secured way.",
 
 );
 
