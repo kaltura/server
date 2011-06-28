49820e54a96dd053af076f1bc66593b77e25adb4
dorp <dorp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-06-28 12:49:10 +0000

    Moved the creation of the sphinx DB to a different DB - DB creation scripts
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@65043 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/base/sql/01.kaltura_ce_tables.sql
+++ deployment/base/sql/01.kaltura_ce_tables.sql
@@ -1782,36 +1782,6 @@ CREATE TABLE `short_link` (
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `sphinx_log`;
-
-CREATE TABLE `sphinx_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `entry_id` varchar(20) DEFAULT NULL,
-  `partner_id` int(11) DEFAULT '0',
-  `dc` int(11) DEFAULT NULL,
-  `sql` longtext,
-  `created_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `entry_id` (`entry_id`),
-  KEY `creatd_at` (`created_at`),
-  KEY `sphinx_log_FI_1` (`partner_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
-
-DROP TABLE IF EXISTS `sphinx_log_server`;
-
-CREATE TABLE `sphinx_log_server` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `server` varchar(63) DEFAULT NULL,
-  `dc` int(11) DEFAULT NULL,
-  `last_log_id` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `sphinx_log_server_FI_1` (`last_log_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
-
 DROP TABLE IF EXISTS `storage_profile`;
 
 CREATE TABLE `storage_profile` (
