3a8f08a539f1bb6df62d1a9b05e5dd522fe3620d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-04 15:48:36 +0000

    Added provider filter
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@65522 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/cue_points/ad_cue_point/lib/api/filters/KalturaAdCuePointFilter.php
+++ plugins/cue_points/ad_cue_point/lib/api/filters/KalturaAdCuePointFilter.php
@@ -5,4 +5,29 @@
  */
 class KalturaAdCuePointFilter extends KalturaAdCuePointBaseFilter
 {
+	private $map_between_objects = array
+	(
+		"providerTypeEqual" => "_eq_sub_type",
+		"providerTypeIn" => "_in_sub_type",
+	);
+
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), $this->map_between_objects);
+	}
+
+	/**
+	 * 
+	 * 
+	 * @var KalturaAdCuePointProviderType
+	 */
+	public $providerTypeEqual;
+
+	/**
+	 * 
+	 * 
+	 * @dynamicType KalturaAdCuePointProviderType
+	 * @var string
+	 */
+	public $providerTypeIn;
 }
