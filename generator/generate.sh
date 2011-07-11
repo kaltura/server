bfb320096499a14d880a0d8dbd9836d37032fecd
ronic <ronic@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-11 15:43:53 +0000

    Changed path for the generate.sh copy
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@66234 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.sh
+++ generator/generate.sh
@@ -5,7 +5,7 @@ rm -fr ../cache/generator/*
 
 php generate.php "$@"
 
-output_path=`php -r 'require_once("bootstrap.php"); echo myContentStorage::getFSContentRootPath() . "/content/generator/output";' 2>&1`
+output_path=`php -r 'require_once("bootstrap.php"); echo myContentStorage::getFSContentRootPath() . "/content/clientlibs";' 2>&1`
 
 rsync -avC $output_path/php5ZendClientAdminConsole/ ../admin_console/lib
 rsync -avC $output_path/batchClient/ ../batch/client
