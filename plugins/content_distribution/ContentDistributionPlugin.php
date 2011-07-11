57a2aaecf4d52abad211f8e6485f448aaaf4ba40
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-11 13:57:41 +0000

    Remove batch services from testme and testmedoc
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@66212 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/ContentDistributionPlugin.php
+++ plugins/content_distribution/ContentDistributionPlugin.php
@@ -343,6 +343,9 @@ class ContentDistributionPlugin extends KalturaPlugin implements IKalturaPermiss
 	{
 		if($configName == 'generator')
 			return new Zend_Config_Ini(dirname(__FILE__) . '/config/generator.ini');
+	
+		if($configName == 'testme')
+			return new Zend_Config_Ini(dirname(__FILE__) . '/config/testme.ini');
 			
 		return null;
 	}
