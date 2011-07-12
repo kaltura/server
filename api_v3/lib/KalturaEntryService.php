ee2a6cd126b9e7cf5273746afcdeb779d7d6579b
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-12 08:15:27 +0000

    Revert change until we find how to fix it
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@66269 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/KalturaEntryService.php
+++ api_v3/lib/KalturaEntryService.php
@@ -759,7 +759,7 @@ class KalturaEntryService extends KalturaBaseService
 			$pager = new KalturaFilterPager();
 		
 		$c = $this->prepareEntriesCriteriaFilter($filter, $pager, $partnerIdForScope);
-		$c->add(entryPeer::DISPLAY_IN_SEARCH, mySearchUtils::DISPLAY_IN_SEARCH_NONE, Criteria::NOT_EQUAL);
+		//$c->add(entryPeer::DISPLAY_IN_SEARCH, mySearchUtils::DISPLAY_IN_SEARCH_NONE, Criteria::NOT_EQUAL);
 		
 		$list = entryPeer::doSelect($c);
 		$totalCount = $c->getRecordsCount();
