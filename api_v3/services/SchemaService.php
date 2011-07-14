b66120cf748624770b3e2da68e874726925378b4
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-14 06:09:08 +0000

    Use single file xsd
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@66435 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/services/SchemaService.php
+++ api_v3/services/SchemaService.php
@@ -32,133 +32,106 @@ class SchemaService extends KalturaBaseService
 	 * 
 	 * @action serve
 	 * @param KalturaSchemaType $type  
-	 * @param string $name
 	 * @return file 
 	 */
-	function serveAction($type, $name = null)
+	function serveAction($type)
 	{
-		$ns = 'http://' . kConf::get('www_host') . "/$type";
-		if($name)
-			$ns .= "/$name";
-						
-		$xsdElement = null;
-		$xsd = '<xs:schema targetNamespace="' . $ns . '" xmlns:xs="http://www.w3.org/2001/XMLSchema"/>';
-			
-		if(!$name)
-		{		
-			$xsdElement = new SimpleXMLElement($xsd);
+		header("Content-Type: text/plain; charset=UTF-8");
 		
-			$redefine = $xsdElement->addChild('redefine');
-			$redefine->addAttribute('schemaLocation', 'http://' . kConf::get('cdn_host') . "/api_v3/service/schema/action/serve/type/$type/name/" . self::CORE_SCHEMA_NAME);
+		$cacheXsdFile = kConf::get("cache_root_path") . "/$type.xsd";
+		if(file_exists($cacheXsdFile))
+			kFile::dumpFile($cacheXsdFile);
 		
-			$import = $xsdElement->addChild('import');
-			$import->addAttribute('schemaLocation', 'http://' . kConf::get('cdn_host') . "/api_v3/service/schema/action/serve/type/$type/name/enum");
-			
+		$xsdFile = fopen($cacheXsdFile, 'w');
+		
+		$namespace = 'http://' . kConf::get('www_host') . "/$type";
+		fwrite($xsdFile, '<xs:schema targetNamespace="' . $namespace . '" xmlns:xs="http://www.w3.org/2001/XMLSchema">');
+	
+		$baseXsdElement = null;
+		if($type == SchemaType::SYNDICATION)
+		{
+			$baseXsdElement = new SimpleXMLElement(kConf::get("syndication_core_xsd_path"), null, true);
+		}
+		else
+		{
+			$plugin = kPluginableEnumsManager::getPlugin($type);
+			if($plugin instanceof IKalturaSchemaContributor)
+			{
+				KalturaLog::debug("Found plugin [" . get_class($plugin) . "]");
+				$baseXsdElement = $plugin->getPluginSchema($type);
+			}
+		}
+	
+		if($baseXsdElement)
+		{
 			$schemaContributors = KalturaPluginManager::getPluginInstances('IKalturaSchemaContributor');
 			foreach($schemaContributors as $key => $schemaContributor)
-				$schemaContributor->contributeToSchema($type, $xsdElement);
-		}
-		elseif ($name == self::ENUM_SCHEMA_NAME)
-		{
-			$cacheFile = kConf::get("cache_root_path") . '/api_v3/enum.xsd';
-			if(file_exists($cacheFile))
-			{
-				$xsd = file_get_contents($cacheFile);
-				$xsdElement = new SimpleXMLElement($xsd);
-			}
-			else
-			{
-				$xmlnsBase = "http://" . kConf::get('www_host') . "/$type";
-				$xmlns = "http://" . kConf::get('www_host') . "/$type/enum";
+				$schemaContributor->getPluginSchema($type, $baseXsdElement);
 				
-				$xsd = '<?xml version="1.0" encoding="UTF-8"?>
-					<xs:schema 
-						xmlns:xs="http://www.w3.org/2001/XMLSchema"
-						xmlns="' . $xmlns . '" 
-						xmlns:core="' . $xmlnsBase . '" 
-						targetNamespace="' . $xmlns . '"
-					>
-				';
+			foreach($baseXsdElement as $element)
+			{
+				/* @var $element SimpleXMLElement */
+				fwrite($xsdFile, $element->asXML());
+			}
+		}
+		
+		$cacheEnumFile = kConf::get("cache_root_path") . '/api_v3/enum.xsd';
+		if(file_exists($cacheEnumFile))
+		{
+			fwrite($xsdFile, file_get_contents($cacheEnumFile));
+		}
+		else
+		{
+			$enumFile = fopen($cacheEnumFile, 'w');
 			
-				$classMapFileLcoation = KAutoloader::getClassMapFilePath();
-				$classMap = unserialize(file_get_contents($classMapFileLcoation));
-				
-				foreach($classMap as $class => $path)
-				{
-					if (strpos($class, 'Kaltura') === 0 && strpos($class, '_') === false && strpos($path, 'api') !== false) // make sure the class is api object
-					{
-						if(!is_subclass_of($class, 'KalturaEnum'))
-							continue;
-							
-						$classTypeReflector = KalturaTypeReflectorCacher::get($class);
-						
-						$xsdType = 'int';
-						if($classTypeReflector->isStringEnum())
-							$xsdType = 'string';
-						
-						$xsd .= '
-							<xs:simpleType name="' . $class . '">
-								<xs:annotation><xs:documentation>http://' . kConf::get('www_host') . '/api_v3/testmeDoc/index.php?object=' . $class . '</xs:documentation></xs:annotation>
-								<xs:restriction base="xs:' . $xsdType . '">';
+			$classMapFileLcoation = KAutoloader::getClassMapFilePath();
+			$classMap = unserialize(file_get_contents($classMapFileLcoation));
+			
+			foreach($classMap as $class => $path)
+			{
+				if (strpos($class, 'Kaltura') !== 0) // class should start with 'Kaltura...'
+					continue;
+
+				if (strpos($class, '_') !== false) // class shouldn't contain underscore like in Zend standard
+					continue;
 					
-						$contants = $classTypeReflector->getConstants();
-						foreach($contants as $contant)
-						{
-							$xsd .= '<xs:enumeration value="' . $contant->getDefaultValue() . '"><xs:annotation><xs:documentation>' . $contant->getName() . '</xs:documentation></xs:annotation></xs:enumeration>';
-						}
+				if(strpos($path, 'api') === false) // class must be under any api folder
+					continue;
+					
+				if(!is_subclass_of($class, 'KalturaEnum')) // class must be enum
+					continue;
 						
-									
-						$xsd .= '</xs:restriction>
-							</xs:simpleType>
-						';
-	
-					}
-				}
+				$classTypeReflector = KalturaTypeReflectorCacher::get($class);
+					
+				$xsdType = 'int';
+				if($classTypeReflector->isStringEnum())
+					$xsdType = 'string';
 				
-				$xsd .= '</xs:schema>';
-				file_put_contents($cacheFile, $xsd);
-				$xsdElement = new SimpleXMLElement($xsd);
-			}
-		}
-		elseif ($name == self::CORE_SCHEMA_NAME)
-		{
-			if($type == SchemaType::SYNDICATION)
-			{
-				$xsd = '<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">';
-				$xsd .= file_get_contents(kConf::get("syndication_core_xsd_path"));
-				$xsd .= '</xs:schema>';
-				$xsdElement = new SimpleXMLElement($xsd);
-			}
-			else
-			{
-				$plugin = kPluginableEnumsManager::getPlugin($type);
-				if($plugin instanceof IKalturaSchemaContributor)
+				$xsd = '
+					<xs:simpleType name="' . $class . '">
+						<xs:annotation><xs:documentation>http://' . kConf::get('www_host') . '/api_v3/testmeDoc/index.php?object=' . $class . '</xs:documentation></xs:annotation>
+						<xs:restriction base="xs:' . $xsdType . '">';
+			
+				$contants = $classTypeReflector->getConstants();
+				foreach($contants as $contant)
 				{
-					KalturaLog::debug("Found plugin [" . get_class($plugin) . "]");
-					$xsdElement = $plugin->getPluginSchema($type);
+					$xsd .= '<xs:enumeration value="' . $contant->getDefaultValue() . '"><xs:annotation><xs:documentation>' . $contant->getName() . '</xs:documentation></xs:annotation></xs:enumeration>';
 				}
-				else
-				{		
-					$xsdElement = new SimpleXMLElement($xsd);
-				}
-			}
-		}
-		else 
-		{
-			$plugin = KalturaPluginManager::getPluginInstance($name);
-			if($plugin && $plugin instanceof IKalturaSchemaContributor)
-			{
-				$xsdElement = $plugin->getPluginSchema($type);
-			}
-			else 
-			{		
-				$xsdElement = new SimpleXMLElement($xsd);
-			}			
-		}
 				
-		header("Content-Type: text/plain; charset=UTF-8");
-		echo $xsdElement->saveXML();
-		kFile::closeDbConnections();
-		exit;
+							
+				$xsd .= '</xs:restriction>
+					</xs:simpleType>
+				';
+				
+				fwrite($xsdFile, $xsd);
+				fwrite($enumFile, $xsd);
+			}
+			fclose($enumFile);
+		}
+		
+		fwrite($xsdFile, '</xs:schema>');
+		fclose($xsdFile);
+		
+		kFile::dumpFile($cacheXsdFile);
 	}
 }
