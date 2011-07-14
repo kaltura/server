e924866dbcf0e3ec9558998afad5910954eb1e2a
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-14 09:33:53 +0000

    api_schema.php: update the location of the schema xml to match the output path of the generator
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@66459 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/web/api_schema.php
+++ api_v3/web/api_schema.php
@@ -1,5 +1,12 @@
 <?php
-$fileLocation = "../../generator/output/KalturaClient.xml";
+
+require_once("../bootstrap.php");
+
+$root = myContentStorage::getFSContentRootPath();
+$outputPathBase = "$root/content/clientlibs";
+
+$fileLocation = "$outputPathBase/KalturaClient.xml";
+
 if (!file_exists($fileLocation))
 	die("KalturaClient.xml was not found");
 	
