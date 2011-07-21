c941d061b35004d07499b5ca940f1722936dda8d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-21 13:36:47 +0000

    Added language code attribute.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@66943 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/caption/lib/api/KalturaCaptionAsset.php
+++ plugins/caption/lib/api/KalturaCaptionAsset.php
@@ -16,9 +16,17 @@ class KalturaCaptionAsset extends KalturaAsset
 	/**
 	 * The language of the caption asset content
 	 * 
-	 * @var string
+	 * @var KalturaLanguage
 	 */
 	public $language;
+	
+	/**
+	 * The language of the caption asset content
+	 * 
+	 * @var KalturaLanguageCode
+	 * @readonly
+	 */
+	public $languageCode;
 	
 	/**
 	 * Is default caption asset of the entry
@@ -55,5 +63,21 @@ class KalturaCaptionAsset extends KalturaAsset
 	public function getMapBetweenObjects ( )
 	{
 		return array_merge ( parent::getMapBetweenObjects() , self::$map_between_objects );
+	}
+	
+	public function fromObject($source_object)
+	{
+		$ret = parent::fromObject($source_object);
+				
+		$languageReflector = KalturaTypeReflectorCacher::get('KalturaLanguage');
+		$languageCodeReflector = KalturaTypeReflectorCacher::get('KalturaLanguageCode');
+		if($languageReflector && $languageCodeReflector)
+		{
+			$languageCode = $languageReflector->getConstantName($this->language);
+			if($languageCode)
+				$this->languageCode = $languageCodeReflector->getConstantValue($languageCode);
+		}
+			
+		return $ret;
 	}
 }
