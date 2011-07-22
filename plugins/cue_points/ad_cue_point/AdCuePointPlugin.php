ba5528d951363b9b990079daf76ba275640e4b55
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-22 07:56:42 +0000

    URL is not valid
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67050 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/cue_points/ad_cue_point/AdCuePointPlugin.php
+++ plugins/cue_points/ad_cue_point/AdCuePointPlugin.php
@@ -172,7 +172,7 @@ class AdCuePointPlugin extends KalturaPlugin implements IKalturaCuePoint
 		if($cuePoint->getName())
 			$scene->addChild('sceneTitle', kMrssManager::stringToSafeXml($cuePoint->getName()));
 		if($cuePoint->getSourceUrl())
-			$scene->addChild('sourceUrl', kMrssManager::stringToSafeXml($cuePoint->getSourceUrl()));
+			$scene->addChild('sourceUrl', $cuePoint->getSourceUrl());
 		if($cuePoint->getAdType())
 			$scene->addChild('adType', $cuePoint->getAdType());
 		if($cuePoint->getSubType())
