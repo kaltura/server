1ba2d50bae78d9e5569fcaa0e57ba0cb026e8139
roman <roman@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-24 13:55:44 +0000

    qnd: generated stuff
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67135 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/content_distribution/providers/comcast_mrss/tests/services/comcastMrss/ComcastMrssServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../../../../../../../../tests/bootstrap.php');
+require_once(dirname(__FILE__) . '/ComcastMrssServiceTestBase.php');
+
+/**
+ * comcastMrss service test case.
+ */
+class ComcastMrssServiceTest extends ComcastMrssServiceTestBase
+{
+	/**
+	 * Set up the test initial data
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+	}
+
+}
+
