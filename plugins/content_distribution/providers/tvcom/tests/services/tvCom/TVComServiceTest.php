9cf1a334c915144fffe57c757efc0aff179c128f
roman <roman@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-24 14:32:15 +0000

    qnd: generated stuff
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67144 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/content_distribution/providers/tvcom/tests/services/tvCom/TVComServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../../../../../../../../tests/bootstrap.php');
+require_once(dirname(__FILE__) . '/TVComServiceTestBase.php');
+
+/**
+ * tvCom service test case.
+ */
+class TVComServiceTest extends TVComServiceTestBase
+{
+	/**
+	 * Set up the test initial data
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+	}
+
+}
+
