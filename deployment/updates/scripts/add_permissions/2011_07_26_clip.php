e2c17da1d52915d43e5ec3a3e65f8c56e64ed129
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-26 12:51:53 +0000

    qnd: trim permission is not requiered
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67270 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/add_permissions/2011_07_26_clip.php
+++ deployment/updates/scripts/add_permissions/2011_07_26_clip.php
@@ -9,5 +9,5 @@
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
-$config = realpath(dirname(__FILE__)) . '/configs/clip_and_trim.ini';
+$config = realpath(dirname(__FILE__)) . '/configs/clip.ini';
 passthru("php $script $config");
