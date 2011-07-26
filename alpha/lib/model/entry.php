d711719a6701b976a6f3f73a21c1da09ebcd7345
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-26 12:23:33 +0000

    qnd: if never passed
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67267 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/entry.php
+++ alpha/lib/model/entry.php
@@ -2079,7 +2079,7 @@ class entry extends Baseentry implements ISyncableFile, IIndexable
 	public function setUpdatedAt($v)
 	{
 		parent::setUpdatedAt($v);
-		if(!in_array(entryPeer::UPDATED_AT, $this->modifiedColumns, false));
+		if(!in_array(entryPeer::UPDATED_AT, $this->modifiedColumns, false))
 			$this->modifiedColumns[] = entryPeer::UPDATED_AT;
 			
 		return $this;
