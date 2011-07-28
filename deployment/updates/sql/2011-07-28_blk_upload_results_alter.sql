2c9160ffb5f5524fee5ded0a6dc4898224758e88
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-28 17:27:26 +0000

    req: 145 - Support different action type on bulk upload results
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67501 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2011-07-28_blk_upload_results_alter.sql
+++ deployment/updates/sql/2011-07-28_blk_upload_results_alter.sql
@@ -1,7 +1,7 @@
 
 ALTER TABLE bulk_upload_result
 CHANGE entry_id object_id VARCHAR(20) NULL,
-ADD object_type INTEGER(11) NULL AFTER object_id DEFAULT '1',
-ADD `action` INTEGER(11) NULL AFTER object_type DEFAULT '1',
+ADD object_type INTEGER(11) NULL DEFAULT '1' AFTER object_id,
+ADD `action` INTEGER(11) NULL DEFAULT '1' AFTER object_type,
 ADD custom_data TEXT NULL AFTER plugins_data;
 
