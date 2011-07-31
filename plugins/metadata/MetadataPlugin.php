2b0f72f75f1fdf1cf91adcc00bfe84dd796fa9ed
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-31 16:30:38 +0000

    qnd: no need for external config file
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67599 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/MetadataPlugin.php
+++ plugins/metadata/MetadataPlugin.php
@@ -541,13 +541,26 @@ class MetadataPlugin extends KalturaPlugin implements IKalturaVersion, IKalturaP
 	/* (non-PHPdoc)
 	 * @see IKalturaSphinxConfiguration::getSphinxSchema()
 	 */
-	public static function getSphinxSchema(){
-		return MetadataSphinxConfiguration::getConfiguration();
+	public static function getSphinxSchema()
+	{
+		$kalturaEntryFields = Array ();
+
+		$numOfDateFields = MetadataPlugin::getSphinxLimitField(MetadataPlugin::SPHINX_EXPENDER_FIELD_DATE);
+		$numOfIntFields = MetadataPlugin::getSphinxLimitField(MetadataPlugin::SPHINX_EXPENDER_FIELD_INT);
+			
+		for ($i=0; $i < $numOfDateFields; $i++)
+			$kalturaEntryFields[MetadataPlugin::getSphinxFieldName(MetadataPlugin::SPHINX_EXPENDER_FIELD_DATE) . $i] = SphinxFieldType::RT_ATTR_TIMESTAMP;
+			
+		for ($i=0; $i < $numOfIntFields; $i++)
+			$kalturaEntryFields[MetadataPlugin::getSphinxFieldName(MetadataPlugin::SPHINX_EXPENDER_FIELD_INT) . $i] = SphinxFieldType::RT_ATTR_BIGINT;
+		
+		$kalturaEntryFields[MetadataPlugin::getSphinxFieldName(MetadataPlugin::SPHINX_EXPENDER_FIELD_DATA)] = SphinxFieldType::RT_FIELD;
+		//TODO - change to be taken using kSphinxManager::getSphinxIndexName('entry::table_name')
+		$sphinxSchema[kSphinxSearchManager::getSphinxIndexName('entry')]['fields'] = $kalturaEntryFields;
+		return $sphinxSchema;
 	}
-	
 
 	/**
-	 * 
 	 * return number of fields in kaltura_entry index (in sphinx) for given type
 	 * @param int $type
 	 */
