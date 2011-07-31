e191713d163daef2dd613487cdc11ed721e1ce82
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-07-31 16:30:52 +0000

    qnd: no need for external config file
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67600 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/cue_points/sphinx/CuePointSphinxPlugin.php
+++ plugins/cue_points/sphinx/CuePointSphinxPlugin.php
@@ -29,7 +29,40 @@ class CuePointSphinxPlugin extends KalturaPlugin implements IKalturaCriteriaFact
 	/* (non-PHPdoc)
 	 * @see IKalturaSphinxConfiguration::getSphinxSchema()
 	 */
-	public static function getSphinxSchema(){
-		return CuePointSphinxConfiguration::getConfiguration();
+	public static function getSphinxSchema()
+	{
+		return array(
+			kSphinxSearchManager::getSphinxIndexName('cue_point') => array (	
+				'path'		=> '/sphinx/kaltura_cue_point_rt',
+				'fields'	=> array (
+					'parent_id' => SphinxFieldType::RT_FIELD,
+					'entry_id' => SphinxFieldType::RT_FIELD,
+					'name' => SphinxFieldType::RT_FIELD,
+					'system_name' => SphinxFieldType::RT_FIELD,
+					'text' => SphinxFieldType::RT_FIELD,
+					'tags' => SphinxFieldType::RT_FIELD,
+					'roots' => SphinxFieldType::RT_FIELD,
+					
+					'int_cue_point_id' => SphinxFieldType::RT_ATTR_BIGINT,
+					'cue_point_int_id' => SphinxFieldType::RT_ATTR_BIGINT,
+					'partner_id' => SphinxFieldType::RT_ATTR_BIGINT,
+					'start_time' => SphinxFieldType::RT_ATTR_BIGINT,
+					'end_time' => SphinxFieldType::RT_ATTR_BIGINT,
+					'duration' => SphinxFieldType::RT_ATTR_BIGINT,
+					'cue_point_status' => SphinxFieldType::RT_ATTR_BIGINT,
+					'cue_point_type' => SphinxFieldType::RT_ATTR_BIGINT,
+					'sub_type' => SphinxFieldType::RT_ATTR_BIGINT,
+					'kuser_id' => SphinxFieldType::RT_ATTR_BIGINT,
+					'partner_sort_value' => SphinxFieldType::RT_ATTR_BIGINT,
+					'force_stop' => SphinxFieldType::RT_ATTR_UINT,
+					
+					'created_at' => SphinxFieldType::RT_ATTR_TIMESTAMP,
+					'updated_at' => SphinxFieldType::RT_ATTR_TIMESTAMP,
+	
+					'str_entry_id' => SphinxFieldType::RT_ATTR_STRING,
+					'str_cue_point_id' => SphinxFieldType::RT_ATTR_STRING
+				)
+			)
+		);
 	}
 }
