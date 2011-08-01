231df92923f8f5aacb872ab6653397fd4d15e2bd
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-01 21:13:05 +0000

    qnd: define caption search permissions
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67739 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/add_permissions/2011_08_01_caption_search.php
+++ deployment/updates/scripts/add_permissions/2011_08_01_caption_search.php
@@ -9,5 +9,5 @@
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
-$config = realpath(dirname(__FILE__)) . '/../../../../plugins/caption/base/config/permissions.ini';
+$config = realpath(dirname(__FILE__)) . '/../../../../plugins/caption/search/config/permissions.ini';
 passthru("php $script $config");
