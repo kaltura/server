1194f69fadeeb35d952249f8a54cf3532e7fdf16
dorp <dorp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-02 11:40:41 +0000

    def: 1 hotfix for the entire partner usage calculations that were multiplied due to a bad SQL join - Last fix had performance issues .
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67811 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/admin_console/admin_console_detail.sql
+++ alpha/apps/kaltura/lib/reports/admin_console/admin_console_detail.sql
@@ -17,31 +17,42 @@ SELECT
 	IFNULL(kalturadw.calc_partner_storage_data_time_range({FROM_DATE_ID}, {TO_DATE_ID}, aggr_p.partner_id), 0) "storage all time mb"
 FROM
 (
-	SELECT	partner_status_id STATUS,
-    	partner_name,
-    	created_at,
-    	partner_package,
-    	dim_partner.partner_id partner_id,	
-		IFNULL(SUM(count_loads), 0) count_loads,
-		IFNULL(SUM(count_plays), 0) count_plays,
-		IFNULL(SUM(count_video), 0) count_video,
-		IFNULL(SUM(count_audio), 0) count_audio,
-		IFNULL(SUM(count_mix), 0) count_mix,
-		IFNULL(SUM(count_image), 0) count_image,
-		IFNULL(SUM(count_bandwidth_kb), 0) count_bandwidth,
-		IFNULL(SUM(count_storage_mb), 0) count_storage
-	FROM kalturadw.dwh_dim_partners dim_partner 
+	SELECT 	STATUS, 	
+	partner_name, 
+	created_at, 
+	partner_package, 
+	media_usage.partner_id, 
+	count_loads, 
+	count_plays, 
+	count_video, 
+	count_audio, 
+	count_mix, 
+	count_image, 
+	count_bandwidth, 
+	count_storage FROM 
+	(	SELECT	partner_status_id STATUS, partner_name, created_at, partner_package,
+	    	dim_partner.partner_id partner_id,	IFNULL(SUM(count_loads), 0) count_loads,
+			IFNULL(SUM(count_plays), 0) count_plays, IFNULL(SUM(count_video), 0) count_video,
+			IFNULL(SUM(count_audio), 0) count_audio, IFNULL(SUM(count_mix), 0) count_mix,
+			IFNULL(SUM(count_image), 0) count_image
+		FROM kalturadw.dwh_dim_partners dim_partner 
 		LEFT JOIN kalturadw.dwh_hourly_partner aggr_partner  
 		ON (aggr_partner.partner_id = dim_partner.partner_id AND aggr_partner.date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID})
-		LEFT JOIN (SELECT date_id, partner_id, SUM(count_bandwidth_kb) count_bandwidth_kb, SUM(count_storage_mb) count_storage_mb FROM kalturadw.dwh_hourly_partner_usage 
-		WHERE date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID} GROUP BY date_id, partner_id) hourly_partner_usage 
-		ON (hourly_partner_usage.partner_id = dim_partner.partner_id)
-	WHERE   {OBJ_ID_CLAUSE} AND
-	dim_partner.created_date_id <= {TO_DATE_ID}
-	GROUP BY dim_partner.partner_id
-	ORDER BY dim_partner.partner_id
-	LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */
-	) aggr_p,
+		WHERE   {OBJ_ID_CLAUSE} AND dim_partner.created_date_id <= {TO_DATE_ID}
+		GROUP BY dim_partner.partner_id
+		ORDER BY dim_partner.partner_id
+		LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */) media_usage,
+	(	SELECT dim_partner.partner_id partner_id, 	IFNULL(SUM(count_bandwidth_kb), 0) count_bandwidth,
+			IFNULL(SUM(count_storage_mb), 0) count_storage
+		FROM kalturadw.dwh_dim_partners dim_partner 
+		LEFT JOIN kalturadw.dwh_hourly_partner_usage hourly_partner_usage 
+		ON (hourly_partner_usage.partner_id = dim_partner.partner_id AND hourly_partner_usage.date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID})
+		WHERE   {OBJ_ID_CLAUSE} AND	dim_partner.created_date_id <= {TO_DATE_ID}
+		GROUP BY dim_partner.partner_id
+		ORDER BY dim_partner.partner_id
+		LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */) resources_usage
+	WHERE media_usage.partner_id = resources_usage.partner_id	
+) aggr_p,
 (
 	SELECT	dim_partner.partner_id, 
 		SUM(IFNULL(count_video, 0) + IFNULL(count_audio, 0) + IFNULL(count_image, 0)) count_media_all_time
