5d067abe275237541a1f320a84c5779c54f3f351
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-02 23:21:59 +0000

    ps: small fix for when syncKey does not exist
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@67885 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/providers/youtube/lib/api/KalturaYouTubeDistributionJobProviderData.php
+++ plugins/content_distribution/providers/youtube/lib/api/KalturaYouTubeDistributionJobProviderData.php
@@ -50,14 +50,16 @@ class KalturaYouTubeDistributionJobProviderData extends KalturaConfigurableDistr
 		if($flavorAsset) 
 		{
 			$syncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
-			$this->videoAssetFilePath = kFileSyncUtils::getLocalFilePathForKey($syncKey, true);
+			if(kFileSyncUtils::fileSync_exists($syncKey))
+			    $this->videoAssetFilePath = kFileSyncUtils::getLocalFilePathForKey($syncKey, true);
 		}
 		
 		$thumbAssets = assetPeer::retrieveByIds(explode(',', $distributionJobData->entryDistribution->thumbAssetIds));
 		if(count($thumbAssets))
 		{
 			$syncKey = reset($thumbAssets)->getSyncKey(thumbAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
-			$this->thumbAssetFilePath = kFileSyncUtils::getLocalFilePathForKey($syncKey, true);
+			if(kFileSyncUtils::fileSync_exists($syncKey))
+			    $this->thumbAssetFilePath = kFileSyncUtils::getLocalFilePathForKey($syncKey, true);
 		}
 		
 		$entryDistributionDb = EntryDistributionPeer::retrieveByPK($distributionJobData->entryDistributionId);
