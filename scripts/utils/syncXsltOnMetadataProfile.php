def74ee9ab454734e17ac601798e26a3998d5de2
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-08 18:43:13 +0000

    qnd: fixed caching problem for script execution
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68212 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/syncXsltOnMetadataProfile.php
+++ scripts/utils/syncXsltOnMetadataProfile.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once(dirname(__FILE__).'/../bootstrap.php');
-require_once(dirname(__FILE__).'/../../api_v3/bootstrap.php');
-
 // ---------------------------------------------------------------------------
 $xsltFilePath = null;         //TODO: change to valid xslt file path
 $metadataProfileId = null;    //TODO: change to a valid metadata profile id
@@ -17,6 +14,17 @@ if (!$metadataProfileId) {
 if (!file_exists($xsltFilePath)) {
     die('Cannot find file at ['.$xsltFilePath.']'.PHP_EOL);
 }
+
+
+require_once(dirname(__FILE__).'/../bootstrap.php');
+require_once(dirname(__FILE__).'/../../api_v3/bootstrap.php');
+
+
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "metadata", "*"));
+KAutoloader::setClassMapFilePath(KALTURA_ROOT_PATH.'/cache/scripts/classMap'.uniqid().'.cache');
+KAutoloader::register();
+
 
 $dbMetadataProfile = MetadataProfilePeer::retrieveById($metadataProfileId);
 if (!$dbMetadataProfile) {
