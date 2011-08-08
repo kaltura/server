619690df193a74e11068b1131d32cf50ba299262
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-08 13:19:31 +0000

    def: 24 - added partner email validation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68170 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ api_v3/lib/types/KalturaUserLoginData.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaUserLoginData extends KalturaObject implements IFilterable 
+{
+	/**
+	 * @var string
+	 */
+	public $id;
+	
+	/**
+	 * @var string
+	 * @filter eq
+	 */
+	public $loginEmail;
+
+
+	private static $map_between_objects = array
+	(
+		"id", 
+		"loginEmail",
+	);
+
+	public function getMapBetweenObjects ( )
+	{
+		return array_merge ( parent::getMapBetweenObjects() , self::$map_between_objects );
+	}
+
+	
+	public function toObject($dbObject = null, $skip = array())
+	{
+		if (is_null($dbObject))
+			$dbObject = new UserLoginData();
+			
+		return parent::toObject($dbObject, $skip);	
+	}
+	
+	
+	public function fromObject($sourceObject)
+	{
+		if(!$sourceObject)
+			return;
+			
+		parent::fromObject($sourceObject);
+	}
+	
+	public function getExtraFilters()
+	{ 
+		return array();		
+	}
+	
+	public function getFilterDocs()
+	{
+		return array();	
+	}
+}
