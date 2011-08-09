dae15b996a08a7e35beda3bed348868593dc923a
nogab <nogab@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-09 02:05:17 +0000

    PS: setmrssParametersForFeed fix
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68225 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/setMrssParametersForFeed.php
+++ scripts/utils/setMrssParametersForFeed.php
@@ -2,13 +2,15 @@
 
 require_once(dirname(__FILE__).'/../bootstrap.php');
 
-/*syndication feed Id*/
-$syndicationFeedId = null;
+//-------------------------------------------------------------------------------------------------------------
+$syndicationFeedId = null; // insert valid syndication feed Id
 
 //setting custom data fields of the syndication feed
-// xpath should be of the following form: /*[local-name()='metadata']/*[local-name()='Custom_Data_Field_Name']
-$itemXpathsToExtend = array();
 
+// xpath should be of the following form: 
+//$itemXpathsToExtend[] = "/*[local-name()='metadata']/*[local-name()='Custom_Data_Field_Name']";
+$itemXpathsToExtend = array();
+//-------------------------------------------------------------------------------------------------------------
 
 // don't add to database if one of the parameters is missing or is an empty string
 if (!$syndicationFeedId) {
