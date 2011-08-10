781fdd5aea2d294c54977862bc6cbfeda0574da4
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-10 17:10:44 +0000

    qnd: tesing different schemas
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68342 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/api/schema/SchemaServiceTestValidate.php
+++ tests/api/schema/SchemaServiceTestValidate.php
@@ -8,26 +8,60 @@ require_once(dirname(__FILE__) . '/../../bootstrap.php');
  */
 class SchemaServiceTestValidate extends KalturaApiTestCase
 {
-
+	private $serviceUrl = 'http://devtests.kaltura.co.cc';
+	
+	private static $types = array(
+		'bulkUploadXml.bulkUploadXML',
+		'dropFolderXmlBulkUpload.dropFolderXml',
+		'cuePoint.ingestAPI',
+		'cuePoint.serveAPI',
+		'syndication',
+	);
+	
 	public function testXsd()
 	{
-		$type = 'bulkUploadXml.bulkUploadXML';
 		$serviceUrl = $this->client->getConfig()->serviceUrl;
-		$xsdPath = "$serviceUrl/api_v3/service/schema/action/serve/type/$type";
+// 		$serviceUrl = $this->serviceUrl;
 		
-		$xsd = new DOMDocument();
-		$xsd->load($xsdPath);
-		$xsd->schemaValidate('http://www.w3.org/2001/XMLSchema.xsd');
+		foreach(self::$types as $type)
+		{
+			echo "Testing XSD Type [$type]\n";
+			
+			$xsdPath = "$serviceUrl/api_v3/service/schema/action/serve/type/$type";
+			
+			$xsd = new DOMDocument();
+			$xsd->load($xsdPath);
+			$xsd->schemaValidate('http://www.w3.org/2001/XMLSchema.xsd');
+		}
 	}
 	
 	public function testXml()
 	{
-		$this->doTest('testsData/bulk_upload.1.xml', KalturaSchemaType::BULK_UPLOAD_XML);
+		foreach(self::$types as $type)
+		{
+			echo "Testing XML Type [$type]\n";
+			
+			$path = dirname(__FILE__) . "/testsData/$type";
+			$this->assertFileExists($path);
+			
+			if(!is_dir($path))
+				continue;
+			
+			$xmlFiles = scandir($path);
+			foreach($xmlFiles as $xmlFile)
+			{
+				$xmlFile = realpath("$path/$xmlFile");
+				if(!is_dir($xmlFile))
+					$this->doTest($xmlFile, $type);
+			}
+		}
 	}
 	
 	public function doTest($xmlPath, $type)
 	{
+		echo "	Testing File [$xmlPath]\n";
 		$serviceUrl = $this->client->getConfig()->serviceUrl;
+// 		$serviceUrl = $this->serviceUrl;
 		$xsdPath = "$serviceUrl/api_v3/service/schema/action/serve/type/$type";
 		
 //		libxml_use_internal_errors(true);
@@ -39,8 +73,7 @@ class SchemaServiceTestValidate extends KalturaApiTestCase
 		if(!$doc->schemaValidate($xsdPath)) 
 		{
 			$description = kXml::getLibXmlErrorDescription(file_get_contents($xmlPath));
-			
-	        throw new Exception($description);
+			$this->fail("Type [$type] File [$xmlPath]: $description");
 		}
 	}
 }
