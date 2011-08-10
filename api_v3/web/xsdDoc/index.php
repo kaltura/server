bfb1f7e3dfa63097c3118d33b8f1fa6db7681fc6
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-10 12:13:04 +0000

    qnd: support xsd documentation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68323 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ api_v3/web/xsdDoc/index.php
@@ -0,0 +1,49 @@
+<?php 
+error_reporting(E_ALL);
+
+require_once("../../bootstrap.php");
+
+// get inputs
+$schemaType = @$_GET["type"];
+
+// get cache file name
+$cachePath = kConf::get("cache_root_path").'/xsdDoc';
+$cacheKey = 'root';
+if ($schemaType)
+	$cacheKey = $schemaType;
+
+$cacheFilePath = "$cachePath/$cacheKey.cache";
+
+// Html headers + scripts
+require_once("header.php");
+
+// if (file_exists($cacheFilePath))
+// {
+// 	print file_get_contents($cacheFilePath);
+// 	die;
+// }
+
+ob_start();
+
+require_once("left_pane.php");
+
+?>
+	<div class="right">
+		<div id="doc" >
+			<?php 
+			
+				if ($schemaType)
+					require_once("schema_info.php");
+			
+			?>
+		</div>
+	</div>
+<?php 
+
+$out = ob_get_contents();
+ob_end_clean();
+print $out;
+
+kFile::setFileContent($cacheFilePath, $out);
+
+require_once("footer.php");
