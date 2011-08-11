4b6e7772d9025302e852562e1040b7da00b599e8
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-11 12:46:15 +0000

    qnd: clean lib folders before copy the file
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68399 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.sh
+++ generator/generate.sh
@@ -8,6 +8,10 @@ rm -fr ../cache/generator/*
 
 php generate.php "$@"
 
+rm -fr ../admin_console/lib/Kaltura/Client
+rm -fr ../batch/client/*
+rm -fr ../tests/lib/*
+
 rsync -avC $output_path/php5ZendClientAdminConsole/ ../admin_console/lib
 rsync -avC $output_path/batchClient/ ../batch/client
 rsync -avC  $output_path/testsClient/* ../tests/lib
