b617a977ea1aa7ee75e1579ae1a86b0b3a140667
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-11 06:32:30 +0000

    qnd: delete output_path before generating
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68363 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+output_path=`php -r 'require_once("bootstrap.php"); echo myContentStorage::getFSContentRootPath() . "/content/clientlibs";' 2>&1`
+
+rm -fr $output_path/*
 rm -fr ../cache/api_v3/*
 rm -fr ../cache/generator/*
 
 php generate.php "$@"
-
-output_path=`php -r 'require_once("bootstrap.php"); echo myContentStorage::getFSContentRootPath() . "/content/clientlibs";' 2>&1`
 
 rsync -avC $output_path/php5ZendClientAdminConsole/ ../admin_console/lib
 rsync -avC $output_path/batchClient/ ../batch/client
