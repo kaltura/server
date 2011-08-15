d608dffa35131bf8d7f758f952ff318d887cb454
atars <atars@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-15 09:14:46 +0000

    fwr: kmc v4.2.11.1
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68563 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/kConf.php
+++ alpha/config/kConf.php
@@ -115,7 +115,7 @@ class kConf extends kConfLocal
 				"clipapp_version" => 'v0.9.9',
 				"kmc_secured_login" => false,
 				
-				"kmc_version" => 'v4.2.11',
+				"kmc_version" => 'v4.2.11.1',
 				"new_partner_kmc_version" => 4,
 				
 				"paypal_data" => array (),
