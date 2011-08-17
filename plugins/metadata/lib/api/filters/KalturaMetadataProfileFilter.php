cdf8c196c8c4d652e305f1d011a8b1747148c656
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-17 16:20:17 +0000

    def: 510 _eq_metadata_object_type should be translated to match the db column _eq_object_type
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68744 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/lib/api/filters/KalturaMetadataProfileFilter.php
+++ plugins/metadata/lib/api/filters/KalturaMetadataProfileFilter.php
@@ -5,4 +5,13 @@
  */
 class KalturaMetadataProfileFilter extends KalturaMetadataProfileBaseFilter
 {
+	private $map_between_objects = array
+	(
+		"metadataObjectTypeEqual" => "_eq_object_type",
+	);
+
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), $this->map_between_objects);
+	}
 }
