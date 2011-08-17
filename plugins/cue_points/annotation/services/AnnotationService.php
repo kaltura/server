b2c16c01f8d26243bb1ad3275d018e2a3c8b8b75
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-17 16:10:16 +0000

    def: 517 backward compatibility to old attribute name (annotation)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68743 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/cue_points/annotation/services/AnnotationService.php
+++ plugins/cue_points/annotation/services/AnnotationService.php
@@ -9,7 +9,7 @@
  * @deprecated use cuePoint service instead
  */
 class AnnotationService extends CuePointService
-{	
+{
 	/**
 	 * @return CuePointType or null to limit the service type
 	 */
@@ -17,12 +17,38 @@ class AnnotationService extends CuePointService
 	{
 		return AnnotationPlugin::getCuePointTypeCoreValue(AnnotationCuePointType::ANNOTATION);
 	}
-	
+
 	public function initService($serviceId, $serviceName, $actionName)
 	{
 		parent::initService($serviceId, $serviceName, $actionName);
-		
+
 		if(!AnnotationPlugin::isAllowedPartner($this->getPartnerId()))
 			throw new KalturaAPIException(KalturaErrors::SERVICE_FORBIDDEN, $this->serviceName.'->'.$this->actionName);
+	}
+
+	/**
+	 * Allows you to add an annotation object associated with an entry
+	 *
+	 * @action add
+	 * @param KalturaAnnotation $annotation
+	 * @return KalturaAnnotation
+	 */
+	function addAction(KalturaAnnotation $annotation)
+	{
+		return parent::addAction($annotation);
+	}
+
+	/**
+	 * Update annotation by id
+	 *
+	 * @action update
+	 * @param string $id
+	 * @param KalturaAnnotation $annotation
+	 * @return KalturaAnnotation
+	 * @throws KalturaCuePointErrors::INVALID_CUE_POINT_ID
+	 */
+	function updateAction($id, KalturaAnnotation $annotation)
+	{
+		return parent::updateAction($id, $annotation);
 	}
 }
