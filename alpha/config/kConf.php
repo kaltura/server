8b8550ffd4992f2d1116377786f3c4c90a0dc7c9
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-22 12:46:28 +0000

    qnd: updated html5 version to v1.4b21
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@68943 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/kConf.php
+++ alpha/config/kConf.php
@@ -111,7 +111,7 @@ class kConf extends kConfLocal
 				"editors_flex_wrapper_version" => 'v1.01',
 				"kdp_wrapper_version" => 'v11.0',
 				"kdp3_wrapper_version" => 'v34.0',
-				"html5_version" => 'v1.4b21',
+				"html5_version" => 'v1.4b27',
 				"clipapp_version" => 'v1.0.1',
 				"kmc_secured_login" => false,
 				
