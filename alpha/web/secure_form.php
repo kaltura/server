d3c2491caf708292c15cb7b5f11b557b03870d4b
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-24 13:44:32 +0000

    qnd: fixed button layout
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69087 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/web/secure_form.php
+++ alpha/web/secure_form.php
@@ -348,11 +348,11 @@ HTML;
 		.error { color: #ff0000; font-weight: bold; font-size: 12px; margin-bottom: -10px; }
 		input { font-size: 13px; width: 170px; }
 		.truncated { overflow: hidden; text-overflow: ellipsis; white-space: nowrap; }
-		button { margin: 0 auto 5px; padding: 0 28px 0 0; height: 25px; border: 0; font: normal 11px arial,sans-serif; color:#2B2B2B; line-height: normal; overflow: visible; background: url(lib/images/kmc/kmc_sprite.png) no-repeat -72px -153px; cursor: pointer; }
-		button span { height:20px; padding: 4px 0 0 28px; margin: 0 1px 0 0; float:left; white-space:nowrap; background:transparent url(lib/images/kmc/kmc_sprite.png) no-repeat scroll 0 -153px;}
+		button { margin: 0 auto 5px; padding: 0 28px 0 0; height: 25px; border: 0; font: normal 11px arial,sans-serif; color:#2B2B2B; line-height: normal; overflow: visible; background: url(lib/images/kmc/kmc_sprite.png) no-repeat -72px -152px; cursor: pointer; }
+		button span { height:20px; padding: 4px 0 0 28px; margin: 1px 1px 0 0; float:left; white-space:nowrap; background:transparent url(lib/images/kmc/kmc_sprite.png) no-repeat scroll 0 -153px;}
 		button:hover span { background-position: 0 -178px;}
 		@-moz-document url-prefix() {
-	   		button span { margin: -1px 2px 0 -3px; }
+	   		button span { margin: -2px 2px 0 -3px; }
 	   		button { display: block; padding-right: 20px; }
 	   	}
 		</style>
