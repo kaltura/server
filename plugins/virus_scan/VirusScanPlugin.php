d4c623f3314af9096738aca2ef6f10a1e82912cf
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-25 10:42:00 +0000

    qnd: virusscanbatch removed
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69129 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/virus_scan/VirusScanPlugin.php
+++ plugins/virus_scan/VirusScanPlugin.php
@@ -177,8 +177,8 @@ class VirusScanPlugin extends KalturaPlugin implements IKalturaPermissions, IKal
 		if($configName == 'generator')
 			return new Zend_Config_Ini(dirname(__FILE__) . '/config/generator.ini');
 	
-		if($configName == 'testme')
-			return new Zend_Config_Ini(dirname(__FILE__) . '/config/testme.ini');
+// 		if($configName == 'testme')
+// 			return new Zend_Config_Ini(dirname(__FILE__) . '/config/testme.ini');
 			
 		return null;
 	}
