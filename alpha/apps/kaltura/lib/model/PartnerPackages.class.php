f3623daa0d247d2de14c201040deb14e1b3c7613
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-25 15:13:59 +0000

    fwr: change partner packages
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69172 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/model/PartnerPackages.class.php
+++ alpha/apps/kaltura/lib/model/PartnerPackages.class.php
@@ -7,11 +7,11 @@
 class PartnerPackages
 {
 	const PARTNER_PACKAGE_FREE = 1;
-	const PARTNER_PACKAGE_20 = 2;
-	const PARTNER_PACKAGE_50 = 3;
-	const PARTNER_PACKAGE_100 = 4;
-	const PARTNER_PACKAGE_250 = 5;
-	const PARTNER_PACKAGE_500 = 6;
+	const PARTNER_PACKAGE_PAID = 2;
+	
+	const CLASS_OF_SERVICE_SILVER = 1;
+	const CLASS_OF_SERVICE_GOLD = 2;
+	const CLASS_OF_SERVICE_PLATINUM = 3;	
 	
 	const PACKAGE_SUPPORT_TYPE_NONE = 0;
 	const PACKAGE_SUPPORT_TYPE_COMMUNITY = 1;
@@ -26,6 +26,8 @@ class PartnerPackages
 	const KALTURA_LOCKED_PACKAGE_UPGRADE = 86;
 	
 	private $packages = null;
+	private $packagesClassOfService = null;
+	private $packagesVertical = null;
 	
 	public function PartnerPackages ()
 	{
@@ -36,6 +38,24 @@ class PartnerPackages
 			$arrPackage = $this->flatXml2arr($package);
 			$this->packages[$arrPackage['id']] = $arrPackage;
 		}
+		
+		$package_class_of_service_config = simplexml_load_file(dirname(__FILE__).'/../../config/partnerPackagesClassOfService.xml');
+		$package_nodes = $package_class_of_service_config->xpath('/packages/package');
+		foreach ($package_nodes as $package)
+		{
+			$arrPackage = $this->flatXml2arr($package);
+			$this->packagesClassOfService[$arrPackage['id']] = $arrPackage;
+		}
+		
+		$package_vertical = simplexml_load_file(dirname(__FILE__).'/../../config/partnerVertical.xml');
+		$package_nodes = $package_vertical->xpath('/packages/package');
+		foreach ($package_nodes as $package)
+		{
+			$arrPackage = $this->flatXml2arr($package);
+			$this->packagesVertical[$arrPackage['id']] = $arrPackage;
+		}
+		
+		
 	}
 	
 	private function flatXml2arr($flatXml) 
@@ -60,9 +80,25 @@ class PartnerPackages
 		return $this->packages[$packageId];
 	}
 	
+	public function getPackageClassOfServiceDetails ( $packageId )
+	{
+		return $this->packagesClassOfService[$packageId];
+	}
+	
 	public function listPackages ()
 	{
 		return $this->packages;
 	}
+	
+	public function listPackagesClassOfService()
+	{
+		return $this->packagesClassOfService;
+	}
+	
+	public function listPackagesVertical()
+	{
+		return $this->packagesVertical;
+	}
+	
 	
 }
