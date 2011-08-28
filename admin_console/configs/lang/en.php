2348435ef4ce553644483b78f87b1e4930761daf
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-28 11:17:03 +0000

    qnd: need the seconds info in all screens
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69215 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/configs/lang/en.php
+++ admin_console/configs/lang/en.php
@@ -1,8 +1,8 @@
 <?php
 return array(
 	// globals
-	'time format' => 'm/d/y H:i',
-	'date format' => 'm/d/y H:i',
+	'time format' => 'm/d/y H:i:s',
+	'date format' => 'm/d/y H:i:s',
 	'csv date' => 'MM-dd-YYYY',
 	'datepicker format' => 'MM/dd/YYYY',
 
