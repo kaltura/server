d5206a2de4c17759b3cb54617adcab32c1e053a4
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-28 12:56:03 +0000

    def: 479 mark flavor as importing even if it's already ready
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69227 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/kJobsManager.php
+++ alpha/apps/kaltura/lib/batch2/kJobsManager.php
@@ -724,11 +724,8 @@ class kJobsManager
  		
  		if($asset)
  		{
- 			if($asset->getStatus() != asset::FLAVOR_ASSET_STATUS_READY)
- 			{
-	 			$asset->setStatus(asset::FLAVOR_ASSET_STATUS_IMPORTING);
-	 			$asset->save();
- 			}
+ 			$asset->setStatus(asset::FLAVOR_ASSET_STATUS_IMPORTING);
+ 			$asset->save();
  			
  			$jobData->setFlavorAssetId($asset->getId());
  		}
