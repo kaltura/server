fc87b705ecf9568427f02c59042ef6c0e288e0be
nogab <nogab@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-29 23:45:46 +0000

    PS: Time Warner distribution connector
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69329 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/content_distribution/providers/time_warner/lib/api/TimeWarnerDistributionErrors.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package plugins.timeWarnerDistribution
+ * @subpackage api
+ */
+class TimeWarnerDistributionErrors extends KalturaErrors
+{
+	const INVALID_FEED_URL = 'INVALID_FEED_URL,Invalid feed URL';
+}
