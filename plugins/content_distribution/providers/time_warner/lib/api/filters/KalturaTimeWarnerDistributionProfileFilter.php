3dc69796b259cf48de2012ca4ccd749d91b6f233
nogab <nogab@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-29 23:44:53 +0000

    PS: Time Warner distribution connector
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69328 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/content_distribution/providers/time_warner/lib/api/filters/KalturaTimeWarnerDistributionProfileFilter.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package plugins.timeWarnerDistribution
+ * @subpackage api.filters
+ */
+class KalturaTimeWarnerDistributionProfileFilter extends KalturaTimeWarnerDistributionProfileBaseFilter
+{
+}
---
+++ plugins/content_distribution/providers/time_warner/lib/api/filters/orderEnums/KalturaTimeWarnerDistributionProfileOrderBy.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package plugins.timeWarnerDistribution
+ * @subpackage api.filters.enum
+ */
+class KalturaTimeWarnerDistributionProfileOrderBy extends KalturaConfigurableDistributionProfileOrderBy
+{
+}
