c95aa2ee81ca72ba1123cc0522a341adca52b291
ohada <ohada@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-30 13:33:13 +0000

    qnd: reorder methods to match unit tests
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69414 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/services/MetadataService.php
+++ plugins/metadata/services/MetadataService.php
@@ -8,6 +8,7 @@
  */
 class MetadataService extends KalturaBaseService
 {
+
 	public function initService($serviceId, $serviceName, $actionName)
 	{
 		parent::initService($serviceId, $serviceName, $actionName);
@@ -21,95 +22,20 @@ class MetadataService extends KalturaBaseService
 			throw new KalturaAPIException(KalturaErrors::SERVICE_FORBIDDEN, $this->serviceName.'->'.$this->actionName);
 	}
 	
-	
-	/**
-	 * List metadata objects by filter and pager
-	 * 
-	 * @action list
-	 * @param KalturaMetadataFilter $filter
-	 * @param KalturaFilterPager $pager
-	 * @return KalturaMetadataListResponse
-	 */
-	function listAction(KalturaMetadataFilter $filter = null, KalturaFilterPager $pager = null)
-	{
-		if (!$filter)
-			$filter = new KalturaMetadataFilter;
-			
-		$metadataFilter = new MetadataFilter();
-		$filter->toObject($metadataFilter);
-		
-		$c = new Criteria();
-		$metadataFilter->attachToCriteria($c);
-		$count = MetadataPeer::doCount($c);
-		
-		if ($pager)
-			$pager->attachToCriteria($c);
-		$list = MetadataPeer::doSelect($c);
-		
-		$response = new KalturaMetadataListResponse();
-		$response->objects = KalturaMetadataArray::fromDbArray($list);
-		$response->totalCount = $count;
-		
-		return $response;
-	}
-	
-	
-	/**
-	 * Adds a metadata object associated with Kaltura object
-	 * 
-	 * @param int $metadataProfileId
-	 * @param KalturaMetadataObjectType $objectType
-	 * @param string $objectId
-	 * @return Metadata
-	 * @throws MetadataErrors::METADATA_ALREADY_EXISTS
-	 * @throws MetadataErrors::INVALID_METADATA_PROFILE
-	 * @throws MetadataErrors::INVALID_METADATA_OBJECT
-	 */
-	protected function addMetadata($metadataProfileId, $objectType, $objectId)
-	{
-		$objectType = kPluginableEnumsManager::apiToCore('MetadataObjectType', $objectType);
-		
-		$check = MetadataPeer::retrieveByObject($metadataProfileId, $objectType, $objectId);
-		if($check)
-			throw new KalturaAPIException(MetadataErrors::METADATA_ALREADY_EXISTS, $check->getId());
-			
-		$dbMetadataProfile = MetadataProfilePeer::retrieveByPK($metadataProfileId);
-		if(!$dbMetadataProfile)
-			throw new KalturaAPIException(MetadataErrors::INVALID_METADATA_PROFILE, $metadataProfileId);
-		
-		$dbMetadata = new Metadata();
-		
-		$dbMetadata->setPartnerId($this->getPartnerId());
-		$dbMetadata->setMetadataProfileId($metadataProfileId);
-		$dbMetadata->setMetadataProfileVersion($dbMetadataProfile->getVersion());
-		$dbMetadata->setObjectType($objectType);
-		$dbMetadata->setObjectId($objectId);
-		$dbMetadata->setStatus(KalturaMetadataStatus::INVALID);
 
-		// validate object exists
-		$object = kMetadataManager::getObjectFromPeer($dbMetadata);
-		if(!$object)
-			throw new KalturaAPIException(MetadataErrors::INVALID_METADATA_OBJECT, $objectId);
-		
-		$dbMetadata->save();
-		
-		$this->deleteOldVersions($dbMetadata);
-		
-		return $dbMetadata;
-	}
-	
-	
-	/**
+/**
 	 * Allows you to add a metadata object and metadata content associated with Kaltura object
 	 * 
 	 * @action add
 	 * @param int $metadataProfileId
+
 	 * @param KalturaMetadataObjectType $objectType
 	 * @param string $objectId
 	 * @param string $xmlData XML metadata
 	 * @return KalturaMetadata
 	 * @throws MetadataErrors::METADATA_ALREADY_EXISTS
 	 * @throws MetadataErrors::INVALID_METADATA_DATA
+
 	 */
 	function addAction($metadataProfileId, $objectType, $objectId, $xmlData)
 	{
@@ -150,34 +76,54 @@ class MetadataService extends KalturaBaseService
 		
 		return $metadata;
 	}
+
 	
 	/**
-	 * @param Metadata $metadata
-	 * @return int affected records
+	 * Adds a metadata object associated with Kaltura object
+	 * 
+	 * @param int $metadataProfileId
+	 * @param KalturaMetadataObjectType $objectType
+
+	 * @param string $objectId
+	 * @return Metadata
+	 * @throws MetadataErrors::METADATA_ALREADY_EXISTS
+	 * @throws MetadataErrors::INVALID_METADATA_PROFILE
+	 * @throws MetadataErrors::INVALID_METADATA_OBJECT
 	 */
-	protected function deleteOldVersions(Metadata $metadata)
+	protected function addMetadata($metadataProfileId, $objectType, $objectId)
 	{
-		$c = new Criteria();
-		$c->add(MetadataPeer::OBJECT_ID, $metadata->getObjectId());
-		$c->add(MetadataPeer::OBJECT_TYPE, $metadata->getObjectType());
-		$c->add(MetadataPeer::METADATA_PROFILE_ID, $metadata->getMetadataProfileId());
-		$c->add(MetadataPeer::METADATA_PROFILE_VERSION, $metadata->getMetadataProfileVersion(), Criteria::LESS_THAN);
-		$c->add(MetadataPeer::STATUS, KalturaMetadataStatus::DELETED, Criteria::NOT_EQUAL);
+		$objectType = kPluginableEnumsManager::apiToCore('MetadataObjectType', $objectType);
 		
-		MetadataPeer::setUseCriteriaFilter(false);
-		$metadatas = MetadataPeer::doSelect($c);
-		foreach($metadatas as $metadata)
-			kEventsManager::raiseEvent(new kObjectDeletedEvent($metadata));
-		
-		$update = new Criteria();
-		$update->add(MetadataPeer::STATUS, KalturaMetadataStatus::DELETED);
+		$check = MetadataPeer::retrieveByObject($metadataProfileId, $objectType, $objectId);
+		if($check)
+			throw new KalturaAPIException(MetadataErrors::METADATA_ALREADY_EXISTS, $check->getId());
 			
-		$con = Propel::getConnection(MetadataPeer::DATABASE_NAME, Propel::CONNECTION_READ);
-		$count = BasePeer::doUpdate($c, $update, $con);
-		return $count;
-	}	
-	
-	/**
+		$dbMetadataProfile = MetadataProfilePeer::retrieveByPK($metadataProfileId);
+		if(!$dbMetadataProfile)
+			throw new KalturaAPIException(MetadataErrors::INVALID_METADATA_PROFILE, $metadataProfileId);
+		
+		$dbMetadata = new Metadata();
+		
+		$dbMetadata->setPartnerId($this->getPartnerId());
+		$dbMetadata->setMetadataProfileId($metadataProfileId);
+		$dbMetadata->setMetadataProfileVersion($dbMetadataProfile->getVersion());
+		$dbMetadata->setObjectType($objectType);
+		$dbMetadata->setObjectId($objectId);
+		$dbMetadata->setStatus(KalturaMetadataStatus::INVALID);
+
+		// validate object exists
+		$object = kMetadataManager::getObjectFromPeer($dbMetadata);
+		if(!$object)
+			throw new KalturaAPIException(MetadataErrors::INVALID_METADATA_OBJECT, $objectId);
+		
+		$dbMetadata->save();
+		
+		$this->deleteOldVersions($dbMetadata);
+		
+		return $dbMetadata;
+	}
+
+/**
 	 * Allows you to add a metadata object and metadata file associated with Kaltura object
 	 * 
 	 * @action addFromFile
@@ -229,11 +175,13 @@ class MetadataService extends KalturaBaseService
 	/**
 	 * Allows you to add a metadata xml data from remote URL
 	 * 
+
 	 * @action addFromUrl
 	 * @param int $metadataProfileId
 	 * @param KalturaMetadataObjectType $objectType
 	 * @param string $objectId
 	 * @param string $url XML metadata remote url
+
 	 * @return KalturaMetadata
 	 */
 	function addFromUrlAction($metadataProfileId, $objectType, $objectId, $url)
@@ -257,6 +205,7 @@ class MetadataService extends KalturaBaseService
 	 * 
 	 * @action addFromBulk
 	 * @param int $metadataProfileId
+
 	 * @param KalturaMetadataObjectType $objectType
 	 * @param string $objectId
 	 * @param string $url XML metadata remote url
@@ -269,50 +218,12 @@ class MetadataService extends KalturaBaseService
 
 	
 	/**
-	 * Delete an existing metadata
-	 * 
-	 * @action delete
-	 * @param int $id
-	 * @throws KalturaErrors::INVALID_OBJECT_ID
-	 */		
-	function deleteAction($id)
-	{
-		$dbMetadata = MetadataPeer::retrieveByPK($id);
-		
-		if(!$dbMetadata)
-			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_ID, $id);
-		
-		$dbMetadata->setStatus(KalturaMetadataStatus::DELETED);
-		$dbMetadata->save();
-	}
-
-	
-	/**
-	 * Mark existing metadata as invalid
-	 * Used by batch metadata transform
-	 * 
-	 * @action invalidate
-	 * @param int $id
-	 * @throws KalturaErrors::INVALID_OBJECT_ID
-	 */		
-	function invalidateAction($id)
-	{
-		$dbMetadata = MetadataPeer::retrieveByPK($id);
-		
-		if(!$dbMetadata)
-			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_ID, $id);
-		
-		$dbMetadata->setStatus(KalturaMetadataStatus::INVALID);
-		$dbMetadata->save();
-	}
-
-	
-	/**
 	 * Retrieve a metadata object by id
 	 * 
 	 * @action get
 	 * @param int $id 
 	 * @return KalturaMetadata
+
 	 * @throws KalturaErrors::INVALID_OBJECT_ID
 	 */		
 	function getAction($id)
@@ -341,12 +252,14 @@ class MetadataService extends KalturaBaseService
 //	function updateAction($id, KalturaMetadata $metadata, $xmlData = null)
 	
 	/**
+
 	 * Update an existing metadata object with new XML content
 	 * 
 	 * @action update
 	 * @param int $id 
 	 * @param string $xmlData XML metadata
 	 * @return KalturaMetadata
+
 	 * @throws KalturaErrors::INVALID_OBJECT_ID
 	 * @throws MetadataErrors::INVALID_METADATA_DATA
 	 */	
@@ -397,12 +310,14 @@ class MetadataService extends KalturaBaseService
 	
 	/**
 	 * Update an existing metadata object with new XML file
+
 	 * 
 	 * @action updateFromFile
 	 * @param int $id 
 	 * @param file $xmlFile XML metadata
 	 * @return KalturaMetadata
 	 * @throws KalturaErrors::INVALID_OBJECT_ID
+
 	 * @throws MetadataErrors::METADATA_FILE_NOT_FOUND
 	 * @throws MetadataErrors::INVALID_METADATA_DATA
 	 */	
@@ -460,6 +375,107 @@ class MetadataService extends KalturaBaseService
 		
 		return $metadata;
 	}		
+	
+	/**
+	 * List metadata objects by filter and pager
+	 * 
+	 * @action list
+	 * @param KalturaMetadataFilter $filter
+	 * @param KalturaFilterPager $pager
+	 * @return KalturaMetadataListResponse
+	 */
+	function listAction(KalturaMetadataFilter $filter = null, KalturaFilterPager $pager = null)
+	{
+		if (!$filter)
+			$filter = new KalturaMetadataFilter;
+			
+		$metadataFilter = new MetadataFilter();
+		$filter->toObject($metadataFilter);
+		
+		$c = new Criteria();
+		$metadataFilter->attachToCriteria($c);
+		$count = MetadataPeer::doCount($c);
+		
+		if ($pager)
+			$pager->attachToCriteria($c);
+		$list = MetadataPeer::doSelect($c);
+		
+		$response = new KalturaMetadataListResponse();
+		$response->objects = KalturaMetadataArray::fromDbArray($list);
+		$response->totalCount = $count;
+		
+		return $response;
+	}
+	
+
+	
+	
+	/**
+	 * @param Metadata $metadata
+	 * @return int affected records
+	 */
+	protected function deleteOldVersions(Metadata $metadata)
+	{
+		$c = new Criteria();
+		$c->add(MetadataPeer::OBJECT_ID, $metadata->getObjectId());
+		$c->add(MetadataPeer::OBJECT_TYPE, $metadata->getObjectType());
+		$c->add(MetadataPeer::METADATA_PROFILE_ID, $metadata->getMetadataProfileId());
+		$c->add(MetadataPeer::METADATA_PROFILE_VERSION, $metadata->getMetadataProfileVersion(), Criteria::LESS_THAN);
+		$c->add(MetadataPeer::STATUS, KalturaMetadataStatus::DELETED, Criteria::NOT_EQUAL);
+		
+		MetadataPeer::setUseCriteriaFilter(false);
+		$metadatas = MetadataPeer::doSelect($c);
+		foreach($metadatas as $metadata)
+			kEventsManager::raiseEvent(new kObjectDeletedEvent($metadata));
+		
+		$update = new Criteria();
+		$update->add(MetadataPeer::STATUS, KalturaMetadataStatus::DELETED);
+			
+		$con = Propel::getConnection(MetadataPeer::DATABASE_NAME, Propel::CONNECTION_READ);
+		$count = BasePeer::doUpdate($c, $update, $con);
+		return $count;
+	}	
+	
+	
+	/**
+	 * Delete an existing metadata
+	 * 
+	 * @action delete
+	 * @param int $id
+	 * @throws KalturaErrors::INVALID_OBJECT_ID
+	 */		
+	function deleteAction($id)
+	{
+		$dbMetadata = MetadataPeer::retrieveByPK($id);
+		
+		if(!$dbMetadata)
+			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_ID, $id);
+		
+		$dbMetadata->setStatus(KalturaMetadataStatus::DELETED);
+		$dbMetadata->save();
+	}
+
+	
+	/**
+	 * Mark existing metadata as invalid
+	 * Used by batch metadata transform
+	 * 
+	 * @action invalidate
+	 * @param int $id
+	 * @throws KalturaErrors::INVALID_OBJECT_ID
+	 */		
+	function invalidateAction($id)
+	{
+		$dbMetadata = MetadataPeer::retrieveByPK($id);
+		
+		if(!$dbMetadata)
+			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_ID, $id);
+		
+		$dbMetadata->setStatus(KalturaMetadataStatus::INVALID);
+		$dbMetadata->save();
+	}
+
+	
 
 	/**
 	 * Serves metadata XML file
