117058000a684279984e89a14f0448834a1e399e
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-08-30 13:19:14 +0000

    qnd: changed fileds names
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69407 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/configAdminConsoleAuditTrail.php
+++ deployment/updates/scripts/configAdminConsoleAuditTrail.php
@@ -24,7 +24,7 @@ $objectsToTrack = array(
 			KalturaAuditTrailAction::CHANGED,
 		),
 		'descriptors' => array(
-			"extendedFreeTrailExpiryReasonDate",
+			"extendedFreeTrailExpiryDate",
 			"extendedFreeTrailExpiryReason",
 			
 		),
