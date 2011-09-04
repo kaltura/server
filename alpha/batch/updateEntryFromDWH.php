06d6dbecef872b38c13cac3b232f802dc3bbbd48
assafb <assafb@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-04 12:30:44 +0000

    qnd: added log using kalturaLog
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@69733 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/batch/updateEntryFromDWH.php
+++ alpha/batch/updateEntryFromDWH.php
@@ -1,19 +1,34 @@
 <?php
 error_reporting ( E_ALL );
+set_time_limit(0);
 
-define ( 'SF_ROOT_DIR', realpath ( dirname ( __FILE__ ) . '/../../alpha/' ) );
-define ( 'SF_APP', 'kaltura' );
-define ( 'SF_DEBUG', true );
+ini_set("memory_limit","700M");
 
-require_once (SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
-require_once (SF_ROOT_DIR . '/../infra/bootstrap_base.php');
+define('ROOT_DIR', realpath(dirname(__FILE__) . '/../../'));
+require_once(ROOT_DIR . '/alpha/config/kConf.php');
+require_once(ROOT_DIR . '/infra/bootstrap_base.php');
+require_once(ROOT_DIR . '/infra/KAutoloader.php');
 
-DbManager::setConfig ( kConf::getDB () );
-DbManager::initialize ();
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
+KAutoloader::setClassMapFilePath(kConf::get("cache_root_path") . '/scripts/classMap.cache');
+KAutoloader::register();
+
+date_default_timezone_set(kConf::get("date_default_timezone"));
+
+$loggerConfigPath = ROOT_DIR.'/scripts/logger.ini';
+$config = new Zend_Config_Ini($loggerConfigPath);
+KalturaLog::initLog($config);
+KalturaLog::setContext(basename(__FILE__));
+KalturaLog::info("Starting script");
+
+KalturaLog::info("Initializing database...");
+DbManager::setConfig(kConf::getDB());
+DbManager::initialize();
+KalturaLog::info("Database initialized successfully");
 
 $syncType = 'entry';
 $dbh = myDbHelper::getConnection ( myDbHelper::DB_HELPER_CONN_DWH );
-$sql = "CALL get_data_for_operational('$syncType')";
+$sql = "CALL get_data_for_operational($syncType)";
 $count = 0;
 $rows = $dbh->query ( $sql )->fetchAll ();
 foreach ( $rows as $row ) {
@@ -24,12 +39,15 @@ foreach ( $rows as $row ) {
 	}
 	$entry->setViews ( $row ['views'] );
 	$entry->setPlays ( $row ['plays'] );
-	$entry->save();
-	$count++;
-	KalturaLog::debug ( 'Successfully saved entry [' . $row ['entry_id'] . ']' );
+	if ($entry->save ()) {
+		$count ++;
+		KalturaLog::debug ( 'Successfully saved entry [' . $row ['entry_id'] . ']' );
+	} else {
+		KalturaLog::err ( 'Error while saving entry [' . $row ['entry_id'] . ']' );
+	}
 	if ($count % 500)
 		entryPeer::clearInstancePool ();
 }
-$sql = "CALL mark_operational_sync_as_done('$syncType')";
+$sql = "CALL mark_operational_sync_as_done($syncType)";
 $dbh->query ( $sql );
-KalturaLog::debug ( "Done updating $count entries from DWH to operational DB" );
+KalturaLog::debug ( "Done updating entries from DWH to operational DB" );
