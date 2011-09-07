cc585c0cc2db2f1060706f2f1a67dcbba80d7fbf
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-07 21:57:27 +0000

    qnd: PHP Fatal error:  Cannot override final method Exception::getMessage()
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70001 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/batches/BulkUpload/KalturaBulkUploadAbortedException.php
+++ batch/batches/BulkUpload/KalturaBulkUploadAbortedException.php
@@ -1,44 +1,19 @@
 <?php
-
 /**
- * 
  * Represents a bulk upload aborted exception caused by job beeing aborted
  * @author Roni
- *
+ * @package Scheduler
+ * @subpackage Bulk-Upload
  */
 class KalturaBulkUploadAbortedException extends kException
 {
 	/**
-	 * 
 	 * Represents that a job was aborted
 	 * @var int
 	 */
 	const JOB_ABORTED = 0;
 	
 	/**
-	 * 
-	 * The exception message
-	 * @var string
-	 */
-	public $message = null;
-
-	/**
-	 * @return the $message
-	 */
-	public function getMessage() {
-		return $this->message;
-	}
-
-	/**
-	 * @param string $message
-	 */
-	public function setMessage($message) {
-		$this->message = $message;
-	}
-
-	/**
-	 * 
-	 * Creates a new KalturaBulkUploadException
 	 * @param string $message - the exception message
 	 * @param int $errorCode - the exception error code
 	 */
