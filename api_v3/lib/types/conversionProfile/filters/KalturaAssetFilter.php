0b51222b6a6f0556b788d8b56a27cc7193d909f0
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-10 19:16:37 +0000

    qnd: validate that asset is always filtered by entry ids
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70094 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/conversionProfile/filters/KalturaAssetFilter.php
+++ api_v3/lib/types/conversionProfile/filters/KalturaAssetFilter.php
@@ -5,4 +5,11 @@
  */
 class KalturaAssetFilter extends KalturaAssetBaseFilter
 {
+	public function toObject ( $object_to_fill = null, $props_to_skip = array() )
+	{
+		if(is_null($this->entryIdEqual) && is_null($this->entryIdIn) )
+			throw new KalturaAPIException(KalturaErrors::PROPERTY_VALIDATION_CANNOT_BE_NULL, $this->getFormattedPropertyNameWithClassName('entryIdEqual') . '/' . $this->getFormattedPropertyNameWithClassName('entryIdIn'));
+		
+		return $this->toObject($object_to_fill, $props_to_skip);
+	}
 }
