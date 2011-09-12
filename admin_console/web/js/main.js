18391d8c7b8f46b5355f6c9c3089df4da277feb0
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-12 15:34:39 +0000

    def: 703 disable form submission using "Enter" key
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70168 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/web/js/main.js
+++ admin_console/web/js/main.js
@@ -12,3 +12,20 @@ function autoSizeIframe(){
 	$('iframe.auto-height').height(height);
 	$('#wrapper').height(height); // fixes weird scrolling
 }
+
+function supressFormSubmit(e){
+
+	if(window.event) // IE
+	{
+		keynum = e.keyCode;
+	}
+	else if(e.which) // Netscape/Firefox/Opera
+	{
+		keynum = e.which;
+	}
+
+	if(keynum == 13)
+		return false;
+		
+	return true;
+}
