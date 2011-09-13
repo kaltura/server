9fcaae13e07be1468ed4b7fc7c54cde3d2948bf9
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-13 16:19:55 +0000

    qnd: fixed root dir path
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70250 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/unitTests/SphinxSearch/bootstrapSphinxSearchTest.php
+++ tests/unitTests/SphinxSearch/bootstrapSphinxSearchTest.php
@@ -5,7 +5,7 @@ error_reporting ( E_ALL );
 
 $GLOBALS ["sphinxTest"] = true;
 
-define ( 'ROOT_DIR', dirname(__FILE__) . "/../../" );
+define ( 'ROOT_DIR', dirname(__FILE__) . "/../../../" );
 define ( 'SPHINX_SEARCH_API_SESSION', 'testsData/SphinxSearchApiSession.csv' );
 define ( 'KALTURA_API_LOGGER_FILE_PATH', ROOT_DIR . "/api_v3/config/logger.ini");
 
