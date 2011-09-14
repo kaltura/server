94cf51e8c38670201a32d2219d1ad3c2a91a34f6
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-14 14:50:33 +0000

    qnd: added indexs to system_partner
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70318 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/updates/sql/2011-09-14_system_name_index.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `access_control` ADD INDEX `system_name_partner_id` ( `system_name` , `partner_id` );
+ALTER TABLE `user_role` ADD INDEX `system_name_partner_id` ( `partner_id` , `system_name` );
+ALTER TABLE `flavor_params` ADD INDEX `system_name_partner_id` ( `partner_id` , `system_name` );
+ALTER TABLE `conversion_profile_2` ADD INDEX `system_name_partner_id` ( `partner_id` , `system_name` );
+ALTER TABLE `flavor_params_conversion_profile` ADD INDEX `system_name` ( `system_name` );
+ALTER TABLE `storage_profile` ADD INDEX `system_name_partner_id` ( `partner_id` , `system_name` );
