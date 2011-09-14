63c4c2354550add0a26d6ac5812ca52ce18ef564
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-14 06:52:36 +0000

    qnd: change end of session texts.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70272 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/unitTests/SphinxSearch/getSphinxSearchApiSessionIdsFromLog.sh
+++ tests/unitTests/SphinxSearch/getSphinxSearchApiSessionIdsFromLog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # usage
 # ./ getSphinxSearchApiSessionIdsFromLog.sh KalturaApiV3LogFilePath SphinxSearchApiSessionIdsFromLog 
-grep -v 'INSERT' ${1} |  grep -v 'SELECT COUNT(*)' | egrep ' kaltura_entry|FROM `entry` WHERE|DEBUG: Disptach took - ' >${2}
+grep -v 'INSERT' ${1} |  grep -v 'SELECT COUNT(*)' | egrep ' kaltura_entry|FROM `entry` WHERE|DEBUG: Disptach finished' >${2}
 
 exit 0
 
