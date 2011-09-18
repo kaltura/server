1ebea89857f469aaa1b2aa87a5d7505644d3d96f
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-18 09:02:50 +0000

    qnd: auto generated
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70476 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ tests/api/thumbParamsOutput/ThumbParamsOutputServiceTestBase.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * thumbParamsOutput service base test case.
+ */
+abstract class ThumbParamsOutputServiceTestBase extends KalturaApiTestCase
+{
+	/**
+	 * Set up the test initial data
+	 */
+	protected function setUp()
+	{
+		$this->setGetActionTestData();
+		$this->setListActionTestData();
+
+		parent::setUp();
+	}
+
+	/**
+	 * Set up the testGetAction initial data (If needed)
+	 */
+	protected function setGetActionTestData(){}
+
+	/**
+	 * Set up the testListAction initial data (If needed)
+	 */
+	protected function setListActionTestData(){}
+
+	/**
+	 * Tests thumbParamsOutput->get action
+	 * @param int $id 
+	 * @param KalturaThumbParamsOutput $reference 
+	 * @depends testAdd
+	 * @dataProvider provideData
+	 */
+	public function testGet($id, KalturaThumbParamsOutput $reference)
+	{
+		$resultObject = $this->client->thumbParamsOutput->get($id);
+		$this->assertInstanceOf('KalturaThumbParamsOutput', $resultObject);
+		$this->compareApiObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
+		$this->validateGet($id, $reference);
+	}
+
+	/**
+	 * Validates testGet results
+	 */
+	protected function validateGet($id, KalturaThumbParamsOutput $reference)
+	{
+	}
+
+	/**
+	 * Tests thumbParamsOutput->listAction action
+	 * @param KalturaThumbParamsOutputFilter $filter 
+	 * @param KalturaFilterPager $pager 
+	 * @param KalturaThumbParamsOutputListResponse $reference 
+	 * @dataProvider provideData
+	 */
+	public function testListAction(KalturaThumbParamsOutputFilter $filter = null, KalturaFilterPager $pager = null, KalturaThumbParamsOutputListResponse $reference)
+	{
+		$resultObject = $this->client->thumbParamsOutput->listAction($filter, $pager);
+		$this->assertInstanceOf('KalturaThumbParamsOutputListResponse', $resultObject);
+		$this->compareApiObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
+		$this->validateListAction($filter, $pager, $reference);
+	}
+
+	/**
+	 * Validates testListAction results
+	 */
+	protected function validateListAction(KalturaThumbParamsOutputFilter $filter = null, KalturaFilterPager $pager = null, KalturaThumbParamsOutputListResponse $reference)
+	{
+	}
+
+	/**
+	 * Called when all tests are done
+	 * @param int $id
+	 * @return int
+	 * TODO: replace testAdd with last test function that uses that id
+	 * @depends testAdd
+	 */
+	public function testFinished($id)
+	{
+		return $id;
+	}
+
+	/**
+	 * 
+	 * Returns the suite for the test
+	 */
+	public static function suite()
+	{
+		return new KalturaTestSuite('ThumbParamsOutputServiceTest');
+	}
+
+}
