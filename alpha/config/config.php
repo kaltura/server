8df226c8da203a0d6801ae2c5facf3620faa5e24
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-21 11:41:51 +0000

    qnd: add zend to include path in case it's installed in the pear include path
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70760 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/config/config.php
+++ alpha/config/config.php
@@ -6,5 +6,5 @@ require_once ( dirname(__FILE__) . "/../../infra/kConf.php" ); // kaltura config
 $sf_symfony_lib_dir = realpath(dirname(__FILE__).'/../../symfony');
 $sf_symfony_data_dir = realpath(dirname(__FILE__).'/../../symfony-data');
 
-$include_path = get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__).'/../../vendor/ZendFramework/library');
+$include_path = realpath(dirname(__FILE__).'/../../vendor/ZendFramework/library') . PATH_SEPARATOR . get_include_path();
 set_include_path($include_path);
