be9d135df13766b90125af76db4d7968fe533b4d
raffya <raffya@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-22 11:15:32 +0000

    ps: more generic version
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70914 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/importFromBrightcove.php
+++ scripts/utils/importFromBrightcove.php
@@ -1,5 +1,6 @@
 <?php
 ini_set("memory_limit","1024M");
+$ini_file = "brightcove.ini";
 
 if ( $argc == 2)
 {
@@ -10,9 +11,17 @@ else
 	die ( 'usage: php ' . $_SERVER ['SCRIPT_NAME'] . " [Brightcove API token]" . PHP_EOL ); 
 }
 
+$extras = parse_ini_file($ini_file);
+
 $page_num = 0;
 $run = TRUE;
-echo "*title,description,tags,url,category,contentType,thumbnailUrl\n";
+$header = "*title,description,tags,url,category,contentType";
+if(!empty($extras)) {
+	foreach($extras as $key=>$value) {
+		$header .= ",".$key;
+	}
+}
+echo $header, PHP_EOL;
 while($run) {
 	$url = "http://api.brightcove.com/services/library?command=find_all_videos&media_delivery=http&token=".$token."&page_number=".$page_num++;
 	$json = file_get_contents($url);
@@ -30,7 +39,18 @@ while($run) {
 			}
 			echo $tags,",";
 			echo addslashes($item["videoFullLength"]["url"]);
-			echo ",,Video,",addslashes($item["videoStillURL"]),PHP_EOL;
+			echo ",,Video,",addslashes($item["videoStillURL"]);
+			if(!empty($extras)) {
+				foreach($extras as $key=>$value) {
+					if(strpos($value, ',') === FALSE) {
+						echo ",".addslashes($item[$value]);
+					} else {
+						$indices = explode(',', $value, 2);
+						echo ",".addslashes($item[$indices[0]][$indices[1]]);
+					}
+				}
+			}
+			echo PHP_EOL;
 		}
 	}
 	if(count($data["items"]) < 100) {
