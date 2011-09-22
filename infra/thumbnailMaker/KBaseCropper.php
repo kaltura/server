9282f99febb0a456058e2f8efbef8e7f98a27c9a
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-22 12:13:42 +0000

    qnd: KBaseCropper.php - added support for cropping/resizing animated GIFs - takes the file of the first frame
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@70936 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/thumbnailMaker/KBaseCropper.php
+++ infra/thumbnailMaker/KBaseCropper.php
@@ -65,6 +65,20 @@ abstract class KBaseCropper
 			
 			if($returnValue)
 				return false;
+
+			// Support animated gifs - KImageMagick generates multiple images with a postfix of '-<frame num>'
+			if (!@filesize($this->targetPath))
+			{
+				$targetFiledir = pathinfo($this->targetPath, PATHINFO_DIRNAME);
+				$targetFilename = pathinfo($this->targetPath, PATHINFO_FILENAME);
+				$targetFileext = pathinfo($this->targetPath, PATHINFO_EXTENSION);
+				
+				$firstFrameTargetFile = "$targetFiledir/$targetFilename-0.$targetFileext";
+				if (@filesize($firstFrameTargetFile))
+				{
+					kFile::moveFile($firstFrameTargetFile, $this->targetPath);
+				}
+			}			
 				
 			return true;
 		}
