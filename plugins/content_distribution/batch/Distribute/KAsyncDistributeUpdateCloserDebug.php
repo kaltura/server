5423f36c903784bd07122e7d3f279d5a96046af7
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-09-30 14:02:12 +0000

    qnd: Fixed batch config file path
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@71290 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/batch/Distribute/KAsyncDistributeUpdateCloserDebug.php
+++ plugins/content_distribution/batch/Distribute/KAsyncDistributeUpdateCloserDebug.php
@@ -9,7 +9,7 @@ chdir(dirname( __FILE__ ) . "/../../../../batch");
 
 require_once("bootstrap.php");
 
-$iniFile = "batch_config.ini";		// should be the full file path
+$iniFile = "../configurations/batch.ini";		// should be the full file path
 
 $kdebuger = new KGenericDebuger($iniFile);
 $kdebuger->run('KAsyncDistributeUpdateCloser');
