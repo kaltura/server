1aca74806fc04de4344be6283fd3c836ca3ff103
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-02 10:14:46 +0000

    qnd: Updated the overview of the testmeDoc with Debbie's comments
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@71310 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/web/testmeDoc/static_doc/overview.php
+++ api_v3/web/testmeDoc/static_doc/overview.php
@@ -1,9 +1,9 @@
 <h2>Overview</h2>
 <p>
-Kaltura API documentation provides information to web developers who wish to implement a direct usage of Kaltura's Application Programming Interfaces (APIs). For gaining a wider understanding of Kaltura open source video platform, please review the Introduction to Kaltura Platform document.
+Kaltura API documentation provides information to web developers who wish to use Kaltura's Application Programming Interfaces (APIs). To gain a wider understanding of the Kaltura open source video platform, please review the Introduction to Kaltura Platform document.
 </p>
 <p>
-Kaltura open source video platform exposes its Server API to be available, via a web services, layer, to web applications by implementing a standard HTTP POST/GET URL encoded requests structure. Kaltura server API's, also named Kaltura Partner Services, are designed to follow <a href="http://en.wikipedia.org/wiki/Representational_State_Transfer" target="_blank">REST</a> principles. It consists of several service actions for querying/setting/updating/listing entities and for activating processes within Kaltura open source video Platform. Service actions are grouped according to the entity type they apply on, and provide all actions relevant to the specific entity.
+The Kaltura open source video platform exposes its Server API to web applications by implementing a standard HTTP POST/GET url-encoded requests structure. Kaltura server API's, also named Kaltura Partner Services, are designed to follow <a href="http://en.wikipedia.org/wiki/Representational_State_Transfer" target="_blank">REST</a> principles. The server APIs consist of service actions for querying, setting, updating and listing entities as well as for activating processes within Kaltura open source video platform. Service actions are grouped according to the entity type they are applied on, and provide all actions relevant to the specific entity.
 </p>
 <p> 
 	This API documentation provides specific information on:
@@ -11,15 +11,15 @@ Kaltura open source video platform exposes its Server API to be available, via a
 <ul>
 	<li>Kaltura Services and their related actions</li>
 	<li>Kaltura Objects and their related properties</li> 
-	<li>Kaltura Enumerated Types and their values.</li>
+	<li>Kaltura Enumerated Types and their values</li>
 </ul>
 
 <p>
 A prerequisite for using Kaltura APIs is obtaining Kaltura partner identifiers, such as Partner ID. Learn more about <a href="http://www.kaltura.org/how-obtain-partner-identifiers#comment-form" target="_blank">obtaining Kaltura partner identifiers</a>.
 </p>
 <p>
-In addition to this documentation, Kaltura provides an <a href="../testme/index.php" target="_blank">API test console application</a>, allowing developers to experience an actual usage of Kaltura API.
+In addition to this documentation, Kaltura provides an <a href="../testme/index.php" target="_blank">API Test Console Application</a>, allowing you to develop applications using the Kaltura API.
 </p>  
 <p>
-For additional support, please visit <a href="http://www.kaltura.org/forums" target="_blank">Kaltura Community Forums</a>
+For additional support, please visit <a href="http://www.kaltura.org/forums" target="_blank">Kaltura Community Forums</a>.
 </p>
