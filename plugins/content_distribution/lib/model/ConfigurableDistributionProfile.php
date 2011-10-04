43fa3fac71feb6b31247fb060ad9fcc741ea0005
nogab <nogab@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-04 22:11:02 +0000

    PS: add support for xsl templates.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@71528 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/lib/model/ConfigurableDistributionProfile.php
+++ plugins/content_distribution/lib/model/ConfigurableDistributionProfile.php
@@ -211,13 +211,14 @@ abstract class ConfigurableDistributionProfile extends DistributionProfile
                 $xsl .= '</value>';
             }
         }
-        
-        $xsl .= '
+             
+      $xsl .= '
         		</distribution_values>
         	</xsl:template>
+        	'.implode(PHP_EOL.PHP_EOL, $this->getXslTemplates()).' 
         </xsl:stylesheet>
         ';
-        
+      
         KalturaLog::debug('Result XSL: '. $xsl);
         return $xsl;
     }
@@ -499,6 +500,35 @@ abstract class ConfigurableDistributionProfile extends DistributionProfile
 			$this->requiredFields[$fieldName] = $fieldName;
 		}
 	}
+	
+	/**
+	 * @return array<string> an array containing xsl templates to add to the default xsl
+	 */
+	protected function getXslTemplates()
+	{
+		$templates = array();
+		$stringReplaceAllTemplate = '<xsl:template name="string-replace-all">
+				    <xsl:param name="text" />
+				    <xsl:param name="replace" />
+				    <xsl:param name="by" />
+				    <xsl:choose>
+				      <xsl:when test="contains($text, $replace)">
+				        <xsl:value-of select="substring-before($text,$replace)" />
+				        <xsl:value-of select="$by" />
+				        <xsl:call-template name="string-replace-all">
+				          <xsl:with-param name="text" select="substring-after($text,$replace)" />
+				          <xsl:with-param name="replace" select="$replace" />
+				          <xsl:with-param name="by" select="$by" />
+				        </xsl:call-template>
+				      </xsl:when>
+				      <xsl:otherwise>
+				        <xsl:value-of select="$text" />
+				      </xsl:otherwise>
+				    </xsl:choose>
+			 	</xsl:template>';
 		
+		$templates[] = $stringReplaceAllTemplate;
+		return $templates;
+	}
 	
 }
