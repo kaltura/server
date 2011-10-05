926c06518fa655eb4e93fa51d2040effe36953ed
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-05 09:04:38 +0000

    fwr: new event for saving object, new event consumer for said event.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@71531 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/apps/kaltura/lib/events/events/kObjectSavedEvent.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Auto-raised event that raised by the propel generated objects
+ * This event is fired regardless of whether changes were made to the object being saved - it is fired when the object's postSave function is called.
+ * @package Core
+ * @subpackage events
+ */
+class kObjectSavedEvent extends KalturaEvent implements IKalturaDatabaseEvent
+{
+	const EVENT_CONSUMER = 'kObjectSavedEventConsumer';
+	
+	/**
+	 * @var BaseObject
+	 */
+	private $object;
+	
+	/**
+	 * @param BaseObject $object
+	 */
+	public function __construct(BaseObject $object)
+	{
+		$this->object = $object;
+		
+		$additionalLog = '';
+		if(method_exists($object, 'getId'))
+			$additionalLog .= ' id [' . $object->getId() . ']';
+			
+		KalturaLog::debug("Event [" . get_class($this) . "] object type [" . get_class($object) . "]" . $additionalLog);
+	}
+	
+	public function getConsumerInterface()
+	{
+		return self::EVENT_CONSUMER;
+	}
+	
+	/**
+	 * @param kObjectSavedEventConsumer $consumer
+	 * @return bool true if should continue to the next consumer
+	 */
+	protected function doConsume(KalturaEventConsumer $consumer)
+	{
+		if(!$consumer->shouldConsumeSavedEvent($this->object))
+			return true;
+	
+		$additionalLog = '';
+		if(method_exists($this->object, 'getId'))
+			$additionalLog .= 'id [' . $this->object->getId() . ']';
+			
+		KalturaLog::debug(get_class($this) . ' event consumed by ' . get_class($consumer) . ' object type [' . get_class($this->object) . '] ' . $additionalLog);
+		return $consumer->objectSaved($this->object);
+	}
+
+}
