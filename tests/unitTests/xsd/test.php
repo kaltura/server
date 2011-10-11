4939ac015464eab4b2958c896a69736e9aefa594
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-11 09:14:42 +0000

    qnd: added xsd transform tests
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@71744 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ tests/unitTests/xsd/test.php
@@ -0,0 +1,51 @@
+<?php
+require_once(dirname(__FILE__) . '/../../bootstrap.php');
+
+class thumbnailTest extends PHPUnit_Framework_TestCase
+{
+	public function testXsd()
+	{
+		$path = realpath(__DIR__ . '/xsd');
+		$xsdDir = dir($path);
+		while (false !== ($entry = $xsdDir->read())) 
+		{
+			if($entry == '.' || $entry == '..')
+				continue;
+				
+			if(is_dir("$path/$entry"))
+				$this->doTest(realpath("$path/$entry"));
+		}
+		$xsdDir->close();
+	}
+	
+	public function doTest($path)
+	{
+		$xsdFiles = array();
+		$xsdDir = dir($path);
+		while (false !== ($entry = $xsdDir->read())) 
+		{
+			$matches = null;
+			if(!preg_match('/(\d+)\.xsd/', $entry, $matches))
+				continue;
+				
+			$version = $matches[1];
+			$xsdFiles[$version] = realpath("$path/$entry");
+		}
+		$xsdDir->close();
+		$this->assertGreaterThan(0, count($xsdFiles), "Folder [$path] has no XSD files");
+		
+		$versions = array_keys($xsdFiles);
+		sort($versions);
+		$prevVersion = array_shift($versions);
+		foreach($versions as $currentVersion)
+		{
+			$fromXsd = $xsdFiles[$prevVersion];
+			$toXsd = $xsdFiles[$currentVersion];
+			
+			$xsl = kXsd::compareXsd($fromXsd, $toXsd);
+			$this->assertTrue($xsl, "XSD [$fromXsd => $toXsd] Created XSL");
+			
+			$prevVersion = $currentVersion;
+		}
+	}
+}
