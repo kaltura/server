4f51b0ccad85b9d247214f80b0833736c67dfa1e
assafb <assafb@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-11 15:20:36 +0000

    qnd: mantis 12840.set default criteria (for not retrieving deleted permissions)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@71762 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/PermissionPeer.php
+++ alpha/lib/model/PermissionPeer.php
@@ -70,6 +70,16 @@ class PermissionPeer extends BasePermissionPeer
 		}
 	}
 	
+	public static function setDefaultCriteriaFilter ()
+	{
+		if(is_null(self::$s_criteria_filter))
+			self::$s_criteria_filter = new criteriaFilter();
+		
+		$c = new Criteria(); 
+		$c->add(self::STATUS, PermissionStatus::DELETED,criteria::NOT_EQUAL);
+		self::$s_criteria_filter->setFilter($c);
+	}
+	
 	
 	public static function removePermissionFromPartner($permissionName, $partnerId)
 	{
