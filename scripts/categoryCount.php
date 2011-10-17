bb0fab5e7ad5326c84faea995bbc78a1676d7eb3
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-17 09:22:59 +0000

    fwr: script for ticket n 7622
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@71907 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ scripts/categoryCount.php
@@ -0,0 +1,43 @@
+<?php
+ini_set("memory_limit","256M");
+
+require_once 'bootstrap.php';
+
+
+if(!count($argv))
+	die("No partner_id passed to script!");
+	
+$partnerId = $argv[1];
+
+var_dump($partnerId);
+
+if ( !PartnerPeer::retrieveByPK($partnerId) )
+	die("Please enter a valid partner Id!");
+
+$criteria = new Criteria();
+$criteria->add(categoryPeer::PARTNER_ID,$partnerId,Criteria::EQUAL);
+$allCats = categoryPeer::doSelect($criteria);
+
+
+foreach ($allCats as $cat)
+{
+	/* @var $cat category */
+	$allChildren = $cat->getAllChildren();
+	$allSubCatsIds = array();
+	$allSubCatsIds[] = $cat->getId();
+	
+	if (count($allChildren))
+	{
+		foreach ($allChildren as $child)
+			$allSubCatsIds[] = $child->getId();	
+	}
+
+	$c = KalturaCriteria::create(entryPeer::OM_CLASS);
+	$entryFilter = new entryFilter();
+	$entryFilter->set("_matchor_categories_ids", implode(',',$allSubCatsIds));
+	$entryFilter->attachToCriteria($c);
+	$entries = entryPeer::doSelect($c);
+
+	$cat->setEntriesCount(count($entries));
+	$cat->save();
+}
