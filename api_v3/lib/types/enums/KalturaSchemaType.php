1469530570df250f3f66870c374a7db08e059e1b
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-24 16:50:13 +0000

    qnd: added getDescription for the documentation pages
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@72081 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/enums/KalturaSchemaType.php
+++ api_v3/lib/types/enums/KalturaSchemaType.php
@@ -14,6 +14,15 @@ class KalturaSchemaType extends KalturaDynamicEnum implements SchemaType
 		return self::mergeDescriptions(self::getEnumClass(), $descriptions);
 	}
 
+	public static function getDescription($type)
+	{
+		$descriptions = self::getDescriptions();
+		if(isset($descriptions[$type]))
+			return $descriptions[$type];
+			
+		return null;
+	}
+
 	public static function getEnumClass()
 	{
 		return 'SchemaType';
