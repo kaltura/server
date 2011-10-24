0efa9dfcad665417622a9245fddd297c2b887258
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-24 16:53:35 +0000

    qnd: new extJs docs
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@72083 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ api_v3/web/docs/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Kaltura API</title>
+	<link rel="stylesheet" type="text/css" href="resources/css/ext-all-gray.css" />
+	
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<link rel="stylesheet" type="text/css" href="css/code.example.css" />
+	<link rel="stylesheet" type="text/css" href="css/jquery.tooltip.css" />
+	
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	
+	<script type="text/javascript" src="js/jquery-1.3.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery.bgiframe.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.dimensions.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.tooltip.js"></script>
+	<script type="text/javascript" src="js/jquery-ui-1.7.1.custom.min.js"></script>
+	<script type="text/javascript" src="js/jquery.json.min.js"></script>
+	<script type="text/javascript" src="js/code.example.js"></script>
+	<script type="text/javascript" src="js/testme.js"></script>
+	
+</head>
+<body>
+</body>
+</html>
