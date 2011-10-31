044f960e232a19802a32b4b71981d5803b1199b6
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-31 15:29:27 +0000

    qnd: query cache: 1. Simplify the way rules are defined - peer classes will only return a constant rule structure instead of containing the logic to calculate the invalidation keys 2. Use the query cache to decide whether to query the slave or master
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@72430 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/propel/php5/KalturaPeerBuilder.php
+++ infra/propel/php5/KalturaPeerBuilder.php
@@ -127,19 +127,21 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 		$script .= "
 		".$this->getPeerClassname()."::attachCriteriaFilter(\$criteria);
 
+		\$queryDB = kQueryCache::QUERY_DB_UNDEFINED;
 		\$cacheKey = null;
 		\$cachedResult = kQueryCache::getCachedQueryResults(
 			\$criteria, 
 			kQueryCache::QUERY_TYPE_COUNT,
 			'".$this->getPeerClassname()."', 
-			\$cacheKey);
+			\$cacheKey, 
+			\$queryDB);
 		if (\$cachedResult !== null)
 		{
 			return \$cachedResult;
 		}
 		
-		// set the connection to slave server
-		\$con = ".$this->getPeerClassname()."::alternativeCon (\$con);
+		// select the connection for the query
+		\$con = ".$this->getPeerClassname()."::alternativeCon (\$con, \$queryDB);
 		
 		// BasePeer returns a PDOStatement
 		\$stmt = BasePeer::doCount(\$criteria, \$con);
@@ -175,12 +177,10 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 	 * is compared to the time saved in the invalidation key.
 	 * A cached query will only be used if it's newer than the matching invalidation key.
 	 *  
-	 * @param      Criteria \$criteria The Criteria object used to build the SELECT statement.
-	 * @param      string \$queryType The type of the query: select / count.
-	 * @return     string The invalidation key that should be checked before returning a cached result for this criteria.
-	 *		 if null is returned, the query cache won't be used - the query will be performed on the DB.
+	 * @return     array The invalidation keys that should be checked before returning a cached result for this criteria.
+	 *		 if an empty array is returned, the query cache won't be used - the query will be performed on the DB.
 	 */
-	public static function getCacheInvalidationKeys(Criteria \$criteria, \$queryType)
+	public static function getCacheInvalidationKeys()
 	{
 		return array();
 	}
@@ -242,20 +242,22 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 	{		
 		\$criteria = ".$this->getPeerClassname()."::prepareCriteriaForSelect(\$criteria);
 		
+		\$queryDB = kQueryCache::QUERY_DB_UNDEFINED;
 		\$cacheKey = null;
 		\$cachedResult = kQueryCache::getCachedQueryResults(
 			\$criteria, 
 			kQueryCache::QUERY_TYPE_SELECT,
 			'".$this->getPeerClassname()."', 
-			\$cacheKey);
+			\$cacheKey, 
+			\$queryDB);
 		if (\$cachedResult !== null)
 		{
 			".$this->getPeerClassname()."::filterSelectResults(\$cachedResult);
 			".$this->getPeerClassname()."::updateInstancePool(\$cachedResult);
 			return \$cachedResult;
 		}
-
-		\$con = ".$this->getPeerClassname()."::alternativeCon(\$con);
+		
+		\$con = ".$this->getPeerClassname()."::alternativeCon(\$con, \$queryDB);
 		
 		\$queryResult = ".$this->getPeerClassname()."::populateObjects(".$this->basePeerClassname."::doSelect(\$criteria, \$con));
 		
@@ -340,8 +342,22 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 
 		$script .= "
 
-	public static function alternativeCon(\$con)
+	public static function alternativeCon(\$con, \$queryDB = kQueryCache::QUERY_DB_UNDEFINED)
 	{
+		if (\$con === null)
+		{
+			switch (\$queryDB)
+			{
+			case QUERY_DB_MASTER:
+				\$con = myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_MASTER);
+				break;
+
+			case QUERY_DB_SLAVE:
+				\$con = myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_PROPEL2);
+				break;
+			}
+		}
+	
 		if(\$con === null)
 			\$con = myDbHelper::alternativeCon(\$con);
 			
@@ -516,7 +532,7 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 		// attach default criteria
 		".$this->getPeerClassname()."::attachCriteriaFilter(\$criteria);
 		
-		// set the connection to slave server
+		// select the connection for the query
 		\$con = ".$this->getPeerClassname()."::alternativeCon ( \$con );
 		
 		// BasePeer returns a PDOStatement
