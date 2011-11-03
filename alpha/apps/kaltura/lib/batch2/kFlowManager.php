d8ba97c9ea7f9f7fbef8f2fcdb856ca4cdca6704
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-11-03 06:54:21 +0000

    fwr: Alldigital intergration minor
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@72532 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/kFlowManager.php
+++ alpha/apps/kaltura/lib/batch2/kFlowManager.php
@@ -386,7 +386,7 @@ class kFlowManager implements kBatchJobStatusEventConsumer, kObjectAddedEventCon
 							// For the other types - use the ordinary kFileSyncUtils::getLocalFilePathForKey.
 						$fsArr=kFileSyncUtils::getReadyFileSyncForKey($syncKey,true,false);
 						$fs=$fsArr[0];
-						if($fs->getFileType()==3) {
+						if($fs->getFileType()==FileSync::FILE_SYNC_FILE_TYPE_URL) {
 							$path = $fs->getFilePath();
 						}
 						else{
