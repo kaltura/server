dba0fa42ce8316483497bc1a54e139b6979343c8
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-11-13 16:22:31 +0000

    qnd: propel-gen - search for build.properties in 2 levels to support for example plugins\cue_points\base\config
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@72811 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/propel/propel-gen.php
+++ infra/propel/propel-gen.php
@@ -1,4 +1,37 @@
 <?php
+
+function searchFolder($pluginsFolder, $level = 1)
+{
+	foreach(scandir($pluginsFolder) as $pluginDir)
+	{
+		if ($pluginDir[0] == ".")
+		{
+			continue;
+		}
+		$path = "$pluginsFolder/$pluginDir";
+		if (!is_dir($path))
+		{
+			continue;
+		}
+		
+		if ($level < 2)
+		{
+			searchFolder($path, $level + 1);
+		}
+		
+		$pluginConfig = "$path/config";
+		$buildProps = "$pluginConfig/build.properties";
+		if (!file_exists($buildProps))
+		{
+			continue;
+		}
+
+		chdir($pluginConfig);	
+		print $pluginConfig;
+		passthru("propel-gen $pluginConfig");
+	}
+}
+
 $origWD = getcwd();
 
 $rootFolder = realpath(dirname(__FILE__)."/../..");
@@ -10,27 +43,6 @@ passthru("propel-gen $alphaConfigFolder");
 
 // Plugin
 $pluginsFolder = "$rootFolder/plugins"; 
-foreach(scandir($pluginsFolder) as $pluginDir)
-{
-	if ($pluginDir[0] == ".")
-	{
-		continue;
-	}
-	$path = "$pluginsFolder/$pluginDir";
-	if (!is_dir($path))
-	{
-		continue;
-	}
-	
-	$pluginConfig = "$path/config";
-	$buildProps = "$pluginConfig/build.properties";
-	if (!file_exists($buildProps))
-	{
-		continue;
-	}
-
-	chdir($pluginConfig);	
-	passthru("propel-gen $pluginConfig");
-}
+searchFolder($pluginsFolder);
 
 chdir($origWD);	
