3b67557ef31132b96a9ff7d5134b4e25e3d7bece
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-11-16 15:34:26 +0000

    qnd: fix logic foractionslimit - when ks is created on the client side
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@72936 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/invalidSessionPeer.php
+++ alpha/lib/model/invalidSessionPeer.php
@@ -16,48 +16,17 @@
 class invalidSessionPeer extends BaseinvalidSessionPeer {
 
 	/**
-	 * @param      string $ks
+	 * @param      string $hash
 	 * @param      PropelPDO $con the connection to use
 	 * @return     bool
 	 */
-	public static function isInvalid($ks, PropelPDO $con = null)
+	public static function isInvalid($hash, PropelPDO $con = null)
 	{
-		$ksDecoded = base64_decode($ks);
-		list ( $hash , $real_str) = explode ( "|" , $ksDecoded , 2 );
-			
 		$criteria = new Criteria();
 		$criteria->add(invalidSessionPeer::KS, $hash);
 		$criteria->addAnd(invalidSessionPeer::KS_VALID_UNTIL, null, Criteria::NOT_EQUAL);
 		$cnt = invalidSessionPeer::doCount($criteria, false, $con);
 		return ($cnt > 0);
-	}
-	
-	/**
-	 * @param      string $ks
-	 * @param      PropelPDO $con the connection to use
-	 * @return     bool
-	 */
-	public static function isValidActionsLimit($ks, PropelPDO $con = null)
-	{
-		$ksDecoded = base64_decode($ks);
-		list ( $hash , $real_str) = explode ( "|" , $ksDecoded , 2 );
-		
-		$criteria = new Criteria();
-		$criteria->add(invalidSessionPeer::KS, $hash);
-		$cnt = invalidSessionPeer::doSelectOne($criteria, $con);
-		
-		if ($cnt){
-			$currentActionLimit = $cnt->getActionsLimit();
-			
-			if (!is_null($currentActionLimit) && $currentActionLimit <= 0)
-				return false;
-			
-			$cnt->setActionsLimit($currentActionLimit - 1);
-			$cnt->save();
-		}
-		
-		return true;
-		
 	}
 	
 	/**
@@ -74,8 +43,6 @@ class invalidSessionPeer extends BaseinvalidSessionPeer {
 		
 		return $invalidSession;
 	}
-
-	
 	
 	/**
 	 * @param      ks $ks
