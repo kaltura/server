10135327e33078e621a478f6f0fc8c394b7c0a41
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-11-17 10:33:39 +0000

    fwr: mantis ticket 12743 - changed wrong ini file path
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@72964 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/batches/Storage/KAsyncStorageDeleteDebug.php
+++ batch/batches/Storage/KAsyncStorageDeleteDebug.php
@@ -8,7 +8,7 @@ chdir(dirname( __FILE__ ) . "/../../");
 
 require_once("bootstrap.php");
 
-$iniFile = "C:\opt\kaltura\app\configurations\batch.ini"; //dirname ( __FILE__ ) . "/../configurations/batch.ini";		// should be the full file path
+$iniFile = dirname ( __FILE__ ) . "/../configurations/batch.ini";		// should be the full file path
  
 $kdebuger = new KGenericDebuger($iniFile);
 $kdebuger->run('KAsyncStorageDelete');
