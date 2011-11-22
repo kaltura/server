c35ccdb3f2cbaec60bcf8083df915fc1bf2c70e1
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-11-22 14:50:09 +0000

    fwr: sphinx - Use the same columns for ints & date + add the ability to set a different index per partner
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@73169 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ scripts/utils/setSearchIndexForPartner.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * add sub partner to existing partner group (as adding it to the master partner's group). 
+ */
+require_once '../bootstrap.php';
+
+
+if ($argc !== 3) {
+	die ( 'usage: php ' . $_SERVER ['SCRIPT_NAME'] . "[partner_id] [search_index]" . PHP_EOL );
+}
+$partnerId = $argv [1];
+$searchIndex = $argv [2];
+
+$partner = PartnerPeer::retrieveByPK ( $partnerId );
+if (! $partner)
+	die ( "no such sub partner [$partnerId]." . PHP_EOL );
+
+$searchIndexes = kConf::get('search_indexes');
+
+if(!isset($searchIndexes[$searchIndex]))
+	die ( "no such search index [$searchIndex]." . print_r($searchIndexes,true) . PHP_EOL );
+	
+$partner->setSearchIndex($searchIndex);
+$partner->save();
+
+echo "done." . PHP_EOL;
+
+
