917cd994fffcb0f9433c534b5ef881176c49a0e6
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-12-13 16:29:53 +0000

    qnd: mantis 13706 when same email but kuser is deleted - the select with limit 1 - select the deleted kuser and not the active
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@74298 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/kuserPeer.php
+++ alpha/lib/model/kuserPeer.php
@@ -50,6 +50,7 @@ class kuserPeer extends BasekuserPeer
 			$c = new Criteria();
 			$c->add(self::PARTNER_ID, $partner_id);
 			$c->add(self::PUSER_ID, $puser_id);
+			$c->add(self::status, KuserStatus::ACTIVE);
 			return self::doSelectOne($c);			
 		}
 		
