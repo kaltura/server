a791f989a9b653f10cc3d66b0830003dc7eb05ec
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-01-22 08:43:58 +0000

    qnd: SphinxUtils::escapeString define only the minimum set of required arrays
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@75441 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/lib/SphinxUtils.php
+++ plugins/sphinx_search/lib/SphinxUtils.php
@@ -9,12 +9,16 @@ class SphinxUtils
 		//		" and ! are escaped once to enable clients to use them, " = exact match, ! = AND NOT
 		//	This code could have been implemented more elegantly using array_map, but this implementation is the fastest
 		$from 		= array ('\\', 		'"', 		'!',		'(',		')',		'|',		'-',		'@',		'~',		'&',		'/',		'^',		'$',		'=',		'_',		'%', 		'\'',		);
-		$toSingle   = array ('\\\\', 	'\\"', 		'\\!',		'\\(',		'\\)',		'\\|',		'\\-',		'\\@',		'\\~',		'\\&',		'\\/',		'\\^',		'\\$',		'\\=',		'\\_',		'\\%',		'\\\'',		);
-		$toDouble   = array ('\\\\', 	'\\"', 		'\\!',		'\\\\\\(',	'\\\\\\)',	'\\\\\\|',	'\\\\\\-',	'\\\\\\@',	'\\\\\\~',	'\\\\\\&',	'\\\\\\/',	'\\\\\\^',	'\\\\\\$',	'\\\\\\=',	'\\\\\\_',	'\\\\\\%',	'\\\\\\\'',	);
 
 		if ($iterations == 2)
+		{
+			$toDouble   = array ('\\\\', 	'\\"', 		'\\!',		'\\\\\\(',	'\\\\\\)',	'\\\\\\|',	'\\\\\\-',	'\\\\\\@',	'\\\\\\~',	'\\\\\\&',	'\\\\\\/',	'\\\\\\^',	'\\\\\\$',	'\\\\\\=',	'\\\\\\_',	'\\\\\\%',	'\\\\\\\'',	);
 			return str_replace($from, $toDouble, $str);
+		}
 		else
+		{
+			$toSingle   = array ('\\\\', 	'\\"', 		'\\!',		'\\(',		'\\)',		'\\|',		'\\-',		'\\@',		'\\~',		'\\&',		'\\/',		'\\^',		'\\$',		'\\=',		'\\_',		'\\%',		'\\\'',		);
 			return str_replace($from, $toSingle, $str);
+		}
 	}
 }
