d68906d67561fb47ab6d036027b3a96a28083d35
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-01-24 10:22:00 +0000

    ps: ticket #13086 - fix for edgecast ec_seek funcionality
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@75516 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/edgecast/lib/kEdgeCastUrlManager.php
+++ plugins/edgecast/lib/kEdgeCastUrlManager.php
@@ -10,10 +10,15 @@ class kEdgeCastUrlManager extends kUrlManager
 		$url = parent::getFlavorAssetUrl($flavorAsset);
 		
 		// move version param to "behind" the flavor asset id
-		$flavorIdStr = '/flavorId/'.$flavorAsset->getId();
+		$flavorAssetId = $flavorAsset->getId();
+		$flavorIdStr = '/flavorId/'.$flavorAssetId;
 		$url = str_replace($flavorIdStr, '', $url);
 		$url = str_replace('serveFlavor', 'serveFlavor'.$flavorIdStr, $url);
-        
+		
+		if ($this->extention) {
+			$url .= "/name/$flavorAssetId.$this->extention";
+		}
+			
 		$syncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
 		$url = $this->addEcSeek($url, $syncKey);			
 		return $url;
