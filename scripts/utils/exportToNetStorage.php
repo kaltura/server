0740c467338326191d3a8927bc7e305408222e5c
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-01-30 14:39:40 +0000

    qnd: modify exportToNetStorage to work in chunks + allow to set maxmim number of entries to export
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@75670 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/exportToNetStorage.php
+++ scripts/utils/exportToNetStorage.php
@@ -3,11 +3,16 @@
 if($argc != 3)
 {
 	echo "Arguments missing.\n\n";
-	echo "Usage: php exportToNetStorage.php {partner id} {storage profile id}\n";
+	echo "Usage: php exportToNetStorage.php {partner id} {storage profile id} {max number of entries}(optional)\n";
 	exit;
 } 
 $partnerId = $argv[1];
 $storageProfileId = $argv[2];
+$maxEntriesToExport = $argv[3];
+
+if (empty($maxEntriesToExport) || $maxEntriesToExport <= 0) {
+    $maxEntriesToExport = -1;
+}
 
 set_time_limit(0);
 ini_set("memory_limit","1024M");
@@ -37,47 +42,73 @@ if(!$storageProfile)
 	exit;
 }
 
-$c = new Criteria();
-$c->add(entryPeer::PARTNER_ID, $partnerId);
-$entries = entryPeer::doSelect($c);
-foreach($entries as $entry)
+$moreEntries = true;
+$limitPerLoop = 500;
+$lastIntId = -1;
+$totalExported = 0;
+
+while ($moreEntries)
 {
-	$keys = array();
-	$keys[] = $entry->getSyncKey(entry::FILE_SYNC_ENTRY_SUB_TYPE_ISM);
-	$keys[] = $entry->getSyncKey(entry::FILE_SYNC_ENTRY_SUB_TYPE_ISMC);
-	
-	$flavors = assetPeer::retrieveReadyByEntryId($entry->getId());
-	foreach($flavors as $flavor)
-		$keys[] = $flavor->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
-	
-	foreach($keys as $index => $key)
-	{
-		if(!kFileSyncUtils::fileSync_exists($key))
-		{
-			unset($keys[$index]);			
-			continue;
-		}
-	
-		if(kFileSyncUtils::getReadyExternalFileSyncForKey($key, $storageProfileId))
-			unset($keys[$index]);
-	}
-	
-	if(!count($keys))
-	{
-		echo $entry->getId() . " - has no keys to export\n";
-		continue;
-	}
-	
-	foreach($keys as $key)
-	{
-		$fileSync = kFileSyncUtils::createPendingExternalSyncFileForKey($key, $storageProfile);
-		$srcFileSyncLocalPath = kFileSyncUtils::getLocalFilePathForKey($key, true);
-		kJobsManager::addStorageExportJob(null, $entry->getId(), $partnerId, $storageProfile, $fileSync, $srcFileSyncLocalPath);
-	}
-		
-	echo $entry->getId() . " - " . count($keys) . " keys exported\n\n";
-	
-	usleep(100);
+    $currentExported = 0;
+    $c = new Criteria();
+    $c->add(entryPeer::PARTNER_ID, $partnerId);
+    $c->add(entryPeer::INT_ID, $lastIntId, Criteria::GREATER_THAN);
+    $c->addAscendingOrderByColumn(entryPeer::INT_ID);
+    $c->setLimit($limitPerLoop);
+    $entries = entryPeer::doSelect($c);
+    foreach($entries as $entry)
+    {
+        $lastIntId = $entry->getIntId();
+    	$keys = array();
+    	$keys[] = $entry->getSyncKey(entry::FILE_SYNC_ENTRY_SUB_TYPE_ISM);
+    	$keys[] = $entry->getSyncKey(entry::FILE_SYNC_ENTRY_SUB_TYPE_ISMC);
+    	
+    	$flavors = assetPeer::retrieveReadyByEntryId($entry->getId());
+    	foreach($flavors as $flavor)
+    		$keys[] = $flavor->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
+    	
+    	foreach($keys as $index => $key)
+    	{
+    		if(!kFileSyncUtils::fileSync_exists($key))
+    		{
+    			unset($keys[$index]);			
+    			continue;
+    		}
+    	
+    		if(kFileSyncUtils::getReadyExternalFileSyncForKey($key, $storageProfileId))
+    			unset($keys[$index]);
+    	}
+    	
+    	if(!count($keys))
+    	{
+    		echo $entry->getId() . " - has no keys to export\n";
+    		continue;
+    	}
+    	
+    	foreach($keys as $key)
+    	{
+    		$fileSync = kFileSyncUtils::createPendingExternalSyncFileForKey($key, $storageProfile);
+    		$srcFileSyncLocalPath = kFileSyncUtils::getLocalFilePathForKey($key, true);
+    		kJobsManager::addStorageExportJob(null, $entry->getId(), $partnerId, $storageProfile, $fileSync, $srcFileSyncLocalPath);
+    	}
+    		
+    	echo $entry->getId() . " - " . count($keys) . " keys exported\n\n";
+    	
+    	$totalExported++;
+    	if ($maxEntriesToExport > 0 && $totalExported >= $maxEntriesToExport) {
+    	    echo 'Max entries limit of ['.$maxEntriesToExport.'] reached - stopping executin';
+    	    $moreEntries = false;
+    	    break;
+    	}
+    	
+    	usleep(100);
+    }
+    
+    if (count($entries) < $limitPerLoop) {
+        $moreEntries = false;
+    }
+    $entries = null;
+    entryPeer::clearInstancePool();
 }
 
 echo "Done\n";
