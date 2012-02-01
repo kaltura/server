63698c44a5cba92b5e73be4e82a33da53a428a94
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-02-01 14:05:32 +0000

    fwr: mantis #14070 SQLs didn't support NY timezone: The time shift (zero in NY because the server is in NY) compared to be smaller than zero, should be smaller or equal in order to compare no timezone shifting.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@75759 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/content_dropoff/content_dropoff_count.sql
+++ alpha/apps/kaltura/lib/reports/content_dropoff/content_dropoff_count.sql
@@ -7,9 +7,9 @@ WHERE
 	AND {CATEGORIES_MATCH}
 	AND ev.partner_id = {PARTNER_ID} /* PARTNER_ID*/
     AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
-    			AND     IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
+    			AND     IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
 			AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - 7, ABS(7)), 0)
-			AND hour_id < IF (date_id = IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - 7, ABS(7)), 24)
+			AND hour_id < IF (date_id = IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - 7, ABS(7)), 24)
 	AND 
 		( count_plays > 0 OR
 		  count_plays_25 > 0 OR
--- alpha/apps/kaltura/lib/reports/map_overlay/map_overlay_map.sql
+++ alpha/apps/kaltura/lib/reports/map_overlay/map_overlay_map.sql
@@ -11,9 +11,9 @@ FROM kalturadw.dwh_hourly_events_country ev, dwh_dim_locations c
 	AND c.location_type_name = 'COUNTRY' 
 	AND ev.partner_id = {PARTNER_ID} /* PARTNER_ID*/
 	AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
-    			AND     IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
+    			AND     IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
 			AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 0)
-			AND hour_id < IF (date_id = IF({TIME_SHIFT}<0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 24)
+			AND hour_id < IF (date_id = IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 24)
 GROUP BY ev.country_id,c.country
 ORDER BY count_plays DESC
 LIMIT 0,365  /* pagination  */ 
