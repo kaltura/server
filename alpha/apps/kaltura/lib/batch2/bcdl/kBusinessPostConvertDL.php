62e2f8eaeb0838c3cdfe3afe887b8436b14ddc0e
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-02-01 10:12:58 +0000

    qnd: debug message can't use $profile if it's null, and root job tpye can't be remote convert since it's never used.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@75744 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/bcdl/kBusinessPostConvertDL.php
+++ alpha/apps/kaltura/lib/batch2/bcdl/kBusinessPostConvertDL.php
@@ -120,7 +120,6 @@ class kBusinessPostConvertDL
 		{
 			KalturaLog::err($e->getMessage());
 		}
-		KalturaLog::debug("profile [" . $profile->getId() . "]");
 				
 		$currentReadyBehavior = self::getReadyBehavior($currentFlavorAsset, $profile);
 		KalturaLog::debug("Current ready behavior [$currentReadyBehavior]");
@@ -128,16 +127,8 @@ class kBusinessPostConvertDL
 		$rootBatchJob = null;
 		if($dbBatchJob)
 			$rootBatchJob = $dbBatchJob->getRootJob();
-		if($rootBatchJob){
+		if($rootBatchJob)
 			KalturaLog::debug("root batch job id [" . $rootBatchJob->getId() . "] type [" . $rootBatchJob->getJobType() . "]");
-		// update the root job end exit
-			if($rootBatchJob->getJobType() == BatchJobType::REMOTE_CONVERT)
-			{
-				KalturaLog::debug("finish remote convert root job");
-				kJobsManager::updateBatchJob($rootBatchJob, BatchJob::BATCHJOB_STATUS_FINISHED);
-				return $dbBatchJob;
-			}
-		}
 		
 		// update the root job end exit
 		if($rootBatchJob && $rootBatchJob->getJobType() == BatchJobType::BULKDOWNLOAD)
