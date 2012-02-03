f670b00895aab3a1b05709dafa5a3eccbf6d989a
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-02-03 14:51:37 +0000

    qnd: added FILE_SYNC_ALREADY_EXISTS
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@75823 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/exceptions/kFileSyncException.php
+++ alpha/apps/kaltura/lib/exceptions/kFileSyncException.php
@@ -4,4 +4,5 @@ class kFileSyncException extends kCoreException
 {
 	const FILE_DOES_NOT_EXIST_ON_CURRENT_DC = 1;
 	const FILE_SYNC_PARTNER_ID_NOT_DEFINED = 2;
+	const FILE_SYNC_ALREADY_EXISTS = 3;
 }
