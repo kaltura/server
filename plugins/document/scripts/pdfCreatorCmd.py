95ed5738dc020f6609dec954eb8ccf82bc12d04f
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-02-07 09:23:42 +0000

    qnd: added a new pdfCreator wrapper script that build the command line + waits until the printer queue becomes empty
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@75954 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/document/scripts/pdfCreatorCmd.py
@@ -0,0 +1,43 @@
+import win32print
+import os.path
+import time
+import sys
+
+if len(sys.argv) < 4:
+    print 'wrong usage of this script. usage: %s {inputFile} {outFile} [--readOnly]' % os.path.dirname(__file__);
+    sys.exit(1)
+
+# build the command line
+if sys.argv[-1] == '--readonly':
+    readOnly = True
+    inputFile = sys.argv[-3]
+    outputFile = sys.argv[-2]
+    commandParams = sys.argv[1:-3]
+else:
+    readOnly = False
+    inputFile = sys.argv[-2]
+    outputFile = sys.argv[-1]
+    commandParams = sys.argv[1:-2]
+
+commandParams.append('/NoStart')
+
+inputFileExt = os.path.splitext(inputFile)[1].lower()
+if readOnly and inputFileExt == '.pdf':
+    commandParams.append('/IF"%s"' % inputFile)
+    commandParams.append('/OF"%s.pdf"' % outputFile)
+else:
+    commandParams.append('/PF"%s"' % inputFile)
+
+command = ' '.join(commandParams)
+
+# execute the command
+print '\ncommand: %s' % command
+os.system(command)
+
+# wait until the printer queue becomes empty
+printer = win32print.OpenPrinter('PDFCreator')
+
+while True:
+    if len(win32print.EnumJobs(printer, 0, 1, 2)) == 0:
+        break
+    time.sleep(.5)
