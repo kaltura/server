d4ca80c90473a78eadce2cf9c63bb152e0026ebc
atars <atars@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-02-12 09:58:21 +0000

    def: 1680 new KMC user manual
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@76083 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/web/lib/js/kmc5.js
+++ alpha/web/lib/js/kmc5.js
@@ -4,7 +4,7 @@
 kmc.vars.debug = false;
 
 // Quickstart guide (should be moved to kmc4success.php)
-kmc.vars.quickstart_guide = "/content/docs/pdf/KMC3_Quick_Start_Guide.pdf";
+kmc.vars.quickstart_guide = "/content/docs/pdf/KMC_Eagle_User_Manual.pdf";
 kmc.vars.help_url = kmc.vars.service_url + '/kmc5help.html';
 
 // Log function
