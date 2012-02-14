750343f5defb59633d844806a42aed9db90f3102
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-02-14 09:50:31 +0000

    fwr: new actions for manually exporting entries&assets
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@76167 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/storage/kStorageExporter.php
+++ alpha/apps/kaltura/lib/storage/kStorageExporter.php
@@ -33,7 +33,7 @@ class kStorageExporter implements kObjectChangedEventConsumer, kBatchJobStatusEv
 			foreach($externalStorages as $externalStorage)
 			{
 				if($externalStorage->getTrigger() == StorageProfile::STORAGE_TEMP_TRIGGER_MODERATION_APPROVED)
-					$this->exportEntry($object, $externalStorage);
+					self::exportEntry($object, $externalStorage);
 			}
 		}
 		
@@ -55,7 +55,7 @@ class kStorageExporter implements kObjectChangedEventConsumer, kBatchJobStatusEv
 						)
 					)
 				{
-					$this->exportFlavorAsset($object, $externalStorage);
+					self::exportFlavorAsset($object, $externalStorage);
 				}
 			}
 		}
@@ -66,7 +66,7 @@ class kStorageExporter implements kObjectChangedEventConsumer, kBatchJobStatusEv
 	 * @param flavorAsset $flavor
 	 * @param StorageProfile $externalStorage
 	 */
-	protected function exportFlavorAsset(flavorAsset $flavor, StorageProfile $externalStorage)
+	public static function exportFlavorAsset(flavorAsset $flavor, StorageProfile $externalStorage)
 	{
 		$flavorParamsIds = $externalStorage->getFlavorParamsIds();
 		KalturaLog::log(__METHOD__ . " flavorParamsIds [$flavorParamsIds]");
@@ -182,7 +182,7 @@ class kStorageExporter implements kObjectChangedEventConsumer, kBatchJobStatusEv
 	 * @param entry $entry
 	 * @param StorageProfile $externalStorage
 	 */
-	protected function exportEntry(entry $entry, StorageProfile $externalStorage)
+	public static function exportEntry(entry $entry, StorageProfile $externalStorage)
 	{
 		$checkFileSyncsKeys = $this->getEntrySyncKeys($entry, $externalStorage);
 		foreach($checkFileSyncsKeys as $key)
@@ -231,7 +231,7 @@ class kStorageExporter implements kObjectChangedEventConsumer, kBatchJobStatusEv
 				{
 					$sourceFlavor = assetPeer::retrieveOriginalReadyByEntryId($dbBatchJob->getEntryId());
 					if($sourceFlavor)
-						$this->exportFlavorAsset($sourceFlavor, $externalStorage);
+						self::exportFlavorAsset($sourceFlavor, $externalStorage);
 				}
 			}
 		}
