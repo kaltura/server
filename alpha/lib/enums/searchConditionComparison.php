cf0157d1902182630b82958cf5e97abc99ebe3aa
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-12 12:07:30 +0000

    qnd: typo
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@76991 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/enums/searchConditionComparison.php
+++ alpha/lib/enums/searchConditionComparison.php
@@ -5,9 +5,15 @@
  */ 
 interface searchConditionComparison extends BaseEnum
 {
-	const EQUEL = 1;
+	const EQUAL = 1;
 	const GREATER_THAN = 2;
 	const GREATER_THAN_OR_EQUEL = 3;
 	const LESS_THAN = 4;
 	const LESS_THAN_OR_EQUEL = 5;
+	
+	
+	/**
+	 * @deprecated use EQUAL instead
+	 */
+	const EQUEL = 1;
 }
