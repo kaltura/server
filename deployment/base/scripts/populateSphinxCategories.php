29e6a199b7de85c4835787e61f67af6f6c762f90
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-14 09:21:54 +0000

    fwr: populate categories to sphinx
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77100 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/base/scripts/populateSphinxCategories.php
@@ -0,0 +1,63 @@
+<?php
+
+set_time_limit(0);
+
+ini_set("memory_limit","700M");
+
+chdir(dirname(__FILE__));
+
+define('ROOT_DIR', realpath(dirname(__FILE__) . '/../../../'));
+require_once(ROOT_DIR . '/infra/bootstrap_base.php');
+require_once(ROOT_DIR . '/infra/KAutoloader.php');
+require_once(ROOT_DIR . '/infra/kConf.php');
+
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
+KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "*"));
+KAutoloader::setClassMapFilePath(kConf::get("cache_root_path") . '/deploy/classMap.cache');
+KAutoloader::register();
+
+error_reporting(E_ALL);
+
+KalturaLog::setLogger(new KalturaStdoutLogger());
+
+$dbConf = kConf::getDB();
+DbManager::setConfig($dbConf);
+DbManager::initialize();
+
+$c = new Criteria();
+
+if($argc > 1 && is_numeric($argv[1]))
+	$c->add(categoryPeer::INT_ID, $argv[1], Criteria::GREATER_EQUAL);
+
+$c->addAscendingOrderByColumn(categoryPeer::INT_ID);
+$c->setLimit(10000);
+
+$con = myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_PROPEL2);
+//$sphinxCon = DbManager::getSphinxConnection();
+
+$categories = categoryPeer::doSelect($c, $con);
+$sphinx = new kSphinxSearchManager();
+while(count($categories))
+{
+	foreach($categories as $category)
+	{
+		KalturaLog::log('category id ' . $category->getId() . ' int id[' . $category->getIntId() . '] crc id[' . $sphinx->getSphinxId($category) . ']');
+		
+		try {
+			$ret = $sphinx->saveToSphinx($category, true);
+		}
+		catch(Exception $e){
+			KalturaLog::err($e->getMessage());
+			exit -1;
+		}
+	}
+	
+	$c->setOffset($c->getOffset() + count($categories));
+	categoryPeer::clearInstancePool();
+	MetadataPeer::clearInstancePool();
+	MetadataProfilePeer::clearInstancePool();
+	MetadataProfileFieldPeer::clearInstancePool();
+	$categories = categoryPeer::doSelect($c, $con);
+}
+
+KalturaLog::log('Done');
