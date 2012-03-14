efb0c83225a508416780460d809feb2e65910aa2
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-14 18:55:27 +0000

    fwr: new notifications plugin
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77140 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/event_notification/EventNotificationPlugin.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package plugins.eventNotification
+ */
+class EventNotificationPlugin extends KalturaPlugin implements IKalturaVersion, IKalturaPermissions, IKalturaEventConsumers, IKalturaServices, IKalturaMemoryCleaner, IKalturaAdminConsolePages, IKalturaConfigurator
+{
+	const PLUGIN_NAME = 'eventNotification';
+	const PLUGIN_VERSION_MAJOR = 1;
+	const PLUGIN_VERSION_MINOR = 0;
+	const PLUGIN_VERSION_BUILD = 0;
+	
+	const EVENT_NOTIFICATION_FLOW_MANAGER = 'kEventNotificationFlowManager';
+	
+	/* (non-PHPdoc)
+	 * @see IKalturaPlugin::getPluginName()
+	 */
+	public static function getPluginName()
+	{
+		return self::PLUGIN_NAME;
+	}
+	
+	/* (non-PHPdoc)
+	 * @see IKalturaVersion::getVersion()
+	 */
+	public static function getVersion()
+	{
+		return new KalturaVersion(
+			self::PLUGIN_VERSION_MAJOR,
+			self::PLUGIN_VERSION_MINOR,
+			self::PLUGIN_VERSION_BUILD
+		);		
+	}
+	
+	/* (non-PHPdoc)
+	 * @see IKalturaPermissions::isAllowedPartner()
+	 */
+	public static function isAllowedPartner($partnerId)
+	{
+		$partner = PartnerPeer::retrieveByPK($partnerId);
+		return $partner->getPluginEnabled(self::PLUGIN_NAME);		
+	}
+			
+	/* (non-PHPdoc)
+	 * @see IKalturaEventConsumers::getEventConsumers()
+	 */
+	public static function getEventConsumers()
+	{
+		return array(self::EVENT_NOTIFICATION_FLOW_MANAGER);
+	}
+
+	/* (non-PHPdoc)
+	 * @see IKalturaConfigurator::getConfig()
+	 */
+	public static function getConfig($configName) 
+	{
+		if($configName == 'generator')
+			return new Zend_Config_Ini(dirname(__FILE__) . '/config/generator.ini');
+	
+		if($configName == 'testme')
+			return new Zend_Config_Ini(dirname(__FILE__) . '/config/testme.ini');
+			
+		return null;
+	}
+
+	/* (non-PHPdoc)
+	 * @see IKalturaAdminConsolePages::getAdminConsolePages()
+	 */
+	public static function getAdminConsolePages() 
+	{
+		return array(
+			// TODO
+//			new EventNotificationListAction(),
+//			new EventNotificationConfigureAction(),
+//			new EventNotificationSetStatusAction(),
+		);
+	}
+
+	/* (non-PHPdoc)
+	 * @see IKalturaMemoryCleaner::cleanMemory()
+	 */
+	public static function cleanMemory() 
+	{
+		EventNotificationTemplatePeer::clearInstancePool();
+	}
+
+	/* (non-PHPdoc)
+	 * @see IKalturaServices::getServicesMap()
+	 */
+	public static function getServicesMap() 
+	{
+		return array(
+			'eventNotificationTemplate' => 'EventNotificationTemplateService',
+		);
+	}
+}
