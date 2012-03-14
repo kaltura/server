7e238e07101f6b940254e5c4921473c80f4162c1
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-14 17:55:24 +0000

    fwr: new event & event consumer for database deletion event
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77136 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/apps/kaltura/lib/events/consumers/kObjectErasedEventConsumer.php
@@ -0,0 +1,18 @@
+<?php
+interface kObjectErasedEventConsumer extends KalturaEventConsumer
+{
+    /**
+	 * @param BaseObject $object
+	 * @param string $previousVersion
+	 * @param BatchJob $raisedJob
+	 * @return bool true if should continue to the next consumer
+	 */
+	public function objectErased(BaseObject $object);
+	
+	/**
+	 * @param BaseObject $object
+	 * @param string $previousVersion
+	 * @return bool true if the consumer should handle the event
+	 */
+	public function shouldConsumeErasedEvent(BaseObject $object);
+}
---
+++ alpha/apps/kaltura/lib/events/events/kObjectErasedEvent.php
@@ -0,0 +1,43 @@
+<?php
+class kObjectErasedEvent extends KalturaEvent implements IKalturaDatabaseEvent
+{
+    const EVENT_CONSUMER = 'kObjectErasedEventConsumer';
+	/* (non-PHPdoc)
+     * @see KalturaEvent::doConsume()
+     */
+    protected function doConsume (KalturaEventConsumer $consumer)
+    {
+        if(!$consumer->shouldConsumeErasedEvent($this->object))
+			return true;
+			
+		$additionalLog = '';
+		if(method_exists($this->object, 'getId'))
+			$additionalLog .= 'id [' . $this->object->getId() . ']';
+			
+		KalturaLog::debug(get_class($this) . ' event consumed by ' . get_class($consumer) . ' object type [' . get_class($this->object) . '] ' . $additionalLog);
+		return $consumer->objectErased($this->object);
+        
+    }
+
+	/* (non-PHPdoc)
+     * @see KalturaEvent::getConsumerInterface()
+     */
+    public function getConsumerInterface ()
+    {
+        return self::EVENT_CONSUMER;
+        
+    }
+    
+    public function __construct(BaseObject $object)
+	{
+		$this->object = $object;
+		
+		$additionalLog = '';
+		if(method_exists($object, 'getId'))
+			$additionalLog .= ' id [' . $object->getId() . ']';
+			
+		KalturaLog::debug("Event [" . get_class($this) . "] object type [" . get_class($object) . "]" . $additionalLog);
+	}
+
+
+}
