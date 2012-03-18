9009c1b0b8672ff22cf9ac27b6ed677dee2371fa
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-18 12:53:50 +0000

    qnd: show all admin console permissions
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77215 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/forms/UserRoleConfiguration.php
+++ admin_console/forms/UserRoleConfiguration.php
@@ -34,41 +34,59 @@ class Form_UserRoleConfiguration extends Zend_Form
 		$element = $this->getElement('role');
 		
 		$client = Infra_ClientHelper::getClient();
-		$filter = new Kaltura_Client_Type_UserRoleFilter();
-		$filter->nameIn = 'System Administrator';
-		$filter->statusEqual = Kaltura_Client_Enum_UserRoleStatus::ACTIVE;
-		$userRoles = $client->userRole->listAction($filter);
-		if ($userRoles && isset($userRoles->objects) && count($userRoles->objects)) {
-			$adminUserRole = $userRoles->objects[0];
-			$permissions = explode(',', $adminUserRole->permissionNames);
-			sort($permissions);
-			foreach ($permissions as $permission)
+		$filter = new Kaltura_Client_Type_PermissionFilter();
+		$filter->statusEqual = Kaltura_Client_Enum_PermissionStatus::ACTIVE;
+		$filter->typeEqual = Kaltura_Client_Enum_PermissionType::NORMAL;
+		$filter->orderBy = Kaltura_Client_Enum_PermissionOrderBy::NAME_ASC;
+		
+		$pager = new Kaltura_Client_Type_FilterPager();
+		$pager->pageSize = 1000;
+		
+		$permissions = $client->permission->listAction($filter, $pager);
+		/* @var $permissions Kaltura_Client_Type_PermissionListResponse */
+		if ($permissions && isset($permissions->objects) && count($permissions->objects)) 
+		{
+			foreach($permissions->objects as $index => $permission)
 			{
-				if ($permission != '')
-				{
-					$permissionId = str_replace(".", "___", $permission);
-					$this->addElement('checkbox', $permissionId, array(
-						'label'=> $permission,
-						'decorators' => array('ViewHelper', array('Label', array('placement' => 'append')), array('HtmlTag',  array('class' => 'partner_configuration_checkbox_field_only'))
-					)));
-				}
+				/* @var $permission Kaltura_Client_Type_Permission */
+				
+				$permissionId = str_replace(".", "___", $permission->name);
+				$this->addElement('checkbox', $permissionId, array(
+					'label'=> $permission->friendlyName,
+					'decorators' => array('ViewHelper', array('Label', array('placement' => 'append')), array('HtmlTag',  array('class' => 'partner_configuration_checkbox_field_only'))
+				)));
 			}
+			
+//			$adminUserRole = $userRoles->objects[0];
+//			$permissions = explode(',', $adminUserRole->permissionNames);
+//			sort($permissions);
+//			foreach ($permissions as $permission)
+//			{
+//				if ($permission != '')
+//				{
+//					$permissionId = str_replace(".", "___", $permission);
+//					$this->addElement('checkbox', $permissionId, array(
+//						'label'=> $permission,
+//						'decorators' => array('ViewHelper', array('Label', array('placement' => 'append')), array('HtmlTag',  array('class' => 'partner_configuration_checkbox_field_only'))
+//					)));
+//				}
+//			}
 		}
 	}
 	
-	public function populateFromObject($user)
+	public function populateFromObject($userRole)
 	{
-		$this->getElement('name')->setValue($user->name);
-		$this->getElement('description')->setValue($user->description);
+		$this->getElement('name')->setValue($userRole->name);
+		$this->getElement('description')->setValue($userRole->description);
 		
-		$permissions = explode(',', $user->permissionNames);
+		$permissions = explode(',', $userRole->permissionNames);
 		
 		foreach ($permissions as $permission)
 		{
 			if ($permission != '')
 			{
-				$permission = str_replace(".", "___", $permission);
-				$this->setDefault($permission, true);
+				$permissionId = str_replace(".", "___", $permission);
+				$this->setDefault($permissionId, true);
 			}
 		}
 	}
@@ -85,8 +103,8 @@ class Form_UserRoleConfiguration extends Zend_Form
 			{
 				if ($element->isChecked())
 				{
-					$permission = str_replace("___", ".", $element->getId());
-					$permissionNames .=',' . $permission;
+					$permissionId = str_replace("___", ".", $element->getId());
+					$permissionNames .=',' . $permissionId;
 				}
 			}
 		}
