43d2ef5e91ad79639e25d40e31ffcd712aea0fba
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-19 08:39:06 +0000

    fwr: Faclon - entitlement: model + api objects + services -CR
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77253 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/enums/UpdateMethodType.php
+++ alpha/lib/enums/UpdateMethodType.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * last update method to be saved on the object and later to b able to distinguish beetween manual changes and nightly batch sync scripts. 
+ * Last update method to be saved on the object and later used to distinguish beetween manual 
+ * changes and async automatic scripts (for example, nightly sync scripts). 
  * @package Core
  * @subpackage model.enum
  */ 
