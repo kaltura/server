f6595c6a8d8430b6157b19a95d404357d6a46871
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-19 08:55:32 +0000

    fwr: Faclon - entitlement: model + api objects + services -CR
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77259 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/enums/InheritanceType.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Last update method to be saved on the object and later used to distinguish beetween manual 
+ * changes and async automatic scripts (for example, nightly sync scripts). 
+ * @package Core
+ * @subpackage model.enum
+ */ 
+interface InheritanceType extends BaseEnum
+{
+	const INHERIT = 0;
+	const MANUAL = 1;
+}
