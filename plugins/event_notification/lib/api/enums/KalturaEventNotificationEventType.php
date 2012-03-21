abfa62f6a27592c222e3094ce8bd1e91ae42fcf6
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-21 13:16:16 +0000

    qnd: added descriptions
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77397 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/event_notification/lib/api/enums/KalturaEventNotificationEventType.php
+++ plugins/event_notification/lib/api/enums/KalturaEventNotificationEventType.php
@@ -10,4 +10,21 @@ class KalturaEventNotificationEventType extends KalturaDynamicEnum implements Ev
 	{
 		return 'EventNotificationEventType';
 	}
+	
+	public static function getAdditionalDescriptions()
+	{
+		return array(
+			EventNotificationEventType::BATCH_JOB_STATUS => 'Status of batch job changed.',
+			EventNotificationEventType::OBJECT_ADDED => 'Object with all its related files and sub objects added, defined by the developer.',
+			EventNotificationEventType::OBJECT_CHANGED => 'Object model changed, automatically raised from the database.',
+			EventNotificationEventType::OBJECT_COPIED => 'Object copied into a new object, automatically raised from the database.',
+			EventNotificationEventType::OBJECT_CREATED => 'New object created, automatically raised from the database.',
+			EventNotificationEventType::OBJECT_DATA_CHANGED => 'Object content changed, defined by the developer.',
+			EventNotificationEventType::OBJECT_DELETED => 'Object with all its related files and sub objects deleted, defined by the developer.',
+			EventNotificationEventType::OBJECT_ERASED => 'Object erased from the database, automatically raised from the database.',
+			EventNotificationEventType::OBJECT_READY_FOR_REPLACMENT => 'Object is ready to be replaced by temporary object content, defined by the developer.',
+			EventNotificationEventType::OBJECT_SAVED => 'Object saved to the database, new, existing or deleted, automatically raised from the database.',
+			EventNotificationEventType::OBJECT_UPDATED => 'Object with all its related files and sub objects updated, defined by the developer.',
+		);
+	}
 }
