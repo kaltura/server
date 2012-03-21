09f26eb8249fc10771dcc92711a843a6c6cce8ab
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-21 09:13:48 +0000

    fwr: #14320 allowe passing segmentDuration as a paramter to the segementer, default is 10 sec (previously-hardcoded 2)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77380 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/segmenter/lib/KDLOperatorSegmenter.php
+++ plugins/segmenter/lib/KDLOperatorSegmenter.php
@@ -2,6 +2,8 @@
 /**
  * @package plugins.segmenter
  * @subpackage lib
+ * 
+ * Segmenter plugin accepts 'segmentDuration' settings in the 'extra' field json operator
  */
 class KDLOperatorSegmenter extends KDLOperatorBase {
     public function __construct($id, $name=null, $sourceBlacklist=null, $targetBlacklist=null) {
@@ -15,8 +17,30 @@ class KDLOperatorSegmenter extends KDLOperatorBase {
 	{
 	//segmenter 0_3eq4pxgw_0_j5b7ubqa_1.mpeg 2 zzz/segment zzz/playlist.m3u8 ./
 	// out_dummyk:/opt/kaltura/tmp/convert/convert_0_6olnx72l_4a32a//out_dummy-1.ts
+	
+		/*
+		 * The segment duration can be evaluated from the 'segmentDuration' field in the operator->extra
+		 * or set to the gop value if such values are available.
+		 * Otherwise set to 10.
+		 */
+	$segmentDur = 10;
+		$paramsMap = KDLUtils::parseParamStr2Map($extra);
+		if (isset($paramsMap) && array_key_exists('segmentDuration', $paramsMap)) {
+			$auxDur = $paramsMap['segmentDuration'];
+			if($auxDur>0)
+				$segmentDur=$auxDur;
+		}
+/*		else
+		if(isset($target->_video) 
+		&&(isset($target->_video->_gop) && $target->_video->_gop>0)
+		&&(isset($target->_video->_frameRate) && $target->_video->_frameRate>0)){
+			$auxDur = round($target->_video->_gop/$target->_video->_frameRate);
+			if($auxDur>0)
+				$segmentDur=$auxDur;
+		}
+*/
 		$cmdStr = " ".KDLCmdlinePlaceholders::InFileName;
-		$cmdStr .= " 2";
+		$cmdStr .= " $segmentDur";
 //		$cmdStr .= " ".KDLCmdlinePlaceholders::OutFileName."/segment"; // output MPEG-TS file prefix
 //		$cmdStr .= " ".KDLCmdlinePlaceholders::OutFileName."/playlist.m3u8"; // output m3u8 index file
 //		$cmdStr .= "zzzz"; // http prefix
