2ba72711e268b0407adf9119b522ae31380af1d8
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-27 18:55:10 +0000

    fwr: fix for critical AC bug
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77594 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/access_control/model/conditions/kAuthenticatedCondition.php
+++ alpha/apps/kaltura/lib/access_control/model/conditions/kAuthenticatedCondition.php
@@ -5,54 +5,61 @@
  */
 class kAuthenticatedCondition extends kCondition
 {
-	/* (non-PHPdoc)
-	 * @see kCondition::__construct()
-	 */
-	public function __construct($not = false)
-	{
-		$this->setType(ConditionType::AUTHENTICATED);
-		parent::__construct($not);
-	}
-	
-	/**
-	 * The privelege needed to remove the restriction
-	 * 
-	 * @var array
-	 */
-	protected $privileges = array(ks::PRIVILEGE_VIEW, ks::PRIVILEGE_VIEW_ENTRY_OF_PLAYLIST);
-	
-	/**
-	 * @param array $privileges
-	 */
-	public function setPrivileges(array $privileges)
-	{
-		$this->privileges = $privileges;
-	}
-	
-	/**
-	 * @return array
-	 */
-	function getPrivileges()
-	{
-		return $this->privileges;
-	}
-	
-	/* (non-PHPdoc)
-	 * @see kCondition::internalFulfilled()
-	 */
-	public function internalFulfilled(accessControl $accessControl)
-	{
-		$scope = $accessControl->getScope();
-		if (!$scope->getKs() || (!$scope->getKs() instanceof ks))
-			return false;
-		
-		if ($scope->getKs()->isAdmin())
-			return true;
-		
-		$privilegeVerified = true;
-		foreach($this->privileges as $privilege)
-			$privilegeVerified = $privilegeVerified && $scope->getKs()->verifyPrivileges($privilege, $scope->getEntryId());
+        /* (non-PHPdoc)
+         * @see kCondition::__construct()
+         */
+        public function __construct($not = false)
+        {
+                $this->setType(ConditionType::AUTHENTICATED);
+                parent::__construct($not);
+        }
 
-		return $privilegeVerified;
-	}
+        /**
+         * The privelege needed to remove the restriction
+         *
+         * @var array
+         */
+        protected $privileges = array(ks::PRIVILEGE_VIEW, ks::PRIVILEGE_VIEW_ENTRY_OF_PLAYLIST);
+
+        /**
+         * @param array $privileges
+         */
+        public function setPrivileges(array $privileges)
+        {
+                $this->privileges = $privileges;
+        }
+
+        /**
+         * @return array
+         */
+        function getPrivileges()
+        {
+                return $this->privileges;
+        }
+
+        /* (non-PHPdoc)
+         * @see kCondition::internalFulfilled()
+         */
+        public function internalFulfilled(accessControl $accessControl)
+        {
+                $scope = $accessControl->getScope();
+                if (!$scope->getKs() || (!$scope->getKs() instanceof ks))
+                        return false;
+
+                if ($scope->getKs()->isAdmin())
+                        return true;
+
+                foreach($this->privileges as $privilege)
+                {
+                        if(is_object($privilege))
+                                $privilege = $privilege->getValue();
+
+                        if($scope->getKs()->verifyPrivileges($privilege, $scope->getEntryId()))
+                        {
+                                return true;
+                        }
+                }
+
+                return false;
+        }
 }
