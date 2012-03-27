50aea98cfdfabca899165f62f50a4d9f00c0358d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-27 19:56:23 +0000

    qnd: added log messages
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77600 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/access_control/model/conditions/kAuthenticatedCondition.php
+++ alpha/apps/kaltura/lib/access_control/model/conditions/kAuthenticatedCondition.php
@@ -5,61 +5,65 @@
  */
 class kAuthenticatedCondition extends kCondition
 {
-        /* (non-PHPdoc)
-         * @see kCondition::__construct()
-         */
-        public function __construct($not = false)
-        {
-                $this->setType(ConditionType::AUTHENTICATED);
-                parent::__construct($not);
-        }
+	/* (non-PHPdoc)
+	 * @see kCondition::__construct()
+	 */
+	public function __construct($not = false)
+	{
+		$this->setType(ConditionType::AUTHENTICATED);
+		parent::__construct($not);
+	}
+	
+	/**
+	 * The privelege needed to remove the restriction
+	 * 
+	 * @var array
+	 */
+	protected $privileges = array(ks::PRIVILEGE_VIEW, ks::PRIVILEGE_VIEW_ENTRY_OF_PLAYLIST);
+	
+	/**
+	 * @param array $privileges
+	 */
+	public function setPrivileges(array $privileges)
+	{
+		$this->privileges = $privileges;
+	}
+	
+	/**
+	 * @return array
+	 */
+	function getPrivileges()
+	{
+		return $this->privileges;
+	}
+	
+	/* (non-PHPdoc)
+	 * @see kCondition::internalFulfilled()
+	 */
+	public function internalFulfilled(accessControl $accessControl)
+	{
+		$scope = $accessControl->getScope();
+		if (!$scope->getKs() || (!$scope->getKs() instanceof ks))
+			return false;
+		
+		if ($scope->getKs()->isAdmin())
+			return true;
+		
+		KalturaLog::debug(print_r($this->privileges, true));
+		foreach($this->privileges as $privilege)
+		{
+			if(is_object($privilege))
+				$privilege = $privilege->getValue();
+				
+			KalturaLog::debug("Checking privilege [$privilege] with entry [$scope->getEntryId()]");
+			if($scope->getKs()->verifyPrivileges($privilege, $scope->getEntryId()))
+			{
+				KalturaLog::debug("Privilege [$privilege] verified");
+				return true;
+			}
+		}
 
-        /**
-         * The privelege needed to remove the restriction
-         *
-         * @var array
-         */
-        protected $privileges = array(ks::PRIVILEGE_VIEW, ks::PRIVILEGE_VIEW_ENTRY_OF_PLAYLIST);
-
-        /**
-         * @param array $privileges
-         */
-        public function setPrivileges(array $privileges)
-        {
-                $this->privileges = $privileges;
-        }
-
-        /**
-         * @return array
-         */
-        function getPrivileges()
-        {
-                return $this->privileges;
-        }
-
-        /* (non-PHPdoc)
-         * @see kCondition::internalFulfilled()
-         */
-        public function internalFulfilled(accessControl $accessControl)
-        {
-                $scope = $accessControl->getScope();
-                if (!$scope->getKs() || (!$scope->getKs() instanceof ks))
-                        return false;
-
-                if ($scope->getKs()->isAdmin())
-                        return true;
-
-                foreach($this->privileges as $privilege)
-                {
-                        if(is_object($privilege))
-                                $privilege = $privilege->getValue();
-
-                        if($scope->getKs()->verifyPrivileges($privilege, $scope->getEntryId()))
-                        {
-                                return true;
-                        }
-                }
-
-                return false;
-        }
+		KalturaLog::debug("No privilege verified");
+		return false;
+	}
 }
