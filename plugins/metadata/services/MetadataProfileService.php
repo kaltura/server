cd17782434e58ae4fcc3fe9c2aec31154397b89e
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-03-29 07:37:14 +0000

    qnd: validated that xsd is a valid xml
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77662 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/services/MetadataProfileService.php
+++ plugins/metadata/services/MetadataProfileService.php
@@ -89,7 +89,7 @@ class MetadataProfileService extends KalturaBaseService
 		libxml_use_internal_errors(true);
 		libxml_clear_errors();
 		$xml = new DOMDocument();
-		if(!$xml->load($xsdFile))
+		if(!$xml->load($filePath))
 		{
 			$errorMessage = kXml::getLibXmlErrorDescription(file_get_contents($xsdFile));
 			throw new KalturaAPIException(MetadataErrors::INVALID_METADATA_PROFILE_SCHEMA, $errorMessage);
