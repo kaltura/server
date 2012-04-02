a805f4fc77d0d2d2a9c94bc4667237364c3ba40e
roman <roman@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-02 12:57:24 +0000

    qnd: removed configuration file parsing dependency with zf2 in php5.3 client library test file
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77771 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/sources/php53/tests/run.php
+++ generator/sources/php53/tests/run.php
@@ -1,5 +1,7 @@
 <?php
-/* set this path to the location of Zend/Loader/StandardAutoloader.php */
+/* set this path to the location of Zend/Loader/StandardAutoloader.php, 
+ * the client library can be used with any other php5.3 namespace style autoloaders (for example symfony2 & doctrine2)
+*/
 define('ZEND_AUTOLOADER_PATH', '/_path_/_to_/_zend_/_framework_/Zend/Loader/StandardAutoloader.php');
 define('CONFIG_FILE', 'config.ini');
 
@@ -17,14 +19,14 @@ $loader->registerNamespace('Kaltura', dirname(__FILE__).'/../library/Kaltura');
 $loader->registerNamespace('Test', dirname(__FILE__).'/Test');
 $loader->register();
 
-$testerConfig = new \Zend\Config\Ini(dirname(__FILE__).'/'.CONFIG_FILE);
+$testerConfig = parse_ini_file(dirname(__FILE__).'/'.CONFIG_FILE);
 
-if ($testerConfig->partnerId == 123)
+if ($testerConfig['partnerId'] == 123)
 	die('Please edit the configuration file (config.ini)');
 	
 // init kaltura configuration
-$config = new KalturaConfiguration($testerConfig->partnerId);
-$config->setServiceUrl($testerConfig->serviceUrl);
+$config = new KalturaConfiguration($testerConfig['partnerId']);
+$config->setServiceUrl($testerConfig['serviceUrl']);
 $config->setCurlTimeout(30);
 $config->setLogger(new \Test\SampleLoggerImplementation());
 
@@ -32,7 +34,7 @@ $config->setLogger(new \Test\SampleLoggerImplementation());
 $client = new KalturaClient($config);
 
 // generate session
-$ks = $client->generateSession('59cc61bfe5b0cfd70c8d4fd543423123', $testerConfig->userId, KalturaSessionType::ADMIN, $config->getPartnerId());
+$ks = $client->generateSession($testerConfig['adminSecret'], $testerConfig['userId'], KalturaSessionType::ADMIN, $config->getPartnerId());
 $config->getLogger()->log('Kaltura session (ks) was generated successfully: ' . $ks);
 $client->setKs($ks);
 
