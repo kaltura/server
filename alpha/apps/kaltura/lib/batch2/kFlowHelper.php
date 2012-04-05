b722db8f7ccbb99d73d899e8f9835d0b5032f4eb
omrib <omrib@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-05 09:53:53 +0000

    qnd: re-committed the fix to trunk
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@77897 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/kFlowHelper.php
+++ alpha/apps/kaltura/lib/batch2/kFlowHelper.php
@@ -1849,7 +1849,7 @@ class kFlowHelper
 		if(!$entry)
 		{
 			KalturaLog::err("Real entry id [" . $tempEntry->getReplacedEntryId() . "] not found");
-			myEntryUtils::deleteEntry($tempEntry);
+			myEntryUtils::deleteEntry($tempEntry,null,true);
 			return;
 		}
 
@@ -1871,7 +1871,7 @@ class kFlowHelper
 			case entryReplacementStatus::NONE:
 			default:
 				KalturaLog::err("Real entry id [" . $tempEntry->getReplacedEntryId() . "] replacement canceled");
-				myEntryUtils::deleteEntry($tempEntry);
+				myEntryUtils::deleteEntry($tempEntry,null,true);
 				break;
 		}
 	}
