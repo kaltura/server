b2fde3e81e87667e229a9a0d248d1db2de932419
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-09 07:21:17 +0000

    fwr: added object type and id, and executed server id
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78022 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2012-04-09_sphinx_executed_server.sql
+++ deployment/updates/sql/2012-04-09_sphinx_executed_server.sql
@@ -1,4 +1,4 @@
 ALTER TABLE sphinx_log 
 ADD executed_server_id	INT				NOT NULL AFTER id,
-ADD object_type			INT 			NOT NULL AFTER executed_server_id,
+ADD object_type			VARCHAR (255)	NOT NULL AFTER executed_server_id,
 ADD object_id			VARCHAR (20)	NOT NULL AFTER object_type;
