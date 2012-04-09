5f42802bfd87f899a7da9ffd8d1424f3838259ee
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-09 14:51:16 +0000

    qnd: KALTURA_API_V3 should be defined in api_v3/bootstrap.php and not in infra/bootstrap_base.php
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78057 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/bootstrap_base.php
+++ infra/bootstrap_base.php
@@ -6,8 +6,6 @@
 define("KALTURA_ROOT_PATH", realpath(dirname(__FILE__).DIRECTORY_SEPARATOR."../"));
 define("KALTURA_INFRA_PATH", KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."infra");
 
-define("KALTURA_API_V3", true); // used for different logic in alpha libs
-
 if (!defined("SF_ROOT_DIR"))    // when bootstraping api v3 under symfony, this will throw notices
 {
 	define("SF_ROOT_DIR",   KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."alpha");
