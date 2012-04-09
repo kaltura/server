9d9d81eb219d24a472807cf259dfff2fc7c87d70
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-09 05:45:54 +0000

    fwr: new item to store information about a serivce and its actions.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78000 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ api_v3/lib/KalturaServiceActionItem.php
@@ -0,0 +1,33 @@
+<?php
+class KalturaServiceActionItem
+{
+    /**
+     * @var string
+     */
+    public $serviceId;
+    
+    /**
+     * @var string
+     */
+    public $serviceClass;
+    
+    /**
+     * @var KalturaDocCommentParser
+     */
+    public $serviceInfo;
+    
+    /**
+     * @var array
+     */
+    public $actionMap;
+    
+    public static function cloneItem (KalturaServiceActionItem $item)
+    {
+        $serviceActionItem = new KalturaServiceActionItem();
+        $serviceActionItem->serviceId = $item->serviceId;
+        $serviceActionItem->serviceClass = $item->serviceClass;
+        $serviceActionItem->serviceInfo = $item->serviceInfo;
+        $serviceActionItem->actionMap = $item->actionMap;
+        return $serviceActionItem;
+    }
+}
