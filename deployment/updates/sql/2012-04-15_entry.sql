a7b0596b7852e4937bdc4d653313222b376a0726
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-15 14:19:03 +0000

    qnd: split category sql and entry alter tables
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78326 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2012-04-15_entry.sql
+++ deployment/updates/sql/2012-04-15_entry.sql
@@ -1,6 +1,3 @@
 ALTER TABLE entry
-ADD `creator_kuser_id` INTEGER,
-ADD `creator_puser_id` VARCHAR(64),
-ADD	`entitled_users_edit` TEXT,
-ADD	`entitled_users_publish` TEXT;
+ADD `creator_kuser_id` INTEGER;
 	
