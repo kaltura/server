0c12551d4acc0299177dcaf91429ab15bb09dde0
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-16 06:12:19 +0000

    qnd: phpdoc only
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78366 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/IP2Location.inc.php
+++ alpha/apps/kaltura/lib/IP2Location.inc.php
@@ -19,7 +19,8 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * 
- * @package IP2Location
+ * @package External
+ * @subpackage IP2Location
  */
 
 define("VERSION", "3.00");
