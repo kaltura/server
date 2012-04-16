459d040314cb8867ef0dc5efde77973362b0de48
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-16 12:53:23 +0000

    qnd: moved entry categories migration to be part of deployment updates
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78445 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/migrateEntryCategories.php
+++ deployment/updates/scripts/migrateEntryCategories.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__).'/../bootstrap.php');
+require_once(dirname(__FILE__).'/../../bootstrap.php');
 
 // command: php migrateEntryCategories.php [realRun|dryRun] [partner id] [start entry int id] [max entries]
 
