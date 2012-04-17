cfa8467e0fd2fe0773e629b47fb09b1e1490af28
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-17 10:37:04 +0000

    fwr: added 3 new columns to kvote table: partner, type and custom data
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78564 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/map/kvoteTableMap.php
+++ alpha/lib/model/map/kvoteTableMap.php
@@ -43,6 +43,7 @@ class kvoteTableMap extends TableMap {
 		$this->addForeignKey('KUSER_ID', 'KuserId', 'INTEGER', 'kshow', 'ID', false, null, null);
 		$this->addColumn('PARTNER_ID', 'PartnerId', 'INTEGER', false, null, null);
 		$this->addColumn('RANK', 'Rank', 'INTEGER', false, null, null);
+		$this->addColumn('STATUS', 'Status', 'INTEGER', false, null, null);
 		$this->addColumn('KVOTE_TYPE', 'KvoteType', 'INTEGER', false, null, 1);
 		$this->addColumn('CREATED_AT', 'CreatedAt', 'TIMESTAMP', false, null, null);
 		$this->addColumn('CUSTOM_DATA', 'CustomData', 'LONGVARCHAR', false, null, null);
