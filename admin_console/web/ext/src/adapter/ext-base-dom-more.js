cbcbec51af9fe2776a7db0cb8da4fbf6df363589
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-20 14:39:22 +0000

    qnd: removed unused ext js lib
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78777 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/web/ext/src/adapter/ext-base-dom-more.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Ext JS Library 3.1.0
- * Copyright(c) 2006-2009 Ext JS, LLC
- * licensing@extjs.com
- * http://www.extjs.com/license
- */
-Ext.lib.Dom.getRegion = function(el) {
-    return Ext.lib.Region.getRegion(el);
-};
