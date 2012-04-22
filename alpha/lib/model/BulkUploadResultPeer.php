1c6e21dc2102004e302571cf18701d0cf8010440
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-22 16:59:07 +0000

    fwr: $class_types_cache is now a static var
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78840 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/BulkUploadResultPeer.php
+++ alpha/lib/model/BulkUploadResultPeer.php
@@ -10,6 +10,13 @@
  */ 
 class BulkUploadResultPeer extends BaseBulkUploadResultPeer
 {
+    protected static $class_types_cache = array(
+        BulkUploadResultObjectType::ENTRY => 'BulkUploadResultEntry',
+        BulkUploadResultObjectType::CATEGORY => 'BulkUploadResultCategory',
+        BulkUploadResultObjectType::USER => 'BulkUploadResultKuser',
+        BulkUploadResultObjectType::CATEGORY_USER => 'BulkUploadResultCategoryKuser',
+    );
+    
 	public static function retrieveByBulkUploadId($bulkUploadId)
 	{
 		$criteria = new Criteria();
@@ -59,6 +66,23 @@ class BulkUploadResultPeer extends BaseBulkUploadResultPeer
 		
 		return self::doCount($criteria);
 	}
+	
+	/**
+	 * Function counts amount of bulk upload results for a given bulk upload job ID and object type
+	 * @param int $bulkUploadJobId
+	 * @param int $bulkUploadObjectType
+	 * @return int
+	 */
+	public static function countWithObjectTypeByBulkUploadId ($bulkUploadJobId, $bulkUploadObjectType)
+	{
+	    $criteria = new Criteria();
+		$criteria->add(BulkUploadResultPeer::BULK_UPLOAD_JOB_ID, $bulkUploadJobId);
+		$criteria->add(BulkUploadResultPeer::OBJECT_ID, null, Criteria::ISNOTNULL);
+		$criteria->add(BulkUploadResultPeer::OBJECT_TYPE, $bulkUploadObjectType);
+		
+		return self::doCount($criteria);
+	}
+	
 	/**
 	 * function to retrieve the BulkUploadResults with type ENTRY,and objectId<>null
 	 * @param int $bulkUploadId
@@ -73,5 +97,35 @@ class BulkUploadResultPeer extends BaseBulkUploadResultPeer
 		$criteria->addAscendingOrderByColumn(BulkUploadResultPeer::LINE_INDEX);
 		
 		return self::doSelect($criteria);
+	}
+	
+/**
+	 * The returned Class will contain objects of the default type or
+	 * objects that inherit from the default.
+	 *
+	 * @param      array $row PropelPDO result row.
+	 * @param      int $colnum Column to examine for OM class information (first is 0).
+	 * @throws     PropelException Any exceptions caught during processing will be
+	 *		 rethrown wrapped into a PropelException.
+	 */
+	public static function getOMClass($row, $colnum)
+	{
+		if($row)
+		{
+			$typeField = self::translateFieldName(BulkUploadResultPeer::OBJECT_TYPE, BasePeer::TYPE_COLNAME, BasePeer::TYPE_NUM);
+			$bulkUploadReultObjectType = $row[$typeField];
+			if(isset(self::$class_types_cache[$bulkUploadReultObjectType]))
+				return self::$class_types_cache[$bulkUploadReultObjectType];
+				
+			$extendedCls = KalturaPluginManager::getObjectClass(parent::OM_CLASS, $bulkUploadReultObjectType);
+			if($extendedCls)
+			{
+				self::$class_types_cache[$bulkUploadReultObjectType] = $extendedCls;
+				return $extendedCls;
+			}
+			self::$class_types_cache[$bulkUploadReultObjectType] = parent::OM_CLASS;
+		}
+			
+		return parent::OM_CLASS;
 	}
 }
