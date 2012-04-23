a6c184cf1cd03796bd095ec4d03a7679f8e4c463
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-23 10:30:39 +0000

    fwr: new object types for classes category, kuser, categoryKuser
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@78906 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/metadata/lib/model/Metadata.php
+++ plugins/metadata/lib/model/Metadata.php
@@ -21,6 +21,9 @@ class Metadata extends BaseMetadata implements ISyncableFile
 	const STATUS_DELETED = 3;
 	
 	const TYPE_ENTRY = 1;
+	const TYPE_CATEGORY = 2;
+	const TYPE_USER = 3;
+	const TYPE_CATEGORY_USER = 4;
 	
 	/**
 	 * @var MetadataProfile
