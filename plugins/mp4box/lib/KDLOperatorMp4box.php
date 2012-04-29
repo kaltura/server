f3aabfd23ee46ba0daa4a9c1d153a81466aaeed6
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-29 07:27:57 +0000

    fwr: #14521 - Pearson - embed captions into mp4
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79069 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/mp4box/lib/KDLOperatorMp4box.php
+++ plugins/mp4box/lib/KDLOperatorMp4box.php
@@ -4,6 +4,10 @@
  * @subpackage lib
  */
 class KDLOperatorMp4box extends KDLOperatorBase {
+
+	const ACTION_HINT = "actionHint";
+	const ACTION_EMBED_SUBTITLES = "actionEmbedSubtitles";
+
     public function __construct($id, $name=null, $sourceBlacklist=null, $targetBlacklist=null) {
     	parent::__construct($id,$name,$sourceBlacklist,$targetBlacklist);
     }
@@ -14,29 +18,67 @@ class KDLOperatorMp4box extends KDLOperatorBase {
     public function GenerateCommandLine(KDLFlavor $design, KDLFlavor $target, $extra=null)
 	{
 		
+$action="hint";
+		$paramsMap = KDLUtils::parseParamStr2Map($extra);
+		if (isset($paramsMap)){
+			if(array_key_exists('action', $paramsMap)) {
+				$action = $paramsMap['action'];
+			}
+		}
+
+$cmdStr = null;
+		switch($action){
+		case self::ACTION_EMBED_SUBTITLES:
+			$cmdStr = $this->generateEmbedSubtitlesCommandLine($design, $target, $paramsMap);
+			break;
+		case self::ACTION_HINT:
+		default:
+			$cmdStr = $this->generateHintCommandLine($design, $target, $paramsMap);
+			break;
+		}
+
+		return $cmdStr;
+	}
+	
+	/* ---------------------------
+	 * generateHintCommandLine
+	 */
+    private function generateHintCommandLine(KDLFlavor $design, KDLFlavor $target, $paramsMap)
+	{
 /*
 MP4Box -hint C:\Users\Anatol\Downloads\src_3.3gp -out c:\tmp\rtsp_3.3gp
  */
-$cmdStr = "-hint";
-
-$format = "fl";
-$acodec = "libmp3lam";
-
-		if(isset($target->_inFileName)){
-			$cmdStr .= " ".$target->_inFileName;
+		$cmdStr = " -hint ".KDLCmdlinePlaceholders::InFileName;
+		$cmdStr.= " -out ".KDLCmdlinePlaceholders::OutFileName;
+		return self::ACTION_HINT.$cmdStr;
+	}
+	
+	/* ---------------------------
+	 * generateEmbedSubtitlesCommandLine
+	 */
+    private function generateEmbedSubtitlesCommandLine(KDLFlavor $design, KDLFlavor $target, $paramsMap)
+	{
+/*
+MP4Box -add InFileName#video -add InFileName#audio -add InCfgFileName:hdlr=sbtl:lang=en:group=2:layer=-1 -new OutFileName
+MP4Box -add /web/content/r70v1/entry/data/77/287/1_vlm98u6b_1_ktl7nvmm_1.mp4#video -add /web/content/r70v1/entry/data/77/287/1_vlm98u6b_1_ktl7nvmm_1.mp4#audio -add /web/content/r70v1/entry/data/77/287/1_vlm98u6b_1_y2grw85h_1.srt:hdlr=sbtl:lang=en:group=2:layer=-1 -new /web/content/shared/tmp/1_vlm98u6b_subt.1.mp4
+ */
+$lang="en";
+		if (isset($paramsMap)){
+			if(array_key_exists('lang', $paramsMap)) {
+				$lang = $paramsMap['lang'];
+			}
 		}
-		else {
-			$cmdStr .= " ".KDLCmdlinePlaceholders::InFileName;
-		}
-
-		$cmdStr .= " -out";
-		if(isset($target->_outFileName)){
-			$cmdStr .= " ".$target->_outFileName;
-		}
-		else {
-			$cmdStr .= " ".KDLCmdlinePlaceholders::OutFileName;
-		}
-		return $cmdStr;
+		$cmdStr = " -add ".KDLCmdlinePlaceholders::InFileName."#video";
+		$cmdStr.= " -add ".KDLCmdlinePlaceholders::InFileName."#audio";
+		/*
+		 * hdlr - required operation, 'sbtl' for subtitles
+		 * lang - language, 'en' or other from flavorparams
+		 * group - 0-choose first available, otherwise set the passed value
+		 * layer - -1 - ignore
+		 */
+		$cmdStr.= " -add ".KDLCmdlinePlaceholders::OutFileName.".temp.srt:hdlr=sbtl:lang=$lang:group=0:layer=-1";
+		$cmdStr.= " -new ".KDLCmdlinePlaceholders::OutFileName;
+		return self::ACTION_EMBED_SUBTITLES.$cmdStr;
 	}
 	
 }
--- plugins/mp4box/lib/KOperationEngineMp4box.php
+++ plugins/mp4box/lib/KOperationEngineMp4box.php
@@ -6,23 +6,88 @@
 class KOperationEngineMp4box  extends KSingleOutputOperationEngine
 {
 
+	/**
+	* @var KalturaConvertJobData
+	*/
+	protected $data = null;
+
+	/**
+	* @var KalturaClient
+	*/
+	protected $client = null;
+
 	public function __construct($cmd, $outFilePath)
 	{
 		parent::__construct($cmd,$outFilePath);
 		KalturaLog::info(": cmd($cmd), outFilePath($outFilePath)");
 	}
 
-	protected function getCmdLine()
+	protected function getCmdLine() 
 	{
 		$exeCmd =  parent::getCmdLine();
-		KalturaLog::info(print_r($this,true));
-		return $exeCmd;
+		
+		if(strstr($exeCmd, KDLOperatorMp4box::ACTION_EMBED_SUBTITLES)!==FALSE) {
+			$exeCmd = str_replace (KDLOperatorMp4box::ACTION_EMBED_SUBTITLES,"", $exeCmd);
+			$this->retrieveCaptionFile($this->data, $this->client);
+		}
+		else if(strstr($exeCmd, KDLOperatorMp4box::ACTION_HINT)!==FALSE) {
+			$exeCmd = str_replace (KDLOperatorMp4box::ACTION_HINT,"", $exeCmd);
+		}
+//		KalturaLog::info(print_r($this,true));
+		return $exeCmd; 
 	}
 
 	public function configure(KSchedularTaskConfig $taskConfig, KalturaConvartableJobData $data, KalturaClient $client)
 	{
 		parent::configure($taskConfig, $data, $client);
-		KalturaLog::info("taskConfig-->".print_r($taskConfig,true)."\ndata->".print_r($data,true));
+		$this->data = $data;
+		$this->client = $client;
+
+//		KalturaLog::info("client-->".print_r($client,true));
+//		KalturaLog::info("taskConfig-->".print_r($taskConfig,true)."\ndata->".print_r($data,true));
 	}
 
+	private static function retrieveCaptionFile(KalturaConvartableJobData $data, KalturaClient $client)
+	{
+			// inpersonize
+		$client->getConfig()->partnerId = $data->flavorParamsOutput->partnerId;
+//		$results = $client->flavorAsset->get($data->flavorAssetId);
+		$flrAsst = $client->flavorAsset->get($data->flavorAssetId);
+		if(!isset($flrAsst)){
+			throw new KOperationEngineException("Failed to retrieve the flavor asset object (".$data->flavorAssetId.")");
+		}
+		$filter = new KalturaAssetFilter();
+		$filter->entryIdEqual = $flrAsst->entryId;
+		$cptList = $client->captionAsset->listAction($filter, null); 
+		if(!isset($cptList) || count($cptList->objects)==0){
+			throw new KOperationEngineException("No caption assets for entry (".$flrAsst->entryId.")");
+		}
+		
+		$cptUrl = $client->captionAsset->getUrl($cptList->objects[0]->id, null);
+		if(!isset($cptUrl)){
+			throw new KOperationEngineException("Failed to retrieve caption asset url (".$cptList->objects[0]->id.")");
+		}
+//		throw new KOperationEngineException("!!!caption url (".print_r($cptUrl,1).")");
+		{
+			KalturaLog::debug("Executing curl to retrieve caption asset file from - $cptUrl");
+			$curlWrapper = new KCurlWrapper($cptUrl);
+			$cptFilePath = $data->destFileSyncLocalPath.".temp.srt";
+			$res = $curlWrapper->exec($cptFilePath);
+			KalturaLog::debug("Curl results: $res");
+		
+			if(!$res || $curlWrapper->getError())
+			{
+				$errDescription = "Error: " . $curlWrapper->getError();
+				$curlWrapper->close();
+				throw new KOperationEngineException("Failed to curl the caption file url(".$cptUrl."). Error ($errDescription)");
+			}
+			$curlWrapper->close();
+			
+			if(!file_exists($cptFilePath))
+			{
+				throw new KOperationEngineException("Error: output file ($cptFilePath) doesn't exist");
+			}
+			KalturaLog::debug("Finished");
+		}
+	}
 }
