bedf7662747c90d12f995841f2950e35cefa31c6
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-04-30 15:59:32 +0000

    qnd: end user level reports
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79219 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_graph_by_user_by_app.sql
+++ alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_graph_by_user_by_app.sql
@@ -8,12 +8,12 @@ FROM
 	dwh_hourly_events_entry_user_app ev, kalturadw.dwh_dim_pusers us, kalturadw.dwh_dim_application ap
 WHERE 	
 	{OBJ_ID_CLAUSE} # ev.entry_id in 
-	us.partner_id = {PARTNER_ID}
+	AND us.partner_id = {PARTNER_ID}
 	AND us.puser_id = ev.user_id
 	AND us.name IN {PUSER_ID}  
 	AND ap.name = {APPLICATION_NAME}
 	AND ap.application_id = ev.application_id
-	AND ev.partner_id =  {PARTNER_ID} # PARTNER_ID
+	AND ev.partner_id =  us.partner_id # PARTNER_ID
 	AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
     			AND     IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
 			AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 0)
