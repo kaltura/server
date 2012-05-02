4864017324ae86bcbdd56c64257cf5800f6b45c7
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-02 12:44:01 +0000

    fwr: falcon - category batch + lock categories + partner features status
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79405 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/filters/orderEnums/KalturaCategoryOrderBy.php
+++ api_v3/lib/types/filters/orderEnums/KalturaCategoryOrderBy.php
@@ -9,14 +9,16 @@ class KalturaCategoryOrderBy extends KalturaStringEnum
 	const DEPTH_DESC = "-depth";
 	const NAME_ASC = "+name";
 	const NAME_DESC = "-name";
+	const FULL_IDS_ASC = "+fullIds";
+	const FULL_IDS_DESC = "-fullIds";
 	const ENTRIES_COUNT_ASC = "+entriesCount";
 	const ENTRIES_COUNT_DESC = "-entriesCount";
 	const CREATED_AT_ASC = "+createdAt";
 	const CREATED_AT_DESC = "-createdAt";
 	const UPDATED_AT_ASC = "+updatedAt";
 	const UPDATED_AT_DESC = "-updatedAt";
-	const DIRECT_ENTRIES_COUNT_ASC = "+directEntriesCount";
-	const DIRECT_ENTRIES_COUNT_DESC = "-directEntriesCount";
+	const MEMBERS_COUNT_ASC = "+membersCount";
+	const MEMBERS_COUNT_DESC = "-membersCount";
 	const PARTNER_SORT_VALUE_ASC = "+partnerSortValue";
 	const PARTNER_SORT_VALUE_DESC = "-partnerSortValue";
 }
