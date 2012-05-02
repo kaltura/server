712294b0908efff3dda94dcc41e8701e96ad5c2f
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-02 19:08:26 +0000

    qnd: end user level reports - support context aggregation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79443 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_unique_total_by_context.sql
+++ alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_unique_total_by_context.sql
@@ -4,7 +4,7 @@ COUNT(DISTINCT entry_id) unique_videos
 FROM 
 	dwh_hourly_events_context_entry_user_app
 WHERE 	{OBJ_ID_CLAUSE}
-	AND {OBJ_CAT_NAME}
+	AND {CAT_ID_CLAUSE}
 	AND partner_id =  {PARTNER_ID} # PARTNER_ID
 	AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
     			AND     IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
