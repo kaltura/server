298e125128c317bfacc50abf291e95e11597b8dc
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-02 19:39:03 +0000

    qnd: end user level reports - support context aggregation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79450 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_unique_total_by_user_by_app.sql
+++ alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_unique_total_by_user_by_app.sql
@@ -15,7 +15,7 @@ WHERE 	{OBJ_ID_CLAUSE}
 	AND us.puser_id = ev.user_id
 	AND ap.name = {APPLICATION_NAME}
 	AND ap.application_id = ev.application_id
-	AND partner_id =  {PARTNER_ID} # PARTNER_ID
+	AND ev.partner_id =  {PARTNER_ID} # PARTNER_ID
 	AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
     			AND     IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
 			AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 0)
