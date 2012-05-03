427b38bb4673214c48e813f9efb72137042a3102
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-03 10:09:13 +0000

    qnd: end user level reports - support context aggregation remove match clause for categories
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79493 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_unique_total_no_text.sql
+++ alpha/apps/kaltura/lib/reports/user_engagement/user_engagement_unique_total_no_text.sql
@@ -1,21 +1,16 @@
 /* Unique Known Users |  Unique Videos |  Plays | Minutes Viewed | Avg. View time | player impressions  | Impression to play ratio */
 SELECT 
-SUM(count_plays) count_plays,
-SUM(sum_time_viewed) sum_time_viewed,
-SUM(sum_time_viewed)/SUM(count_plays) avg_time_viewed,
-SUM(count_loads) count_loads,
-(SUM(count_plays) / SUM(count_loads)) load_play_ratio
+COUNT(DISTINCT user_id) unique_known_users,
+COUNT(DISTINCT entry_id) unique_videos,
 
 FROM 
-	dwh_hourly_events_entry ev, dwh_dim_user_reports_allowed_partners p
+	dwh_hourly_events_context_entry_user_app ev
 WHERE 	{OBJ_ID_CLAUSE} # ev.entry_id in 
-	AND ev.partner_id =  {PARTNER_ID} # PARTNER_ID
-	AND ev.partner_id = ev.partner_id
+	AND partner_id =  {PARTNER_ID} # PARTNER_ID
 	AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
     			AND     IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
 			AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 0)
 			AND hour_id < IF (date_id = IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 24)
-	AND date_id >= p.created_date_id and hour_id >= p.created_hour_id
 	AND 
 		( count_time_viewed > 0 OR
 		  count_plays > 0 OR
