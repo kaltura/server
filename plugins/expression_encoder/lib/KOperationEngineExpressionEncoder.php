b7415b23be7e933cbbaec2ee790383685f1d9d22
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-07 06:16:20 +0000

    fwr: #15104 - EE3 fails on short term source lock. - detecting this case, perform 3 retres
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79680 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/expression_encoder/lib/KOperationEngineExpressionEncoder.php
+++ plugins/expression_encoder/lib/KOperationEngineExpressionEncoder.php
@@ -49,8 +49,50 @@ class KOperationEngineExpressionEncoder  extends KSingleOutputOperationEngine
 	}
 	
 	public function operate(kOperator $operator = null, $inFilePath, $configFilePath = null)
-	{
-		parent::operate($operator, $inFilePath, $configFilePath);
+	{	
+			/*
+			 * The EE3 tends to lock the source file for a short period. Since the same source file might be used 
+			 * by number of transcoding process, occasionally, one of the transcoding processes gets denied on an 
+			 * attempt to access the source file. 
+			 * The code catches the Exceptions thrown by the KOperrationEngine. 
+			 * On exception caused by the above state - 3 retries are performed w/10sec delay in between.
+			 * All other exceptions are 're-thrown'.
+			 *
+			 * Note: For PHP 5.3 the exception re-throw should be enhanced with 'e->getPrevious()'
+			 */
+		$ex=null;
+		for($iEx=0; $iEx<3; $iEx++) {
+			try {
+					/*
+					 * Successfull execution attempt
+					 */
+				parent::operate($operator, $inFilePath, $configFilePath);
+				return;
+			}
+			catch(Exception $e) {
+				$ex=$e;
+					/* 
+					 * If there is no log file (to check the error type) 
+					 * - halt retries and re-through the same exception
+					 */ 
+				if(file_exists($this->logFilePath)==false){
+					break;
+				}
+//				$usedMsg="Cannot access the file because it is being used by another process";
+				$usedMsg="Cannot access the file because it is being used";
+				$logStr = $this->getLogData();
+					/* 
+					 * If the log does not contain the 'being used' error 
+					 * - halt retries and re-through the same exception
+					 */ 
+				if(strstr($logStr,$usedMsg)==false)
+					break;
+				$secsToSleep=10;
+				KalturaLog::info("EE3 failed with 'Cannot access the file because it is being used by another process'. Waiting $secsToSleep sec. Attempt:$iEx");
+				sleep($secsToSleep);
+			}
+		}
+		throw new KOperationEngineException($ex->getMessage(), $ex->getCode());
 	}
 	
 }
