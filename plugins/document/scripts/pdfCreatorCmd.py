6241c876429ccfcbcfb15ba47fea54cb8fec6807
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-10 09:41:35 +0000

    qnd: pdfCreatorCmd.py  - kill all office / pdfcreator processes before converting  - run pdfcreator /clearcache
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@79910 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/document/scripts/pdfCreatorCmd.py
+++ plugins/document/scripts/pdfCreatorCmd.py
@@ -1,5 +1,8 @@
 import win32print
+import win32con
+import win32api
 import os.path
+import ctypes
 import time
 import sys
 
@@ -8,6 +11,59 @@ def getDefaultPrinter():
         return win32print.GetDefaultPrinter()
     except RuntimeError:        # The default printer was not found.
         return None
+
+
+TH32CS_SNAPPROCESS = 0x00000002
+class PROCESSENTRY32(ctypes.Structure):
+     _fields_ = [("dwSize", ctypes.c_ulong),
+                 ("cntUsage", ctypes.c_ulong),
+                 ("th32ProcessID", ctypes.c_ulong),
+                 ("th32DefaultHeapID", ctypes.c_ulong),
+                 ("th32ModuleID", ctypes.c_ulong),
+                 ("cntThreads", ctypes.c_ulong),
+                 ("th32ParentProcessID", ctypes.c_ulong),
+                 ("pcPriClassBase", ctypes.c_ulong),
+                 ("dwFlags", ctypes.c_ulong),
+                 ("szExeFile", ctypes.c_char * 260)]
+
+def getProcessList():
+     # See http://msdn2.microsoft.com/en-us/library/ms686701.aspx
+     CreateToolhelp32Snapshot = ctypes.windll.kernel32.\
+                                CreateToolhelp32Snapshot
+     Process32First = ctypes.windll.kernel32.Process32First
+     Process32Next = ctypes.windll.kernel32.Process32Next
+     CloseHandle = ctypes.windll.kernel32.CloseHandle
+     hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+     pe32 = PROCESSENTRY32()
+     pe32.dwSize = ctypes.sizeof(PROCESSENTRY32)
+     if Process32First(hProcessSnap,
+                       ctypes.byref(pe32)) == win32con.FALSE:
+         print >> sys.stderr, "Failed getting first process."
+         return
+     while True:
+         yield (pe32.szExeFile, pe32.th32ProcessID)
+         if Process32Next(hProcessSnap, ctypes.byref(pe32)) == win32con.FALSE:
+             break
+     CloseHandle(hProcessSnap)
+
+def killProcess(processId):
+    try:
+        handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, processId)
+        if handle:
+            win32api.TerminateProcess(handle, 0)
+            win32api.CloseHandle(handle)
+    except pywintypes.error:
+        pass
+
+
+def killProcessesByName(exeNames):
+    result = False
+    for (exeName, processId) in getProcessList():
+        if exeName.lower() in exeNames:
+            print 'Killing %s %s' % (exeName, processId)
+            killProcess(processId)
+            result = True
+    return result
 
 if len(sys.argv) < 4:
     print 'wrong usage of this script. usage: %s {inputFile} {outFile} [--readOnly]' % os.path.dirname(__file__);
@@ -25,6 +81,20 @@ else:
     outputFile = sys.argv[-1]
     commandParams = sys.argv[1:-2]
 
+# clean up any previous convert leftovers
+if killProcessesByName(['powerpnt.exe', 'excel.exe', 'winword.exe', 'pdfcreator.exe']):
+    time.sleep(5)
+clearCacheCmd = '%s /CLEARCACHE /NoStart' % ' '.join(commandParams)
+print '\nclearing cache: %s' % clearCacheCmd
+os.system(clearCacheCmd)
+
+# make sure the default printer is set appropriately
+PDF_CREATOR_PRINTER = 'PDFCreator'
+if getDefaultPrinter() != PDF_CREATOR_PRINTER:
+    print 'setting default printer to %s' % PDF_CREATOR_PRINTER
+    win32print.SetDefaultPrinter(PDF_CREATOR_PRINTER)
+
+# build the command line
 commandParams.append('/NoStart')
 
 inputFileExt = os.path.splitext(inputFile)[1].lower()
@@ -35,12 +105,6 @@ else:
     commandParams.append('/PF"%s"' % inputFile)
 
 command = ' '.join(commandParams)
-
-# make sure the default printer is set appropriately
-PDF_CREATOR_PRINTER = 'PDFCreator'
-if getDefaultPrinter() != PDF_CREATOR_PRINTER:
-    print 'setting default printer to %s' % PDF_CREATOR_PRINTER
-    win32print.SetDefaultPrinter(PDF_CREATOR_PRINTER)
 
 # execute the command
 print '\ncommand: %s' % command
