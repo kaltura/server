4d5bc5758fcb9d3139405ccca86a18c75f1d354c
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-17 06:03:44 +0000

    qnd: ignore file syncing for specific file types.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80307 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/multi_centers/lib/kMultiCentersSynchronizer.php
+++ plugins/multi_centers/lib/kMultiCentersSynchronizer.php
@@ -1,6 +1,12 @@
 <?php
 class kMultiCentersSynchronizer implements kObjectAddedEventConsumer
 {
+	/**
+	 * Contain all object types and sub types that shouldn't be synced 
+	 * @var array
+	 */
+	protected $excludedSyncFileObjectTypes = null;
+	
 	public function getEntryId(FileSync $fileSync)
 	{
 		if($fileSync->getObjectType() == FileSyncObjectType::ENTRY)
@@ -23,6 +29,58 @@ class kMultiCentersSynchronizer implements kObjectAddedEventConsumer
 		return null;
 	}
 	
+	/**
+	 * Check if specific file sync that belong to object type and sub type should be synced
+	 * 
+	 * @param int $objectType
+	 * @param int $objectSubType
+	 * @return bool
+	 */
+	public function shouldSyncFileObjectType($objectType, $objectSubType)
+	{
+		if(is_null($this->excludedSyncFileObjectTypes))
+		{
+			$this->excludedSyncFileObjectTypes = array();
+			$dcConfig = kConf::getMap("dc_config");
+			if($dcConfig->sync_exclude_types)
+			{
+				foreach($dcConfig->sync_exclude_types as $syncExcludeType)
+				{
+					$configObjectType = $syncExcludeType;
+					$configObjectSubType = null;
+					
+					if(strpos($syncExcludeType, ':') > 0)
+						list($configObjectType, $configObjectSubType) = explode(':', $syncExcludeType, 2);
+						
+					// translate api dynamic enum, such as contentDistribution.EntryDistribution - {plugin name}.{object name}
+					if(!is_numeric($configObjectType))
+						$configObjectType = kPluginableEnumsManager::apiToCore('FileSyncObjectType', $configObjectType);
+							
+					// translate api dynamic enum, including the enum type, such as conversionEngineType.mp4box.Mp4box - {enum class name}.{plugin name}.{object name}
+					if(!is_null($configObjectSubType) && !is_numeric($configObjectSubType))
+					{
+						list($enumType, $configObjectSubType) = explode('.', $configObjectSubType);
+						$configObjectSubType = kPluginableEnumsManager::apiToCore($enumType, $configObjectSubType);
+					}
+					
+					if(!isset($this->excludedSyncFileObjectTypes[$configObjectType]))
+						$this->excludedSyncFileObjectTypes[$configObjectType] = array();
+						
+					if(!is_null($configObjectSubType))
+						$this->excludedSyncFileObjectTypes[$configObjectType] = true;
+				}
+			}
+		}
+		
+		if(!in_array($objectType, $this->excludedSyncFileObjectTypes))
+			return true;
+			
+		if(count($this->excludedSyncFileObjectTypes[$objectType]) && !in_array($objectSubType, $this->excludedSyncFileObjectTypes[$objectType]))
+			return true;
+			
+		return false;
+	}
+	
 	/* (non-PHPdoc)
 	 * @see kObjectAddedEventConsumer::shouldConsumeAddedEvent()
 	 */
@@ -36,6 +94,8 @@ class kMultiCentersSynchronizer implements kObjectAddedEventConsumer
 			$object->getFileType() == FileSync::FILE_SYNC_FILE_TYPE_FILE 
 			&&
 			$object->getDc() != kDataCenterMgr::getCurrentDcId()
+			&&
+			$this->shouldSyncFileObjectType($object->getObjectType(), $object->getObjectSubType())
 		)
 			return true;
 		
