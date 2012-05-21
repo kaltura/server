d8845ea4b79a11511432b8cb644eba777cbf57f2
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-21 07:35:58 +0000

    qnd: partner usage revamp - partner usage reports
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80481 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/partner_bandwidth_usage/partner_bandwidth_usage_detail_no_text.sql
+++ alpha/apps/kaltura/lib/reports/partner_bandwidth_usage/partner_bandwidth_usage_detail_no_text.sql
@@ -1,2 +1,2 @@
 #CALL kalturadw.calc_partner_billing_data("{START_DATE}", {PARTNER_ID});
-CALL kalturadw.calc_partner_billing_data("{FROM_DATE_ID}", {PARTNER_ID});
+CALL kalturadw.calc_partner_usage_data("{FROM_DATE_ID}", {PARTNER_ID}, {IS_FREE_PACKAGE});
