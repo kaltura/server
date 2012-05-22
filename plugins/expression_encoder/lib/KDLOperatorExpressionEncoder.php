d37110f45d9ec1cfae4f5f6d3f10cb31db88b0a8
anatol <anatol@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-22 06:22:54 +0000

    fwr: uses new KDLExpressionEncoder3 from infra/cdl/kdl
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80530 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/expression_encoder/lib/KDLOperatorExpressionEncoder.php
+++ plugins/expression_encoder/lib/KDLOperatorExpressionEncoder.php
@@ -3,31 +3,7 @@
  * @package plugins.expressionEncoder
  * @subpackage lib
  */
-class KDLOperatorExpressionEncoder extends KDLOperatorBase {
-    public function __construct($id, $name=null, $sourceBlacklist=null, $targetBlacklist=null) {
-    	parent::__construct($id,$name,$sourceBlacklist,$targetBlacklist);
-    }
+class KDLOperatorExpressionEncoder extends KDLExpressionEncoder3 {
 
-    public function GenerateConfigData(KDLFlavor $design, KDLFlavor $target)
-	{
-		$ee3 = new KDLExpressionEncoder3();
-				// Remove slaches that were added to solve
-				// JSON serialization issue
-		$xmlStr=str_replace ('"' , '\"' ,  $ee3->GeneratePresetFile($target));
-		return $xmlStr;
-	}
-
-    public function GenerateCommandLine(KDLFlavor $design, KDLFlavor $target, $extra=null)
-    {
-    	return KDLCmdlinePlaceholders::InFileName . ' ' . KDLCmdlinePlaceholders::ConfigFileName;
-    }
-    
-	/* ---------------------------
-	 * CheckConstraints
-	 */
-	public function CheckConstraints(KDLMediaDataSet $source, KDLFlavor $target, array &$errors=null, array &$warnings=null)
-	{
-	    return parent::CheckConstraints($source, $target, $errors, $warnings);
-	}
 }
 
