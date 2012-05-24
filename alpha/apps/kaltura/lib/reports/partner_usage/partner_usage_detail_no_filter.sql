67755da48617745febd01aa43fe2c95fd0f25e2c
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-24 16:18:24 +0000

    qnd: return all requested dates (even if there is no data)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80902 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/partner_usage/partner_usage_detail_no_filter.sql
+++ alpha/apps/kaltura/lib/reports/partner_usage/partner_usage_detail_no_filter.sql
@@ -1,17 +1,20 @@
 SELECT
-	{GROUP_COLUMN}, /*partner_id, */ 
-	SUM(count_bandwidth) as bandwidth_consumption,
-	MAX(aggr_storage_mb) as used_storage,
-	SUM(count_bandwidth) + MAX(aggr_storage_mb) AS combined_bandwidth_storage
-FROM (
+	IF('{GROUP_COLUMN}' = 'date_id',day_id,t.month_id) {GROUP_COLUMN}, 
+	IFNULL(SUM(count_bandwidth),0) as bandwidth_consumption,
+	IFNULL(MAX(aggr_storage_mb),0) as used_storage,
+	IFNULL(SUM(count_bandwidth),0) + IFNULL(MAX(aggr_storage_mb),0)	AS combined_bandwidth_storage
+FROM  kalturadw.dwh_dim_time t LEFT JOIN (
 	SELECT
-		date_id, FLOOR(date_id/100) month_id, partner_id, ifnull(count_bandwidth_kb, 0)/1024 count_bandwidth, ifnull(aggr_storage_mb, 0) aggr_storage_mb
+		date_id, FLOOR(date_id/100) month_id, partner_id, IFNULL(count_bandwidth_kb, 0)/1024 count_bandwidth, IFNULL(aggr_storage_mb, 0) aggr_storage_mb
 	FROM
 		kalturadw.dwh_hourly_partner_usage
         WHERE
 		{OBJ_ID_CLAUSE}
 		AND
 		date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}
-        ) raw_data
-GROUP BY {GROUP_COLUMN}
+        ) raw_data ON date_id = day_id
+WHERE day_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}
+GROUP BY IF('{GROUP_COLUMN}' = 'date_id',day_id,t.month_id) 
 LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */ 
+
+
