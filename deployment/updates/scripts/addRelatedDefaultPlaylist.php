867f4ba18f12ceec0acf3296296e7bc310dd99cd
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-24 08:46:44 +0000

    fwr: related default playlist
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80783 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/addRelatedDefaultPlaylist.php
+++ deployment/updates/scripts/addRelatedDefaultPlaylist.php
@@ -18,6 +18,7 @@ $dbPlaylist->setPartnerId(0);
 $dbPlaylist->setStatus ( entryStatus::READY );
 $dbPlaylist->setKshowId ( null );
 $dbPlaylist->setType ( entryType::PLAYLIST );
+$dbPlaylist->setMediaType(entry::ENTRY_MEDIA_TYPE_XML);
 $dbPlaylist->setDataContent('<?xml version="1.0"?><playlist><total_results>12</total_results><filters><filter><in_status>2,1</in_status><in_type>1,2,7</in_type><in_moderation_status>2,5,6,1</in_moderation_status><order_by>-plays</order_by><limit>12</limit></filter></filters></playlist>');
 $dbPlaylist->setDisplayInSearch ( 2 );
 $dbPlaylist->save();
