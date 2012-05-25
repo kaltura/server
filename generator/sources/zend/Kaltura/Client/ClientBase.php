80361bf423cdd960b3503799db7491fe1f8f9a4e
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-25 13:23:27 +0000

    fwr: allow client to be constructed with partner Id null
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80956 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/sources/zend/Kaltura/Client/ClientBase.php
+++ generator/sources/zend/Kaltura/Client/ClientBase.php
@@ -141,7 +141,7 @@ class Kaltura_Client_ClientBase
 	public function queueServiceActionCall($service, $action, $params = array(), $files = array())
 	{
 		// in start session partner id is optional (default -1). if partner id was not set, use the one in the config
-		if ($params["partnerId"] === -1)
+		if (isset($params["partnerId"]) && $params["partnerId"] === -1)
 			$params["partnerId"] = $this->config->partnerId;
 		if (!isset($params["partnerId"]))
 		    $params["partnerId"] = null;
