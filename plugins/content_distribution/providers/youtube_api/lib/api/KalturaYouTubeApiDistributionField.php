dea5c33511f2f00d29044b4186614489c0fd853d
dori <dori@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-28 09:15:49 +0000

    ps: fix for QC #2406 - wrong php doc
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80982 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/providers/youtube_api/lib/api/KalturaYouTubeApiDistributionField.php
+++ plugins/content_distribution/providers/youtube_api/lib/api/KalturaYouTubeApiDistributionField.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package plugins.dailymotionDistribution
+ * @package plugins.youtubeApiDistribution
  * @subpackage api.enum
  */
 class KalturaYouTubeApiDistributionField extends KalturaStringEnum implements YouTubeApiDistributionField
