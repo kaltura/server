aa0e054c26be5eb751fe34fc588928aa3ad476ff
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-28 06:54:44 +0000

    qnd: use POPULATE_FROM_LOG var instead of hardcoded populateFromLog.php.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@80968 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/scripts/watch.populate.sh
+++ plugins/sphinx_search/scripts/watch.populate.sh
@@ -6,14 +6,15 @@ else
 fi
 . `dirname $REAL_SCRIPT`/../../../configurations/system.ini
 
+POPLATE_FROM_LOG="populateFromLog.php"
 SERVER=$1
 MAILTO="$ADMIN_CONSOLE_ADMIN_MAIL"
-KP=$(pgrep -f populateFromLog.php)
+KP=$(pgrep -f $POPLATE_FROM_LOG)
 MAINT=$BASE_DIR/maintenance
 if [[ "X$KP" = "X" && ! -f $MAINT ]]
       then
-          echo "populateFromLog.php `hostname` was restarted" | mail -s "populateFromLog.php script not found on `hostname`" $MAILTO
+          echo "$POPLATE_FROM_LOG `hostname` was restarted" | mail -s "$POPLATE_FROM_LOG script not found on `hostname`" $MAILTO
 	  cd $APP_DIR/plugins/sphinx_search/scripts
-	  php populateFromLog.php ${SERVER} >> $LOG_DIR/kaltura_sphinx_populate.log 2>&1 &
+	  php $POPLATE_FROM_LOG ${SERVER} >> $LOG_DIR/kaltura_sphinx_populate.log 2>&1 &
       fi
 
