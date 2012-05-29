a82d5446c85ff4b80e879e4747cf0d54a71fbc2a
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-29 08:59:59 +0000

    fwr: removed debug logs
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@81109 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- ui_infra/InfraBootstrapper.php
+++ ui_infra/InfraBootstrapper.php
@@ -248,11 +248,9 @@ class InfraBootstrapper extends Zend_Application_Bootstrap_Bootstrap
       			}
       			
       			if ($allow) {
-      			    KalturaLog::debug("### allow $currentRole $resource");
       				$acl->allow($currentRole, $resource);
       			}
       			else {
-      			    KalturaLog::debug("### deny $currentRole $resource");
       				$acl->deny($currentRole, $resource);
       			}
       		}
