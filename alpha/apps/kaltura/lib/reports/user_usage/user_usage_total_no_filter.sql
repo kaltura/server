726b4b5e16704da69575f2a7191fc9a4ed42345a
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-05-29 00:03:19 +0000

    qnd: user usage reports
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@81062 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/user_usage/user_usage_total_no_filter.sql
+++ alpha/apps/kaltura/lib/reports/user_usage/user_usage_total_no_filter.sql
@@ -11,9 +11,9 @@ FROM
 			AND
 	date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}) added,
 	(SELECT
-		total_storage_kb/1024 total_storage_mb,
-		total_entries,
-		total_msecs
+		IFNULL(MAX(total_storage_kb/1024),0) total_storage_mb,
+        IFNULL(MAX(total_entries),0) total_entries,
+        IFNULL(MAX(total_msecs),0) total_msecs
 	FROM
 		kalturadw.dwh_hourly_user_usage u JOIN (SELECT kuser_id, MAX(date_id) date_id FROM kalturadw.dwh_hourly_user_usage WHERE partner_id = {PARTNER_ID} and date_id <= {TO_DATE_ID} GROUP BY kuser_id) total
 		ON u.kuser_id = total.kuser_id AND u.date_id = total.date_id) total
