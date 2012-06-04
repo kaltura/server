392fa9eea6e71d00a7bd5e546c7492aa94983132
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-04 15:10:06 +0000

    fwr: new list reponse for new varConsole service
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@81535 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/var_console/lib/api/KalturaPartnerUsageListResponse.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package plugins.varConsole
+ * @subpackage api.types
+ */
+class KalturaPartnerUsageListResponse extends KalturaObject
+{
+    /**
+     * @var array
+     */
+    public $objects;
+    
+    /**
+     * @var int
+     */
+    public $totalCount;
+}
