973a2aa6a0220b071ff970b71d291ee15c25fc1d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-05 11:26:18 +0000

    qnd: no need for ui conf tags search
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@81605 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/enums/taggedObjectType.php
+++ alpha/lib/enums/taggedObjectType.php
@@ -9,7 +9,6 @@ interface taggedObjectType extends BaseEnum
     const CATEGORY = 2;
     const FLAVORASSET = 4;
     const THUMBASSET = 5;
-    const UICONF = 6;
     const KUSER=8;
     const PERMISSION=9;
     const PERMISSIONITEM=10;
