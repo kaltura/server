8525da345c915ceb6d0c20135a65a027208e99c3
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-05 15:20:44 +0000

    qnd: falcon - entitlement general fixes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@81658 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/services/FlavorAssetService.php
+++ api_v3/services/FlavorAssetService.php
@@ -516,7 +516,7 @@ class FlavorAssetService extends KalturaAssetService
 	public function getWebPlayableByEntryIdAction($entryId)
 	{
 		// entry could be "display_in_search = 2" - in that case we want to pull it although KN is off in services.ct for this action
-		$c = new Criteria();
+		$c = KalturaCriteria::create(entryPeer::OM_CLASS);
 		$c->addAnd(entryPeer::ID, $entryId);
 		$criterionPartnerOrKn = $c->getNewCriterion(entryPeer::PARTNER_ID, $this->getPartnerId());
 		$criterionPartnerOrKn->addOr($c->getNewCriterion(entryPeer::DISPLAY_IN_SEARCH, mySearchUtils::DISPLAY_IN_SEARCH_KALTURA_NETWORK));
