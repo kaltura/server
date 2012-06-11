3db28bac238c4628616cd5019b0594511ee81a6f
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-11 08:14:13 +0000

    def: 3027 BULK_UPLOAD_NOT_FOUND error when downloading original CSV file
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@82025 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/services/BulkUploadService.php
+++ api_v3/services/BulkUploadService.php
@@ -170,10 +170,10 @@ class BulkUploadService extends KalturaBaseService
 function serveAction($id)
 	{
 		$c = new Criteria();
-		$c->addAnd(BatchJobLogPeer::JOB_ID, $id);
-		$c->addAnd(BatchJobLogPeer::PARTNER_ID, $this->getPartnerId());
-		$c->addAnd(BatchJobLogPeer::JOB_TYPE, BatchJobType::BULKUPLOAD);
-		$batchJob = BatchJobLogPeer::doSelectOne($c);
+		$c->addAnd(BatchJobPeer::ID, $id);
+		$c->addAnd(BatchJobPeer::PARTNER_ID, $this->getPartnerId());
+		$c->addAnd(BatchJobPeer::JOB_TYPE, BatchJobType::BULKUPLOAD);
+		$batchJob = BatchJobPeer::doSelectOne($c);
 		
 		if (!$batchJob)
 			throw new KalturaAPIException(KalturaErrors::BULK_UPLOAD_NOT_FOUND, $id);
