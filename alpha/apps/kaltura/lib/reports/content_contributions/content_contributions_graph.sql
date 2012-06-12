4d19907887fdb3188bd6398481eb045cbb5d9d19
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-12 09:58:21 +0000

    def: 3058 - return 0 instead of NULL
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@82163 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/content_contributions/content_contributions_graph.sql
+++ alpha/apps/kaltura/lib/reports/content_contributions/content_contributions_graph.sql
@@ -1,12 +1,12 @@
 SELECT 
 	DATE(created_at - interval {TIME_SHIFT} hour)*1 created_date_id,
 	COUNT(1) count_total,
-	COUNT(IF(is_admin_content = 0, 1,NULL)) count_ugc,
-	COUNT(IF(is_admin_content = 1, 1,NULL)) count_admin,
-	COUNT(IF(entry_media_type_id = 1, 1,NULL)) count_video ,
-	COUNT(IF(entry_media_type_id = 5, 1,NULL)) count_audio ,
-	COUNT(IF(entry_media_type_id = 2, 1,NULL)) count_image ,
-	COUNT(IF(entry_media_type_id = 6, 1,NULL)) count_mix
+	IFNULL(COUNT(IF(is_admin_content = 0, 1,NULL)),0) count_ugc,
+	IFNULL(COUNT(IF(is_admin_content = 1, 1,NULL)),0) count_admin,
+	IFNULL(COUNT(IF(entry_media_type_id = 1, 1,NULL)),0) count_video ,
+	IFNULL(COUNT(IF(entry_media_type_id = 5, 1,NULL)),0) count_audio ,
+	IFNULL(COUNT(IF(entry_media_type_id = 2, 1,NULL)),0) count_image ,
+	IFNULL(COUNT(IF(entry_media_type_id = 6, 1,NULL)),0) count_mix
 FROM dwh_dim_entries
 WHERE
 {OBJ_ID_CLAUSE}
--- alpha/apps/kaltura/lib/reports/top_contributors/top_contributors_graph.sql
+++ alpha/apps/kaltura/lib/reports/top_contributors/top_contributors_graph.sql
@@ -1,10 +1,10 @@
 SELECT 
 	DATE(created_at - interval {TIME_SHIFT} hour)*1 created_date_id,
 	COUNT(1) count_total,
-	COUNT(IF(entry_media_type_id = 1, 1,NULL)) count_video ,
-	COUNT(IF(entry_media_type_id = 2, 1,NULL)) count_image ,
-	COUNT(IF(entry_media_type_id = 5, 1,NULL)) count_audio ,
-	COUNT(IF(entry_media_type_id = 6, 1,NULL)) count_mix
+	IFNULL(COUNT(IF(entry_media_type_id = 1, 1,0)),0) count_video ,
+	IFNULL(COUNT(IF(entry_media_type_id = 2, 1,0)),0) count_image ,
+	IFNULL(COUNT(IF(entry_media_type_id = 5, 1,0)),0) count_audio ,
+	IFNULL(COUNT(IF(entry_media_type_id = 6, 1,0)),0) count_mix
 FROM dwh_dim_entries ev
 WHERE
 {OBJ_ID_CLAUSE}
