e13c6d7017253c3e5f46d6efe14cd133d66ede13
noai <noai@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-14 07:36:50 +0000

    qnd: falcon - entitlement general fixes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@82373 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -11,6 +11,8 @@ require_once(ROOT_DIR . '/infra/KAutoloader.php');
 $include_path = realpath(dirname(__FILE__).'/../../vendor/ZendFramework/library') . PATH_SEPARATOR . get_include_path();
 set_include_path($include_path);
 
+KalturaLog::info("Build cache: it may take a while.");
+
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "*"));
 KAutoloader::setClassMapFilePath(kConf::get("cache_root_path") . '/scripts/classMap.cache');
