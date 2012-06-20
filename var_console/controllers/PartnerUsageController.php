b9886d6094ae7bf8aca5b77d58aca9e1692ef7ac
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-20 06:24:34 +0000

    fwr: remove debug log
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@82777 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- var_console/controllers/PartnerUsageController.php
+++ var_console/controllers/PartnerUsageController.php
@@ -7,7 +7,6 @@ class PartnerUsageController extends Zend_Controller_Action
 {
 	public function indexAction()
 	{
-		KalturaLog::debug("### ".Infra_AuthHelper::getAuthInstance()->getIdentity()->getTimezoneOffset());
 		$request = $this->getRequest();
 		$page = $this->_getParam('page', 1);
 		$pageSize = $this->_getParam('pageSize', 10);
