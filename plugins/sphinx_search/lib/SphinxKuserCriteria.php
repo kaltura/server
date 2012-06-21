074320259c9565a233b31b13a8dd6340be4eab46
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-06-21 08:54:21 +0000

    qnd: removed advanced search for first and last name
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@82934 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/lib/SphinxKuserCriteria.php
+++ plugins/sphinx_search/lib/SphinxKuserCriteria.php
@@ -332,58 +332,52 @@ class SphinxKuserCriteria extends SphinxCriteria
 		
 		if($filter->get('_likex_first_name_or_last_name'))
 		{
-			$freeTexts = $filter->get('_likex_first_name_or_last_name');
-			KalturaLog::debug("Attach free text [$freeTexts]");
+			$names = $filter->get('_likex_first_name_or_last_name');
+			KalturaLog::debug("Attach free text [$names]");
 			
 			$additionalConditions = array();
-			$advancedSearch = $filter->getAdvancedSearch();
-			if($advancedSearch)
+			if(preg_match('/^"[^"]+"$/', $names))
 			{
-				$additionalConditions = $advancedSearch->getFreeTextConditions($freeTexts);
-			}
-			
-			if(preg_match('/^"[^"]+"$/', $freeTexts))
-			{
-				$freeText = str_replace('"', '', $freeTexts);
-				$freeText = SphinxUtils::escapeString($freeText);
-				$freeText = "^$freeText$";
-				$additionalConditions[] = "@(" . kuserFilter::FIRST_NAME_OR_LAST_NAME . ") $freeText\\\*";
+				$name = str_replace('"', '', $names);
+				$name = SphinxUtils::escapeString($name);
+				$name = "^$name$";
+				$additionalConditions[] = "@(" . kuserFilter::FIRST_NAME_OR_LAST_NAME . ") $name\\\*";
 			}
 			else
 			{
-				if(strpos($freeTexts, baseObjectFilter::IN_SEPARATOR) > 0)
+				if(strpos($names, baseObjectFilter::IN_SEPARATOR) > 0)
 				{
-					str_replace(baseObjectFilter::AND_SEPARATOR, baseObjectFilter::IN_SEPARATOR, $freeTexts);
+					str_replace(baseObjectFilter::AND_SEPARATOR, baseObjectFilter::IN_SEPARATOR, $names);
 				
-					$freeTextsArr = explode(baseObjectFilter::IN_SEPARATOR, $freeTexts);
-					foreach($freeTextsArr as $valIndex => $valValue)
+					$namesArr = explode(baseObjectFilter::IN_SEPARATOR, $names);
+					foreach($namesArr as $valIndex => $valValue)
 					{
 						if(!is_numeric($valValue) && strlen($valValue) <= 0)
-							unset($freeTextsArr[$valIndex]);
+							unset($namesArr[$valIndex]);
 						else
-							$freeTextsArr[$valIndex] = SphinxUtils::escapeString($valValue);
+							$namesArr[$valIndex] = SphinxUtils::escapeString($valValue);
 					}
 							
-					foreach($freeTextsArr as $freeText)
+					foreach($namesArr as $name)
 					{
-						$additionalConditions[] = "@(" . kuserFilter::FIRST_NAME_OR_LAST_NAME . ") $freeText\\\*";
+						$additionalConditions[] = "@(" . kuserFilter::FIRST_NAME_OR_LAST_NAME . ") $name\\\*";
 					}
 				}
 				else
 				{
-					$freeTextsArr = explode(baseObjectFilter::AND_SEPARATOR, $freeTexts);
-					foreach($freeTextsArr as $valIndex => $valValue)
+					$namesArr = explode(baseObjectFilter::AND_SEPARATOR, $names);
+					foreach($namesArr as $valIndex => $valValue)
 					{
 						if(!is_numeric($valValue) && strlen($valValue) <= 0)
-							unset($freeTextsArr[$valIndex]);
+							unset($namesArr[$valIndex]);
 						else
-							$freeTextsArr[$valIndex] = SphinxUtils::escapeString($valValue);
+							$namesArr[$valIndex] = SphinxUtils::escapeString($valValue);
 					}
 						
-					$freeTextsArr = array_unique($freeTextsArr);
-					$freeTextExpr = implode(baseObjectFilter::AND_SEPARATOR, $freeTextsArr);
+					$namesArr = array_unique($namesArr);
+					$nameExpr = implode(baseObjectFilter::AND_SEPARATOR, $namesArr);
 					
-					$additionalConditions[] = "@(" . kuserFilter::FIRST_NAME_OR_LAST_NAME . ") $freeTextExpr\\\*";
+					$additionalConditions[] = "@(" . kuserFilter::FIRST_NAME_OR_LAST_NAME . ") $nameExpr\\\*";
 				}
 			}
 			if(count($additionalConditions))
