1d9dfa72294e1eb7a20cc753b6837ed89876dcc3
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-01 18:47:09 +0000

    def: 3720 - older versions of KMC send "true" rather than 1 for boolean values. Change implemented to maintain backward compatibility.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@83911 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/enums/KalturaNullableBoolean.php
+++ api_v3/lib/types/enums/KalturaNullableBoolean.php
@@ -8,11 +8,19 @@ class KalturaNullableBoolean extends KalturaEnum
 	const NULL_VALUE = -1;
 	const FALSE_VALUE = 0;
 	const TRUE_VALUE = 1;
+	const TRUE_VALUE_STRING = 'true';
+	const FALSE_VALUE_STRING = 'false';
 	
 	public static function toBoolean($value)
 	{
 		switch($value)
 		{
+		    case self::TRUE_VALUE_STRING:
+		        return true;
+		    
+		    case self::FALSE_VALUE_STRING:
+		        return false;
+		        
 			case self::FALSE_VALUE:
 				return false;
 				
