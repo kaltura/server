d51528e65552184f80885ea9bbd3aa62cc8574f0
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-02 17:43:54 +0000

    fwr: translation for new columns
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@84032 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- var_console/configs/lang/en.php
+++ var_console/configs/lang/en.php
@@ -841,6 +841,9 @@ return array(
 	'partner-usage table bandwidth' => 'Bandwidth Used',
 	'partner-usage table totalStorage' => 'Total Storage',
 	'partner-usage table storage' => 'Storage Added',
+	'partner-usage table peakStorage' => 'Peak Storage',
+    'partner-usage table averageStorage' => 'Average Storage',
+    'partner-usage table combinedBandwidthStorage' => 'Combined Bandwidth and Storage',
 
 	'partner-usage table partnerStatus tooltip' => 'The status of the Publisher account can be either active or blocked',
 	'partner-usage table partnerName tooltip' => 'The name of the Publisher',
@@ -858,6 +861,9 @@ return array(
 	'partner-usage table bandwidth tooltip' => 'The total bandwidth streaming during the given date range by the specific Publisher account',
 	'partner-usage table totalStorage tooltip' => 'The total storage consumption by the specific Publisher account',
 	'partner-usage table storage tooltip' => 'The amount of new storage consumption (new uploads) during the given date range for the specific Publisher account',
+    'partner-usage table peakStorage tooltip' => 'The peak amount of storage consumption during the given date range for the specific publisher',
+    'partner-usage table averageStorage tooltip' => 'The average amount of storage consumption during the given date range for the specific publisher',
+    'partner-usage table combinedBandwidthStorage tooltip' => 'The combined amount of bandwidth and storage consumed during the given date range for the specific publisher',
 
 
 
