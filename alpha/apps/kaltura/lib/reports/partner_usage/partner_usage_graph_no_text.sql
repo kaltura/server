28031d8edbd37f0618b9b61635181a9cc47d1022
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-03 13:37:49 +0000

    def: 3802 - calculate average by numbers of days in filter time frame instead of number of days in month
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@84093 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/reports/partner_usage/partner_usage_graph_no_text.sql
+++ alpha/apps/kaltura/lib/reports/partner_usage/partner_usage_graph_no_text.sql
@@ -1,12 +1,18 @@
 SELECT
-		{GROUP_COLUMN}, /*partner_id, */
+        IF('{GROUP_COLUMN}' = 'date_id',day_id,t.month_id) AS {GROUP_COLUMN}, 
         SUM(count_bandwidth) AS bandwidth_consumption,
-        IF('{GROUP_COLUMN}' = 'date_id', aggr_storage_mb, SUM(aggr_storage_mb)/DAY(LAST_DAY(DATE(month_id * 100 + 1)))) AS average_storage,
+        IF('{GROUP_COLUMN}' = 'date_id', aggr_storage_mb,
+        IF(FLOOR({FROM_DATE_ID}/100) = FLOOR({TO_DATE_ID}/100), (SUM(aggr_storage_mb)/(DATEDIFF({TO_DATE_ID},{FROM_DATE_ID})+1)), 
+		IF(t.month_id = FLOOR({FROM_DATE_ID}/100),(SUM(aggr_storage_mb)/(DAY(LAST_DAY(DATE(t.month_id * 100 + 1))) - DAY({FROM_DATE_ID}) + 1)),
+        IF(t.month_id = FLOOR({TO_DATE_ID}/100),(SUM(aggr_storage_mb)/DAY({TO_DATE_ID})),(SUM(aggr_storage_mb)/DAY(LAST_DAY(DATE(t.month_id * 100 + 1)))))))) AS average_storage,
         MAX(aggr_storage_mb) AS peak_storage,
         SUM(added_storage_mb) AS added_storage,
-        SUM(count_bandwidth) + IF('{GROUP_COLUMN}' = 'date_id', aggr_storage_mb, SUM(aggr_storage_mb)/DAY(LAST_DAY(DATE(month_id * 100 + 1)))) AS combined_bandwidth_storage
-
-FROM (
+        SUM(count_bandwidth) + 
+        IF('{GROUP_COLUMN}' = 'date_id', aggr_storage_mb,
+        IF(FLOOR({FROM_DATE_ID}/100) = FLOOR({TO_DATE_ID}/100), (SUM(aggr_storage_mb)/(DATEDIFF({TO_DATE_ID},{FROM_DATE_ID})+1)), 
+		IF(t.month_id = FLOOR({FROM_DATE_ID}/100),(SUM(aggr_storage_mb)/(DAY(LAST_DAY(DATE(t.month_id * 100 + 1))) - DAY({FROM_DATE_ID}) + 1)),
+        IF(t.month_id = FLOOR({TO_DATE_ID}/100),(SUM(aggr_storage_mb)/DAY({TO_DATE_ID})),(SUM(aggr_storage_mb)/DAY(LAST_DAY(DATE(t.month_id * 100 + 1)))))))) AS combined_bandwidth_storage
+FROM  kalturadw.dwh_dim_time t LEFT JOIN (
 	SELECT
 		date_id, FLOOR(date_id/100) month_id, partner_id, IFNULL(count_bandwidth_kb, 0)/1024 count_bandwidth, IFNULL(aggr_storage_mb, 0) aggr_storage_mb, IFNULL(count_storage_mb, 0) added_storage_mb
 	FROM
@@ -15,5 +21,9 @@ FROM (
 		{OBJ_ID_CLAUSE}
 		AND
 		date_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}
-        ) raw_data
-GROUP BY {GROUP_COLUMN};
+        ) raw_data ON date_id = day_id
+WHERE day_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}
+GROUP BY IF('{GROUP_COLUMN}' = 'date_id',day_id,t.month_id) 
+LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */ 
+
+
