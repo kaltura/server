3e1f9a559fc21a0a0f4375895c2be9274a409fc2
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-19 08:56:27 +0000

    def: 4195 add action=index
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85127 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- var_console/controllers/PartnerUsageController.php
+++ var_console/controllers/PartnerUsageController.php
@@ -19,6 +19,8 @@ class PartnerUsageController extends Zend_Controller_Action
 		
 		$form->populate($request->getParams());
 		
+		$form->setAction('index');
+		
 		// when no statuses selected
 		if (!$form->getElement('include_active')->getValue() && !$form->getElement('include_blocked')->getValue() && !$form->getElement('include_removed')->getValue())
 		{
