d54efef9889da49f12715449bb3119e7eb732e08
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-23 15:50:22 +0000

    def: 4231 added new flag "filtered"
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85355 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- var_console/forms/PartnerUsagePaginator.php
+++ var_console/forms/PartnerUsagePaginator.php
@@ -5,6 +5,12 @@
  */
 class Form_PartnerUsagePaginator extends Infra_Paginator
 {
+    /**
+     * Flag showing whether the content presented by the paginator is filtered in any way.
+     * @var bool
+     */
+    public $filtered;
+    
     public function getTotal ()
     {
         return $this->_adapter->getTotal();
