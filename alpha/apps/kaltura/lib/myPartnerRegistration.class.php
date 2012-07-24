8cf905c25f2f20fc1b552727f52576680fdcf7f2
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-24 10:08:51 +0000

    fwr: add partner registation timeout on the server side
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85411 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/myPartnerRegistration.class.php
+++ alpha/apps/kaltura/lib/myPartnerRegistration.class.php
@@ -5,7 +5,7 @@ class myPartnerRegistration
 
 	public function __construct( $partnerParentId = null )
 	{
-	    set_time_limit(kConf::get(partner_registration_timeout));
+	    set_time_limit(kConf::get('partner_registration_timeout'));
 		$this->partnerParentId = $partnerParentId;	
 	}
 	
