76c94b1c324c683462bd6fc084f5e3f331ad5550
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-24 14:15:31 +0000

    fwr: fix for alpha code appearing in batch engine
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85454 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/bulk_upload/base/services/BulkService.php
+++ plugins/bulk_upload/base/services/BulkService.php
@@ -26,6 +26,9 @@ class BulkService extends KalturaBaseService
 	 */
 	function addEntriesAction($fileData, KalturaBulkUploadJobData $bulkUploadData = null, KalturaBulkUploadEntryData $bulkUploadEntryData = null)
 	{
+	    if($bulkUploadEntryData->conversionProfileId == self::PARTNER_DEFAULT_CONVERSION_PROFILE_ID)
+			$bulkUploadEntryData->conversionProfileId = $this->getPartner()->getDefaultConversionProfileId();
+	    
 	    if (!$bulkUploadData)
 	    {
 	       $bulkUploadData = KalturaPluginManager::loadObject('KalturaBulkUploadJobData', null);
