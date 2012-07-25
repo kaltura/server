390d5b8bde754b00437c34fa1c914f178d33ab63
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-25 10:45:32 +0000

    def: 4130 add action to count partner's sub-publishers (including partner)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85489 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- var_console/controllers/PartnerController.php
+++ var_console/controllers/PartnerController.php
@@ -84,6 +84,12 @@ class PartnerController extends Zend_Controller_Action
 		
 		$form->setProviders($providers);
 		
+		//If available sub-publisher quota was reached, submit button should be disabled.
+		$submitBtn = $form->getElement('submit');
+        $submitBtn->setOptions(array(
+            'disable' => array(1, 2)
+        ));
+		
 		$this->view->form = $form;
 	}
     
