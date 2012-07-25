f952bf0f1d9f8da0f0482bd58caf31eb54e5d260
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-25 16:44:33 +0000

    def: 4130 add action to count partner's sub-publishers (including partner)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85535 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- var_console/controllers/PartnerController.php
+++ var_console/controllers/PartnerController.php
@@ -100,6 +100,9 @@ class PartnerController extends Zend_Controller_Action
             ));
 		}
 		
+		$this->view->publishersQuota = $currentPartner->publishersQuot;
+		$this->view->remainingPublishers = $currentPartner->publishersQuota - $subPublisherCount->value > 0? $currentPartner->publishersQuota - $subPublisherCount->value : 0;
+		
 		$this->view->form = $form;
 	}
     
