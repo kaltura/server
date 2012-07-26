829d1c0bef02f9eef3b89892f4261df697d043f7
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-26 11:57:50 +0000

    fwr: exit the "authenticate" function instead of throwing an error
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85584 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- ui_infra/Infra/AuthAdapter.php
+++ ui_infra/Infra/AuthAdapter.php
@@ -101,7 +101,7 @@ class Infra_AuthAdapter implements Zend_Auth_Adapter_Interface
     			
     			if (!$authorizedPartnerId)
     			{
-    			    throw new Infra_UIInfraException(self::SYSTEM_USER_INVALID_CREDENTIALS);
+    			    return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID, null);
     			}
     			
     		    $ks = $client->user->loginByLoginId($this->username, $this->password, $authorizedPartnerId);
@@ -112,7 +112,7 @@ class Infra_AuthAdapter implements Zend_Auth_Adapter_Interface
     		
 			
 			if ($partnerId && $user->partnerId != $partnerId) {
-				throw new Infra_UIInfraException(self::SYSTEM_USER_INVALID_CREDENTIALS);
+				return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID, null);
 			}
 			
 			return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS, $identity);
