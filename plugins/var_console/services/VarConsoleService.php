0d7b167ad1e94b25b9fc7b744717685e4c332642
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-27 08:06:35 +0000

    fwr: fix marginals
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85640 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/var_console/services/VarConsoleService.php
+++ plugins/var_console/services/VarConsoleService.php
@@ -228,7 +228,7 @@ class VarConsoleService extends KalturaBaseService
     private function addFiller (array $items, array $partners, KalturaReportInputFilter $usageFilter)
     {
         $format = $usageFilter->interval == KalturaReportInterval::DAYS ? "Ymd" : "Ym";
-        $interval = $format == "Ymd" ? 24*60*60 : 31*24*60*60;
+        $interval = $format == "Ymd" ? 24*60*60 : 30.5*24*60*60;
         for($day = $usageFilter->fromDate; $day < $usageFilter->toDate+48*60*60; $day+=$interval)
         {
             $dayString = date($format, $day);
