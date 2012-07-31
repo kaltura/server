494a7586fd690a76c08a7dec18635c04efea0a98
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-07-31 13:33:26 +0000

    def: 4205
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85838 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- ui_infra/Infra/AuthAdapter.php
+++ ui_infra/Infra/AuthAdapter.php
@@ -78,49 +78,7 @@ class Infra_AuthAdapter implements Zend_Auth_Adapter_Interface
     		$client->setKs($ks);
     		$user = $client->user->getByLoginId($this->username, $partnerId);
     		$identity = new Infra_UserIdentity($user, $ks, $this->timezoneOffset, $partnerId, $this->password);
-			//New logic - if specific permissions are required to authenticate the partner/user, check their existence here.
     		
-    		if (isset($settings->requiredPermissions) && $settings->requiredPermissions)
-    		{
-    		    $requiredPermissionsArr = explode(",", $settings->requiredPermissions);
-    			$userPartners = $client->partner->listPartnersForUser();
-    			
-    			$authorizedPartnerId = null;
-    			
-    			foreach ($userPartners->objects as $userPartner)
-    			{
-    			    $authorizedPartnerId = $userPartner->id;
-    			    foreach ($requiredPermissionsArr as $requiredPermission)
-    			    {
-    			        $permissionFilter = new Kaltura_Client_Type_PermissionFilter();
-    			        $permissionFilter->nameEqual = $requiredPermission;
-    			        $permissionFilter->statusEqual = Kaltura_Client_Enum_PermissionStatus::ACTIVE;
-    			        $permissionFilter->partnerIdEqual = $userPartner->id;
-    			        $permissions = $client->permission->listAction($permissionFilter, new Kaltura_Client_Type_FilterPager());
-    			        if (!$permissions->totalCount)
-    			        {
-    			            $authorizedPartnerId = null;
-    			        }
-    			    }
-    			    
-    			    if ($authorizedPartnerId)
-    			    {
-    			        break;
-    			    }
-    			}
-    			
-    			if (!$authorizedPartnerId)
-    			{
-    			    return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID, null);
-    			}
-    			
-    		    $ks = $client->user->loginByLoginId($this->username, $this->password, $authorizedPartnerId);
-    			$client->setKs($ks);
-    			$user = $client->user->getByLoginId($this->username, $authorizedPartnerId);
-    			$identity = new Infra_UserIdentity($user, $ks, $this->timezoneOffset, $authorizedPartnerId, $this->password);
-    		}
-    		
-			
 			if ($partnerId && $user->partnerId != $partnerId) {
 				return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID, null);
 			}
