1c30d9e2422dcf63344d10fbb654847923513d54
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-08-01 10:33:53 +0000

    qnd: write the services map to a temp file and then rename, to prevent some other apache instance from reading it
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@85889 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/general/kFile.class.php
+++ infra/general/kFile.class.php
@@ -828,6 +828,20 @@ class kFile
 		}
 	}
 
+	public static function safeFilePutContents($filePath, $var)
+	{
+		// write to a temp file and then rename, so that the write will be atomic
+		$tempFilePath = tempnam(dirname($filePath), basename($filePath));
+		if (file_put_contents($tempFilePath, $var) === false)
+			return false;
+		if (rename($tempFilePath, $filePath) === false)
+		{
+			@unlink($tempFilePath);
+			return false;
+		}
+		return true;
+	}
+	
 }
 
 /**
