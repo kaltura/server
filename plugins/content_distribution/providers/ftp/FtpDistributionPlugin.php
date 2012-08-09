2b9c087245f288f38897e628471fbd2602f0f106
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-08-09 16:27:30 +0000

    qnd: Objects loading is not working in API V3 only.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@86230 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/providers/ftp/FtpDistributionPlugin.php
+++ plugins/content_distribution/providers/ftp/FtpDistributionPlugin.php
@@ -101,10 +101,6 @@ class FtpDistributionPlugin extends KalturaPlugin implements IKalturaPermissions
 				return $reflect->newInstanceArgs($constructorArgs);
 			}
 		}
-		
-		// content distribution does not work in partner services 2 context because it uses dynamic enums
-		if (!class_exists('kCurrentContext') || kCurrentContext::$ps_vesion != 'ps3')
-			return null;
 
 		if($baseClass == 'KalturaDistributionJobProviderData' && ($enumValue == self::getDistributionProviderTypeCoreValue(FtpDistributionProviderType::FTP) || $enumValue == self::getDistributionProviderTypeCoreValue(FtpDistributionProviderType::FTP_SCHEDULED)))
 		{
@@ -180,10 +176,6 @@ class FtpDistributionPlugin extends KalturaPlugin implements IKalturaPermissions
 				return 'Kaltura_Client_FtpDistribution_Type_FtpDistributionProfile';
 		}
 		
-		// content distribution does not work in partner services 2 context because it uses dynamic enums
-		if (!class_exists('kCurrentContext') || kCurrentContext::$ps_vesion != 'ps3')
-			return null;
-
 		if($baseClass == 'KalturaDistributionJobProviderData' && ($enumValue == self::getDistributionProviderTypeCoreValue(FtpDistributionProviderType::FTP) || $enumValue == self::getDistributionProviderTypeCoreValue(FtpDistributionProviderType::FTP_SCHEDULED)))
 			return 'KalturaFtpDistributionJobProviderData';
 	
