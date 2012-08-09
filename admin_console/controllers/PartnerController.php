c845fd0bad710f5c50957c8f65bf983a360f366d
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-08-09 14:43:28 +0000

    fwr: add option fror MA login from the admin console
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@86212 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/controllers/PartnerController.php
+++ admin_console/controllers/PartnerController.php
@@ -564,5 +564,34 @@ class PartnerController extends Zend_Controller_Action
 		}
 		$this->view->form = $resetPasswordForm;
 	}
+	
+	public function mpConsoleRedirectAction ()
+	{
+	    $partnerId = $this->_getParam('partner_id');
+		$userId = $this->_getParam('user_id');
+		$client = Infra_ClientHelper::getClient();
+		$systemPartnerPlugin = Kaltura_Client_SystemPartner_Plugin::get($client);
+		try
+		{
+			$ks = $systemPartnerPlugin->systemPartner->getAdminSession($partnerId, $userId);
+		}
+		catch(Exception $e)
+		{
+			$this->view->partnerId = $partnerId;
+			$this->view->errorMessage = $e->getMessage();
+			return;
+		}
+
+		$url = null;
+		$settings = Zend_Registry::get('config')->settings;
+		if($settings->mpConsoleUrl)
+		{
+			$url = Infra_ClientHelper::getServiceUrl();	
+			$url .= $settings->mpConsoleUrl;
+		}
+		
+		$url .= '?ks='.$ks;
+		$this->getResponse()->setRedirect($url);
+	}
 
 }
