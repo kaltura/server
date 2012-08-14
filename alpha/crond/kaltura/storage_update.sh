73dbf289965f79d1de17a301f88191c9cb59d502
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-08-14 10:05:48 +0000

    fwr: run in scripts context using the scripts class auto-loading, logging and caching
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@86391 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/storage_update.sh
+++ alpha/crond/kaltura/storage_update.sh
@@ -9,4 +9,4 @@ fi
 WHEN=$(date -d "yesterday" +%Y-%m-%d)
 
 php $APP_DIR/scripts/findEntriesSizes.php $WHEN >> $LOG_DIR/`hostname`-findEntriesSizes.log
-php $APP_DIR/alpha/batch/batchPartnerUsage.php >> $LOG_DIR/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
+php $APP_DIR/scripts/batch/validatePartnerUsage.php >> $LOG_DIR/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
