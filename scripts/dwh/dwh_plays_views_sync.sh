edd2b30138f5e1854c255ecb24729602627bef08
yulir <yulir@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-08-26 19:47:26 +0000

    fwr: remove win characters
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@86888 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/dwh/dwh_plays_views_sync.sh
+++ scripts/dwh/dwh_plays_views_sync.sh
@@ -6,5 +6,6 @@ else
 fi
 . `dirname $REAL_SCRIPT`/../../configurations/system.ini
 
-mysql -h$DWH_HOST -P$DWH_PORT -u$DWH_USER -p$DWH_PASS < $BASE_DIR/app/scripts/dwh/trigger.sql |sed -e '1d' |php $BASE_DIR/app/scripts/dwh/updateEntryPlaysViews.php 
+mysql -h$DWH_HOST -P$DWH_PORT -u$DWH_USER -p$DWH_PASS < $BASE_DIR/app/scripts/dwh/trigger.sql |sed -e '1d' |php $BASE_DIR/app/scripts/dwh/updateEntryPlaysViews.php
 mysql -h$DWH_HOST -P$DWH_PORT -u$DWH_USER -p$DWH_PASS < $BASE_DIR/app/scripts/dwh/wrap.sql
+
