28c04338c808a64c3891f89be6b5b92e77448869
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-02 05:44:47 +0000

    qnd: typo in method name refreshCurrentUserAllowedPartners
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@87147 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- ui_infra/Infra/AclHelper.php
+++ ui_infra/Infra/AclHelper.php
@@ -6,13 +6,13 @@
 class Infra_AclHelper
 {
 	const ROLE_GUEST = 'guest';
-		
+	
 	/**
 	 * @return string
 	 */
 	public static function getCurrentRole()
 	{
-		if (Infra_AuthHelper::getAuthInstance()->hasIdentity())
+		if(Infra_AuthHelper::getAuthInstance()->hasIdentity())
 		{
 			$roleIds = Infra_AuthHelper::getAuthInstance()->getIdentity()->getUser()->roleIds;
 			
@@ -23,7 +23,7 @@ class Infra_AclHelper
 	
 	public static function getCurrentPermissions()
 	{
-		if (Infra_AuthHelper::getAuthInstance()->hasIdentity())
+		if(Infra_AuthHelper::getAuthInstance()->hasIdentity())
 		{
 			$permissions = Infra_AuthHelper::getAuthInstance()->getIdentity()->getPermissions();
 			return $permissions;
@@ -33,17 +33,17 @@ class Infra_AclHelper
 	
 	/**
 	 * 
-     * @param  Zend_Acl_Resource_Interface|string $resource
-     * @param  string                             $privilege
-     * @return boolean
-     */
+	 * @param  Zend_Acl_Resource_Interface|string $resource
+	 * @param  string                             $privilege
+	 * @return boolean
+	 */
 	public static function isAllowed($resource, $privilege)
 	{
 		if(!$resource)
 			return false;
-			
+		
 		$acl = Zend_Registry::get('acl');
-		if (!$acl->has($resource)) 
+		if(!$acl->has($resource))
 		{
 			KalturaLog::err("Resource [$resource] privilege [$privilege] not found!");
 			return false;
@@ -52,17 +52,20 @@ class Infra_AclHelper
 		return $allowed;
 	}
 	
-	public static function isAllowedPartner($partnerId, $partnerPackage) {
-		if (Infra_AuthHelper::getAuthInstance()->hasIdentity()) 
+	public static function isAllowedPartner($partnerId, $partnerPackage)
+	{
+		if(Infra_AuthHelper::getAuthInstance()->hasIdentity())
 		{
 			$partners = Infra_AuthHelper::getAuthInstance()->getIdentity()->getAllowedPartners();
-			if (in_array('*', $partners)) {
+			if(in_array('*', $partners))
+			{
 				return true;
 			}
 			$packages = Infra_AuthHelper::getAuthInstance()->getIdentity()->getAllowedPartnerPackages();
-			if (in_array($partnerPackage, $packages)) {
+			if(in_array($partnerPackage, $packages))
+			{
 				return true;
-			}			
+			}
 			return in_array((string)$partnerId, $partners);
 		}
 		return false;
@@ -71,26 +74,25 @@ class Infra_AclHelper
 	/**
 	 * Refresh the list of partners the user is allowed to access.
 	 */
-	public static function refreshCurrentUserAllowrdPartners() {
-		if (Infra_AuthHelper::getAuthInstance()->hasIdentity()) 
+	public static function refreshCurrentUserAllowedPartners()
+	{
+		if(Infra_AuthHelper::getAuthInstance()->hasIdentity())
 		{
 			Infra_AuthHelper::getAuthInstance()->getIdentity()->refreshAllowedPartners();
 		}
 	}
 	
-	
-	
 	/**
 	 * 
-     * @param  Zend_Acl_Resource_Interface|string $resource
-     * @param  string                             $privilege
-     * @return boolean
-     */
+	 * @param  Zend_Acl_Resource_Interface|string $resource
+	 * @param  string                             $privilege
+	 * @return boolean
+	 */
 	public static function validateAccess($resource, $privilege)
 	{
-		if (!self::isAllowed($resource, $privilege))
+		if(!self::isAllowed($resource, $privilege))
 		{
-			throw new Exception('Access denied '.$resource.'-'.$privilege);
+			throw new Exception('Access denied ' . $resource . '-' . $privilege);
 		}
 	}
 }
