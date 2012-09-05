40f51a721776a234b720b23fe5d8368742476bd7
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-05 20:35:25 +0000

    qnd: rsync folder content
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@87367 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.sh
+++ generator/generate.sh
@@ -16,11 +16,11 @@ rm -fr ../cache/generator/*
 
 php generate.php "$@"
 
-rsync -avC $output_path/php5ZendClientAdminConsole/Kaltura/Client ../admin_console/lib/Kaltura/Client
-rsync -avC $output_path/php5ZendVarConsole/Kaltura/Client ../var_console/lib/Kaltura/Client
-rsync -avC $output_path/php5ZendHostedPages/Kaltura/Client ../hosted_pages/lib/Kaltura/Client
+rsync -avC $output_path/php5ZendClientAdminConsole/Kaltura/Client/ ../admin_console/lib/Kaltura/Client
+rsync -avC $output_path/php5ZendVarConsole/Kaltura/Client/ ../var_console/lib/Kaltura/Client
+rsync -avC $output_path/php5ZendHostedPages/Kaltura/Client/ ../hosted_pages/lib/Kaltura/Client
 rsync -avC $output_path/batchClient/ ../batch/client
-rsync -avC  $output_path/testsClient/* ../tests/lib
+rsync -avC  $output_path/testsClient/ ../tests/lib
 rsync -avC  $output_path/php5 ../clients
 
 rm -fr ../cache/batch/*
