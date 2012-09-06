66e18e0a0f500ae9f0d28d76c456320554f4dd60
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-06 07:24:03 +0000

    qnd: added reports controller
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@87384 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ hosted_pages/controllers/ReportsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package HostedPages
+ * @subpackage Reports
+ */
+class ReportsController extends Zend_Controller_Action
+{
+    /**
+     * Used to show index of all pages
+     */
+    public function indexAction()
+    {
+    }
+}
+
