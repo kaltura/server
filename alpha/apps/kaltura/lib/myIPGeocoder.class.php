e4f5da17cead60918316d2c1025642e32f27e4d1
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-13 12:32:36 +0000

    qnd: move Ip2Location to infra, in order to avoid infra->alpha dependencies
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@87646 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/myIPGeocoder.class.php
+++ alpha/apps/kaltura/lib/myIPGeocoder.class.php
@@ -1,8 +1,5 @@
 <?php
-require_once(realpath(dirname(__FILE__)).'/../../../config/sfrootdir.php');
 require_once(dirname(__FILE__) . '/access_control/kGeoCoder.php');
-
-@include("IP2Location.inc.php");
 
 class myIPGeocoder extends kGeoCoder
 {
@@ -16,40 +13,11 @@ class myIPGeocoder extends kGeoCoder
 	
 	function iptocountry($ip) 
 	{   
-		$ip2LocationBinFilePath = SF_ROOT_DIR.'/../../data'."/geoip/IP-COUNTRY-ISP.BIN";
-		if (function_exists("IP2Location_open") && file_exists($ip2LocationBinFilePath))
-		{
-			$ip2Location = IP2Location_open($ip2LocationBinFilePath, IP2LOCATION_STANDARD);
-			$record = IP2Location_get_country_short($ip2Location, $ip);
-			IP2Location_close($ip2Location);
-			return $record ? $record->country_short : "";
-		}
-		
-		ini_set('memory_limit', '128M'); // ip_files are large array files, sometimes it might break if doesn't have enough memory
-		$country = "";
-		$numbers = preg_split( "/\./", $ip);   
-		include("ip_files/".$numbers[0].".php");
-		$code=($numbers[0] * 16777216) + ($numbers[1] * 65536) + ($numbers[2] * 256) + ($numbers[3]);   
-		foreach($ranges as $key => $value){
-	        if($key<=$code){
-	            if($ranges[$key][0]>=$code){$country=$ranges[$key][1];break;}
-	            }
-	    }
-	    return $country;
+		return kIP2Location::ipToCountry($ip);
 	}
 	
 	function iptocountryAndCode($ip) 
-	{   
-		$country = "";
-	    $numbers = preg_split( "/\./", $ip);   
-	    include("ip_files/".$numbers[0].".php");
-	    $code=($numbers[0] * 16777216) + ($numbers[1] * 65536) + ($numbers[2] * 256) + ($numbers[3]);   
-	    foreach($ranges as $key => $value){
-	        if($key<=$code){
-	            if($ranges[$key][0]>=$code){$country=$ranges[$key][1];break;}
-	            }
-	    }
-	    return array ( $country , $code );
-	}	
+	{
+		return kIP2Location::ipToCountryAndCode($ip);
+	}
 }
-?>
