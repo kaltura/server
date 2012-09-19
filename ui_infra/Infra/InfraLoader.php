d2d97542568a5fcff1473d960002ca92e80ae8b2
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-19 07:27:47 +0000

    qnd: move server infrastructures
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@87764 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- ui_infra/Infra/InfraLoader.php
+++ ui_infra/Infra/InfraLoader.php
@@ -10,7 +10,6 @@ class Infra_InfraLoader implements Zend_Loader_Autoloader_Interface
 		$infaDir = realpath(dirname(__FILE__) . '/../../infra/');
 		$pluginsDir = realpath(dirname(__FILE__) . '/../../plugins/');
 		
-		require_once($infaDir . DIRECTORY_SEPARATOR . 'bootstrap_base.php');
 		require_once($infaDir . DIRECTORY_SEPARATOR . 'KAutoloader.php');
 		KAutoloader::setClassPath(array($infaDir . DIRECTORY_SEPARATOR . '*'));
 		KAutoloader::addClassPath(KAutoloader::buildPath($pluginsDir, '*'));
