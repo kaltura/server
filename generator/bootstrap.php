dd4c94ed1b22addafee31575f29a610d4e7cae06
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-19 15:26:00 +0000

    qnd: enable running the generator from any path
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@87810 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/bootstrap.php
+++ generator/bootstrap.php
@@ -1,14 +1,16 @@
 <?php
-require_once("..".DIRECTORY_SEPARATOR."server_infra".DIRECTORY_SEPARATOR."kConf.php");
+chdir(__DIR__);
 
-define("KALTURA_API_PATH", KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."api_v3");
-define("KALTURA_PLUGIN_PATH", KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."plugins");
-define("KALTURA_GENERATOR_PATH", KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."generator");
+define("KALTURA_ROOT_PATH", realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR));
+require_once(KALTURA_ROOT_PATH . DIRECTORY_SEPARATOR . "server_infra" . DIRECTORY_SEPARATOR . "kConf.php");
 
-require_once(KALTURA_API_PATH.DIRECTORY_SEPARATOR.'VERSION.php'); //defines KALTURA_API_VERSION
+define("KALTURA_API_PATH", KALTURA_ROOT_PATH . DIRECTORY_SEPARATOR . "api_v3");
+define("KALTURA_PLUGIN_PATH", KALTURA_ROOT_PATH . DIRECTORY_SEPARATOR . "plugins");
+
+require_once(KALTURA_API_PATH . DIRECTORY_SEPARATOR . 'VERSION.php'); //defines KALTURA_API_VERSION
 
 // Autoloader
-require_once(KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."infra".DIRECTORY_SEPARATOR."KAutoloader.php");
+require_once(KALTURA_ROOT_PATH . DIRECTORY_SEPARATOR . "infra" . DIRECTORY_SEPARATOR . "KAutoloader.php");
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_API_PATH, "lib", "*"));
 KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_API_PATH, "services", "*"));
@@ -24,7 +26,7 @@ date_default_timezone_set(kConf::get("date_default_timezone")); // America/New_Y
 
 
 // Logger
-$loggerConfigPath = KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR.'configurations'.DIRECTORY_SEPARATOR."logger.ini";
+$loggerConfigPath = KALTURA_ROOT_PATH . DIRECTORY_SEPARATOR . 'configurations' . DIRECTORY_SEPARATOR . "logger.ini";
 
 try // we don't want to fail when logger is not configured right
 {
