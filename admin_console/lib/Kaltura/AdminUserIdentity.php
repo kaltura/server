9b27319026f3a892d099cb1e3efdccfc1150e656
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-23 18:07:25 +0000

    qnd: move partners permissions to identity class
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@88015 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/lib/Kaltura/AdminUserIdentity.php
+++ admin_console/lib/Kaltura/AdminUserIdentity.php
@@ -5,4 +5,63 @@
  */
 class Kaltura_AdminUserIdentity extends Infra_UserIdentity
 {
+	/**
+	 * Current user partners
+	 * @var array<string>
+	 */
+	private $partners = null;
+	
+	/**
+	@var array<string>
+	 */
+	private $partnerPackages = null;
+	
+	/**
+	 * @return array<string>
+	 */
+	public function getAllowedPartners()
+	{
+		if(is_null($this->partners))
+			$this->initPartners();
+			
+		return $this->partners;
+	}
+	
+	/**
+	 * @return array<string>
+	 */
+	public function getAllowedPartnerPackages()
+	{
+		if(is_null($this->partnerPackages))
+			$this->initPartnerPackages();
+			
+		return $this->partnerPackages;
+	}
+	
+	/**
+	 * Calculates the allowed partner packages
+	 */
+	private function initPartnerPackages()
+	{
+		$this->partnerPackages = array_map('trim', explode(',', $this->user->allowedPartnerPackages));
+	}
+	
+	/**
+	 * Calculates the allowed partners list
+	 */
+	private function initPartners()
+	{
+		$this->partners = array_map('trim', explode(',', $this->user->allowedPartnerIds));
+	}
+	
+	/**
+	 * Reloads the allowed partners list from the server
+	 */
+	public function refreshAllowedPartners()
+	{
+		$client = Infra_ClientHelper::getClient();
+		$user = $client->user->get();
+		$userPartners = array_map('trim', explode(',', $user->allowedPartnerIds));
+		$this->partners = $userPartners;
+	}
 }
