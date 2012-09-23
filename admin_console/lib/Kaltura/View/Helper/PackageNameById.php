6caa5e3cc52fc274624a45a805da3a3154b849c2
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-23 06:07:50 +0000

    qnd: implement the package loading by itself, no need for the helper.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@87936 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/lib/Kaltura/View/Helper/PackageNameById.php
+++ admin_console/lib/Kaltura/View/Helper/PackageNameById.php
@@ -5,8 +5,28 @@
  */
 class Kaltura_View_Helper_PackageNameById extends Zend_View_Helper_Abstract
 {
+	private static $packages = null;
+	
 	public function packageNameById($id)
 	{
-		return Infra_PartnerPackageHelper::getPackageNameById($id);
+		self::loadPackges();
+		foreach(self::$packages as $package)
+		{
+			if ($package->id == $id)
+			{
+				return $package->name;
+			}
+		}
+		return "N/A"; 
+	}
+	
+	private static function loadPackges()
+	{
+		if (is_null(self::$packages))
+		{
+			$client = Infra_ClientHelper::getClient();
+			$systemPartnerPlugin = Kaltura_Client_SystemPartner_Plugin::get($client);
+			self::$packages = $systemPartnerPlugin->systemPartner->getPackages();
+		}
 	}
 }
