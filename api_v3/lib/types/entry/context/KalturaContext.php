d79698c8a8496c40aa9f3c37705d998af5f3c525
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-25 17:45:37 +0000

    def: 3946 added playlist context feature for the implementation of the related playlist
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@88091 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ api_v3/lib/types/entry/context/KalturaContext.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+abstract class KalturaContext extends KalturaObject
+{
+    /**
+     * Function to validate the context.
+     */
+    abstract function validate ();
+}
---
+++ api_v3/lib/types/entry/context/KalturaEntryContext.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaEntryContext extends KalturaContext
+{
+    /**
+     * The entry ID in the context of which the playlist should be built
+     * @var string
+     */
+    public $entryId;
+    
+    /* (non-PHPdoc)
+     * @see KalturaPlaylistContext::validate()
+     */
+    public function validate ()
+    {
+        //Validate the provided entryId belongs to the partner and that it is a valid entry (status READY, etc)
+        $c = KalturaCriteria::create('entry');
+        $c->addAnd(entryPeer::ENTRY_ID, $this->entryId);
+        $entryCount = entryPeer::doCount($c);
+        if (!$entryCount)
+            throw new KalturaAPIException(KalturaErrors::ENTRY_ID_NOT_FOUND, $this->entryId);
+        
+    }
+    
+    /* (non-PHPdoc)
+     * @see KalturaObject::toObject($object_to_fill, $props_to_skip)
+     */
+    public function toObject($dbObject = null, $skip = array())
+    {
+        $this->validate();
+        if (!$dbObject)
+        {
+            $dbObject = new kEntryContext();
+        }
+        
+        parent::toObject($dbObject);
+        $dbObject->setEntry(entryPeer::retrieveByPK($this->entryId));
+    }
+}
