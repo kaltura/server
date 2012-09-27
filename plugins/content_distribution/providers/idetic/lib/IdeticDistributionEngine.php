29db162f9d8b74217b1151cda9ae8f1fe27c947b
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-09-27 13:38:13 +0000

    qnd: remoed the need for escaping
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@88137 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/providers/idetic/lib/IdeticDistributionEngine.php
+++ plugins/content_distribution/providers/idetic/lib/IdeticDistributionEngine.php
@@ -236,7 +236,7 @@ class IdeticDistributionEngine extends DistributionEngine implements
 			}
 			else if (preg_match ( "/{$data->remoteId}.*.err/" , $fileArray[$i] , $matches))
 			{
-				$res = preg_split ("/\./", $matches[0]);
+				$res = preg_split ('/\./', $matches[0]);
 				return $res[1];			
 			}
 		}
