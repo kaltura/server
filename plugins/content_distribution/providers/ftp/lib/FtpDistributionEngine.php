522bc9935c0f350dd4a06bd759af83f996a0bf22
assafb <assafb@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-02 16:45:09 +0000

    ps:P-53307. changing if-else order.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@88233 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/providers/ftp/lib/FtpDistributionEngine.php
+++ plugins/content_distribution/providers/ftp/lib/FtpDistributionEngine.php
@@ -255,13 +255,15 @@ class FtpDistributionEngine extends DistributionEngine implements
 		$protocol = $distributionProfile->protocol;
 		$username = $distributionProfile->username;
 		$password = $distributionProfile->password;
-		if ($protocol == KalturaDistributionProtocol::SFTP || $protocol == KalturaDistributionProtocol::SFTP_CMD || $protocol == KalturaDistributionProtocol::SFTP_SEC_LIB){
-       		$publicKey = $distributionProfile->sftpPublicKey ;
-        	$privateKey = $distributionProfile->sftpPrivateKey ;
+		if ($protocol == KalturaDistributionProtocol::ASPERA)
+		{
+			$publicKey = $distributionProfile->asperaPublicKey;
+        	$privateKey = $distributionProfile->asperaPrivateKey;
 		}
-		else if ($protocol == KalturaDistributionProtocol::ASPERA){
-			$publicKey = $distributionProfile->asperaPublicKey ;
-        	$privateKey = $distributionProfile->asperaPrivateKey ;
+		else 
+		{
+			$publicKey = $distributionProfile->sftpPublicKey;
+        	$privateKey = $distributionProfile->sftpPrivateKey;
 		}
         
         $passphrase = $distributionProfile->passphrase ? $distributionProfile->passphrase : null;
