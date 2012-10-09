c5cbd7f1e4e7258fb04c4863307d221bfd9dac8c
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-09 13:07:51 +0000

    qnd: enable reloading the translation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@88428 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- ui_infra/Infra/TranslateAdapter.php
+++ ui_infra/Infra/TranslateAdapter.php
@@ -6,7 +6,31 @@
  */
 class Infra_TranslateAdapter extends Zend_Translate_Adapter_Array
 {
-    protected function _loadTranslationData($data, $locale, array $options = array())
+    public function __construct($data, $locale = null, array $options = array())
+    {
+        parent::__construct($data, $locale, $options);
+        
+        Infra_AuthHelper::registerNamespaceChangedCallback(array($this, 'reload'), 'Infra_TranslateAdapter::reload');
+    }
+	
+	public function reload()
+    {
+    	$locale = $this->getLocale();
+    	if($locale instanceof Zend_Locale)
+    		$locale = "$locale";
+    		
+		$pluginInstances = KalturaPluginManager::getPluginInstances('IKalturaApplicationTranslations');
+		foreach($pluginInstances as $pluginInstance)
+		{
+			/* @var $pluginInstance IKalturaApplicationTranslations */
+			KalturaLog::debug("Loading plugin[" . $pluginInstance->getPluginName() . "]");
+			$translations =  $pluginInstance->getTranslations($locale);
+			foreach($translations[$locale] as $key => $value)
+				$this->_translate[$locale][$key] = $value;
+		}
+    }
+	
+	protected function _loadTranslationData($data, $locale, array $options = array())
     {
         $translate = parent::_loadTranslationData($data, $locale, $options);
         
