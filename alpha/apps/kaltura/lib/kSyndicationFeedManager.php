ce2ed3e20b5eab9fcc194d36a7602844b2453b57
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-23 12:05:53 +0000

    fwr: replace use of KalturaAPIException
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89043 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/kSyndicationFeedManager.php
+++ alpha/apps/kaltura/lib/kSyndicationFeedManager.php
@@ -259,8 +259,8 @@ class kSyndicationFeedManager
 		$xsl = new DOMDocument();
 		if(!@$xsl->loadXML($xsltStr))
 		{
-			KalturaLog::debug("Could not load xslt");
-			throw new KalturaAPIException(KalturaErrors::INVALID_XSLT, $xsltStr);
+			KalturaLog::err("Invalid XSLT structure");
+			throw new kCoreException("Invalid XSLT", kCoreException::INVALID_XSLT);
 		}
 		
 		$xpath = new DOMXpath($xsl);
@@ -268,15 +268,15 @@ class kSyndicationFeedManager
 		$xslStylesheet = $xpath->query("//xsl:stylesheet");
 		$rss = $xpath->query("//xsl:template[@name='rss']");
 		if ($rss->length == 0)
-			throw new KalturaAPIException(KalturaErrors::INVALID_XSLT_MISSING_TEMPLATE_RSS, $xsltStr);
+		    throw new kCoreException("Invalid XSLT structure - missing template rss", kCoreException::INVALID_XSLT);
 		
 		$item = $xpath->query("//xsl:template[@name='item']");
 		if ($item->length == 0)
-			throw new KalturaAPIException(KalturaErrors::INVALID_XSLT_MISSING_TEMPLATE_ITEM, $xsltStr);
+		    throw new kCoreException("Invalid XSLT structure - missing template item", kCoreException::INVALID_XSLT);
 		
 		$items = $xpath->query("//xsl:apply-templates[@name='item']"); 
 		if ($items->length == 0)
-			throw new KalturaAPIException(KalturaErrors::INVALID_XSLT_MISSING_APPLY_TEMPLATES_ITEM, $xsltStr);
+		    throw new kCoreException("Invalid XSLT structure - missing template apply-templates item", kCoreException::INVALID_XSLT);
 
 		return true;
 	}
