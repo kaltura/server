017a8d6eec34fab6a1b95e358a72615604ab9f8f
rotema <rotema@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-28 15:38:14 +0000

    qnd: removed assetParamsPeer::setIsDefaultInDefaultCriteria(false); for list action. as it caused flavorParams.list to return flavors which are not marked as is_default from partner 0 as well.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89288 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/services/FlavorParamsService.php
+++ api_v3/services/FlavorParamsService.php
@@ -20,9 +20,6 @@ class FlavorParamsService extends KalturaBaseService
 		parent::applyPartnerFilterForClass(new assetPeer());
 		parent::applyPartnerFilterForClass(new assetParamsOutputPeer());
 		
-		if($actionName == 'list')
-			assetParamsPeer::setIsDefaultInDefaultCriteria(false);
-			
 		$partnerGroup = null;
 		if(
 			$actionName == 'get' ||
