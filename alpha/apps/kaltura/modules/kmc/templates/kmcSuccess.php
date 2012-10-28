ce3ace143f31f93b71b0f6d8080bc236c52bc238
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-28 09:06:17 +0000

    qnd: removed unused code. moved login logic to kmc.login.js
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89243 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/templates/kmcSuccess.php
+++ alpha/apps/kaltura/modules/kmc/templates/kmcSuccess.php
@@ -12,23 +12,12 @@ else {
 
 $flash_dir .= $www_host .'/'. myContentStorage::getFSFlashRootPath ();
 
-$beta_str = $beta ? "/beta/{$beta}" : "";
 ?>
-<script type="text/javascript">var service_url = "<?php echo $service_url ?>";</script>
-<script type="text/javascript" src="/lib/js/kmc.login.js"></script>
 
 <style>
  body { background-color:#272929 !important; background-image:none !important;}
   div#login { width:500px; margin: 0 auto; text-align:center;}
 </style>
-
-<form id="form1" action="<?php echo $service_url ?>/index.php/kmc/kmc2<?php echo $beta_str ?>" method="post">
-	<input type="hidden" name="_partner_id">
-	<input type="hidden" name="_subp_id">
-	<input type="hidden" name="_uid">
-	<input type="hidden" name="_ks">
-</form>	
-
 
 <div id="kmcHeader">
 	<img src="<?php echo $service_url; ?>/lib/images/kmc/logo_kmc.png" alt="Kaltura CMS" />
@@ -42,36 +31,17 @@ $beta_str = $beta ? "/beta/{$beta}" : "";
 </div>
 
 <script type="text/javascript">
-// attempt to login without params - see if there are cookies - the remMe is true so the expiry will continue 
-if ( !loginF ( null , null , null , null , true ) ) {
-	var flashVars = {
-		<?php
-			if (isset($setPassHashKey) && $setPassHashKey) {
-				echo 'hashKey: "'.$setPassHashKey.'",';
-			}
-		?>
-		<?php
-			if (isset($hashKeyErrorCode) && $hashKeyErrorCode) {
-				echo 'errorCode: "'.$hashKeyErrorCode.'",';
-			}
-		?>
-		loginF: "loginF",
-		closeF: "closeLoginF",
+var options = {
+	service_url: "<?php echo $service_url ?>",
+	kmc_login_version: "<?php echo $kmc_login_version ?>",
+	flash_dir: "<?php echo $flash_dir ?>",
+	flashVars: {
 		host: "<?php echo $www_host; ?>",
 		displayErrorFromServer: "<?php echo ($displayErrorFromServer)? 'true': 'false'; ?>",
 		visibleSignup: "<?php echo (kConf::get('kmc_login_show_signup_link'))? 'true': 'false'; ?>",
-		urchinNumber: "UA-12055206-1",
-		srvurl: "api_v3/index.php"
+		hashKey: "<?php echo (isset($setPassHashKey) && $setPassHashKey) ? $setPassHashKey : ''; ?>",
+		errorCode: "<?php echo (isset($hashKeyErrorCode) && $hashKeyErrorCode) ? $hashKeyErrorCode : ''; ?>"
 	}
-
-	var params = {
-		allowscriptaccess: "always",
-		allownetworking: "all",
-		bgcolor: "#272929",
-		quality: "high",
-		wmode: "window" ,
-		movie: "<?php echo $flash_dir ?>/kmc/login/<?php echo $kmc_login_version ?>/login.swf"
-	};
-	swfobject.embedSWF("<?php echo $flash_dir ?>/kmc/login/<?php echo $kmc_login_version ?>/login.swf", "login_swf", "384", "350", "10.0.0", "<?php echo $service_url ?>/expressInstall.swf", flashVars, params);
-}
+};
 </script>
+<script type="text/javascript" src="/lib/js/kmc.login.js"></script>
