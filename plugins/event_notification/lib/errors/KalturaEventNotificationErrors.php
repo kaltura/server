b8d19e24a0ac891b893719f6ffa42a82381f02a5
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-29 14:02:30 +0000

    fwr: new error - duplicate system name
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89325 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/event_notification/lib/errors/KalturaEventNotificationErrors.php
+++ plugins/event_notification/lib/errors/KalturaEventNotificationErrors.php
@@ -11,4 +11,6 @@ class KalturaEventNotificationErrors extends KalturaErrors
 	const EVENT_NOTIFICATION_DISPATCH_DISABLED = "EVENT_NOTIFICATION_DISPATCH_DISABLED,Dispatching event notification template id [%s] is not allowed";
 	
 	const EVENT_NOTIFICATION_DISPATCH_FAILED = "EVENT_NOTIFICATION_DISPATCH_FAILED,Dispatching event notification template id [%s] failed";
+    
+    const EVENT_NOTIFICATION_TEMPLATE_DUPLICATE_SYSTEM_NAME = "EVENT_NOTIFICATION_TEMPLATE_DUPLICATE_SYSTEM_NAME,Event notification template with system name [%s] already exists.";
 }
