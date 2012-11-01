20b95a0fd219d64af98899babc7aa8ae2b06d137
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-01 06:23:32 +0000

    qnd: added EVENT_NOTIFICATION_WRONG_TYPE
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89470 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/event_notification/lib/errors/KalturaEventNotificationErrors.php
+++ plugins/event_notification/lib/errors/KalturaEventNotificationErrors.php
@@ -7,6 +7,8 @@
 class KalturaEventNotificationErrors extends KalturaErrors
 {
 	const EVENT_NOTIFICATION_TEMPLATE_NOT_FOUND = "EVENT_NOTIFICATION_TEMPLATE_NOT_FOUND,Event notification template id [%s] not found";
+
+	const EVENT_NOTIFICATION_WRONG_TYPE = "EVENT_NOTIFICATION_WRONG_TYPE,Event notification template id [%s] is of type [%s]";
 	
 	const EVENT_NOTIFICATION_DISPATCH_DISABLED = "EVENT_NOTIFICATION_DISPATCH_DISABLED,Dispatching event notification template id [%s] is not allowed";
 	
