0ae177fd232dbc3cf37d58246da5051bcf729f94
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-04 19:03:25 +0000

    fwr: categoryKuser sphinx population script
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89622 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/base/scripts/populateSphinxCategoryKusers.php
@@ -0,0 +1,46 @@
+<?php
+chdir(dirname(__FILE__));
+
+require_once('../../bootstrap.php');
+
+
+$c = new Criteria();
+
+if($argc > 1 && is_numeric($argv[1]))
+	$c->add(categoryKuserPeer::UPDATED_AT, $argv[1], Criteria::GREATER_EQUAL);
+if($argc > 2 && is_numeric($argv[2]))
+	$c->add(categoryKuserPeer::PARTNER_ID, $argv[2], Criteria::EQUAL);
+if($argc > 3 && is_numeric($argv[3]))
+	$c->add(categoryKuserPeer::ID, $argv[3], Criteria::GREATER_EQUAL);
+
+$c->addAscendingOrderByColumn(categoryKuserPeer::UPDATED_AT);
+$c->addAscendingOrderByColumn(categoryKuserPeer::ID);
+$c->setLimit(10000);
+
+$con = myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_PROPEL2);
+//$sphinxCon = DbManager::getSphinxConnection();
+
+$categoryKusers = categoryKuserPeer::doSelect($c, $con);
+$sphinx = new kSphinxSearchManager();
+while(count($categoryKusers))
+{
+	foreach($categoryKusers as $categoryKuser)
+	{
+	    /* @var $categoryKuser categoryKuser */
+		KalturaLog::log('$categoryKuser id ' . $categoryKuser->getId() . ' updated at '. $categoryKuser->getUpdatedAt(null));
+		
+		try {
+			$ret = $sphinx->saveToSphinx($categoryKuser, true);
+		}
+		catch(Exception $e){
+			KalturaLog::err($e->getMessage());
+			exit -1;
+		}
+	}
+	
+	$c->setOffset($c->getOffset() + count($categoryKusers));
+	kMemoryManager::clearMemory();
+	$entries = categoryKuserPeer::doSelect($c, $con);
+}
+
+KalturaLog::log('Done. Current time: ' . time());
