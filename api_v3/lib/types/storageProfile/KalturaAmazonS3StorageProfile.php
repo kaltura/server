16f1c26574247db268509052c6603448aa225af2
rotema <rotema@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-05 13:01:20 +0000

    qnd: qc 4750 commented out toObject and added toInsertableObject
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89652 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/storageProfile/KalturaAmazonS3StorageProfile.php
+++ api_v3/lib/types/storageProfile/KalturaAmazonS3StorageProfile.php
@@ -20,7 +20,7 @@ class KalturaAmazonS3StorageProfile extends KalturaStorageProfile
 		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
 	}
 
-	public function toObject ($object_to_fill = null , $props_to_skip = array() )
+	/*public function toObject ($object_to_fill = null , $props_to_skip = array() )
 	{
 		if(is_null($object_to_fill))
 			$object_to_fill = new AmazonS3StorageProfile();
@@ -28,6 +28,15 @@ class KalturaAmazonS3StorageProfile extends KalturaStorageProfile
 		$object_to_fill =  parent::toObject($object_to_fill, $props_to_skip);
 				
 		return $object_to_fill;
+	}*/
+	
+	public function toInsertableObject ( $object_to_fill = null , $props_to_skip = array() )
+	{
+		if(is_null($object_to_fill)){
+			$object_to_fill = new AmazonS3StorageProfile();
+		}
+		
+		return parent::toInsertableObject($object_to_fill, $props_to_skip);
 	}
 		
 }
