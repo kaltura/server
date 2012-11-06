254ef86bd2388c91e0325f8c5f1d48439451909d
rotema <rotema@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-06 13:39:28 +0000

    qnd: qc 4750 removed toObject
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89748 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/storageProfile/KalturaAmazonS3StorageProfile.php
+++ api_v3/lib/types/storageProfile/KalturaAmazonS3StorageProfile.php
@@ -19,16 +19,6 @@ class KalturaAmazonS3StorageProfile extends KalturaStorageProfile
 	{
 		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
 	}
-
-	/*public function toObject ($object_to_fill = null , $props_to_skip = array() )
-	{
-		if(is_null($object_to_fill))
-			$object_to_fill = new AmazonS3StorageProfile();
-				
-		$object_to_fill =  parent::toObject($object_to_fill, $props_to_skip);
-				
-		return $object_to_fill;
-	}*/
 	
 	public function toInsertableObject ( $object_to_fill = null , $props_to_skip = array() )
 	{
