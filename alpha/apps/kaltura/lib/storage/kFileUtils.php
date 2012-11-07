2121c566e75ca822cbe0fb1b4b5781d7d9537ec1
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-07 13:26:09 +0000

    fwr: fix setopt
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@89825 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/storage/kFileUtils.php
+++ alpha/apps/kaltura/lib/storage/kFileUtils.php
@@ -208,10 +208,10 @@ class kFileUtils extends kFile
 		else
 		{
 			// Set callback function for body
-			curl_setopt($ch, CURLOPT_WRITEFUNCTION, 'kFileBase::read_body');
+			curl_setopt($ch, CURLOPT_WRITEFUNCTION, 'kFileUtils::read_body');
 		}
 		// Set callback function for headers
-		curl_setopt($ch, CURLOPT_HEADERFUNCTION, 'kFileBase::read_header');
+		curl_setopt($ch, CURLOPT_HEADERFUNCTION, 'kFileUtils::read_header');
 		
 		//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 		
