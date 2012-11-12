0c1ef56ea6fad18a353778400e2926958e97f8fb
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-12 16:44:27 +0000

    qnd: enable batch job status
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90123 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/event_notification/admin/forms/EventNotificationTemplateConfiguration.php
+++ plugins/event_notification/admin/forms/EventNotificationTemplateConfiguration.php
@@ -211,15 +211,11 @@ abstract class Form_EventNotificationTemplateConfiguration extends Infra_Form
 			'value'			=> $this->templateType,
 		));
 	
-		$excludes = array(Kaltura_Client_EventNotification_Enum_EventNotificationEventType::BATCH_JOB_STATUS);
 		$element = $this->getElement('event_type');
 		$reflect = new ReflectionClass('Kaltura_Client_EventNotification_Enum_EventNotificationEventType');
 		$types = $reflect->getConstants();
 		foreach($types as $constName => $value)
 		{
-			if(in_array($value, $excludes))
-				continue;
-				
 			$name = ucfirst(str_replace('_', ' ', strtolower($constName)));
 			$element->addMultiOption($value, $name);
 		}
