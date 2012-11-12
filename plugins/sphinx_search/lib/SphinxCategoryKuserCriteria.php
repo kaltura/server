eb73c789d4c5f757144849e1b6f5de4faa923128
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-12 08:49:34 +0000

    fwr: changes to index
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90019 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/lib/SphinxCategoryKuserCriteria.php
+++ plugins/sphinx_search/lib/SphinxCategoryKuserCriteria.php
@@ -12,7 +12,6 @@ class SphinxCategoryKuserCriteria extends SphinxCriteria
         categoryKuserPeer::SCREEN_NAME => "screen_name" ,
         categoryKuserPeer::PUSER_ID => "puser_id" ,
         categoryKuserPeer::CATEGORY_FULL_IDS => "category_full_ids" ,
-        categoryKuserPeer::INHERIT_FROM_CATEGORY => "inherit_from_category" ,
         categoryKuserPeer::UPDATE_METHOD => "update_method" ,
         categoryKuserPeer::PERMISSION_NAMES => "permission_names" ,
         categoryKuserPeer::PARTNER_ID => "partner_id" ,
@@ -27,16 +26,15 @@ class SphinxCategoryKuserCriteria extends SphinxCriteria
 	);
 	
 	public static $sphinxTypes = array ( 
-        		"category_id" => IIndexable::FIELD_TYPE_INTEGER,
-                "kuser_id" => IIndexable::FIELD_TYPE_INTEGER,
+        		"category_id" => IIndexable::FIELD_TYPE_STRING,
+                "kuser_id" => IIndexable::FIELD_TYPE_STRING,
                 "screen_name" => IIndexable::FIELD_TYPE_STRING,
                 "puser_id" => IIndexable::FIELD_TYPE_STRING,
 				"category_full_ids" => IIndexable::FIELD_TYPE_STRING,
-				"inherit_from_category" => IIndexable::FIELD_TYPE_INTEGER,
-				"update_method" => IIndexable::FIELD_TYPE_INTEGER,
+				"update_method" => IIndexable::FIELD_TYPE_STRING,
 				"permission_names" => IIndexable::FIELD_TYPE_STRING,
-				"partner_id" => IIndexable::FIELD_TYPE_INTEGER,
-				"status" => IIndexable::FIELD_TYPE_INTEGER,
+				"partner_id" => IIndexable::FIELD_TYPE_STRING,
+				"status" => IIndexable::FIELD_TYPE_STRING,
 				"created_at" => IIndexable::FIELD_TYPE_DATETIME,
 				"updated_at" => IIndexable::FIELD_TYPE_DATETIME,
         );
@@ -109,10 +107,14 @@ class SphinxCategoryKuserCriteria extends SphinxCriteria
 	 */
 	public function hasMatchableField($fieldName) {
 		return in_array($fieldName, array(
+			"category_id",
+			"kuser_id",
 			"screen_name",
 			"puser_id",
 			"category_full_ids",
 			"permission_names",
+			"status",
+			"update_method",
 		));
 		
 	}
@@ -140,5 +142,47 @@ class SphinxCategoryKuserCriteria extends SphinxCriteria
 		return categoryKuserPeer::doCount($c);
 	}
 
+	/* (non-PHPdoc)
+	 * @see SphinxCriteria::applyFilterFields()
+	 */
+	protected function applyFilterFields($filter)
+	{
+		if ($filter->get('_like_permission_names'))
+		{
+			
+		}
+		
+		if ($filter->get('_in_permission_names'))
+		{
+			
+		}
+		
+		if ($filter->get('_in_status'))
+		{
+			
+		}
+		
+		if ($filter->get('_eq_status'))
+		{
+			
+		}
+		
+		if ($filter->get('_in_update_method'))
+		{
+			
+		}
+		
+		if ($filter->get('_eq_update_method'))
+		{
+			
+		}
+		
+		return parent::applyFilterFields($filter);
+	}
+	
+	public static function getIndexFieldPrefix ()
+	{
+		
+	}
 	
 }
