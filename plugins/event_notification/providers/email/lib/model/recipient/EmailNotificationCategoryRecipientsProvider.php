22866b2a1d558e3638252d8bf60c0225574a7e96
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-13 10:31:54 +0000

    fwr: new classes related to the change in the email recipient logic
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90157 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/event_notification/providers/email/lib/model/recipient/EmailNotificationCategoryRecipientsProvider.php
+++ plugins/event_notification/providers/email/lib/model/recipient/EmailNotificationCategoryRecipientsProvider.php
@@ -8,35 +8,11 @@
 class EmailNotificationCategoryRecipientsProvider extends EmailNotificationRecipientProvider
 {
 	/**
-	 * Additional permissions for the category_users of the category (the users with these permissions will be added in the CC of the email notification)
-	 * @var string
-	 */
-	protected $cc_permissions;
-	
-	/**
-	 * Additional permissions for the category_users of the category (the users with these permissions will be added in the BCC of the email notification)
-	 * @var string
-	 */
-	protected $bcc_permissions;
-	
-	/**
 	 * ID of the category to whose subscribers the email should be sent
 	 * @var int
 	 */
 	protected $category_id;
-	/**
-	 * @return the $additional_permissions
-	 */
-	public function getAdditionalPermissions() {
-		return $this->additional_permissions;
-	}
 
-	/**
-	 * @param field_type $additional_permissions
-	 */
-	public function setAdditionalPermissions($additional_permissions) {
-		$this->additional_permissions = $additional_permissions;
-	}
 
 	/**
 	 * @return the $category_id
