8a3bdc9bbf496725b3184797333f0895e8619790
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-15 06:01:57 +0000

    qnd: enabled managing storage profiles
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90221 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/add_permissions/2012_11_15_storage_profile_service.php
+++ deployment/updates/scripts/add_permissions/2012_11_15_storage_profile_service.php
@@ -9,5 +9,5 @@
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
-$config = realpath(dirname(__FILE__)) . '/configs/storage_profile_list_imersonate.ini';
+$config = realpath(dirname(__FILE__)) . '/configs/storage_profile_service.ini';
 passthru("php $script $config");
