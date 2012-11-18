2ecd73a5aa4a697a95655c3a1ee45b069a391061
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-18 16:50:10 +0000

    qnd: external media entry tests
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90327 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ tests/api/KalturaPlugins/ExternalMediaService_externalMedia/ExternalMediaServiceTest.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../../bootstrap.php');
+
+/**
+ * externalMedia service test case.
+ */
+class ExternalMediaServiceTest extends ExternalMediaServiceTestBase
+{
+	/**
+	 * Tests externalMedia->count action
+	 * @param KalturaExternalMediaEntry $entry 
+	 * @param KalturaExternalMediaEntryFilter $filter External media entry filter
+	 * @dataProvider provideData
+	 */
+	public function testCount(KalturaExternalMediaEntry $entry, KalturaExternalMediaEntryFilter $filter = null)
+	{
+		$externalMediaPlugin = KalturaExternalMediaClientPlugin::get($this->client);
+		$externalMediaPlugin->externalMedia->add($entry);
+		$resultObject = $externalMediaPlugin->externalMedia->count($filter);
+		
+		if(method_exists($this, 'assertInstanceOf'))
+			$this->assertInstanceOf('int', $resultObject);
+		else
+			$this->assertType('int', $resultObject);
+			
+		$this->assertGreaterThan(0, $resultObject);
+	}
+
+	/**
+	 * Tests externalMedia->listAction action
+	 * @param KalturaExternalMediaEntryFilter $filter External media entry filter
+	 * @param KalturaFilterPager $pager Pager
+	 * @param KalturaExternalMediaEntryListResponse $reference
+	 * @dataProvider provideData
+	 */
+	public function testListAction(KalturaExternalMediaEntryFilter $filter = null, KalturaFilterPager $pager = null, KalturaExternalMediaEntryListResponse $reference)
+	{
+		$resultObject = $this->client->externalMedia->listAction($filter, $pager);
+		/* @var $resultObject KalturaExternalMediaEntryListResponse */
+		if(method_exists($this, 'assertInstanceOf'))
+			$this->assertInstanceOf('KalturaExternalMediaEntryListResponse', $resultObject);
+		else
+			$this->assertType('KalturaExternalMediaEntryListResponse', $resultObject);
+	
+		foreach($resultObject->objects as $entry)
+		{
+			/* @var $entry KalturaExternalMediaEntry */
+			if(method_exists($this, 'assertInstanceOf'))
+				$this->assertInstanceOf('KalturaExternalMediaEntry', $entry, "Entry id [$entry->id]");
+			else
+				$this->assertType('KalturaExternalMediaEntry', $entry, "Entry id [$entry->id]");
+				
+			if($filter->externalSourceTypeEqual)
+				$this->assertEquals($filter->externalSourceTypeEqual, $entry->externalSourceType, "Entry id [$entry->id]");
+			else
+				$this->assertNotNull($entry->externalSourceType, "Entry id [$entry->id]");
+		}
+	}
+}
+
---
+++ tests/api/KalturaPlugins/ExternalMediaService_externalMedia/ExternalMediaServiceTestBase.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * externalMedia service base test case.
+ */
+abstract class ExternalMediaServiceTestBase extends KalturaApiTestCase
+{
+	/**
+	 * Tests externalMedia->add action
+	 * @param KalturaExternalMediaEntry $entry 
+	 * @param KalturaExternalMediaEntry $reference
+	 * @return KalturaExternalMediaEntry
+	 * @dataProvider provideData
+	 */
+	public function testAdd(KalturaExternalMediaEntry $entry, KalturaExternalMediaEntry $reference)
+	{
+		$resultObject = $this->client->externalMedia->add($entry);
+		if(method_exists($this, 'assertInstanceOf'))
+			$this->assertInstanceOf('KalturaExternalMediaEntry', $resultObject);
+		else
+			$this->assertType('KalturaExternalMediaEntry', $resultObject);
+		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
+		$this->assertNotNull($resultObject->id);
+		$this->validateAdd($resultObject);
+		
+		return $resultObject->id;
+	}
+
+	/**
+	 * Validates testAdd results
+	 * Hook to be overriden by the extending class
+	 * 
+	 * @param KalturaExternalMediaEntry $resultObject
+	 */
+	protected function validateAdd(KalturaExternalMediaEntry $resultObject){}
+
+	/**
+	 * Tests externalMedia->get action
+	 * @param string $id External media entry id
+	 * @param KalturaExternalMediaEntry $reference
+	 * @return KalturaExternalMediaEntry
+	 * @depends testAdd with data set #0
+	 * @dataProvider provideData
+	 */
+	public function testGet($id, KalturaExternalMediaEntry $reference)
+	{
+		$resultObject = $this->client->externalMedia->get($id);
+		if(method_exists($this, 'assertInstanceOf'))
+			$this->assertInstanceOf('KalturaExternalMediaEntry', $resultObject);
+		else
+			$this->assertType('KalturaExternalMediaEntry', $resultObject);
+		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
+		$this->assertNotNull($resultObject->id);
+		$this->validateGet($resultObject);
+		
+		return $resultObject->id;
+	}
+
+	/**
+	 * Validates testGet results
+	 * Hook to be overriden by the extending class
+	 * 
+	 * @param KalturaExternalMediaEntry $resultObject
+	 */
+	protected function validateGet(KalturaExternalMediaEntry $resultObject){}
+
+	/**
+	 * Tests externalMedia->update action
+	 * @param string $id External media entry id to update
+	 * @param KalturaExternalMediaEntry $entry External media entry object to update
+	 * @param KalturaExternalMediaEntry $reference
+	 * @return KalturaExternalMediaEntry
+	 * @depends testAdd with data set #0
+	 * @dataProvider provideData
+	 */
+	public function testUpdate($id, KalturaExternalMediaEntry $entry, KalturaExternalMediaEntry $reference)
+	{
+		$resultObject = $this->client->externalMedia->update($id, $entry);
+		if(method_exists($this, 'assertInstanceOf'))
+			$this->assertInstanceOf('KalturaExternalMediaEntry', $resultObject);
+		else
+			$this->assertType('KalturaExternalMediaEntry', $resultObject);
+		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
+		$this->assertNotNull($resultObject->id);
+		$this->validateUpdate($resultObject);
+		
+		return $resultObject->id;
+	}
+
+	/**
+	 * Validates testUpdate results
+	 * Hook to be overriden by the extending class
+	 * 
+	 * @param KalturaExternalMediaEntry $resultObject
+	 */
+	protected function validateUpdate(KalturaExternalMediaEntry $resultObject){}
+
+	/**
+	 * Tests externalMedia->delete action
+	 * @param string $id External media entry id to delete
+	 * @depends testGet with data set #0
+	 * @dataProvider provideData
+	 */
+	public function testDelete($id)
+	{
+		$resultObject = $this->client->externalMedia->delete($id);
+	}
+
+	/**
+	 * Tests externalMedia->listAction action
+	 * @param KalturaExternalMediaEntryFilter $filter External media entry filter
+	 * @param KalturaFilterPager $pager Pager
+	 * @param KalturaExternalMediaEntryListResponse $reference
+	 * @dataProvider provideData
+	 */
+	public function testListAction(KalturaExternalMediaEntryFilter $filter = null, KalturaFilterPager $pager = null, KalturaExternalMediaEntryListResponse $reference)
+	{
+		$resultObject = $this->client->externalMedia->listAction($filter, $pager);
+		if(method_exists($this, 'assertInstanceOf'))
+			$this->assertInstanceOf('KalturaExternalMediaEntryListResponse', $resultObject);
+		else
+			$this->assertType('KalturaExternalMediaEntryListResponse', $resultObject);
+		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
+		$this->validateListAction($resultObject);
+	}
+
+	/**
+	 * Validates testListAction results
+	 * Hook to be overriden by the extending class
+	 * 
+	 * @param KalturaExternalMediaEntryListResponse $resultObject
+	 */
+	protected function validateListAction(KalturaExternalMediaEntryListResponse $resultObject){}
+
+}
