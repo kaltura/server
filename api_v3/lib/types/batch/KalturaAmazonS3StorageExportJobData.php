3f413b83df2649e701b52ad36163454b574eaecb
rotema <rotema@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-21 16:52:30 +0000

    qnd: 4750 added support for setting files permission in Amazon S3
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90467 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ api_v3/lib/types/batch/KalturaAmazonS3StorageExportJobData.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaAmazonS3StorageExportJobData extends KalturaStorageExportJobData 
+{
+	/**
+	 * @var KalturaAmazonS3StorageProfileFilesPermissionLevel
+	 */   	
+    public $filesPermissionInS3;   
+    
+	private static $map_between_objects = array
+	(
+		"filesPermissionInS3",	
+	);
+
+	public function getMapBetweenObjects ( )
+	{
+		return array_merge ( parent::getMapBetweenObjects() , self::$map_between_objects );
+	}
+
+	
+	public function toObject($dbData = null, $props_to_skip = array()) 
+	{
+		if(is_null($dbData))
+			$dbData = new kAmazonS3StorageExportJobData();
+			
+		return parent::toObject($dbData);
+	}
+	
+}
