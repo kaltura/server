dce69d1b912bcab29ebe385dd55605238f1bc05a
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-25 11:16:37 +0000

    fwr: new kCoreException class
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90598 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/event_notification/lib/errors/kEventNotificationException.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package plugins.eventNotification
+ * @subpackage model.errors
+ */
+class kEventNotificationException extends kCoreException
+{
+	const EVENT_NOTIFICATION_TEMPLATE_DUPLICATE_SYSTEM_NAME = 'EVENT_NOTIFICATION_TEMPLATE_DUPLICATE_SYSTEM_NAME';
+}
