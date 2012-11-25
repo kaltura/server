bf10589edfe50f95c42c999ec0624a937b2c2fe4
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-25 08:07:30 +0000

    fwr: reverted unnecessary change
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90571 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- server_infra/propel/php5/KalturaPeerBuilder.php
+++ server_infra/propel/php5/KalturaPeerBuilder.php
@@ -287,7 +287,7 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 		\$queryResult = ".$this->getPeerClassname()."::populateObjects(".$this->basePeerClassname."::doSelect(\$criteriaForSelect, \$con));
 		
 		if(\$criteriaForSelect instanceof KalturaCriteria)
-			\$queryResult = \$criteriaForSelect->applyResultsSort(\$queryResult);
+			\$criteriaForSelect->applyResultsSort(\$queryResult);
 		
 		if (\$cacheKey !== null)
 		{
