717448b6a2cf5e2878a77276214833e543b6800e
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-25 08:21:27 +0000

    fwr: new ini file for testing admin functionality
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90575 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/api/KalturaPlugins/EventNotificationTemplateService_eventNotificationTemplate/EventNotificationTemplateServiceTestAdmin.php
+++ tests/api/KalturaPlugins/EventNotificationTemplateService_eventNotificationTemplate/EventNotificationTemplateServiceTestAdmin.php
@@ -17,22 +17,8 @@ class EventNotificationTemplateServiceAdminTest extends EventNotificationTemplat
 	public function testAdminAdd($eventNotificationTemplate, $impersonatedPartnerId , $reference)
 	{
 		//Impersonate partner
-		$config = $this->client->getConfig();
-		$config->partnerId = $impersonatedPartnerId;
-		$this->client->setConfig($config);
+		$this->impersonate($impersonatedPartnerId);
 		//Perform action
-		$this->testAdd($eventNotificationTemplate, $reference);
-	}
-	
-	/**
-	 * Tests eventNotificationTemplate->add action
-	 * @param KalturaEventNotificationTemplate $eventNotificationTemplate 
-	 * @param KalturaEventNotificationTemplate $reference
-	 * @return KalturaEventNotificationTemplate
-	 * @dataProvider provideData
-	 */
-	public function testAdd(KalturaEventNotificationTemplate $eventNotificationTemplate, KalturaEventNotificationTemplate $reference)
-	{
 		$resultObject = $this->client->eventNotificationTemplate->add($eventNotificationTemplate);
 		if(method_exists($this, 'assertInstanceOf'))
 			$this->assertInstanceOf('KalturaEventNotificationTemplate', $resultObject);
@@ -45,121 +31,30 @@ class EventNotificationTemplateServiceAdminTest extends EventNotificationTemplat
 		return $resultObject->id;
 	}
 	
-	/* (non-PHPdoc)
-	 * @see EventNotificationTemplateServiceTestBase::validateAdd()
-	 */
-	protected function validateAdd(KalturaEventNotificationTemplate $resultObject)
-	{
-		// TODO - add your own validations here
-	}
-
 	/**
-	 * Tests eventNotificationTemplate->cloneAction action
-	 * @param int $id source template to clone
-	 * @param KalturaEventNotificationTemplate $eventNotificationTemplate overwrite configuration object
+	 * Tests eventNotificationTemplate->add action
+	 * @param KalturaEventNotificationTemplate $eventNotificationTemplate 
 	 * @param KalturaEventNotificationTemplate $reference
+	 * @return KalturaEventNotificationTemplate
 	 * @dataProvider provideData
 	 */
-	public function testCloneAction($id, KalturaEventNotificationTemplate $eventNotificationTemplate, KalturaEventNotificationTemplate $reference)
+	public function testAdd(KalturaEventNotificationTemplate $eventNotificationTemplate, KalturaEventNotificationTemplate $reference)
 	{
-		$resultObject = $this->client->eventNotificationTemplate->cloneAction($id, $eventNotificationTemplate, $reference);
-		if(method_exists($this, 'assertInstanceOf'))
-			$this->assertInstanceOf('KalturaEventNotificationTemplate', $resultObject);
-		else
-			$this->assertType('KalturaEventNotificationTemplate', $resultObject);
-		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
-		// TODO - add here your own validations
-		$this->validateCloneAction($resultObject);
+		
 	}
 
-	/* (non-PHPdoc)
-	 * @see EventNotificationTemplateServiceTestBase::validateGet()
-	 */
-	protected function validateGet(KalturaEventNotificationTemplate $resultObject)
-	{
-		// TODO - add your own validations here
-	}
-
-	/* (non-PHPdoc)
-	 * @see EventNotificationTemplateServiceTestBase::validateUpdate()
-	 */
-	protected function validateUpdate(KalturaEventNotificationTemplate $resultObject)
-	{
-		// TODO - add your own validations here
-	}
 
 	/**
-	 * Tests eventNotificationTemplate->updatestatus action
-	 * @param int $id 
-	 * @param KalturaEventNotificationTemplateStatus $status 
-	 * @param KalturaEventNotificationTemplate $reference
-	 * @dataProvider provideData
+	 * Set different partner ID for client config
+	 * @param int $impersonatedPartnerId
 	 */
-	public function testUpdatestatus($id, $status, KalturaEventNotificationTemplate $reference)
+	protected function impersonate ($impersonatedPartnerId)
 	{
-		$resultObject = $this->client->eventNotificationTemplate->updatestatus($id, $status, $reference);
-		if(method_exists($this, 'assertInstanceOf'))
-			$this->assertInstanceOf('KalturaEventNotificationTemplate', $resultObject);
-		else
-			$this->assertType('KalturaEventNotificationTemplate', $resultObject);
-		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
-		// TODO - add here your own validations
-		$this->validateUpdatestatus($resultObject);
+		//Impersonate partner
+		$config = $this->client->getConfig();
+		$config->partnerId = $impersonatedPartnerId;
+		$this->client->setConfig($config);
 	}
-
-	/* (non-PHPdoc)
-	 * @see EventNotificationTemplateServiceTestBase::validateDelete()
-	 */
-	protected function validateDelete(KalturaEventNotificationTemplate $resultObject)
-	{
-		// TODO - add your own validations here
-	}
-
-	/* (non-PHPdoc)
-	 * @see EventNotificationTemplateServiceTestBase::validateListAction()
-	 */
-	protected function validateListAction(KalturaEventNotificationTemplateListResponse $resultObject)
-	{
-		// TODO - add your own validations here
-	}
-
-	/**
-	 * Tests eventNotificationTemplate->listbypartner action
-	 * @param KalturaPartnerFilter $filter 
-	 * @param KalturaFilterPager $pager 
-	 * @param KalturaEventNotificationTemplateListResponse $reference
-	 * @dataProvider provideData
-	 */
-	public function testListbypartner(KalturaPartnerFilter $filter = null, KalturaFilterPager $pager = null, KalturaEventNotificationTemplateListResponse $reference)
-	{
-		$resultObject = $this->client->eventNotificationTemplate->listbypartner($filter, $pager, $reference);
-		if(method_exists($this, 'assertInstanceOf'))
-			$this->assertInstanceOf('KalturaEventNotificationTemplateListResponse', $resultObject);
-		else
-			$this->assertType('KalturaEventNotificationTemplateListResponse', $resultObject);
-		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
-		// TODO - add here your own validations
-		$this->validateListbypartner($resultObject);
-	}
-
-	/**
-	 * Tests eventNotificationTemplate->dispatch action
-	 * @param int $id 
-	 * @param KalturaEventNotificationDispatchJobData $data
-	 * @param int $reference
-	 * @dataProvider provideData
-	 */
-	public function testDispatch($id, KalturaEventNotificationDispatchJobData $data, $reference)
-	{
-		$resultObject = $this->client->eventNotificationTemplate->dispatch($id, $data, $reference);
-		if(method_exists($this, 'assertInstanceOf'))
-			$this->assertInstanceOf('int', $resultObject);
-		else
-			$this->assertType('int', $resultObject);
-		$this->assertAPIObjects($reference, $resultObject, array('createdAt', 'updatedAt', 'id', 'thumbnailUrl', 'downloadUrl', 'rootEntryId', 'operationAttributes', 'deletedAt', 'statusUpdatedAt', 'widgetHTML', 'totalCount', 'objects', 'cropDimensions', 'dataUrl', 'requiredPermissions', 'confFilePath', 'feedUrl'));
-		// TODO - add here your own validations
-		$this->validateDispatch($resultObject);
-	}
-
+	
 }
 
