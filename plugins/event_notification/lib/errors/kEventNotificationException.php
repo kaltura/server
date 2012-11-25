7afb56018ea738b134d5e6926c6e6aa70e7b46b9
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-25 11:27:31 +0000

    fwr: remove unnecessary class
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90600 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/event_notification/lib/errors/kEventNotificationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package plugins.eventNotification
- * @subpackage model.errors
- */
-class kEventNotificationException extends kCoreException
-{
-	const EVENT_NOTIFICATION_TEMPLATE_DUPLICATE_SYSTEM_NAME = 'EVENT_NOTIFICATION_TEMPLATE_DUPLICATE_SYSTEM_NAME';
-}
