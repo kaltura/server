655ca5664d173f69a11b1b013feb6a212da616f4
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-27 09:44:18 +0000

    fwr: permission item for liveStream service new action
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90707 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/updates/scripts/add_permissions/2012_11_27_live_stream_service_permission_item.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage falcon.roles_and_permissions
+ *
+ * Adds category service permissions
+ *
+ * No need to re-run after server code deploy
+ */
+
+$script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
+$config = realpath(dirname(__FILE__)) . '/configs/live_stream_service_permission_item.ini';
+passthru("php $script $config");
