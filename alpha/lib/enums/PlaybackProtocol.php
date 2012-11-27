2290e811912fdad1250f8bfb1968120221d55440
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-27 08:46:43 +0000

    fwr: new playback protocol constant interface
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@90689 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/enums/PlaybackProtocol.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Core
+ * @subpackage model.enum
+ */ 
+interface PlaybackProtocol extends BaseEnum
+{
+	const HTTP = 'http';
+	const RTMP = 'rtmp';
+	const SILVER_LIGHT = 'sl';
+	const APPLE_HTTP = 'applehttp';
+	const RTSP = 'rtsp';
+	const AUTO = 'auto';
+	const HDS = 'hds';
+	const HLS = 'hls';	
+}
