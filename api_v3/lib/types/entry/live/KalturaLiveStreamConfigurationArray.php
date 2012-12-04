909b078840a2b65ac9f073a4285d19f0e0006a9e
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-04 12:56:05 +0000

    fwr: toObject implementation enforces single use for each protocol
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@91053 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/entry/live/KalturaLiveStreamConfigurationArray.php
+++ api_v3/lib/types/entry/live/KalturaLiveStreamConfigurationArray.php
@@ -15,9 +15,7 @@ class KalturaLiveStreamConfigurationArray extends KalturaTypedArray
 			{
 				/* @var $object KLiveStreamConfiguration */
 				$configObject = new KalturaLiveStreamConfiguration();
-				$configObject->protocol = $object->getProtocol();
-				$configObject->url = $object->getUrl();
-				$array[] = $configObject;
+				$configObject->fromObject($object);
 			}
 		}
 		return $configObject;
@@ -26,6 +24,53 @@ class KalturaLiveStreamConfigurationArray extends KalturaTypedArray
 	public function __construct()
 	{
 		return parent::__construct("KalturaLiveStreamConfiguration");
+	}
+	
+	/**
+	 * Function extracts the first item in the array where the property $propertyName has the value $propertyValue
+	 * @param string $propertyName
+	 * @param string $propertyValue
+	 * @return KalturaLiveStreamConfiguration
+	 */
+	public function returnSingleItemByPropertyValue ($propertyName, $propertyValue)
+	{
+		foreach ($this->toArray() as $config)
+		{
+			/* @var $config KalturaLiveStreamConfiguration */
+			if (property_exists("KalturaLiveStreamConfiguration", $propertyName))
+			{
+				if ($config->$propertyName == $propertyValue)
+				{
+					return $config;
+				}
+			}
+				
+		}
+
+		return null;
+	}
+	
+	public function toObjectsArray()
+	{
+		$objects = $this->toArray();
+		for ($i = 0; $i < count($objects); $i++)
+		{
+			for ($j = $i; $j <count($objects); $j++ )
+			{
+				if ($objects[$i]->protocol == $objects[$j]->protocol)
+				{
+					unset($objects[$i]);
+				}
+			}
+		}
+		
+		$ret = array();
+		foreach ($objects as $object)
+		{
+			$ret[] = $object->toObject();
+		}
+		
+		return $ret;
 	}
 	
 }
