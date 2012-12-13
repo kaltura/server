55007946d02db8f4b7e23abecccb765df6dbba9a
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-13 19:34:03 +0000

    qnd: a clearer message.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@91463 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/scripts/watch.populate.sh
+++ plugins/sphinx_search/scripts/watch.populate.sh
@@ -15,10 +15,9 @@ fi
 MAILTO="$ADMIN_CONSOLE_ADMIN_MAIL"
 KP=$(pgrep -f $POPULATE_FROM_LOG)
 MAINT=$BASE_DIR/maintenance
-if [[ "X$KP" = "X" && ! -f $MAINT ]]
-      then
-          echo "$POPULATE_FROM_LOG `hostname` was restarted" | mail -s "$POPULATE_FROM_LOG script not found on `hostname`" $MAILTO
+if [ -z "$KP" -a ! -f $MAINT ];then
+          echo "$POPLATE_FROM_LOG `hostname` was restarted" | mail -s "Sphinx population daemon was restarted. `hostname`\nPlease check $LOG_DIR/kaltura_sphinx_populate.log\n" $MAILTO
 	  cd $APP_DIR/plugins/sphinx_search/scripts
 	  php $POPULATE_FROM_LOG ${SERVER} >> $LOG_DIR/kaltura_sphinx_populate.log 2>&1 &
-      fi
+fi
 
