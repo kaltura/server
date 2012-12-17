c14e7ec0a70edd3234839ffb71ef3d5c60d2309d
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-17 13:45:13 +0000

    fwr: new error class
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@91597 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/event_notification/providers/email/lib/api/enums/KalturaEmailNotificationErrors.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package plugins.emailNotification
+ * @subpackage api.enums
+ */
+class KalturaEmailNotificationErrors extends KalturaErrors
+{
+	const INVALID_FILTER_PROPERTY = 'INVALID_FILTER_PROPERTY,The value of property [%s] cannot be set from this context';
+}
