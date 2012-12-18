5b49caa0ad30cd49ba09e5368aff70971022ddb8
yulir <yulir@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-18 11:13:03 +0000

    def: 6277 add file in uploading mode if file does not exist
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@91674 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/drop_folder/external_script/SyncDropFolderWatcher.php
+++ plugins/drop_folder/external_script/SyncDropFolderWatcher.php
@@ -5,12 +5,32 @@
 // php SyncDropFolderWatcher.php 1 /web/content/drop_folder1/file1.flv 0 >> /var/log/SyncDropFolderWatcher.log
 // php SyncDropFolderWatcher.php 2 /web/content/drop_folder1/file1.flv 1595 >> /var/log/SyncDropFolderWatcher.log
 
+require_once(dirname(__file__).'/lib/KalturaClient.php');
+require_once(dirname(__file__).'/lib/KalturaPlugins/KalturaDropFolderClientPlugin.php');
+
+class SyncDropFolderWatcherLogger implements IKalturaLogger
+{
+	private $prefix = '';
+	
+	function __construct($logPrefix)
+	{
+		$this->prefix = $logPrefix;
+	}
+	
+	function log($msg)
+	{
+		writeLog($this->prefix, $msg);
+	}
+}
+
 const DETECTED = 1;
 const UPLOADED = 2;
 
+$logPrefix = rand();
+
 if($argc < 3)
 {
-	echo 'Wrong number of arguments';
+	writeLog($logPrefix, 'Wrong number of arguments');
 	return;
 }
 $action = $argv[1];
@@ -21,24 +41,21 @@ $fileName=basename($filePath);
 $folderPath = dirname($filePath);
 
 
-echo '---------------------------- Start handling --------------------------'."\n";
-echo 'action:'.$action."\n";
-echo 'file path:'.$filePath."\n";
-echo 'folder path:'.$folderPath."\n";
-echo 'file name:'.$fileName."\n";
-echo 'file size:'.$fileSize."\n";
+writeLog($logPrefix, '---------------------------- Start handling --------------------------');
+writeLog($logPrefix, 'action:'.$action);
+writeLog($logPrefix, 'file path:'.$filePath);
+writeLog($logPrefix, 'folder path:'.$folderPath);
+writeLog($logPrefix, 'file name:'.$fileName);
+writeLog($logPrefix, 'file size:'.$fileSize);
 
 $config = parse_ini_file("config.ini");
 $serviceUrl = $config['service_url'];
-echo 'Service URL '.$serviceUrl."\n";
-
-require_once(dirname(__file__).'/lib/KalturaClient.php');
-require_once(dirname(__file__).'/lib/KalturaPlugins/KalturaDropFolderClientPlugin.php');
-
+writeLog($logPrefix, 'Service URL '.$serviceUrl);
 
 $kClientConfig = new KalturaConfiguration(-1);
 $kClientConfig->serviceUrl = $serviceUrl;
 $kClientConfig->curlTimeout = 180;
+$kClientConfig->setLogger(new SyncDropFolderWatcherLogger($logPrefix));
 
 $kClient = new KalturaClient($kClientConfig);
 $dropFolderPlugin = KalturaDropFolderClientPlugin::get($kClient);
@@ -50,18 +67,18 @@ try
 	$filter->pathEqual = $folderPath;
 	$filter->typeEqual = KalturaDropFolderType::LOCAL;
 	$dropFolders = $dropFolderPlugin->dropFolder->listAction($filter);	
-	echo 'found '.$dropFolders->totalCount.' folders'."\n";
+	writeLog($logPrefix, 'found '.$dropFolders->totalCount.' folders');
 	if($dropFolders->totalCount == 1)
 	{
 		$folder = $dropFolders->objects[0];
-		echo 'drop folder id '.$folder->id."\n";
+		writeLog($logPrefix, 'drop folder id '.$folder->id);
 		
 		$ignorePatterns = array_map('trim', explode(',', $folder->ignoreFileNamePatterns));
 		foreach ($ignorePatterns as $ignorePattern)
 		{
 			if (!is_null($ignorePattern) && ($ignorePattern != '') && fnmatch($ignorePattern, $fileName)) 
 			{
-				echo 'Ignoring file matching ignore pattern ['.$ignorePattern.']'."\n";
+				writeLog($logPrefix, 'Ignoring file matching ignore pattern ['.$ignorePattern.']');
 				return;
 			}
 		}
@@ -72,55 +89,49 @@ try
 		
 		if($action == DETECTED)
 		{
-			echo 'Handle file detected'."\n";
-			$detectedDropFolderFile = new KalturaDropFolderFile();
-	    	$detectedDropFolderFile->dropFolderId = $folder->id;
-	    	$detectedDropFolderFile->fileName = $fileName;
-	    	$detectedDropFolderFile->fileSize = $fileSize;
-	    	$detectedDropFolderFile->lastModificationTime = time(); 
-	    	$detectedDropFolderFile->uploadStartDetectedAt = time();
-			
-			$file = $dropFolderPlugin->dropFolderFile->add($detectedDropFolderFile);
-			echo 'created file with id '.$file->id."\n";
+			writeLog($logPrefix, 'Handle file detected');
+			$file = addFile($folder->id, $fileName, $fileSize, $dropFolderPlugin);			
+			writeLog($logPrefix, 'created file with id '.$file->id);
 		}
 		else if($action == UPLOADED)
 		{
-			echo 'Handle file uploaded'."\n";
+			writeLog($logPrefix, 'Handle file uploaded');
 			$filter = new KalturaDropFolderFileFilter();
 			$filter->dropFolderIdEqual = $folder->id;
 			$filter->fileNameEqual = $fileName;
+			$filter->statusIn = KalturaDropFolderFileStatus::PARSED.','.KalturaDropFolderFileStatus::UPLOADING;
 			$dropFolderFiles = $dropFolderPlugin->dropFolderFile->listAction($filter);
-			echo 'found '.$dropFolderFiles->totalCount.' files'."\n";
+			writeLog($logPrefix, 'found '.$dropFolderFiles->totalCount.' files');
 			if($dropFolderFiles->totalCount == 1)
 			{
 				$file = $dropFolderFiles->objects[0];
-				echo 'drop folder file id '.$file->id."\n";
-				$updateDropFolderFile = new KalturaDropFolderFile();				
-				$updateDropFolderFile->lastModificationTime = time();
-				$updateDropFolderFile->uploadEndDetectedAt = time();
-				$updateDropFolderFile->fileSize = $fileSize;
-				$dropFolderPlugin->dropFolderFile->update($file->id, $updateDropFolderFile);
-				$dropFolderPlugin->dropFolderFile->updateStatus($file->id, KalturaDropFolderFileStatus::PENDING);	
-				echo 'file updated '."\n";					
+				writeLog($logPrefix, 'drop folder file id '.$file->id);
+				updateFile($file->id, $fileSize, $dropFolderPlugin);				
+				writeLog($logPrefix, 'file updated ');					
 			}
 			else
 			{
-				echo 'Error - invalid file name'."\n";
+				writeLog($logPrefix, 'No file exists with status UPLOADING or PARSED, adding new file');
+				$file = addFile($folder->id, $fileName, $fileSize, $dropFolderPlugin);
+				writeLog($logPrefix, 'created file with id '.$file->id);
+				$dropFolderPlugin->dropFolderFile->updateStatus($file->id, KalturaDropFolderFileStatus::PENDING);	
+				writeLog($logPrefix, 'file status updated to PENDING');			
 			}
 		}
 		else 
 		{
-			echo 'Error - invalid action'."\n";
+			writeLog($logPrefix, 'Error - invalid action');
 		}
 	}
 	else
 	{
-		echo 'Error - folder does not exist'."\n";
+		writeLog($logPrefix, 'Error - folder does not exist');
 	}
 }
 catch (Exception $e)
 {
-	echo 'Exception '.$e->getMessage()."\n".$e->getTraceAsString()."\n";
+	writeLog($logPrefix, 'Exception '.$e->getMessage());
+	writeLog($logPrefix, $e->getTraceAsString());
 }
 
 //unimpersonate
@@ -128,3 +139,31 @@ $kClientConfig->partnerId = -1;
 $kClient->setConfig($kClientConfig);
 
 echo '---------------------------- Finish handling --------------------------'."\n";
+
+function writeLog($prefix, $message)
+{
+	echo $prefix.':'.$message."\n";
+}
+
+function addFile($folderId, $fileName, $fileSize, $dropFolderPlugin)
+{
+	$newDropFolderFile = new KalturaDropFolderFile();
+	$newDropFolderFile->dropFolderId = $folderId;
+	$newDropFolderFile->fileName = $fileName;
+	$newDropFolderFile->fileSize = $fileSize;
+	$newDropFolderFile->lastModificationTime = time(); 
+	$newDropFolderFile->uploadStartDetectedAt = time();
+			
+	$file = $dropFolderPlugin->dropFolderFile->add($newDropFolderFile);
+	return $file;
+}
+
+function updateFile($fileId, $fileSize, $dropFolderPlugin)
+{
+	$updateDropFolderFile = new KalturaDropFolderFile();				
+	$updateDropFolderFile->lastModificationTime = time();
+	$updateDropFolderFile->uploadEndDetectedAt = time();
+	$updateDropFolderFile->fileSize = $fileSize;
+	$dropFolderPlugin->dropFolderFile->update($fileId, $updateDropFolderFile);
+	$dropFolderPlugin->dropFolderFile->updateStatus($fileId, KalturaDropFolderFileStatus::PENDING);	
+}
