dff866ce81559b06a9f0fb254a99a482ac6611c3
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-18 11:23:54 +0000

    ps: index kuser with role ids and permission names
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@91676 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/lib/SphinxKuserCriteria.php
+++ plugins/sphinx_search/lib/SphinxKuserCriteria.php
@@ -18,33 +18,10 @@ class SphinxKuserCriteria extends SphinxCriteria
         kuserPeer::FIRST_NAME => "first_name" ,
         kuserPeer::LAST_NAME => "last_name" ,
         kuserPeer::EMAIL => "email" ,
-        kuserPeer::SHA1_PASSWORD => "sha1_password" ,
-        kuserPeer::SALT => "salt" ,
-        kuserPeer::DATE_OF_BIRTH => "date_of_birth" ,
-        kuserPeer::COUNTRY => "country" ,
-        kuserPeer::STATE => "state" ,
-        kuserPeer::CITY => "city" ,
-        kuserPeer::ZIP => "zip" ,
-        kuserPeer::URL_LIST => "url_list" ,
-        kuserPeer::PICTURE => "picture" ,
-        kuserPeer::ICON => "icon" ,
         kuserPeer::ABOUT_ME => "about_me" ,
         kuserPeer::TAGS => "tags" ,
-        kuserPeer::TAGLINE => "tagline" ,
-        kuserPeer::NETWORK_HIGHSCHOOL => "network_highschool" ,
-        kuserPeer::NETWORK_COLLEGE => "network_college" ,
-        kuserPeer::NETWORK_OTHER => "network_other" ,
-        kuserPeer::MOBILE_NUM => "mobile_num" ,
-        kuserPeer::MATURE_CONTENT => "mature_content" ,
-        kuserPeer::GENDER => "gender" ,
-        kuserPeer::REGISTRATION_IP => "registration_ip" ,
-        kuserPeer::REGISTRATION_COOKIE => "registration_cookie" ,
-        kuserPeer::IM_LIST => "im_list" ,
-        kuserPeer::VIEWS => "views" ,
-        kuserPeer::FANS => "fans" ,
         kuserPeer::ENTRIES => "entries" ,
         kuserPeer::STORAGE_SIZE => "storage_size" ,
-        kuserPeer::PRODUCED_KSHOWS => "produced_kshows" ,
         kuserPeer::STATUS => "kuser_status" ,
         kuserPeer::CREATED_AT => "created_at" ,
         kuserPeer::UPDATED_AT => "updated_at" ,
@@ -52,12 +29,12 @@ class SphinxKuserCriteria extends SphinxCriteria
         kuserPeer::DISPLAY_IN_SEARCH => "display_in_search" ,
         kuserPeer::PARTNER_DATA => "partner_data" ,
         kuserPeer::PUSER_ID => "puser_id" ,
-        kuserPeer::ADMIN_TAGS => "admin_tags" ,
         kuserPeer::INDEXED_PARTNER_DATA_INT => "indexed_partner_data_int" ,
         kuserPeer::INDEXED_PARTNER_DATA_STRING => "indexed_partner_data_string" ,
-        kuserPeer::CUSTOM_DATA => "custom_data",
         'kuserPeer.PUSER_ID_OR_SCREEN_NAME' => '(puser_id,screen_name)',
         'kuserPeer.FIRST_NAME_OR_LAST_NAME' => '(full_name,last_name)',
+        'kuserPeer.PERMISSION_NAMES' => 'permission_names',
+        'kuserPeer.ROLE_IDS' => 'role_ids',
 		
 	);
 	
@@ -65,16 +42,8 @@ class SphinxKuserCriteria extends SphinxCriteria
 		kuserPeer::CREATED_AT => 'created_at',
 		kuserPeer::UPDATED_AT => 'updated_at',
 		kuserPeer::DISPLAY_IN_SEARCH => "display_in_search" ,
-		kuserPeer::MATURE_CONTENT => "mature_content" ,
-		kuserPeer::GENDER => "gender",
-		kuserPeer::DATE_OF_BIRTH => "date_of_birth",
 		kuserPeer::LOGIN_DATA_ID => "login_data_id",
         kuserPeer::IS_ADMIN => "is_admin",
-        kuserPeer::VIEWS => "views",
-		kuserPeer::FANS => "fans",
-        kuserPeer::ENTRIES => "entries" ,
-        kuserPeer::STORAGE_SIZE => "storage_size" ,
-        kuserPeer::PRODUCED_KSHOWS => "produced_kshows" ,
 		kuserPeer::INDEXED_PARTNER_DATA_INT => "indexed_partner_data_int",
 	);
 	
@@ -86,33 +55,10 @@ class SphinxKuserCriteria extends SphinxCriteria
                 "first_name" => IIndexable::FIELD_TYPE_STRING,
                 "last_name" => IIndexable::FIELD_TYPE_STRING,
                 "email" => IIndexable::FIELD_TYPE_STRING,
-                "sha1_password" => IIndexable::FIELD_TYPE_STRING,
-                "salt" => IIndexable::FIELD_TYPE_STRING,
-                "date_of_birth" => IIndexable::FIELD_TYPE_DATETIME,
-                "country" => IIndexable::FIELD_TYPE_STRING,
-                "state" => IIndexable::FIELD_TYPE_STRING,
-                "city" => IIndexable::FIELD_TYPE_STRING,
-                "zip" => IIndexable::FIELD_TYPE_STRING,
-                "url_list" => IIndexable::FIELD_TYPE_STRING,
-                "picture" => IIndexable::FIELD_TYPE_STRING,
-                "icon" => IIndexable::FIELD_TYPE_STRING,
                 "about_me" => IIndexable::FIELD_TYPE_STRING,
                 "tags" => IIndexable::FIELD_TYPE_STRING,
-                "tagline" => IIndexable::FIELD_TYPE_STRING,
-                "network_highschool" => IIndexable::FIELD_TYPE_STRING,
-                "network_college" => IIndexable::FIELD_TYPE_STRING,
-                "network_other" => IIndexable::FIELD_TYPE_STRING,
-                "mobile_num" => IIndexable::FIELD_TYPE_STRING,
-                "mature_content" => IIndexable::FIELD_TYPE_INTEGER,
-                "gender" => IIndexable::FIELD_TYPE_INTEGER,
-                "registration_ip" => IIndexable::FIELD_TYPE_STRING,
-                "registration_cookie" => IIndexable::FIELD_TYPE_STRING,
-                "im_list" => IIndexable::FIELD_TYPE_STRING,
-                "views" => IIndexable::FIELD_TYPE_INTEGER,
-                "fans" => IIndexable::FIELD_TYPE_INTEGER,
                 "entries" => IIndexable::FIELD_TYPE_INTEGER,
                 "storage_size" => IIndexable::FIELD_TYPE_INTEGER,
-                "produced_kshows" => IIndexable::FIELD_TYPE_INTEGER,
                 "kuser_status" => IIndexable::FIELD_TYPE_INTEGER,
                 "created_at" => IIndexable::FIELD_TYPE_DATETIME,
                 "updated_at" => IIndexable::FIELD_TYPE_DATETIME,
@@ -120,10 +66,10 @@ class SphinxKuserCriteria extends SphinxCriteria
                 "display_in_search" => IIndexable::FIELD_TYPE_INTEGER,
                 "partner_data" => IIndexable::FIELD_TYPE_STRING,
                 "puser_id" => IIndexable::FIELD_TYPE_STRING,
-                "admin_tags" => IIndexable::FIELD_TYPE_STRING,
                 "indexed_partner_data_int" => IIndexable::FIELD_TYPE_INTEGER,
                 "indexed_partner_data_string" => IIndexable::FIELD_TYPE_STRING,
-                "custom_data" => IIndexable::FIELD_TYPE_STRING,
+				"permission_names" => IIndexable::FIELD_TYPE_STRING,
+				"role_ids"	=> IIndexable::FIELD_TYPE_STRING,
         );
     
 	/* (non-PHPdoc)
@@ -230,31 +176,13 @@ class SphinxKuserCriteria extends SphinxCriteria
             "first_name" ,
             "last_name" ,
             "email" ,
-            "sha1_password" ,
-            "salt" ,
-            "country" ,
-            "state" ,
-            "city" ,
-            "zip" ,
-            "url_list" ,
-            "picture" ,
-            "icon" ,
             "about_me" ,
             "tags" ,
-            "tagline" ,
-            "network_highschool" ,
-            "network_college" ,
-            "network_other" ,
-            "mobile_num" ,
-            "mature_content" ,
-            "registration_ip" ,
-            "registration_cookie" ,
-            "im_list" ,
             "partner_data" ,
             "puser_id" ,
-            "admin_tags" ,
             "indexed_partner_data_string" ,
-            "custom_data",
+        	"permission_names",
+        	"role_ids",
 		));
         
     }
@@ -264,6 +192,24 @@ class SphinxKuserCriteria extends SphinxCriteria
 	 */
 	protected function applyFilterFields(baseObjectFilter $filter)
 	{		
+		//Role ids and kuser permission names are indexed with the partner ID
+		if ($filter->get('_eq_role_ids'))
+		{
+			$filter->set('_eq_role_ids', kuser::getIndexedFieldValue('kuserPeer::ROLE_IDS', $filter->get('_eq_role_ids'), kCurrentContext::getCurrentPartnerId()));
+		}
+		if ($filter->get('_in_role_ids'))
+		{
+			$filter->set('_eq_role_ids', kuser::getIndexedFieldValue('kuserPeer::ROLE_IDS', $filter->get('_eq_role_ids'), kCurrentContext::getCurrentPartnerId()));
+		}
+		if ($filter->get('_mlikeand_permission_names'))
+		{
+			$filter->set('_mlikeand_permission_names', kuser::getIndexedFieldValue('kuserPeer::PERMISSION_NAMES', $filter->get('_mlikeand_permission_names'), kCurrentContext::getCurrentPartnerId()));
+		}
+		if ($filter->get('_mlikeor_permission_names'))
+		{
+			$filter->set('_mlikeor_permission_names', kuser::getIndexedFieldValue('kuserPeer::PERMISSION_NAMES', $filter->get('_mlikeor_permission_names'), kCurrentContext::getCurrentPartnerId()));
+		}
+		
 		if($filter->get('_likex_puser_id_or_screen_name'))
 		{
 			$freeTexts = $filter->get('_likex_puser_id_or_screen_name');
