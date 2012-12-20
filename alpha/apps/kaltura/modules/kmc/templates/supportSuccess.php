645f9139bc02f43c1d5d7142f971bb663bed69f3
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-20 09:45:04 +0000

    qnd: load jquery from local environment
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@91831 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/templates/supportSuccess.php
+++ alpha/apps/kaltura/modules/kmc/templates/supportSuccess.php
@@ -48,7 +48,7 @@
 			  div#thx button { margin-right:214px;}
 		</style>
 	<META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=UTF-8">
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script> 
+	<script type="text/javascript" src="<?php echo requestUtils::getCdnHost( requestUtils::getRequestProtocol() ); ?>/lib/js/jquery_v1.4.2.min.js"></script>
 </head> 
 	<body> 
 
