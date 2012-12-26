baf98e43055b712b3ca4fa1d4184d26539ae055a
rotema <rotema@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-26 10:51:30 +0000

    def: 6284 no need in db manager anymore, changed to use bootstrap.php instaed
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92130 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/setPartnerKMCLanguage.php
+++ scripts/utils/setPartnerKMCLanguage.php
@@ -1,26 +1,6 @@
 <?php
-error_reporting ( E_ALL );
-ini_set("memory_limit","512M");
-
-define ( 'KALTURA_ROOT_PATH', realpath ( dirname ( __FILE__ ) . '/../../' ) );
-define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/../../alpha/'));
-define('SF_APP',         'kaltura');
-define('SF_ENVIRONMENT', 'batch');
-define('SF_DEBUG',       true);
-
-require_once (KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR.'server_infra'.DIRECTORY_SEPARATOR.'kConf.php');
-
-// Autoloader
-require_once(KALTURA_ROOT_PATH . DIRECTORY_SEPARATOR . "infra".DIRECTORY_SEPARATOR."KAutoloader.php");
-KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
-KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "alpha", "plugins", "*")); // needed for testmeDoc
-KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "plugins", "*"));
-KAutoloader::setClassMapFilePath(kConf::get("cache_root_path") . '/scripts/' . basename(__FILE__) . '.cache');
-KAutoloader::register();
-
-$dbConf = kConf::getDB ();
-DbManager::setConfig ( $dbConf );
-DbManager::initialize ();
+chdir('/opt/kaltura/app/scripts/');
+require_once 'bootstrap.php';
 
 if ( $argc == 3)
 {	
