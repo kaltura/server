c2f39a65473d1202cd8568c4ccfbf3b49dcaef7a
roman <roman@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-26 12:57:47 +0000

    qnd: standAloneClient - added support for multirequest and user/password instead of secret
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92141 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/standAloneClient/test.php
+++ tests/standAloneClient/test.php
@@ -42,10 +42,22 @@ $xmlFormat = '<xml>
 			<attr2>value2</attr2>
 		</itemName2>
 	</request>
+	<multirequest>
+		<request service="serviceName" action="actionName" plugin="pluginName">
+			<itemName1 objectType="KalturaObjectClass1">
+				<attr1>value1</attr1>
+				<attr2>value2</attr2>
+			</itemName1>
+			<itemName2 objectType="KalturaObjectClass2">
+				<attr1>value1</attr1>
+				<attr2>value2</attr2>
+			</itemName2>
+		</request>
+	</multirequest>
 </xml>';
 
 $inXml = new SimpleXMLElement(file_get_contents($inFile));
-if($inXml->getName() != 'xml' || !isset($inXml->request))
+if($inXml->getName() != 'xml' || (!isset($inXml->request) && !isset($inXml->multirequest)))
 {
 	echo "Input file must match xml format [$xmlFormat]\n";
 	exit;
@@ -121,6 +133,73 @@ function generateSession($adminSecretForSigning, $userId, $type, $partnerId, $ex
 	return $encoded_str;
 }
 
+function executeRequest(KalturaClient $client, SimpleXMLElement $request)
+{
+	$arguments = array();
+	$inputs = $request->children();
+	foreach($inputs as $input)
+		$arguments[$input->getName()] = parseInputObject($input);
+
+	$serviceName = strval($request['service']);
+	$actionName = strval($request['action']);
+	$impersonatePartner = isset($request['partnerId']) ? strval($request['partnerId']) : null;
+	$pluginName = ucfirst(strval($request['plugin']));
+
+	if(isset($pluginName) && $pluginName != '') //get plugin service
+	{
+		$pluginClass = "Kaltura{$pluginName}ClientPlugin";
+		require_once "lib/KalturaPlugins/$pluginClass.php";
+
+		$plugin = call_user_func(array($pluginClass, 'get'), $client);
+		$service = $plugin->$serviceName;
+	}
+	else //get core service
+	{
+		$services = get_object_vars($client);
+		if(!isset($services[$serviceName]))
+		{
+			echo "Service [$serviceName] not found\n";
+			exit;
+		}
+
+		$service = $services[$serviceName];
+	}
+
+	if(!method_exists($service, $actionName))
+	{
+		echo "Action [$actionName] not found on service [$serviceName]\n";
+		exit;
+	}
+
+	$result = null;
+	try
+	{
+		if (!is_null($impersonatePartner))
+		{
+			$config = $client->getConfig();
+			$configPartnerId = $config->partnerId;
+			$config->partnerId = $impersonatePartner;
+		}
+		if ($client->isMultiRequest())
+			$doing = 'Queuing';
+		else
+			$doing = 'Executing';
+		echo "$doing action [$serviceName.$actionName]\n";
+		$result = call_user_func_array(array($service, $actionName), $arguments);
+		if (!is_null($impersonatePartner))
+		{
+			$config = $client->getConfig();
+			$config->partnerId = $configPartnerId;
+		}
+	}
+	catch(Exception $e)
+	{
+		echo "Executing failed [" . $e->getMessage() . "]\n";
+		$result = $e;
+	}
+	return $result;
+}
+
 require_once 'lib/KalturaClient.php';
 
 $config = new KalturaConfiguration();
@@ -143,64 +222,53 @@ if(isset($inXml->session))
 	$userId = isset($inXml->session->userId) ? strval($inXml->session->userId) : '';
 	$expiry = isset($inXml->session->expiry) ? intval($inXml->session->expiry) : 86400;
 	$privileges = isset($inXml->session->privileges) ? strval($inXml->session->privileges) : '';
+	$email = isset($inXml->session->email) ? $inXml->session->email : null;
+	$password = isset($inXml->session->password) ? $inXml->session->password : null;
 
-	$ks = generateSession($secret, $userId, $sessionType, $partnerId, $expiry, $privileges);
+	if ($email)
+	{
+		if (!$password) // ask for password interactively
+		{
+			throw new Exception('Implement ask for password interactively');
+		}
+		$ks = $client->user->loginByLoginId($email, $password);
+	}
+	else
+	{
+		$ks = generateSession($secret, $userId, $sessionType, $partnerId, $expiry, $privileges);
+	}
 	$client->setKs($ks);
 }
 
 $results = array();
-foreach($inXml->request as $request)
+foreach($inXml->children() as $element)
 {
-	$arguments = array();
-	$inputs = $request->children();
-	foreach($inputs as $input)
-		$arguments[$input->getName()] = parseInputObject($input);
-				
-	$serviceName = strval($request['service']);
-	$actionName = strval($request['action']);
-	$pluginName = ucfirst(strval($request['plugin']));
-	
-	if(isset($pluginName) && $pluginName != '') //get plugin service
+	/** @var $element SimpleXMLElement */
+	if ($element->getName() == 'multirequest')
 	{
-		$pluginClass = "Kaltura{$pluginName}ClientPlugin";
-		require_once "lib/KalturaPlugins/$pluginClass.php";
-		
-		$plugin = call_user_func(array($pluginClass, 'get'), $client);
-		$service = $plugin->$serviceName;
-	}
-	else //get core service
-	{
-		$services = get_object_vars($client);
-		if(!isset($services[$serviceName]))
+		$client->startMultiRequest();
+		foreach($element->request as $request)
 		{
-			echo "Service [$serviceName] not found\n";
-			exit;
+			executeRequest($client, $request);
 		}
-		
-		$service = $services[$serviceName];
+		echo "Executing multirequest\n";
+		$multiResponse = $client->doMultiRequest();
+		foreach($multiResponse as $index => $response)
+		{
+			if ($client->isError($response))
+			{
+				echo "Executing failed for request #".($index+1)." with error [" . $response['message'] . "]\n";
+				$response = new KalturaException($response["message"], $response["code"]); // regular request will also put exception object into the result
+			}
+
+			$results[] = $response;
+		}
 	}
-	
-	if(!method_exists($service, $actionName))
+	elseif ($element->getName() == 'request')
 	{
-		echo "Action [$actionName] not found on service [$serviceName]\n";
-		exit;
+		$results[] = executeRequest($client, $element);
 	}
-	
-	$result = null;
-	try 
-	{
-		echo "Executing action [$serviceName.$actionName]\n";
-		$result = call_user_func_array(array($service, $actionName), $arguments);
-	}
-	catch(Exception $e)
-	{
-		echo "Executing failed [" . $e->getMessage() . "]\n";
-		$result = $e;
-	}
-	$results[] = $result;
 }
-
-
 
 
 /* --------  Generate output XML -------- */
@@ -227,7 +295,7 @@ function appandObject(SimpleXMLElement $outXml, $object, $name)
 	}
 	else
 	{
-		$objectXml = $outXml->addChild($name, $object);
+		$objectXml = $outXml->addChild($name, htmlentities($object));
 	}
 }
 
