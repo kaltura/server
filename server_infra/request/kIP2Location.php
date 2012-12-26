7889ddffc604da704c86b2f144a3c0962378782e
svnuser <svnuser@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-26 09:54:10 +0000

    qnd: retain the old UK country code till all components are adjusted to list GB
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92122 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- server_infra/request/kIP2Location.php
+++ server_infra/request/kIP2Location.php
@@ -19,7 +19,11 @@ class kIP2Location
 			$ip2Location = IP2Location_open($ip2LocationBinFilePath, IP2LOCATION_STANDARD);
 			$record = IP2Location_get_country_short($ip2Location, $ip);
 			IP2Location_close($ip2Location);
-			return $record ? $record->country_short : "";
+			$result = $record ? $record->country_short : "";
+			if ($result == "GB") // retain the old UK country code till all components are adjusted to list GB
+				$result = "UK";
+
+			return $result;
 		}
 		
 		ini_set('memory_limit', '128M'); // ip_files are large array files, sometimes it might break if doesn't have enough memory
