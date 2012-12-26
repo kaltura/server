c650f5762131bba08d86434af33b1f58de0b7b24
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-26 14:41:25 +0000

    fwr: new interface for plugins which need to edit the manifest
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92151 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ infra/plugins/interfaces/IKalturaPlayManifestContributor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Interface which allows plugin to add its own content to the playManifest action output.
+ * @package infra
+ * @subpackage Plugins
+ */
+interface IKalturaPlayManifestContributor extends IKalturaBase
+{
+	/**
+	 * Function receives the manifest renderer and edits its output as it requires.
+	 * @param kManifestEditorConfig $config
+	 * @returns array<BaseManifestEditor>
+	 */
+	public static function getManifestEditors ($config);
+}
