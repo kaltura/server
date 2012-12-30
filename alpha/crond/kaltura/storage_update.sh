af49dd740e012ec8d6f8ce514b5202deaabe1cda
svnuser <svnuser@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-12-30 14:53:49 +0000

    cnf: align script with the code in gemeni tag
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92280 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/storage_update.sh
+++ alpha/crond/kaltura/storage_update.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-if [ -L $0 ];then
-	REAL_SCRIPT=`readlink $0`
-else
-	REAL_SCRIPT=$0
-fi
-. `dirname $REAL_SCRIPT`/../../../configurations/system.ini
-
-echo `date`
 
 WHEN=$(date -d "yesterday" +%Y-%m-%d)
 
-php $APP_DIR/scripts/findEntriesSizes.php $WHEN >> $LOG_DIR/`hostname`-findEntriesSizes.log
-php $APP_DIR/scripts/batch/validatePartnerUsage.php >> $LOG_DIR/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
+#php /opt/kaltura/app/scripts/findEntriesSizes.php $WHEN >> /var/log/`hostname`-findEntriesSizes.log
+php /opt/kaltura/app/scripts/batch/validatePartnerUsage.php >> /var/log/`hostname`-BatchPartnerUsage_upgradeProcess.log.${WHEN} 2>&1
+tail /var/log/`hostname`-BatchPartnerUsage_upgradeProcess.log.${WHEN} | mail -s "batchPartnerUsage on `hostname`" it.prod@kaltura.com,records@kaltura.com
