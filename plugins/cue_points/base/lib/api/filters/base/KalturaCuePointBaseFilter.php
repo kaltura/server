e8a240679d214425b020252e598d6ab4fee39a6f
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-03 12:47:44 +0000

    fwr: changed base filter generation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92529 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/cue_points/base/lib/api/filters/base/KalturaCuePointBaseFilter.php
+++ plugins/cue_points/base/lib/api/filters/base/KalturaCuePointBaseFilter.php
@@ -50,12 +50,12 @@ abstract class KalturaCuePointBaseFilter extends KalturaFilter
 
 	public function getMapBetweenObjects()
 	{
-		return array_merge(parent::getMapBetweenObjects(), KalturaCuePointBaseFilter::$map_between_objects);
+		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
 	}
 
 	public function getOrderByMap()
 	{
-		return array_merge(parent::getOrderByMap(), KalturaCuePointBaseFilter::$order_by_map);
+		return array_merge(parent::getOrderByMap(), self::$order_by_map);
 	}
 
 	/**
