b7e7006cbd471cf7dc2ea75a08aca6f9a54993cf
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-03 12:35:16 +0000

    fwr: changed base filter generation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92524 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/partner/filters/base/KalturaPartnerBaseFilter.php
+++ api_v3/lib/types/partner/filters/base/KalturaPartnerBaseFilter.php
@@ -43,12 +43,12 @@ abstract class KalturaPartnerBaseFilter extends KalturaFilter
 
 	public function getMapBetweenObjects()
 	{
-		return array_merge(parent::getMapBetweenObjects(), KalturaPartnerBaseFilter::$map_between_objects);
+		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
 	}
 
 	public function getOrderByMap()
 	{
-		return array_merge(parent::getOrderByMap(), KalturaPartnerBaseFilter::$order_by_map);
+		return array_merge(parent::getOrderByMap(), self::$order_by_map);
 	}
 
 	/**
