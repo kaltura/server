f27a4966be62a11f0c408a31a5344f83f2d1a8b5
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-06 10:44:15 +0000

    qnd: avoid running hashes logic if parameter hashes was not passed
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@92577 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/extwidget/actions/playManifestAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/playManifestAction.class.php
@@ -277,15 +277,17 @@ class playManifestAction extends kalturaAction
 		// enforce access control
 		$base64Referrer = $this->getRequestParameter("referrer");
 		$hashes = $this->getRequestParameter("hashes");
-		$hashes = urldecode($hashes);
-		$hashes = explode(",", $hashes);
 		$keyValueHashes = array(); 
-		foreach ($hashes as $keyValueHashString)
+		if ($hashes)
 		{
-			list ($key, $value) = explode('=', $keyValueHashString);
-			$keyValueHashes[$key] = $value;
+			$hashes = urldecode($hashes);
+			$hashes = explode(",", $hashes);
+			foreach ($hashes as $keyValueHashString)
+			{
+				list ($key, $value) = explode('=', $keyValueHashString);
+				$keyValueHashes[$key] = $value;
+			}
 		}
-		
 		// replace space in the base64 string with + as space is invalid in base64 strings and caused
 		// by symfony calling str_parse to replace + with spaces.
 		// this happens only with params passed in the url path and not the query strings. specifically the ~ char at
