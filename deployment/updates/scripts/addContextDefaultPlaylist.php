09cc2d2bfe209f3dbbc7bf1c91fe5bae9d291cba
yulir <yulir@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-16 16:48:57 +0000

    def: 6649, change limit to 13
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93045 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/addContextDefaultPlaylist.php
+++ deployment/updates/scripts/addContextDefaultPlaylist.php
@@ -29,7 +29,7 @@ else
 	$dbPlaylist->setKshowId ( null );
 	$dbPlaylist->setType ( entryType::PLAYLIST );
 	$dbPlaylist->setMediaType(entry::ENTRY_MEDIA_TYPE_XML);
-	$dbPlaylist->setDataContent('<?xml version="1.0"?><playlist><total_results>12</total_results><filters><filter><in_status>2,1</in_status><in_type>1,2,7</in_type><in_moderation_status>2,5,6,1</in_moderation_status><free_text dynamic="1">context::entry::tags</free_text><_notin_id dynamic="1">context::entry::id</_notin_id><limit>12</limit></filter></filters></playlist>');
+	$dbPlaylist->setDataContent('<?xml version="1.0"?><playlist><total_results>12</total_results><filters><filter><in_status>2,1</in_status><in_type>1,2,7</in_type><in_moderation_status>2,5,6,1</in_moderation_status><free_text dynamic="1">context::entry::tags</free_text><limit>13</limit></filter></filters></playlist>');
 	$dbPlaylist->setDisplayInSearch ( 2 );
 	$dbPlaylist->save();
 }
