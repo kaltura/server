66e37b2b60ae309008ea2bda524331e4d93ad211
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-25 08:31:10 +0000

    qnd: moved unique id to a different file
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93326 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/log/KalturaLog.php
+++ infra/log/KalturaLog.php
@@ -158,30 +158,6 @@ class LogTime
  * @package infra
  * @subpackage log
  */
-class UniqueId
-{
-	static $_uniqueId = null;
-	public function __toString()
-	{
-		if (self::$_uniqueId === null)
-		{
-			self::$_uniqueId = (string)rand();
-			// add a the unique id to Apache's internal variable so we can later log it using the %{KalturaLog_UniqueId}n placeholder
-			// within the LogFormat apache directive. This way each access_log record can be matched with its kaltura log lines.
-			// before setting the apache note name and value, a condition checks if function exists,
-			// due to fact that running from command line will not define this function
-			if (function_exists('apache_note'))
-				apache_note("KalturaLog_UniqueId", self::$_uniqueId);
-		}
-			
-		return self::$_uniqueId;
-	}
-}
-
-/**
- * @package infra
- * @subpackage log
- */
 class LogMethod
 {
 	public function __toString()
