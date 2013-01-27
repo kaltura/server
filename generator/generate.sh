1862fa6958e7847ab32dadd1410a7c76e3347a34
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-27 16:47:23 +0000

    qnd: copy standAloneClient
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93426 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.sh
+++ generator/generate.sh
@@ -19,6 +19,7 @@ php generate.php "$@"
 rsync -avC $output_path/php5ZendClientAdminConsole/Kaltura/Client/ ../admin_console/lib/Kaltura/Client
 rsync -avC $output_path/php5ZendVarConsole/Kaltura/Client/ ../var_console/lib/Kaltura/Client
 rsync -avC $output_path/batchClient/ ../batch/client
+rsync -avC  $output_path/php5/ ../tests/standAloneClient/lib
 rsync -avC  $output_path/php5 ../clients
 
 rm -fr ../cache/batch/*
