80761ab3d90aa79a8c2513fd73960117da19ce0e
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-30 17:36:00 +0000

    qnd: execute first all the files that add permissions
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93553 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/base/scripts/insertPermissions.php
+++ deployment/base/scripts/insertPermissions.php
@@ -16,13 +16,34 @@ KalturaLog::info("Adding permissions from directory [$dirPath]");
 $dir = dir($dirPath);
 /* @var $dir Directory */
 
+$files = array();
 $returnValue = null;
 while (false !== ($fileName = $dir->read()))
 {
-	$filePath = realpath("$dirPath/$fileName");
 	if($fileName[0] == '.')
 		continue;
 
+	$files[] = $fileName;
+}
+
+// execute first all the files that add permissions
+foreach($files as $index => $fileName)
+{
+	$filePath = realpath("$dirPath/$fileName");
+	if(!preg_match('/\[permissions\]/', file_get_contents($filePath)))
+		continue;
+	
+	unset($files[$index]);
+	KalturaLog::info("Adding permissions from file [$filePath]");
+	passthru("php $scriptPath $filePath", $returnValue);
+	if($returnValue !== 0)
+		exit(-1);
+}
+
+// execute all other files
+foreach($files as $index => $fileName)
+{
+	$filePath = realpath("$dirPath/$fileName");
 	KalturaLog::info("Adding permissions from file [$filePath]");
 	passthru("php $scriptPath $filePath", $returnValue);
 	if($returnValue !== 0)
