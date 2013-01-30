64970b70f54c7078b9766c4cf21f0e1310f21466
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-30 15:59:46 +0000

    fwr: when worker starts. move the job to status "processing"
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93548 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/batches/DeleteFile/KAsyncDeleteFile.class.php
+++ batch/batches/DeleteFile/KAsyncDeleteFile.class.php
@@ -27,10 +27,14 @@ class KAsyncDeleteFile extends KJobHandlerWorker
 	 */
 	protected function exec(KalturaBatchJob $job)
 	{
+		$this->updateJob($job, "File deletion started", KalturaBatchJobStatus::PROCESSING);
 		$jobData = $job->data;
 		
 		/* @var $jobData KalturaDeleteFileJobData */
-		unlink($jobData->localFileSyncPath);
+		$result = unlink($jobData->localFileSyncPath);
+		
+		if (!$result)
+			return $this->closeJob($job, KalturaBatchJobErrorTypes::RUNTIME, null, "Failed to delete file from disk", KalturaBatchJobStatus::FAILED);
 		
 		return $this->closeJob($job, null, null, null, KalturaBatchJobStatus::FINISHED);
 		
