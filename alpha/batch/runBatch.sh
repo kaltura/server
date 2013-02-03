823936cf71cc2176662d822366ab4cc9a8240f95
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-03 07:38:29 +0000

    qnd: removed unused scripts
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93598 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/batch/runBatch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. /etc/kaltura.d/system.ini
-
-# $1 is the action (start|stop|restart)
-# $2 is the batch name (optional) 
-cd $(dirname $0)
-source kaltura_env.sh
-$PHP_PATH runBatch.php $1 $2
