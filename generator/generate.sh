89120a071d80a73f5390980fe2de06f9d4c146cc
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-04 14:49:22 +0000

    qnd: updated rsync to delete destination files
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93664 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.sh
+++ generator/generate.sh
@@ -9,10 +9,10 @@ rm -fr $APP_DIR/cache/generator/*
 
 php $APP_DIR/generator/generate.php "$@"
 
-rsync -avC $output_path/php5ZendClientAdminConsole/Kaltura/Client/ $APP_DIR/admin_console/lib/Kaltura/Client
-rsync -avC $output_path/php5ZendVarConsole/Kaltura/Client/ $APP_DIR/var_console/lib/Kaltura/Client
-rsync -avC $output_path/batchClient/ $APP_DIR/batch/client
-rsync -avC  $output_path/php5/ $APP_DIR/tests/standAloneClient/lib
-rsync -avC  $output_path/php5 $APP_DIR/clients
+rsync -av --delete $output_path/php5ZendClientAdminConsole/Kaltura/Client/ $APP_DIR/admin_console/lib/Kaltura/Client
+rsync -av --delete $output_path/php5ZendVarConsole/Kaltura/Client/ $APP_DIR/var_console/lib/Kaltura/Client
+rsync -av --delete $output_path/batchClient/ $APP_DIR/batch/client
+rsync -av --delete $output_path/php5/ $APP_DIR/tests/standAloneClient/lib
+rsync -av --delete $output_path/php5 $APP_DIR/clients
 
 rm -fr $APP_DIR/cache/batch/*
