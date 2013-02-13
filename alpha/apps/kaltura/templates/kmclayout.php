898bfdc77024f81db6f04ef2147cdfb9126913d1
rany <rany@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-13 10:11:39 +0000

    qnd: added ngApp directive
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93927 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/templates/kmclayout.php
+++ alpha/apps/kaltura/templates/kmclayout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html ng-app="kmcApp" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <base href="<?php echo requestUtils::getRequestHost() ?>/" />
 
