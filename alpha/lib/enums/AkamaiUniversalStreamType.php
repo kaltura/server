bd5f334924677030310be16212b703e88c184ff0
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-14 09:36:14 +0000

    fwr: new enum
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@93986 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ alpha/lib/enums/AkamaiUniversalStreamType.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Core
+ * @subpackage model.enum
+ */ 
+interface AkamaiUniversalStreamType extends BaseEnum
+{
+	const UNIVERSAL_STREAMING_LIVE = 'Universal Streaming Live';
+	
+	const HD_IPHONE_IPAD_LIVE = 'HD iPhone/iPad Live';
+	
+}
---
+++ api_v3/lib/types/enums/KalturaAkamaiUniversalStreamType.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package api
+ * @subpackage enum
+ */
+class KalturaAkamaiUniversalStreamType extends KalturaStringEnum implements AkamaiUniversalStreamType
+{
+	 
+}
