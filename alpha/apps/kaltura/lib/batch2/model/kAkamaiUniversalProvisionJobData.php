b83b2f4ec6a9be71b23bf48b281ca45eb22d80d9
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-14 13:14:40 +0000

    fwr: stream name setter not called
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@94013 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/model/kAkamaiUniversalProvisionJobData.php
+++ alpha/apps/kaltura/lib/batch2/model/kAkamaiUniversalProvisionJobData.php
@@ -212,7 +212,7 @@ class kAkamaiUniversalProvisionJobData extends kProvisionJobData
  		$this->setDvrEnabled($entry->getDvrStatus());
  		$this->setDvrWindow($entry->getDvrWindow());
  		$this->setStreamType(AkamaiUniversalStreamType::UNIVERSAL_STREAMING_LIVE);
- 		$this->setStreamName($entry->getStreamName());
+ 		$this->setStreamName($entry->getName());
 		
 	}
 
