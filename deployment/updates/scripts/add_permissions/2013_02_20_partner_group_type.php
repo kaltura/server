7f1357da826f901fb7c9960a4828b949a06c15a4
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-20 08:25:01 +0000

    qnd: granted admin console users to view partner group type
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@94277 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ deployment/updates/scripts/add_permissions/2013_02_20_partner_group_type.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage dragonfly.roles_and_permissions
+ *
+ * Adds permission to view partner group type
+ *
+ * No need to re-run after server code deploy
+ */
+
+$script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
+$config = realpath(dirname(__FILE__) . '../../../../') . '/permissions/object.KalturaPartner.ini';
+passthru("php $script $config");
