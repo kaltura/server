70fecfccd030d522e11dca597b24fffe1ff9c403
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-25 10:42:54 +0000

    fwr: fixed strpos comparison to null==> should be "false"
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@94420 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/KalturaCategoryUser.php
+++ api_v3/lib/types/KalturaCategoryUser.php
@@ -238,7 +238,7 @@ class KalturaCategoryUser extends KalturaObject implements IFilterable {
 		{
 			if($sourceObject->getKuserId() == $category->getKuserId())
 			{
-				if (strpos($this->permissionNames, PermissionName::CATEGORY_EDIT) === null)
+				if (strpos($this->permissionNames, PermissionName::CATEGORY_EDIT) === false)
 				{
 					throw new KalturaAPIException(KalturaErrors::CANNOT_UPDATE_CATEGORY_USER_OWNER);
 				}
