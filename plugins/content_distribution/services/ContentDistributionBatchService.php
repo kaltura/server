41e0ea190eea45855ce9cd6dd953a85d2df11357
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-26 08:23:42 +0000

    qnd: created common base batch service
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@94485 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/content_distribution/services/ContentDistributionBatchService.php
+++ plugins/content_distribution/services/ContentDistributionBatchService.php
@@ -4,14 +4,14 @@
  * @package plugins.contentDistribution
  * @subpackage api.services
  */
-class ContentDistributionBatchService extends BatchService 
+class ContentDistributionBatchService extends KalturaBaseService
 {
-	
+
 // --------------------------------- Distribution Synchronizer functions 	--------------------------------- //
 
 	/**
 	 * updates entry distribution sun status in the search engine
-	 * 
+	 *
 	 * @action updateSunStatus
 	 */
 	function updateSunStatusAction()
@@ -24,8 +24,8 @@ class ContentDistributionBatchService extends BatchService
 		$entryDistributions = EntryDistributionPeer::doSelect($criteria);
 		foreach($entryDistributions as $entryDistribution) // raise the updated events to trigger index in search engine (sphinx)
 			kEventsManager::raiseEvent(new kObjectUpdatedEvent($entryDistribution));
-		
-			
+
+
 		// serach all records that their sun status changed to after sunrise
 		$criteria = KalturaCriteria::create(EntryDistributionPeer::OM_CLASS);
 		$criteria->add(EntryDistributionPeer::SUN_STATUS, EntryDistributionSunStatus::BEFORE_SUNRISE);
@@ -34,11 +34,11 @@ class ContentDistributionBatchService extends BatchService
 		foreach($entryDistributions as $entryDistribution) // raise the updated events to trigger index in search engine (sphinx)
 			kEventsManager::raiseEvent(new kObjectUpdatedEvent($entryDistribution));
 	}
-	
+
 
 	/**
-	 * creates all required jobs according to entry distribution dirty flags 
-	 * 
+	 * creates all required jobs according to entry distribution dirty flags
+	 *
 	 * @action createRequiredJobs
 	 */
 	function createRequiredJobsAction()
@@ -55,8 +55,8 @@ class ContentDistributionBatchService extends BatchService
 			else
 				KalturaLog::err("Distribution profile [" . $entryDistribution->getDistributionProfileId() . "] not found for entry distribution [" . $entryDistribution->getId() . "]");
 		}
-		
-		
+
+
 		// serach all records that arrived their sunrise time and requires submittion
 		$criteria = KalturaCriteria::create(EntryDistributionPeer::OM_CLASS);
 		$criteria->add(EntryDistributionPeer::DIRTY_STATUS, EntryDistributionDirtyStatus::SUBMIT_REQUIRED);
@@ -70,8 +70,8 @@ class ContentDistributionBatchService extends BatchService
 			else
 				KalturaLog::err("Distribution profile [" . $entryDistribution->getDistributionProfileId() . "] not found for entry distribution [" . $entryDistribution->getId() . "]");
 		}
-		
-		
+
+
 		// serach all records that arrived their sunrise time and requires enable
 		$criteria = KalturaCriteria::create(EntryDistributionPeer::OM_CLASS);
 		$criteria->add(EntryDistributionPeer::DIRTY_STATUS, EntryDistributionDirtyStatus::ENABLE_REQUIRED);
@@ -85,8 +85,8 @@ class ContentDistributionBatchService extends BatchService
 			else
 				KalturaLog::err("Distribution profile [" . $entryDistribution->getDistributionProfileId() . "] not found for entry distribution [" . $entryDistribution->getId() . "]");
 		}
-		
-		
+
+
 		// serach all records that arrived their sunset time and requires deletion
 		$criteria = KalturaCriteria::create(EntryDistributionPeer::OM_CLASS);
 		$criteria->add(EntryDistributionPeer::DIRTY_STATUS, EntryDistributionDirtyStatus::DELETE_REQUIRED);
@@ -100,8 +100,8 @@ class ContentDistributionBatchService extends BatchService
 			else
 				KalturaLog::err("Distribution profile [" . $entryDistribution->getDistributionProfileId() . "] not found for entry distribution [" . $entryDistribution->getId() . "]");
 		}
-		
-		
+
+
 		// serach all records that arrived their sunset time and requires disable
 		$criteria = KalturaCriteria::create(EntryDistributionPeer::OM_CLASS);
 		$criteria->add(EntryDistributionPeer::DIRTY_STATUS, EntryDistributionDirtyStatus::DISABLE_REQUIRED);
@@ -116,14 +116,14 @@ class ContentDistributionBatchService extends BatchService
 				KalturaLog::err("Distribution profile [" . $entryDistribution->getDistributionProfileId() . "] not found for entry distribution [" . $entryDistribution->getId() . "]");
 		}
 	}
-	
-	
+
+
 // --------------------------------- Distribution Synchronizer functions 	--------------------------------- //
 
 
 	/**
 	 * returns absolute valid url for asset file
-	 * 
+	 *
 	 * @action getAssetUrl
 	 * @param string $assetId
 	 * @return string
@@ -136,13 +136,13 @@ class ContentDistributionBatchService extends BatchService
 		$asset = assetPeer::retrieveById($assetId);
 		if(!$asset)
 			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_ID, $assetId);
-		
+
 		$ext = $asset->getFileExt();
 		if(is_null($ext))
 			$ext = 'jpg';
-			
+
 		$fileName = $asset->getEntryId() . "_" . $asset->getId() . ".$ext";
-		
+
 		$syncKey = $asset->getSyncKey(asset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
 		if(!kFileSyncUtils::fileSync_exists($syncKey))
 			throw new KalturaAPIException(KalturaErrors::FLAVOR_ASSET_IS_NOT_READY, $asset->getId());
@@ -150,7 +150,7 @@ class ContentDistributionBatchService extends BatchService
 		list($fileSync, $local) = kFileSyncUtils::getReadyFileSyncForKey($syncKey, true, false);
 		if(!$fileSync)
 			throw new KalturaAPIException(KalturaErrors::FLAVOR_ASSET_ID_NOT_FOUND, $asset->getId());
-			
+
 		return $fileSync->getExternalUrl($asset->getEntryId());
 	}
 }
--- plugins/multi_centers/services/FileSyncImportBatchService.php
+++ plugins/multi_centers/services/FileSyncImportBatchService.php
@@ -4,48 +4,49 @@
  * @package plugins.multiCenters
  * @subpackage api.services
  */
-class FileSyncImportBatchService extends BatchService 
+class FileSyncImportBatchService extends KalturaBatchService
 {
 
 
 // --------------------------------- FileSyncImportJob functions 	--------------------------------- //
-	
-	
+
+
 	/**
-	 * batch getExclusiveFileSyncImportJob action allows to get a BatchJob of type FILESYNC_IMPORT 
-	 * 
+	 * batch getExclusiveFileSyncImportJob action allows to get a BatchJob of type FILESYNC_IMPORT
+	 *
 	 * @action getExclusiveFileSyncImportJobs
-	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism  
+	 * @param KalturaExclusiveLockKey $lockKey The unique lock key from the batch-process. Is used for the locking mechanism
 	 * @param int $maxExecutionTime The maximum time in seconds the job reguarly take. Is used for the locking mechanism when determining an unexpected termination of a batch-process.
-	 * @param int $numberOfJobs The maximum number of jobs to return. 
-	 * @param KalturaBatchJobFilter $filter Set of rules to fetch only rartial list of jobs  
-	 * @return KalturaBatchJobArray 
-	 * 
+	 * @param int $numberOfJobs The maximum number of jobs to return.
+	 * @param KalturaBatchJobFilter $filter Set of rules to fetch only rartial list of jobs
+	 * @return KalturaBatchJobArray
+	 *
 	 * TODO remove the destFilePath from the job data and get it later using the api, then delete this method
 	 */
 	function getExclusiveFileSyncImportJobsAction(KalturaExclusiveLockKey $lockKey, $maxExecutionTime, $numberOfJobs, KalturaBatchJobFilter $filter = null)
 	{
-		$jobs = $this->getExclusiveJobsAction($lockKey, $maxExecutionTime, $numberOfJobs, $filter, BatchJobType::FILESYNC_IMPORT );
-		
+		$coreJobs = $this->getExclusiveJobs($lockKey, $maxExecutionTime, $numberOfJobs, $filter, BatchJobType::FILESYNC_IMPORT);
+		$jobs = KalturaBatchJobArray::fromBatchJobArray($jobs);
+
 		if($jobs)
 		{
-			
+
 			foreach ($jobs as $job)
 			{
 				$data = $job->data;
 				// try to get destination path from file sync
 				$fileSyncId = $data->filesyncId;
-				
+
 				$fileSync = FileSyncPeer::retrieveByPK($fileSyncId);
 				if (!$fileSync) {
 					throw new KalturaAPIException(MultiCentersErrors::INVALID_FILESYNC_RECORD, $fileSyncId);
 				}
 				$fileSyncRoot = $fileSync->getFileRoot();
 				$fileSyncPath = $fileSync->getFilePath();
-				
+
 				if ($fileSyncRoot && $fileSyncPath) {
 					// destination path set on filesync
-					$dest_path = $fileSyncRoot.$fileSyncPath;					
+					$dest_path = $fileSyncRoot.$fileSyncPath;
 				}
 				else {
 					// not set on filesync - get path from path manager
@@ -57,33 +58,33 @@ class FileSyncImportBatchService extends BatchService
 					$fileSync->setFilePath($real_path);
 					$fileSync->save();
 				}
-				
+
 				// update job data with destination path if needed
-				if (!$data->destFilePath) {	
+				if (!$data->destFilePath) {
 					$data->destFilePath = $dest_path;
 					$job->data = $data;
 					KalturaLog::log('Updating destination path for job id [$job->id]');
 					$this->updateJob($job);
 				}
-				
+
 				if (!is_dir(dirname($dest_path)) && !@mkdir(dirname($dest_path), 0755, true)) {
 					KalturaLog::ERR("Cannot create directory [$dest_path] - ".error_get_last());
 				}
 			}
 		}
-				
+
 		return $jobs;
 	}
-	
+
 	private function updateJob(KalturaBatchJob $job)
 	{
 		$dbJob = BatchJobPeer::retrieveByPK($job->id);
 		$dbJob = $job->toObject($dbJob);
 		$dbJob->save();
 	}
-	
+
 // --------------------------------- End of FileSyncImportJob functions 	------------------------ //
 
 
-	
+
 }
