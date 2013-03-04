e04536b3a1e5cded5b8a52efc93e75cf5622bde6
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-04 08:57:13 +0000

    qnd: sub forms do not require additional validation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@94884 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/drop_folder/admin/forms/ContentFileHandlerConfig.php
+++ plugins/drop_folder/admin/forms/ContentFileHandlerConfig.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 /**
  * @package plugins.dropFolder
  * @subpackage Admin
  */
-class Form_ContentFileHandlerConfig extends Infra_Form
+class Form_ContentFileHandlerConfig extends Zend_Form_SubForm
 {
 	public function init()
 	{
@@ -11,13 +11,13 @@ class Form_ContentFileHandlerConfig extends Infra_Form
 		$fileDeletePolicies->setLabel('Content Match Policy:');
 		$fileDeletePolicies->setRequired(true);
 		$this->addElement($fileDeletePolicies);
-		
+
 		$this->addElement('text', 'slugRegex', array(
 			'label' 		=> 'Slug Regex:',
 		    'value' 		=> '/(?P<referenceId>.+)[.]\w{2,}/',
 			'filters'		=> array('StringTrim'),
 		));
-				
+
 		$this->setDecorators(array(
 	        'FormElements',
 	        array('HtmlTag', array('tag' => 'span', 'id' => 'frmContentFileHandlerConfig')),
--- plugins/drop_folder/admin/forms/TroubleshootConfig.php
+++ plugins/drop_folder/admin/forms/TroubleshootConfig.php
@@ -3,64 +3,64 @@
  * @package plugins.dropFolder
  * @subpackage Admin
  */
-class Form_TroubleshootConfig extends Infra_Form
+class Form_TroubleshootConfig extends Zend_Form_SubForm
 {
 	//		$this->view->tr
-	
+
 	public function init()
 	{
 		$this->addElement('hidden', 'crossLine5', array(
 			'decorators' => array('ViewHelper', array('Label', array('placement' => 'append')), array('HtmlTag',  array('tag' => 'hr', 'class' => 'crossLine')))
 		 ));
-		
+
 		$titleElement = new Zend_Form_Element_Hidden('troubleshootTitle');
 		$titleElement->setLabel('Troubleshoot');
 		$titleElement->setDecorators(array('ViewHelper', array('Label', array('placement' => 'append')), array('HtmlTag',  array('tag' => 'b'))));
 		$this->addElement($titleElement);
-		
+
 		$status = new Kaltura_Form_Element_EnumSelect('status', array('enum' => 'Kaltura_Client_DropFolder_Enum_DropFolderStatus'));
 		$status->setLabel('Status:');
 		$status->setAttrib('readonly', true);
 		$status->setAttrib('disabled', 'disabled');
 		$this->addElement($status);
-		
+
 		$this->addElement('text', 'lastAccessedAt', array(
 			'label'			=> 'Last Drop Folder Access Time/Date:',
 			'disabled'		=>true,
 			'filters'		=> array('StringTrim'),
 		));
-			
+
 		$this->addElement('textarea', 'errorDescription', array(
 			'label'			=> 'Error Description:',
 			'disabled'		=>true,
 			'rows'			=> 3,
 			'filters'		=> array('StringTrim'),
-		));	
+		));
 
 		$this->setDecorators(array(
 	    	'FormElements',
 	        array('HtmlTag', array('tag' => 'span', 'id' => 'frmTroubleshootConfig')),
         ));
-		
+
 	}
 
 	public function populateFromObject($object, $add_underscore = true)
 	{
 		parent::populateFromObject($object, $add_underscore);
-				
-		$lastAccessedAtElm = $this->getElement('lastAccessedAt');	
+
+		$lastAccessedAtElm = $this->getElement('lastAccessedAt');
 		if($lastAccessedAtElm->getValue())
 			$formattedDate = date($this->getView()->translate('time format'), $lastAccessedAtElm->getValue());
 		else
 			$formattedDate = 'N/A';
 		$lastAccessedAtElm->setValue($formattedDate);
-		
+
 		if($object->status !== Kaltura_Client_DropFolder_Enum_DropFolderStatus::ERROR)
 		{
-			$descElm = $this->getElement('errorDescription');		
+			$descElm = $this->getElement('errorDescription');
 			$descElm->setAttrib('style', 'display:none');
 			$descElm->setLabel('');
 		}
 	}
-	
+
  }
