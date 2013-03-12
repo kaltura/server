923d88f4ce731cd4b7c72b4765437c31c505f39a
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-12 23:31:28 +0000

    fwr: permission script
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@95434 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/add_permissions/2013_03_13_tagsearch_add_new_action.php
+++ deployment/updates/scripts/add_permissions/2013_03_13_tagsearch_add_new_action.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * @package deployment
- * @subpackage dragonfly.roles_and_permissions
- *
- * Adds permission to view partner group type
- *
+ * @subpackage falcon.roles_and_permissions
+ * 
+ * Adds event notification plugin permissions
  * No need to re-run after server code deploy
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
-$config = realpath(dirname(__FILE__) . '../../../../') . '/permissions/service.tagsearch.tag.ini';
-passthru("php $script $config");
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.tagsearch.tag.ini';
+passthru("php $script $config"); 
