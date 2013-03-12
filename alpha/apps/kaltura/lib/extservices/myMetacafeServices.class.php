4e98ed4dd3e09466dad77bb00215e3d2ed55447a
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-12 15:42:25 +0000

    qnd: support https installation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@95382 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/extservices/myMetacafeServices.class.php
+++ alpha/apps/kaltura/lib/extservices/myMetacafeServices.class.php
@@ -5,24 +5,24 @@
  */
 class myMetacafeServices extends myBaseMediaSource implements IMediaSource
 {
-	protected $supported_media_types =  self::SUPPORT_MEDIA_TYPE_VIDEO ;  
+	protected $supported_media_types =  self::SUPPORT_MEDIA_TYPE_VIDEO ;
 	protected $source_name = "Metacafe";
 	protected $auth_method = array ( self::AUTH_METHOD_PUBLIC );
-	protected $search_in_user = false; 
+	protected $search_in_user = false;
 	protected $logo = "http://www.kaltura.com/images/wizard/logo_metacafe.png";
 	protected $id = entry::ENTRY_MEDIA_SOURCE_METACAFE;
-	
+
 	private static $domain = "http://www.metacafe.com";
-		
+
 	private static $NEED_MEDIA_INFO = "0";
-		
+
 	const USER_AGENT = "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)";
 
-	public function getMediaInfo( $media_type ,$objectId) 
+	public function getMediaInfo( $media_type ,$objectId)
 	{
-		return null;		
+		return null;
 	}
-		
+
 	public function searchMedia( $media_type , $searchText, $page, $pageSize, $authData = null, $extraData = null)
 	{
 		if( $media_type == entry::ENTRY_MEDIA_TYPE_VIDEO )
@@ -33,38 +33,38 @@ class myMetacafeServices extends myBaseMediaSource implements IMediaSource
 		else
 		{
 			// this provider does not supply media type $media_type
-		}		
-		return null;		
-		
+		}
+		return null;
+
 	}
 
 	public function getAuthData( $kuserId, $userName, $password, $token)
 	{
 		return;
 	}
-	
+
 // --------------------------------------------------------------------------------
-	
+
 	private static function search($searchText, $page, $pageSize)
 	{
 		if (!$pageSize)
 			$pageSize = 100;
-			
+
 		$pageSize = min($pageSize, 100);
-		
+
 		$startIndex = ($page - 1) * $pageSize;
 		$url = self::$domain."/api/videos/?vq=".urlencode($searchText)."&max-results=".$pageSize."&start-index=".$startIndex."&time=all_time";
 
 		$content = kFile::downloadUrlToString($url);
-		
+
 		return $content;
 	}
-	
+
 	private static function parse($content)
 	{
 		$items = array();
 		$message = '';
-		
+
 		$doc = new DOMDocument();
 		if ($doc->loadXML($content))
 		{
@@ -89,20 +89,20 @@ class myMetacafeServices extends myBaseMediaSource implements IMediaSource
 					$tags = self::getXmlNodeValue($itemNode, "keywords", null, $ns);
 					$credit = self::getXmlNodeValue($itemNode, "credit", null, $ns);
 					$link = self::getXmlNodeValue($itemNode, "link");
-					
+
 					$items[] = array(
 							'id' => $id,
 							'title' => $title,
-							'thumb' => $thumbnail, 
+							'thumb' => $thumbnail,
 							'description' => $description,
 							'tags' => $tags,
-							'license' => '', 
+							'license' => '',
 							'credit' => $credit,
 							'source_link' => $link,
-							'url' => "http://".kConf::get("www_host")."/index.php/extservices/metacafeRedirect/itemId/".$id
+							'url' => kConf::get("apphome_url") . "/index.php/extservices/metacafeRedirect/itemId/".$id
 						);
 				}
-									
+
 				$status = "ok";
 			}
 		}
@@ -113,18 +113,18 @@ class myMetacafeServices extends myBaseMediaSource implements IMediaSource
 
 		return array('status' => $status, 'message' => $message, 'objects' => $items , "needMediaInfo" => self::$NEED_MEDIA_INFO);
 	}
-	
+
 	private static function getXmlNodeValue($element, $nodeName, $attrName = null, $ns = null)
 	{
 		if ($ns)
 			$titleNodes = $element->getElementsByTagNameNS($ns, $nodeName);
 		else
 			$titleNodes = $element->getElementsByTagName($nodeName);
-		
+
 		if ($titleNodes->length)
 		{
 			$node = $titleNodes->item(0);
-			
+
 			return $attrName ? $node->getAttribute($attrName) : $node->nodeValue;
 		}
 		return "";
