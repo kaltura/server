ddc880b21efe5bffc4502c42a946fe706f6f69cc
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-13 13:40:48 +0000

    qnd: typo in method name
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@95491 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/batches/Cleanup/KAsyncDirectoryCleanup.class.php
+++ batch/batches/Cleanup/KAsyncDirectoryCleanup.class.php
@@ -5,7 +5,7 @@
  */
 
 /**
- * Will run periodically and cleanup directories from old files that have a specific pattern (older than x days) 
+ * Will run periodically and cleanup directories from old files that have a specific pattern (older than x days)
  *
  * @package Scheduler
  * @subpackage Cleanup
@@ -19,7 +19,7 @@ class KAsyncDirectoryCleanup extends KPeriodicWorker
 	{
 		return KalturaBatchJobType::CLEANUP;
 	}
-	
+
 	/* (non-PHPdoc)
 	 * @see KBatchBase::getJobType()
 	 */
@@ -27,50 +27,50 @@ class KAsyncDirectoryCleanup extends KPeriodicWorker
 	{
 		return self::getType();
 	}
-	
+
 	/* (non-PHPdoc)
 	 * @see KBatchBase::run()
 	*/
 	public function run($jobs = null)
 	{
 		KalturaLog::info("Directory Cleanup is running");
-		
+
 		$path = $this->getAdditionalParams("path");
 		$pattern = $this->getAdditionalParams("pattern");
 		$simulateOnly = $this->getAdditionalParams("simulateOnly");
 		$minutesOld = $this->getAdditionalParams("minutesOld");
 		$searchPath = $path . $pattern;
 		KalturaLog::debug("Searching [$searchPath]");
-		
+
 		if($this->getAdditionalParams("usePHP"))
 		{
-			$this->deleteFilePHP($searchPath, $minutesOld, $simulateOnly);
+			$this->deleteFilesPHP($searchPath, $minutesOld, $simulateOnly);
 		}
 		else
 		{
 			$this->deleteFilesLinux($searchPath, $minutesOld, $simulateOnly);
 		}
 	}
-	
+
 	// XXX - If this function forces deletion of files in the given directory. If it is used with params
 	// given from the user - Please add input validation.
 	protected function deleteFilesLinux($searchPath, $minutesOld, $simulateOnly)
 	{
 		$command = "find $searchPath -mmin +$minutesOld -exec rm -rf {} \;";
 		KalturaLog::info("Executing command: $command");
-		
+
 		$returnedValue = null;
 		passthru($command, $returnedValue);
 		KalturaLog::info("Returned value [$returnedValue]");
 	}
-	
+
 	protected function deleteFilesPHP($searchPath, $minutesOld, $simulateOnly)
 	{
 		$secondsOld = $minutesOld * 60;
-		
+
 		$files = glob ( $searchPath);
 		KalturaLog::debug("Found [" . count ( $files ) . "] to scan");
-		
+
 		$now = time();
 		KalturaLog::debug("The time now is: " . date('c', $now));
 		KalturaLog::debug("Deleting files that are " . $secondsOld ." seconds old (modified before " . date('c', $now - $secondsOld) . ")");
@@ -78,20 +78,20 @@ class KAsyncDirectoryCleanup extends KPeriodicWorker
 		foreach ( $files as $file )
 		{
 			$filemtime = filemtime($file);
-			if ($filemtime > $now - $secondsOld) 
+			if ($filemtime > $now - $secondsOld)
 				continue;
-			
+
 			if ( $simulateOnly )
 			{
-				KalturaLog::debug( "Simulating: Deleting file [$file], it's last modification time was " . date('c', $filemtime)); 
+				KalturaLog::debug( "Simulating: Deleting file [$file], it's last modification time was " . date('c', $filemtime));
 				continue;
 			}
-			
+
 			KalturaLog::debug("Deleting file [$file], it's last modification time was " . date('c', $filemtime));
 			$res = @unlink ( $file );
 			if ( ! $res ){
 				KalturaLog::debug("Error: problem while deleting [$file]");
-				continue;	
+				continue;
 			}
 			$deletedCount++;
 		}
