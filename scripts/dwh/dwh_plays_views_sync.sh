2203d8bd296da0b7bc3869372a5384ab5c395f0d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-04-07 11:17:43 +0000

    def: 6398: log time and script info
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@96348 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/dwh/dwh_plays_views_sync.sh
+++ scripts/dwh/dwh_plays_views_sync.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 . /etc/kaltura.d/system.ini
 
-mysql -h$DWH_HOST -P$DWH_PORT -u$DWH_USER -p$DWH_PASS < $BASE_DIR/app/scripts/dwh/trigger.sql |sed -e '1d' |php $BASE_DIR/app/scripts/dwh/updateEntryPlaysViews.php
-mysql -h$DWH_HOST -P$DWH_PORT -u$DWH_USER -p$DWH_PASS < $BASE_DIR/app/scripts/dwh/wrap.sql
+echo `date`
 
+echo "start dwh triggers"
+mysql -h$DWH_HOST -P$DWH_PORT -u$DWH_USER -p$DWH_PASS < $BASE_DIR/app/scripts/dwh/trigger.sql |sed -e '1d' |php $BASE_DIR/app/scripts/dwh/updateEntryPlaysViews.php
+echo "start dwh wrap"
+mysql -h$DWH_HOST -P$DWH_PORT -u$DWH_USER -p$DWH_PASS < $BASE_DIR/app/scripts/dwh/wrap.sql
+echo "end dwh"
