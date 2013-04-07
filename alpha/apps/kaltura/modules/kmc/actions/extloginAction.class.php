446e5c188120356312b32213e205cdfee094d17e
svnuser <svnuser@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-04-07 09:21:08 +0000

    qnd: revert https enforcement bug
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@96340 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/actions/extloginAction.class.php
+++ alpha/apps/kaltura/modules/kmc/actions/extloginAction.class.php
@@ -5,7 +5,7 @@
  */
 class extloginAction extends kalturaAction
 {
-
+	
 	private function dieOnError($error_code)
 	{
 		if ( is_array ( $error_code ) )
@@ -18,35 +18,35 @@ class extloginAction extends kalturaAction
 			$args = func_get_args();
 		}
 		array_shift($args);
-
+		
 		$error = explode(",", $error_code, 2);
-
+		
 		$error_code = $error[0];
 		$error_message = $error[1];
 
-		$formated_desc = @call_user_func_array('sprintf', array_merge((array)$error_message, $args));
-
+		$formated_desc = @call_user_func_array('sprintf', array_merge((array)$error_message, $args)); 
+		
 		header("X-Kaltura:error-$error_code");
 		header('X-Kaltura-App: exiting on error '.$error_code.' - '.$formated_desc);
-
+		
 		die();
 	}
-
+	
 	public function execute()
 	{
 		$ks = $this->getP ( "ks" );
 		$requestedPartnerId = $this->getP ( "partner_id" );
-
+		
 		$expired = $this->getP ( "exp" );
 
 		$ksObj = kSessionUtils::crackKs($ks);
 		$ksPartnerId = $ksObj->partner_id;
-
+		
 
 		if (!$requestedPartnerId) {
-			$requestedPartnerId = $ksPartnerId;
+			$requestedPartnerId = $ksPartnerId; 
 		}
-
+		
 		try {
 			$adminKuser = UserLoginDataPeer::userLoginByKs($ks, $requestedPartnerId, true);
 		}
@@ -78,19 +78,19 @@ class extloginAction extends kalturaAction
 		if (!$adminKuser || !$adminKuser->getIsAdmin()) {
 			$this->dieOnError  ( APIErrors::ADMIN_KUSER_NOT_FOUND );
 		}
-
-
+		
+		
 		if ($requestedPartnerId != $adminKuser->getPartnerId()) {
 			$this->dieOnError  ( APIErrors::UNKNOWN_PARTNER_ID );
 		}
-
+		
 		$partner = PartnerPeer::retrieveByPK( $adminKuser->getPartnerId() );
-
+		
 		if (!$partner)
 		{
-			$this->dieOnError  ( APIErrors::UNKNOWN_PARTNER_ID );
+			$this->dieOnError  ( APIErrors::UNKNOWN_PARTNER_ID );	
 		}
-
+		
 		if (!$partner->validateApiAccessControl())
 		{
 			$this->dieOnError  ( APIErrors::SERVICE_ACCESS_CONTROL_RESTRICTED );
@@ -99,31 +99,31 @@ class extloginAction extends kalturaAction
 		$partner_id = $partner->getId();
 		$subp_id = $partner->getSubpId() ;
 		$admin_puser_id = $adminKuser->getPuserId();
-
+		
 		$exp = (isset($expired) && is_numeric($expired)) ? time() + $expired: 0;
-
+		
 		$noUserInKs = is_null($ksObj->user) || $ksObj->user === '';
 		if ( ($ksPartnerId != $partner_id) || ($partner->getKmcVersion() >= 4 && $noUserInKs) )
 		{
-			$ks = null;
+			$ks = null;	
 			$sessionType = $adminKuser->getIsAdmin() ? SessionType::ADMIN : SessionType::USER;
 			kSessionUtils::createKSessionNoValidations ( $partner_id ,  $admin_puser_id , $ks , 30 * 86400 , $sessionType , "" , "*," . kSessionBase::PRIVILEGE_DISABLE_ENTITLEMENT );
 		}
-
-
+		
+		
 		$path = "/";
 		$domain = null;
 		$force_ssl = PermissionPeer::isValidForPartner(PermissionName::FEATURE_KMC_ENFORCE_HTTPS, $partner_id);
-		$secure = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || $force_ssl) ? true : false;
+		$secure = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' && $force_ssl) ? true : false;
 		$http_only = true;
-
+		
 		$this->getResponse()->setCookie("pid", $partner_id, $exp, $path, $domain, $secure, $http_only);
 		$this->getResponse()->setCookie("subpid", $subp_id, $exp, $path, $domain, $secure, $http_only);
 		$this->getResponse()->setCookie("kmcks", $ks, $exp, $path, $domain, $secure, $http_only);
 
-		$redirect_url =  ($secure) ? 'https' : 'http';
+		$redirect_url =  ($force_ssl) ? 'https' : 'http';
 		$redirect_url .= '://' . $_SERVER["HTTP_HOST"] . '/index.php/kmc/kmc2';
 		$this->redirect($redirect_url);
 	}
-
+	
 }
