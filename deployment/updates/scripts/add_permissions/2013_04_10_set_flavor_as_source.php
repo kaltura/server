8d7f99802ccae31d93fe7632811664cbbb64709b
sharona <sharona@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-04-11 08:32:06 +0000

    qnd: Script path update.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@96655 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/add_permissions/2013_04_10_set_flavor_as_source.php
+++ deployment/updates/scripts/add_permissions/2013_04_10_set_flavor_as_source.php
@@ -9,5 +9,5 @@
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
-$config = realpath(dirname(__FILE__) . '../../../') . '/permissions/service.flavorasset.ini';
+$config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/service.flavorasset.ini';
 passthru("php $script $config");
