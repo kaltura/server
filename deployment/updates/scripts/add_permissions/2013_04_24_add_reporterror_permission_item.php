f5f2767389691ed43d32d4f067bb20bcafbdd480
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-04-24 07:56:34 +0000

    fwr: added action "reportError" to permissions
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@97160 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/scripts/add_permissions/2013_04_24_add_reporterror_permission_item.php
+++ deployment/updates/scripts/add_permissions/2013_04_24_add_reporterror_permission_item.php
@@ -9,5 +9,5 @@
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/scripts/utils/permissions/addPermissionsAndItems.php';
-$config = realpath(dirname(__FILE__) . '../../../../') . '/permissions/service.stats.ini';
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.stats.ini';
 passthru("php $script $config");
