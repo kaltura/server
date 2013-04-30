fcdf5d5323eecd300b458e45f509bfb283c6ee73
erank <erank@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-04-30 17:52:12 +0000

    qnd: changed syncInvalidSessionToMemcache to use the regular scripts bootstrap
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@97571 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/utils/syncInvalidSessionsToMemcache.php
+++ scripts/utils/syncInvalidSessionsToMemcache.php
@@ -9,22 +9,7 @@ $MC_PORT = 	 	$argv[2];
 define('EXPIRY_TIME_MARGIN', 600);
 define('PAGE_SIZE', 1000);
 
-set_time_limit(0);
-ini_set("memory_limit","1024M");
-
-define('ROOT_DIR', realpath(dirname(__FILE__) . '/../../'));
-require_once(ROOT_DIR . '/infra/KAutoloader.php');
-
-KAutoloader::addClassPath(KAutoloader::buildPath(KALTURA_ROOT_PATH, "vendor", "propel", "*"));
-KAutoloader::setClassMapFilePath(kConf::get("cache_root_path") . '/scripts/' . basename(__FILE__) . '.cache');
-KAutoloader::register();
-
-error_reporting(E_ALL);
-KalturaLog::setLogger(new KalturaStdoutLogger());
-
-$dbConf = kConf::getDB();
-DbManager::setConfig($dbConf);
-DbManager::initialize();
+require_once(dirname(__FILE__).'/../bootstrap.php');
 
 myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_PROPEL3;
 
