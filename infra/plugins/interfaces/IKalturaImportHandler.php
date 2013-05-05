111de06972ce4090594d5a4d0ec6661a58ff5987
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-05-05 07:12:05 +0000

    fwr: added doccomment
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@97718 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/plugins/interfaces/IKalturaImportHandler.php
+++ infra/plugins/interfaces/IKalturaImportHandler.php
@@ -7,6 +7,7 @@
 interface IKalturaImportHandler extends IKalturaBase
 {
 	/**
+	 * This method makes an intermediate change to the imported file or its related data under certain conditions.
 	 * @param KCurlHeaderResponse $curlInfo
 	 * @param KalturaImportJobData $importData
 	 */
