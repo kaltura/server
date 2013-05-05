88dbb4578381902164ce436b8b5535e0c965392d
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-05-05 11:51:25 +0000

    qnd: use testsClient configuration  for tests client lib
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@97756 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- generator/generate.sh
+++ generator/generate.sh
@@ -12,7 +12,7 @@ php $APP_DIR/generator/generate.php "$@"
 rsync -av --delete $output_path/php5ZendClientAdminConsole/Kaltura/Client/ $APP_DIR/admin_console/lib/Kaltura/Client
 rsync -av --delete $output_path/php5ZendVarConsole/Kaltura/Client/ $APP_DIR/var_console/lib/Kaltura/Client
 rsync -av --delete $output_path/batchClient/ $APP_DIR/batch/client
-rsync -av --delete $output_path/php5/ $APP_DIR/tests/lib
+rsync -av --delete $output_path/testsClient/ $APP_DIR/tests/lib
 rsync -av --delete $output_path/php5 $APP_DIR/clients
 
 rm -fr $APP_DIR/cache/batch/*
