7a7ccc36dfe0a5bc212d0c940511abb76d485c73
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-05-16 13:52:18 +0000

    qnd: PHP Warning:  DOMDocument::loadXML(): Empty string supplied as input
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@98327 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/general/KDOMDocument.php
+++ infra/general/KDOMDocument.php
@@ -13,11 +13,17 @@ class KDOMDocument extends DOMDocument
 
 	public function load ( $filename , $options = 0 )
 	{
+		if(!file_exists($filename) || !filesize($filename))
+			throw new Exception('Empty file supplied as input');
+			
 		return parent::loadXML(file_get_contents($filename), $options);
 	}
 
 	public function schemaValidate ( $filename )
 	{
+		if(!file_exists($filename) || !filesize($filename))
+			throw new Exception('Empty file supplied as input');
+		
 		return parent::schemaValidateSource(file_get_contents($filename));
 	}
 
