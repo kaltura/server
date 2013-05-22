b0dc1528e7d896291f3d95b640b561a75b3e0e71
svnuser <svnuser@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-05-22 12:27:10 +0000

    qnd: fix alter, change  delete_id default value to 0 instead of NULL
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@98612 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2013_05_07_file_sync_large_numbers.sql
+++ deployment/updates/sql/2013_05_07_file_sync_large_numbers.sql
@@ -1,6 +1,6 @@
 ALTER TABLE file_sync 
 MODIFY id BIGINT(20) NOT NULL AUTO_INCREMENT, 
 MODIFY linked_id BIGINT, 
-ADD deleted_id BIGINT DEFAULT NULL AFTER custom_data,
+ADD deleted_id BIGINT DEFAULT 0 AFTER custom_data,
 DROP INDEX object_id_object_type_version_subtype_dc, 
 ADD UNIQUE KEY unique_index (object_id,object_type,version,object_sub_type,dc,deleted_id);
