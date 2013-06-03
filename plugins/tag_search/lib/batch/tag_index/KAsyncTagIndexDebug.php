e90633acb8e83ea3342fe36a2a5961b44af0f93d
hila <hila@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-03 10:53:24 +0000

    fwr: updated debug file
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@99201 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/tag_search/lib/batch/tag_index/KAsyncTagIndexDebug.php
+++ plugins/tag_search/lib/batch/tag_index/KAsyncTagIndexDebug.php
@@ -4,11 +4,11 @@
  * @subpackage Debug
  */
 
-chdir(dirname( __FILE__ ) . "/../../");
+chdir(dirname( __FILE__ ) . "/../../../../batch");
 
 require_once("bootstrap.php");
 
-$iniFile = "batch_config.ini";		// should be the full file path
+$iniFile = "../configurations/batch";;		// should be the full file path
 
 $kdebuger = new KGenericDebuger($iniFile);
 $kdebuger->run('KAsyncTagIndex');
