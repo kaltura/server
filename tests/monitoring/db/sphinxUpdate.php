cd75d3ac9c813249ace010d1ac096e53133b0b54
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-04 06:22:07 +0000

    qnd: added debug mesages
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@99259 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- tests/monitoring/db/sphinxUpdate.php
+++ tests/monitoring/db/sphinxUpdate.php
@@ -31,6 +31,12 @@ $config = parse_ini_file(__DIR__ . '/../config.ini', true);
 try
 {
 	// connect to the db
+	if(isset($options['debug']))
+	{
+		echo "Log dns [" . $config['sphinx-log']['dsn'] . "]\n";
+		echo "Sphinx host [$hostname]\n";
+	}
+		
 	$logPdo = new PDO($config['sphinx-log']['dsn'], $config['sphinx-log']['username'], $config['sphinx-log']['password']);
 	$sphinxPdo = new PDO("mysql:host=$hostname;port=9312;");
 	
@@ -39,12 +45,20 @@ try
 	// insert or update sphinx log
 	$replaceQuery = "replace into kaltura_entry (id, entry_id, str_entry_id) values(1, \'MONITOR_TEST\', \'$updatedEntryId\')";
 	$insertLogQuery = "INSERT INTO sphinx_log (object_type, object_id, partner_id, dc, `sql`, created_at) VALUES ('test', '$updatedEntryId', -4, 0, '$replaceQuery', NOW())";
+	$selectQuery = "select str_entry_id from kaltura_entry where id = 1";
+
+	if(isset($options['debug']))
+	{
+		echo "Replace Query [$replaceQuery]\n";
+		echo "Insert Log Query [$insertLogQuery]\n";
+		echo "Select Query [$selectQuery]\n";
+	}
+	
 	$affectedRows = $logPdo->exec($insertLogQuery);
 	if(!$affectedRows)
 		throw new Exception("Unable to insert log: $insertLogQuery");
 		
 	$timeoutTime = time() + $timeout;
-	$selectQuery = "select str_entry_id from kaltura_entry where id = 1";
 	$selectedEntryId = null;
 	
 	while($selectedEntryId != $updatedEntryId && $timeoutTime > time())
