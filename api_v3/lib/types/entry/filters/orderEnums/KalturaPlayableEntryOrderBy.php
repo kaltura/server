ac43d911747901c6711d297aa18e58dc9eab0586
rotema <rotema@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-04 14:04:57 +0000

    def: 8094 removing msDuration filtering and ordering
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@99307 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/entry/filters/orderEnums/KalturaPlayableEntryOrderBy.php
+++ api_v3/lib/types/entry/filters/orderEnums/KalturaPlayableEntryOrderBy.php
@@ -11,6 +11,4 @@ class KalturaPlayableEntryOrderBy extends KalturaBaseEntryOrderBy
 	const VIEWS_DESC = "-views";
 	const DURATION_ASC = "+duration";
 	const DURATION_DESC = "-duration";
-	const MS_DURATION_ASC = "+msDuration";
-	const MS_DURATION_DESC = "-msDuration";
 }
