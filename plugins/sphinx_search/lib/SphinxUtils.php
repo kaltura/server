98c3749c39017483b5f592090e45517c0e32985a
yulir <yulir@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-16 13:35:28 +0000

    def: 8440 add < > to sphinx escape characters
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@99854 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/lib/SphinxUtils.php
+++ plugins/sphinx_search/lib/SphinxUtils.php
@@ -13,16 +13,16 @@ class SphinxUtils
 			// NOTE: it appears that sphinx performs double decoding on SELECT values, so we encode twice.
 			//		" and ! are escaped once to enable clients to use them, " = exact match, ! = AND NOT
 			//	This code could have been implemented more elegantly using array_map, but this implementation is the fastest
-			$from 		= array ('\\', 		'"', 		'!',		'(',		')',		'|',		'-',		'@',		'~',		'&',		'/',		'^',		'$',		'=',		'_',		'%', 		'\'',		);
+			$from 		= array ('\\', 		'"', 		'!',		'(',		')',		'|',		'-',		'@',		'~',		'&',		'/',		'^',		'$',		'=',		'_',		'%', 		'\'',		'<',		'>',		);
 	
 			if ($iterations == 2)
 			{
-				$toDouble   = array ('\\\\', 	'\\"', 		'\\!',		'\\\\\\(',	'\\\\\\)',	'\\\\\\|',	'\\\\\\-',	'\\\\\\@',	'\\\\\\~',	'\\\\\\&',	'\\\\\\/',	'\\\\\\^',	'\\\\\\$',	'\\\\\\=',	'\\\\\\_',	'\\\\\\%',	'\\\\\\\'',	);
+				$toDouble   = array ('\\\\', 	'\\"', 		'\\!',		'\\\\\\(',	'\\\\\\)',	'\\\\\\|',	'\\\\\\-',	'\\\\\\@',	'\\\\\\~',	'\\\\\\&',	'\\\\\\/',	'\\\\\\^',	'\\\\\\$',	'\\\\\\=',	'\\\\\\_',	'\\\\\\%',	'\\\\\\\'',	'\\\\\\<',	'\\\\\\>',	);
 				return str_replace($from, $toDouble, $str);
 			}
 			else
 			{
-				$toSingle   = array ('\\\\', 	'\\"', 		'\\!',		'\\(',		'\\)',		'\\|',		'\\-',		'\\@',		'\\~',		'\\&',		'\\/',		'\\^',		'\\$',		'\\=',		'\\_',		'\\%',		'\\\'',		);
+				$toSingle   = array ('\\\\', 	'\\"', 		'\\!',		'\\(',		'\\)',		'\\|',		'\\-',		'\\@',		'\\~',		'\\&',		'\\/',		'\\^',		'\\$',		'\\=',		'\\_',		'\\%',		'\\\'',		'\\<',		'\\>',		);
 				return str_replace($from, $toSingle, $str);
 			}
 		}
