1e89d321fe15527e95a54572f827f1835c1a7ed5
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-16 08:58:15 +0000

    qnd: reorganized initialization
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@99791 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- server_infra/bootstrap_base.php
+++ server_infra/bootstrap_base.php
@@ -4,11 +4,3 @@
  * @subpackage autoloader
  */
 define("KALTURA_ROOT_PATH", realpath(dirname(__FILE__).DIRECTORY_SEPARATOR."../"));
-
-if (!defined("SF_ROOT_DIR"))    // when bootstraping api v3 under symfony, this will throw notices
-{
-	define("SF_ROOT_DIR",   KALTURA_ROOT_PATH.DIRECTORY_SEPARATOR."alpha");
-	define("SF_APP",         'kaltura'); 
-	define("SF_ENVIRONMENT", 'prod');
-	define("SF_DEBUG",       false);
-}
