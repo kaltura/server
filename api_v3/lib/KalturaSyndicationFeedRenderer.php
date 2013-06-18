af746d8741e61e1c4290805403298d65d8773cb9
sharona <sharona@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-18 15:26:41 +0000

    qnd: syndication feed - don't fetch more entries than the feed limit
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/core@100027 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/KalturaSyndicationFeedRenderer.php
+++ api_v3/lib/KalturaSyndicationFeedRenderer.php
@@ -398,11 +398,11 @@ class KalturaSyndicationFeedRenderer
 		
 		if($this->staticPlaylist)
 		{
-			$c->setLimit(self::STATIC_PLAYLIST_ENTRY_PEER_LIMIT_QUERY);
+			$c->setLimit(min(self::STATIC_PLAYLIST_ENTRY_PEER_LIMIT_QUERY, $this->limit));
 		}
 		else 
 		{
-			$c->setLimit(self::ENTRY_PEER_LIMIT_QUERY);
+			$c->setLimit(min(self::ENTRY_PEER_LIMIT_QUERY, $this->limit));
 		}
 
 		if(!count($this->entryFilters))
