84a43f53b63f027467f9e399f3d6b198379147ab
eran.kornblau <eran.kornblau@kaltura.com>
2013-07-14 16:31:46 +0300

    no need to update the categoryEntry fullIds when the categoryEntry is being deleted (it caused a problem while attempting to delete a categoryEntry of a deleted category)

--- alpha/lib/model/categoryEntry.php
+++ alpha/lib/model/categoryEntry.php
@@ -38,11 +38,15 @@ class categoryEntry extends BasecategoryEntry {
 	 */
 	public function preSave(PropelPDO $con = null)
 	{
-		$category = categoryPeer::retrieveByPK($this->getCategoryId());
-		if(!$category)
-			return false;
+		if ($this->getStatus() != CategoryEntryStatus::DELETED)
+		{
+			$category = categoryPeer::retrieveByPK($this->getCategoryId());
+			if(!$category)
+				return false;
 			
-		$this->setCategoryFullIds($category->getFullIds());
+			$this->setCategoryFullIds($category->getFullIds());
+		}
+			
 		return parent::preSave();
 	}
 	
