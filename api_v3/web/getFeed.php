343cfbc138745578e0d00cd53caf0fb817dee9c3
eran.kornblau <eran.kornblau@kaltura.com>
2013-07-14 16:27:55 +0300

    getFeed: support getting the parameters in lowercase

--- api_v3/web/getFeed.php
+++ api_v3/web/getFeed.php
@@ -1,5 +1,19 @@
 <?php
-if(!isset($_GET['feedId']))
+
+function getRequestParameter($paramName)
+{
+	if (isset($_GET[$paramName]))
+		return $_GET[$paramName];
+
+	// try lowercase
+	$paramName = strtolower($paramName);
+	if (isset($_GET[$paramName]))
+		return $_GET[$paramName];
+	
+	return null;
+}
+
+if(!getRequestParameter('feedId'))
 	die('feedId not supplied');
 	
 ini_set( "memory_limit" , "256M" );
@@ -31,10 +45,10 @@ kCurrentContext::$host = (isset($_SERVER["HOSTNAME"]) ? $_SERVER["HOSTNAME"] : n
 kCurrentContext::$user_ip = requestUtils::getRemoteAddress();
 kCurrentContext::$ps_vesion = "ps3";
 
-$feedId = $_GET['feedId'];
-$entryId = (isset($_GET['entryId']) ? $_GET['entryId'] : null);
-$limit = (isset($_GET['limit']) ? $_GET['limit'] : null);
-$ks = isset($_GET['ks']) ? $_GET['ks'] : null;
+$feedId = getRequestParameter('feedId');
+$entryId = getRequestParameter('entryId');
+$limit = getRequestParameter('limit');
+$ks = getRequestParameter('ks');
 
 $feedProcessingKey = "feedProcessing_{$feedId}_{$entryId}_{$limit}";
 if (function_exists('apc_fetch'))
