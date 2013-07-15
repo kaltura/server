988cbcb04eefd6977ac9a493bc105100db90b473
hilak <hila.karimov@kaltura.com>
2013-07-15 21:32:14 +0300

    change categoryKuser not to be deletable from the table

--- alpha/lib/enums/CategoryKuserStatus.php
+++ alpha/lib/enums/CategoryKuserStatus.php
@@ -8,4 +8,5 @@ interface CategoryKuserStatus extends BaseEnum
 	const ACTIVE = 1;
 	const PENDING = 2;
 	const NOT_ACTIVE = 3; //for archive CategoryUsers but without regecting them.
+	const DELETED = 4;
 }
