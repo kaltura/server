135f394ebeca45b04c4271ab27c25dab64c143c2
hilak <hila.karimov@kaltura.com>
2013-08-07 13:29:13 +0300

    plat-133

--- api_v3/lib/types/batch/KalturaStorageDeleteJobData.php
+++ api_v3/lib/types/batch/KalturaStorageDeleteJobData.php
@@ -37,7 +37,7 @@ class KalturaStorageDeleteJobData extends KalturaStorageJobData
             case KalturaStorageProfileProtocol::KALTURA_DC:
                 return $subType;	
 			default:
-				return kPluginableEnumsManager::apiToCore('StorageProfileProtocol', $subType);
+				return kPluginableEnumsManager::apiToCore('KalturaStorageProfileProtocol', $subType);
 		}
 	}
 	
@@ -55,7 +55,7 @@ class KalturaStorageDeleteJobData extends KalturaStorageJobData
             case StorageProfileProtocol::KALTURA_DC:
                 return $subType;    
             default:
-                return kPluginableEnumsManager::coreToApi('KalturaStorageProfileProtocol', $subType);
+                return kPluginableEnumsManager::coreToApi('StorageProfileProtocol', $subType);
         }
 	}
 }
