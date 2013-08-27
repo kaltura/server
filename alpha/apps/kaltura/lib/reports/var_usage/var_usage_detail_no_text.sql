1a95645b48824618d7cab0e0228e3851df7fcf83
Orly Lampert <orly.lampert@kaltura.com>
2013-08-27 14:30:10 +0300

    add transcoding usage to partner usage reports

--- alpha/apps/kaltura/lib/reports/var_usage/var_usage_detail_no_text.sql
+++ alpha/apps/kaltura/lib/reports/var_usage/var_usage_detail_no_text.sql
@@ -8,7 +8,8 @@ SELECT {GROUP_COLUMN},
 	IFNULL(peak_storage,0) peak_storage,
 	IFNULL(added_storage,0) added_storage,
 	IFNULL(deleted_storage,0) deleted_storage,
-	IFNULL(combined_bandwidth_storage,0) combined_bandwidth_storage
+	IFNULL(combined_bandwidth_storage,0) combined_bandwidth_storage,
+	IFNULL(count_transcoding,0) transcoding_usage
 FROM (SELECT DISTINCT(IF('{GROUP_COLUMN}' = 'date_id',day_id,t.month_id)) AS {GROUP_COLUMN},
 	partner_status_id status,
 	partner_name, 
@@ -21,6 +22,7 @@ AND day_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}) partner_date LEFT JOIN
         {GROUP_COLUMN} usage_date_id,
 	partner_id usage_partner_id,
         SUM(count_bandwidth) AS bandwidth_consumption,
+		SUM(count_transcoding) AS count_transcoding,
         IF('{GROUP_COLUMN}' = 'date_id', aggr_storage_mb,
         IF(FLOOR({FROM_DATE_ID}/100) = FLOOR({TO_DATE_ID}/100), (SUM(aggr_storage_mb)/(DATEDIFF({TO_DATE_ID},{FROM_DATE_ID})+1)), 
 		IF(month_id = FLOOR({FROM_DATE_ID}/100),(SUM(aggr_storage_mb)/(DAY(LAST_DAY(DATE(month_id * 100 + 1))) - DAY({FROM_DATE_ID}) + 1)),
@@ -35,7 +37,7 @@ AND day_id BETWEEN {FROM_DATE_ID} AND {TO_DATE_ID}) partner_date LEFT JOIN
         IF(month_id = FLOOR({TO_DATE_ID}/100),(SUM(aggr_storage_mb)/DAY({TO_DATE_ID})),(SUM(aggr_storage_mb)/DAY(LAST_DAY(DATE(month_id * 100 + 1)))))))) AS combined_bandwidth_storage
 FROM  (
 	SELECT
-		date_id, FLOOR(date_id/100) month_id, partner_id, IFNULL(count_bandwidth_kb, 0)/1024 count_bandwidth, IFNULL(aggr_storage_mb, 0) aggr_storage_mb, IFNULL(added_storage_mb, 0) added_storage_mb, IFNULL(deleted_storage_mb, 0) deleted_storage_mb
+		date_id, FLOOR(date_id/100) month_id, partner_id, IFNULL(count_bandwidth_kb, 0)/1024 count_bandwidth, IFNULL(count_transcoding_mb, 0) count_transcoding, IFNULL(aggr_storage_mb, 0) aggr_storage_mb, IFNULL(added_storage_mb, 0) added_storage_mb, IFNULL(deleted_storage_mb, 0) deleted_storage_mb
 	FROM
 		kalturadw.dwh_hourly_partner_usage
         WHERE
