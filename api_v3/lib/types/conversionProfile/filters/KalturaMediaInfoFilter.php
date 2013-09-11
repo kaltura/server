cae1577036d0b8ccaf40e7093051e0ab1ce7d549
hilak <hila.karimov@kaltura.com>
2013-09-11 15:47:59 +0300

    isset check unnecessary

--- api_v3/lib/types/conversionProfile/filters/KalturaMediaInfoFilter.php
+++ api_v3/lib/types/conversionProfile/filters/KalturaMediaInfoFilter.php
@@ -7,7 +7,7 @@ class KalturaMediaInfoFilter extends KalturaMediaInfoBaseFilter
 {
 	public function toObject ( $object_to_fill = null, $props_to_skip = array() )
 	{
-		if(!isset ($this->flavorAssetIdEqual) || !$this->flavorAssetIdEqual)
+		if(!$this->flavorAssetIdEqual)
 			throw new KalturaAPIException(KalturaErrors::PROPERTY_VALIDATION_CANNOT_BE_NULL, $this->getFormattedPropertyNameWithClassName('flavorAssetIdEqual'));
 		return parent::toObject($object_to_fill, $props_to_skip);
 	}
