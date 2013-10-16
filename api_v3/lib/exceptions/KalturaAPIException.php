14fb05e6092163b9d57b2859fa52e1646ae651ed
Avi Bueno <avi.bueno@kaltura.com>
2013-10-16 13:22:22 +0300

    Enhanced KalturaAPIException protocol to support param names

--- api_v3/lib/exceptions/KalturaAPIException.php
+++ api_v3/lib/exceptions/KalturaAPIException.php
@@ -6,26 +6,71 @@
 class KalturaAPIException extends Exception 
 {
 	protected $code;
+	private $args;
 	
-	
-	public function KalturaAPIException($errorString)
+	/**
+	 * 
+	 * @param type $errorString A string in the format: "ERR_CODE;PARAMS;MSG_STRING"
+	 * @throws Exception
+	 */
+	function KalturaAPIException($errorString)
 	{
-		$pos = strpos($errorString, ",");
-		if ($pos === false)
+		$components = explode(';', $errorString, 3);
+
+		if ( count($components) != 3 )
 		{
 			$errorString = KalturaErrors::INTERNAL_SERVERL_ERROR;
-			$pos = strpos($errorString, ",");
+			$components = explode(';', $errorString, 3);
 		}
-		$this->code = substr($errorString, 0, $pos);
-		$message = substr($errorString, $pos + 1);
+
+		$this->code = $components[0];
+		$this->message = $components[2];
 		
-		$args = func_get_args();
-		array_shift($args);
-		$this->message = @call_user_func_array('sprintf', array_merge(array($message), $args)); 
+		if ( ! empty($components[1]) ) // Need to process arguments?
+		{
+			$paramNames = explode(',', $components[1]);
+			$numParamNames = count($paramNames);
+			
+			$funcArgs = func_get_args();
+			array_shift( $funcArgs ); // Get rid of the first arg (= $errorString)
+			$numArgs = count( $funcArgs );
+			
+			if ( $numArgs != $numParamNames ) // Test if the number of args and params match
+			{
+/**
+ * TODO: TBD -				throw new Exception("Error: {$this->code} expected $numParamNames args but received $numArgs");
+ */
+			}
+
+			// Create and fill the args dictionary
+			$this->args = array();
+			for ( $i = 0; $i < $numParamNames; $i++ )
+			{
+				// Map the arg's name to its value
+				$this->args[ $paramNames[$i] ] = $funcArgs[$i];
+				
+				// Replace the arg's placeholder with its value in the destination string
+				$this->message = str_replace("@{$paramNames[$i]}@", $funcArgs[$i], $this->message);
+			}			
+		}
+		else
+		{
+			// Make sure we have an [empty] array
+			$this->args = array();
+		}
+	}
+	
+	/**
+	 * Get an dictionary (ARG_NAME => ARG_VALUE) of all the arguments that were passed to the exception.
+	 * @return array If no args were passed the array will be empty.
+	 */
+	public function getArgs()
+	{
+		return $this->args;
 	}
 	
 	public function __sleep()
 	{
-		return array('code', 'message');
+		return array('code', 'message', 'args');
 	}
 }
