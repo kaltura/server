51310f541737d74d5a49b60236f03989877ee7cc
tan-tan-kanarek <jonathan.kanarek@kaltura.com>
2013-10-17 18:31:33 +0200

    New MediaServer data model

--- deployment/base/sql/01.kaltura_ce_tables.sql
+++ deployment/base/sql/01.kaltura_ce_tables.sql
@@ -2169,3 +2169,14 @@ CREATE TABLE `api_server`
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `media_server`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	`hostname` VARCHAR(255),
+	`dc` INTEGER,
+	`custom_data` TEXT,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
