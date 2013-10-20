a129aff093cfe6b981ddc49b00479b6a8d5aa3f2
Avi Bueno <avi.bueno@kaltura.com>
2013-10-20 16:17:47 +0300

    Made $args protected and removed a few code checks

--- api_v3/lib/exceptions/KalturaAPIException.php
+++ api_v3/lib/exceptions/KalturaAPIException.php
@@ -6,23 +6,15 @@
 class KalturaAPIException extends Exception 
 {
 	protected $code;
-	private $args;
+	protected $args = array ();
 	
 	/**
-	 * 
-	 * @param type $errorString A string in the format: "ERR_CODE;PARAMS;MSG_STRING"
+	 * @param string $errorString A string in the format: "ERR_CODE;PARAMS;MSG_STRING"
 	 * @throws Exception
 	 */
 	function KalturaAPIException($errorString)
 	{
 		$components = explode(';', $errorString, 3);
-
-		if ( count($components) != 3 )
-		{
-			$errorString = KalturaErrors::INTERNAL_SERVERL_ERROR;
-			$components = explode(';', $errorString, 3);
-		}
-
 		$this->code = $components[0];
 		$this->message = $components[2];
 		
@@ -33,17 +25,8 @@ class KalturaAPIException extends Exception
 			
 			$funcArgs = func_get_args();
 			array_shift( $funcArgs ); // Get rid of the first arg (= $errorString)
-			$numArgs = count( $funcArgs );
-			
-			if ( $numArgs != $numParamNames ) // Test if the number of args and params match
-			{
-/**
- * TODO: TBD -				throw new Exception("Error: {$this->code} expected $numParamNames args but received $numArgs");
- */
-			}
 
 			// Create and fill the args dictionary
-			$this->args = array();
 			for ( $i = 0; $i < $numParamNames; $i++ )
 			{
 				// Map the arg's name to its value
@@ -51,12 +34,7 @@ class KalturaAPIException extends Exception
 				
 				// Replace the arg's placeholder with its value in the destination string
 				$this->message = str_replace("@{$paramNames[$i]}@", $funcArgs[$i], $this->message);
-			}			
-		}
-		else
-		{
-			// Make sure we have an [empty] array
-			$this->args = array();
+			}
 		}
 	}
 	
