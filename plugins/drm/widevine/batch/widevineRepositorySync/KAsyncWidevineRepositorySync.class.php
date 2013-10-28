782c7da378bbd127a5b444b9978fc9d7be811caa
yuly-roberman <yuli.roberman@kaltura.com>
2013-10-28 14:53:35 +0200

    fix failure when deleting an entry

--- plugins/drm/widevine/batch/widevineRepositorySync/KAsyncWidevineRepositorySync.class.php
+++ plugins/drm/widevine/batch/widevineRepositorySync/KAsyncWidevineRepositorySync.class.php
@@ -63,8 +63,8 @@ class KAsyncWidevineRepositorySync extends KJobHandlerWorker
 			$this->updateWidevineAsset($assetId, $licenseStartDate, $licenseEndDate);
 		}
 	
-		
-		$this->updateFlavorAssets($job, $dataWrap);
+		if($data->monitorSyncCompletion)
+			$this->updateFlavorAssets($job, $dataWrap);
 			
 													  
 	}
