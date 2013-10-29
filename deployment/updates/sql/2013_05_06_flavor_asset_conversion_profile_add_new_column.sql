1b2cc506439688f87aed60d90e3ea046416e61b0
hilak <hila.karimov@kaltura.com>
2013-10-29 18:23:44 +0200

    remove redundant scripts from updates

--- deployment/updates/sql/2013_05_06_flavor_asset_conversion_profile_add_new_column.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `flavor_params_conversion_profile`
-ADD `delete_policy` tinyint(4) DEFAULT '0';
--- deployment/updates/sql/2013_05_29_batch_job_id_to_bigint.sql
+++
@@ -1,5 +0,0 @@
-alter table batch_job_sep modify id bigint NOT NULL AUTO_INCREMENT, modify parent_job_id bigint DEFAULT NULL, modify bulk_job_id bigint DEFAULT NULL, modify root_job_id bigint DEFAULT NULL,modify batch_job_lock_id bigint DEFAULT NULL;
-
-alter table batch_job_log modify id bigint NOT NULL AUTO_INCREMENT,modify job_id bigint DEFAULT NULL,modify twin_job_id bigint DEFAULT NULL,modify bulk_job_id bigint DEFAULT NULL,modify root_job_id bigint DEFAULT NULL,modify parent_job_id bigint DEFAULT NULL;
-
-alter table batch_job_lock modify id bigint NOT NULL, modify batch_job_id bigint NOT NULL;
