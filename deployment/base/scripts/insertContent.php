56ab02220c15a4154a34115a8f1994071d467db6
hilak <hila.karimov@kaltura.com>
2013-11-02 19:17:04 +0200

    save executed insertContent scripts in the table version_management, in order to run only the delta between versions on upgrade.

--- deployment/base/scripts/insertContent.php
+++ deployment/base/scripts/insertContent.php
@@ -11,14 +11,28 @@ require_once(__DIR__ . '/../../bootstrap.php');
 
 myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_MASTER;
 
-$criteria = new Criteria();
-$criteria->add(entryPeer::PARTNER_ID, 99);
-$templateEntries = entryPeer::doCount($criteria);
-if($templateEntries)
+$dbConf = kConf::getDB();
+$dsn = $dbConf['datasources']['propel']['connection']['dsn'];
+$dsn = explode(":", $dsn);
+$dsnArray = explode(";", $dsn[1]);
+
+// init with default port
+$dbParams = $dbConf['datasources']['propel']['connection'];
+$dbParams['port'] = '3306';
+
+foreach($dsnArray as $param)
 {
-	KalturaLog::info("Content already ingested.");
-	exit(0);
+	$items = explode("=", $param);
+	if(count($items) == 2)
+		$this->dbParams[$items[0]] = $items[1];
 }
+
+foreach($dbParams as $key => $value)
+{
+	echo $key . "=>" . $value . "\n";
+}
+
+$deployedScripts = getDeployedScripts($dbParams);
 
 $dirPath = __DIR__ . '/init_content';
 $scriptPath = realpath(__DIR__ . '/../../../') . '/tests/standAloneClient/exec.php';
@@ -31,11 +45,14 @@ $dir = dir($dirPath);
 $fileNames = array();
 while (false !== ($fileName = $dir->read()))
 {
-	$filePath = realpath("$dirPath/$fileName");
-	if($fileName[0] == '.' || is_dir($filePath) || preg_match('/template.xml$/', $fileName))
-		continue;
-
-	$fileNames[] = $fileName;
+	if (!in_array ($fileName, array_keys($deployedScripts)))
+	{
+		$filePath = realpath("$dirPath/$fileName");
+		if($fileName[0] == '.' || is_dir($filePath) || preg_match('/template.xml$/', $fileName))
+			continue;
+	
+		$fileNames[] = $fileName;
+	}
 }
 $dir->close();
 
@@ -51,6 +68,42 @@ foreach($fileNames as $fileName)
 	passthru("php $scriptPath $filePath", $returnValue);
 	if($returnValue !== 0)
 		exit(-1);
+		
+	saveScriptAsRun ($fileName);
 }
 
+
+function getDeployedScripts(array $dbParams)
+{
+	$link = mysql_connect($dbParams['host'] . ':' . $dbParams['port'], $dbParams['user'], $dbParams['password'], null);
+	
+	$db_selected = mysql_select_db($dbParams['dbname'], $link);
+	$result = mysql_query('select filename from version_management');
+	if($result)
+	{
+		$res = array();
+		
+		while($row = mysql_fetch_assoc($result))
+		{
+			$res[$row['filename']] = true;
+		}
+	}
+	
+	mysql_free_result($result);
+	mysql_close($link);
+	return $res;
+}
+
+function saveScriptAsRun (array $dbParams, $fileName)
+{
+	$link = mysql_connect($dbParams['host'] . ':' . $dbParams['port'], $dbParams['user'], $dbParams['password'], null);
+	$db_selected = mysql_select_db($dbParams['dbname'], $link);
+	$result =   mysql_query("insert into version_management (version, filename, created_at) values ('7000', '$fileName', '". time() ."')");
+	
+	if ($result)
+		KalturaLog::info ("$fileName saved as run");
+	
+	mysql_free_result($result);
+	mysql_close($link);
+}
 
