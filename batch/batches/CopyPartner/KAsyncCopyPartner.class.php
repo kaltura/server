de7d4f8691e17d953f43980af6929bf90c398a11
AVIDDeveloper <avi.bueno@kaltura.com>
2013-11-24 17:46:22 +0200

    Updated copy partner code

--- batch/batches/CopyPartner/KAsyncCopyPartner.class.php
+++ batch/batches/CopyPartner/KAsyncCopyPartner.class.php
@@ -40,10 +40,9 @@ class KAsyncCopyPartner extends KJobHandlerWorker
 		}
 		catch ( Exception $e )
 		{
-			$this->log( $e->getMessage() . "\nStack trace:\n" . $e->getTraceAsString() );
+			self::unimpersonate(); // Make sure we're not impresonating anymore
+			throw $e;
 		}
-		
-		self::unimpersonate(); // Make sure we're not impresonating anymore
 		
 		$this->log("KAsyncCopyPartner done.");
 		
@@ -58,145 +57,63 @@ class KAsyncCopyPartner extends KJobHandlerWorker
 	{
 		$this->fromPartnerId = $jobData->fromPartnerId;
 		$this->toPartnerId = $jobData->toPartnerId;
-		$this->log( "KAsyncCopyPartner::doCopyPartner(), Job id [$job->id]" );
+		$this->log( "CopyPartner job id [$job->id], From PID: $jobData->fromPartnerId, To PID: $jobData->toPartnerId" );
+
+		try
+		{
+			// copy permssions before trying to copy additional objects such as distribution profiles which are not enabled yet for the partner
+	 		$this->copyAllEntries();
+		}
+		catch ( Exception $e )
+		{
+			self::unimpersonate(); // Make sure we're not impresonating anymore
+			throw $e;
+		}
 		
-		$this->log( "CopyPartner job id [$job->id]" );
-
-		// copy permssions before trying to copy additional objects such as distribution profiles which are not enabled yet for the partner
- 		$this->copyPermissions();
-
  		$res = $this->closeJob($job, null, null, "CopyPartner finished", KalturaBatchJobStatus::FINISHED);
-		return $res;
+		
+ 		return $res;
 	}
 	
 	/**
-	 * copyPermissions()
+	 * copyAllEntries()
 	 */
-	protected function copyPermissions()
+	protected function copyAllEntries()
 	{
-		$this->log( "Copying permissions" );
+		$entryFilter = new KalturaBaseEntryFilter();
+ 		$entryFilter->order = KalturaBaseEntryOrderBy::CREATED_AT_ASC;
 		
 		$pageFilter = new KalturaFilterPager();
 		$pageFilter->pageSize = 50;
 		$pageFilter->pageIndex = 0;
 		
-		$totalReceivedObjectsCount = 0;
+		$totalReceivedObjectsCount = 0;		
 		
 		/* @var $this->getClient() KalturaClient */
 		do
-		{			
+		{
+			// Get the source partner's entries list
 			self::impersonate( $this->fromPartnerId );
-			$permissionsList = $this->getClient()->permission->listAction( null, $pageFilter );
-			
-			$totalCount = $permissionsList->totalCount;
-			$receivedObjectsCount = count($permissionsList->objects);
+			$entriesList = $this->getClient()->baseEntry->listAction( $entryFilter, $pageFilter );
+
+			$totalCount = $entriesList->totalCount;
+			$receivedObjectsCount = count($entriesList->objects);
 			$totalReceivedObjectsCount += $receivedObjectsCount; 
 			$pageFilter->pageIndex++;
 			
-			$this->log( "Got $receivedObjectsCount permission object(s) [= $totalReceivedObjectsCount/$totalCount]" );
+			$this->log( "Got $receivedObjectsCount entry object(s) [= $totalReceivedObjectsCount/$totalCount]" );
 			
 			if ( $receivedObjectsCount > 0 )
 			{
+				// Write the source partner's entries to the destination partner 
 				self::impersonate( $this->toPartnerId );
-				
-				foreach ( $permissionsList->objects as $permission )
+				foreach ( $entriesList->objects as $entry )
 				{
-					/* @var $permission KalturaPermission */
-					if ( $permission->partnerId != 0 )
-					{
-						$newPermission = CopyPropertiesHelper::copyPublicWritableProperties( $permission, new KalturaPermission() );
-						
-						try
-						{
-							$this->getClient()->permission->add( $newPermission );
-							$added = true;
-						}
-						catch ( Exception $e )
-						{
-							// We assume the exception was thrown because the premission already exists
-							$added = false;
-						}
-						
-						if ( $added )
-						{
-							$this->log( "Added permission: " . $permission->name );
-						}							
-					}
+					$newEntry = $this->getClient()->baseEntry->cloneAction( $entry->id /* anything else? */ );
 				}
 			}			
-		} while ( $totalReceivedObjectsCount < $totalCount );		
-	}
-}
-
-class CopyPropertiesHelper
-{
-	static private $classPropertiesCacheMap = array();
-
-	/**
-	 * Copy all public writable (= non read-only) properties
-	 * between two objects of the [exact] same class
-	 * 
-	 * @example $newObject = CopyPropertiesHelper::copyPublicWritableProperties( $permission, new KalturaPermission() );
-	 * 
-	 * @param mixed $fromObject Source object
-	 * @param mixed $toObject Destination object
-	 * @throws Exception in case the object types are not identical
-	 * @return mixed The destination object ($toObject) 
-	 */
-	public static function copyPublicWritableProperties( $fromObject, $toObject )
-	{
-		$className = get_class( $fromObject );
-		
-		if ( get_class($toObject) !== $className )
-		{
-			throw new Exception("Object types don't match");
-		}
-		
-		$writableProperties = self::getPublicWritableProperties( $className );
-		foreach ( $writableProperties as $property )
-		{
-			$toObject->$property = $fromObject->$property;
-		}
-		
-		return $toObject;
-	}
+		} while ( $totalReceivedObjectsCount < $totalCount );
 	
-	/**
-	 * Get all public class properties, which do not contain @readonly in their php-doc comment
-	 * 
-	 * @param mixed $className
-	 * @return array The array of public writable propoerties (or an empty array if none exist)
-	 */
-	private static function getPublicWritableProperties( $className )
-	{
-		if ( ! array_key_exists( $className, self::$classPropertiesCacheMap ) ) // First time? 
-		{
-			$propertiesArray = array();
-			
-			$r = new ReflectionClass($className);
-			$props = $r->getProperties( ReflectionProperty::IS_PUBLIC & ~ReflectionProperty::IS_STATIC );
-			foreach ( $props as $p )
-			{
-				/* @var $p ReflectionProperty */
-				$comments = $p->getDocComment();
-				if ( ! empty( $comments )
-						&& strpos( $comments, "@readonly" ) !== false )
-				{
-					continue;
-				}
-	
-				$propertiesArray[] = $p->name;
-			}
-			
-			// Cache the results for next time
-			self::$classPropertiesCacheMap[ $className ] = $propertiesArray;
-		}
-		else
-		{
-			// Get the already cached results
-			$propertiesArray = self::$classPropertiesCacheMap[ $className ];
-		}
-		
-		return $propertiesArray;
+		self::unimpersonate();
 	}	
 }
