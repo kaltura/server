72f13ec53b320c7d441b7818c0b3c11b6ff20ead
roman-kreichman <roman.kreichman@gmail.com>
2013-12-11 20:31:45 +0200

    added event notification content parameters of the current object when dispatching the template

--- plugins/scheduled_task/plugins/event_notification/lib/object_task_engine/KObjectTaskDispatchEventNotificationEngine.php
+++ plugins/scheduled_task/plugins/event_notification/lib/object_task_engine/KObjectTaskDispatchEventNotificationEngine.php
@@ -20,6 +20,19 @@ class KObjectTaskDispatchEventNotificationEngine extends KObjectTaskEntryEngineB
 		$templateId = $objectTask->eventNotificationTemplateId;
 		$eventNotificationPlugin = KalturaEventNotificationClientPlugin::get($client);
 		$data = new KalturaEventNotificationDispatchJobData();
+
+		$objectProperties = get_object_vars($object);
+		foreach($objectProperties as $property => $value)
+		{
+			// append only scalar variables and nulls
+			if (!is_null($value) && !is_scalar($value))
+				continue;
+
+			$keyValue = new KalturaKeyValue();
+			$keyValue->key = $property;
+			$keyValue->value = (string)$value;
+			$data->contentParameters[] = $keyValue;
+		}
 		$data->templateId = $templateId;
 		$eventNotificationPlugin->eventNotificationTemplate->dispatch($templateId, $data);
 	}
