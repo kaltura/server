42c20dc198f3b6f21007dc50f7041cd02a63f796
yossipapi <yossi.papiashvili@kaltura.com>
2013-12-16 09:51:35 +0200

    In case of Bulk Upload always force Async Download

--- plugins/bulk_upload/csv/batch/BulkUploadEntryEngineCsv.php
+++ plugins/bulk_upload/csv/batch/BulkUploadEntryEngineCsv.php
@@ -43,8 +43,7 @@ class BulkUploadEntryEngineCsv extends BulkUploadEngineCsv
 		        $resource = new KalturaUrlResource();
 		    }
 			$resource->url = $url;
-			if($bulkUploadResult->contentType && strtolower($bulkUploadResult->contentType) == 'image')
-				$resource->forceAsyncDownload = true;
+			$resource->forceAsyncDownload = true;
 
 			KBatchBase::impersonate($this->currentPartnerId);;
 			KBatchBase::$kClient->media->addContent($bulkUploadResult->entryId, $resource);
