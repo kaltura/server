b85b145a07f56df6f7adbe9d4f42c4ca8d8a78b9
anatol.schwartz <anatol.schwartz@kaltura.con>
2014-01-06 14:02:26 +0200

    added retrival of the lic. data via api calls

--- plugins/transcoding/smoothProtect/lib/KOperationEngineSmoothProtect.php
+++ plugins/transcoding/smoothProtect/lib/KOperationEngineSmoothProtect.php
@@ -91,10 +91,43 @@ class KOperationEngineSmoothProtect  extends KSingleOutputOperationEngine
 	*/
 	private function getLicenseParamsStr()
 	{
+		// impersonite
+		KBatchBase::impersonate($this->job->partnerId);
+		
+		$drmPlugin = KalturaDrmClientPlugin::get(KBatchBase::$kClient);
+		if(!isset($drmPlugin)) {
+			KalturaLog::err("FAILED to get drmPlugin");
+			return false;
+		}
+		$profile=$drmPlugin->drmProfile->getByProvider(KalturaDrmProviderType::PLAY_READY);
+		if(!isset($profile)) {
+			KalturaLog::err("FAILED to get profile");
+			return false;
+		}
+		$playReadyPlugin = KalturaPlayReadyClientPlugin::get(KBatchBase::$kClient);
+		if(!isset($playReadyPlugin)) {
+			KalturaLog::err("FAILED to get playReadyPlugin");
+			return false;
+		}
+		$playReadyData = $playReadyPlugin->playReadyDrm->getEntryContentKey($this->job->entryId, true);
+		if(!isset($playReadyData)) {
+			KalturaLog::err("FAILED to get playReadyData");
+			return false;
+		}
+
+		// un-impersonite
+		KBatchBase::unimpersonate();
+			
+		$paramsStr = " -keyId $playReadyData->keyId -contentKey $playReadyData->contentKey -laUrl $profile->licenseServerUrl";
+		KalturaLog::info($paramsStr);
+		return $paramsStr;
+
+/*
 		$keyId = "b6e16839-eebd-4ff6-ab76-8d482d8d2b6a";
 		$keySeed = "XVBovsmzhP9gRIZxWfFta3VVRPzVEWmJsazEJ46I";
 		$laSrv = "http://playready.directtaps.net/pr/svc/rightsmanager.asmx?";
 		$paramsStr = " -keyId $keyId -keySeed $keySeed -laUrl $laSrv";
 		return $paramsStr;
+*/
 	}
 }
