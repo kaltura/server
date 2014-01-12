1991c70fe8240bfb2f1d5f11f80cb927a3424237
AVIDDeveloper <avi.bueno@kaltura.com>
2014-01-12 15:51:32 +0200

    Replaced getDstCategoryIdBasedOnSrcCategoryId() with a map of src->dst category IDs PLAT-688 #time 6h

--- alpha/apps/kaltura/lib/myEntryUtils.class.php
+++ alpha/apps/kaltura/lib/myEntryUtils.class.php
@@ -1190,6 +1190,46 @@ PuserKuserPeer::getCriteriaFilter()->disable();
 		$categoryEntries = categoryEntryPeer::doSelect($c);
 		categoryEntryPeer::setUseCriteriaFilter(true);
 		
+		// Create srcCategoryIdToDstCategoryIdMap - a map of source partner category ids -> dst. partner category ids
+		//
+		// Build src category IDs set
+		$srcCategoryIdSet = array();
+		foreach($categoryEntries as $categoryEntry)
+		{
+			// Map an ID to itself in order to aviod duplicates
+			$srcCategoryIdSet[ $categoryEntry->getCategoryId() ] = $categoryEntry->getCategoryId();
+		}
+
+		// Get src category objects
+		categoryPeer::setUseCriteriaFilter(false);
+		$srcCategories = categoryPeer::retrieveByPks( $srcCategoryIdSet );
+		categoryPeer::setUseCriteriaFilter(true);
+		
+		// Map the category names to their IDs
+		$fullNamesToSrcCategoryIdMap = array();
+		foreach ( $srcCategories as $category )
+		{
+			$fullNamesToSrcCategoryIdMap[ $category->getFullName() ] = $category->getId();
+		}
+
+		$c = KalturaCriteria::create(categoryPeer::OM_CLASS);
+		$c->add(categoryPeer::FULL_NAME, array_keys( $fullNamesToSrcCategoryIdMap ), KalturaCriteria::IN);
+		$c->addAnd(categoryPeer::PARTNER_ID, $newEntry->getPartnerId());
+		categoryPeer::setUseCriteriaFilter(false);
+		$dstCategories = categoryPeer::doSelect($c);
+		categoryPeer::setUseCriteriaFilter(true);
+
+		$srcCategoryIdToDstCategoryIdMap = array();
+		foreach ( $dstCategories as $dstCategory )
+		{
+			$fullName = $dstCategory->getFullName();
+			if ( array_key_exists( $fullName, $fullNamesToSrcCategoryIdMap ) )
+			{
+				$srcCategoryId = $fullNamesToSrcCategoryIdMap[ $fullName ];
+				$srcCategoryIdToDstCategoryIdMap[ $srcCategoryId ] = $dstCategory->getId();
+			}
+		}
+
 		foreach($categoryEntries as $categoryEntry)
 		{
 			/* @var $categoryEntry categoryEntry */
@@ -1197,7 +1237,7 @@ PuserKuserPeer::getCriteriaFilter()->disable();
 			$newCategoryEntry->setPartnerId($newEntry->getPartnerId());
 			$newCategoryEntry->setEntryId($newEntry->getId());
 		
-			$categoryId = self::getDstCategoryIdBasedOnSrcCategoryId( $entry->getPartnerId(), $categoryEntry->getCategoryId(), $newEntry->getPartnerId() );
+			$categoryId = $srcCategoryIdToDstCategoryIdMap[ $categoryEntry->getCategoryId() ];
 			if ( $categoryId !== false )
 			{
 				$newCategoryEntry->setCategoryId($categoryId);
@@ -1213,47 +1253,6 @@ PuserKuserPeer::getCriteriaFilter()->disable();
 		return $newEntry;
  	} 	
  	
- 	/**
- 	 * Get the id of a dst. partner category entry based on the category entry's id on the src. partner (by matching full names)
- 	 *
- 	 * @param int $srcPartnerId
- 	 * @param int $srcPartnerCategoryId
- 	 * @param int $dstPartnerId
- 	 * @return int|false The category id on the dst. partner, or false in case of any error
- 	 */
- 	private static function getDstCategoryIdBasedOnSrcCategoryId( $srcPartnerId, $srcPartnerCategoryId, $dstPartnerId )
- 	{
- 		$dstCategoryId = false;
- 		
- 		categoryPeer::setUseCriteriaFilter(false);
- 		
- 		// Get the source partner's category based on the given source category id
-		/* @var $srcCategory category */
-		$srcCategory = categoryPeer::retrieveByPk( $srcPartnerCategoryId );
-		
-		if ( $srcCategory )
-		{
-			$categoryFullName = $srcCategory->getFullName();
-			
-			// Get the dst category based on category name 
-			$c = KalturaCriteria::create(categoryPeer::OM_CLASS);
-			$c->addAnd(categoryPeer::PARTNER_ID, $dstPartnerId);
-			$c->addAnd(categoryPeer::FULL_NAME, $categoryFullName);
-
-			/* @var $dstCategory category */
-			$dstCategory = categoryPeer::doSelectOne($c);
-			
-			if ( $dstCategory )
-			{
-				$dstCategoryId = $dstCategory->getId();
-			}
-	 	}
-	 	
-		categoryPeer::setUseCriteriaFilter(true);
-
-		return $dstCategoryId;
- 	}
- 	 	
  	/*
  	 * re-index to search index, and recalculate fields.
  	 */
