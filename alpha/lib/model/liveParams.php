f96993641046d16b2ef14bd189857a999acc4631
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-01-14 20:11:00 +0200

    Undefined class constant CUSTOM_DATA_FIELD_STREAM_SUFFIX

--- alpha/lib/model/liveParams.php
+++ alpha/lib/model/liveParams.php
@@ -13,10 +13,10 @@ class liveParams extends flavorParams
 	const CUSTOM_DATA_FIELD_STREAM_SUFFIX = "streamSuffix";
 	
 	public function getStreamSuffix() {
-		return $this->getFromCustomData ( flavorParams::CUSTOM_DATA_FIELD_STREAM_SUFFIX );
+		return $this->getFromCustomData ( liveParams::CUSTOM_DATA_FIELD_STREAM_SUFFIX );
 	}
 	
 	public function setStreamSuffix($v) {
-		$this->putInCustomData ( flavorParams::CUSTOM_DATA_FIELD_STREAM_SUFFIX, $v );
+		$this->putInCustomData ( liveParams::CUSTOM_DATA_FIELD_STREAM_SUFFIX, $v );
 	}
 }
