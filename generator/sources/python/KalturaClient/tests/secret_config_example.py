35612db029ae45a0e5e72267ee8f23511e1e97f4
flipmcf <flipmcf@gmail.com>
2014-01-16 13:12:10 -0500

    remove template secret_config_example - use only secret_config.py

--- generator/sources/python/KalturaClient/tests/secret_config_example.py
+++
@@ -1,9 +0,0 @@
-#put your Kaltura account info in here, then rename to 'secret_config.py'
-
-#be sure the .gitignore file has 'secret_config.py' listed so you don't check this in.
-
-PARTNER_ID = 54321
-SECRET = "YOUR_USER_SECRET"
-ADMIN_SECRET = "YOUR_ADMIN_SECRET"
-SERVICE_URL = "http://www.kaltura.com"
-USER_NAME = "testUser"
