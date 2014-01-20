885fcd74e27a0abfe0b85cf16231aa402ddd19b5
hilak <hila.karimov@kaltura.com>
2014-01-20 12:15:17 +0200

    Fix notice and error brought on when there is only one DC

--- api_v3/services/LiveStreamService.php
+++ api_v3/services/LiveStreamService.php
@@ -99,8 +99,11 @@ class LiveStreamService extends KalturaLiveEntryService
 			$dbEntry->save();
 			
 			$broadcastUrlManager = kBroadcastUrlManager::getInstance($dbEntry->getPartnerId());
-			$otherDc = reset(kDataCenterMgr::getAllDcs());
-			$otherDcId = $otherDc['id'];
+			$otherDCs = kDataCenterMgr::getAllDcs();
+			$otherDc = reset($otherDCs);
+			
+			//In case there is only 1 DC
+			$otherDcId = $otherDc ? $otherDc['id'] : kDataCenterMgr::getCurrentDcId();
 			
 			$dbEntry->setPrimaryBroadcastingUrl($broadcastUrlManager->getBroadcastUrl($dbEntry, kDataCenterMgr::getCurrentDcId(), kBroadcastUrlManager::PRIMARY_MEDIA_SERVER_INDEX));
 			$dbEntry->setSecondaryBroadcastingUrl($broadcastUrlManager->getBroadcastUrl($dbEntry, $otherDcId, kBroadcastUrlManager::SECONDARY_MEDIA_SERVER_INDEX));
