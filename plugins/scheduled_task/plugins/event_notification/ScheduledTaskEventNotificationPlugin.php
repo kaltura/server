2625950c660e20d13800e2d5d622f89e989bed9e
roman-kreichman <roman.kreichman@gmail.com>
2014-01-20 16:28:09 +0200

    return from loadObject when in admin console context

--- plugins/scheduled_task/plugins/event_notification/ScheduledTaskEventNotificationPlugin.php
+++ plugins/scheduled_task/plugins/event_notification/ScheduledTaskEventNotificationPlugin.php
@@ -55,6 +55,9 @@ class ScheduledTaskEventNotificationPlugin extends KalturaPlugin implements IKal
 	 */
 	public static function loadObject($baseClass, $enumValue, array $constructorArgs = null)
 	{
+		if (class_exists('Kaltura_Client_Client'))
+			return null;
+
 		if (class_exists('KalturaClient'))
 		{
 			if ($baseClass == 'KObjectTaskEntryEngineBase' && $enumValue == KalturaObjectTaskType::DISPATCH_EVENT_NOTIFICATION)
