653fc8a6fc7cd132c3abf52d690ddb1721ac172e
hila.karimov <hila.karimov@kaltura.com>
2014-02-06 19:10:25 +0200

    Adding script in order to set partner's custom broadcasting URLs

---
+++ alpha/scripts/utils/setPartnerBroadcastingUrls.php
@@ -0,0 +1,17 @@
+<?php
+require_once(dirname(__FILE__).'/../bootstrap.php');
+
+
+/*partner Id*/
+$partnerId = null;
+$primaryBroadcastUrl = "";
+$secondaryBroadcastUrl = "";
+
+if (!$partnerId)
+{
+	die ("Missing parameter");
+}
+
+$partner = PartnerPeer::retrieveByPK($partnerId);
+$partner->setPrimaryBroadcastUrl($primaryBroadcastUrl);
+$partner->setSecondaryBroadcastUrl($secondaryBroadcastUrl);
