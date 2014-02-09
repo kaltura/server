a58edfe3434a3be9d07752a2a82cd81e4f2c99b7
yossipapi <yossi.papiashvili@kaltura.com>
2014-02-09 14:25:47 +0200

    LiveEntry.php php Fatal Error In case media servers found are not instance of KLiveMediaServer continue

--- alpha/lib/model/LiveEntry.php
+++ alpha/lib/model/LiveEntry.php
@@ -296,16 +296,25 @@ abstract class LiveEntry extends entry
 		
 		foreach($kMediaServers as $kMediaServer)
 		{
-			/* @var $kMediaServer kLiveMediaServer */
-			KalturaLog::debug("mediaServer->getDc [" . $kMediaServer->getDc() . "] == kDataCenterMgr::getCurrentDcId [" . kDataCenterMgr::getCurrentDcId() . "]");
-			if($kMediaServer->getDc() == kDataCenterMgr::getCurrentDcId())
-				return $kMediaServer->getMediaServer();
+			if($kMediaServer instanceof kLiveMediaServer)
+			{
+				/* @var $kMediaServer kLiveMediaServer */
+				KalturaLog::debug("mediaServer->getDc [" . $kMediaServer->getDc() . "] == kDataCenterMgr::getCurrentDcId [" . kDataCenterMgr::getCurrentDcId() . "]");
+				if($kMediaServer->getDc() == kDataCenterMgr::getCurrentDcId())
+					return $kMediaServer->getMediaServer();
+			}
 		}
 		if($currentDcOnly)
 			return null;
 		
 		$kMediaServer = reset($kMediaServers);
-		return $kMediaServer->getMediaServer();
+		if($kMediaServer instanceof kLiveMediaServer)
+			return $kMediaServer->getMediaServer();
+		else
+		{
+			KalturaLog::debug("No Valid Media Servers Were Found For Current Live Entry [" . $this->getEntryId() . "]" );
+			return null;
+		}
 	}
 	
 	/**
