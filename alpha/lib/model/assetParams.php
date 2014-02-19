60a62971a225a7e70eb3268c7848c65c0ae9f560
anatol.schwartz <anatol.schwartz@kaltura.con>
2014-02-19 13:13:12 +0200

    IX-9.11.0-PLAT-307-FFMPEG-2.1.3 VP9 & HLS support

--- alpha/lib/model/assetParams.php
+++ alpha/lib/model/assetParams.php
@@ -27,6 +27,7 @@ class assetParams extends BaseassetParams
 	const CONTAINER_FORMAT_MPEGTS = "mpegts";
 	const CONTAINER_FORMAT_APPLEHTTP = "applehttp";
 	const CONTAINER_FORMAT_WAV = "wav";
+	const CONTAINER_FORMAT_HLS = "hls";
 	const CONTAINER_FORMAT_COPY = "copy";
 	
 	const CONTAINER_FORMAT_PDF = 'pdf';
