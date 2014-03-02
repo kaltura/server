f8b24a4abf743ef7b739a58e549f74cea7a1bce7
hila.karimov <hila.karimov@kaltura.com>
2014-03-02 17:27:41 +0200

    Fix issue where calling flavorParams->add would not work for live params

--- alpha/lib/model/liveParams.php
+++ alpha/lib/model/liveParams.php
@@ -12,6 +12,18 @@ class liveParams extends flavorParams
 {
 	const CUSTOM_DATA_FIELD_STREAM_SUFFIX = "streamSuffix";
 	
+	/**
+	 * Applies default values to this object.
+	 * This method should be called from the object's constructor (or
+	 * equivalent initialization method).
+	 * @see        __construct()
+	 */
+	public function applyDefaultValues()
+	{
+		parent::applyDefaultValues();
+		$this->setType(assetType::LIVE);
+	}
+	
 	public function getStreamSuffix() {
 		return $this->getFromCustomData ( liveParams::CUSTOM_DATA_FIELD_STREAM_SUFFIX );
 	}
