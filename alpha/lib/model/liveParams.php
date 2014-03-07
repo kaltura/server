3417e0ede290cd62c7043ab385bfc26c8b6d5fd2
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-03-07 12:41:00 +0200

    Apply type on instantiation. SUP-1689 #time 20m

--- alpha/lib/model/liveParams.php
+++ alpha/lib/model/liveParams.php
@@ -11,6 +11,12 @@
 class liveParams extends flavorParams
 {
 	const CUSTOM_DATA_FIELD_STREAM_SUFFIX = "streamSuffix";
+		
+	public function applyDefaultValues()
+	{
+		parent::applyDefaultValues();
+		$this->setType(assetType::LIVE);
+	}
 	
 	public function getStreamSuffix() {
 		return $this->getFromCustomData ( liveParams::CUSTOM_DATA_FIELD_STREAM_SUFFIX );
