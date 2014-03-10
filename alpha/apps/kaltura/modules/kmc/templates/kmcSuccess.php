8f9f0c0c1ac97d411efcb645dfd2ac405212a45a
amirch1 <amir.chervinsky@kaltura.com>
2014-03-10 10:01:46 +0200

    #FEC-743 #comment updated php files

--- alpha/apps/kaltura/modules/kmc/templates/kmcSuccess.php
+++ alpha/apps/kaltura/modules/kmc/templates/kmcSuccess.php
@@ -2,18 +2,21 @@
 body { background-color:#272929 !important; background-image:none !important;}
 div#login { width:500px; margin: 0 auto; text-align:center;}
 </style>
-
+<link rel="stylesheet" type="text/css" media="screen" href="/lib/css/kmc5.css" />
 <div id="kmcHeader">
 	<?php if( $logoUrl ) { ?>
 	<img src="<?php echo $logoUrl; ?>" />
 	<?php } else { ?>
 	<img src="/lib/images/kmc/logo_kmc.png" alt="Kaltura CMS" />
 	<?php } ?>
-	<div id="user_links">
+	<div id="langIcon"></div>
+	<div id="user_links" style="right: 36px">
     	<a href="/content/docs/pdf/KMC_User_Manual.pdf" target="_blank">User Manual</a>
 	</div> 
 </div><!-- end kmcHeader -->
-    
+
+<div id="langMenu"></div>
+
 <div id="login">
 	<div id="notSupported">Thank you for your logging into the Kaltura Management Console.<br />The KMC is no longer supported in Internet Explorer 7.<br />Please upgrade your Internet Explorer to a higher version or browse to the KMC from another browser.</div>
     <div id="login_swf"><img src="/lib/images/kmc/flash.jpg" alt="Install Flash Player" /><span>You must have flash installed. <a href="http://get.adobe.com/flashplayer/" target="_blank">click here to download</a></span></div>
@@ -35,4 +38,5 @@ var options = {
 	}
 };
 </script>
+<script src="/lib/js/kmc/6.0.10/langMenu.min.js"></script>
 <script type="text/javascript" src="/lib/js/kmc.login.js"></script>
