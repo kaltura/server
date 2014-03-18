e3760e6a97f461e2c49f06bdac0e8d6758696072
Avi Bueno <avi.bueno@kaltura.com>
2014-03-18 14:55:20 +0200

    Added referenceId for MA Partners, which is configurable via AdminConsole PLAT-974 #time 8h

--- api_v3/lib/types/partner/KalturaPartner.php
+++ api_v3/lib/types/partner/KalturaPartner.php
@@ -309,6 +309,11 @@ class KalturaPartner extends KalturaObject implements IFilterable
 	 */
 	public $crmId;
 
+	/**
+	 * @var string
+	 */
+	public $referenceId;
+
 	private static $map_between_objects = array
 	(
 		'id' , 'name', 'website' => 'url1' , 'notificationUrl' => 'url2' , 'appearInSearch' , 'createdAt' , 'adminName' , 'adminEmail' ,
@@ -317,7 +322,7 @@ class KalturaPartner extends KalturaObject implements IFilterable
 		'maxUploadSize' , 'partnerPackage' , 'secret' , 'adminSecret' , 'allowMultiNotification', 'adminLoginUsersQuota', 'adminUserId',
 		'firstName' , 'lastName' , 'country' , 'state' , 'publishersQuota', 'partnerGroupType', 'defaultEntitlementEnforcement', 
 		'defaultDeliveryType', 'defaultEmbedCodeType', 'deliveryTypes', 'embedCodeTypes',  'templatePartnerId', 'ignoreSeoLinks', 
-		'host', 'cdnHost', 'rtmpUrl', 'isFirstLogin', 'logoutUrl', 'partnerParentId','crmId',
+		'host', 'cdnHost', 'rtmpUrl', 'isFirstLogin', 'logoutUrl', 'partnerParentId','crmId', 'referenceId',
 	);
 	
 	public function getMapBetweenObjects ( )
