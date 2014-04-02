7d904ed49ebbc687de701f4258a809942bdea2de
flipmcf <flipmcf@gmail.com>
2014-04-02 17:23:36 -0400

    additional test case to exercise media.list with utf8 tag search | new test utility 'readyWait' to block until an uploaded media entry is ready to be queried for | whitespace cleanup

--- generator/sources/python/KalturaClient/tests/test_media.py
+++ generator/sources/python/KalturaClient/tests/test_media.py
@@ -6,6 +6,7 @@ from utils import getTestFile
 
 from KalturaClient.Plugins.Core import KalturaMediaListResponse
 from KalturaClient.Plugins.Core import KalturaMediaEntry, KalturaMediaType
+from KalturaClient.Plugins.Core import KalturaMediaEntryFilter
 
 
 class MediaTests(KalturaBaseTest):
@@ -19,7 +20,6 @@ class MediaTests(KalturaBaseTest):
         
         [self.assertIsInstance(o, KalturaMediaEntry) for o in objs]
         
-
     def test_createRemote(self):
         mediaEntry = KalturaMediaEntry()
         mediaEntry.setName('pytest.MediaTests.test_createRemote')
@@ -34,13 +34,15 @@ class MediaTests(KalturaBaseTest):
         
         #cleanup
         self.client.media.delete(mediaEntry.id)
-        
+
+
 class Utf8_tests(KalturaBaseTest):
     
+    test_unicode = u'\u03dd\xf5\xf6'  #an odd representation of the word 'FOO'
+    
     def test_utf8_name(self):
-        test_unicode = u'\u03dd\xf5\xf6'  #an odd representation of the word 'FOO'
         mediaEntry = KalturaMediaEntry()
-        mediaEntry.setName(u'pytest.MediaTests.test_UTF8_name'+test_unicode)
+        mediaEntry.setName(u'pytest.MediaTests.test_UTF8_name'+self.test_unicode)
         mediaEntry.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
         ulFile = getTestFile('DemoVideo.flv')
         uploadTokenId = self.client.media.upload(ulFile)
@@ -50,22 +52,45 @@ class Utf8_tests(KalturaBaseTest):
             
         self.addCleanup(self.client.media.delete, mediaEntry.getId())
     
-    def test_utf8_tags(self):
-
-        test_unicode = u'\u03dd\xf5\xf6'  #an odd representation of the word 'FOO'
+    def test_utf8_tags(self):        
         mediaEntry = KalturaMediaEntry()
         mediaEntry.setName('pytest.MediaTests.test_UTF8_tags')
         mediaEntry.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
         ulFile = getTestFile('DemoVideo.flv')
         uploadTokenId = self.client.media.upload(ulFile)
 
-        mediaEntry.setTags(test_unicode)
+        mediaEntry.setTags(self.test_unicode)
         
         #this will throw an exception if fail.
         mediaEntry = self.client.media.addFromUploadedFile(mediaEntry, uploadTokenId)
             
         self.addCleanup(self.client.media.delete, mediaEntry.getId())
         
+    def test_list_utf8_search(self):
+        """Get a list of videos based on a tag search containing unicode"""
+        mediaEntry = KalturaMediaEntry()
+        mediaEntry.setName('pytest.MediaTests.test_UTF8_tags')
+        mediaEntry.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
+        ulFile = getTestFile('DemoVideo.flv')
+        uploadTokenId = self.client.media.upload(ulFile)
+        mediaEntry.setTags(self.test_unicode)
+        mediaEntry = self.client.media.addFromUploadedFile(mediaEntry, uploadTokenId)
+        self.addCleanup(self.client.media.delete, mediaEntry.getId())
+        
+        self.readyWait(mediaEntry.getId())
+        
+        #find it!
+        kfilter = KalturaMediaEntryFilter()
+        kfilter.setTagsLike(self.test_unicode)
+        result = self.client.media.list(filter=kfilter)
+        
+        self.assertEqual(1, len(result.objects), 
+                         msg="Did not get expected number of objects back from result")
+        
+        mediaResult = result.objects[0]
+        self.assertEqual(mediaEntry.getId(), mediaResult.getId(),
+                         msg="Did not get expected media object from result")
+
 
 def test_suite():
     return unittest.TestSuite((
