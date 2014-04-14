9cf2452587d9610d982d060635e42a4d8f0bf634
sharonadar <blitzkrig@gmail.com>
2014-04-14 10:34:03 +0300

    Velocix forgotten api tokenizer.

---
+++ plugins/delivery/velocix/lib/api/KalturaUrlTokenizerVelocix.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaUrlTokenizerVelocix extends KalturaUrlTokenizer {
+	
+	/**
+	 * hdsPaths
+	 *
+	 * @var string
+	 */
+	public $hdsPaths;
+	
+	/**
+	 * tokenParamName
+	 *
+	 * @var string
+	 */
+	public $tokenParamName;
+	
+	private static $map_between_objects = array
+	(
+			"hdsPaths",
+			"tokenParamName"
+	);
+	
+	public function getMapBetweenObjects ( )
+	{
+		return array_merge ( parent::getMapBetweenObjects() , self::$map_between_objects );
+	}
+	
+	public function toObject($dbObject = null, $skip = array())
+	{
+		if (is_null($dbObject))
+			$dbObject = new kVelocixUrlTokenizer();
+			
+		parent::toObject($dbObject, $skip);
+	
+		return $dbObject;
+	}
+}
