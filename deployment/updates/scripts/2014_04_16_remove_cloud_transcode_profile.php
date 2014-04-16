c44087d3f18a54381c4505d416c5982a2176ec8c
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-04-16 18:49:23 +0300

    Create dependency between cloud transcoding and live-plus permission PLAT-1254 #time 1h

---
+++ deployment/updates/scripts/2014_04_16_remove_cloud_transcode_profile.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage live.liveParams
+ *
+ * Create dependency between cloud transcoding and FEATURE_KALTURA_LIVE_STREAM_TRANSCODE permission
+ *
+ * No need to re-run after server code deploy
+ */
+
+chdir(__DIR__);
+require_once (__DIR__ . '/../../bootstrap.php');
+
+$realRun = isset($argv[1]) && $argv[1] == 'realrun';
+KalturaStatement::setDryRun(!$realRun);
+
+function handleCloudTranscodeProfiles($partnerId, $isTemplate)
+{
+	$c = new Criteria();
+	$c->add(conversionProfile2Peer::STATUS, ConversionProfileStatus::ENABLED);
+	$c->add(conversionProfile2Peer::PARTNER_ID, $partnerId);
+	$c->add(conversionProfile2Peer::SYSTEM_NAME, 'Default_Live');
+	
+	$conversionProfiles = conversionProfile2Peer::doSelect($c);
+	foreach($conversionProfiles as $conversionProfile)
+	{
+		/* @var $conversionProfile conversionProfile2 */
+		if($isTemplate)
+		{
+			$conversionProfile->setRequiredCopyTemplatePermissions(PermissionName::FEATURE_KALTURA_LIVE_STREAM_TRANSCODE);
+		}
+		elseif(!PermissionPeer::isValidForPartner(PermissionName::FEATURE_KALTURA_LIVE_STREAM_TRANSCODE, $partnerId))
+		{
+			$conversionProfile->setStatus(ConversionProfileStatus::DELETED);
+		}
+		$conversionProfile->save();
+	}
+}
+
+function handleLivePartners($partnerIds)
+{
+	$c = new Criteria();
+	$c->add(PartnerPeer::STATUS, Partner::PARTNER_STATUS_ACTIVE);
+	$c->add(PartnerPeer::ID, $partnerIds, Criteria::IN);
+	
+	$partners = PartnerPeer::doSelect($c);
+	foreach($partners as $partner)
+	{
+		/* @var $partner Partner */
+		handleCloudTranscodeProfiles($partner->getId(), $partner->getPartnerGroupType() == PartnerGroupType::TEMPLATE);
+	}
+}
+
+$c = new Criteria();
+$c->add(PermissionPeer::STATUS, PermissionStatus::ACTIVE);
+$c->add(PermissionPeer::NAME, PermissionName::FEATURE_LIVE_STREAM);
+$c->addAscendingOrderByColumn(PermissionPeer::ID);
+$c->setLimit(100);
+
+$offset = 0;
+$permissions = PermissionPeer::doSelect($c);
+while(count($permissions))
+{
+	$partnerIds = array();
+	foreach($permissions as $permission)
+	{
+		/* @var $permission Permission */
+		$partnerIds[] = $permission->getPartnerId();
+	}
+	
+	handleLivePartners($partnerIds);
+	
+	$offset += count($permissions);
+	$c->setOffset($offset);
+	$permissions = PermissionPeer::doSelect($c);
+}
