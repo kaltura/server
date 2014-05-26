2c91c30059b145d963f3d55121ee98e009599f98
David Kohen <kohen.d@gmail.com>
2014-05-26 13:32:58 +0300

    Working version

--- plugins/admin_console/kaltura_internal_tools/admin/forms/NewFlavorParam.php
+++ plugins/admin_console/kaltura_internal_tools/admin/forms/NewFlavorParam.php
@@ -9,18 +9,26 @@ class Form_NewFlavorParam extends Infra_Form
     {
         // Set the method for the display form to POST
         $this->setMethod('post');
-        $this->setDecorators(array(
-            'FormElements',
-            'Form',
-            array('HtmlTag', array('tag' => 'fieldset'))
-        ));
+       // $this->setDecorators(array(
+       ///     'FormElements',
+       //     'Form',
+       //     array('HtmlTag', array('tag' => 'fieldset'))
+       // ));
 
         $this->setAttrib('class', 'inline-form');
         $this->addElement('text', 'name', array(
             'label' => 'Name:',
             'required' => true,
             'filters' => array('StringTrim'),
-            'validators' => array()
+            'validators' => array(array('validator' => 'Regex',
+                'options'=> array('pattern' => '/^[-()\/\_A-Za-z0-9, ]+$/')))
+        ));
+
+        $this->addElement('text', 'partner_id', array(
+            'label' => 'Partner ID (Not Zero):',
+            'required' => true,
+            'filters' => array('Int'),
+            'validators' => array('Int')
         ));
 
         $this->addElement('text', 'video_bitrate', array(
@@ -54,40 +62,37 @@ class Form_NewFlavorParam extends Infra_Form
         $this->addElement('select', 'two_pass', array(
             'label' => 'Two pass encoding:',
             'required' => true,
-            'multiOptions' => array('true'=>'true','false'=>'false')
+            'multiOptions' => array('true' => 'true', 'false' => 'false')
         ));
 
-        $this->addElement('text', 'tags', array(
-            'label' => 'Tags:',
-            'required' => true,
-            'filters' => array('StringTrim'),
-            'validators' => array()
-        ));
+
+
+
 
         $this->addElement('text', 'description', array(
             'label' => 'Description:',
             'required' => true,
             'filters' => array('StringTrim'),
-            'validators' => array()
+            'validators' => array(array('validator' => 'Regex',
+                'options'=> array('pattern' => '/^[-+:_^\'.\/\|=()A-Za-z0-9, ]+$/')))
         ));
 
         $this->addElement('text', 'extra_params', array(
-            'label' => 'Extra engine params:',
-            'required' => true,
-            'filters' => array('StringTrim'),
-            'validators' => array()
+            'label' => 'Extra engine params (optional):',
+            'filters' => array('StringTrim')
         ));
 
-        $this->addElement('text', 'conversion_engines', array(
-            'label' => 'Conversion engines:',
-            'required' => true,
-            'filters' => array('StringTrim'),
-            'validators' => array()
-        ));
-
-        $this->addElements(array(new Kaltura_Form_Element_EnumSelect('video_codec', array('enum' => 'Kaltura_Client_Enum_VideoCodec')),
+        $element_array = array(new Kaltura_Form_Element_EnumSelect('video_codec', array('enum' => 'Kaltura_Client_Enum_VideoCodec')),
             new Kaltura_Form_Element_EnumSelect('audio_codec', array('enum' => 'Kaltura_Client_Enum_AudioCodec')),
-            new Kaltura_Form_Element_EnumSelect('container_format', array('enum' => 'Kaltura_Client_Enum_ContainerFormat'))));
+            new Kaltura_Form_Element_EnumSelect('container_format', array('enum' => 'Kaltura_Client_Enum_ContainerFormat')));
+
+        $element_array[0]->setLabel('Video Codec:');
+
+        $element_array[1]->setLabel('Audio Codec:');
+
+        $element_array[2]->setLabel('Container Format:');
+
+        $this->addElements($element_array);
 
         // Add the submit button
         $this->addElement('button', 'submit', array(
@@ -97,24 +102,28 @@ class Form_NewFlavorParam extends Infra_Form
             'decorators' => array('ViewHelper')
         ));
 
-        $this->addDisplayGroup(array('name',
-            'description',
-                'tags',
-            'video_codec',
-            'audio_codec',
-            'container_format',
-            'bitrate',
-            'video_height',
-            'video_width',
-            'two_pass',
-            'submit'),
+        $this->addDisplayGroup(array('partner_id',
+                'name',
+                'description',
+                'video_codec',
+                'audio_codec',
+                'container_format',
+                'video_bitrate',
+                'audio_bitrate',
+                'video_height',
+                'video_width',
+                'two_pass',
+                'extra_params',
+                'submit'),
             'flavor_info', array(
-            'decorators' => array(
-#            'Description',
-                'FormElements',
-                array('HtmlTag', array('tag' => 'div', 'class' => 'buttons')),
-            )
-        ));
+                'decorators' => array(
+#                   'Description',
+                    'FormElements',
+                    array('HtmlTag', array('tag' => 'div', 'class' => 'buttons')),
+                    array('Fieldset'),
+                ),
+                'legend' => 'Flavor properties',
+            ));
 
 
     }
