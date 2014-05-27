7ed1ce9134587a030d82162e02908a17df705ac6
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-05-27 19:01:13 +0300

    Entry id is required in play-server URL

--- alpha/lib/model/DeliveryProfileLiveAppleHttp.php
+++ alpha/lib/model/DeliveryProfileLiveAppleHttp.php
@@ -126,7 +126,7 @@ class DeliveryProfileLiveAppleHttp extends DeliveryProfileLive {
 		$partnerId = $entry->getPartnerId();
 		$playServerHost = myPartnerUtils::getPlayServerHost($partnerId, $this->params->getMediaProtocol());
 		
-		$url = "$playServerHost/manifest/master";
+		$url = "$playServerHost/manifest/master/entryId/$entryId";
 		if(count($this->params->getPlayerConfig()))
 			$url .= '/playerConfig/' . $this->params->getPlayerConfig();
 			
