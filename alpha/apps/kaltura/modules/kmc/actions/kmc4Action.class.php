aaa44938974506d6700c085c3c01aee8c572b1ec
amirch1 <amir.chervinsky@kaltura.com>
2014-06-05 12:16:33 +0300

    #FEC-1454 #time 2h #comment get studio UIConf according to the base.ini studio version (get the UIConf that has a tag like "HTML5Studio_" + version)

--- alpha/apps/kaltura/modules/kmc/actions/kmc4Action.class.php
+++ alpha/apps/kaltura/modules/kmc/actions/kmc4Action.class.php
@@ -153,7 +153,7 @@ class kmc4Action extends kalturaAction
 		$this->content_uiconds_clipapp_kdp = kmcUtils::find_confs_by_usage_tag($kmcGeneralTemplateUiConf, "kmc_kdpClipApp", false, $kmcGeneralUiConf);
 		$this->content_uiconds_clipapp_kclip = kmcUtils::find_confs_by_usage_tag($kmcGeneralTemplateUiConf, "kmc_kClipClipApp", false, $kmcGeneralUiConf);
 		
-		$this->studioUiConf = kmcUtils::getStudioUiconf();
+		$this->studioUiConf = kmcUtils::getStudioUiconf(kConf::get("studio_version"));
 		$this->content_uiconfs_studio_v2 = isset($this->studioUiConf) ? array_values($this->studioUiConf) : null;
 		$this->content_uiconf_studio_v2 = (is_array($this->content_uiconfs_studio_v2) && reset($this->content_uiconfs_studio_v2)) ? reset($this->content_uiconfs_studio_v2) : null;
 		
