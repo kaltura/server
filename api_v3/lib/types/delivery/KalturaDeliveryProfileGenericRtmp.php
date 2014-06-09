ea67854e09d7fa9f7e63a99e4e92df6158bbf56d
sharonadar <blitzkrig@gmail.com>
2014-06-09 16:21:52 +0300

    Added support in Delviery profile generic RTMP with roman. usefull for bitgravity.

---
+++ api_v3/lib/types/delivery/KalturaDeliveryProfileGenericRtmp.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaDeliveryProfileGenericRtmp extends KalturaDeliveryProfile {
+	
+	/**
+	 * @var string
+	 */
+	public $pattern;
+	
+	/**
+	 * rendererClass
+	 * @var string
+	 */
+	public $rendererClassParam;
+	
+	
+	private static $map_between_objects = array
+	(
+			"pattern",
+			"rendererClassParam",
+	);
+	
+	public function getMapBetweenObjects ( )
+	{
+		return array_merge ( parent::getMapBetweenObjects() , self::$map_between_objects );
+	}
+}
+
