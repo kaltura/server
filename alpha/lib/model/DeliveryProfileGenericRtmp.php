a376f508cea369e0dfd0b2767050831d9af41644
sharonadar <blitzkrig@gmail.com>
2014-06-10 18:22:02 +0300

    Changed paramter name.

--- alpha/lib/model/DeliveryProfileGenericRtmp.php
+++ alpha/lib/model/DeliveryProfileGenericRtmp.php
@@ -15,21 +15,14 @@ class DeliveryProfileGenericRtmp extends DeliveryProfileRtmp {
 		return $this->getFromCustomData("pattern");
 	}
 	
-	public function setRendererClassParam($v)
+	public function setRendererClass($v)
 	{
 		$this->putInCustomData("rendererClass", $v);
 	}
 	
-	public function getRendererClassParam()
+	public function getRendererClass()
 	{
-		return $this->getFromCustomData("rendererClass");
-	}
-	
-	protected function getRendererClass() {
-		$rendererClass = $this->getRendererClassParam();
-		if($rendererClass)
-			return $rendererClass;
-		return $this->DEFAULT_RENDERER_CLASS;
+		return $this->getFromCustomData("rendererClass", null , $this->DEFAULT_RENDERER_CLASS);
 	}
 	
 	protected function doGetFlavorAssetUrl(flavorAsset $flavorAsset) 
