630ede09983db7488452a52e5db989c300f661b7
shpiNi <liron.shpinerman@Kaltura.com>
2014-06-15 11:29:22 +0300

    adding deployment script & release notes

---
+++ deployment/updates/scripts/2014_06_15_add_entry_changed_email_notification.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package deployment
+ * 
+ * Add 'ENTRY_CHANGED' email notification template
+ */
+
+$script = realpath(dirname(__FILE__) . '/../../../') . '/tests/standAloneClient/exec.php';
+$xmlRequest = realpath(dirname(__FILE__) . '/../../../') . '/tests/standAloneClient/emailEntryChangedNotificationTemplate.xml';
+passthru("php $script $xmlRequest");
