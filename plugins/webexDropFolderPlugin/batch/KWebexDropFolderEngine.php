bef875e1598492a8dbec4bbb490bb5abfb8366ba
Avi Bueno <avi.bueno@kaltura.com>
2014-07-06 14:05:15 +0300

    Ensuring autoFileDeleteDays = 0 is not being ignored and interpreted as 'delete immediately' PLAT-1404

--- plugins/webexDropFolderPlugin/batch/KWebexDropFolderEngine.php
+++ plugins/webexDropFolderPlugin/batch/KWebexDropFolderEngine.php
@@ -149,11 +149,6 @@ class KWebexDropFolderEngine extends KDropFolderEngine
 	{
 		KalturaLog::info('Deleting older files from Webex account');
 		
-		if (!$this->dropFolder->autoFileDeleteDays)
-		{
-			KalturaLog::err("Illegal value for property [autoFileDeleteDays] on drop folder with ID [" . $this->dropFolder->id . "]. Exiting.");
-			return;
-		}
 		$createTimeEnd = strtotime ("now") - ($this->dropFolder->autoFileDeleteDays*86400);
 		$fileList = $this->listRecordings(self::ZERO_DATE, date('m/j/Y H:i:s',$createTimeEnd));
 		KalturaLog::debug("Files to delete: " . count($fileList));
