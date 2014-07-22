44e71be78abaeed9bad3d3376a989dd5ff0b1c32
hila.karimov <hila.karimov@kaltura.com>
2014-07-22 18:24:36 +0300

    HDS local path delivery profile

---
+++ alpha/lib/model/DeliveryProfileLocalPathHds.php
@@ -0,0 +1,33 @@
+<?php
+class DeliveryProfileLocalPathHds extends DeliveryProfileHds {
+	
+// doGetFlavorAssetUrl - Fully inherit from parent
+	
+	function __construct() {
+		parent::__construct();
+		$this->DEFAULT_RENDERER_CLASS = 'kF4Mv2ManifestRenderer';
+	}
+	
+	public function setRendererClass($v)
+	{
+		$this->putInCustomData("rendererClass", $v);
+	}
+	
+	public function getRendererClass()
+	{
+		return $this->getFromCustomData("rendererClass", null, $this->DEFAULT_RENDERER_CLASS);
+	}
+	
+	protected function doGetFlavorAssetUrl(flavorAsset $flavorAsset)
+	{
+		$syncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
+		$fileSync = kFileSyncUtils::getReadyInternalFileSyncForKey($syncKey);
+		return $this->doGetFileSyncUrl($fileSync);
+	}
+	
+	protected function doGetFileSyncUrl(FileSync $fileSync) {
+		$url = parent::doGetFileSyncUrl($fileSync);
+		return $url . "/manifest.f4m";
+	}
+	
+}
