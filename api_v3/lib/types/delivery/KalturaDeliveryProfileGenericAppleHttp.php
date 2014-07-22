1a76d83d3d626ca6155a4b67c667f0d1296f17e7
erankor <eran.kornblau@kaltura.com>
2014-07-22 18:13:10 +0300

    generic hls - add support for playManifest redirect
    the playManifest redirect is in order to make the playManifest request
    share the same domain with the flavor m3u8s and ts segments for token
    cookies

--- api_v3/lib/types/delivery/KalturaDeliveryProfileGenericAppleHttp.php
+++ api_v3/lib/types/delivery/KalturaDeliveryProfileGenericAppleHttp.php
@@ -16,11 +16,19 @@ class KalturaDeliveryProfileGenericAppleHttp extends KalturaDeliveryProfile {
 	 */
 	public $rendererClass;
 	
+	/**
+	 * Enable to make playManifest redirect to the domain of the delivery profile
+	 *
+	 * @var KalturaNullableBoolean
+	 */
+	public $manifestRedirect;
+	
 	
 	private static $map_between_objects = array
 	(
 			"pattern",
 			"rendererClass",
+			"manifestRedirect",
 	);
 	
 	public function getMapBetweenObjects ( )
