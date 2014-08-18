0209ab74c8d76bdbc556576109bcdb271c514f9d
Avi Bueno <avi.bueno@kaltura.com>
2014-08-18 19:31:03 +0300

    Replaced while condition + updates KMS-3488 #time 3h

--- deployment/updates/scripts/2014_07_31_match_plays_to_views_for_image_entries.php
+++ deployment/updates/scripts/2014_07_31_match_plays_to_views_for_image_entries.php
@@ -15,23 +15,42 @@ KalturaStatement::setDryRun(!$realRun);
 $c = KalturaCriteria::create(entryPeer::OM_CLASS);
 $c->add( entryPeer::MEDIA_TYPE, entry::ENTRY_MEDIA_TYPE_IMAGE );
 $c->add( entryPeer::TYPE, entryType::MEDIA_CLIP);
-$c->addAscendingOrderByColumn(entryPeer::INT_ID);
+$c->addAscendingOrderByColumn(entryPeer::CREATED_AT);
 $c->addCondition("plays <> views");
 $c->setLimit( 100 ); // Select in bulks of 100
 
-$lastIntId = 0;
-$entries = entryPeer::doSelect($c);
-while(count($entries))
+$processing = true;
+$lastCreatedAt = 0;
+$processedEntries = array();
+while( $processing )
 {
+	$entries = entryPeer::doSelect($c);
+	$processing = false;
+
 	foreach($entries as $entry)
 	{
-		$entry->setPlays( $entry->getViews() );
-		$entry->save();
-		$lastIntId = $entry->getIntId();
+		$entryId = $entry->getId();
+
+		if ( ! in_array($entryId, $processedEntries) )
+		{
+			$entry->setPlays( $entry->getViews() );
+			$entry->save();
+			$processing = true;
+
+			$createdAt = $entry->getCreatedAt('%s');
+			if ( $createdAt > $lastCreatedAt )
+			{
+				$lastCreatedAt = $createdAt;
+				$processedEntries = array();
+			}
+			else
+			{
+				$processedEntries[] = $entryId;
+			}
+		}
 	}
 
 	usleep( 50 * 1000 ); // Rest for 50 msec
 	
-	$c->add(entryPeer::INT_ID, $lastIntId, Criteria::GREATER_THAN);
-	$entries = entryPeer::doSelect( $c );
+	$c->add(entryPeer::CREATED_AT, $lastCreatedAt, Criteria::GREATER_EQUAL);
 }
