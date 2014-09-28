6aa1153541e4d1fa79eb0e8f056f73f54d3fe3a5
shpiNi <liron.shpinerman@Kaltura.com>
2014-09-28 16:03:16 +0300

    use array locally

--- batch/batches/BulkUpload/Engines/KBulkUploadEngine.php
+++ batch/batches/BulkUpload/Engines/KBulkUploadEngine.php
@@ -179,11 +179,11 @@ abstract class KBulkUploadEngine
 	protected function isUrl($str)
 	{
 		$str = KCurlWrapper::encodeUrl($str);
-		
-		$redundant_chars = kConf::get("redundant_url_chars");
-		
-		$str = str_replace($redundant_chars,"",$str);
-		
+
+		$redundant_url_chars = array("_");
+
+		$str = str_replace($redundant_url_chars , "" , $str);
+
 		return filter_var($str, FILTER_VALIDATE_URL);
 	}
 		
