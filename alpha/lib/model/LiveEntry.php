e58987d563c5f1c7eafceabfa24a07c86f2d2549
hila.karimov <hila.karimov@kaltura.com>
2014-10-01 18:36:46 +0300

    Remove DVR + Recording fix

--- alpha/lib/model/LiveEntry.php
+++ alpha/lib/model/LiveEntry.php
@@ -554,14 +554,7 @@ abstract class LiveEntry extends entry
 	}
 	
 	public function unsetMediaServer($index, $hostname)
-	{
-		if (!$this->validateLiveRecordingStatus())
-		{
-			KalturaLog::info("Postpone unregister - recorded entry is not ready yet.");
-			$this->setFirstUnregisterAttemptTimestamp(time());
-			return;	
-		}
-		
+	{	
 		$server = $this->getFromCustomData("server-$index", LiveEntry::CUSTOM_DATA_NAMESPACE_MEDIA_SERVERS);
 		if($server && $server->getHostname() == $hostname)
 			$server = $this->removeFromCustomData("server-$index", LiveEntry::CUSTOM_DATA_NAMESPACE_MEDIA_SERVERS);
@@ -572,16 +565,6 @@ abstract class LiveEntry extends entry
 	 */
 	public function validateMediaServers()
 	{
-		if (!$this->validateLiveRecordingStatus())
-		{
-			if (time() - $this->getFirstUnregisterAttemptTimestamp() < intval(kConf::get('default_live_recording_timeout')))
-			{
-				KalturaLog::info("Postpone unregister - recorded entry is not ready yet.");
-				return;	
-			}
-		}
-		
-		$this->setFirstUnregisterAttemptTimestamp(null);
 		$listChanged = false;
 		$kMediaServers = $this->getFromCustomData(null, LiveEntry::CUSTOM_DATA_NAMESPACE_MEDIA_SERVERS, array());
 		foreach($kMediaServers as $key => $kMediaServer)
@@ -595,24 +578,6 @@ abstract class LiveEntry extends entry
 		}
 		
 		return $listChanged;
-	}
-	
-	protected function validateLiveRecordingStatus ()
-	{
-		if ($this->getDvrStatus() == DVRStatus::ENABLED && $this->getRecordedEntryId())
-		{
-			$recording = entryPeer::retrieveByPK($this->getRecordedEntryId());
-			if ($recording)
-			{
-				if ($recording->getStatus() != entryStatus::READY || !is_null($recording->getReplacingEntryId()))
-				{
-					KalturaLog::info("Recorded entry is not ready yet.");
-					return false;
-				}
-			}
-		}
-		
-		return true;
 	}
 	
 	/**
@@ -682,16 +647,6 @@ abstract class LiveEntry extends entry
 	public function setPushPublishConfigurations ($v)
 	{
 		$this->putInCustomData('push_publish_configurations', $v);
-	}
-	
-	public function getFirstUnregisterAttemptTimestamp ()
-	{
-		return $this->getFromCustomData('first_unregister_attempt_timestamp',null);
-	}
-	
-	public function setFirstUnregisterAttemptTimestamp ($v)
-	{
-		$this->putInCustomData('first_unregister_attempt_timestamp', $v);
 	}
 	
 	/**
