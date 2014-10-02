591fb77a6fdd67900e73b59db6b7654b925fcb30
Atar Shadmi <atar.shadmi@kaltura.com>
2014-10-02 11:37:12 +0300

    FEC-1370 only save features if actual change

--- deployment/uiconf/deploy_v2.php
+++ deployment/uiconf/deploy_v2.php
@@ -508,9 +508,11 @@ class uiConfDeployment
 		$conf_file = $uiconf->getConfFile(true);
 		$featuresFile = $uiconf->getConfFileFeatures(true);
 		$newFeatures = str_replace($replacementToken, $replacementString, $featuresFile);
-		$uiconf->setConfFile($conf_file);
-		$uiconf->setConfFileFeatures($newFeatures);
-		$uiconf->save();
+		if ($newFeatures != $featuresFile) {
+			$uiconf->setConfFile($conf_file);
+			$uiconf->setConfFileFeatures($newFeatures);
+			$uiconf->save();
+		}
 	}
 	
 	/**
