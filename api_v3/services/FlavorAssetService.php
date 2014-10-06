600069e0854c25622dc9462acf15d9cdc738627a
assafb <asafber5@gmail.com>
2014-10-06 11:43:49 +0300

    ps:CR-179513 validation that will block the deletion of local file syncs if no remote file syncs exists

--- api_v3/services/FlavorAssetService.php
+++ api_v3/services/FlavorAssetService.php
@@ -927,6 +927,10 @@ class FlavorAssetService extends KalturaAssetService
 		// get the local file syncs for this asset
 		$syncable = kFileSyncObjectManager::retrieveObject(FileSyncObjectType::ASSET ,$assetId);
 		$srcSyncKey = $syncable->getSyncKey(asset::FILE_SYNC_ASSET_SUB_TYPE_ASSET);
+		$externalFileSyncs = kFileSyncUtils::getReadyExternalFileSyncForKey($srcSyncKey);
+		if (!$externalFileSyncs)
+			throw new KalturaAPIException(KalturaErrors::NO_EXTERNAL_CONTENT_EXISTS);
+
 		$fileSyncs = kFileSyncUtils::getReadyInternalFileSyncsForKey($srcSyncKey);
 		foreach ($fileSyncs as $fileSync){
 			/* @var $fileSync FileSync*/
