217285318e817a6ab00af0056d7588c9761545e5
assafb <asafber5@gmail.com>
2014-10-21 16:04:50 +0300

    add directory seperator

--- deployment/updates/scripts/add_permissions/2014_10_05_flavorAsset_delete_local_content.php
+++ deployment/updates/scripts/add_permissions/2014_10_05_flavorAsset_delete_local_content.php
@@ -8,5 +8,5 @@
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
-$config = realpath(dirname(__FILE__) . '../../../') . '/permissions/service.flavorasset.ini';
+$config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/service.flavorasset.ini';
 passthru("php $script $config");
