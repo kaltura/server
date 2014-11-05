c9e5febc2d50794a2144d1fbb6a9f5b3ca6676ab
Avi Bueno <avi.bueno@kaltura.com>
2014-11-05 16:05:42 +0200

    Added KalturaMediaEntry::sourceType filters PLAT-2038 #time 1d

--- api_v3/lib/types/entry/filters/KalturaMediaEntryFilter.php
+++ api_v3/lib/types/entry/filters/KalturaMediaEntryFilter.php
@@ -5,4 +5,16 @@
  */
 class KalturaMediaEntryFilter extends KalturaMediaEntryBaseFilter
 {
+	static private $map_between_objects = array
+	(
+		"sourceTypeEqual" => "_eq_source",
+		"sourceTypeNotEqual" => "_not_source",
+		"sourceTypeIn" => "_in_source",
+		"sourceTypeNotIn" => "_notin_source",
+	);
+	
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
+	}
 }
