2b3fb27dc72706a3b8256d5edc6de406c212f910
shpiNi <liron.shpinerman@Kaltura.com>
2014-11-18 17:25:27 +0200

    enable non-admin session to get users only if self user SUP-2899 #time 2h

--- api_v3/services/UserService.php
+++ api_v3/services/UserService.php
@@ -189,7 +189,10 @@ class UserService extends KalturaBaseUserService
 	    {
             $userId = kCurrentContext::$ks_uid;	        
 	    }
-	    
+
+		if (!kCurrentContext::$is_admin_session && kCurrentContext::$ks_uid != $userId)
+			throw new KalturaAPIException(KalturaErrors::DIFFERENT_USER_QUERY_USING_NON_ADMIN_SESSSION);
+
 		$dbUser = kuserPeer::getKuserByPartnerAndUid($this->getPartnerId(), $userId);
 	
 		if (!$dbUser)
