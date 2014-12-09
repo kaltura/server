115b9e77d16536811c39a1e6d19af218734687ca
Anatol <anatol.schwartz@kaltura.com>
2014-12-09 11:04:05 +0200

    DAR/PAR calc fix
    Less strict diff (original was diff>0.1) test to allow hadling of 5:4 to 4:3 adjustments

--- infra/cdl/kdl/KDLFlavor.php
+++ infra/cdl/kdl/KDLFlavor.php
@@ -756,7 +756,7 @@ $plannedDur = 0;
 			$darSrc = $source->_dar;
 			$diff = abs(1-$darSrc/$darSrcFrame);
 				// Less strict diff (original was diff>0.1) test to allow hadling of 5:4 to 4:3 adjustments
-			if($diff>0.05) { 
+			if($diff>0.05) {  
 				$widSrc = $darSrc*$hgtSrc;
 				$darSrcFrame = $darSrc;
 			}
