118e0616a5dbdec1404b5fdb2e6d76d3c9fbbfff
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-12-31 16:48:03 +0200

    Support closers

--- plugins/content_distribution/providers/unicorn/UnicornDistributionPlugin.php
+++ plugins/content_distribution/providers/unicorn/UnicornDistributionPlugin.php
@@ -90,6 +90,12 @@ class UnicornDistributionPlugin extends KalturaPlugin implements IKalturaPermiss
 					
 			if($baseClass == 'IDistributionEngineUpdate')
 				return 'UnicornDistributionEngine';
+					
+			if($baseClass == 'IDistributionEngineCloseSubmit')
+				return 'UnicornDistributionEngine';
+					
+			if($baseClass == 'IDistributionEngineCloseUpdate')
+				return 'UnicornDistributionEngine';
 		}
 		
 		if (class_exists('Kaltura_Client_Client') && $enumValue == Kaltura_Client_ContentDistribution_Enum_DistributionProviderType::UNICORN)
