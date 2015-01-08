9384dce3ee78a3bb4febe0f5448e19846a7a87b5
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-01-08 13:17:54 +0200

    Added BPM Abort enum

--- admin_console/configs/lang/en.php
+++ admin_console/configs/lang/en.php
@@ -1120,6 +1120,7 @@ return array(
 	'Kaltura_Client_EventNotification_Enum_EventNotificationTemplateType::HTTP' => 'HTTP',
 	'Kaltura_Client_EventNotification_Enum_EventNotificationTemplateType::BPM_START' => 'Start Business-Process',
 	'Kaltura_Client_EventNotification_Enum_EventNotificationTemplateType::BPM_SIGNAL' => 'Signal Business-Process',
+	'Kaltura_Client_EventNotification_Enum_EventNotificationTemplateType::BPM_ABORT' => 'Abort Business-Process',
 
 	'Kaltura_Client_EventNotification_Enum_EventNotificationEventType::BATCH_JOB_STATUS' => 'Batch job status (Very dangerous)',
 	'Kaltura_Client_EventNotification_Enum_EventNotificationEventType::OBJECT_ADDED' => 'Object added',
