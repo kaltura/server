e74f169499718c8f732a38af9f8529a34187a2e2
Orly Lampert <orly.lampert@kaltura.com>
2015-01-20 10:55:38 +0200

    add top playbackContext report

---
+++ alpha/apps/kaltura/lib/reports/top_playback_context/top_playback_context_detail_by_context_for_objects.sql
@@ -0,0 +1,38 @@
+SELECT 
+	cat.name name,
+	count_plays,
+	sum_time_viewed,
+	avg_time_viewed,
+	avg_view_drop_off,
+	count_loads,
+	load_play_ratio
+FROM
+(
+	SELECT 
+		context_id,
+		SUM(sum_time_viewed) sum_time_viewed,
+		SUM(sum_time_viewed)/SUM(count_plays) avg_time_viewed,
+		SUM(count_plays) count_plays,
+		SUM(count_loads) count_loads,
+		(SUM(IFNULL(count_plays_25,0)) + SUM(IFNULL(count_plays_50,0)) + SUM(IFNULL(count_plays_75,0)) + SUM(IFNULL(count_plays_100,0)))/4/SUM(count_plays) avg_view_drop_off,
+		( SUM(count_plays) / SUM(count_loads) ) load_play_ratio
+	FROM 
+		dwh_hourly_events_context_entry_user_app ev
+	WHERE 	
+	    {OBJ_ID_CLAUSE} # ev.entry_id in 
+		AND {CAT_ID_CLAUSE}
+		AND partner_id =  {PARTNER_ID} # PARTNER_ID
+        AND date_id BETWEEN IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID})  
+    			AND     IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID})
+			AND hour_id >= IF (date_id = IF({TIME_SHIFT}>0,(DATE({FROM_DATE_ID}) - INTERVAL 1 DAY)*1, {FROM_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 0)
+			AND hour_id < IF (date_id = IF({TIME_SHIFT}<=0,(DATE({TO_DATE_ID}) + INTERVAL 1 DAY)*1, {TO_DATE_ID}), IF({TIME_SHIFT}>0, 24 - {TIME_SHIFT}, ABS({TIME_SHIFT})), 24)
+		AND ( count_time_viewed > 0 OR
+		  count_plays > 0 OR
+		  count_loads > 0 OR 
+		  sum_time_viewed > 0 )
+	GROUP BY context_id
+	ORDER BY {SORT_FIELD}
+	LIMIT {PAGINATION_FIRST},{PAGINATION_SIZE}  /* pagination  */
+) AS ev_stats, dwh_dim_category cat
+WHERE ev_stats.context_id = cat.category_id
+AND cat.partner_id = {PARTNER_ID}
