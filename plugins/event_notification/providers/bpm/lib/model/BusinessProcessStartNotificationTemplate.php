06a5c2d8c81f6ac38e023ad80d9ae2e8f3ec365f
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-01-21 17:13:16 +0200

    Disable parameters for abort actions

--- plugins/event_notification/providers/bpm/lib/model/BusinessProcessStartNotificationTemplate.php
+++ plugins/event_notification/providers/bpm/lib/model/BusinessProcessStartNotificationTemplate.php
@@ -5,10 +5,28 @@
  */
 class BusinessProcessStartNotificationTemplate extends BusinessProcessNotificationTemplate
 {
+	private $disableParameters = false;
+	
 	public function __construct()
 	{
 		$this->setType(BusinessProcessNotificationPlugin::getBusinessProcessNotificationTemplateTypeCoreValue(BusinessProcessNotificationTemplateType::BPM_START));
 		parent::__construct();
+	}
+	
+	/* (non-PHPdoc)
+	 * @see EventNotificationTemplate::getContentParameters()
+	 */
+	public function getContentParameters()
+	{
+		return $this->disableParameters ? array() : parent::getContentParameters();
+	}
+	
+	/* (non-PHPdoc)
+	 * @see EventNotificationTemplate::getUserParameters()
+	 */
+	public function getUserParameters()
+	{
+		return $this->disableParameters ? array() : parent::getUserParameters();
 	}
 	
 	/* (non-PHPdoc)
@@ -17,6 +35,17 @@ class BusinessProcessStartNotificationTemplate extends BusinessProcessNotificati
 	public function abort($scope)
 	{
 		$abortCaseJobType = BusinessProcessNotificationPlugin::getBusinessProcessNotificationTemplateTypeCoreValue(BusinessProcessNotificationTemplateType::BPM_ABORT);
-		return $this->dispatchPerCase($scope, $abortCaseJobType);
+		$this->disableParameters = true;
+		try 
+		{
+			$ret = $this->dispatchPerCase($scope, $abortCaseJobType);	
+		}
+		catch (Exception $e)
+		{
+			$this->disableParameters = false;
+			throw $e;
+		}
+		$this->disableParameters = false;
+		return $ret;
 	}
 }
