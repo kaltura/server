afd9665fe639d86224cedeadcb6eb614321e4437
Anatol <anatol.schwartz@kaltura.com>
2015-01-29 11:41:54 +0200

    forced aspect ratio support

--- infra/cdl/kdl/KDLOperatorFfmpeg2_2.php
+++ infra/cdl/kdl/KDLOperatorFfmpeg2_2.php
@@ -6,19 +6,22 @@
 class KDLOperatorFfmpeg2_2 extends KDLOperatorFfmpeg2_1_3 {
 
 	/* ---------------------------
-	 * getVideoCodecSpecificParams
+	 * generateVideoParams
 	 */
-	protected function getVideoCodecSpecificParams(KDLFlavor $design, KDLFlavor $target)
+	protected function generateVideoParams(KDLFlavor $design, KDLFlavor $target)
 	{
-		$vid = $target->_video;
-		if($vid->_id==KDLVideoTarget::H265) {
-			$cmdStr = "libx265 -pix_fmt yuv420p";
-			if((isset($vid->_width) && $vid->_width>0)
-			&& (isset($vid->_height) && $vid->_height>0)){
-				$cmdStr.= " -aspect $vid->_width:$vid->_height";
-			}
+		$cmdStr = parent::generateVideoParams($design, $target);
+		if(!isset($target->_video))
 			return $cmdStr;
+	
+		$vid = $target->_video;
+			/*
+			 * Force explicit aspect ratio setting
+			 * origionaly required by x265, but 
+			 */
+		if($vid->_arProcessingMode==4 && isset($vid->_width) && $vid->_width>0 && isset($vid->_height) && $vid->_height>0){
+			$cmdStr.= " -aspect $vid->_width:$vid->_height"; 
 		}
-		return parent::getVideoCodecSpecificParams($design, $target);
+		return $cmdStr;
 	}
 }
