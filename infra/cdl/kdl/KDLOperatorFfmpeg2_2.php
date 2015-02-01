9f59d0241169d46df6ad66e61f2f5f5470719994
Anatol <anatol.schwartz@kaltura.com>
2015-02-01 16:35:49 +0200

    PLAT-2452: EBU - Fixing in-accurate aspect ratio

--- infra/cdl/kdl/KDLOperatorFfmpeg2_2.php
+++ infra/cdl/kdl/KDLOperatorFfmpeg2_2.php
@@ -17,9 +17,14 @@ class KDLOperatorFfmpeg2_2 extends KDLOperatorFfmpeg2_1_3 {
 		$vid = $target->_video;
 			/*
 			 * Force explicit aspect ratio setting
-			 * origionaly required by x265, but 
+			 * for following cases:
+			 * - x265
+			 * - x264
+			 * - on _arProcessingMode==4
 			 */
-		if($vid->_arProcessingMode==4 && isset($vid->_width) && $vid->_width>0 && isset($vid->_height) && $vid->_height>0){
+		if((isset($vid->_width) && $vid->_width>0 && isset($vid->_height) && $vid->_height>0) 
+		&& (in_array($vid->_id, array(KDLVideoTarget::H265,KDLVideoTarget::H264,KDLVideoTarget::H264B,KDLVideoTarget::H264M,KDLVideoTarget::H264H))
+			|| $vid->_arProcessingMode==4)){
 			$cmdStr.= " -aspect $vid->_width:$vid->_height"; 
 		}
 		return $cmdStr;
