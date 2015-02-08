9a400ebc12576d3fa1d889c468516a7cb235b7a9
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-02-08 09:22:27 +0200

    Version all assets serve URLs

--- alpha/lib/model/entry.php
+++ alpha/lib/model/entry.php
@@ -2177,7 +2177,10 @@ class entry extends Baseentry implements ISyncableFile, IIndexable, IOwnable
 
 	public function setCacheFlavorVersion($v)       {$this->putInCustomData("cache_flavor_version", $v);}
 	public function getCacheFlavorVersion()       {return $this->getFromCustomData("cache_flavor_version");}
-
+	
+	public function setCacheThumbnailVersion($v)       {$this->putInCustomData("cache_thumb_version", $v);}
+	public function getCacheThumbnailVersion()       {return $this->getFromCustomData("cache_thumb_version");}
+	
 	private $m_puser_id = null;
 	public function tempSetPuserId ( $puser_id )
 	{
