b097af0bde77c3e9f8a6ff2001575e675c17520b
Shahar Danos <shahar.danos@kaltura.com>
2015-03-04 11:26:38 +0200

    additional implementation to the PushNotificationTemplate

--- plugins/event_notification/providers/push/lib/api/KalturaPushNotificationTemplate.php
+++ plugins/event_notification/providers/push/lib/api/KalturaPushNotificationTemplate.php
@@ -9,4 +9,16 @@ class KalturaPushNotificationTemplate extends KalturaEventNotificationTemplate
     {
         $this->type = PushNotificationPlugin::getApiValue(PushNotificationTemplateType::PUSH);
     }
+
+
+    /* (non-PHPdoc)
+     * @see KalturaObject::toObject()
+     */
+    public function toObject($dbObject = null, $propertiesToSkip = array())
+    {
+        if(is_null($dbObject))
+            $dbObject = new PushNotificationTemplate();
+        	
+        return parent::toObject($dbObject, $propertiesToSkip);
+    }
 }
--- plugins/event_notification/providers/push/lib/model/PushNotificationTemplate.php
+++ plugins/event_notification/providers/push/lib/model/PushNotificationTemplate.php
@@ -5,6 +5,12 @@
 */
 class PushNotificationTemplate extends EventNotificationTemplate
 {
+    public function __construct()
+    {
+        $this->setType(PushNotificationPlugin::getPushNotificationTemplateTypeCoreValue(PushNotificationTemplateType::PUSH));
+        parent::__construct();
+    }
+        
     public function dispatch(kScope $scope) 
     {
         //TODO send message to queue (rabbitmq)
