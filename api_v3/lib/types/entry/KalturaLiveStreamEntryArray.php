063dd172b3d318e14f9f009e6fd41c0c0a921bf8
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-03-12 18:05:27 +0200

    Compat-check, it should never return the admin object

--- api_v3/lib/types/entry/KalturaLiveStreamEntryArray.php
+++ api_v3/lib/types/entry/KalturaLiveStreamEntryArray.php
@@ -7,13 +7,12 @@ class KalturaLiveStreamEntryArray extends KalturaTypedArray
 {
 	public static function fromDbArray($arr, KalturaDetachedResponseProfile $responseProfile = null)
 	{
-		$isAdmin = kCurrentContext::$is_admin_session;
 		$newArr = new KalturaLiveStreamEntryArray();
 		if ($arr == null)
 			return $newArr;		
 		foreach ($arr as $obj)
 		{
-    		$nObj = KalturaEntryFactory::getInstanceByType($obj->getType(), $isAdmin);
+    		$nObj = KalturaEntryFactory::getInstanceByType($obj->getType());
 			$nObj->fromObject($obj, $responseProfile);
 			$newArr[] = $nObj;
 		}
--- api_v3/lib/types/entry/KalturaMediaEntryArray.php
+++ api_v3/lib/types/entry/KalturaMediaEntryArray.php
@@ -7,13 +7,12 @@ class KalturaMediaEntryArray extends KalturaTypedArray
 {
 	public static function fromDbArray($arr, KalturaDetachedResponseProfile $responseProfile = null)
 	{
-		$isAdmin = kCurrentContext::$is_admin_session;
 		$newArr = new KalturaMediaEntryArray();
 		if ($arr == null)
 			return $newArr;		
 		foreach ($arr as $obj)
 		{
-    		$nObj = KalturaEntryFactory::getInstanceByType($obj->getType(), $isAdmin);
+    		$nObj = KalturaEntryFactory::getInstanceByType($obj->getType());
 			$nObj->fromObject($obj, $responseProfile);
 			$newArr[] = $nObj;
 		}
