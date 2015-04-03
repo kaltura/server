0dd5149b94e7f5cbed35b3fd87316652a56e9fe8
Noam Arad <noam.arad@kaltura.com>
2015-04-03 04:14:45 +0300

    in line 1245 there was '=' instead of '==' Jupiter-10.9.0_SUP-4385

--- alpha/lib/model/entry.php
+++ alpha/lib/model/entry.php
@@ -1242,7 +1242,7 @@ class entry extends Baseentry implements ISyncableFile, IIndexable, IOwnable
 			$maxCategoriesPerEntry = entry::MAX_CATEGORIES_PER_ENTRY_DISABLE_LIMIT_FEATURE;
 			
 		// When batch move entry between categories it's adding the new category before deleting the old one
-		if(kCurrentContext::$ks_partner_id = Partner::BATCH_PARTNER_ID && kCurrentContext::$ks_object)
+		if(kCurrentContext::$ks_partner_id == Partner::BATCH_PARTNER_ID && kCurrentContext::$ks_object)
 		{
 			$batchJobType = kCurrentContext::$ks_object->getPrivilegeValue(ks::PRIVILEGE_BATCH_JOB_TYPE);
 			if(intval($batchJobType) == BatchJobType::MOVE_CATEGORY_ENTRIES)
