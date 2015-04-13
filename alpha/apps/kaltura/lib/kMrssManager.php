d1c479971d707bd5ab2f8f4903136388604a4c8c
erankor <eran.kornblau@kaltura.com>
2015-04-13 12:10:48 +0300

    added more parenthesis

--- alpha/apps/kaltura/lib/kMrssManager.php
+++ alpha/apps/kaltura/lib/kMrssManager.php
@@ -298,7 +298,7 @@ class kMrssManager
 		// get the file size
 		$syncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
 		list($fileSync, $local) = kFileSyncUtils::getReadyFileSyncForKey($syncKey, false, false);
-		$fileSize = ($fileSync && $fileSync->getFileSize() > 0) ? $fileSync->getFileSize() : $flavorAsset->getSize() * 1024;
+		$fileSize = ($fileSync && $fileSync->getFileSize() > 0) ? $fileSync->getFileSize() : ($flavorAsset->getSize() * 1024);
 		
 		$mediaParams = array(
 			'format' => $flavorAsset->getContainerFormat(),
