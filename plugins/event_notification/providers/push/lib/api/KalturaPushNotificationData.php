dc78fcf89ab3fd71e699131ac72ef917d31f100e
Shahar Danos <shahar.danos@kaltura.com>
2015-04-13 15:57:02 +0300

    Yet another new file made for the push plugin, was missed in my last commit

---
+++ plugins/event_notification/providers/push/lib/api/KalturaPushNotificationData.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package plugins.pushNotification
+ * @subpackage api.objects
+ */
+class KalturaPushNotificationData extends KalturaObject 
+{
+	/**
+	 * @var string
+	 * @readonly
+	 */
+	public $key;
+	
+	/**
+	 * @var string
+	 * @readonly
+	 */
+	public $url;
+}
