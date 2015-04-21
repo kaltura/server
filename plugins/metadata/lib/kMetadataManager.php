f395a142b15474b9c1adf287582cb97bfa0aa427
roman-kreichman <roman.kreichman@gmail.com>
2015-04-21 19:27:10 +0300

    call setAdditionalProfileFieldData for non searchable fields

--- plugins/metadata/lib/kMetadataManager.php
+++ plugins/metadata/lib/kMetadataManager.php
@@ -220,7 +220,9 @@ class kMetadataManager
 			$profileField->setStatus(MetadataProfileField::STATUS_NONE_SEARCHABLE);
 			$profileField->setMetadataProfileVersion($metadataProfile->getVersion());
 			$profileField->save();
-			
+
+			self::setAdditionalProfileFieldData($metadataProfile, $profileField, $xPathData);
+
 			unset($xPaths[$xPath]);
 		}
 		
@@ -239,7 +241,10 @@ class kMetadataManager
 			if(isset($xPathData['label']))
 				$profileField->setLabel($xPathData['label']);
 			if(isset($xPathData['type']))
+			{
 				$profileField->setType($xPathData['type']);
+				self::setAdditionalProfileFieldData($metadataProfile, $profileField, $xPathData);
+			}
 
 			$profileField->save();
 		}
