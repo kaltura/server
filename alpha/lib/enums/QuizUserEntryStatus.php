40f457bb5980f7ba040a036a9a7aab9923c56c7c
Noam Arad <noam.arad@kaltura.com>
2015-05-07 07:30:01 +0300

    Add score and submitted status to QuizUserEntry PLAT_2857_1 #time 0.5 days

---
+++ alpha/lib/enums/QuizUserEntryStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @package Core
+ * @subpackage model.enum
+ */
+interface QuizUserEntryStatus extends UserEntryStatus {
+
+	const SUBMITTED = 3;
+}
--- alpha/lib/model/QuizUserEntry.php
+++ alpha/lib/model/QuizUserEntry.php
@@ -6,4 +6,12 @@
  */
 class QuizUserEntry extends UserEntry{
 
+	/**
+	 * @var int
+	 */
+	protected $score;
+
+	public function setScore($v){ $this->putInCustomData("score", $v);}
+	public function getScore($v){ return $this->getFromCustomData("score");}
+
 }
