9e8f227a1f5f1ecfddc4abe373658f512a28e9e6
yossipapi <yossi.papiashvili@kaltura.com>
2015-06-03 11:30:56 +0300

    Update kaltura ce tables to latest chganges

--- deployment/base/sql/01.kaltura_ce_tables.sql
+++ deployment/base/sql/01.kaltura_ce_tables.sql
@@ -2402,20 +2402,19 @@ CREATE TABLE response_profile
 
 CREATE TABLE `edge_server`
 (
-        `id` INTEGER  NOT NULL AUTO_INCREMENT,
-        `created_at` DATETIME,
-        `updated_at` DATETIME,
-        `partner_id` INTEGER,
-        `name` VARCHAR(31),
-        `system_name` VARCHAR(128),
-        `desciption` VARCHAR(127),
-        `status` INTEGER,
-        `tags` TEXT,
-        `host_name` VARCHAR(127),
-        `parent_id` INTEGER default 0,
-        `custom_data` TEXT,
-        PRIMARY KEY (`id`),
-        KEY `partner_id_status`(`partner_id`, `status`),
-        KEY `partner_id_created_at`(`partner_id`, `created_at`),
-        KEY `status_created_at`(`status`, `created_at`)
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	`partner_id` INTEGER,
+	`name` VARCHAR(31),
+	`system_name` VARCHAR(128),
+	`desciption` VARCHAR(127),
+	`status` INTEGER,
+	`tags` TEXT,
+	`host_name` VARCHAR(127),
+	'playback_host_name' VARCHAR(127),
+	`parent_id` INTEGER default 0,
+	`custom_data` TEXT,
+	PRIMARY KEY (`id`),
+	KEY partner_id_status('partner_id', 'status', 'system_name')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
