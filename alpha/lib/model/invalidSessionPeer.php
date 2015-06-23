f0b9017bd149d67b77ecac8c794f8aae428783b8
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-06-23 13:16:34 +0300

    Set invalidation expiry

--- alpha/lib/model/invalidSessionPeer.php
+++ alpha/lib/model/invalidSessionPeer.php
@@ -47,7 +47,7 @@ class invalidSessionPeer extends BaseinvalidSessionPeer {
 		return $result;
 	}
 	
-	public static function invalidateByKey($key, $type, $validUntil = null, PropelPDO $con = null) {
+	public static function invalidateByKey($key, $type, $validUntil, PropelPDO $con = null) {
 		$criteria = new Criteria();
 		$criteria->add(invalidSessionPeer::KS, $key);
 		$criteria->add(invalidSessionPeer::TYPE, $type);
