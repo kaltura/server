51ccb04555180813437db89225723cfadc4752e0
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-06-23 13:15:37 +0300

    Set invalidation expiry

--- api_v3/services/AppTokenService.php
+++ api_v3/services/AppTokenService.php
@@ -88,7 +88,7 @@ class AppTokenService extends KalturaBaseService
 		if(!$dbAppToken)
 			throw new KalturaAPIException(KalturaErrors::APP_TOKEN_ID_NOT_FOUND, $id);
 		
-		invalidSessionPeer::invalidateByKey($id, invalidSession::INVALID_SESSION_TYPE_SESSION_ID);
+		invalidSessionPeer::invalidateByKey($id, invalidSession::INVALID_SESSION_TYPE_SESSION_ID, $dbAppToken->getExpiry());
 		$dbAppToken->setStatus(AppTokenStatus::DELETED);
 		$dbAppToken->save();
 	}
