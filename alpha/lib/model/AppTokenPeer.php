1677f39e6c587f80ad35cdd94f04f1fae48ed240
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-07-01 15:03:18 +0300

    Added default criteria to prevent deleted app-tokens listing PLAT-3375 #time 30m

--- alpha/lib/model/AppTokenPeer.php
+++ alpha/lib/model/AppTokenPeer.php
@@ -15,6 +15,15 @@
  */
 class AppTokenPeer extends BaseAppTokenPeer {
 
+	public static function setDefaultCriteriaFilter ()
+	{
+		if(is_null(self::$s_criteria_filter))
+			self::$s_criteria_filter = new criteriaFilter();
+		
+		$c = new Criteria(); 
+		$c->add(self::STATUS, AppTokenStatus::DELETED, Criteria::NOT_EQUAL);
+		self::$s_criteria_filter->setFilter($c);
+	}
 	
 	/**
 	 * Retrieve a single object by pkey with no filter
