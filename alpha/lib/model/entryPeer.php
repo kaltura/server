84d46df10f70e5a2a3f4cd25ed945aca17155615
shpiNi <liron.shpinerman@Kaltura.com>
2015-07-06 13:45:12 +0300

    using no-filter func with flag

--- alpha/lib/model/entryPeer.php
+++ alpha/lib/model/entryPeer.php
@@ -280,10 +280,11 @@ class entryPeer extends BaseentryPeer
 		return $res;
 	}
 
-	public static function retrieveByPKNoFilter ($pk, $con = null)
+	public static function retrieveByPKNoFilter ($pk, $con = null, $disableEntitlementFiltering = false)
 	{
 		KalturaCriterion::disableTags(array(KalturaCriterion::TAG_ENTITLEMENT_ENTRY, KalturaCriterion::TAG_WIDGET_SESSION));
-		self::$filerResults = true;
+		if (!$disableEntitlementFiltering)
+			self::$filerResults = true;
 		self::setUseCriteriaFilter ( false );
 		$res = parent::retrieveByPK( $pk , $con );
 		self::setUseCriteriaFilter ( true );
