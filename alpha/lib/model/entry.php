9271f10733a2bca55643c5e5dc8c22e25affec77
Noam Arad <noam.arad@kaltura.com>
2015-07-14 10:31:51 +0300

    Move copying of reference-id to happen only for externalMedia entries PLAT-3314 #time 10 minutes

--- alpha/lib/model/entry.php
+++ alpha/lib/model/entry.php
@@ -3390,6 +3390,5 @@ class entry extends Baseentry implements ISyncableFile, IIndexable, IOwnable
 		parent::copyInto($copyObj,$deepCopy);
 		$copyObj->setEntitledPusersEdit($this->getEntitledPusersEdit());
 		$copyObj->setEntitledPusersPublish($this->getEntitledPusersPublish());
-		$copyObj->setReferenceID($this->getReferenceID());
 	}
 }
