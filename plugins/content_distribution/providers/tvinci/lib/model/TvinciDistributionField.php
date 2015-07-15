9ed61c5820cde03a919ce4897f4e620a276b25e5
EladCohenKaltura <elad.cohen@kaltura.com>
2015-07-15 09:58:04 +0300

    PLAT-3392 support update on metada change PLAT-3392 Distribution update is enabled for: METADATA changes CUSTOM_DATA changes (includes referenceID ) NAME DESCRIPTION

--- plugins/content_distribution/providers/tvinci/lib/model/TvinciDistributionField.php
+++ plugins/content_distribution/providers/tvinci/lib/model/TvinciDistributionField.php
@@ -5,4 +5,10 @@
  */ 
 interface TvinciDistributionField extends BaseEnum
 {
+    // ENTRY related fields
+    const CUSTOM    = 'CUSTOM_DATA';
+
+    // METADATA related fields    
+    const META      = '*';
+
 }
