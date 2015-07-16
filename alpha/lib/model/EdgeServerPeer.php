c202027de36882aac48fcd8d1cd3ae1dfc378c22
yossipapi <yossi.papiashvili@kaltura.com>
2015-07-16 13:33:30 +0300

    Change function name + add status to criteria

--- alpha/lib/model/EdgeServerPeer.php
+++ alpha/lib/model/EdgeServerPeer.php
@@ -36,7 +36,7 @@ class EdgeServerPeer extends BaseEdgeServerPeer {
 		return EdgeServerPeer::doSelectOne($c);
 	}
 	
-	public static function retrieveArrayOrderdEdgeServerByPKs($pks, PropelPDO $con = null)
+	public static function retrieveOrderedEdgeServersArrayByPKs($pks, PropelPDO $con = null)
 	{
 		if (empty($pks)) {
 			$objs = array();
@@ -44,6 +44,7 @@ class EdgeServerPeer extends BaseEdgeServerPeer {
 		else {
 			$criteria = new Criteria(EdgeServerPeer::DATABASE_NAME);
 			$criteria->add(EdgeServerPeer::ID, $pks, Criteria::IN);
+			$criteria->add(EdgeServerPeer::STATUS, EdgeServerStatus::ACTIVE);
 			$orderBy = "FIELD (" . self::ID . "," . implode(",", $pks) . ")";  // first take the pattner_id and then the rest
 			$criteria->addAscendingOrderByColumn($orderBy);
 			$objs = EdgeServerPeer::doSelect($criteria, $con);
