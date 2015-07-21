b7afa1611beb97910df5ffd8ecd003df638421cd
erankor <eran.kornblau@kaltura.com>
2015-07-21 13:03:16 +0300

    remove amp from the list of url allowed chars
    in preparation for handling all serveFlavor's with fileName in nginx-vod

--- infra/general/kString.class.php
+++ infra/general/kString.class.php
@@ -442,7 +442,7 @@ class kString
 
 	public static function keepOnlyValidUrlChars($url)
 	{
-	    return preg_replace('/[^A-Za-z0-9\-._~!$&()*+,;=:@]/', '_', $url);
+	    return preg_replace('/[^A-Za-z0-9\-._~!$()*+,;=:@]/', '_', $url);
 	}
 	
 }
