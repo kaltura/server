9ca28410331faaef3a557e68f49cf63817704bef
yuly-roberman <yuli.roberman@kaltura.com>
2015-08-05 13:34:19 +0300

    update monthly partner usage script

--- alpha/crond/kaltura/monthly_quota_storage_update.sh
+++ alpha/crond/kaltura/monthly_quota_storage_update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+LOGDIR=/web/logs/investigate/`/bin/date +%Y/%m/%d`/partnerusage
 WHEN=$(date -d "yesterday" +%Y-%m-%d)
 
-php /opt/kaltura/app/scripts/batch/validatePartnerUsage.php null 100 >> /var/log/`hostname`-BatchPartnerUsage_monthlyUpgradeProcess.log.${WHEN} 2>&1
-tail /var/log/`hostname`-BatchPartnerUsage_monthlyUpgradeProcess.log.${WHEN} | mail -s "batchPartnerUsage on `hostname`" it.prod@kaltura.com,records@kaltura.com
+mkdir -p $LOGDIR
+php /opt/kaltura/app/alpha/scripts/batch/validatePartnerUsage.php  null 100 >> $LOGDIR/`hostname`-BatchPartnerUsage_monthlyUpgradeProcess.log.${WHEN} 2>&1
+tail $LOGDIR/`hostname`-BatchPartnerUsage_monthlyUpgradeProcess.log.${WHEN} | mail -s "batchPartnerMonthlyUsage on `hostname`" production-it@kaltura.com
