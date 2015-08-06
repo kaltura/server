cd701d0f96d70e8da8530f2a9d335fcf89db2a71
EladCohenKaltura <elad.cohen@kaltura.com>
2015-08-07 01:02:24 +0300

    PLAT-3078 PLAT-3078 Change to hidden iframe in order to reduce JQuery useage and get files in form

---
+++ api_v3/web/testme/testme.result.php
@@ -0,0 +1,9 @@
+<!-- This file is used to support an iframe inside index.php that will get the testme result-->
+<html>
+    <head>
+        <script type="text/javascript" src="js/ace-builds/src/ace.js"></script>
+    </head>
+    <body style="height: 98%; width: 98%">
+        <div id="editorDiv" style="height: 100%; width: 100%"></div>
+    </body>
+</html>
