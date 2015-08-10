338b2bea193b955e5fb2161a1fd5af40482e2211
yuly-roberman <yuli.roberman@kaltura.com>
2015-08-10 09:10:39 +0300

    add partner id to usage query

--- alpha/apps/kaltura/lib/myPartnerUtils.class.php
+++ alpha/apps/kaltura/lib/myPartnerUtils.class.php
@@ -939,7 +939,7 @@ class myPartnerUtils
 		$reportFilter = new reportsInputFilter();
 		$reportFilter->from_day = str_replace('-','',$fromDate);
 		$reportFilter->to_day = str_replace('-','',$report_date);		
-		list($header, $data) = myReportsMgr::getTotal($partner->getId(), myReportsMgr::REPORT_TYPE_PARTNER_USAGE, $reportFilter);
+		list($header, $data) = myReportsMgr::getTotal($partner->getId(), myReportsMgr::REPORT_TYPE_PARTNER_USAGE, $reportFilter, $partner->getId());
 
 		$bandwidth_consumption = array_search('bandwidth_consumption', $header);
 		$deleted_storage = array_search('deleted_storage', $header);
