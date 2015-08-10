27c85581fdb256dbcfae61952b3b37d97453bc76
Jess Portnoy <jess.portnoy@kaltura.com>
2015-08-10 13:20:51 +0100

    following https://github.com/kaltura/server/pull/2971.

--- generator/sources/java/testAPI/test.sh
+++ generator/sources/java/testAPI/test.sh
@@ -9,13 +9,12 @@ if [ ! -x "`which javac 2>/dev/null`" ];then
     echo "Need to install javac."
     exit 2
 fi
+if [ ! -x "`which mvn 2>/dev/null`" ];then
+    echo "Need to install mvn."
+    exit 3
+fi
 
-mkdir -p bin
-cd bin
-cp ../src/DemoImage.jpg ../src/DemoVideo.flv ../src/test.properties .
-javac -d . -sourcepath ../src -cp ../lib/commons-codec-1.4.jar:../lib/commons-httpclient-3.1.jar:../lib/commons-logging-1.1.1.jar:../lib/junit-4.7.jar:../lib/log4j-1.2.15.jar ../src/com/kaltura/client/test/KalturaTestSuite.java
-jar cvf  kalturaClient.jar .
-cd ../
-echo "log4j.logger.com.kaltura=DEBUG" >> src/log4j/log4j.properties
-java -Dlog4j.configuration=file:src/log4j/log4j.properties -cp lib/commons-codec-1.4.jar:lib/commons-httpclient-3.1.jar:lib/commons-logging-1.1.1.jar:lib/junit-4.7.jar:lib/log4j-1.2.15.jar:bin/kalturaClient.jar org.junit.runner.JUnitCore   com.kaltura.client.test.KalturaTestSuite
-
+# compile
+mvn -Dmaven.test.skip=true package
+# test
+mvn package
