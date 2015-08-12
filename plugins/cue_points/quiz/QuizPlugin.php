3311ec84164b3f14587ca520161031b541c28677
Noam Arad <noam.arad@kaltura.com>
2015-08-12 18:58:34 +0300

    dont return any dynamic attributes if there is no quiz defined so there is no need to index that dynamic attribute. When applying the condition of isQuiz=0 it still is true if the attribute is empty. When the quiz is added this will also be added.

--- plugins/cue_points/quiz/QuizPlugin.php
+++ plugins/cue_points/quiz/QuizPlugin.php
@@ -271,12 +271,11 @@ class QuizPlugin extends KalturaPlugin implements IKalturaCuePoint, IKalturaServ
 	public static function getDynamicAttributes(IIndexable $object)
 	{
 		if ( $object instanceof entry ) {
-			$isQuiz = 0;
 			if ( !is_null($object->getFromCustomData(self::QUIZ_DATA)) )
-				$isQuiz = 1;
-
-			$dynamicAttribute = array(self::getDynamicAttributeName() => $isQuiz);
-			return $dynamicAttribute;
+			{
+				$dynamicAttribute = array(self::getDynamicAttributeName() => 1);
+				return $dynamicAttribute;
+			}
 		}
 
 		return array();
