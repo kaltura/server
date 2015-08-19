556386df1c18b02d82ec53a241b76d7f63e7ba94
Noam Arad <noam.arad@kaltura.com>
2015-08-19 16:39:03 +0300

    Add the ability to decide weather specific users' entries should go through moderation. The users list can be defined by adding a filter to the partner configuration to the field autoModerateEntryFilter of type KalturaBaseEntryFilter with the field userIdIn set to the user names list comma seperated. SUP-4934 #time 3 hours

--- alpha/apps/kaltura/lib/model/objectfilters/entryFilter.class.php
+++ alpha/apps/kaltura/lib/model/objectfilters/entryFilter.class.php
@@ -735,6 +735,11 @@ class entryFilter extends baseObjectFilter
 			return false;
 		}
 		
+		if ($this->get('_in_user_id') && !in_array($entry->getUserScreenName(), explode(',', $this->get('_in_user_id'))) )
+		{
+			return false;
+		}
+		
 		return true;
 	}
 	
