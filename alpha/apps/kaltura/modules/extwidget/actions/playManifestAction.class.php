894b3403e249b37faf53819bc7968b5ddf1b4bf4
Shahar Danos <shahar.danos@kaltura.com>
2015-08-25 15:45:26 +0300

    Changed the expected parameter (once again) due to another request

--- alpha/apps/kaltura/modules/extwidget/actions/playManifestAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/playManifestAction.class.php
@@ -799,7 +799,7 @@ class playManifestAction extends kalturaAction
 		$partner = PartnerPeer::retrieveByPK($partnerId);
 		$partnerTimeAligned = $partner->getTimeAlignedRenditions();
 		
-		if ( ($partnerTimeAligned) && ((bool) $this->getRequestParameter("flashPlayer")) ) {
+		if ( ($partnerTimeAligned) && ( $this->getRequestParameter("playerType") === 'flash' ) ) {
 			// check entry's flavors
 			$entryFlavorParams = assetParamsPeer::retrieveByPKs(explode(',', $this->entry->getFlavorParamsIds()));
 			$hasTranscode = false;
