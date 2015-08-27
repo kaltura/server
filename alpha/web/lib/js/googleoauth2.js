65863b036bb1caec4a56f610d695deb5f8d2fddf
Galmoss <gal.mossinson@kaltura.com>
2015-08-27 16:10:36 +0300

    Fix indentation

--- alpha/web/lib/js/googleoauth2.js
+++ alpha/web/lib/js/googleoauth2.js
@@ -43,14 +43,14 @@
 		}
 		
 		function getQueryVariable(variable) {
-               var query = window.location.search.substring(1);
-               var vars = query.split("&");
-               for (var i=0;i<vars.length;i++) {
-                       var pair = vars[i].split("=");
-                       if(pair[0] == variable){return pair[1];}
-               }
-               return(false);
-        }
+	               var query = window.location.search.substring(1);
+	               var vars = query.split("&");
+	               for (var i=0;i<vars.length;i++) {
+	                       var pair = vars[i].split("=");
+	                       if(pair[0] == variable){return pair[1];}
+               		}
+               		return(false);
+        	}
 
 
 		function onLoginApiSuccess(data) {
