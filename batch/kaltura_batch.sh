7ac45f7fa0368c4eb4ece81932a3c08988a7cadf
David Bezemer <themmai@gmail.com>
2015-09-10 17:59:02 +0200

    Update kaltura_batch.sh
    Refactor batch init script to correctly cover start/restart/stop commands with correct status detection and pid/lockfile handling.
    Updated with advice from @jessp01

--- batch/kaltura_batch.sh
+++ batch/kaltura_batch.sh
@@ -26,43 +26,48 @@ BATCHDIR=$APP_DIR/batch
 BATCHEXE=KGenericBatchMgr.class.php
 CONFIG_FILE=$APP_DIR/configurations/batch
 LOCKFILE="$LOG_DIR/batch/batch.pid"
+GREEN='\e[1;32m'
+RED='\e[1;31m'
+NORMAL='\e[0m'
 
-if [ $# != 1 ]; then
+if [ $# -ne 1 ]; then
     echo "Usage: $0 [start|stop|force-stop|restart|status]"
-    exit 0  
+    exit 1  
 fi
 
 echo_status() {
+    echo -n "$1"
     if [ $2 -eq 0 ]; then
-        echo -n "$1"
-        echo -e "\t[\e[1;32m    OK    \e[0m]"
+        echo -e "\t[$GREEN    OK    $NORMAL]"
     else
-        echo -n "$1"
-        echo -e "\t[\e[1;31m  FAILED  \e[0m]"
+        echo -e "\t[$RED  FAILED  $NORMAL]"
     fi
 }
 
+get_pids() {
+    KP=`ps axf | awk '!/\\_ / {b=0} /php [K]GenericBatchMgr.class.php/ {b=1} b{print $1}'|xargs`
+    KP_PARENT=`cat $LOCKFILE 2>/dev/null`
+}
 
 start() {
-    if [ -f $BASE_DIR/maintenance ]; then
+    if [ -r $BASE_DIR/maintenance ]; then
         echo "Server is on maintenance mode - batchMgr will not start!"
         return 1
     fi
     
-    KP=`ps axf | awk '!/\\_ / {b=0} /php [K]GenericBatchMgr.class.php/ {b=1} b{print $1}'|xargs`
-    if [ -f "$LOCKFILE" ]; then
-        KP_PARENT=`cat $LOCKFILE 2>/dev/null`
-        kill -0 $KP_PARENT
+    get_pids
+    if [ -r "$LOCKFILE" ]; then
+        kill -0 $KP_PARENT > /dev/null
         if [ $? -eq 0 ]; then
             echo_status "Service Batch Manager already running [$KP_PARENT]" 0
-        return 0
-    else
-        echo "Service Batch Manager isn't running but stale lock file exists"
-        echo "Removing stale lock file $LOCKFILE"
-        rm -f $LOCKFILE
-        start_scheduler
-        return 1
-    fi
+            return 0
+        else
+            echo "Service Batch Manager isn't running but stale lock file exists"
+            echo "Removing stale lock file $LOCKFILE"
+            rm -f $LOCKFILE
+            start_scheduler
+            return 1
+        fi
     elif [ -n "$KP" ]; then
         echo "Batch Manager is running as $KP without $LOCKFILE"
         start_scheduler
@@ -82,7 +87,7 @@ start_scheduler() {
     if [ "$?" -eq 0 ]; then
         echo ". "
         sleep 1
-        KP_PARENT=`cat $LOCKFILE 2>/dev/null`
+        get_pids
         echo_status "Batch Manager started with PID $KP_PARENT" 0
         return 0
     else
@@ -93,28 +98,28 @@ start_scheduler() {
 }
 
 show_status() {
-    KP=`ps axf | awk '!/\\_ / {b=0} /php [K]GenericBatchMgr.class.php/ {b=1} b{print $1}'|xargs`
-    KP_PARENT=`cat $LOCKFILE 2>/dev/null`
+    get_pids
     if [ -n "$KP" ]; then
         echo_status "Batch Manager running with PID $KP_PARENT" 0
         return 0
     else
         echo_status "Service Batch Manager isn't running" 1
-        return 1 
+        return 1
     fi
 }
 
 stop() {
-    KP_PARENT=`cat $LOCKFILE 2>/dev/null`
-    KP=`ps axf | awk '!/\\_ / {b=0} /php [K]GenericBatchMgr.class.php/ {b=1} b{print $1}'|xargs`
+    echo "Stopping Batch Manager.... "
+    get_pids
+    SIGNAL=$1
     if [ -n "$KP" ]; then
-        if [ -f $BASE_DIR/keepAlive ]; then
+        if [ -r $BASE_DIR/keepAlive ]; then
             echo "Server is on Keep Alive mode - workers won't be killed!"
             echo_status "Killing Batch Manager with PID $KP_PARENT" 0
-            kill $1 $KP_PARENT
+            kill -s $SIGNAL $KP_PARENT > /dev/null
         else
             echo_status "Killing Batch Manager with PID $KP_PARENT and related workers" 0
-            kill $1 $KP
+            kill -s $SIGNAL $KP > /dev/null
         fi
         rm $LOCKFILE
         RC=$?
@@ -130,21 +135,21 @@ case "$1" in
         start
         ;;
     stop)
-        stop
+        stop 15
         ;;
     force-stop)
-        stop -9
+        stop 9
         ;;
     status)
         show_status
         ;;
     restart)
-        stop
+        stop 15
         start
         ;;
     *)
         echo "Usage: [start|stop|force-stop|restart|status]"
-        exit 0
+        exit 1
         ;;
 esac
 exit $?
