6b98fe342e19a2edc14760cf26821b3281a1da49
shpiNi <liron.shpinerman@Kaltura.com>
2015-10-14 11:35:09 +0300

    minor adjustments

--- plugins/like/lib/api/filters/KalturaLikeFilter.php
+++ plugins/like/lib/api/filters/KalturaLikeFilter.php
@@ -12,33 +12,30 @@ class KalturaLikeFilter extends KalturaLikeBaseFilter
 	{
 		return new LikeFilter();
 	} 
-
+	
 	/* (non-PHPdoc)
 	 * @see KalturaRelatedFilter::getListResponse()
 	 */
 	public function getListResponse(KalturaFilterPager $pager, KalturaDetachedResponseProfile $responseProfile = null)
 	{
 		myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_PROPEL2;
-
-                $c = new Criteria();
+	
+		$c = new Criteria();
 		$c->add(kvotePeer::PARTNER_ID, kCurrentContext::$ks_partner_id);
-
-                if($this->entryIdEqual)
-                        $c->add(kvotePeer::ENTRY_ID, $this->entryIdEqual);
-                if($this->userIdEqual)
-                        $c->add(kvotePeer::PUSER_ID, $this->userIdEqual);
-
-                $pager->attachToCriteria($c);
-
-                $list = kvotePeer::doSelect($c);
-
-
-                $response = new KalturaLikeListResponse();
-                $response->objects = KalturaLikeArray::fromDbArray($list, $responseProfile);
-                $response->totalCount = count($list);
-                return $response;
-
-
+	
+		if($this->entryIdEqual)
+				$c->add(kvotePeer::ENTRY_ID, $this->entryIdEqual);
+		if($this->userIdEqual)
+				$c->add(kvotePeer::PUSER_ID, $this->userIdEqual);
+	
+		$pager->attachToCriteria($c);
+	
+		$list = kvotePeer::doSelect($c);
+	
+		$response = new KalturaLikeListResponse();
+		$response->objects = KalturaLikeArray::fromDbArray($list, $responseProfile);
+		$response->totalCount = count($list);
+		return $response;
 	}
-
+	
 }
