cef0234032525dc0a21a3bbe6f0fa8f1374b1e3c
shpiNi <liron.shpinerman@Kaltura.com>
2015-10-18 16:26:14 +0300

    extending KalturaRelatedFilter

--- plugins/like/lib/api/filters/base/KalturaLikeBaseFilter.php
+++ plugins/like/lib/api/filters/base/KalturaLikeBaseFilter.php
@@ -4,7 +4,7 @@
  * @subpackage api.filters.base
  * @abstract
  */
-abstract class KalturaLikeBaseFilter extends KalturaFilter
+abstract class KalturaLikeBaseFilter extends KalturaRelatedFilter
 {
 	static private $map_between_objects = array
 	(
