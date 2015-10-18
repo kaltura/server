5482d0a91b12d5f2c39b0b35529a4f4d5ad1cec1
shpiNi <liron.shpinerman@Kaltura.com>
2015-10-18 15:40:35 +0300

    permission adjustments

--- deployment/updates/scripts/add_permissions/2015_10_25_add_like_list_permission.php
+++ deployment/updates/scripts/add_permissions/2015_10_25_add_like_list_permission.php
@@ -4,10 +4,28 @@
  * 
  * Add like->list permission-item
  */
- 
+
+const BASE_USER_ROLE_SYSTEM_NAME = "Basic User Session Role";
+const LIKE_LIST_USER_PERMISSION_TO_ADD = "LIKE_LIST_USER";
+
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/partner.0.ini';
 passthru("php $script $config");
 
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/service.like.like.ini';
 passthru("php $script $config");
+
+require_once(__DIR__ . "/../../../../" . "alpha/scripts/bootstrap.php");
+
+$c = new Criteria();
+$c->add(UserRolePeer::SYSTEM_NAME, BASE_USER_ROLE_SYSTEM_NAME);
+$c->add(UserRolePeer::PARTNER_ID, PartnerPeer::GLOBAL_PARTNER);
+
+$baseUserRole = UserRolePeer::doSelectOne($c);
+$permissionNameStr = $baseUserRole->getPermissionNames();
+$permissionNameArr = explode(",", $permissionNameStr);
+$permissionNameArr[] = LIKE_LIST_USER_PERMISSION_TO_ADD;
+$permissionNameStr = implode(",", $permissionNameArr);
+$baseUserRole->setPermissionNames($permissionNameStr);
+$baseUserRole->save();
+ 
