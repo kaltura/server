5a1d208bc26a559eec1f8d7ecb8e79bacee4eabb
shpiNi <liron.shpinerman@Kaltura.com>
2015-10-25 11:49:15 +0200

    adding creation date

--- plugins/like/lib/api/KalturaLike.php
+++ plugins/like/lib/api/KalturaLike.php
@@ -18,11 +18,18 @@ class KalturaLike extends KalturaObject implements IRelatedFilterable
 	 * @filter eq
 	 */
 	public $userId;
+
+	/**
+	 * The date of the like's creation
+	 * @var string
+	 */
+	public $createdAt;
 	
 	private static $map_between_objects = array
 	(
 		"entryId",
-		"userId" => "puserId"		
+		"userId" => "puserId",
+		"createdAt"
 	);
 	
 	public function getMapBetweenObjects ( )
