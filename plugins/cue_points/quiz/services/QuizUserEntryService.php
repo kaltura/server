49f65032755289a9c636ca1336fcc4cf331efd0d
root <root@localhost.localdomain>
2015-11-09 09:13:47 -0500

    Show score upon sunbmit only if flag is set PLAT-4050#time 2d

--- plugins/cue_points/quiz/services/QuizUserEntryService.php
+++ plugins/cue_points/quiz/services/QuizUserEntryService.php
@@ -24,20 +24,32 @@ class QuizUserEntryService extends KalturaBaseService{
 			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_ID, $id);
 
 		if ($dbUserEntry->getType() != QuizPlugin::getCoreValue('UserEntryType',QuizUserEntryType::QUIZ))
-		{
-			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_TYPE, $dbUserEntry->getType());
-		}
+			throw new KalturaAPIException(KalturaQuizErrors::PROVIDED_ENTRY_IS_NOT_A_QUIZ, $id);
+
 		/**
 		 * @var QuizUserEntry $dbUserEntry
 		 */
 		$score = $dbUserEntry->calculateScore();
-		$dbUserEntry->setScore($score);
-//		$dbUserEntry->setStatus(QuizUserEntryStatus::QUIZ_SUBMITTED);
+    $dbUserEntry->setScore($score);
+    KalturaLog::debug("Quiz score is [" .$score."]");
+    
 		$dbUserEntry->setStatus(QuizPlugin::getCoreValue('UserEntryStatus', QuizUserEntryStatus::QUIZ_SUBMITTED));
-		$dbUserEntry->save();
-
-		$userEntry = new KalturaQuizUserEntry();
+ 		$userEntry = new KalturaQuizUserEntry();
 		$userEntry->fromObject($dbUserEntry, $this->getResponseProfile());
+    $entryId = $dbUserEntry->getEntryId();
+		$entry = entryPeer::retrieveByPK($entryId);
+		if(!$entry)
+			throw new KalturaAPIException(KalturaErrors::INVALID_OBJECT_ID, $entryId);
+
+		$kQuiz = QuizPlugin::getQuizData($entry);
+		if (!$kQuiz)
+			throw new KalturaAPIException(KalturaQuizErrors::PROVIDED_ENTRY_IS_NOT_A_QUIZ, $entryId);
+
+		if (!$kQuiz->getShowGradeAfterSubmission() || !$this->getKuser()->getIsAdmin())
+				$userEntry->score = null;
+	  
+    $dbUserEntry->save();
+
 		return $userEntry;
 	}
 }
