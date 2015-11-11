c91ee872d43c9a9a2f6e5748ae5cf487d190d50b
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-11-11 16:56:05 +0200

    Follow Kaltura plugins standrads

---
+++ plugins/capture_space/lib/api/KalturaCaptureSpaceUpdateResponseInfoHash.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package plugins.captureSpace
+ * @subpackage api.objects
+ */
+class KalturaCaptureSpaceUpdateResponseInfoHash extends KalturaObject {
+    /**
+     * @var KalturaCaptureSpaceHashAlgorithm
+     */
+    public $algorithm;
+
+    /**
+     * @var string
+     */
+    public $value;
+}
