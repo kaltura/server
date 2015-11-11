72f832d61bd001dfdcd977883026148afd2b6e8c
yossipapi <yossi.papiashvili@kaltura.com>
2015-11-11 16:29:00 +0200

    fix indent

--- generator/sources/csharp/KalturaClientTester/KalturaClientTester.cs
+++ generator/sources/csharp/KalturaClientTester/KalturaClientTester.cs
@@ -36,8 +36,8 @@ namespace Kaltura
     {
 		private const int PARTNER_ID = @YOUR_PARTNER_ID@; //enter your partner id
 		private const string ADMIN_SECRET = "@YOUR_ADMIN_SECRET@"; //enter your admin secret
-		private const string SERVICE_URL = "http://www.kaltura.com";
-        private const string USER_ID = "@SERVICE_URL@";
+		private const string SERVICE_URL = "@SERVICE_URL@";
+		private const string USER_ID = "testUser";
         
         private static string uniqueTag;
 
