2e316adb09a7c3573cb3e4aa08e1902dc4083bcd
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2015-11-18 14:23:49 +0200

    Fixed server path

--- plugins/capture_space/services/CaptureSpaceService.php
+++ plugins/capture_space/services/CaptureSpaceService.php
@@ -23,7 +23,7 @@ class CaptureSpaceService extends KalturaBaseService
 			throw new KalturaAPIException(CaptureSpaceErrors::NO_UPDATE_IS_AVAILABLE, $version, $os);
 		}
 			
-		$path = "/api_v3/service/captureSpace_captureSpace/action/serve/serveUpdateAction/os/$os/version/$version";
+		$path = "/api_v3/service/captureSpace_captureSpace/action/serveUpdate/os/$os/version/$version";
 		$downloadUrl = myPartnerUtils::getCdnHost(null) . $path;
 		
 		$info = new KalturaCaptureSpaceUpdateResponseInfo();
