9093dcc1e40dfe07039848ac868792b1fa461ae5
yossipapi <yossi.papiashvili@kaltura.com>
2015-11-29 15:19:20 +0200

    Use correct param name when calling preg_replace

--- alpha/lib/model/DeliveryProfileLiveAppleHttpToMulticast.php
+++ alpha/lib/model/DeliveryProfileLiveAppleHttpToMulticast.php
@@ -12,7 +12,7 @@ class DeliveryProfileLiveAppleHttpToMulticast extends DeliveryProfileLiveAppleHt
 		$deliveryUrl = parent::getDeliveryServerNodeUrl($removeAfterUse);
 		
 		if($deliveryUrl)
-			$baseUrl = preg_replace('/' . EdgeServerNode::EDGE_SERVER_DEFAULT_LIVE_CACHE_APPLICATION_NAME . '/', EdgeServerNode::EDGE_SERVER_DEFAULT_LIVE_UNICAST_TO_MC_APPLICATION_NAME , $deliveryUrl, 1);
+			$deliveryUrl = preg_replace('/' . EdgeServerNode::EDGE_SERVER_DEFAULT_LIVE_CACHE_APPLICATION_NAME . '/', EdgeServerNode::EDGE_SERVER_DEFAULT_LIVE_UNICAST_TO_MC_APPLICATION_NAME , $deliveryUrl, 1);
 		
 		return $deliveryUrl;
 	}
