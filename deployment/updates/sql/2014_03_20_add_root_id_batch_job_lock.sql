fb793e6f5944a57692a963a013666b27a4525360
kobimichaeli <root@builder-be.dev.kaltura.com>
2015-12-09 13:05:37 +0200

    Removed unneeded iris scripts from packaging PLAT-4666

--- deployment/updates/sql/2014_03_20_add_root_id_batch_job_lock.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `batch_job_lock` 
-	ADD `root_job_id` bigint(20) AFTER `batch_version`,
-	ADD INDEX `root_job_id_index` (`root_job_id`);
-ALTER TABLE `batch_job_lock_suspend` 
-	ADD `root_job_id` bigint(20) AFTER `batch_version`,
-	ADD INDEX `root_job_id_index` (`root_job_id`);
