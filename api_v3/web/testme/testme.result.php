7e37b2996b95c50e76116177401c73a8fe2012a7
root <root@localhost.kaltura>
2015-12-21 08:03:50 -0800

    Fix Ace Editor behavior in testme result SUP-6753

--- api_v3/web/testme/testme.result.php
+++ api_v3/web/testme/testme.result.php
@@ -5,5 +5,19 @@
     </head>
     <body style="height: 98%; width: 98%">
         <div id="editorDiv" style="height: 100%; width: 100%"></div>
+        <script>
+            function setAceEditorWithText(text, format){
+                var editor = ace.edit("editorDiv");
+                var session = editor.getSession();
+                editor.$blockScrolling = Infinity;
+                editor.setTheme("ace/theme/chrome");
+                session.setMode("ace/mode/" + format);
+                session.setTabSize(4);
+                session.setUseWrapMode(true);
+                editor.setShowPrintMargin(false);
+                editor.setValue(text,-1);
+                editor.setReadOnly(true);
+            }
+        </script>
     </body>
 </html>
