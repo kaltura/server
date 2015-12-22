6c24bfe9d8a3646a2d4ee2e477808a7f88558ab6
root <root@TaliH.(none)>
2015-12-22 11:17:12 +0200

    Enhance BaseEntry->cloneAction() in a way that not all the entry properties will be cloned Kajam-11.5.0-PLAT-4058 # 5 days

---
+++ api_v3/lib/types/KalturaBaseEntryCloneOptionsArray.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaBaseEntryCloneOptionsArray extends KalturaTypedArray
+{
+    public function __construct()
+    {
+        parent::__construct("KalturaBaseEntryCloneOptionItem");
+    }
+}
