bf8ca2be540103722f1ca4cf2ff98bee298c4957
shpiNi <liron.shpinerman@Kaltura.com>
2016-01-04 14:31:11 +0200

    minor fix

--- alpha/apps/kaltura/lib/myBaseObject.class.php
+++ alpha/apps/kaltura/lib/myBaseObject.class.php
@@ -210,7 +210,7 @@ abstract class myBaseObject implements Iterator
 	 * WARNING - this method's params are different from fillObjectFromRequest due to the structure of the xml 
 	 * The second parameter $prefix_to_add represents the string to append as prefix to each of the elements names.
 	 */
-	public function fillObjectFromXml ( SimpleXMLElement $simple_xml_node , $prefix_to_add , $exclude_params=null , $time_params_to_relative = array())
+	public function fillObjectFromXml ( SimpleXMLElement $simple_xml_node , $prefix_to_add , $exclude_params=null )
 	{
 
 		$set_field_count = 0;
@@ -218,9 +218,6 @@ abstract class myBaseObject implements Iterator
 		// iterate over all the paramters of the request
 		foreach ( $simple_xml_node as $param => $value )
 		{
-			if(in_array($param, $time_params_to_relative))
-				$value = kTime::getRelativeTime($value);
-
 			// ignore empty strings in the filter !
 			if ( $value ==NULL || strlen ($value) == 0 ) continue;
 
