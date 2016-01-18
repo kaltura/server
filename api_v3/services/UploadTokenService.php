31d140acff085e78719bc90e66285289392e7a96
Eran Etam <eran.etam@kaltura.com>
2016-01-18 16:50:34 +0200

    added documentation to the uploadtoken upload action

--- api_v3/services/UploadTokenService.php
+++ api_v3/services/UploadTokenService.php
@@ -66,7 +66,16 @@ class UploadTokenService extends KalturaBaseService
 	}
 	
 	/**
-	 * Upload a file using the upload token id, returns an error on failure (an exception will be thrown when using one of the Kaltura clients) 
+c	 * Upload a file using the upload token id, returns an error on failure (an exception will be thrown when using one of the Kaltura clients)
+	 * Chunks can be uploaded in parallel and they will be appended according to their resumeAt position.
+	 * 
+	 * A parallel upload session should have three stages:
+	 * 1. A single upload with resume=false and finalChunk=false
+	 * 2. Parallel upload requests each with resume=true,finalChunk=false and the expected resumetAt position.
+	 *    If a chunk fails to upload it can be re-uploaded.
+	 * 3. After all of the chunks have been uploaded a final chunk (can be of zero size) should be uploaded 
+	 *    with resume=true, finalChunk=true and the expected resumeAt position. In case an UPLOAD_TOKEN_CANNOT_MATCH_EXPECTED_SIZE exception
+	 *    has been returned (indicating not all of the chunks were appended yet) the final request can be retried.     
 	 * 
 	 * @action upload
 	 * @param string $uploadTokenId
