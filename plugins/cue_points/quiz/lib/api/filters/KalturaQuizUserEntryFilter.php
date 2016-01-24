23a18e0e756cceadf72db1c47152602f62fa928e
MosheMaorKaltura <moshe.maor@kaltura.com>
2016-01-24 11:21:57 -0200

    Add anonymous flag to filter uknown users PLAT-4970#time 3h

--- plugins/cue_points/quiz/lib/api/filters/KalturaQuizUserEntryFilter.php
+++ plugins/cue_points/quiz/lib/api/filters/KalturaQuizUserEntryFilter.php
@@ -5,4 +5,29 @@
  */
 class KalturaQuizUserEntryFilter extends KalturaQuizUserEntryBaseFilter
 {
+    /**
+     * @var KalturaNullableBoolean
+     */
+    public $isAnonymous;
+
+
+	public function getListResponse(KalturaFilterPager $pager, KalturaDetachedResponseProfile $responseProfile = null)
+	{
+		if(!is_null($this->isAnonymous))
+		{
+			if(KalturaNullableBoolean::toBoolean($this->isAnonymous)===false)
+			{
+				if($this->userIdNotIn==null)
+				{
+					$anonKuserIds = "";
+					$anonKusers = kuserPeer::getKuserByPartnerAndUids(kCurrentContext::getCurrentPartnerId(), array('', 0));
+					foreach ($anonKusers as $anonKuser) {
+						$anonKuserIds .= $anonKuser->getKuserId().",";
+					}
+					$this->userIdNotIn = $anonKuserIds;
+				}
+			}
+		}
+		return parent::getListResponse($pager,$responseProfile);
+	}
 }
