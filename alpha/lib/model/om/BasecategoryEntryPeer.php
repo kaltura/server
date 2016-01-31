fb24fca8ac781e137c817ecd6fb18dfd1882150c
yossipapi <yossi.papiashvili@kaltura.com>
2016-01-31 14:59:15 +0200

    Add missing function definition to base peer classes

--- alpha/lib/model/om/BasecategoryEntryPeer.php
+++ alpha/lib/model/om/BasecategoryEntryPeer.php
@@ -866,6 +866,15 @@ abstract class BasecategoryEntryPeer {
 	{
 		return array();
 	}
+	
+	/**
+	 * Return array of custom-data fields that shouldn't be auto-updated.
+	 * @return array
+	 */
+	public static function getAtomicCustomDataFields()
+	{
+		return array();
+	}
 
 	/**
 	 * Method to DELETE all rows from the category_entry table.
