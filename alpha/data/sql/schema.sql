0fcdb1e916629c2cf7d1e2cf22bd6a511ff0106a
EladCohenKaltura <elad.cohen@kaltura.com>
2016-02-08 16:43:36 +0200

    EntryServerNode first commit - table PLAT-5018

--- alpha/data/sql/schema.sql
+++ alpha/data/sql/schema.sql
@@ -2446,5 +2446,26 @@ CREATE TABLE app_token
 	PRIMARY KEY (id)
 )Type=InnoDB;
 
+#-----------------------------------------------------------------------------
+#-- entry_server_node
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS entry_server_node;
+
+
+CREATE TABLE entry_server_node
+(
+	id INTEGER  NOT NULL AUTO_INCREMENT,
+	entry_id VARCHAR(20),
+	server_node_id INTEGER,
+	partner_id INTEGER,
+	created_at DATETIME,
+	updated_at DATETIME,
+	status INTEGER,
+	server_type INTEGER,
+	custom_data TEXT,
+	PRIMARY KEY (id)
+)Type=InnoDB;
+
 # This restores the fkey checks, after having unset them earlier
 SET FOREIGN_KEY_CHECKS = 1;
