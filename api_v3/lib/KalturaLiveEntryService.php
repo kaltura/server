b22a4e55117e0f919ddd34dbbf0e5f735a36693c
asafrobinovich <asaf.robinovich@kaltura.com>
2016-02-17 10:37:30 +0200

    change setVersion(0) to incrementVersion()

--- api_v3/lib/KalturaLiveEntryService.php
+++ api_v3/lib/KalturaLiveEntryService.php
@@ -173,7 +173,7 @@ class KalturaLiveEntryService extends KalturaEntryService
 		$recordedAsset->setStatus(asset::FLAVOR_ASSET_STATUS_QUEUED);
 		$recordedAsset->setFlavorParamsId($flavorParams->getId());
 		$recordedAsset->setFromAssetParams($flavorParams);
-		$recordedAsset->setVersion(0);
+		$recordedAsset->incrementVersion();
 		if ( $dbAsset->hasTag(assetParams::TAG_RECORDING_ANCHOR) )
 		{
 			$recordedAsset->addTags(array(assetParams::TAG_RECORDING_ANCHOR));
