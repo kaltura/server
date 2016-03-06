98b11fdd6f4eaebca7167faf30c320ecb2a6a875
Gonen Radai <gonen.r@gmail.com>
2016-03-06 16:48:00 +0200

    fix comparison

--- generator/sources/csharp/KalturaClientTester/KalturaClientTester.cs
+++ generator/sources/csharp/KalturaClientTester/KalturaClientTester.cs
@@ -1,35 +1,8 @@
-﻿// ===================================================================================================
-//                           _  __     _ _
-//                          | |/ /__ _| | |_ _  _ _ _ __ _
-//                          | ' </ _` | |  _| || | '_/ _` |
-//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
-//
-// This file is part of the Kaltura Collaborative Media Suite which allows users
-// to do with audio, video, and animation what Wiki platfroms allow them to do with
-// text.
-//
-// Copyright (C) 2006-2011  Kaltura Inc.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// @ignore
-// ===================================================================================================
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.Threading;
+﻿// =================================================================================================== // _ __ _ _ // | |/ /__ _| | |_ _ _ _ _ __ _ // | ' </ _` | | _| || | '_/ _` | // |_|\_\__,_|_|\__|\_,_|_| \__,_| // // This file is part of the Kaltura Collaborative Media Suite which allows users // to do 
+with audio, video, and animation what Wiki platfroms allow them to do with // text. // // Copyright (C) 2006-2011 Kaltura Inc. // // This program is free software: you can redistribute it and/or modify // it under the terms of the GNU Affero General Public License as // published by the Free Software 
+Foundation, either version 3 of the // License, or (at your option) any later version. // // This program is distributed in the hope that it will be useful, // but WITHOUT ANY WARRANTY; without even the implied warranty of // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the // GNU Affero General 
+Public License for more details. // // You should have received a copy of the GNU Affero General Public License // along with this program.  If not, see <http://www.gnu.org/licenses/>. // // @ignore // =================================================================================================== using 
+System; using System.Collections.Generic; using System.Text; using System.IO; using System.Threading;
 
 namespace Kaltura
 {
@@ -55,7 +28,7 @@ namespace Kaltura
             uniqueTag = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 20);
             try
             {
-                if(args.Length > 0 && args[0] == '--with-threads')
+                if(args.Length > 0 && args[0].Equals("--with-threads"))
                 {
                     SampleThreadedChunkUpload();
                 }
