e8dcd1b6505bbb56a2e578e7d2b841501a72b8c4
Noam Arad <noam.arad@kaltura.com>
2016-03-15 12:47:12 +0200

    fixes from code review, move test to relevant user-entry type only (quizUserEntry)

--- plugins/cue_points/quiz/lib/api/KalturaQuizUserEntry.php
+++ plugins/cue_points/quiz/lib/api/KalturaQuizUserEntry.php
@@ -39,4 +39,29 @@ class KalturaQuizUserEntry extends KalturaUserEntry{
 		return parent::toObject($object_to_fill, $props_to_skip);
 	}
 
+	/* (non-PHPdoc)
+	 * @see KalturaObject::validateForInsert()
+	 */
+	public function validateForInsert($propertiesToSkip = array())
+	{
+		KalturaLog::debug("@@NA");
+	}
+	
+	/* (non-PHPdoc)
+	 * @see KalturaObject::toInsertableObject()
+	 */
+	public function toInsertableObject ( $object_to_fill = null , $props_to_skip = array() )
+	{
+		$object_to_fill = parent::toInsertableObject($object_to_fill, $props_to_skip);
+		$c = new Criteria();
+		$c->add(UserEntryPeer::KUSER_ID, $object_to_fill->getKuserId());
+		$c->add(UserEntryPeer::ENTRY_ID, $this->entryId);
+		$userEntry = UserEntryPeer::doSelect($c);
+		if (count($userEntry) > 0)
+		{
+			throw new KalturaAPIException(KalturaQuizErrors::QUIZ_USER_ENTRY_ALREADY_EXISTS, $this->entryId);
+		}
+		return $object_to_fill;
+	}
+
 }
