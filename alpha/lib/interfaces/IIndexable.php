ad7ea569c83cf1fa0fd13fdf2c5f8f16459f351a
Jonathan.Kanarek <Jonathan.Kanarek@Kaltura.com>
2016-03-17 13:16:19 +0200

    Support sphinx for schedule

--- alpha/lib/interfaces/IIndexable.php
+++ alpha/lib/interfaces/IIndexable.php
@@ -7,6 +7,7 @@ interface IIndexable extends IBaseObject
 {
 	const FIELD_TYPE_STRING = 'string';
 	const FIELD_TYPE_INTEGER = 'int';
+	const FIELD_TYPE_FLOAT = 'float';
 	const FIELD_TYPE_DATETIME = 'datetime';
 	const FIELD_TYPE_JSON = 'json';
 	
