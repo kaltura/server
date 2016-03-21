6bf8888d5e92cdaf7aee32bceee98cc73cda6d95
yossipapi <yossi.papiashvili@kaltura.com>
2016-03-21 16:07:44 +0200

    When live entry server node status has changes we need to re-index live entry to update sphinx live status

--- alpha/lib/model/LiveEntryServerNode.php
+++ alpha/lib/model/LiveEntryServerNode.php
@@ -3,13 +3,50 @@
  * @package Core
  * @subpackage model
  */
-class LiveEntryServerNode extends EntryServerNode{
-
+class LiveEntryServerNode extends EntryServerNode
+{
 	const OM_CLASS = 'LiveEntryServerNode';
 	
 	const CUSTOM_DATA_STREAMS = "streams";
 	const CUSTOM_DATA_APPLICATION_NAME = "application_name";
 	const CUSTOM_DATA_DC = "dc";
+	
+	private $statusChanged = false;
+	
+	/* (non-PHPdoc)
+	 * @see BaseEntryServerNode::postSave()
+	 */
+	public function postSave(PropelPDO $con = null)
+	{
+		if($this->statusChanged)
+		{
+			$liveEntry = entryPeer::retrieveByPK($this->getEntryId());
+			if($liveEntry)
+			{
+				KalturaLog::debug("Live Status for entry [" . $this->getEntryId() . "] updated to [" . $this->getStatus() . "] re-indexing entry with new live status");
+				$liveEntry->indexToSearchIndex();
+			}
+			
+			$this->statusChanged = false;
+		}
+			
+		parent::postSave($con);
+	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseEntryServerNode::postDelete()
+	 */
+	public function postDelete(PropelPDO $con = null)
+	{
+		$liveEntry = entryPeer::retrieveByPK($this->getEntryId());
+		if($liveEntry)
+		{
+			KalturaLog::debug("Live Entry server node for entry [" . $this->getEntryId() . "] has been deleted, re-indexing entry with new live status");
+			$liveEntry->indexToSearchIndex();
+		}
+			
+		parent::postDelete($con);
+	}
 
 	public function setStreams(KalturaLiveStreamParamsArray $v) 
 	{ 
@@ -39,5 +76,13 @@ class LiveEntryServerNode extends EntryServerNode{
 	public function getDc()
 	{
 		return $this->getFromCustomData(self::CUSTOM_DATA_DC);
+	}
+	
+	public function setStatus($v)
+	{
+		if($this->getStatus() !== $v)
+			$this->statusChanged = true;
+		
+		return parent::setStatus($v);
 	}
 }
