07593f1664770357f838f7410c571bc6703e67d5
MosheMaorKaltura <moshe.maor@kaltura.com>
2016-03-23 02:16:52 -0200

    Backward comp support for annotation cue point without isPublic set and tags = chaptering SUP-7707#time2h

--- plugins/cue_points/annotation/lib/api/filters/KalturaAnnotationFilter.php
+++ plugins/cue_points/annotation/lib/api/filters/KalturaAnnotationFilter.php
@@ -5,6 +5,8 @@
  */
 class KalturaAnnotationFilter extends KalturaAnnotationBaseFilter
 {
+	const CHAPTERS_PUBLIC_TAG = 'chaptering';
+
 	/* (non-PHPdoc)
 	 * @see KalturaRelatedFilter::validateForResponseProfile()
 	 */
@@ -19,6 +21,10 @@ class KalturaAnnotationFilter extends KalturaAnnotationBaseFilter
 
 	public function getTypeListResponse(KalturaFilterPager $pager, KalturaDetachedResponseProfile $responseProfile = null, $type = null)
 	{
+		//Was added to avoid braking backward compatibility for old player chapters module
+		if(isset($this->tagsLike) && $this->tagsLike==self::CHAPTERS_PUBLIC_TAG)
+			KalturaCriterion::disableTag(KalturaCriterion::TAG_WIDGET_SESSION);
+
 		return parent::getTypeListResponse($pager, $responseProfile, AnnotationPlugin::getCuePointTypeCoreValue(AnnotationCuePointType::ANNOTATION));
 	}
 }
