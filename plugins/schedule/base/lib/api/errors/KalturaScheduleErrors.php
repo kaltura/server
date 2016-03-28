4ba440300d43ceaaa5574492b64f937819a9c52c
Jonathan.Kanarek <Jonathan.Kanarek@Kaltura.com>
2016-03-28 17:05:10 +0300

    Text typo

--- plugins/schedule/base/lib/api/errors/KalturaScheduleErrors.php
+++ plugins/schedule/base/lib/api/errors/KalturaScheduleErrors.php
@@ -7,5 +7,5 @@ class KalturaScheduleErrors extends KalturaErrors
 {
 	const INVALID_SCHEDULE_END_BEFORE_START = "INVALID_SCHEDULE_END_BEFORE_START;START,END;End time [@END@] must be after start time [@START@]";
 	const MAX_SCHEDULE_DURATION_REACHED = "MAX_SCHEDULE_DURATION_REACHED;MAX;Maximum schedule duration [@MAX@] reached";
-	const MAX_SCHEDULE_DURATION_MUST_MATCH_END_TIME = "MAX_SCHEDULE_DURATION_MUST_MATCH_END_TIME;;Duration must match end time, keep one of them null to calcualte automatically";
+	const MAX_SCHEDULE_DURATION_MUST_MATCH_END_TIME = "MAX_SCHEDULE_DURATION_MUST_MATCH_END_TIME;;Duration must match end time, keep one of them null to calculate automatically";
 }
