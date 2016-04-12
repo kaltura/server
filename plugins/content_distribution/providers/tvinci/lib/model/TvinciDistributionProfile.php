e04f9431f9ca1a9c1af21603c6d4f705cb27a408
noam-arad <noam.arad@kaltura.com>
2016-04-12 13:10:43 +0300

    Revert "Kajam 11.12.0 plat 4645 new xslt"

--- plugins/content_distribution/providers/tvinci/lib/model/TvinciDistributionProfile.php
+++ plugins/content_distribution/providers/tvinci/lib/model/TvinciDistributionProfile.php
@@ -9,8 +9,21 @@ class TvinciDistributionProfile extends ConfigurableDistributionProfile
  	const CUSTOM_DATA_USERNAME = 'username';
  	const CUSTOM_DATA_PASSWORD = 'password';
  	const CUSTOM_DATA_XSLT = 'xsltFile';
-	const CUSTOM_TAGS = 'tags';
-	
+	const CUSTOM_ISM_FILENAME = 'ismFileName';
+	const CUSTOM_ISM_PPV_MODULE = 'ismPpvModule';
+	const CUSTOM_IPADNEW_FILENAME = 'ipadnewFileName';
+	const CUSTOM_IPADNEW_PPV_MODULE = 'ipadnewPpvModule';
+	const CUSTOM_IPHONENEW_FILENAME = 'iphonenewFileName';
+	const CUSTOM_IPHONENEW_PPV_MODULE = 'iphonenewPpvModule';
+	const CUSTOM_MBR_FILENAME = 'mbrFileName';
+	const CUSTOM_MBR_PPV_MODULE = 'mbrPpvModule';
+	const CUSTOM_DASH_FILENAME = 'dashFileName';
+	const CUSTOM_DASH_PPV_MODULE = 'dashPpvModule';
+	const CUSTOM_WIDEVINE_FILENAME = 'widevineFileName';
+	const CUSTOM_WIDEVINE_PPV_MODULE = 'widevinePpvModule';
+	const CUSTOM_WIDEVINE_MBR_FILENAME = 'widevineMbrFileName';
+	const CUSTOM_WIDEVINE_MBR_PPV_MODULE = 'widevineMbrPpvModule';
+
 	/* (non-PHPdoc)
 	 * @see DistributionProfile::getProvider()
 	 */
@@ -98,6 +111,38 @@ class TvinciDistributionProfile extends ConfigurableDistributionProfile
 	public function getPublisher()				{return $this->getFromCustomData(self::CUSTOM_DATA_PUBLISHER);}
 	public function setPublisher($v)			{$this->putInCustomData(self::CUSTOM_DATA_PUBLISHER, $v);}
 
-	public function getTags()	                {return $this->getFromCustomData(self::CUSTOM_TAGS);}
-	public function setTags($v)	                {$this->putInCustomData(self::CUSTOM_TAGS, $v);}
+	public function getIsmFileName()			{return $this->getFromCustomData(self::CUSTOM_ISM_FILENAME);}
+	public function setIsmFileName($v)			{$this->putInCustomData(self::CUSTOM_ISM_FILENAME, $v);}
+	public function getIsmPpvModule()			{return $this->getFromCustomData(self::CUSTOM_ISM_PPV_MODULE);}
+	public function setIsmPpvModule($v)			{$this->putInCustomData(self::CUSTOM_ISM_PPV_MODULE, $v);}
+
+	public function getMbrFileName()			{return $this->getFromCustomData(self::CUSTOM_MBR_FILENAME);}
+	public function setMbrFileName($v)			{$this->putInCustomData(self::CUSTOM_MBR_FILENAME, $v);}
+	public function getMbrPpvModule()			{return $this->getFromCustomData(self::CUSTOM_MBR_PPV_MODULE);}
+	public function setMbrPpvModule($v)			{$this->putInCustomData(self::CUSTOM_MBR_PPV_MODULE, $v);}
+
+	public function getDashFileName()			{return $this->getFromCustomData(self::CUSTOM_DASH_FILENAME);}
+	public function setDashFileName($v)			{$this->putInCustomData(self::CUSTOM_DASH_FILENAME, $v);}
+	public function getDashPpvModule()			{return $this->getFromCustomData(self::CUSTOM_DASH_PPV_MODULE);}
+	public function setDashPpvModule($v)			{$this->putInCustomData(self::CUSTOM_DASH_PPV_MODULE, $v);}
+
+	public function getIphonenewFileName()		{return $this->getFromCustomData(self::CUSTOM_IPHONENEW_FILENAME);}
+	public function setIphonenewFileName($v)	{$this->putInCustomData(self::CUSTOM_IPHONENEW_FILENAME, $v);}
+	public function getIphonenewPpvModule()		{return $this->getFromCustomData(self::CUSTOM_IPHONENEW_PPV_MODULE);}
+	public function setIphonenewPpvModule($v)	{$this->putInCustomData(self::CUSTOM_IPHONENEW_PPV_MODULE, $v);}
+
+	public function getIpadnewFileName()		{return $this->getFromCustomData(self::CUSTOM_IPADNEW_FILENAME);}
+	public function setIpadnewFileName($v)		{$this->putInCustomData(self::CUSTOM_IPADNEW_FILENAME, $v);}
+	public function getIpadnewPpvModule()		{return $this->getFromCustomData(self::CUSTOM_IPADNEW_PPV_MODULE);}
+	public function setIpadnewPpvModule($v)		{$this->putInCustomData(self::CUSTOM_IPADNEW_PPV_MODULE, $v);}
+
+	public function getWidevineFileName()		{return $this->getFromCustomData(self::CUSTOM_WIDEVINE_FILENAME);}
+	public function setWidevineFileName($v)		{$this->putInCustomData(self::CUSTOM_WIDEVINE_FILENAME, $v);}
+	public function getWidevinePpvModule()		{return $this->getFromCustomData(self::CUSTOM_WIDEVINE_PPV_MODULE);}
+	public function setWidevinePpvModule($v)	{$this->putInCustomData(self::CUSTOM_WIDEVINE_PPV_MODULE, $v);}
+
+	public function getWidevineMbrFileName()	{return $this->getFromCustomData(self::CUSTOM_WIDEVINE_MBR_FILENAME);}
+	public function setWidevineMbrFileName($v)	{$this->putInCustomData(self::CUSTOM_WIDEVINE_MBR_FILENAME, $v);}
+	public function getWidevineMbrPpvModule()	{return $this->getFromCustomData(self::CUSTOM_WIDEVINE_MBR_PPV_MODULE);}
+	public function setWidevineMbrPpvModule($v)	{$this->putInCustomData(self::CUSTOM_WIDEVINE_MBR_PPV_MODULE, $v);}
 }
