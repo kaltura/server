5e84e141cf8015c8103b1e94c6e0d1774e8470a9
elad.benedict <elad.benedict@kaltura.com>
2016-04-21 15:33:23 +0300

    (add the rest of the files)

---
+++ api_v3/lib/types/report/KalturaReportFilter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaReportFilter extends KalturaObject
+{
+	/**
+	 * @var string
+	 */
+	public $dimension;
+
+	/**
+	 * @var string
+	 */
+	public $values;
+
+}
