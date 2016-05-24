66ff9c81d1b5715f6ad881deb0dd18d9e244eade
MosheMaorKaltura <moshe.maor@kaltura.com>
2016-05-24 17:53:19 -0200

    validate filter object not null

--- plugins/like/services/LikeService.php
+++ plugins/like/services/LikeService.php
@@ -129,9 +129,9 @@ class LikeService extends KalturaBaseService
 	 */
 	public function listAction(KalturaLikeFilter $filter = null, KalturaFilterPager $pager = null)
 	{
-		if( !$filter->entryIdEqual && !$filter->userIdEqual && !$filter->entryIdIn)
+		if(!$filter || (!$filter->entryIdEqual && !$filter->userIdEqual && !$filter->entryIdIn))
 			throw new KalturaAPIException(KalturaErrors::MISSING_MANDATORY_PARAMETER ,"entryIdEqual or userIdEqual or entryIdIn");
-		
+
 		if($filter->entryIdEqual && !entryPeer::retrieveByPK($filter->entryIdEqual))
 			throw new KalturaAPIException(KalturaErrors::ENTRY_ID_NOT_FOUND, $filter->entryIdEqual);
 		if($filter->userIdEqual && !kuserPeer::getActiveKuserByPartnerAndUid(kCurrentContext::$ks_partner_id, $filter->userIdEqual))
