8b56b123241e599cc3891f06157f084076cd4225
gotlieb <gotlieb.erez@gmail.com>
2016-05-25 17:35:29 +0300

    added missing extends of KalturaRelatedFilter from generating filters PLAT-5545

--- plugins/schedule/base/lib/api/filters/base/KalturaScheduleEventBaseFilter.php
+++ plugins/schedule/base/lib/api/filters/base/KalturaScheduleEventBaseFilter.php
@@ -4,7 +4,7 @@
  * @subpackage api.filters.base
  * @abstract
  */
-abstract class KalturaScheduleEventBaseFilter extends KalturaFilter
+abstract class KalturaScheduleEventBaseFilter extends KalturaRelatedFilter
 {
 	static private $map_between_objects = array
 	(
