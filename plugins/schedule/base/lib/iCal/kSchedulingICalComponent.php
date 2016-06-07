f8e94e6970cd7d1084cf73602003f1ee7039e979
gotlieb <gotlieb.erez@gmail.com>
2016-06-07 09:56:18 +0300

    Timezone conversions and parsing modifications for ical files. PLAT-5519

--- plugins/schedule/base/lib/iCal/kSchedulingICalComponent.php
+++ plugins/schedule/base/lib/iCal/kSchedulingICalComponent.php
@@ -6,22 +6,27 @@ abstract class kSchedulingICalComponent
 	 * @var array
 	 */
 	protected $fields = array();
-	
+
+	/**
+	 * @var array
+	 */
+	protected $configurationFields = array();
+
 	/**
 	 * @var array
 	 */
 	protected $components = array();
-	
+
 	/**
 	 * @var kSchedulingICalComponent
 	 */
 	protected $parent = null;
-	
+
 	/**
 	 * @var KalturaScheduleEventType
 	 */
 	protected $eventsType = null;
-	
+
 	/**
 	 * @var boolean
 	 */
@@ -31,13 +36,13 @@ abstract class kSchedulingICalComponent
 
 	public function __construct($data = null)
 	{
-		if($data)
+		if ($data)
 		{
 			$lines = explode($this->getLineDelimiter(), $data);
 			$this->parseLines($lines);
 		}
 	}
-	
+
 	public static function setWriteToStdout($write)
 	{
 		self::$writeToStdout = $write;
@@ -45,110 +50,151 @@ abstract class kSchedulingICalComponent
 
 	protected function getLineDelimiter()
 	{
-		 return "\r\n";
+		return "\r\n";
 	}
 
 	protected function getFieldDelimiter()
 	{
-		 return ':';
+		return ':';
 	}
-	
+
+	protected function getFieldSemiColonDelimiter()
+	{
+		return ';';
+	}
+
 	public function parseLines(array &$lines)
 	{
 		do
 		{
 			$line = array_shift($lines);
-			
-			if(!trim($line))
+
+			if (!trim($line))
 				continue;
-			
 			list($field, $value) = explode($this->getFieldDelimiter(), $line, 2);
-			$field = strtoupper($field);
-			$value = trim($value);
-			
-			if(strtoupper($value) === $this->getType())
+
+			$tagConfiguration = null;
+			if (strpos($field, ';') !== FALSE)
 			{
-				if($field === 'BEGIN')
+				list($field, $tagConfiguration) = explode($this->getFieldSemiColonDelimiter(), $field, 2);
+			}
+			$field = strtoupper($field);
+
+			$value = trim($value);
+
+			if (strtoupper($value) === $this->getType())
+			{
+				if ($field === 'BEGIN')
 					continue;
 
-				if($field === 'END')
+				if ($field === 'END')
 					break;
 			}
 
-			if($field === 'BEGIN')
+			if ($field === 'BEGIN')
 			{
 				$component = kSchedulingICal::parseComponent(strtoupper($value), $lines);
-				$this->addComponent($component);
+				if ($component != null)
+				{
+					$this->addComponent($component);
+				}
 				continue;
 			}
-				
+
+			while (count($lines))
+			{
+				$additionalLine = array_shift($lines);
+				if (strpos($additionalLine, ':') !== FALSE)
+				{
+					array_unshift($lines, $additionalLine);
+					break;
+				} else
+				{
+					$additionalLine = trim($additionalLine);
+					$value .= $additionalLine;
+				}
+			}
+
 			$setter = 'set' . str_replace('-', '', preg_replace('/^x-/', '', $field));
-			if(method_exists($this, $setter))
+			if (method_exists($this, $setter))
 			{
 				$this->$setter($value);
 			}
-			
+
 			$this->setField($field, $value);
-			
-		} while(count($lines));
+			if (isset($tagConfiguration))
+				$this->setConfigurationField($field, $tagConfiguration);
+
+		} while (count($lines));
 	}
-	
+
 	public function getRaw()
 	{
 		$lines = array();
 		$lines[] = 'BEGIN:' . $this->getType();
-		foreach($this->fields as $field => $value)
+		foreach ($this->fields as $field => $value)
 			$lines[] = "{$field}:{$value}";
 		$lines[] = 'END:' . $this->getType();
-		
+
 		return implode("\r\n", $lines);
 	}
-	
+
 	public function setParentComponent(kSchedulingICalComponent $parent)
 	{
 		$this->parent = $parent;
 	}
-	
+
 	public function addComponent(kSchedulingICalComponent $component)
 	{
 		$component->setParentComponent($this);
 		$this->components[] = $component;
 	}
-	
+
 	public function getComponents()
 	{
 		return $this->components;
 	}
-	
+
 	public function setField($field, $value)
 	{
 		$this->fields[strtoupper($field)] = $value;
 	}
-	
+
 	public function getField($field)
 	{
-		if(isset($this->fields[strtoupper($field)]))
+		if (isset($this->fields[strtoupper($field)]))
 			return $this->fields[strtoupper($field)];
-		
 		return null;
 	}
-	
+
+	public function setConfigurationField($field, $configutationvalue)
+	{
+		$this->configurationFields[strtoupper($field)] = $configutationvalue;
+	}
+
+	public function getConfigurationField($field)
+	{
+		if (isset($this->configurationFields[strtoupper($field)]))
+			return $this->configurationFields[strtoupper($field)];
+		return null;
+	}
+
 	public function addFields($fields, $prefix = null)
 	{
-		foreach($fields as $field => $value)
+		foreach ($fields as $field => $value)
 		{
-			if($prefix)
+			if ($prefix)
 				$field = "{$prefix}-{$field}";
-			
+
 			$this->setField($field, $value);
 		}
 	}
-	
+
 	public function toObject()
 	{
 		return null;
 	}
-	
+
 	/**
 	 * @param KalturaScheduleEventType $type
 	 */
@@ -156,57 +202,57 @@ abstract class kSchedulingICalComponent
 	{
 		$this->eventsType = $type;
 	}
-	
+
 	/**
 	 * @return KalturaScheduleEventType
 	 */
 	protected function getKalturaType()
 	{
-		if($this->eventsType)
+		if ($this->eventsType)
 			return $this->eventsType;
 
-		if($this->parent)
+		if ($this->parent)
 			return $this->parent->getKalturaType();
-		
+
 		return null;
 	}
-	
+
 	public function writeField($field, $value)
 	{
 		$str = $field . $this->getFieldDelimiter() . $value . $this->getLineDelimiter();
-		if(self::$writeToStdout)
+		if (self::$writeToStdout)
 			echo $str;
-		
+
 		return $str;
 	}
-	
+
 	protected function writeBody()
 	{
 		$ret = '';
-		foreach($this->fields as $field => $value)
+		foreach ($this->fields as $field => $value)
 			$ret .= $this->writeField($field, $value);
-		
+
 		return $ret;
 	}
-	
+
 	public function begin()
-	{	
+	{
 		return $this->writeField('BEGIN', $this->getType());
 	}
-	
+
 	public function end()
 	{
 		return $this->writeField('END', $this->getType());
 	}
-	
+
 	public function write()
 	{
 		$ret = '';
-		
+
 		$ret .= $this->begin();
 		$ret .= $this->writeBody();
 		$ret .= $this->end();
-		
+
 		return $ret;
 	}
 }
--- plugins/schedule/base/lib/iCal/kSchedulingICalEvent.php
+++ plugins/schedule/base/lib/iCal/kSchedulingICalEvent.php
@@ -21,12 +21,12 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 		'comment',
 		'organizer',
 	);
-	
+
 	private static $dateFields = array(
 		'startDate' => 'dtstart',
 		'endDate' => 'dtend',
 	);
-	
+
 	/**
 	 * {@inheritDoc}
 	 * @see kSchedulingICalComponent::getType()
@@ -40,17 +40,17 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 	{
 		return $this->getField('uid');
 	}
-	
+
 	public function getMethod()
 	{
 		return $this->getField('method');
 	}
-	
+
 	public function setRRule($rrule)
 	{
 		$this->rule = new kSchedulingICalRule($rrule);
 	}
-	
+
 	/**
 	 * @return kSchedulingICalRule
 	 */
@@ -58,12 +58,12 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 	{
 		return $this->rule;
 	}
-	
+
 	public function setRule(kSchedulingICalRule $rule)
 	{
 		$this->rule = $rule;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 * @see kSchedulingICalComponent::writeBody()
@@ -71,13 +71,13 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 	protected function writeBody()
 	{
 		$ret = parent::writeBody();
-		
-		if($this->rule)
+
+		if ($this->rule)
 			$ret .= $this->writeField('RRULE', $this->rule->getBody());
-		
+
 		return $ret;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 * @see kSchedulingICalComponent::toObject()
@@ -86,7 +86,7 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 	{
 		$type = $this->getKalturaType();
 		$event = null;
-		switch($type)
+		switch ($type)
 		{
 			case KalturaScheduleEventType::RECORD:
 				$event = new KalturaRecordScheduleEvent();
@@ -95,40 +95,43 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 			case KalturaScheduleEventType::LIVE_STREAM:
 				$event = new KalturaLiveStreamScheduleEvent();
 				break;
-				
+
 			default:
 				throw new Exception("Event type [$type] not supported");
 		}
 
 		$event->referenceId = $this->getUid();
 
-		foreach(self::$stringFields as $string)
+		foreach (self::$stringFields as $string)
 		{
 			$event->$string = $this->getField($string);
 		}
 
-		foreach(self::$dateFields as $date => $field)
+		foreach (self::$dateFields as $date => $field)
 		{
-			$event->$date = kSchedulingICal::parseDate($this->getField($field));
+			$configurationField = $this->getConfigurationField($field);
+			preg_match('/"([^"]+)"/', $configurationField, $matches);
+			$timezoneFormat = $matches[1];
+			$val = kSchedulingICal::parseDate($this->getField($field), $timezoneFormat);
+			$event->$date = $val;
 		}
-		
+
 		$classificationTypes = array(
 			'PUBLIC' => KalturaScheduleEventClassificationType::PUBLIC_EVENT,
 			'PRIVATE' => KalturaScheduleEventClassificationType::PRIVATE_EVENT,
 			'CONFIDENTIAL' => KalturaScheduleEventClassificationType::CONFIDENTIAL_EVENT
 		);
-		
+
 		$classificationType = $this->getField('class');
-		if(isset($classificationTypes[$classificationType]))
+		if (isset($classificationTypes[$classificationType]))
 			$event->classificationType = $classificationTypes[$classificationType];
 
 		$rule = $this->getRule();
-		if($rule)
+		if ($rule)
 		{
 			$event->recurrenceType = KalturaScheduleEventRecurrenceType::RECURRING;
 			$event->recurrence = $rule->toObject();
-		}
-		else
+		} else
 		{
 			$event->recurrenceType = KalturaScheduleEventRecurrenceType::NONE;
 		}
@@ -136,17 +139,17 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 		$event->parentId = $this->getField('x-kaltura-parent-id');
 		$event->tags = $this->getField('x-kaltura-tags');
 		$event->ownerId = $this->getField('x-kaltura-owner-id');
-		
-		if($event instanceof KalturaEntryScheduleEvent)
+
+		if ($event instanceof KalturaEntryScheduleEvent)
 		{
 			$event->templateEntryId = $this->getField('x-kaltura-template-entry-id');
 			$event->entryIds = $this->getField('x-kaltura-entry-ids');
 			$event->categoryIds = $this->getField('x-kaltura-category-ids');
 		}
-						
+
 		return $event;
 	}
-	
+
 	/**
 	 * @param KalturaScheduleEvent $event
 	 * @return kSchedulingICalEvent
@@ -155,31 +158,31 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 	{
 		$object = new kSchedulingICalEvent();
 
-		if($event->referenceId)
+		if ($event->referenceId)
 			$object->setField('uid', $event->referenceId);
-		
-		foreach(self::$stringFields as $string)
+
+		foreach (self::$stringFields as $string)
 		{
-			if($event->$string)
+			if ($event->$string)
 				$object->setField($string, $event->$string);
 		}
-		
-		foreach(self::$dateFields as $date => $field)
+
+		foreach (self::$dateFields as $date => $field)
 		{
-			if($event->$date)
+			if ($event->$date)
 				$object->setField($field, kSchedulingICal::formatDate($event->$date));
 		}
-		
+
 		$classificationTypes = array(
 			KalturaScheduleEventClassificationType::PUBLIC_EVENT => 'PUBLIC',
 			KalturaScheduleEventClassificationType::PRIVATE_EVENT => 'PRIVATE',
 			KalturaScheduleEventClassificationType::CONFIDENTIAL_EVENT => 'CONFIDENTIAL'
 		);
 
-		if($event->classificationType && isset($classificationTypes[$event->classificationType]))
+		if ($event->classificationType && isset($classificationTypes[$event->classificationType]))
 			$classificationType = $object->setField('class', $classificationTypes[$event->classificationType]);
-			
-		if($event->recurrence)
+
+		if ($event->recurrence)
 		{
 			$rule = kSchedulingICalRule::fromObject($event->recurrence);
 			$object->setRule($rule);
@@ -191,58 +194,58 @@ class kSchedulingICalEvent extends kSchedulingICalComponent
 		$object->setField('x-kaltura-partner-id', $event->partnerId);
 		$object->setField('x-kaltura-status', $event->status);
 		$object->setField('x-kaltura-owner-id', $event->ownerId);
-		
 
-		if($event->parentId)
+
+		if ($event->parentId)
 		{
 			$parent = ScheduleEventPeer::retrieveByPK($event->parentId);
-			if($parent)
+			if ($parent)
 			{
 				$object->setField('x-kaltura-parent-id', $event->parentId);
-				if($parent->getReferenceId())
+				if ($parent->getReferenceId())
 					$object->setField('x-kaltura-parent-uid', $parent->getReferenceId());
 			}
 		}
 
-		if($event->tags)
+		if ($event->tags)
 			$object->setField('x-kaltura-tags', $event->tags);
-		
-		if($event instanceof KalturaEntryScheduleEvent)
+
+		if ($event instanceof KalturaEntryScheduleEvent)
 		{
-			if($event->templateEntryId)
+			if ($event->templateEntryId)
 				$object->setField('x-kaltura-template-entry-id', $event->templateEntryId);
 
-			if($event->entryIds)
+			if ($event->entryIds)
 				$object->setField('x-kaltura-entry-ids', $event->entryIds);
-				
-			if($event->categoryIds)
+
+			if ($event->categoryIds)
 			{
 				$object->setField('x-kaltura-category-ids', $event->categoryIds);
-				
+
 				// hack, to be removed after x-kaltura-category-ids will be fully supported by other partners
 				$pks = explode(',', $event->categoryIds);
 				$categories = categoryPeer::retrieveByPKs($pks);
 				$fullIds = array();
-				foreach($categories as $category)
+				foreach ($categories as $category)
 				{
 					/* @var $category category */
 					$fullIds[] = $category->getFullIds();
 				}
-				if(count($fullIds))
+				if (count($fullIds))
 					$object->setField('related-to', implode(';', $fullIds));
 			}
 		}
 
 		$resources = ScheduleEventResourcePeer::retrieveByEventId($event->id);
 		$resourceIds = array();
-		foreach($resources as $resource)
+		foreach ($resources as $resource)
 		{
 			/* @var $resource ScheduleEventResource */
 			$resourceIds[] = $resource->getResourceId();
 		}
-		if(count($resourceIds))
+		if (count($resourceIds))
 			$object->setField('x-kaltura-resource-ids', implode(',', $resourceIds));
-		
+
 		return $object;
 	}
 }
