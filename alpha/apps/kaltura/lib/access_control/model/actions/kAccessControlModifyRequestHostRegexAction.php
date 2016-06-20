fbe1dbe444f6052d1e08f32d31276d353b8ae4f4
yossipapi <yossi.papiashvili@kaltura.com>
2016-06-20 18:08:04 +0300

    Support defining serverNode to calculate the replacement host

--- alpha/apps/kaltura/lib/access_control/model/actions/kAccessControlModifyRequestHostRegexAction.php
+++ alpha/apps/kaltura/lib/access_control/model/actions/kAccessControlModifyRequestHostRegexAction.php
@@ -15,6 +15,11 @@ class kAccessControlModifyRequestHostRegexAction extends kRuleAction
 	 */
 	protected $replacement;
 	
+	/**
+	 * @var int
+	 */
+	protected $replacmenServerNodeId;
+	
 	public function __construct() 
 	{
 		parent::__construct(RuleActionType::REQUEST_HOST_REGEX);
@@ -40,7 +45,29 @@ class kAccessControlModifyRequestHostRegexAction extends kRuleAction
 	 */
 	public function getReplacement()
 	{
-		return $this->replacement;
+		$replacement = "";
+		
+		if(isset($this->replacement) && strpos($this->replacement, "{hostname}") == false)
+		{
+			$replacement = $this->replacement;
+		}
+		elseif(isset($this->replacmenServerNodeId))
+		{
+			$serverNode = ServerNodePeer::retrieveByPK($this->replacmenServerNodeId);
+			if($serverNode)
+			{
+				if(isset($this->replacement) && strpos($this->replacement, "{hostname}") >= 0)
+				{
+					$replacement = str_replace("{hostname}", $serverNode->getHostName(), $this->replacement);
+				}
+				else 
+				{
+					$replacement = "$1//" . $serverNode->getHostName() . "/" . EdgeServerNode::EDGE_SERVER_DEFAULT_KAPI_APPLICATION_NAME ."/$2";
+				}
+			}
+		}
+		
+		return $replacement;
 	}
 	/**
 	 * @param string $replacement
@@ -48,5 +75,20 @@ class kAccessControlModifyRequestHostRegexAction extends kRuleAction
 	public function setReplacement($replacement)
 	{
 		$this->replacement = $replacement;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getReplacmenServerNodeId()
+	{
+		return $this->replacmenServerNodeId;
+	}
+	/**
+	 * @param string $replacement
+	 */
+	public function setReplacmenServerNodeId($replacmenServerNodeId)
+	{
+		$this->replacmenServerNodeId = $replacmenServerNodeId;
 	}
 }
