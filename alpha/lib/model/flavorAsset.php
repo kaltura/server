1ddc4fb72a2ba126523467a44517f617ed4427a8
Noam Arad <noam.arad@kaltura.com>
2016-06-21 15:48:27 +0300

    create exportableAsset to define which type of asset should be exportable. Make thumbAsset and flavorAsset inherit from it. PLAT-5718

--- alpha/lib/model/flavorAsset.php
+++ alpha/lib/model/flavorAsset.php
@@ -8,7 +8,7 @@
  * @package Core
  * @subpackage model
  */
-class flavorAsset extends asset
+class flavorAsset extends exportableAsset
 {
 
 	const KALTURA_TOKEN_MARKER = '{kt}';
@@ -104,76 +104,6 @@ class flavorAsset extends asset
 		$this->setBitrate($dbAssetParams->getVideoBitrate()+$dbAssetParams->getAudioBitrate());
 		$this->setFrameRate($dbAssetParams->getFrameRate());
 		$this->setVideoCodecId($dbAssetParams->getVideoCodec());
-	}
-	
-		
-    /**
-     * (non-PHPdoc)
-     * @see asset::setStatusLocalReady()
-     */
-    public function setStatusLocalReady()
-    {
-	    $newStatus = asset::ASSET_STATUS_READY;
-	    
-	    $externalStorages = StorageProfilePeer::retrieveExternalByPartnerId($this->getPartnerId());
-	    foreach($externalStorages as $externalStorage)
-	    {
-	    	if($this->requiredToExportFlavor($externalStorage))
-			{
-				KalturaLog::info('Asset id ['.$this->getId().'] is required to export to profile ['.$externalStorage->getId().'] - setting status to [EXPORTING]');
-				$newStatus = asset::ASSET_STATUS_EXPORTING;
-				break;			
-			}			
-	    }
-        KalturaLog::info('Setting status to ['.$newStatus.']');
-	    $this->setStatus($newStatus);
-	}
-	
-	private function requiredToExportFlavor(StorageProfile $storage)
-	{
-		// check if storage profile should affect the asset ready status
-		if ($storage->getReadyBehavior() != StorageProfileReadyBehavior::REQUIRED)
-		{
-			// current storage profile is not required for asset readiness - skipping
-		        return false;
-		}
-		    
-		// check if export should happen now or wait for another trigger
-		if (!$storage->triggerFitsReadyAsset($this->getEntryId())) {
-			KalturaLog::info('Asset id ['.$this->getId().'] is not ready to export to profile ['.$storage->getId().']');
-		    return false;
-		}
-		    
-		// check if asset needs to be exported to the remote storage
-		if (!$storage->shouldExportFlavorAsset($this, true))
-		{
-			KalturaLog::info('Should not export asset id ['.$this->getId().'] to profile ['.$storage->getId().']');
-    		return false;
-    	}
-		    
-		$keys = array(
-		    		$this->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET), 
-		    		$this->getSyncKey(flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ISM), 
-		    		$this->getSyncKey(flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ISMC));
-				
-		foreach ($keys as $key) 
-		{
-			if($storage->shoudlExportFileSync($key))
-    		{
-				return true;
-    		}
-		}
-		    
-		foreach ($keys as $key) 
-		{
-			// check if asset is currently being exported to the remote storage
-    		if ($storage->isPendingExport($key))
-    		{
-    		   	KalturaLog::info('Asset id ['.$this->getId().'] is currently being exported to profile ['.$storage->getId().']');
-		      	return true;
-			}
-		}
-		return false;			
 	}
 
 	/**
@@ -341,5 +271,13 @@ class flavorAsset extends asset
 	{
 		$token = sha1(kConf::get('url_token_secret') . $url);
 		return str_replace(self::KALTURA_TOKEN_MARKER, $token, $url);
+	}
+
+	protected function getSyncKeysForExporting()
+	{
+		return array(
+			$this->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET),
+			$this->getSyncKey(flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ISM),
+			$this->getSyncKey(flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ISMC));
 	}
 }
