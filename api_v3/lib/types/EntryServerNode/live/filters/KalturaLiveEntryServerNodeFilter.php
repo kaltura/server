57f24cbb8ccc01bd794aebd1a20614d3ef76c68b
yossipapi <yossi.papiashvili@kaltura.com>
2016-07-07 15:50:40 +0300

    modify validateMediaServer batch worker to moniter only streams created within the current dc

--- api_v3/lib/types/EntryServerNode/live/filters/KalturaLiveEntryServerNodeFilter.php
+++ api_v3/lib/types/EntryServerNode/live/filters/KalturaLiveEntryServerNodeFilter.php
@@ -5,4 +5,16 @@
  */
 class KalturaLiveEntryServerNodeFilter extends KalturaLiveEntryServerNodeBaseFilter
 {
+	/**
+	 * @var bool
+	 */
+	public $currentDcOnly;
+	
+	public function getListResponse(KalturaFilterPager $pager, KalturaDetachedResponseProfile $responseProfile = null)
+	{
+		if($this->currentDcOnly)
+			$this->dcEqual = kDataCenterMgr::getCurrentDcId();
+		
+		return parent::getListResponse($pager, $responseProfile);
+	}
 }
