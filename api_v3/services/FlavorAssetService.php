6d028f5c8fb456ac2b18b96b66dd1e3e6f3fa29f
Anatol <anatol.schwartz@kaltura.com>
2016-07-18 10:53:49 +0300

    Added generateAdStitchingCmdline

--- api_v3/services/FlavorAssetService.php
+++ api_v3/services/FlavorAssetService.php
@@ -938,4 +938,129 @@ class FlavorAssetService extends KalturaAssetService
 		//todo - implement and return the command line
 		
 	}
+	
+	private static function generateAdStitchingCmdline($ffprobeJson, $flavorParams, $flavorParamsOutput)
+	{
+//		$asset = assetPeer::retreiveById($assetId);
+//		$flavorParamsId = $asset->getFlavorParamsId();
+//		$flavorParams = assetParamsPeer::retrieveByPK($flavorParamsId);
+//		$flavorParamsOutput = assetParamsOutputPeer::retrieveByPK($assetId);
+		
+		$parser = new KFFMpegMediaParserAdStitchHelper($ffprobeJson);
+		$srcMedInf = $parser->getMediaInfo();
+		$srcMedSet = KFFMpegMediaParserAdStitchHelper::mediaInfoToKDL($srcMedInf);
+
+/**
+ * To match from flavor params output-
+ * - frame size
+ * - fps
+ * - gop
+ * - audio params
+ * Other matching
+ * - AvoidVideoShrinkFramesizeToSource = on
+ * - two pass - off
+ * - encypted - off
+ * - letter boxing
+ * - other extra params (from live sticthing)
+ */
+		$kdlFlavor = KDLWrap::ConvertFlavorCdl2Kdl($flavorParams);
+		$kdlFlavor->_isTwoPass = false;
+		$kdlFlavor->_isEncrypted = false;
+		$kdlFlavor->_video->_arProcessingMode = 2; // letter boxing
+		$kdlFlavor->_video->_isShrinkFramesizeToSource = false;
+		if($flavorParamsOutput->getWidth()){
+			$kdlFlavor->_video->_width = $flavorParamsOutput->getWidth();
+		}
+		if($flavorParamsOutput->getHeight()){
+			$kdlFlavor->_video->_height = $flavorParamsOutput->getHeight();
+		}
+		if($flavorParamsOutput->getGopSize()){
+			$kdlFlavor->_video->_gop = $flavorParamsOutput->getGopSize();
+		}
+		if($flavorParamsOutput->getFrameRate()){
+			$kdlFlavor->_video->_frameRate = $flavorParamsOutput->getFrameRate();
+		}
+		if($flavorParamsOutput->getAudioBitrate()){
+			$kdlFlavor->_audio->_bitRate = $flavorParamsOutput->getAudioBitrate();
+		}
+		if($flavorParamsOutput->getAudioSampleRate()){
+			$kdlFlavor->_audio->_sampleRate = $flavorParamsOutput->getAudioSampleRate();
+		}
+		$target = $kdlFlavor->GenerateTarget($srcMedSet);
+		$cmdLine = $target->_transcoders[0]->_cmd;
+		return $cmdLine;
+	}
 }
+
+class KFFMpegMediaParserAdStitchHelper extends KFFMpegMediaParser{
+	
+	public function __construct($rawData)
+	{
+		$this->filePath = $rawData;
+	}
+	
+	/**
+	 * @method
+	 * @return string
+	 */
+	public function getRawMediaInfo($filePath=null)
+	{
+		return $this->filePath;
+	}
+	
+	/**
+	 * @method
+	 * @param unknown_type $mediaInfo
+	 * @return KDLMediaDataSet
+	 */
+	static public function mediaInfoToKDL($mediaInfo)
+	{
+		$medSet = new KDLMediaDataSet();
+	
+		$medSet->_container = new KDLContainerData();
+	
+		$contentStreams = $mediaInfo->contentStreams;
+		if(isset($contentStreams)) {
+			$fromJson = json_decode($contentStreams);
+			$medSet->_contentStreams = isset($fromJson)? $fromJson: null;
+		}
+	
+	
+		$medSet->_container->_id=$mediaInfo->containerId;
+		$medSet->_container->_format=$mediaInfo->containerFormat;
+		$medSet->_container->_duration=$mediaInfo->containerDuration;
+		$medSet->_container->_bitRate=$mediaInfo->containerBitRate;
+		$medSet->_container->_fileSize=$mediaInfo->fileSize;
+		if($medSet->_container->IsDataSet()==false)
+			$medSet->_container = null;
+	
+		$medSet->_video = new KDLVideoData();
+		$medSet->_video->_id = $mediaInfo->videoCodecId;
+		$medSet->_video->_format = $mediaInfo->videoFormat;
+		$medSet->_video->_duration = $mediaInfo->videoDuration;
+		$medSet->_video->_bitRate = $mediaInfo->videoBitRate;
+		$medSet->_video->_width = $mediaInfo->videoWidth;
+		$medSet->_video->_height = $mediaInfo->videoHeight;
+		$medSet->_video->_frameRate = $mediaInfo->videoFrameRate;
+		$medSet->_video->_dar = $mediaInfo->videoDar;
+		$medSet->_video->_rotation = $mediaInfo->videoRotation;
+		$medSet->_video->_scanType = $mediaInfo->scanType;
+		if($medSet->_video->IsDataSet()==false)
+			$medSet->_video = null;
+	
+		$medSet->_audio = new KDLAudioData();
+		$medSet->_audio->_id = $mediaInfo->audioCodecId;
+		$medSet->_audio->_format = $mediaInfo->audioFormat;
+		$medSet->_audio->_duration = $mediaInfo->audioDuration;
+		$medSet->_audio->_bitRate = $mediaInfo->audioBitRate;
+		$medSet->_audio->_channels = $mediaInfo->audioChannels;
+		$medSet->_audio->_sampleRate = $mediaInfo->audioSamplingRate;
+		$medSet->_audio->_resolution = $mediaInfo->audioResolution;
+		if($medSet->_audio->IsDataSet()==false)
+			$medSet->_audio = null;
+		return $medSet;
+	}
+	
+	
+}
+
