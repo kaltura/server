cab53fcab41a84bafe411c36783bbe59022175d6
Orly Lampert <orly.lampert@kaltura.com>
2016-07-20 09:34:29 +0300

    increase memory limit to handle reports with large resultset

--- api_v3/services/ReportService.php
+++ api_v3/services/ReportService.php
@@ -265,7 +265,7 @@ class ReportService extends KalturaBaseService
 		
 		$execParams = KalturaReportHelper::getValidateExecutionParameters($dbReport, $params);
 		
-		ini_set( "memory_limit","1024M" );
+		ini_set( "memory_limit","256M" );
 
 		$kReportsManager = new kReportManager($dbReport);
 		list($columns, $rows) = $kReportsManager->execute($execParams);
