4c64987e3e14e126e60d337ae96e1244a6930089
yossipapi <yossi.papiashvili@kaltura.com>
2016-09-27 14:43:15 +0300

    Restore else to be consistent with other functions as well

--- plugins/cue_points/base/lib/model/index/CuePointIndex.php
+++ plugins/cue_points/base/lib/model/index/CuePointIndex.php
@@ -302,8 +302,8 @@ class CuePointIndex extends BaseIndexObject
 	{
 		if (is_null($object))
 			return array(array("cue_point:id=%s", CuePointPeer::ID), array("cue_point:entryId=%s", CuePointPeer::ENTRY_ID), array("cue_point:partnerId=%s", CuePointPeer::PARTNER_ID));
-		
-		return array("cue_point:id=" . strtolower($object->getId()), "cue_point:entryId=" . strtolower($object->getEntryId()), "cue_point:partnerId=" . strtolower($object->getPartnerId()));
+		else
+			return array("cue_point:id=" . strtolower($object->getId()), "cue_point:entryId=" . strtolower($object->getEntryId()), "cue_point:partnerId=" . strtolower($object->getPartnerId()));
 	}
 
 }
