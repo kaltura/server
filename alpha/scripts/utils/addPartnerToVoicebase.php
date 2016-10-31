9381944ded1b4bafbf1964685ceac522408ebd7b
roman-kreichman <roman.kreichman@gmail.com>
2016-10-31 16:13:41 +0200

    TR-1663 - Added option to transform DFXP files to a more "common" format

--- alpha/scripts/utils/addPartnerToVoicebase.php
+++ alpha/scripts/utils/addPartnerToVoicebase.php
@@ -1,15 +1,17 @@
 <?php
-	require_once(__dir__ . '/../bootstrap.php');	
+	require_once(__dir__ . '/../bootstrap.php');
 
 	if($argc < 4)
 	{
-		die("Usage: php " . basename(__FILE__) . " [partner id] [apiKey] [apiPassword]" . PHP_EOL);
+		die("Usage: php " . basename(__FILE__) . " [partner id] [apiKey] [apiPassword] <[transformDfxp]>" . PHP_EOL);
 	}
-	
+
 	$partnerId = $argv[1];
 	$apiKey = $argv[2];
 	$apiPassword = $argv[3];
-	
+	$transformDfxp = isset($argv[4]) ? (bool)$argv[4] : false;
+
 	$options = new VoicebaseOptions($apiKey, $apiPassword);
+	$options->transformDfxp = $transformDfxp;
 	VoicebasePlugin::setPartnerVoicebaseOptions($partnerId, $options);
 	
