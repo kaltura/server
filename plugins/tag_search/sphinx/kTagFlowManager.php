3375858bdaf84e5089ab5772be04a288a9bf99a0
Noam Arad <noam.arad@kaltura.com>
2016-11-08 17:36:03 +0200

    make sure that if privacy tag is empty we use NULL_PC instead

--- plugins/tag_search/sphinx/kTagFlowManager.php
+++ plugins/tag_search/sphinx/kTagFlowManager.php
@@ -246,6 +246,7 @@ class kTagFlowManager implements kObjectCreatedEventConsumer, kObjectDeletedEven
 	        	$missingPrivacyContexts = array_diff($privacyContexts, $foundPrivacyContexts);
 	        	if (!is_null($missingPrivacyContexts) && count($missingPrivacyContexts))
 	        	{
+			        $missingPrivacyContexts = $privacyContexts ? $privacyContexts : array(self::NULL_PC);
 	        		$tagsToAdd [$tag] = $missingPrivacyContexts;
 	        	}
 	        }
