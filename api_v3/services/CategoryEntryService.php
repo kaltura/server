4bc249eec225a0fb75df07dc8ca996c5424ee0d5
Hila.Karimov <hila.karimov@kalturaadmins-MacBook-Air-3.local>
2016-11-11 17:39:19 -0500

    testing - fixes

--- api_v3/services/CategoryEntryService.php
+++ api_v3/services/CategoryEntryService.php
@@ -100,7 +100,8 @@ class CategoryEntryService extends KalturaBaseService
 		$dbCategoryEntry->setPartnerId($partnerId);
 		
 		$kuser = kCurrentContext::getCurrentKsKuser();
-		$dbCategoryEnry->setCreatorKuserId();
+		$dbCategoryEnry->setCreatorKuserId($kuser->getId());
+		$dbCategoryEnry->setCreatorPuserId($kuser->getPuserId());
 		
 		$dbCategoryEntry->save();
 		
