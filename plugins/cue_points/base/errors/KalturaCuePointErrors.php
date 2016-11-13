7bb745790ee3e04310b22985fd63d92365f04a0e
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2016-11-13 18:56:27 +0200

    Expose error codes in API XML descriptor

--- plugins/cue_points/base/errors/KalturaCuePointErrors.php
+++ plugins/cue_points/base/errors/KalturaCuePointErrors.php
@@ -15,9 +15,9 @@ class KalturaCuePointErrors extends KalturaErrors implements kCuePointErrors
 	
 	const CUE_POINT_SYSTEM_NAME_EXISTS = "CUE_POINT_SYSTEM_NAME_EXISTS;NAME,ID;Cue point system name already exists [@NAME@] with id [@ID@]";
 
-	const PARENT_CUE_POINT_NOT_FOUND = "PARENT_CUE_POINT_NOT_FOUND;ID; Parent cue point id [@ID@] not found";
+	const PARENT_CUE_POINT_NOT_FOUND = "PARENT_CUE_POINT_NOT_FOUND;ID;Parent cue point id [@ID@] not found";
 
-	const PARENT_CUE_POINT_DO_NOT_BELONG_TO_THE_SAME_ENTRY = "PARENT_CUE_POINT_DO_NOT_BELONG_TO_THE_SAME_ENTRY;Parent cue point does not belong to the same entry";
+	const PARENT_CUE_POINT_DO_NOT_BELONG_TO_THE_SAME_ENTRY = "PARENT_CUE_POINT_DO_NOT_BELONG_TO_THE_SAME_ENTRY;;Parent cue point does not belong to the same entry";
 	
 	const USER_KS_CANNOT_LIST_RELATED_CUE_POINTS = "USER_KS_CANNOT_LIST_RELATED_CUE_POINTS;FILTER;Cue-points should not be listed [@FILTER@] as related-objects with unprivileged user ks";
 
--- plugins/cue_points/quiz/errors/KalturaQuizErrors.php
+++ plugins/cue_points/quiz/errors/KalturaQuizErrors.php
@@ -6,14 +6,14 @@
 
 class KalturaQuizErrors extends KalturaErrors
 {
-	const PROVIDED_ENTRY_IS_ALREADY_A_QUIZ = "PROVIDED_ENTRY_IS_ALREADY_A_QUIZ;ENTRY_ID; provided entry [@ENTRY_ID@] is already a quiz";
-	const PROVIDED_ENTRY_IS_NOT_A_QUIZ = "PROVIDED_ENTRY_IS_NOT_A_QUIZ;ENTRY_ID; provided entry [@ENTRY_ID@] is not a quiz";
-	const PARENT_ID_IS_MISSING = "PARENT_ID_IS_MISSING, parent ID is missing";
-	const WRONG_PARENT_TYPE = "WRONG_PARENT_TYPE;ENTRY_ID; Parent cue point id [@ENTRY_ID@] has the wrong type";
-	const ANSWER_UPDATE_IS_NOT_ALLOWED = "ANSWER_UPDATE_IS_NOT_ALLOWED;ENTRY_ID; Answer update is not allowed for quiz [@ENTRY_ID@]";
-	const USER_ENTRY_QUIZ_ALREADY_SUBMITTED = 'USER_ENTRY_QUIZ_ALREADY_SUBMITTED;The user-entry-quiz id is already submitted, answers cannot be added/updated';
-	const ENTRY_ID_NOT_GIVEN = 'ENTRY_ID_NOT_GIVEN; No entry id given';
-	const NO_SUCH_FILE_TYPE = 'NO_SUCH_FILE_TYPE; Document cannot be provided. No such file type';
-	const QUIZ_CANNOT_BE_DOWNLOAD = 'QUIZ_CANNOT_BE_DOWNLOAD; Quiz cannot be download';
+	const PROVIDED_ENTRY_IS_ALREADY_A_QUIZ = "PROVIDED_ENTRY_IS_ALREADY_A_QUIZ;ENTRY_ID;Provided entry [@ENTRY_ID@] is already a quiz";
+	const PROVIDED_ENTRY_IS_NOT_A_QUIZ = "PROVIDED_ENTRY_IS_NOT_A_QUIZ;ENTRY_ID;Provided entry [@ENTRY_ID@] is not a quiz";
+	const PARENT_ID_IS_MISSING = "PARENT_ID_IS_MISSING;;Parent ID is missing";
+	const WRONG_PARENT_TYPE = "WRONG_PARENT_TYPE;ENTRY_ID;Parent cue point id [@ENTRY_ID@] has the wrong type";
+	const ANSWER_UPDATE_IS_NOT_ALLOWED = "ANSWER_UPDATE_IS_NOT_ALLOWED;ENTRY_ID;Answer update is not allowed for quiz [@ENTRY_ID@]";
+	const USER_ENTRY_QUIZ_ALREADY_SUBMITTED = 'USER_ENTRY_QUIZ_ALREADY_SUBMITTED;;The user-entry-quiz id is already submitted, answers cannot be added/updated';
+	const ENTRY_ID_NOT_GIVEN = 'ENTRY_ID_NOT_GIVEN;;No entry id given';
+	const NO_SUCH_FILE_TYPE = 'NO_SUCH_FILE_TYPE;;Document cannot be provided. No such file type';
+	const QUIZ_CANNOT_BE_DOWNLOAD = 'QUIZ_CANNOT_BE_DOWNLOAD;;Quiz cannot be download';
 	const QUIZ_USER_ENTRY_ALREADY_EXISTS = 'QUIZ_USER_ENTRY_ALREADY_EXISTS;ENTRY_ID;A quiz user-entry for the given user-id and entry-id [@ENTRY_ID@] already exists, cannot create duplicate';
 }
