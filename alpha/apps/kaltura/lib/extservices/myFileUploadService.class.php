72cd4c223173fca2a701f5d0d9da5380fef8cf08
Liron Shpinerman <liron.shpinerman@IL-TEMP-LAP.Kaltura.com>
2016-12-06 15:32:08 +0200

    minor fix

--- alpha/apps/kaltura/lib/extservices/myFileUploadService.class.php
+++ alpha/apps/kaltura/lib/extservices/myFileUploadService.class.php
@@ -6,26 +6,26 @@
 class myFileUploadService extends myBaseMediaSource implements IMediaSource
 {
 	const MAX_FILES = 100;
-
+	
 	private static $NEED_MEDIA_INFO = "0";
-
+		
 	protected $source_type ="upload";
-	protected $supported_media_types = 7 ; // bitwise for all 3
+	protected $supported_media_types = 7 ; // bitwise for all 3 
 	protected $source_name = "File Upload";
 //	protected $auth_method = self::AUTH_METHOD_NONE;
-	protected $search_in_user = false;
+	protected $search_in_user = false; 
 	protected $logo = ""; //"http://ccmixter.org/mixter-files/ccdj.gif";
 	protected $module_url = "UploadView.swf";
 	protected $id = entry::ENTRY_MEDIA_SOURCE_FILE;
 
 	// empty implementation for search  methods
-	public function getMediaInfo( $media_type ,$objectId) {}
+	public function getMediaInfo( $media_type ,$objectId) {} 
 
 	public function searchMedia( $media_type , $searchText, $page, $pageSize, $authData = null, $extraData = null) {}
-
+	
 	public function getAuthData( $kuserId, $userName, $password, $token) {}
-
-	public function getConfigCustomData()
+	
+	public function getConfigCustomData() 
 	{
 		return array("maxFiles" => self::MAX_FILES );
 	}
@@ -52,7 +52,6 @@ class myFileUploadService extends myBaseMediaSource implements IMediaSource
 	static public function getMediaTypeFromFileExt ( $ext )
 	{
 		// notice that video is checked first since it has precedence over audio (both may have the same ext.)
-
 		$ext = strtolower($ext);
 	    if (in_array($ext, kConf::get("video_file_ext")))
 	    {
@@ -66,10 +65,10 @@ class myFileUploadService extends myBaseMediaSource implements IMediaSource
 		{
 			return entry::ENTRY_MEDIA_TYPE_AUDIO;
 		}
-
+		
 		return entry::ENTRY_MEDIA_TYPE_AUTOMATIC;
 	}
-
+	
 	protected function getFileExt ( $type )
 	{
 		if ( $type == self::SUPPORT_MEDIA_TYPE_VIDEO )
