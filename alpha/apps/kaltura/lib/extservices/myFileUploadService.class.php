e15a27f1d8ebc91f8d8572bb1602490e02eddb1a
Liron Shpinerman <liron.shpinerman@IL-TEMP-LAP.Kaltura.com>
2016-12-06 15:22:02 +0200

    check curl response existance

--- alpha/apps/kaltura/lib/extservices/myFileUploadService.class.php
+++ alpha/apps/kaltura/lib/extservices/myFileUploadService.class.php
@@ -6,37 +6,37 @@
 class myFileUploadService extends myBaseMediaSource implements IMediaSource
 {
 	const MAX_FILES = 100;
-	
+
 	private static $NEED_MEDIA_INFO = "0";
-	
+
 	protected $source_type ="upload";
-	protected $supported_media_types = 7 ; // bitwise for all 3 
+	protected $supported_media_types = 7 ; // bitwise for all 3
 	protected $source_name = "File Upload";
 //	protected $auth_method = self::AUTH_METHOD_NONE;
-	protected $search_in_user = false; 
+	protected $search_in_user = false;
 	protected $logo = ""; //"http://ccmixter.org/mixter-files/ccdj.gif";
 	protected $module_url = "UploadView.swf";
 	protected $id = entry::ENTRY_MEDIA_SOURCE_FILE;
 
 	// empty implementation for search  methods
-	public function getMediaInfo( $media_type ,$objectId) {} 
+	public function getMediaInfo( $media_type ,$objectId) {}
 
 	public function searchMedia( $media_type , $searchText, $page, $pageSize, $authData = null, $extraData = null) {}
-	
+
 	public function getAuthData( $kuserId, $userName, $password, $token) {}
-	
-	public function getConfigCustomData() 
+
+	public function getConfigCustomData()
 	{
 		return array("maxFiles" => self::MAX_FILES );
 	}
-	
+
 	public static function getExtensionByContentType($url)
 	{
 		$curlWrapper = new KCurlWrapper();
 		$curlHeaderResponse = $curlWrapper->getHeader($url, false);
 		$curlWrapper->close();
 
-		$headerContentType = isset($curlHeaderResponse->headers["content-type"]) ? strtolower($curlHeaderResponse->headers["content-type"]) : null;
+		$headerContentType = $curlHeaderResponse && isset($curlHeaderResponse->headers["content-type"]) ? strtolower($curlHeaderResponse->headers["content-type"]) : null;
 		$contentTypes = kConf::get("video_curl_content_type", 'base', array());
 
 		if($headerContentType && isset($contentTypes[$headerContentType]))
@@ -45,14 +45,14 @@ class myFileUploadService extends myBaseMediaSource implements IMediaSource
 			KalturaLog::debug("extension - $ext");
 			return $ext;
 		}
-		
+
 		return null;
 	}
 
 	static public function getMediaTypeFromFileExt ( $ext )
 	{
 		// notice that video is checked first since it has precedence over audio (both may have the same ext.)
-		
+
 		$ext = strtolower($ext);
 	    if (in_array($ext, kConf::get("video_file_ext")))
 	    {
@@ -66,10 +66,10 @@ class myFileUploadService extends myBaseMediaSource implements IMediaSource
 		{
 			return entry::ENTRY_MEDIA_TYPE_AUDIO;
 		}
-		
+
 		return entry::ENTRY_MEDIA_TYPE_AUTOMATIC;
 	}
-	
+
 	protected function getFileExt ( $type )
 	{
 		if ( $type == self::SUPPORT_MEDIA_TYPE_VIDEO )
