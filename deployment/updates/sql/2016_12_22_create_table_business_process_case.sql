eaa112a36b7c2f7d564f8dab7f4cd923ae1c25d4
Hila.Karimov <hila.karimov@gmail.com>
2016-12-23 18:05:24 -0500

    update table into deployments

---
+++ deployment/updates/sql/2016_12_22_create_table_business_process_case.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `business_process_case`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`partner_id` INTEGER  NOT NULL,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	`case_id` INTEGER,
+	`process_id` VARCHAR(255),
+	`template_id` INTEGER,
+	`server_id` INTEGER,
+	`object_id` VARCHAR(20),
+	`object_type` INTEGER,
+	PRIMARY KEY (`id`),
+	KEY `partner_id`(`partner_id`),
+	KEY `object_id_and_type`(`object_type`, `object_id`),
+	KEY `server_id_and_process_id`(`server_id`, `process_id`, `object_type`, `object_id`)
+)Type=InnoDB DEFAULT CHARSET=utf8;
