8e99a3320952691db92da9b5e7d4c94928d63a90
Moshe Maor <moshe.maor@kaltura.com>
2017-01-11 10:38:13 +0200

    Revert "Revert "Revert "fix thumbnail generation when offset in bigger then duration"""

--- batch/batches/CaptureThumb/KAsyncCaptureThumb.class.php
+++ batch/batches/CaptureThumb/KAsyncCaptureThumb.class.php
@@ -119,8 +119,7 @@ class KAsyncCaptureThumb extends KJobHandlerWorker
 				
 				// generates the thumbnail
 				$thumbMaker = new KFFMpegThumbnailMaker($mediaFile, $capturePath, self::$taskConfig->params->FFMpegCmd);
-				$videoOffset = max(0 ,min($thumbParamsOutput->videoOffset, $mediaInfoVidDur-1));
-				$created = $thumbMaker->createThumnail($videoOffset, $mediaInfoWidth, $mediaInfoHeight, null ,null, $mediaInfoDar, $mediaInfoVidDur);
+				$created = $thumbMaker->createThumnail($thumbParamsOutput->videoOffset, $mediaInfoWidth, $mediaInfoHeight, null ,null, $mediaInfoDar, $mediaInfoVidDur);
 				if(!$created || !file_exists($capturePath))
 					return $this->closeJob($job, KalturaBatchJobErrorTypes::APP, KalturaBatchJobAppErrors::THUMBNAIL_NOT_CREATED, "Thumbnail not created", KalturaBatchJobStatus::FAILED);
 				
