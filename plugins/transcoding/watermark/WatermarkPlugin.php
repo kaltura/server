ba1d29f2210fdf11b121a729a29c63ac9fc483e2
yossipapi <yossi.papiashvili@kaltura.com>
2017-01-25 15:15:13 +0200

    Support captions and watermark info copy to replaing entry

--- plugins/transcoding/watermark/WatermarkPlugin.php
+++ plugins/transcoding/watermark/WatermarkPlugin.php
@@ -44,34 +44,15 @@ class WatermarkPlugin extends KalturaPlugin implements IKalturaPending, IKaltura
 	public function adjustAssetParams($entryId, array &$flavors)
 	{
 		$entry = entryPeer::retrieveByPK($entryId);
-		if(!isset($entry)){
+		if(!isset($entry))
+		{
 			KalturaLog::warning("Bad entry id ($entryId).");
 			return;
 		}
-
-		$partnerId = $entry->getPartnerId();
-		$profile = MetadataProfilePeer::retrieveBySystemName(self::TRANSCODING_METADATA_PROF_SYSNAME,$partnerId);
-		if(!isset($profile)){
-			KalturaLog::log("No Transcoding Metadata Profile (sysName:".self::TRANSCODING_METADATA_PROF_SYSNAME.", partner:$partnerId). Nothing to adjust");
-			return;
-		}
-
-		$metadata = MetadataPeer::retrieveByObject($profile->getId(), MetadataObjectType::ENTRY, $entryId);
-		if(!isset($metadata)){
-			KalturaLog::log("No Metadata for entry($entryId), metadata profile (id:".$profile->getId()."). Nothing to adjust");
-			return;
-		}
-
-		KalturaLog::log("Entry ($entryId) has following metadata fields:".print_r($metadata,1));
 		
-		// Retrieve the associated XML file
-		$key = $metadata->getSyncKey(Metadata::FILE_SYNC_METADATA_DATA);
-		if(!isset($key)){
-			KalturaLog::log("Entry($entryId) metadata object misses file sync key! Nothing to adjust");
-			return;
-		}
-		$xmlStr = kFileSyncUtils::file_get_contents($key, true, false);
-		if(!isset($xmlStr)){
+		$xmlStr = kWatermarkManager::getWatermarkMetadataXml($entry);
+		if(!isset($xmlStr))
+		{
 			KalturaLog::log("Entry($entryId) metadata object misses valid file sync! Nothing to adjust");
 			return;
 		}
@@ -90,12 +71,15 @@ class WatermarkPlugin extends KalturaPlugin implements IKalturaPending, IKaltura
 		$xml = new SimpleXMLElement($xmlStr);
 		$fldName = self::TRANSCODING_METADATA_WATERMMARK_SETTINGS;
 
-		if(isset($xml->$fldName)) {
+		if(isset($xml->$fldName)) 
+		{
 			$watermarkSettingsStr =(string)$xml->$fldName;
 			KalturaLog::log("Found custom metadata - $fldName($watermarkSettingsStr)");
-			if(isset($watermarkSettingsStr)) {
+			if(isset($watermarkSettingsStr)) 
+			{
 				$watermarkSettings = json_decode($watermarkSettingsStr);
-				if(!is_array($watermarkSettings)) {
+				if(!is_array($watermarkSettings)) 
+				{
 					$watermarkSettings = array($watermarkSettings);
 				}
 				KalturaLog::log("WM($fldName) object:".serialize($watermarkSettings));
@@ -110,14 +94,17 @@ class WatermarkPlugin extends KalturaPlugin implements IKalturaPending, IKaltura
 		 */
 		$wmTmp = null;
 		$fldName = self::TRANSCODING_METADATA_WATERMMARK_IMAGE_ENTRY;
-		if(isset($xml->$fldName)) {
+		if(isset($xml->$fldName)) 
+		{
 			$wmTmp->imageEntry =(string)$xml->$fldName;
 			KalturaLog::log("Found custom metadata - $fldName($wmTmp->imageEntry)");
 		}
-		else {
+		else 
+		{
 			KalturaLog::log("No custom metadata - $fldName");
 			$fldName = self::TRANSCODING_METADATA_WATERMMARK_IMAGE_URL;
-			if(isset($xml->$fldName)) {
+			if(isset($xml->$fldName)) 
+			{
 				$fldVal = (string)$xml->$fldName;
 				$wmTmp->url =(string)$xml->$fldName;
 				KalturaLog::log("Found custom metadata - $fldName($wmTmp->url)");
@@ -130,7 +117,7 @@ class WatermarkPlugin extends KalturaPlugin implements IKalturaPending, IKaltura
 		 * Merge the imageEntry/imageUrl values into previously aquired 'full' WM settings (if provided).
 		 */
 		if(isset($wmTmp))
-			$watermarkSettings = self::adjustWatermarSettings($watermarkSettings, $wmTmp);
+			$watermarkSettings = kWatermarkManager::adjustWatermarkSettings($watermarkSettings, $wmTmp);
 		KalturaLog::log("Custom meta data WM settings:".serialize($watermarkSettings));
 
 		/*
@@ -138,12 +125,16 @@ class WatermarkPlugin extends KalturaPlugin implements IKalturaPending, IKaltura
 		 * If none - leave
 		 */
 		{
-			foreach($watermarkSettings as $wmI=>$wmTmp){
-				if(isset($wmTmp)){
+			$fldCnt = 0;
+			foreach($watermarkSettings as $wmI=>$wmTmp)
+			{
+				if(isset($wmTmp))
+				{
 					$fldCnt+= count((array)$wmTmp);
 				}
 			}
-			if($fldCnt==0){
+			if($fldCnt==0)
+			{
 				KalturaLog::log("No WM custom data to merge");
 				return;
 			}
@@ -153,18 +144,20 @@ class WatermarkPlugin extends KalturaPlugin implements IKalturaPending, IKaltura
 		 * Loop through the flavor params to update the WM settings,
 		 * if it is required.
 		 */
-		foreach($flavors as $k=>$flavor) {
+		foreach($flavors as $k=>$flavor) 
+		{
 			KalturaLog::log("Processing flavor id:".$flavor->getId());
 			$wmDataFixed = null;
 			$wmPredefined = null;
 			$wmPredefinedStr = $flavor->getWatermarkData();
-			if(!(isset($wmPredefinedStr) && ($wmPredefined=json_decode($wmPredefinedStr))!=null)){
+			if(!(isset($wmPredefinedStr) && ($wmPredefined=json_decode($wmPredefinedStr))!=null))
+			{
 				KalturaLog::log("No WM data for flavor:".$flavor->getId());
 				continue;
 			}
 			KalturaLog::log("wmPredefined : count(".count($wmPredefined).")-".serialize($wmPredefined));
 
-			$wmDataFixed = self::adjustWatermarSettings($wmPredefined, $watermarkSettings);
+			$wmDataFixed = kWatermarkManager::adjustWatermarkSettings($wmPredefined, $watermarkSettings);
 
 			/*
 			 * The 'full' WM settings in the custom metadata overides any exitings WM settings 
@@ -174,51 +167,5 @@ class WatermarkPlugin extends KalturaPlugin implements IKalturaPending, IKaltura
 			$flavors[$k]= $flavor;
 			KalturaLog::log("Update flavor (".$flavor->getId().") WM to: $wmJsonStr");
 		}
-	}
-
-	/**
-	 * 
-	 * @param unknown_type $watermarkData
-	 * @param unknown_type $watermarkToMerge
-	 */
-	protected static function adjustWatermarSettings($watermarkData, $watermarkToMerge)
-	{
-		KalturaLog::log("Merge WM (".serialize($watermarkToMerge).") into (".serialize($watermarkData).")");
-		if(is_array($watermarkData))
-			$watermarkDataArr = $watermarkData;
-		else 
-			$watermarkDataArr = array($watermarkData);
-		
-		if(is_array($watermarkToMerge))
-			$watermarkToMergeArr = $watermarkToMerge;
-		else 
-			$watermarkToMergeArr = array($watermarkToMerge);
-		
-		foreach($watermarkToMergeArr as $wmI=>$watermarkToMerge){
-			KalturaLog::log("Merging WM:$wmI");
-			if(!array_key_exists($wmI, $watermarkDataArr)){
-				$watermarkDataArr[$wmI] = $watermarkToMerge;
-				KalturaLog::log("Added object ($wmI)-".serialize($watermarkToMerge));
-				continue;
-			}
-
-			foreach($watermarkToMerge as $fieldName=>$fieldValue){
-				$watermarkDataArr[$wmI]->$fieldName = $fieldValue;
-				KalturaLog::log("set($fieldName):".$fieldValue);
-				switch($fieldName){
-				case "imageEntry":
-					KalturaLog::log("unset(url):".$watermarkDataArr[$wmI]->url);
-					unset($watermarkDataArr[$wmI]->url);
-					break;
-				case  "url":
-					KalturaLog::log("unset(imageEntry):".$watermarkDataArr[$wmI]->imageEntry);
-					unset($watermarkDataArr[$wmI]->imageEntry);
-					break;
-				}
-			}
-		}
-		
-		KalturaLog::log("Merged WM (".serialize($watermarkDataArr).")");
-		return $watermarkDataArr;
 	}
 }
---
+++ plugins/transcoding/watermark/lib/kWatermarkFlowManager.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @package plugins.watermark
+ * @subpackage lib
+ */
+class kWatermarkFlowManager implements kObjectAddedEventConsumer
+{
+	/* (non-PHPdoc)
+ 	* @see kObjectAddedEventConsumer::shouldConsumeAddedEvent()
+ 	*/
+	public function shouldConsumeAddedEvent(BaseObject $object)
+	{
+		if($object instanceof entry && $object->getReplacedEntryId() && !in_array($object->getReplacementStatus(), array(entryReplacementStatus::NONE, entryReplacementStatus::FAILED)))
+			return true;
+		
+		return false;
+	}
+	
+	/* (non-PHPdoc)
+ 	* @see kObjectAddedEventConsumer::objectAdded()
+ 	*/
+	public function objectAdded(BaseObject $object, BatchJob $raisedJob = null)
+	{
+		if($object instanceof entry && $object->getReplacedEntryId() && !in_array($object->getReplacementStatus(), array(entryReplacementStatus::NONE, entryReplacementStatus::FAILED)))
+		{
+			$this->copyWatermarkData($object);
+		}
+		
+		return true;
+	}
+	
+	protected function copyWatermarkData(entry $entry)
+	{
+		$originalEntryId = $entry->getReplacedEntryId();
+		$originalEntry = entryPeer::retrieveByPK($originalEntryId);
+		if(!$originalEntry)
+		{
+			KalturaLog::debug("Original entry with id [$originalEntryId], not found");
+			return true;
+		}
+		
+		KalturaLog::debug("Original entry id $originalEntryId");
+		KalturaLog::debug("Replacing entry id [{$entry->getId()}]");
+		
+		$watermarkMetadata = kWatermarkManager::getWatermarkMetadata($originalEntry);
+		if(!$watermarkMetadata)
+		{
+			KalturaLog::debug("Watermark data not found for entry [$originalEntryId]");
+			return true;
+		}
+		
+		kWatermarkManager::copyWatermarkData($watermarkMetadata, $originalEntry, $entry);
+	}
+}
