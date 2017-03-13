526479f8a2298194dbe596cf03b5d777c300d7b9
david-winder-kaltura <david.winder@kaltura.com>
2017-03-13 15:12:46 +0200

    add prekill to frop folder batch PLAT-7135 #time 3h

--- plugins/drop_folder/batch/DropFolderWatcher/KAsyncDropFolderWatcherExe.php
+++ plugins/drop_folder/batch/DropFolderWatcher/KAsyncDropFolderWatcherExe.php
@@ -4,6 +4,15 @@ ini_set ( "memory_limit", "256M" );
 
 require_once(__DIR__ . "/../../../../batch/bootstrap.php");
 
+function sigint()
+{
+	global $instance;
+	$instance->preKill();
+	exit;
+}
+pcntl_signal(SIGINT, 'sigint');
+pcntl_signal(SIGTERM, 'sigint');
+
 /**
  * Executes the KAsyncDropFolderWatcher
  * 
