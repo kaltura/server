8b0547c5b4aca53cd1cd69041dc559f703979bfb
david-winder-kaltura <david.winder@kaltura.com>
2017-03-13 16:02:18 +0200

    add comments

--- plugins/drop_folder/batch/DropFolderWatcher/KAsyncDropFolderWatcherExe.php
+++ plugins/drop_folder/batch/DropFolderWatcher/KAsyncDropFolderWatcherExe.php
@@ -4,15 +4,16 @@ ini_set ( "memory_limit", "256M" );
 
 require_once(__DIR__ . "/../../../../batch/bootstrap.php");
 
-function sigint()
+//will free the exclusive drop folder when getting kill SIGNAL
+function gracefullyKill()
 {
-	global $instance;
+	global $instance; //as the KAsyncDropFolderWatcher
 	$instance->preKill();
 	exit;
 }
 declare(ticks = 1);
-pcntl_signal(SIGINT, 'sigint');
-pcntl_signal(SIGTERM, 'sigint');
+pcntl_signal(SIGINT, 'gracefullyKill');
+pcntl_signal(SIGTERM, 'gracefullyKill');
 
 /**
  * Executes the KAsyncDropFolderWatcher
