eb6e3ac9c68ac22bcd31b1fe27c2464038e71567
Hila.Karimov <hila.karimov@gmail.com>
2017-04-12 18:03:32 -0400

    Add missing method

--- api_v3/lib/types/delivery/KalturaUrlTokenizerChinaCache.php
+++ api_v3/lib/types/delivery/KalturaUrlTokenizerChinaCache.php
@@ -21,6 +21,11 @@ class KalturaUrlTokenizerChinaCache extends KalturaUrlTokenizer {
 			"keyId",
 	);
 	
+	public function getMapBetweenObjects ( )
+	{
+		return array_merge ( parent::getMapBetweenObjects() , self::$map_between_objects );
+	}
+	
 	public function toObject($dbObject = null, $skip = array())
 	{
 		if (is_null($dbObject))
