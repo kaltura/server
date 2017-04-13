e2211c7b1673b67aabfcf5aea838621ddf294025
Hila.Karimov <hila.karimov@gmail.com>
2017-04-13 15:09:51 -0400

    changes after testing

--- admin_console/forms/Delivery/UrlTokenizerChinaCache.php
+++ admin_console/forms/Delivery/UrlTokenizerChinaCache.php
@@ -9,14 +9,11 @@ class Form_Delivery_UrlTokenizerChinaCache extends Form_Delivery_DeliveryProfile
 	{
 		parent::init();
 		
-		$type = new Kaltura_Form_Element_EnumSelect('type', array('enum' => 'Kaltura_Client_Enum_ChinaCacheAlgorithmType'));
-		$this->addElement('text', 'algorithm_id', array(
-			'label'			=> 'Algorithm ID:',
-			'filters'		=> array('StringTrim'),
-			'multiOptions'  => array($type),
-		));		
+		$type = new Kaltura_Form_Element_EnumSelect('algorithmId', array('enum' => 'Kaltura_Client_Enum_ChinaCacheAlgorithmType'));
+		$type->setLabel('Algorithm ID:');
+		$this->addElements(array($type));	
 		
-		$this->addElement('text', 'key_id', array(
+		$this->addElement('text', 'keyId', array(
 				'label'			=> 'Key ID:',
 				'validators'	=> array('Int'),
 		));
