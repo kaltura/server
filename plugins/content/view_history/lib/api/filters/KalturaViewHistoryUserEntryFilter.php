e369e8a06d8057b77aa1757312a85b14d7e5cfdf
Hila.Karimov <hila.karimov@gmail.com>
2017-04-26 01:31:29 -0400

    Fix warning

--- plugins/content/view_history/lib/api/filters/KalturaViewHistoryUserEntryFilter.php
+++ plugins/content/view_history/lib/api/filters/KalturaViewHistoryUserEntryFilter.php
@@ -8,12 +8,12 @@ class KalturaViewHistoryUserEntryFilter extends KalturaUserEntryFilter
 	public function getListResponse(KalturaFilterPager $pager, KalturaDetachedResponseProfile $responseProfile = null)
 	{
 		$this->typeEqual = ViewHistoryPlugin::getApiValue(ViewHistoryUserEntryType::VIEW_HISTORY);
-		$response = parent::getListResponse($pager, $responseProfile, $privacyContexts);
+		$response = parent::getListResponse($pager, $responseProfile);
 		
 		return $response;
 	}
 	
-	protected function modifyCriteria ($c)
+	protected function modifyCriteria (Criteria $c)
 	{
 		if (kEntitlementUtils::getEntitlementEnforcement())
 		{
