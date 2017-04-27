077eaf348334ca030112d306e1f6998eab07820e
david-winder-kaltura <david.winder@kaltura.com>
2017-04-27 10:19:07 +0300

    fix fatal error

--- plugins/scheduled_task/admin/MediaRepurposingUtils.php
+++ plugins/scheduled_task/admin/MediaRepurposingUtils.php
@@ -87,7 +87,6 @@ class MediaRepurposingUtils
 		$result = $scheduledTaskPlugin->scheduledTaskProfile->add($mr);
 
 		$mrId = $result->id;
-		$mr = new Kaltura_Client_ScheduledTask_Type_ScheduledTaskProfile();
 		$mr->description = self::handleSts($partnerId, $mrId, $name, $filterTypeEngine, $filter, $taskArray, $maxEntriesAllowed);
 		$mr->objectFilter->advancedSearch->items[] = self::createMrConditionFilter($mrId);
 		$scheduledTaskPlugin = self::getPluginByName('Kaltura_Client_ScheduledTask_Plugin', $partnerId);
