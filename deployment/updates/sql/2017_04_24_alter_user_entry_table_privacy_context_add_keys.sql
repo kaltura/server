fe299ff5f7e3c0abc6425db7a042f151b00c39ad
hilak <hila.karimov@kaltura.com>
2017-05-15 13:10:09 -0400

    Update 2017_04_24_alter_user_entry_table_privacy_context_add_keys.sql

--- deployment/updates/sql/2017_04_24_alter_user_entry_table_privacy_context_add_keys.sql
+++ deployment/updates/sql/2017_04_24_alter_user_entry_table_privacy_context_add_keys.sql
@@ -3,6 +3,6 @@ ADD privacy_context VARCHAR(255) AFTER extended_status,
 DROP KEY `entry_id`,
 DROP KEY `kuser_id_updated_at`,
 DROP KEY `kuser_id_extended_status_updated_at`,
-ADD KEY `entry_id_kuser_id` (`entry_id`, `kuser_id`, `privacy_context`),
+ADD KEY (`entry_id`, `kuser_id`, `privacy_context`),
 ADD KEY `kuser_id_updated_at` (`kuser_id`,`updated_at`),
 ADD KEY `kuser_id_extended_status_updated_at` (`kuser_id`, `extended_status`, `updated_at`, `privacy_context`);
