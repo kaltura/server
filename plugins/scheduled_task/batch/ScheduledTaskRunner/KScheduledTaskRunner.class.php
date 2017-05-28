faba999e3643dee8b1663a9d3da60eb296d72fde
david-winder-kaltura <david.winder@kaltura.com>
2017-05-28 17:31:08 +0300

    do not update metadata when deleting entry

--- plugins/scheduled_task/batch/ScheduledTaskRunner/KScheduledTaskRunner.class.php
+++ plugins/scheduled_task/batch/ScheduledTaskRunner/KScheduledTaskRunner.class.php
@@ -433,6 +433,8 @@ class KScheduledTaskRunner extends KPeriodicWorker
 	}
 
 	private function updateMetadataStatusForMediaRepurposing(KalturaScheduledTaskProfile $profile, $object, $error) {
+		if (self::getMediaRepurposingProfileTaskType($profile) == ObjectTaskType::DELETE_ENTRY)
+			return null; //can not update metadata for deleted entry
 		$metadataProfileId = self::getMrAdvancedSearchFilter($profile)->metadataProfileId;
 
 		self::impersonate($object->partnerId);
@@ -454,8 +456,6 @@ class KScheduledTaskRunner extends KPeriodicWorker
 				$result = $metadataPlugin->metadata->add($metadataProfileId, KalturaMetadataObjectType::ENTRY,$object->id, $xml->asXML());
 
 		} catch (Exception $e) {
-			if (self::getMediaRepurposingProfileTaskType($profile) == ObjectTaskType::DELETE_ENTRY)
-				return null; //delete entry should get exception when update metadata for deleted entry
 			throw new KalturaException("Error in metadata for entry [$object->id] with ". $e->getMessage());
 		}
 		
