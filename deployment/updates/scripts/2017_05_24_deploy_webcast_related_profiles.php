224b36404062167d779c1a873a793f5a51f40a16
yossipapi <yossi.papiashvili@kaltura.com>
2017-06-11 15:16:31 +0300

    PLAT-7535: typo fixes

--- deployment/updates/scripts/2017_05_24_deploy_webcast_related_profiles.php
+++ deployment/updates/scripts/2017_05_24_deploy_webcast_related_profiles.php
@@ -10,8 +10,12 @@ require_once (__DIR__ . '/../../bootstrap.php');
 $script = realpath(dirname(__FILE__) . '/../../../') . '/tests/standAloneClient/exec.php';
 
 $config = realpath(dirname(__FILE__)) . '/xml/responseProfiles/polls_response_profile.xml';
+if(!file_exists($config))
+	KalturaLog::err("Missing file [$config] will not deploy");
 passthru("php $script $config");
 
 $config = realpath(dirname(__FILE__)) . '/xml/responseProfiles/qna_response_profiles.xml';
+if(!file_exists($config))
+	KalturaLog::err("Missing file [$config] will not deploy");
 passthru("php $script $config");
 
