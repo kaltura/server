9b7a24512613ccdfe4fa78e5f5c2c72ac711e0e6
yossipapi <yossi.papiashvili@kaltura.com>
2017-06-13 18:59:10 +0300

    Remove clients generation from install webcast script since this will be handled by installign RPM

--- deployment/base/scripts/installWebcast.php
+++ deployment/base/scripts/installWebcast.php
@@ -1,15 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__) . '/../../bootstrap.php');
-myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_MASTER;
-
-$pluginsUpdated = installRequirePlugins();
-if($pluginsUpdated)
-{
-	installPlugins();
-	generateClients();
-}
-
+checkMandatoryPluginsEnabled();
 deployWebcastPushNotifications();
 
 function deployWebcastPushNotifications()
@@ -27,68 +18,36 @@ function deployWebcastPushNotifications()
 		return;
 	}
 	
-	passthru("php $script $config");	
-	passthru("php $script $config");
-	passthru("php $script $config");
-	passthru("php $script $config");
-}
-
-function generateClients()
-{
-	KalturaLog::debug("Generating updated clientLibs");
-	$script = realpath(dirname(__FILE__) . "/../../../generator/generate.php");
-	passthru("php $script");
-	KalturaLog::debug("Done Generating updated clientLibs");
-}
-
-function installPlugins()
-{
-	KalturaLog::debug("Running Install plugins");
-	$script = realpath(dirname(__FILE__) . "/../scripts/installPlugins.php");
-	passthru("php $script");
-	KalturaLog::debug("Done Installing new plugins");
+	passthru("php $script $codeQnaConfig");	
+	passthru("php $script $publicQnaConfig");
+	passthru("php $script $userQnaConfig");
+	passthru("php $script $pollsConfig");
 }
 
 /**
  * install required plugisn needed fot webcast to work
  * @return bool If plugis file updated or not
  */
-function installRequirePlugins()
+function checkMandatoryPluginsEnabled()
 {
 	$requiredPlugins = array("PushNotification", "Queue", "RabbitMQ");
-	$pluginsFileUpdated = false;
-	
 	$pluginsFilePath = realpath(dirname(__FILE__) . "/../../../configurations/plugins.ini");
-	KalturaLog::debug("Loading Plugins config from [$pluginsFilePath]");
+	echo ("Loading Plugins config from [$pluginsFilePath]" . PHP_EOL);
 	
 	$pluginsData = file_get_contents($pluginsFilePath);
-	echo "pluginsData = " . print_r($pluginsData, true) . "\n"; 
 	foreach ($requiredPlugins as $requiredPlugin)
 	{
 		//check if plugin exists in file but is disabled
 		if(strpos($pluginsData, ";".$requiredPlugin) !== false)
 		{
-			KalturaLog::debug("[$requiredPlugin] disabled enabling it");
-			$pluginsData = str_replace(";".$requiredPlugin, $requiredPlugin, $pluginsData);
-			$pluginsFileUpdated = true;
-			continue;
+			echo ("[$requiredPlugin] is disabled, aborting execution" . PHP_EOL);
+			exit(-2);
 		}
 		
-		//check if plugin already enbaled
-		if(strpos($pluginsData, $requiredPlugin) !== false)
+		if(strpos($pluginsData, $requiredPlugin) === false)
 		{
-			KalturaLog::debug("[$requiredPlugin] already enabled");
-			continue;
+			echo ("[$requiredPlugin] not found in plugins data, aborting execution" . PHP_EOL);
+			exit(-2);
 		}
-		
-		//Plugin does not exist in file add it
-		KalturaLog::debug("[$requiredPlugin] not foudn in plugins file, adding it");
-		$pluginsData .= "\n$requiredPlugin\n";
-		$pluginsFileUpdated = true;
 	}
-	
-	if($pluginsFileUpdated)
-		file_put_contents($pluginsFilePath, $pluginsData);
-	
-	return $pluginsFileUpdated;
 }
