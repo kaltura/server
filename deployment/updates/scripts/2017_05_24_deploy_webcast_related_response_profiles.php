48f3e47bcd547d0d86e8c7ab403806c665a1eee7
yossipapi <yossi.papiashvili@kaltura.com>
2017-06-14 10:00:13 +0300

    More clear deployment instrunctions File and some minor code changes

---
+++ deployment/updates/scripts/2017_05_24_deploy_webcast_related_response_profiles.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package deployment
+ *
+ * Deploy webcast defualt profiles & temlates
+ *
+ * No need to re-run after server code deploy
+ */
+require_once (__DIR__ . '/../../bootstrap.php');
+$script = realpath(dirname(__FILE__) . '/../../../') . '/tests/standAloneClient/exec.php';
+
+$config = realpath(dirname(__FILE__)) . '/xml/responseProfiles/polls_response_profile.xml';
+if(!file_exists($config))
+	KalturaLog::err("Missing file [$config] will not deploy");
+passthru("php $script $config");
+
+$config = realpath(dirname(__FILE__)) . '/xml/responseProfiles/qna_response_profiles.xml';
+if(!file_exists($config))
+	KalturaLog::err("Missing file [$config] will not deploy");
+passthru("php $script $config");
+
