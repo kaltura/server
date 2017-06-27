57b71e4b65ce46a54b841473d5a4fd0763c2275d
MosheMaorKaltura <Moshe.Maor@kaltura.com>
2017-06-26 21:25:36 -0400

    Add '.' to the allowed chars in JASONP callback

--- alpha/apps/kaltura/lib/kManifestRenderers.php
+++ alpha/apps/kaltura/lib/kManifestRenderers.php
@@ -951,7 +951,7 @@ class kJSONPManifestRenderer extends kJSONManifestRenderer
 	{
 		$callback = isset($_GET["callback"]) ? $_GET["callback"] : null;
 		// check for a valid callback, prevent xss
-		if (is_null($callback) || !preg_match("/^[0-9_a-zA-Z]*$/", $callback))
+		if (is_null($callback) || !preg_match("/^[0-9_a-zA-Z.]*$/", $callback))
 			die("Expecting \"callback\" parameter for jsonp format");
 		
 		$result = $this->buildFlavorsArray();
