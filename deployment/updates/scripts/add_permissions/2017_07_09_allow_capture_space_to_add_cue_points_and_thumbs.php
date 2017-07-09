3ff3eaa98b713e6827958b1a909fc53ad42cb7d3
Noam Arad <noam.arad@kaltura.com>
2017-07-09 10:22:45 +0300

    PLAT-7682: allow capture space to add cue points and thumb assets

---
+++ deployment/updates/scripts/add_permissions/2017_07_09_allow_capture_space_to_add_cue_points_and_thumbs.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.cuepoint.cuepoint.ini';
+passthru("php $script $config");
+
+$script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.thumbasset.ini';
+passthru("php $script $config");
