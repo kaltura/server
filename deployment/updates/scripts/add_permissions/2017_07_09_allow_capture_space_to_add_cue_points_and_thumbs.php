7d29a7409f2c6560f70fe976acfeb0fc27732730
Noam Arad <noam.arad@kaltura.com>
2017-07-10 11:20:02 +0300

    PLAT-7682: need to add insert permission for kalturathumbcuepoint

--- deployment/updates/scripts/add_permissions/2017_07_09_allow_capture_space_to_add_cue_points_and_thumbs.php
+++ deployment/updates/scripts/add_permissions/2017_07_09_allow_capture_space_to_add_cue_points_and_thumbs.php
@@ -2,9 +2,12 @@
 
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/service.cuepoint.cuepoint.ini';
 passthru("php $script $config");
 
-$script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/service.thumbasset.ini';
+passthru("php $script $config");
+
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/object.KalturaThumbCuePoint.ini';
 passthru("php $script $config");
