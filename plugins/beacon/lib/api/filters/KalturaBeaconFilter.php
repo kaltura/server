73ab4bf0d8073e8671f08cc9f566b0ba1ebd21e5
MosheMaorKaltura <Moshe.Maor@kaltura.com>
2017-07-15 02:47:21 +0300

    plugins/beacon/*

---
+++ plugins/beacon/lib/api/filters/KalturaBeaconFilter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: moshe.maor
+ * Date: 7/18/2017
+ * Time: 11:53 PM
+ */
+
+/**
+ * @package plugins.beacon
+ * @subpackage api.filters
+ */
+class KalturaBeaconFilter extends KalturaBeacon{
+    /**
+     * @var string
+     */
+    public $created_at;
+}
