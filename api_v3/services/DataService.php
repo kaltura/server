cb301db02547ee8d120d284d1295711b13712ad8
=inbal-ben-david <inbal.bendavid@kaltura.com>
2017-07-21 01:25:06 +0300

    return to previous version

--- api_v3/services/DataService.php
+++ api_v3/services/DataService.php
@@ -228,9 +228,13 @@ class DataService extends KalturaEntryService
 		if(($resource->getType() == 'kLocalFileResource') && (!isset($resource->getSourceType) ||  $resource->getSourceType != KalturaSourceType::WEBCAM))
 		{
 			$file_path = $resource->getLocalFilePath();
-			$validType = kFileUtils::validateTextualMimeType($resource, $file_path);
-			if($validType){
+			$fileType = kFile::mimeType($file_path);
+			if((substr($fileType, 0, 5) == 'text/') || ($fileType == 'application/xml')) {
 				$dbEntry->setDataContent(kFile::getFileContent($file_path));
+			}
+			else{
+				KalturaLog::err("Resource of type [" . get_class($resource) . "] with file type ". $fileType. " is not supported");
+				throw new KalturaAPIException(KalturaErrors::FILE_TYPE_NOT_SUPPORTED, $fileType);
 			}
 		}
 		else
@@ -239,5 +243,4 @@ class DataService extends KalturaEntryService
 			throw new KalturaAPIException(KalturaErrors::RESOURCE_TYPE_NOT_SUPPORTED, get_class($resource));
 		}
 	}
-
 }
