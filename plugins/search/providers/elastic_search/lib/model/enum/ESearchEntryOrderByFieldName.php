d481c671792c68f5d4e139d37065964e094a75b8
Nadav Harnik <nadav.harnik@kaltura.com>
2017-08-02 11:09:36 +0300

    add order by start date and end date

--- plugins/search/providers/elastic_search/lib/model/enum/ESearchEntryOrderByFieldName.php
+++ plugins/search/providers/elastic_search/lib/model/enum/ESearchEntryOrderByFieldName.php
@@ -7,4 +7,6 @@ interface ESearchEntryOrderByFieldName extends BaseEnum
 {
     const ENTRY_UPDATED_AT = 'updated_at';
     const ENTRY_CREATED_AT = 'created_at';
+    const ENTRY_START_DATE = 'start_date';
+    const ENTRY_END_DATE = 'end_date';
 }
