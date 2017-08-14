ad23b3e78bb27f400a4b4a98b6d17f84340c0c49
Erez Gotlieb <erez.gotlieb@kaltura.com>
2017-08-14 14:57:55 +0300

    Update unlockJobsByScheduler.php
    modified unlock batch jobs script

--- alpha/scripts/utils/unlockJobsByScheduler.php
+++ alpha/scripts/utils/unlockJobsByScheduler.php
@@ -1,19 +1,37 @@
 <?php
-if($argc != 2)
-	die ('Usage: php unlockJobsByScheduler.php {scheduler id}.' . PHP_EOL);
 require_once(__DIR__ . '/../bootstrap.php');
 
+$host = gethostname();
+echo "Running for $host";
+
 $c = new Criteria();
-$c->add(BatchJobLockPeer::SCHEDULER_ID, $argv[1]);
+$c->add(SchedulerPeer::HOST, $host);
+$scheduler = SchedulerPeer::doSelectOne( $c);
+if (!$scheduler)
+{
+	echo "Could not find scheduler for host.\n";
+	exit(0);
+}
+
+if (!$scheduler->getConfiguredId())
+{
+	echo "Could not find scheduler id for scheduler.\n";
+	exit(0);
+}
+
+echo "Found Scheduler configured Id: ". $scheduler->getConfiguredId(). "\n";
+
+$c = new Criteria();
+$c->add(BatchJobLockPeer::SCHEDULER_ID, $scheduler->getConfiguredId());
 $batchLocks = BatchJobLockPeer::doSelect( $c);
 echo "Got " . count($batchLocks) . " Job to reset from machine ID: " . $argv[1] . PHP_EOL;
 
 foreach ($batchLocks as $batchLock){
-	echo "resetting jobLock " .$batchLock->getId() . PHP_EOL;
-	$batchLock->setStatus(BatchJob::BATCHJOB_STATUS_PENDING);
-	$batchLock->setSchedulerId(null);
-	$batchLock->setWorkerId(null);
-	$batchLock->setBatchIndex(null);
-	$batchLock->setExpiration(time());
-	$batchLock->save();
+        echo "resetting jobLock " .$batchLock->getId() . PHP_EOL;
+        $batchLock->setStatus(BatchJob::BATCHJOB_STATUS_PENDING);
+        $batchLock->setSchedulerId(null);
+        $batchLock->setWorkerId(null);
+        $batchLock->setBatchIndex(null);
+        $batchLock->setExpiration(time());
+        $batchLock->save();
 }
