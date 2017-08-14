d9d2e8469e7ff49e0273562aad08c5084d8ab771
Erez Gotlieb <erez.gotlieb@kaltura.com>
2017-08-14 15:37:19 +0300

    Update SchedulerPeer.php
    add SchedulerPeer:getConfiguredIdByHostName method

--- alpha/lib/model/SchedulerPeer.php
+++ alpha/lib/model/SchedulerPeer.php
@@ -22,4 +22,21 @@ class SchedulerPeer extends BaseSchedulerPeer
 		SchedulerStatusPeer::deleteBySchedulerConfigId($schedulerId);
 		ControlPanelCommandPeer::deleteBySchedulerConfigId($schedulerId);
 	}
+	
+	public static function getConfiguredIdByHostName($hostname)
+	{
+		$c = new Criteria();
+		$c->add(SchedulerPeer::HOST, $hostname);
+		$result = SchedulerPeer::doSelect( $c);
+		if (!$result)
+			throw new KalturaAPIException("Could not find scheduler for host $hostname");
+
+		if ( count($result)> 1 )
+			throw new KalturaAPIException("More than one result for host $hostname");
+
+		if (!$result[0]->getConfiguredId())
+			throw new KalturaAPIException("Could not find scheduler id for scheduler.");
+
+		return $result[0]->getConfiguredId();
+	}
 }
