1c90649afc516ba64b0c8fb64e9f2d32796e2cfe
yossipapi <yossi.papiashvili@kaltura.com>
2017-09-10 10:25:26 +0300

    Remove indent changes + specify possible error types

--- api_v3/services/UserService.php
+++ api_v3/services/UserService.php
@@ -7,9 +7,9 @@
  * @package api
  * @subpackage services
  */
-class UserService extends KalturaBaseUserService
+class UserService extends KalturaBaseUserService 
 {
-	
+
 	/**
 	 * Adds a new user to an existing account in the Kaltura database.
 	 * Input param $id is the unique identifier in the partner's system.
@@ -33,14 +33,14 @@ class UserService extends KalturaBaseUserService
 		{
 			throw new KalturaAPIException(KalturaErrors::INVALID_FIELD_VALUE, 'id');
 		}
-		
+
 		if ($user instanceof KalturaAdminUser)
 		{
 			$user->isAdmin = true;
 		}
 		$user->partnerId = $this->getPartnerId();
-		
-		
+
+
 		$lockKey = "user_add_" . $this->getPartnerId() . $user->id;
 		return kLock::runLocked($lockKey, array($this, 'adduserImpl'), array($user));
 	}
@@ -81,7 +81,7 @@ class UserService extends KalturaBaseUserService
 					$invalidPasswordStructureMessage = $partner->getInvalidPasswordStructureMessage();
 				throw new KalturaAPIException(KalturaErrors::PASSWORD_STRUCTURE_INVALID,$invalidPasswordStructureMessage);
 			}
-			throw $e;
+			throw $e;			
 		}
 		catch (kPermissionException $e)
 		{
@@ -97,17 +97,17 @@ class UserService extends KalturaBaseUserService
 			}
 			throw $e;
 		}
-		
+
 		$newUser = new KalturaUser();
 		$newUser->fromObject($dbUser, $this->getResponseProfile());
 		
 		return $newUser;
 	}
-	
+
 	/**
 	 * Updates an existing user object.
 	 * You can also use this action to update the userId.
-	 *
+	 * 
 	 * @action update
 	 * @param string $userId The user's unique identifier in the partner's system
 	 * @param KalturaUser $user The user parameters to update
@@ -119,16 +119,16 @@ class UserService extends KalturaBaseUserService
 	 * @throws KalturaErrors::ACCOUNT_OWNER_NEEDS_PARTNER_ADMIN_ROLE
 	 */
 	public function updateAction($userId, KalturaUser $user)
-	{
+	{		
 		$dbUser = kuserPeer::getKuserByPartnerAndUid($this->getPartnerId(), $userId);
 		
 		if (!$dbUser)
 			throw new KalturaAPIException(KalturaErrors::INVALID_USER_ID, $userId);
-		
+
 		if ($dbUser->getIsAdmin() && !is_null($user->isAdmin) && !$user->isAdmin) {
 			throw new KalturaAPIException(KalturaErrors::CANNOT_SET_ROOT_ADMIN_AS_NO_ADMIN);
 		}
-		
+			
 		// update user
 		try
 		{
@@ -142,13 +142,13 @@ class UserService extends KalturaBaseUserService
 			if (!is_null($user->id) && $user->id != $userId) {
 				if(!preg_match(kuser::PUSER_ID_REGEXP, $user->id)) {
 					throw new KalturaAPIException(KalturaErrors::INVALID_FIELD_VALUE, 'id');
-				}
+				} 
 				
 				$existingUser = kuserPeer::getKuserByPartnerAndUid($this->getPartnerId(), $user->id);
 				if ($existingUser) {
 					throw new KalturaAPIException(KalturaErrors::DUPLICATE_USER_BY_ID, $user->id);
 				}
-			}
+			}			
 			$dbUser = $user->toUpdatableObject($dbUser);
 			$dbUser->save();
 		}
@@ -174,40 +174,40 @@ class UserService extends KalturaBaseUserService
 			if ($code == kUserException::CANNOT_DELETE_OR_BLOCK_ROOT_ADMIN_USER) {
 				throw new KalturaAPIException(KalturaErrors::CANNOT_DELETE_OR_BLOCK_ROOT_ADMIN_USER);
 			}
-			throw $e;
+			throw $e;			
 		}
-		
+				
 		$user = new KalturaUser();
 		$user->fromObject($dbUser, $this->getResponseProfile());
 		
 		return $user;
 	}
-	
+
 	
 	/**
 	 * Retrieves a user object for a specified user ID.
-	 *
+	 * 
 	 * @action get
 	 * @param string $userId The user's unique identifier in the partner's system
 	 * @return KalturaUser The specified user object
 	 *
 	 * @throws KalturaErrors::INVALID_USER_ID
-	 */
+	 */		
 	public function getAction($userId = null)
 	{
-		if (is_null($userId) || $userId == '')
-		{
-			$userId = kCurrentContext::$ks_uid;
-		}
-		
+	    if (is_null($userId) || $userId == '')
+	    {
+            $userId = kCurrentContext::$ks_uid;	        
+	    }
+
 		if (!kCurrentContext::$is_admin_session && kCurrentContext::$ks_uid != $userId)
 			throw new KalturaAPIException(KalturaErrors::CANNOT_RETRIEVE_ANOTHER_USER_USING_NON_ADMIN_SESSION, $userId);
-		
+
 		$dbUser = kuserPeer::getKuserByPartnerAndUid($this->getPartnerId(), $userId);
-		
+	
 		if (!$dbUser)
 			throw new KalturaAPIException(KalturaErrors::INVALID_USER_ID, $userId);
-		
+
 		$user = new KalturaUser();
 		$user->fromObject($dbUser, $this->getResponseProfile());
 		
@@ -217,11 +217,11 @@ class UserService extends KalturaBaseUserService
 	/**
 	 * Retrieves a user object for a user's login ID and partner ID.
 	 * A login ID is the email address used by a user to log into the system.
-	 *
+	 * 
 	 * @action getByLoginId
 	 * @param string $loginId The user's email address that identifies the user for login
 	 * @return KalturaUser The user object represented by the login and partner IDs
-	 *
+	 * 
 	 * @throws KalturaErrors::LOGIN_DATA_NOT_FOUND
 	 * @throws KalturaErrors::USER_NOT_FOUND
 	 */
@@ -236,7 +236,7 @@ class UserService extends KalturaBaseUserService
 		if (!$kuser) {
 			throw new KalturaAPIException(KalturaErrors::USER_NOT_FOUND);
 		}
-		
+
 		// users that are not publisher administrator are only allowed to get their own object   
 		if ($kuser->getId() != kCurrentContext::getCurrentKsKuserId() && !in_array(PermissionName::MANAGE_ADMIN_USERS, kPermissionManager::getCurrentPermissions()))
 			throw new KalturaAPIException(KalturaErrors::INVALID_USER_ID, $loginId);
@@ -246,24 +246,24 @@ class UserService extends KalturaBaseUserService
 		
 		return $user;
 	}
-	
+
 	/**
 	 * Deletes a user from a partner account.
-	 *
+	 * 
 	 * @action delete
 	 * @param string $userId The user's unique identifier in the partner's system
 	 * @return KalturaUser The deleted user object
 	 *
 	 * @throws KalturaErrors::INVALID_USER_ID
-	 */
+	 */		
 	public function deleteAction($userId)
 	{
 		$dbUser = kuserPeer::getKuserByPartnerAndUid($this->getPartnerId(), $userId);
-		
+	
 		if (!$dbUser) {
 			throw new KalturaAPIException(KalturaErrors::INVALID_USER_ID, $userId);
 		}
-		
+					
 		try {
 			$dbUser->setStatus(KalturaUserStatus::DELETED);
 		}
@@ -272,7 +272,7 @@ class UserService extends KalturaBaseUserService
 			if ($code == kUserException::CANNOT_DELETE_OR_BLOCK_ROOT_ADMIN_USER) {
 				throw new KalturaAPIException(KalturaErrors::CANNOT_DELETE_OR_BLOCK_ROOT_ADMIN_USER);
 			}
-			throw $e;
+			throw $e;			
 		}
 		$dbUser->save();
 		
@@ -286,7 +286,7 @@ class UserService extends KalturaBaseUserService
 	 * Lists user objects that are associated with an account.
 	 * Blocked users are listed unless you use a filter to exclude them.
 	 * Deleted users are not listed unless you use a filter to include them.
-	 *
+	 * 
 	 * @action list
 	 * @param KalturaUserFilter $filter A filter used to exclude specific types of users
 	 * @param KalturaFilterPager $pager A limit for the number of records to display on a page
@@ -296,21 +296,21 @@ class UserService extends KalturaBaseUserService
 	{
 		if (!$filter)
 			$filter = new KalturaUserFilter();
-		
+			
 		if(!$pager)
 			$pager = new KalturaFilterPager();
-		
+			
 		return $filter->getListResponse($pager, $this->getResponseProfile());
 	}
 	
 	/**
 	 * Notifies that a user is banned from an account.
-	 *
+	 * 
 	 * @action notifyBan
 	 * @param string $userId The user's unique identifier in the partner's system
 	 *
 	 * @throws KalturaErrors::INVALID_USER_ID
-	 */
+	 */		
 	public function notifyBan($userId)
 	{
 		$dbUser = kuserPeer::getKuserByPartnerAndUid($this->getPartnerId(), $userId);
@@ -319,10 +319,10 @@ class UserService extends KalturaBaseUserService
 		
 		myNotificationMgr::createNotification(kNotificationJobData::NOTIFICATION_TYPE_USER_BANNED, $dbUser);
 	}
-	
+
 	/**
 	 * Logs a user into a partner account with a partner ID, a partner user ID (puser), and a user password.
-	 *
+	 * 
 	 * @action login
 	 * @param int $partnerId The identifier of the partner account
 	 * @param string $userId The user's unique identifier in the partner's system
@@ -339,7 +339,7 @@ class UserService extends KalturaBaseUserService
 	 * @throws KalturaErrors::LOGIN_BLOCKED
 	 * @throws KalturaErrors::PASSWORD_EXPIRED
 	 * @throws KalturaErrors::USER_IS_BLOCKED
-	 */
+	 */		
 	public function loginAction($partnerId, $userId, $password, $expiry = 86400, $privileges = '*')
 	{
 		// exceptions might be thrown
@@ -348,9 +348,9 @@ class UserService extends KalturaBaseUserService
 	
 	/**
 	 * Logs a user into a partner account with a user login ID and a user password.
-	 *
+	 * 
 	 * @action loginByLoginId
-	 *
+	 * 
 	 * @param string $loginId The user's email address that identifies the user for login
 	 * @param string $password The user's password
 	 * @param int $partnerId The identifier of the partner account
@@ -367,7 +367,7 @@ class UserService extends KalturaBaseUserService
 	 * @throws KalturaErrors::LOGIN_BLOCKED
 	 * @throws KalturaErrors::PASSWORD_EXPIRED
 	 * @throws KalturaErrors::USER_IS_BLOCKED
-	 */
+	 */		
 	public function loginByLoginIdAction($loginId, $password, $partnerId = null, $expiry = 86400, $privileges = '*', $otp = null)
 	{
 		// exceptions might be thrown
@@ -377,9 +377,9 @@ class UserService extends KalturaBaseUserService
 	
 	/**
 	 * Updates a user's login data: email, password, name.
-	 *
+	 * 
 	 * @action updateLoginData
-	 *
+	 * 
 	 * @param string $oldLoginId The user's current email address that identified the user for login
 	 * @param string $password The user's current email address that identified the user for login
 	 * @param string $newLoginId Optional, The user's email address that will identify the user for login
@@ -396,15 +396,15 @@ class UserService extends KalturaBaseUserService
 	 * @throws KalturaErrors::LOGIN_ID_ALREADY_USED
 	 */
 	public function updateLoginDataAction( $oldLoginId , $password , $newLoginId = "" , $newPassword = "", $newFirstName = null, $newLastName = null)
-	{
+	{	
 		return parent::updateLoginDataImpl($oldLoginId , $password , $newLoginId, $newPassword, $newFirstName, $newLastName);
 	}
 	
 	/**
 	 * Reset user's password and send the user an email to generate a new one.
-	 *
+	 * 
 	 * @action resetPassword
-	 *
+	 * 
 	 * @param string $email The user's email address (login email)
 	 * @ksIgnored
 	 *
@@ -413,7 +413,7 @@ class UserService extends KalturaBaseUserService
 	 * @throws KalturaErrors::PASSWORD_ALREADY_USED
 	 * @throws KalturaErrors::INVALID_FIELD_VALUE
 	 * @throws KalturaErrors::LOGIN_ID_ALREADY_USED
-	 */
+	 */	
 	public function resetPasswordAction($email)
 	{
 		return parent::resetPasswordImpl($email);
@@ -421,9 +421,9 @@ class UserService extends KalturaBaseUserService
 	
 	/**
 	 * Set initial users password
-	 *
+	 * 
 	 * @action setInitialPassword
-	 *
+	 * 
 	 * @param string $hashKey The hash key used to identify the user (retrieved by email)
 	 * @param string $newPassword The new password to set for the user
 	 * @ksIgnored
@@ -434,7 +434,7 @@ class UserService extends KalturaBaseUserService
 	 * @throws KalturaErrors::NEW_PASSWORD_HASH_KEY_INVALID
 	 * @throws KalturaErrors::PASSWORD_ALREADY_USED
 	 * @throws KalturaErrors::INTERNAL_SERVERL_ERROR
-	 */
+	 */	
 	public function setInitialPasswordAction($hashKey, $newPassword)
 	{
 		return parent::setInitialPasswordImpl($hashKey, $newPassword);
@@ -442,23 +442,23 @@ class UserService extends KalturaBaseUserService
 	
 	/**
 	 * Enables a user to log into a partner account using an email address and a password
-	 *
+	 * 
 	 * @action enableLogin
-	 *
+	 * 
 	 * @param string $userId The user's unique identifier in the partner's system
 	 * @param string $loginId The user's email address that identifies the user for login
 	 * @param string $password The user's password
 	 * @return KalturaUser The user object represented by the user and login IDs
-	 *
+	 * 
 	 * @throws KalturaErrors::USER_LOGIN_ALREADY_ENABLED
 	 * @throws KalturaErrors::USER_NOT_FOUND
 	 * @throws KalturaErrors::ADMIN_LOGIN_USERS_QUOTA_EXCEEDED
 	 * @throws KalturaErrors::PASSWORD_STRUCTURE_INVALID
 	 * @throws KalturaErrors::LOGIN_ID_ALREADY_USED
 	 *
-	 */
+	 */	
 	public function enableLoginAction($userId, $loginId, $password = null)
-	{
+	{		
 		try
 		{
 			$user = kuserPeer::getKuserByPartnerAndUid($this->getPartnerId(), $userId);
@@ -474,7 +474,7 @@ class UserService extends KalturaBaseUserService
 			
 			// Gonen 2011-05-29 : NOTE - 3rd party uses this action and expect that email notification will not be sent by default
 			// if this call ever changes make sure you do not change default so mails are sent.
-			$user->enableLogin($loginId, $password, true);
+			$user->enableLogin($loginId, $password, true);	
 			$user->save();
 		}
 		catch (Exception $e)
@@ -514,20 +514,20 @@ class UserService extends KalturaBaseUserService
 	/**
 	 * Disables a user's ability to log into a partner account using an email address and a password.
 	 * You may use either a userId or a loginId parameter for this action.
-	 *
+	 * 
 	 * @action disableLogin
-	 *
+	 * 
 	 * @param string $userId The user's unique identifier in the partner's system
 	 * @param string $loginId The user's email address that identifies the user for login
-	 *
+	 * 
 	 * @return KalturaUser The user object represented by the user and login IDs
-	 *
+	 * 
 	 * @throws KalturaErrors::USER_LOGIN_ALREADY_DISABLED
 	 * @throws KalturaErrors::PROPERTY_VALIDATION_CANNOT_BE_NULL
 	 * @throws KalturaErrors::USER_NOT_FOUND
 	 * @throws KalturaErrors::CANNOT_DISABLE_LOGIN_FOR_ADMIN_USER
 	 *
-	 */
+	 */	
 	public function disableLoginAction($userId = null, $loginId = null)
 	{
 		if (!$loginId && !$userId)
@@ -577,11 +577,11 @@ class UserService extends KalturaBaseUserService
 	
 	/**
 	 * Index an entry by id.
-	 *
+	 * 
 	 * @action index
 	 * @param string $id
 	 * @param bool $shouldUpdate
-	 * @return string
+	 * @return string 
 	 * @throws KalturaErrors::USER_NOT_FOUND
 	 */
 	function indexAction($id, $shouldUpdate = true)
@@ -592,7 +592,7 @@ class UserService extends KalturaBaseUserService
 			throw new KalturaAPIException(KalturaErrors::USER_NOT_FOUND);
 		
 		$kuser->indexToSearchIndex();
-		
+			
 		return $kuser->getPuserId();
 	}
 	
@@ -603,7 +603,18 @@ class UserService extends KalturaBaseUserService
 	 * @param int $requestedPartnerId
 	 * @throws APIErrors::PARTNER_CHANGE_ACCOUNT_DISABLED
 	 *
-	 * * @return KalturaSessionResponse The generated session information
+	 * @return KalturaSessionResponse The generated session information
+	 * 
+	 * @throws KalturaErrors::INVALID_USER_ID
+	 * @throws KalturaErrors::PARTNER_CHANGE_ACCOUNT_DISABLED
+	 * @throws KalturaErrors::ADMIN_KUSER_NOT_FOUND
+	 * @throws KalturaErrors::LOGIN_DATA_NOT_FOUND
+	 * @throws KalturaErrors::LOGIN_BLOCKED
+	 * @throws KalturaErrors::USER_IS_BLOCKED
+	 * @throws KalturaErrors::INTERNAL_SERVERL_ERROR
+	 * @throws KalturaErrors::UNKNOWN_PARTNER_ID
+	 * @throws KalturaErrors::SERVICE_ACCESS_CONTROL_RESTRICTED
+	 * 
 	 */
 	public function loginByKsAction($requestedPartnerId)
 	{
@@ -616,5 +627,5 @@ class UserService extends KalturaBaseUserService
 		
 		return $res;
 	}
-	
+
 }
