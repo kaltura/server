989f69c1c057c9c5eb1e45a7ae6ab6a50cd647d2
Anatol <anatol.schwartz@kaltura.com>
2017-09-13 13:18:34 +0300

    PLAT-7917-remove debug code

--- batch/batches/Convert/KChunkedEncodeJobSchedulerExe.php
+++ batch/batches/Convert/KChunkedEncodeJobSchedulerExe.php
@@ -8,22 +8,6 @@
 //require_once(__DIR__ . "/../../bootstrap.php");
 require_once("/opt/kaltura/app/batch/bootstrap.php");
 
-				/********************************************************
-				 * The bellow includes to be removed for production
-				 ********************************************************
-				 */
-	if (class_exists('KChunkedEncodeJobScheduler')) {
-		require_once "/opt/kaltura/app/batch/client/KalturaTypes.php";
-
-		require_once "KChunkedEncodeUtils.php";
-		require_once "KChunkedEncode.php";
-		require_once "KBaseChunkedEncodeSessionManager.php";
-		require_once "KChunkedEncodeSessionManager.php";
-
-		require_once "KChunkedEncodeDistrExecInterface.php";
-		require_once "KChunkedEncodeMemcacheWrap.php";
-	}
-
 $instance = new KChunkedEncodeJobScheduler();
 $instance->run();
 $instance->done();
