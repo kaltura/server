8022b6f715f4751b0a6ae4ad4882531fba186e6b
david-winder-kaltura <david.winder@kaltura.com>
2017-09-13 11:32:57 +0300

    Update MediaService.php

--- api_v3/services/MediaService.php
+++ api_v3/services/MediaService.php
@@ -698,7 +698,7 @@ class MediaService extends KalturaEntryService
 	function getAction($entryId, $version = -1)
 	{
 		$dbEntry = entryPeer::retrieveByPK($entryId);
-		if (!$dbEntry || !(KalturaEntryFactory::getInstanceByType($dbEntry->getType() instanceof KalturaMediaEntry)))
+		if (!$dbEntry || !(KalturaEntryFactory::getInstanceByType($dbEntry->getType()) instanceof KalturaMediaEntry))
 			throw new KalturaAPIException(KalturaErrors::ENTRY_ID_NOT_FOUND, $entryId);
 
 		return $this->getEntry($entryId, $version);
