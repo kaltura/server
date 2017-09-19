aac5b90b6b387ffc38209cbcb2a01c5e6ce45dff
yossipapi <yossi.papiashvili@kaltura.com>
2017-09-19 17:45:22 +0300

    No Plat: All elstic fields should be lower cased

--- plugins/beacon/lib/api/KalturaBeacon.php
+++ plugins/beacon/lib/api/KalturaBeacon.php
@@ -64,13 +64,13 @@ class KalturaBeacon extends KalturaObject implements IFilterable
 	private static $map_between_objects = array
 	(
 		'id',
-		'indexType',
-		'updatedAt',
-		'relatedObjectType',
-		'eventType',
-		'objectId',
-		'privateData',
-		'rawData',
+		'indexType' => "indexType",
+		'updatedAt' => "updated_at",
+		'relatedObjectType' => 'related_object_type',
+		'eventType' => 'event_type',
+		'objectId' => "object_id",
+		'privateData' => 'private_data',
+		'rawData' => 'raw_data',
 	);
 	
 	public function validateForInsert($propertiesToSkip = array())
@@ -111,7 +111,7 @@ class KalturaBeacon extends KalturaObject implements IFilterable
 	{
 		parent::fromArray($source_array);
 		
-		if (isset($source_array['privateData']))
-			$this->privateData = json_encode($source_array['privateData']);
+		if (isset($source_array[kBeacon::FIELD_PRIVATE_DATA]))
+			$this->privateData = json_encode($source_array[kBeacon::FIELD_PRIVATE_DATA]);
 	}
 }
