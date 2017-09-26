b675ff11912ebc3b06e619e292f9e92a001e80a5
david-winder-kaltura <david.winder@kaltura.com>
2017-09-26 17:00:07 +0300

    create new function to get local file content by fileSync

--- plugins/file_sync/lib/api/KalturaFileSync.php
+++ plugins/file_sync/lib/api/KalturaFileSync.php
@@ -331,7 +331,7 @@ class KalturaFileSync extends KalturaObject implements IFilterable
 				$this->fileDiscSize = kFile::fileSize($path);
 			if($this->shouldGet('fileContent', $responseProfile))
 			{
-				$content = substr(kFileSyncUtils::getContentsByFileSync($source_object), 0, 1024);
+				$content = kFileSyncUtils::getLocalContentsByFileSync($source_object, false, null, 0, 1024);
 				if(ctype_print($content) || ctype_cntrl($content))
 					$this->fileContent = $content;
 			}
