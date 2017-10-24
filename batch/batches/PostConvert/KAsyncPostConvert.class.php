86c3cca38c76bdffbb1b25898809e2698cd6163a
Anatol <anatol.schwartz@kaltura.com>
2017-10-24 14:40:26 +0300

    SUP-11979-Thumbs-deinterlace

--- batch/batches/PostConvert/KAsyncPostConvert.class.php
+++ batch/batches/PostConvert/KAsyncPostConvert.class.php
@@ -150,7 +150,9 @@ class KAsyncPostConvert extends KJobHandlerWorker
 					
 			// generates the thumbnail
 			$thumbMaker = new KFFMpegThumbnailMaker($mediaFile, $thumbPath, KBatchBase::$taskConfig->params->FFMpegCmd);
-			$created = $thumbMaker->createThumnail($data->thumbOffset, $mediaInfo->videoWidth, $mediaInfo->videoHeight, null, null, $mediaInfo->videoDar);
+			$params['dar'] = $mediaInfo->videoDar;
+			$params['scanType'] = $mediaInfo->scanType();
+			$created = $thumbMaker->createThumnail($data->thumbOffset, $mediaInfo->videoWidth, $mediaInfo->videoHeight, $params);
 			
 			if(!$created || !file_exists($thumbPath))
 			{
