21a9922fe030114451ced8352da1b8f1c9d196d3
ZurKaltura <zur.magen@kaltura.com>
2017-10-24 11:36:11 +0300

    revert our sftp youtube back to the old api version

--- plugins/content_distribution/providers/youtube/lib/YouTubeDistributionGoogleClientHelper.php
+++ plugins/content_distribution/providers/youtube/lib/YouTubeDistributionGoogleClientHelper.php
@@ -22,10 +22,8 @@ class YouTubeDistributionGoogleClientHelper
 			return self::$_googleClient;
 
 		// add google client library to include path
-		set_include_path(get_include_path().PATH_SEPARATOR.KALTURA_ROOT_PATH.'/vendor/google-api-php-client-1.1.2/src/Google');
-		require_once 'autoload.php';
-		require_once 'Client.php';
-
+		set_include_path(get_include_path().PATH_SEPARATOR.KALTURA_ROOT_PATH.'/vendor/google-api-php-client/src/');
+		require_once 'Google_Client.php';
 
 		$client = new Google_Client();
 		$client->setClientId($clientId);
