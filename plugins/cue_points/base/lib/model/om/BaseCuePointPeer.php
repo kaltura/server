3f99a7e411190e5addcbed464ac9a90a39e32133
david-winder-kaltura <david.winder@kaltura.com>
2017-10-30 14:24:24 +0200

    Update BaseCuePointPeer.php

--- plugins/cue_points/base/lib/model/om/BaseCuePointPeer.php
+++ plugins/cue_points/base/lib/model/om/BaseCuePointPeer.php
@@ -408,6 +408,8 @@ abstract class BaseCuePointPeer {
 		{
 			$cacheKey = null;
 			CuePointPeer::filterSelectResults($cachedResult, $criteriaForSelect);
+			if ($criteria instanceof KalturaCriteria)
+				$criteria->setRecordsCount($criteriaForSelect->getRecordsCount());
 			CuePointPeer::updateInstancePool($cachedResult);
 			return $cachedResult;
 		}
