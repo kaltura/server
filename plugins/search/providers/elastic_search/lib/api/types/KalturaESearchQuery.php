a20436b0a2be9edfcbea2bd1401244d6ec49928a
gotlieb <gotlieb.erez@gmail.com>
2017-11-01 13:30:23 +0200

    PLAT-8134 "remove irrelevant code"

--- plugins/search/providers/elastic_search/lib/api/types/KalturaESearchQuery.php
+++ plugins/search/providers/elastic_search/lib/api/types/KalturaESearchQuery.php
@@ -20,10 +20,4 @@ class KalturaESearchQuery extends KalturaESearchObject
 		return array_merge(parent::getMapBetweenObjects(), self::$mapBetweenObjects);
 	}
 
-	public function toObject($object_to_fill = null, $props_to_skip = array())
-	{
-		if (!$object_to_fill)
-			$object_to_fill = new ESearchParams();
-		return parent::toObject($object_to_fill, $props_to_skip);
-	}
 }
