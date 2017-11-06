2fbd4b46fee6bd554770ff95d579faf270f8c63d
Tanya Libatter <Tanya.Libatter@kaltura.com>
2017-11-06 09:23:34 +0200

    FEC-7394 - add env configuration object t KalturaPlayer

--- alpha/apps/kaltura/modules/extwidget/actions/embedPlaykitJsAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/embedPlaykitJsAction.class.php
@@ -102,6 +102,7 @@ class embedPlaykitJsAction extends sfAction
 	{
 		$bundleContentParts = explode(",", $bundleContent, 2);
 		$bundleContent = $this->appendUiConfToContent($bundleContentParts[1]);
+		$bundleContent = $this->appendEnvConfigToContent($bundleContent);
 		
 		$autoEmbed = $this->getRequestParameter(self::AUTO_EMBED_PARAM_NAME);
 		$iframeEmbed = $this->getRequestParameter(self::IFRAME_EMBED_PARAM_NAME);
@@ -142,6 +143,66 @@ class embedPlaykitJsAction extends sfAction
 		$content .= $kalturaPlayerConfig;
 
 		return $content;
+	}
+
+	private function appendEnvConfigToContent($content)
+	{
+
+		$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? "https" : "http";
+
+		// The default Kaltura service url:
+		$serviceUrl = requestUtils::getApiCdnHost().'/api_v3';
+		// Default Kaltura CDN url:
+		$cdnUrl = requestUtils::getCdnHost($protocol);
+		// Default Stats URL
+		$statsServiceUrl = ($protocol == "https") ? $this->buildUrl($protocol,"stats_host_https") : $this->buildUrl($protocol,"stats_host");
+		// Default Live Stats URL
+		$liveStatsServiceUrl = ($protocol == "https") ? $this->buildUrl($protocol,"live_stats_host_https") : $this->buildUrl($protocol,"live_stats_host");
+		// Default Kaltura Analytics URL
+		$analyticsServiceUrl = ($protocol == "https") ? $this->buildUrl($protocol,"analytics_host_https") : $this->buildUrl($protocol,"analytics_host");
+		// Get Kaltura Supported API Features
+		$apiFeatures = $this->getFromConfig('features');
+
+		$envConfig = array(
+			"ServiceUrl" => $serviceUrl,
+			"CDNUrl" => $cdnUrl,
+			"StatsServiceUrl" => $statsServiceUrl,
+			"LiveStatsServiceUrl" => $liveStatsServiceUrl,
+			"AnalyticsServiceUrl" => $analyticsServiceUrl,
+			"ApiFeatures" => $apiFeatures
+		);
+
+		$envConfig = json_encode($envConfig);	
+		
+		if ($envConfig !== false)
+		{
+			$kalturaPlayerEnvConfig = "
+			(function(){KalturaPlayer.EnvConfig = $envConfig;
+			})();";
+			return $content . $kalturaPlayerEnvConfig;
+		}
+
+		return $content;
+	}
+
+	private function getFromConfig($key)
+	{
+		if( kConf::hasParam($key) ) {
+			return kConf::get($key);
+		}
+		return '';
+	}
+
+	private function buildUrl($protocol, $key)
+	{
+		$configValue = $this->getFromConfig($key);
+		$port = (($_SERVER['SERVER_PORT']) != '80' && $_SERVER['SERVER_PORT'] != '443')?':'.$_SERVER['SERVER_PORT']:'';
+		if( $key && $configValue)
+		{
+			return $protocol . '://' . $configValue . $port;
+		}
+
+		return '';
 	}
 	
 	private function sendHeaders($content, $lastModified)
