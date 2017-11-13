56eaf9037ad9780d210b09579ceda3ff8fd06132
Moshe Maor <moshe.maor@kaltura.com>
2017-11-13 13:08:06 +0200

    Update kFile.class.php

--- infra/storage/kFile.class.php
+++ infra/storage/kFile.class.php
@@ -504,7 +504,7 @@ class kFile extends kFileBase
 			$fileBrief = shell_exec('file -b ' . $realPath);
 			if(kString::beginsWith($fileBrief,self::MO_PATTERN))
 				$fileType = 'application/mo';
-			else if(strpos($fileBrief,"text")!==false)
+			else if(strpos($fileBrief,self::TEXT)!==false)
 				$fileType = self::TEXT;
 		}
 		return $fileType;
