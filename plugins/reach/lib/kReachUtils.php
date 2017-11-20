8897e612021fd593638aba31ca8c4427accd191f
gotlieb <gotlieb.erez@gmail.com>
2017-11-20 16:06:42 +0200

    PLAT-8280 - generate vendor ks for relevant partner with new Vendor role privileges

--- plugins/reach/lib/kReachUtils.php
+++ plugins/reach/lib/kReachUtils.php
@@ -4,4 +4,34 @@
  */
 class kReachUtils
 {
+	/**
+	 * @param $entryId
+	 * @return string
+	 * @throws Exception
+	 */
+	public static function generateReachVendorKs($entryId)
+	{
+		$entry = entryPeer::retrieveByPK($entryId);
+		if (!$entry)
+			throw new Exception("Entry Id [$entryId] not Found to create REACH Vendor limited session");
+
+		$partner = $entry->getPartner();
+
+		// Limit the KS to edit access a specific entry
+		$privileges = kSessionBase::PRIVILEGE_EDIT . ':' . $entryId;
+
+		// Limit the KS to use only the Vendor Role
+		$privileges .= ',' . kSessionBase::PRIVILEGE_SET_ROLE . ':' . UserRoleId::REACH_VENDOR_ROLE;
+
+		// Disable entitlement to avoid entitlement validation when accessing an entry
+		$privileges .= ',' . kSessionBase::PRIVILEGE_DISABLE_ENTITLEMENT_FOR_ENTRY. ':' . $entryId;
+
+		$limitedKs = '';
+		$result = kSessionUtils::startKSession($partner->getId(), $partner->getSecret(), '', $limitedKs, dateUtils::DAY, kSessionBase::SESSION_TYPE_USER, '', $privileges, null, null);
+		if ($result < 0)
+			throw new Exception('Failed to create REACH Vendor limited session for partner '.$partner->getId());
+
+		return $limitedKs;
+
+	}
 }
