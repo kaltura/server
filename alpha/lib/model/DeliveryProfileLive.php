7cd3177457ac9abaac90a238b3f39f4bc0ddde0d
Noam Arad <noam.arad@kaltura.com>
2017-12-04 16:57:41 +0200

    PLAT-8313: if explicit live is used always add user_type

--- alpha/lib/model/DeliveryProfileLive.php
+++ alpha/lib/model/DeliveryProfileLive.php
@@ -1,7 +1,9 @@
 <?php
 
 abstract class DeliveryProfileLive extends DeliveryProfile {
-	
+	const USER_TYPE_ADMIN = 'admin';
+	const USER_TYPE_USER = 'user';
+
 	/**
 	 * @var kLiveStreamConfiguration
 	 */
@@ -324,8 +326,13 @@ abstract class DeliveryProfileLive extends DeliveryProfile {
 		
 		$livePackagerUrl = "$livePackagerUrl/p/$partnerID/e/$entryId/sd/$segmentDuration/";
 		$entry = $this->getDynamicAttributes()->getEntry();
-		if ($entry->getExplicitLive() &&  (!$entry->canViewExplicitLive()))
-			$livePackagerUrl .= 'type/user/';
+		if ($entry->getExplicitLive())
+		{
+			$userType = self::USER_TYPE_ADMIN;
+		 	if (!$entry->canViewExplicitLive())
+				$userType = self::USER_TYPE_USER;
+			$livePackagerUrl .= "type/$userType/";
+		}
 		$secureToken = $this->generateLiveSecuredPackagerToken($livePackagerUrl);
 		$livePackagerUrl .= "t/$secureToken/";
 
