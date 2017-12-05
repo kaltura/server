448d9c03b4f6dbb743c4222c714db236d1c90fb8
ravit <ravit.shalem@kaltua.com>
2017-12-05 14:50:10 +0200

    SUPPS-1311:Raise the limit of notification recipients

--- plugins/event_notification/providers/email/lib/batch/recipients/KEmailNotificationCategoryRecipientEngine.php
+++ plugins/event_notification/providers/email/lib/batch/recipients/KEmailNotificationCategoryRecipientEngine.php
@@ -15,24 +15,33 @@ class KEmailNotificationCategoryRecipientEngine extends KEmailNotificationRecipi
 		$recipients = array();
 		$pager = new KalturaFilterPager();
 		$pager->pageSize = 500;
-		//List categoryKusers
-		$categoryUserList = KBatchBase::$kClient->categoryUser->listAction($this->recipientJobData->categoryUserFilter, $pager);
-		if (!count($categoryUserList->objects))
-			return $recipients;
-		
+		$pager->pageIndex = 1;
 		$categoryUserIds = array();
-		foreach ($categoryUserList->objects as $categoryUser)
-			$categoryUserIds[] = $categoryUser->userId;
-		
+		do {
+			//List categoryKusers
+			$categoryUserList = KBatchBase::$kClient->categoryUser->listAction($this->recipientJobData->categoryUserFilter, $pager);
+			foreach ($categoryUserList->objects as $categoryUser)
+				$categoryUserIds[] = $categoryUser->userId;
+
+			$pager->pageIndex ++;
+		}while ($pager->pageSize == count($categoryUserList->objects));
+
+		if (count($categoryUserIds)==0)
+	            return $recipients;
+
+		$pager->pageIndex = 1;
 		$userFilter = new KalturaUserFilter();
 		$userFilter->idIn = implode(',', $categoryUserIds);
-		$userList = KBatchBase::$kClient->user->listAction($userFilter, $pager);
-		foreach ($userList->objects as $user)
-		{
-			/* @var $user KalturaUser */
-			$recipients[$user->email] = $user->firstName. ' ' . $user->lastName;
-		}
-		
+		do{
+			$userList = KBatchBase::$kClient->user->listAction($userFilter, $pager);
+			foreach ($userList->objects as $user)
+			{
+				/* @var $user KalturaUser */
+				$recipients[$user->email] = $user->firstName. ' ' . $user->lastName;
+			}
+			$pager->pageIndex ++;
+		}while ($pager->pageSize == count($userList->objects));
+
 		return $recipients;
 	}
 }
