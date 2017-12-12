8335eedbc556b23c7d81e9b022e53f7cfc370363
gotlieb <gotlieb.erez@gmail.com>
2017-12-12 14:23:56 +0200

    PLAT-8312 Add nested query name for eSearchMetadataItem in case xpath is defined on the item

--- plugins/search/providers/elastic_search/lib/model/items/ESearchMetadataItem.php
+++ plugins/search/providers/elastic_search/lib/model/items/ESearchMetadataItem.php
@@ -360,6 +360,9 @@ class ESearchMetadataItem extends ESearchNestedObjectItem
 
 	public function getNestedQueryName()
 	{
+		if($this->getXpath())
+			return ESearchItemDataType::METADATA.self::QUERY_NAME_DELIMITER.self::DEFAULT_GROUP_NAME.md5($this->getXpath());
+
 		return ESearchItemDataType::METADATA.self::QUERY_NAME_DELIMITER.self::DEFAULT_GROUP_NAME;
 	}
 
