08325feb7416766dc4067d7aec968425563d67f3
david-winder-kaltura <david.winder@kaltura.com>
2017-12-24 13:12:17 +0200

    remove adding 40 k due to mpeg2-ts overhead

--- alpha/apps/kaltura/lib/kManifestRenderers.php
+++ alpha/apps/kaltura/lib/kManifestRenderers.php
@@ -922,11 +922,8 @@ class kM3U8ManifestRenderer extends kMultiFlavorManifestRenderer
 	private function calculateBitRate($flavor)
 	{
 		$bitrate = (isset($flavor['bitrate']) ? $flavor['bitrate'] : 0) * 1024;
-		// in case of Akamai HDN1.0 increase the reported bitrate due to mpeg2-ts overhead
-		if (strpos($flavor['url'], "index_0_av.m3u8"))
-			$bitrate += 40 * 1024;
-
 		$frameRate = (isset($flavor['frameRate']) ? $flavor['frameRate'] : 0);
+		// to match the bitrate calculation function from the NGINX
 		$bitrate = ($bitrate * 188 / 184) + ($frameRate * 188 * 4);
 		return $bitrate;
 	}
