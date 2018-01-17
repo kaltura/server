97d9de92e8bfa7402bd78bed58a27992fb42e999
yossipapi <yossi.papiashvili@kaltura.com>
2018-01-17 14:15:03 +0200

    Add support for viewing Kava dashboard from the admin console

--- admin_console/controllers/IndexController.php
+++ admin_console/controllers/IndexController.php
@@ -48,5 +48,46 @@ class IndexController extends Zend_Controller_Action
     {
     	
     }
-    
+	
+
+	public function kavaAction()
+	{
+		$settings = Zend_Registry::get('config')->settings;
+		$sessionExpiry = $settings->sessionExpiry;
+		$kavaDashboardUrl = $settings->kavaDashboard->url;
+		$jwtKey = $settings->kavaDashboard->jwtKey;
+		$partnerId = $settings->partnerId;
+		
+		if(!$kavaDashboardUrl || $kavaDashboardUrl == "" || !$jwtKey || $jwtKey == "")
+			$this->view->kavaDashboardUrl = null;
+		else
+			$this->view->kavaDashboardUrl = $this->getnerateSignedKavaDashboardUrl($kavaDashboardUrl, $jwtKey, $partnerId, $sessionExpiry);
+	}
+	
+	private function getnerateSignedKavaDashboardUrl($kavaDashboardUrl, $jwtKey, $partnerId, $sessionExpiry)
+	{
+		$jwtPayload = array(
+			'partnerId' => $partnerId,
+			'iat' => time(),
+			'exp' => time() + $sessionExpiry,
+		);
+		$jwt = $this->encode($jwtPayload, $jwtKey);
+		return rtrim($kavaDashboardUrl, "/") . "/?jwt=" . $jwt;
+	}
+	
+	private function urlsafeB64Encode($input)
+	{
+		return str_replace('=', '', strtr(base64_encode($input), '+/', '-_'));
+	}
+
+	private function encode($payload, $key)
+	{
+		$header = array('typ' => 'JWT', 'alg' => 'HS256');
+		$result = $this->urlsafeB64Encode(json_encode($header)) . '.' .
+			$this->urlsafeB64Encode(json_encode($payload));
+		$signature = hash_hmac('sha256', $result, $key, true);
+		$result .= '.' . $this->urlsafeB64Encode($signature);
+		return $result;
+	}
+
 }
