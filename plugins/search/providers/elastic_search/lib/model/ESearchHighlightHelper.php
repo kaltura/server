c59fe528e000306e672fe17120c2a09a316e7563
ZurKaltura <zur.magen@kaltura.com>
2018-01-18 10:53:42 +0200

    Changing strings to const

--- plugins/search/providers/elastic_search/lib/model/ESearchHighlightHelper.php
+++ plugins/search/providers/elastic_search/lib/model/ESearchHighlightHelper.php
@@ -39,9 +39,9 @@ class ESearchHighlightHelper
 	private static function getFieldTypePriority($fieldName)
 	{
 		$priority =  10;
-		if(kString::endsWith($fieldName,"raw"))
+		if(kString::endsWith($fieldName,kESearchQueryManager:: RAW_FIELD_SUFFIX ))
 			$priority =  1;
-		else if(kString::endsWith($fieldName,"ngrams"))
+		else if(kString::endsWith($fieldName,kESearchQueryManager:: NGRAMS_FIELD_SUFFIX ))
 			$priority =  20;
 
 		return $priority;
