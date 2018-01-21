dcc2cd27d30899cf5ec02f4e5d10192df97cc956
yossipapi <yossi.papiashvili@kaltura.com>
2018-01-21 16:22:41 +0200

    Fix indent

--- plugins/metadata/lib/api/filters/KalturaMetadataFilter.php
+++ plugins/metadata/lib/api/filters/KalturaMetadataFilter.php
@@ -123,21 +123,21 @@ class KalturaMetadataFilter extends KalturaMetadataBaseFilter
 	    if(kConf::hasParam('metadata_list_without_object_filtering_partners'))
 	    	$metadataListNoFilterExcludePartners = kConf::get('metadata_list_without_object_filtering_partners');
 		
-	    if(!count($metadataListNoFilterExcludePartners))
-	    	return true;
+		if(!count($metadataListNoFilterExcludePartners))
+			return true;
 		
-        if(!array_key_exists(kCurrentContext::getCurrentPartnerId(), $metadataListNoFilterExcludePartners))
-        	return true;
+		if(!array_key_exists(kCurrentContext::getCurrentPartnerId(), $metadataListNoFilterExcludePartners))
+			return true;
 		
 		$allowedFilterTypes = $metadataListNoFilterExcludePartners[kCurrentContext::getCurrentPartnerId()];
 		if($allowedFilterTypes == "");
 			return false;
 		
-        $allowedFilterTypesArray = explode(",", $allowedFilterTypes);
-        if(!in_array($this->metadataObjectTypeEqual, $allowedFilterTypesArray))
+		$allowedFilterTypesArray = explode(",", $allowedFilterTypes);
+		if(!in_array($this->metadataObjectTypeEqual, $allowedFilterTypesArray))
         	return true;
 		
-        return false;
+		return false;
 	}
 	
 	public function getObjectIdsFiltered()
