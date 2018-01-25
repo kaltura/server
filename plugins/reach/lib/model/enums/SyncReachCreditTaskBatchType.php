4a3075432b5ccea12c6264a9da0d654097b68d77
gotlieb <gotlieb.erez@gmail.com>
2018-01-25 11:51:36 +0200

    test

---
+++ plugins/reach/lib/model/enums/SyncReachCreditTaskBatchType.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package plugins.reach
+ * @subpackage model.enum
+ */ 
+class SyncReachCreditTaskBatchType implements IKalturaPluginEnum, BatchJobType
+{
+	const SYNC_REACH_CREDIT_TASK = 'SyncReachCreditTask';
+	
+	/* (non-PHPdoc)
+	 * @see IKalturaPluginEnum::getAdditionalValues()
+	 */
+	public static function getAdditionalValues()
+	{
+		return array(
+			'SYNC_REACH_CREDIT_TASK' => self::SYNC_REACH_CREDIT_TASK,
+		);
+	}
+
+	/* (non-PHPdoc)
+	 * @see IKalturaPluginEnum::getAdditionalDescriptions()
+	 */
+	public static function getAdditionalDescriptions() app/plugins/reach/lib/model/enums/SyncReachCreditTaskBatchType.php:26
+	{
+		return array();
+	}
+}
