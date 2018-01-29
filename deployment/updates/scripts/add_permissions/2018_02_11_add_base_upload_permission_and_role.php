d5e0c0fcdf1fe8581bc97105893457d54027112b
Liron Shpinerman <liron.shpinerman@IL-TEMP-LAP.Kaltura.com>
2018-01-29 17:00:42 +0200

    PLAT-8435: Base Upload new permission and user-role

---
+++ deployment/updates/scripts/add_permissions/2018_02_11_add_base_upload_permission_and_role.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage lynx.roles_and_permissions
+ */
+
+$addPermissionsAndItemsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+$insertDefaultsScript = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/insertDefaults.php';
+
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/partner.0.ini';
+passthru("php $addPermissionsAndItemsScript $config");
+
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.uploadtoken.ini';
+passthru("php $addPermissionsAndItemsScript $config");
+
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.baseentry.ini';
+passthru("php $addPermissionsAndItemsScript $config");
+
+$config = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/init_data/03.UserRole.ini';
+passthru("php $insertDefaultsScript $config");
+
