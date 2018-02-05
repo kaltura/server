8361d0ac04bcfb77c9f328b5ef7e1538cbbd25e9
Eran Etam <eran.etam@kaltura.com>
2018-02-05 12:47:42 +0200

    add .sql to file name

---
+++ deployment/updates/sql/2018_02_05_drop_indexes.sql
@@ -0,0 +1,6 @@
+alter table caption_asset_item drop index partner_caption_asset;
+
+alter table entry drop index kshow_created_index;
+alter table entry drop index entry_FI_2;
+alter table entry drop index status_created_index;
+alter table entry drop index partner_created_at;
