d34b5af8df6e69d27fcf73092de8559b0c12d1b1
Noam Arad <noam.arad@kaltura.com>
2018-02-27 20:07:16 +0200

    PLAT-8492: add new permission name ADMIN_TAGS_ALLOW_WRITE

---
+++ deployment/updates/scripts/add_permissions/2018_02_28_add_allow_write_admin_tags_permission.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage lynx.roles_and_permissions
+ */
+
+$addPermissionsAndItemsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/partner.0.ini';
+passthru("php $addPermissionsAndItemsScript $config");
+
