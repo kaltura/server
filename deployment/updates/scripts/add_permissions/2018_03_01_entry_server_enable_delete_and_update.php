f850fbc495dce9256c868220b6e50401cbca862d
Lilach Maliniak <lilach.maliniak@kaltura.com>
2018-03-01 13:09:59 +0200

    name of deployment script refactoring

---
+++ deployment/updates/scripts/add_permissions/2018_03_01_entry_server_enable_delete_and_update.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage lynx.roles_and_permissions
+ */
+$addPermissionsAndItemsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.entryservernode.ini';
+passthru("php $addPermissionsAndItemsScript $config");
