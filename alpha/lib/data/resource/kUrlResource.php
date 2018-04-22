ca51d390ec47001cd03b5470936790032a76990d
Zohar Babin <z.babin@gmail.com>
2018-04-22 12:42:53 +0300

    Update kUrlResource.php

--- alpha/lib/data/resource/kUrlResource.php
+++ alpha/lib/data/resource/kUrlResource.php
@@ -14,7 +14,7 @@ class kUrlResource extends kContentResource
 	private $url;
 	
 	/**
-	 * Force the URL download as an asynchronous batch job (false will execute the download imidiately upon attaching the resource to an entry or asset).
+	 * Force the URL download as an asynchronous batch job (false will execute the download immediately upon attaching the resource to an entry or asset).
 	 * @var bool
 	 */
 	private $forceAsyncDownload = false;
