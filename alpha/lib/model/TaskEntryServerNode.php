a1c444d07360e67fcaf06ca128614621e41e3126
david-winder-kaltura <david.winder@kaltura.com>
2018-04-24 13:55:25 +0300

    add ;

--- alpha/lib/model/TaskEntryServerNode.php
+++ alpha/lib/model/TaskEntryServerNode.php
@@ -8,8 +8,8 @@ abstract class TaskEntryServerNode extends EntryServerNode
     protected function addTrackEntryInfo($trackEventType, $description, $entryId = null)
     {
         $desc = 'TaskId='.$this->getId().':Status='.$this->getStatus().':ServerNodeId='.$this->getServerNodeId().':'.$description;
-        $trackEventType = $trackEventType ? $trackEventType : TrackEntry::TRACK_ENTRY_EVENT_TYPE_UPDATE_ENTRY_SERVER_NODE_TASK
+        $trackEventType = $trackEventType ? $trackEventType : TrackEntry::TRACK_ENTRY_EVENT_TYPE_UPDATE_ENTRY_SERVER_NODE_TASK;
         parent::addTrackEntryInfo($trackEventType, $desc, $entryId);
     }
-    
+
 }
