c0dd19a6b6c6d302cddb90336bd20087298a5870
ZurKaltura <zur.magen@kaltura.com>
2018-04-25 11:17:37 +0300

    Mercury-13.19.0-sup-14047 improving the createDropFolderFilesFromWebex script

--- plugins/webexDropFolderPlugin/lib/model/data/kWebexHandleFilesResult.php
+++ plugins/webexDropFolderPlugin/lib/model/data/kWebexHandleFilesResult.php
@@ -6,6 +6,7 @@
 class kWebexHandleFilesResult
 {
 	const FILE_ADDED_TO_DROP_FOLDER = 'fileAddedToDropFolder';
+	const FILE_NOT_ADDED_TO_DROP_FOLDER = 'fileNotAddedToDropFolder';
 	const FILE_NOT_HANDLED = 'fileNotHandled';
 	const FILE_HANDLED = 'fileHandled';
 
