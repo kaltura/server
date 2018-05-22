957391a258f34530586dc58411649106926cc016
hilak <hila.karimov@kaltura.com>
2018-05-22 16:22:09 -0400

    PSVAMB-3216
    Only overwrite the asset's fileExt property if the incoming value is not null.

--- api_v3/lib/types/resource/KalturaStringResource.php
+++ api_v3/lib/types/resource/KalturaStringResource.php
@@ -1,42 +1,40 @@
 <?php
 /**
  * Used to ingest string content.
- * 
+ *
  * @package api
  * @subpackage objects
  */
 class KalturaStringResource extends KalturaContentResource
 {
-	const TXT_EXT = ".txt";
+    /**
+     * Textual content
+     * @var string
+     */
+    public $content;
 
-	/**
-	 * Textual content
-	 * @var string
-	 */
-	public $content;
-	
-	/* (non-PHPdoc)
-	 * @see KalturaObject::validateForUsage($sourceObject, $propertiesToSkip)
-	 */
-	public function validateForUsage($sourceObject, $propertiesToSkip = array())
-	{
-		parent::validateForUsage($sourceObject, $propertiesToSkip);
-		
-		$this->validatePropertyNotNull('content');
-	}
-	
-	/* (non-PHPdoc)
-	 * @see KalturaObject::toObject($object_to_fill, $props_to_skip)
-	 */
-	public function toObject($object_to_fill = null, $props_to_skip = array())
-	{
-		if(!$object_to_fill)
-			$object_to_fill = new kLocalFileResource();
-		
-		kFileBase::setFileContent($fname = tempnam(myContentStorage::getFSUploadsPath(), "KFR") . self::TXT_EXT, $this->content);
-		$object_to_fill->setLocalFilePath($fname);
-		$object_to_fill->setSourceType(entry::ENTRY_MEDIA_SOURCE_TEXT);
-		
-		return parent::toObject($object_to_fill, $props_to_skip);
-	}
+    /* (non-PHPdoc)
+     * @see KalturaObject::validateForUsage($sourceObject, $propertiesToSkip)
+     */
+    public function validateForUsage($sourceObject, $propertiesToSkip = array())
+    {
+        parent::validateForUsage($sourceObject, $propertiesToSkip);
+
+        $this->validatePropertyNotNull('content');
+    }
+
+    /* (non-PHPdoc)
+     * @see KalturaObject::toObject($object_to_fill, $props_to_skip)
+     */
+    public function toObject($object_to_fill = null, $props_to_skip = array())
+    {
+        if(!$object_to_fill)
+            $object_to_fill = new kLocalFileResource();
+
+        kFileBase::setFileContent($fname = tempnam(myContentStorage::getFSUploadsPath(), "KFR"), $this->content);
+        $object_to_fill->setLocalFilePath($fname);
+        $object_to_fill->setSourceType(entry::ENTRY_MEDIA_SOURCE_TEXT);
+
+        return parent::toObject($object_to_fill, $props_to_skip);
+    }
 }
