30d4a5fac02c4369ab40fc6ec5569e3bf8c8f7d1
yossipapi <yossi.papiashvili@kaltura.com>
2018-05-24 14:45:55 +0300

    Update 2018_05_24_deploy_thumb_cue_point_ready_notification.php

--- deployment/updates/scripts/2018_05_24_deploy_thumb_cue_point_ready_notification.php
+++ deployment/updates/scripts/2018_05_24_deploy_thumb_cue_point_ready_notification.php
@@ -6,7 +6,7 @@ require_once (__DIR__ . '/../../bootstrap.php');
 
 $script = realpath(dirname(__FILE__) . "/../../../tests/standAloneClient/exec.php");
 
-$newTemplateUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/thumb_cue_point_notification.xml");
+$newTemplateUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/thumb_cue_point_notification.xml");
 
 if(!file_exists($newTemplateUpdate) || !file_exists($script))
 {
