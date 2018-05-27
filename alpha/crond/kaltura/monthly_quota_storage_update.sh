513e106fe07e5e8c991a05d0cdcc1463c462b3dc
InbalBD <inbal.bendavid@kaltura.com>
2018-05-27 23:48:06 +0300

    PLAT-8721: remove partnerusage folder from path

---
+++ alpha/crond/kaltura/monthly_quota_storage_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+LOGDIR=/web/logs/investigate/`/bin/date +%Y/%m/%d`
+WHEN=$(date -d "yesterday" +%Y-%m-%d)
+
+mkdir -p $LOGDIR
+php /opt/kaltura/app/alpha/scripts/batch/validatePartnerUsage.php  null 100 >> $LOGDIR/`hostname`-BatchPartnerUsage_monthlyUpgradeProcess.log.${WHEN} 2>&1
+tail $LOGDIR/`hostname`-BatchPartnerUsage_monthlyUpgradeProcess.log.${WHEN} | mail -s "batchPartnerMonthlyUsage on `hostname`" production-it@kaltura.com
