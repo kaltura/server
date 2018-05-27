7f4848b2b7e1cf3cccb4e0a478c4041f336ab096
gotlieb <gotlieb.erez@gmail.com>
2018-05-27 15:18:02 +0300

    Reach plugin verification on template deployment

--- deployment/updates/scripts/2018_01_29_deploy_reach_notifications.php
+++ deployment/updates/scripts/2018_01_29_deploy_reach_notifications.php
@@ -4,6 +4,7 @@
  */
 require_once (__DIR__ . '/../../bootstrap.php');
 
+checkMandatoryPluginsEnabled();
 $script = realpath(dirname(__FILE__) . "/../../../tests/standAloneClient/exec.php");
 
 $taskApprovedModeration = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2018_02_22_entry_vendor_task_approved_moderation.xml");
@@ -36,4 +37,32 @@ function deployTemplate($script, $config)
 	}
 	
 	passthru("php $script $config");
+}
+
+
+/**
+ * @return bool If all required plugins are installed
+ */
+function checkMandatoryPluginsEnabled()
+{
+	$requiredPlugins = array("Reach");
+	$pluginsFilePath = realpath(dirname(__FILE__) . "/../../../configurations/plugins.ini");
+	KalturaLog::debug("Loading Plugins config from [$pluginsFilePath]");
+
+	$pluginsData = file_get_contents($pluginsFilePath);
+	foreach ($requiredPlugins as $requiredPlugin)
+	{
+		//check if plugin exists in file but is disabled
+		if(strpos($pluginsData, ";".$requiredPlugin) !== false)
+		{
+			KalturaLog::debug("[$requiredPlugin] is disabled, aborting execution");
+			exit(-2);
+		}
+
+		if(strpos($pluginsData, $requiredPlugin) === false)
+		{
+			KalturaLog::debug("[$requiredPlugin] not found in plugins data, aborting execution");
+			exit(-2);
+		}
+	}
 }
--- deployment/updates/scripts/2018_01_29_deploy_reach_vendor_related_response_profiles.php
+++ deployment/updates/scripts/2018_01_29_deploy_reach_vendor_related_response_profiles.php
@@ -7,6 +7,8 @@
  * No need to re-run after server code deploy
  */
 require_once (__DIR__ . '/../../bootstrap.php');
+checkMandatoryPluginsEnabled();
+
 $script = realpath(dirname(__FILE__) . '/../../../') . '/tests/standAloneClient/exec.php';
 
 $config = realpath(dirname(__FILE__)) . '/../../updates/scripts/xml/responseProfiles/reach_vendor_response_profiles.xml';
@@ -14,3 +16,31 @@ if(!file_exists($config))
 	KalturaLog::err("Missing file [$config] will not deploy");
 
 passthru("php $script $config");
+
+
+/**
+ * @return bool If all required plugins are installed
+ */
+function checkMandatoryPluginsEnabled()
+{
+	$requiredPlugins = array("Reach");
+	$pluginsFilePath = realpath(dirname(__FILE__) . "/../../../configurations/plugins.ini");
+	KalturaLog::debug("Loading Plugins config from [$pluginsFilePath]");
+
+	$pluginsData = file_get_contents($pluginsFilePath);
+	foreach ($requiredPlugins as $requiredPlugin)
+	{
+		//check if plugin exists in file but is disabled
+		if(strpos($pluginsData, ";".$requiredPlugin) !== false)
+		{
+			KalturaLog::debug("[$requiredPlugin] is disabled, aborting execution");
+			exit(-2);
+		}
+
+		if(strpos($pluginsData, $requiredPlugin) === false)
+		{
+			KalturaLog::debug("[$requiredPlugin] not found in plugins data, aborting execution");
+			exit(-2);
+		}
+	}
+}
