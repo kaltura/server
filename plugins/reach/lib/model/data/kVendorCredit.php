e47dd3d964af034dd72f57c736b3ea9b928d3a02
gotlieb <gotlieb.erez@gmail.com>
2018-06-03 15:31:19 +0300

    Reach credit sync when credit was not synced in the current day

--- plugins/reach/lib/model/data/kVendorCredit.php
+++ plugins/reach/lib/model/data/kVendorCredit.php
@@ -141,6 +141,11 @@ class kVendorCredit
 	{
 	}
 
+	public function isSynced()
+	{
+		return (intval(time() / 86400) == (intval($this->lastSyncTime / 86400)));
+	}
+
 	public function syncCredit($reachProfileId)
 	{
 		$c = new Criteria();
