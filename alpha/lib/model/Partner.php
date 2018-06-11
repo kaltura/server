19426a2a473f2eb474bc7edf92853f502377c0ff
Noam Arad <noam.arad@kaltura.com>
2018-06-11 13:41:39 +0300

    PLAT-8985: from code review

--- alpha/lib/model/Partner.php
+++ alpha/lib/model/Partner.php
@@ -70,6 +70,14 @@ class Partner extends BasePartner
 
 	private $partnerUsagePercent;
 
+	const CUSTOM_DATA_LIVE_STREAM_INPUTS = 'live_stream_inputs';
+
+	const CUSTOM_DATA_LIVE_STREAM_OUTPUTS = 'live_stream_outputs';
+
+	const PARTNER_MAX_LIVE_STREAM_INPUTS_DEFAULT = 10;
+
+	const PARTNER_MAX_LIVE_STREAM_OUTPUTS_DEFAULT = 1;
+
 	public function save(PropelPDO $con = null)
 	{
 		PartnerPeer::removePartnerFromCache( $this->getId() );
@@ -983,8 +991,8 @@ class Partner extends BasePartner
 	public function setMonthlyStorageAndBandwidth($v)	{$this->putInCustomData('monthly_storage_and_bandwidth', $v);}
 	public function setEndUsers($v)						{$this->putInCustomData('end_users', $v);}
 	public function setAccessControls($v)				{$this->putInCustomData('access_controls', $v);}
-	public function setMaxLiveStreamInputs($v)			{$this->putInCustomData('live_stream_inputs', $v);}
-	public function setMaxLiveStreamOutputs($v)			{$this->putInCustomData('live_stream_outputs', $v);}
+	public function setMaxLiveStreamInputs($v)			{$this->putInCustomData(self::CUSTOM_DATA_LIVE_STREAM_INPUTS, $v);}
+	public function setMaxLiveStreamOutputs($v)			{$this->putInCustomData(self::CUSTOM_DATA_LIVE_STREAM_OUTPUTS, $v);}
 	public function setMaxLiveRtcStreamInputs($v)		{$this->putInCustomData('live_rtc_stream_inputs', $v);}
 	
 	public function setLoginUsersOveragePrice($v)		{$this->putInCustomData('login_users_overage_price', $v);}
@@ -1036,17 +1044,17 @@ class Partner extends BasePartner
 	public function getAccessControls()					{return $this->getFromCustomData('access_controls', null, self::MAX_ACCESS_CONTROLS);}
 	public function getMaxLiveStreamInputs()
 	{
-		$live_stream_inputs = $this->getFromCustomData('live_stream_inputs');
+		$live_stream_inputs = $this->getFromCustomData(self::CUSTOM_DATA_LIVE_STREAM_INPUTS);
 		if ($live_stream_inputs === null)
-			$live_stream_inputs = kConf::get('partner_max_live_stream_inputs', 'local', 10);
+			$live_stream_inputs = kConf::get('partner_max_live_stream_inputs', 'local', self::PARTNER_MAX_LIVE_STREAM_INPUTS_DEFAULT);
 		
 		return $live_stream_inputs;
 	}
 	public function getMaxLiveStreamOutputs()
 	{
-		$live_stream_outputs = $this->getFromCustomData('live_stream_outputs');
+		$live_stream_outputs = $this->getFromCustomData(self::CUSTOM_DATA_LIVE_STREAM_OUTPUTS);
 		if ($live_stream_outputs === null)
-			$live_stream_outputs = kConf::get('partner_max_live_stream_outputs', 'local', 1);
+			$live_stream_outputs = kConf::get('partner_max_live_stream_outputs', 'local', self::PARTNER_MAX_LIVE_STREAM_OUTPUTS_DEFAULT);
 		
 		return $live_stream_outputs;
 	}
