e8056f84db50cbcefa4195a7f7238ecb7f7ca3e1
Eran Sakal <eran@sakalim.com>
2018-07-17 13:01:03 +0300

    add ext login support

---
+++ alpha/apps/kaltura/modules/kmcng/actions/extloginAction.class.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * @package    Core
+ * @subpackage KMCNG
+ */
+class extloginAction extends kalturaAction
+{
+
+	private function dieOnError($error_code)
+	{
+		if ( is_array ( $error_code ) )
+		{
+			$args = $error_code;
+			$error_code = $error_code[0];
+		}
+		else
+		{
+			$args = func_get_args();
+		}
+		array_shift($args);
+
+		$errorData = APIErrors::getErrorData( $error_code, $args );
+		$error_code = $errorData['code'];
+		$formated_desc = $errorData['message'];
+
+		header("X-Kaltura:error-$error_code");
+		header('X-Kaltura-App: exiting on error '.$error_code.' - '.$formated_desc);
+
+		die();
+	}
+
+	public function execute()
+	{
+		$ks = $this->getP ( "ks" );
+		if(!$ks)
+			$this->dieOnError  ( APIErrors::MISSING_KS );
+
+		$requestedPartnerId = $this->getP ( "partner_id" );
+
+		$expired = $this->getP ( "exp" );
+
+		$ksObj = kSessionUtils::crackKs($ks);
+		$ksPartnerId = $ksObj->partner_id;
+
+		if($ksObj->getPrivilegeByName(kSessionBase::PRIVILEGE_DISABLE_PARTNER_CHANGE_ACCOUNT) && $requestedPartnerId != $ksPartnerId)
+			$this->dieOnError  ( APIErrors::PARTNER_CHANGE_ACCOUNT_DISABLED );
+
+		if (!$requestedPartnerId) {
+			$requestedPartnerId = $ksPartnerId;
+		}
+
+		try {
+			$adminKuser = UserLoginDataPeer::userLoginByKs($ks, $requestedPartnerId, true);
+		}
+		catch (kUserException $e) {
+			$code = $e->getCode();
+			if ($code == kUserException::USER_NOT_FOUND) {
+				$this->dieOnError  ( APIErrors::ADMIN_KUSER_NOT_FOUND );
+			}
+			if ($code == kUserException::LOGIN_DATA_NOT_FOUND) {
+				$this->dieOnError  ( APIErrors::ADMIN_KUSER_NOT_FOUND );
+			}
+			else if ($code == kUserException::LOGIN_RETRIES_EXCEEDED) {
+				$this->dieOnError  ( APIErrors::LOGIN_RETRIES_EXCEEDED );
+			}
+			else if ($code == kUserException::LOGIN_BLOCKED) {
+				$this->dieOnError  ( APIErrors::LOGIN_BLOCKED );
+			}
+			else if ($code == kUserException::PASSWORD_EXPIRED) {
+				$this->dieOnError  ( APIErrors::PASSWORD_EXPIRED );
+			}
+			else if ($code == kUserException::WRONG_PASSWORD) {
+				$this->dieOnError  (APIErrors::ADMIN_KUSER_NOT_FOUND);
+			}
+			else if ($code == kUserException::USER_IS_BLOCKED) {
+				$this->dieOnError  (APIErrors::USER_IS_BLOCKED);
+			}
+			$this->dieOnError  ( APIErrors::INTERNAL_SERVERL_ERROR );
+		}
+		if (!$adminKuser || !$adminKuser->getIsAdmin()) {
+			$this->dieOnError  ( APIErrors::ADMIN_KUSER_NOT_FOUND );
+		}
+
+
+		if ($requestedPartnerId != $adminKuser->getPartnerId()) {
+			$this->dieOnError  ( APIErrors::UNKNOWN_PARTNER_ID );
+		}
+
+		$partner = PartnerPeer::retrieveByPK( $adminKuser->getPartnerId() );
+
+		if (!$partner)
+		{
+			$this->dieOnError  ( APIErrors::UNKNOWN_PARTNER_ID );
+		}
+
+		if (!$partner->validateApiAccessControl())
+		{
+			$this->dieOnError  ( APIErrors::SERVICE_ACCESS_CONTROL_RESTRICTED );
+		}
+
+		$partner_id = $partner->getId();
+		$subp_id = $partner->getSubpId() ;
+		$admin_puser_id = $adminKuser->getPuserId();
+
+		$exp = (isset($expired) && is_numeric($expired)) ? time() + $expired: 0;
+
+		$noUserInKs = is_null($ksObj->user) || $ksObj->user === '';
+
+		if ( ($ksPartnerId != $partner_id) || ($noUserInKs) )
+		{
+			$ks = null;
+			$sessionType = $adminKuser->getIsAdmin() ? SessionType::ADMIN : SessionType::USER;
+			kSessionUtils::createKSessionNoValidations ( $partner_id ,  $admin_puser_id , $ks , 30 * 86400 , $sessionType , "" , "*," . kSessionBase::PRIVILEGE_DISABLE_ENTITLEMENT );
+		}
+
+
+		$use_ssl = (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on');
+
+		$redirect_url =  ($use_ssl) ? 'https' : 'http';
+		$redirect_url .= '://' . $_SERVER["HTTP_HOST"] . '/index.php/kmcng/actions/login-by-ks/' . $ks;
+		$this->redirect($redirect_url);
+	}
+
+}
