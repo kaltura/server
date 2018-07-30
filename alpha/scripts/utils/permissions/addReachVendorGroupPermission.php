f5db5d0c9038366d0087e00a937c62368c5de6e5
yossipapi <yossi.papiashvili@kaltura.com>
2018-07-30 10:24:49 +0300

    No-plat: support re-using vendor permission if exists

--- alpha/scripts/utils/permissions/addReachVendorGroupPermission.php
+++ alpha/scripts/utils/permissions/addReachVendorGroupPermission.php
@@ -14,21 +14,21 @@ $c = new Criteria();
 $c->add(PermissionPeer::PARTNER_ID, $partnerId);
 $c->add(PermissionPeer::NAME, "REACH_VENDOR_PARTNER_GROUP_*_PERMISSION");
 $c->add(PermissionPeer::STATUS, PermissionStatus::ACTIVE);
-$existingPermission = PermissionPeer::doSelectOne($c);
+$permission = PermissionPeer::doSelectOne($c);
 
-if($existingPermission)
-	die("Partner id provided [$partnerId] already has required permission" . PHP_EOL);
-
-$permission = new Permission();
-$permission->setPartnerId($partnerId);
-$permission->setType(PermissionType::PARTNER_GROUP);
-$permission->setName("REACH_VENDOR_PARTNER_GROUP_*_PERMISSION");
-$permission->setFriendlyName("REACH Vendor group permission");
-$permission->setDescription("Reach permission for all partners");
-$permission->setDependsOnPermissionNames("REACH_VENDOR_PARTNER_PERMISSION");
-$permission->setPartnerGroup("*");
-$permission->setStatus(PermissionStatus::ACTIVE);
-$permission->save();
+if(!$permission)
+{
+	$permission = new Permission();
+	$permission->setPartnerId($partnerId);
+	$permission->setType(PermissionType::PARTNER_GROUP);
+	$permission->setName("REACH_VENDOR_PARTNER_GROUP_*_PERMISSION");
+	$permission->setFriendlyName("REACH Vendor group permission");
+	$permission->setDescription("Reach permission for all partners");
+	$permission->setDependsOnPermissionNames("REACH_VENDOR_PARTNER_PERMISSION");
+	$permission->setPartnerGroup("*");
+	$permission->setStatus(PermissionStatus::ACTIVE);
+	$permission->save();
+}
 
 echo "Done, created permission with id [{$permission->getId()}]" . PHP_EOL;
 
