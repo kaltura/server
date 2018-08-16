1cbc8a87d3b6bd28cc0e34f8d1fb89bf6737bba9
MosheMaorKaltura <moshe.maor@kaltura.com>
2018-08-16 09:43:37 +0300

    PLAT-8932

---
+++ configurations/configCacheParams.php
@@ -0,0 +1,4 @@
+<?php
+$finalCacheSourceConfiguration=array("port"=>"11212","host"=>"127.0.0.1");//URL of the final cache source should be set different per dc
+$localCacheSourceConfiguration=array("port"=>"11211","host"=>"127.0.0.1");
+
