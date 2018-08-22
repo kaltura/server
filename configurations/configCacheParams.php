9cbd13082bf1d1bb2323b201c35e02ac34492395
MosheMaorKaltura <moshe.maor@kaltura.com>
2018-08-22 13:19:11 +0300

    remove files

--- configurations/configCacheParams.php
+++
@@ -1,4 +0,0 @@
-<?php
-$remoteCacheSourceConfiguration=array("port"=>"11212","host"=>"127.0.0.1");//URL of the final cache source should be set different per dc
-$localCacheSourceConfiguration=array("port"=>"11211","host"=>"127.0.0.1");
-
