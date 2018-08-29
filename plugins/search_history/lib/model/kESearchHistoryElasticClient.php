9e1e3cfe689b863c0dfdcbd1b7aba4cbf88ba407
Nadav Harnik <nadav.harnik@kaltura.com>
2018-08-29 08:01:12 +0300

    PLAT-7981: change delete handling + add doc ids

--- plugins/search_history/lib/model/kESearchHistoryElasticClient.php
+++ plugins/search_history/lib/model/kESearchHistoryElasticClient.php
@@ -10,6 +10,10 @@ class kESearchHistoryElasticClient
 	const TYPE_KEY = 'type';
 	const BODY_KEY = 'body';
 	const QUERY_KEY = 'query';
+	const ACTION_KEY = '_action';
+	const DELETE_KEY = 'delete';
+	const IDS_TO_DELETE = 'ids_to_delete';
+	const MAX_SEARCH_TERMS_TO_DELETE = 1000;
 
 	protected $client;
 
@@ -39,7 +43,7 @@ class kESearchHistoryElasticClient
 		$deleteByQuery->addToFilter($partnerTerm);
 		$kuserTerm = new kESearchTermQuery(ESearchHistoryFieldName::KUSER_ID, $kuserId);
 		$deleteByQuery->addToFilter($kuserTerm);
-		$searchContextTerm = new kESearchTermQuery(ESearchHistoryFieldName::SEARCH_CONTEXT, searchHistoryUtils::getSearchContext());
+		$searchContextTerm = new kESearchTermQuery(ESearchHistoryFieldName::SEARCH_CONTEXT, elasticSearchUtils::formatSearchTerm(searchHistoryUtils::getSearchContext()));
 		$deleteByQuery->addToFilter($searchContextTerm);
 		$searchTermQuery = new kESearchTermQuery(ESearchHistoryFieldName::SEARCH_TERM, elasticSearchUtils::formatSearchTerm($searchTerm));
 		$deleteByQuery->addToFilter($searchTermQuery);
@@ -47,11 +51,34 @@ class kESearchHistoryElasticClient
 			self::INDEX_KEY => ESearchHistoryIndexMap::SEARCH_HISTORY_SEARCH_ALIAS,
 			self::TYPE_KEY => ESearchHistoryIndexMap::SEARCH_HISTORY_TYPE,
 			self::BODY_KEY => array(
+				kESearchQueryManager::FROM_KEY => 0,
+				kESearchQueryManager::SIZE_KEY => self::MAX_SEARCH_TERMS_TO_DELETE,
 				self::QUERY_KEY => $deleteByQuery->getFinalQuery()
 			)
 		);
 
-		$this->client->deleteByQuery($query);
+		$result = $this->client->search($query, true);
+		$ids = kESearchHistoryCoreAdapter::getIdsToDeleteFromHitsResults($result);
+		if (!$ids)
+			return;
+
+		$body = array(
+			self::ACTION_KEY => self::DELETE_KEY,
+			'_'.self::TYPE_KEY => ESearchHistoryIndexMap::SEARCH_HISTORY_TYPE,
+			self::IDS_TO_DELETE => $ids
+		);
+		$document = json_encode($body);
+		try
+		{
+			$constructorArgs['exchangeName'] = kESearchHistoryManager::HISTORY_EXCHANGE_NAME;
+			$queueProvider = QueueProvider::getInstance(null, $constructorArgs);
+			$queueProvider->send(kESearchHistoryManager::HISTORY_QUEUE_NAME, $document);
+		}
+		catch (Exception $e)
+		{
+			//don't fail the request, just log
+			KalturaLog::err("cannot connect to rabbit");
+		}
 	}
 
 	public function searchRecentForUser($queryBody)
