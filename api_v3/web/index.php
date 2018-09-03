ec51f321aa3b2fbb6f23846f71e4d4f25f714939
Muli Dayan <muli.dayan@kaltura.com>
2018-09-03 09:30:44 +0300

    add Access-Control-Max-Age header to API calls
    See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age.

--- api_v3/web/index.php
+++ api_v3/web/index.php
@@ -7,6 +7,7 @@ if($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
 	header('Access-Control-Allow-Origin: *');
 	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Range, Cache-Control');
 	header('Access-Control-Allow-Methods: POST, GET, HEAD, OPTIONS');
+	header('Access-Control-Max-Age: 86400');
 	exit;
 }
 
