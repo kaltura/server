853580e7872e66dec2a88cb59c37b3130fe144d6
KALTURA\roie.beck <Roie.Beck@kaltura.com>
2018-09-16 14:32:28 +0300

    Commit preety print

--- deployment/updates/sql/2018_08_28_create_vendor_integration_table.sql
+++ deployment/updates/sql/2018_08_28_create_vendor_integration_table.sql
@@ -3,10 +3,11 @@ CREATE TABLE  IF NOT EXISTS `vendor_integration` (
 	`account_id` varchar(64) NOT NULL,
 	`partner_id` int NOT NULL,
 	`vendor_Type` smallint(6) NOT NULL,
-	`custom_data` text, PRIMARY KEY (`id`),
+	`custom_data` text,
 	`status` smallint(6) NOT NULL,
 	`created_at` DATETIME,
 	`updated_at` DATETIME,
+	PRIMARY KEY (`id`),
 	KEY `partner_id_vendor_Type_status_index`(`partner_id`,`vendor_Type`,`status`),
 	KEY `account_id_vendor_Type_index`(`account_id`,`vendor_Type`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
