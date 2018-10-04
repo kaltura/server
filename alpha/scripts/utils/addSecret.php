9f0f87d88404cd99a5567cfdf42d0c170173c194
KALTURA\roie.beck <Roie.Beck@kaltura.com>
2018-10-04 16:03:50 +0300

    return intellij warning Conding convention

--- alpha/scripts/utils/addSecret.php
+++ alpha/scripts/utils/addSecret.php
@@ -1,6 +1,4 @@
 <?php
-/** @noinspection PhpParamsInspection */
-/** @noinspection PhpUndefinedMethodInspection */
 
 require_once(__DIR__ . '/../bootstrap.php');
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
