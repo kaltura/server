18037481a661ffa478a4d6d5dfe0a1f07ae07516
MosheMaorKaltura <moshe.maor@kaltura.com>
2018-10-05 06:01:17 +0300

    Plat9237: seperate deplyment scripts

--- deployment/updates/scripts/add_permissions/2018_10_03_update_groupuser_sync_action.php
+++ deployment/updates/scripts/add_permissions/2018_10_03_update_groupuser_sync_action.php
@@ -4,12 +4,6 @@
  * @subpackage mercury.roles_and_permissions
  */
 
-$removePermissionsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/removePermissionsAndItems.php';
-
-$removeConfig = realpath(dirname(__FILE__)) . '/../ini_files/2018_10_02_bulk_update_permissions.ini';
-passthru("php $removePermissionsScript $removeConfig");
-
-
 $addPermissionsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
 
 $addConfig = realpath(dirname(__FILE__)) . '/../../../permissions/service.groupuser.ini';
