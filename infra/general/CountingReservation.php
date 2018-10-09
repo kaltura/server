399c7f4153035a6380aed66061cf222bc5c9ea38
InbalBD <inbal.bendavid@kaltura.com>
2018-10-09 06:25:25 +0300

    PLAT-8342: convert string to int

--- infra/general/CountingReservation.php
+++ infra/general/CountingReservation.php
@@ -52,9 +52,9 @@ class CountingReservation
 		$cacheCounter = $this->cache->get($this->getCacheKeyForResource($resourceId));
 		if ($cacheCounter)
 		{
-			$cacheCounter = trim($cacheCounter);
+			$cacheCounter = (int)($cacheCounter);
 			KalturaLog::info("Resource id [$resourceId] is already stored. Existing counter value: [$cacheCounter]");
-			$cacheCounter = (int)$cacheCounter - 1;
+			$cacheCounter = $cacheCounter - 1;
 			if ($cacheCounter <= 0)
 			{
 				KalturaLog::info("Resource was acquired more then [$this->maxValue] times");
