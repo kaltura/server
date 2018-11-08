bd4b9915c4d751a24c7032bb5a1f7f52b459166c
MosheMaorKaltura <moshe.maor@kaltura.com>
2018-11-08 09:12:26 +0200

    PLAT-9393:add permission BASE_UPLOAD_PERMISSION to media:addFromUploadedFile

---
+++ deployment/updates/scripts/add_permissions/2018_11_08_update_media_addfromuploadedfile.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage mercury.roles_and_permissions
+ */
+$script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.media.ini';
+passthru("php $script $config");
