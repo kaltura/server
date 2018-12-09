d4c600995037feb641d6d4549875cc4c703648cd
david-winder-kaltura <david.winder@kaltura.com>
2018-12-09 11:54:50 +0200

    Formatting and rename

--- plugins/cue_points/base/batch/copyCuePoints/Engine/KLiveClippingCopyCuePointEngine.php
+++ plugins/cue_points/base/batch/copyCuePoints/Engine/KLiveClippingCopyCuePointEngine.php
@@ -14,7 +14,9 @@ class KLiveClippingCopyCuePointEngine extends KLiveToVodCopyCuePointEngine
         $cuePointEndTime = $this->getOffsetForTimestamp($cuePoint->calculatedEndTime * 1000, false);
         KalturaLog::debug("Checking times to know if copy is needed for id[". $cuePoint->id ."]: start- [$cuePointStartTime], end- [$cuePointEndTime], calculatedEndTime - " . $cuePoint->calculatedEndTime);
         if ($cuePointStartTime < 0 && is_null($cuePoint->calculatedEndTime))
-            return $this->checkShouldCopyCuePointBeforeTimeWindow($cuePoint); //if cue point started before the clip but end afterward (no next cue point)
+        {
+            return $this->shouldCopyCuePointBeforeTimeWindow($cuePoint); //if cue point started before the clip but end afterward (no next cue point)
+        }
         return ($cuePointStartTime >= 0 || $cuePointEndTime > 0);
     }
 
@@ -29,7 +31,7 @@ class KLiveClippingCopyCuePointEngine extends KLiveToVodCopyCuePointEngine
      * @param KalturaCuePoint $cuePoint
      * @return boolean
      */
-    private function checkShouldCopyCuePointBeforeTimeWindow($cuePoint)
+    private function shouldCopyCuePointBeforeTimeWindow($cuePoint)
     {
         $noCopiedTag = array("poll-data","select-poll-state","poll-results");
         return (count(array_intersect(explode(",", $cuePoint->tags), $noCopiedTag)) == 0);
