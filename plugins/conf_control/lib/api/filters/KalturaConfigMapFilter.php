1b2c0b1666cd34e0ef1dbffdaef6eda306eedc7e
MosheMaorKaltura <moshe.maor@kaltura.com>
2018-12-21 01:50:20 +0200

    plugins/conf_control/lib/api/filters/

--- plugins/conf_control/lib/api/filters/KalturaConfigMapFilter.php
+++ plugins/conf_control/lib/api/filters/KalturaConfigMapFilter.php
@@ -3,26 +3,6 @@
  * @package plugins.confControl
  * @subpackage api.filters
  */
-class KalturaConfigMapFilter extends KalturaFilter
+class KalturaConfigMapFilter extends KalturaConfigMapBaseFilter
 {
-	/**
-	 * Name of the map
-	 *
-	 * @var string
-	 * @insertonly
-	 */
-	public $name;
-
-	/**
-	 * Regex that represent the host/s that this map affect
-	 *
-	 * @var string
-	 */
-	public $relatedHost;
-
-
-	public function getCoreFilter()
-	{
-		return null;
-	}
 }
