b64e746f4a739f048edc6ffc8074dd73cf56f95e
Nadav Harnik <nadav.harnik@kaltura.com>
2019-01-22 15:22:44 +0200

    esearch - update mapping + add logstash playsviews conf

--- plugins/search/providers/elastic_search/lib/api/enum/KalturaESearchEntryOrderByFieldName.php
+++ plugins/search/providers/elastic_search/lib/api/enum/KalturaESearchEntryOrderByFieldName.php
@@ -14,4 +14,10 @@ class KalturaESearchEntryOrderByFieldName extends KalturaStringEnum
 	const VIEWS = 'views';
 	const PLAYS = 'plays';
 	const LAST_PLAYED_AT = 'last_played_at';
+	const VIEWS_LAST_30_DAYS = 'views_last_30_days';
+	const PLAYS_LAST_30_DAYS = 'plays_last_30_days';
+	const VIEWS_LAST_7_DAYS = 'views_last_7_days';
+	const PLAYS_LAST_7_DAYS = 'plays_lst_7_days';
+	const VIEWS_LAST_1_DAYS = 'views_last_1_days';
+	const PLAYS_LAST_1_DAYS = 'plays_last_1_days';
 }
