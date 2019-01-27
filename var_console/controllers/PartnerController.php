36104e5ff44ff1f0417d77a6cba0b206f86b3184
yossipapi <yossi.papiashvili@kaltura.com>
2019-01-27 11:56:40 +0200

    No-Plat: Limit change account to only partners which the ma logged in user belongs to

--- var_console/controllers/PartnerController.php
+++ var_console/controllers/PartnerController.php
@@ -220,7 +220,6 @@ class PartnerController extends Zend_Controller_Action
 	{
 		$impersonatedPartnerId = $this->_getParam('partner_id');
 		$userId = $this->_getParam('user_id');
-		$maLogenInUserId = Zend_Auth::getInstance()->getIdentity()->getUser()->id;;
 		$client = Infra_ClientHelper::getClient();
 		$client->startMultiRequest();
 		$client->partner->getInfo();
@@ -249,19 +248,10 @@ class PartnerController extends Zend_Controller_Action
 		$adminSecret = $result[0]->adminSecret;
 		$partnerId =  $result[0]->id;
 		
-		if($userId != $maLogenInUserId)
+		if($userId != Zend_Auth::getInstance()->getIdentity()->getUser()->id)
 			return $client->session->impersonate($adminSecret, $impersonatedPartnerId, $userId, Kaltura_Client_Enum_SessionType::ADMIN, $partnerId, null, "disableentitlement,enablechangeaccount:$impersonatedPartnerId");
 		
-		$kalturaPartnerFilter = new Kaltura_Client_Type_PartnerFilter();
-		$kalturaPartnerFilter->statusEqual = 1;
-		$userKs = $result[count($result)-1];
-		$masterKs = $client->getKs();
-		$client->setKs($userKs);
-		$partnersResult = $client->partner->listPartnersForUser($kalturaPartnerFilter);
-		$partnersId = self::getPartnersIdsFromPartnerListResponse($partnersResult);
-		$client->setKs($masterKs);
-		$ks = $client->session->impersonate($adminSecret, $impersonatedPartnerId, $userId, Kaltura_Client_Enum_SessionType::ADMIN, $partnerId, null, "disableentitlement,enablechangeaccount:".$partnersId);
-
+		$ks = $client->session->impersonate($adminSecret, $impersonatedPartnerId, $userId, Kaltura_Client_Enum_SessionType::ADMIN, $partnerId, null, "disableentitlement");
 		return $ks;
 	}
 
