df60d4f2fed3dd68a1f2c218eade696ecac4e104
yossipapi <yossi.papiashvili@kaltura.com>
2019-01-31 13:57:32 +0200

    PHP7: KalturaException expects 3 argument arguments but only 2 passed

--- plugins/bulk_upload/xml/batch/BulkUploadEngineXml.php
+++ plugins/bulk_upload/xml/batch/BulkUploadEngineXml.php
@@ -722,7 +722,7 @@ class BulkUploadEngineXml extends KBulkUploadEngine
 			foreach($requestResults as $requestResult)
 			{
 				if(is_array($requestResult) && isset($requestResult['code']))
-					throw new KalturaException($requestResult['message'], $requestResult['code']);
+					throw new KalturaException($requestResult['message'], $requestResult['code'], $requestResult['args']);
 				
 				if($requestResult instanceof Exception)
 					throw $requestResult;
