655ffb4d9125e96a34768062594b3980b3958bed
MosheMaorKaltura <moshe.maor@kaltura.com>
2019-02-11 11:14:21 +0200

    PLAT-9661: Update logic of setting Last-Modified of thumbnail

--- alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
@@ -341,7 +341,8 @@ class thumbnailAction extends sfAction
 		}
 		
 		// not allow capturing frames if the partner has FEATURE_DISALLOW_FRAME_CAPTURE permission
-		if(($vid_sec != -1) || ($vid_slice != -1) || ($vid_slices != -1))
+		$isCapturing = ($vid_sec != -1) || ($vid_slice != -1) || ($vid_slices != -1);
+		if($isCapturing)
 		{
 			if ($partner->getEnabledService(PermissionName::FEATURE_BLOCK_THUMBNAIL_CAPTURE))
 			{
@@ -523,8 +524,8 @@ class thumbnailAction extends sfAction
 			$cache = new myCache("thumb", 2592000); // 30 days, the max memcache allows
 		}
 
-		$isCapturing = ($vid_sec != -1) || ($vid_slice != -1) || ($vid_slices != -1);
-		$lastModified = $this->getLastModifiled($entry, $isCapturing);
+
+		$lastModified = $this->getLastModified($entry, $isCapturing);
 
 		$entryKey = kFileUtils::isFileEncrypt($tempThumbPath) ? $entry->getGeneralEncryptionKey() : null;
 		$entryIv = $entryKey ? $entry->getEncryptionIv() : null;
@@ -548,27 +549,21 @@ class thumbnailAction extends sfAction
 		// however dumpfile dies at the end so we cant just write it here (maybe register a shutdown callback)
 	}
 
-	protected function getLastModifiled(entry $entry, $isCapturing)
+	protected function getLastModified(entry $entry, $isCapturing)
 	{
-		$lastModified =  null;
-		if($isCapturing)
-		{
-			$lastModifiedFlavor = assetPeer::retrieveLastModifiedFlavorByEntryId($entry->getId());
-			$lastModified = $lastModifiedFlavor ? $lastModifiedFlavor->getUpdatedAt(null) : null;
-		}
-		else
+		if(!$isCapturing)
 		{
 			$entryImageSyncKey = $entry->getSyncKey(entry::FILE_SYNC_ENTRY_SUB_TYPE_THUMB);
-			$fileSyncs= kFileSyncUtils::getAllReadyFileSyncForKey($entryImageSyncKey);
-			foreach ($fileSyncs as $fileSync)
+			$fileSync= kFileSyncUtils::getOriginFileSyncForKey($entryImageSyncKey);
+			if($fileSync)
 			{
-				if($fileSync->getOriginal())
-				{
-					$lastModified = $fileSync->getUpdatedAt(null);
-					break;
-				}
+				$lastModified = $fileSync->getUpdatedAt(null);
+				return $lastModified;
 			}
 		}
+		$lastModifiedFlavor = assetPeer::retrieveLastModifiedFlavorByEntryId($entry->getId());
+		$lastModified = $lastModifiedFlavor ? $lastModifiedFlavor->getUpdatedAt(null) : null;
+
 		return $lastModified;
 	}
 
