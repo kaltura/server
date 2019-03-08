a9463c5622026a4627a19c8e3da8b35c9ebc5738
ravitshalem <ravit.shalem@kaltura.com>
2019-03-08 02:21:04 +0200

    PLAT-9752:sorting by user id and by screen name in esearch-search entry

--- plugins/search/providers/elastic_search/lib/model/enum/ESearchUserOrderByFieldName.php
+++ plugins/search/providers/elastic_search/lib/model/enum/ESearchUserOrderByFieldName.php
@@ -7,4 +7,6 @@ interface ESearchUserOrderByFieldName extends BaseEnum
 {
     const UPDATED_AT = 'updated_at';
     const CREATED_AT = 'created_at';
+    const SCREEN_NAME = 'screen_name.raw';
+    const USER_ID = 'puser_id.raw';
 }
