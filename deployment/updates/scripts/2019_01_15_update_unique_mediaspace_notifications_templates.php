bbcb0ceed7e18e06f80114eaa4b7d8b14881fefa
yusitnikov <y.sitnikoff@gmail.com>
2019-03-13 14:23:42 +0200

    PSVAMB-4944 PSVAMB-4967 KMS Email Notifications - deployment script - point to the final XML file

--- deployment/updates/scripts/2019_01_15_update_unique_mediaspace_notifications_templates.php
+++ deployment/updates/scripts/2019_01_15_update_unique_mediaspace_notifications_templates.php
@@ -9,7 +9,7 @@ require_once (DEPLOYMENT_DIR . '/bootstrap.php');
 
 $scriptPath = realpath(DEPLOYMENT_DIR . '/../tests/standAloneClient/exec.php');
 
-$templateUpdateXmlPath = realpath(DEPLOYMENT_DIR . '/updates/scripts/xml/notifications/2019_01_15_update_unique_mediaspace_notifications_templates.template.xml');
+$templateUpdateXmlPath = realpath(DEPLOYMENT_DIR . '/updates/scripts/xml/notifications/2019_01_15_update_unique_mediaspace_notifications_templates.xml');
 
 if(!file_exists($templateUpdateXmlPath) || !file_exists($scriptPath))
 {
