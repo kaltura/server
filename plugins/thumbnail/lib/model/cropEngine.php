6850746685312b7640f8593b98cd4fdc87806510
ZurKaltura <zur.magen@kaltura.com>
2019-03-31 16:35:23 +0300

    Changing focus to be enum

--- plugins/thumbnail/lib/model/cropEngine.php
+++ plugins/thumbnail/lib/model/cropEngine.php
@@ -6,17 +6,19 @@
 
 class cropEngine
 {
-	function thumbnail($image, $new_w, $new_h, $focus = 'center')
+	function thumbnail($image, $new_w, $new_h, $focus = kCropGravityPoint::CENTER)
 	{
 		$image->setImagePage(0, 0, 0, 0);
 		$w = $image->getImageWidth();
 		$h = $image->getImageHeight();
 
-		if ($w > $h) {
+		if ($w > $h)
+		{
 			$resize_w = $w * $new_h / $h;
 			$resize_h = $new_h;
 		}
-		else {
+		else
+		{
 			$resize_w = $new_w;
 			$resize_h = $h * $new_w / $w;
 		}
@@ -24,24 +26,12 @@ class cropEngine
 		$image->resizeImage($resize_w, $resize_h, Imagick::FILTER_LANCZOS, 0.9);
 
 		switch ($focus) {
-			case 'northwest':
+			case kCropGravityPoint::TOP:
 				$image->cropImage($new_w, $new_h, 0, 0);
 				break;
 
-			case 'center':
+			case kCropGravityPoint::CENTER:
 				$image->cropImage($new_w, $new_h, ($resize_w - $new_w) / 2, ($resize_h - $new_h) / 2);
-				break;
-
-			case 'northeast':
-				$image->cropImage($new_w, $new_h, $resize_w - $new_w, 0);
-				break;
-
-			case 'southwest':
-				$image->cropImage($new_w, $new_h, 0, $resize_h - $new_h);
-				break;
-
-			case 'southeast':
-				$image->cropImage($new_w, $new_h, $resize_w - $new_w, $resize_h - $new_h);
 				break;
 		}
 	}
