651309b97df15fa0e4bcd42df63c9127541645c4
gotlieb <gotlieb.erez@gmail.com>
2019-04-02 18:51:11 +0300

    SUP-17626

--- deployment/updates/scripts/add_permissions/2019_04_01_update_annotation_permission.php
+++ deployment/updates/scripts/add_permissions/2019_04_01_update_annotation_permission.php
@@ -7,7 +7,7 @@
 $addPermissionsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
 
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/partner.-1.ini';
-passthru("php $script $config");
+passthru("php $addPermissionsScript $config");
 
 $addConfig = realpath(dirname(__FILE__)) . '/../../../permissions/service.annotation.annotation.ini';
 passthru("php $addPermissionsScript $addConfig");
