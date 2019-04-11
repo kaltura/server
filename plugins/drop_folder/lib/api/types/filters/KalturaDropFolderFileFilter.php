827b63a9d6d405936909b46d554be9040bdd398b
yossipapi <yossi.papiashvili@kaltura.com>
2019-04-11 16:15:25 +0300

    No-Plat: If list contains 0 results don't call do select

--- plugins/drop_folder/lib/api/types/filters/KalturaDropFolderFileFilter.php
+++ plugins/drop_folder/lib/api/types/filters/KalturaDropFolderFileFilter.php
@@ -12,4 +12,39 @@ class KalturaDropFolderFileFilter extends KalturaDropFolderFileBaseFilter
 	{
 		return new DropFolderFileFilter();
 	}
+	
+	/**
+	 * @param KalturaFilterPager $pager
+	 * @param KalturaDetachedResponseProfile $responseProfile
+	 * @return KalturaListResponse
+	 */
+	public function getListResponse(KalturaFilterPager $pager, KalturaDetachedResponseProfile $responseProfile = null)
+	{
+		$dropFolderFileFilter = $this->toObject();
+		
+		$c = new Criteria();
+		$dropFolderFileFilter->attachToCriteria($c);
+		$count = DropFolderFilePeer::doCount($c);
+		
+		if($count == 0)
+		{
+			return $this->getEmptyListResponse();
+		}
+		
+		$pager->attachToCriteria($c);
+		$list = DropFolderFilePeer::doSelect($c);
+		
+		$response = new KalturaDropFolderFileListResponse();
+		$response->objects = KalturaDropFolderFileArray::fromDbArray($list, $responseProfile);
+		$response->totalCount = $count;
+		return $response;
+	}
+	
+	public function getEmptyListResponse()
+	{
+		$response = new KalturaDropFolderFileListResponse();
+		$response->objects = array();
+		$response->totalCount = 0;
+		return $response;
+	}
 }
