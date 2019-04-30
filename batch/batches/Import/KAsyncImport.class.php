ca8a9581170ae89e25a526c208b38422f1e9c7f8
yossipapi <yossi.papiashvili@kaltura.com>
2019-04-30 12:08:59 +0300

    PLAT-9845: Newer PHP version dont parse url correctly when sftp url user/pass contains the # char

--- batch/batches/Import/KAsyncImport.class.php
+++ batch/batches/Import/KAsyncImport.class.php
@@ -308,7 +308,6 @@ class KAsyncImport extends KJobHandlerWorker
 			//Replace # sign to avoid cases where it's part of the user/password. The # sign is considered as fragment part of the URL.
 			//https://bugs.php.net/bug.php?id=73754
 			$sourceUrl = preg_replace("/#/", "kHash", $sourceUrl, -1, $replaceCount);
-			KalturaLog::debug("Testing:: sourceUrl [$sourceUrl]");
 			
 			// extract information from URL and job data
 			$parsedUrl = parse_url($sourceUrl);
@@ -316,7 +315,6 @@ class KAsyncImport extends KJobHandlerWorker
 			{
 				$parsedUrl = preg_replace("/kHash/", "#", $parsedUrl);
 			}
-			KalturaLog::debug("Testing:: parsedUrl = " . print_r($parsedUrl, true));
 
 			$host = isset($parsedUrl['host']) ? $parsedUrl['host'] : null;
 			$remotePath = isset($parsedUrl['path']) ? $parsedUrl['path'] : null;
