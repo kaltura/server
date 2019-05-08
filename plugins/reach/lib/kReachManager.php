27f9e4e89af2020eec7477aea90cc9767b276be5
yossipapi <yossi.papiashvili@kaltura.com>
2019-05-08 10:19:45 +0300

    REACH2-579: Correctly handle cases where task could not be created

--- plugins/reach/lib/kReachManager.php
+++ plugins/reach/lib/kReachManager.php
@@ -463,7 +463,10 @@ class kReachManager implements kObjectChangedEventConsumer, kObjectCreatedEventC
 		}
 
 		$entryVendorTask = self::addEntryVendorTask($entry, $reachProfile, $vendorCatalogItem, false, $sourceFlavorVersion, $context, EntryVendorTaskCreationMode::AUTOMATIC);
-		$entryVendorTask->save();
+		if($entryVendorTask)
+		{
+			$entryVendorTask->save();
+		}
 		return $entryVendorTask;
 	}
 
@@ -471,7 +474,7 @@ class kReachManager implements kObjectChangedEventConsumer, kObjectCreatedEventC
 	{
 		if($entry->getIsTemporary())
 		{
-			KalturaLog::debug("Entry [$entryId] is temporary, entry vendor task object wont be created for it");
+			KalturaLog::debug("Entry [{$entry->getId()}] is temporary, entry vendor task object wont be created for it");
 			return;
 		}
 		
