ef72f19ed231e370e65ea5b07755b937eb7a6e49
MosheMaorKaltura <moshe.maor@kaltura.com>
2019-06-03 13:59:13 +0300

    PLAT-9895:Update group status when it is cloned

---
+++ alpha/lib/enums/GroupProcessStatus.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: moshe.maor
+ * Date: 6/3/2019
+ * Time: 11:31 AM
+ */
+
+interface GroupProcessStatus extends BaseEnum
+{
+	const NONE = 0;
+	const PROCESSING = 1;
+}
