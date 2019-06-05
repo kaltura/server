5e4a171ab6a2ef7572d5dd1a76e37ef28271c16e
yossipapi <yossi.papiashvili@kaltura.com>
2019-06-05 11:22:59 +0300

    REACH2-583: Add new chaptering service

--- admin_console/configs/lang/en.php
+++ admin_console/configs/lang/en.php
@@ -1425,6 +1425,7 @@ return array(
 	'Kaltura_Client_Reach_Enum_VendorServiceFeature::TRANSLATION' => 'TRANSLATION',
 	'Kaltura_Client_Reach_Enum_VendorServiceFeature::ALIGNMENT' => 'ALIGNMENT',
 	'Kaltura_Client_Reach_Enum_VendorServiceFeature::AUDIO_DESCRIPTION' => 'AUDIO_DESCRIPTION',
+	'Kaltura_Client_Reach_Enum_VendorServiceFeature::CHAPTERING' => 'CHAPTERING',
 	'Kaltura_Client_Reach_Enum_VendorServiceType::HUMAN' => 'HUMAN',
 	'Kaltura_Client_Reach_Enum_VendorServiceType::MACHINE' => 'MACHINE',
 	
