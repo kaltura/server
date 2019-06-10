922d099721079a5ed0fe77ef79af95722aa57c10
InbalBD <inbal.bendavid@kaltura.com>
2019-06-10 14:26:59 +0300

    KMCNG-2114: change types for clients

--- api_v3/lib/types/partner/KalturaPartner.php
+++ api_v3/lib/types/partner/KalturaPartner.php
@@ -338,7 +338,7 @@ class KalturaPartner extends KalturaObject implements IFilterable
 	public $eSearchLanguages;
 
 	/**
-	 * @var int
+	 * @var KalturaPartnerAuthenticationType
 	 * @readonly
 	 */
 	public $authenticationType;
