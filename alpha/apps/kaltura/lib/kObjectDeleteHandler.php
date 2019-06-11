1658bcd6a5dc3e35475d6e5669614f1ff0c616c2
MosheMaorKaltura <moshe.maor@kaltura.com>
2019-06-11 06:09:54 +0300

    SUP-18090: When user is deleted, delete all dedicated appTokens

--- alpha/apps/kaltura/lib/kObjectDeleteHandler.php
+++ alpha/apps/kaltura/lib/kObjectDeleteHandler.php
@@ -164,16 +164,18 @@ class kObjectDeleteHandler extends kObjectDeleteHandlerBase implements kObjectDe
 				kJobsManager::addDeleteJob($kuser->getPartnerId(), DeleteObjectType::GROUP_USER, $filter);
 			}
 		}
-		
+
+		AppToken::onUserDeleted($kuser->getId());
+
 		$userEntryFilter = new UserEntryFilter();
 		$userEntryFilter->set("_eq_user_id", $kuser->getId());
-		
+
 		$c = new Criteria();
 		$c->add(UserEntryPeer::KUSER_ID, $kuser->getId());
 		if(!UserEntryPeer::doSelectOne($c)) {
 			return;
 		}
-		
+
 		kJobsManager::addDeleteJob($kuser->getPartnerId(), DeleteObjectType::USER_ENTRY, $userEntryFilter);
 	}
 	
