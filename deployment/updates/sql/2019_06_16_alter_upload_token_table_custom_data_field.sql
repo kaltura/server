65186775cb7468edb0a5027a76a6436d4e5c42a8
Inbal Ben David <inbal.bendavid@kaltura.com>
2019-06-30 14:29:27 +0300

    PLAT-9971: update add custom_data column mysql script

--- deployment/updates/sql/2019_06_16_alter_upload_token_table_custom_data_field.sql
+++ deployment/updates/sql/2019_06_16_alter_upload_token_table_custom_data_field.sql
@@ -1,3 +1,12 @@
-ALTER TABLE `upload_token`
-ADD `custom_data` TEXT
-AFTER `object_id`;
+SELECT count(*)
+INTO @exist
+FROM information_schema.columns 
+WHERE table_schema = database()
+AND COLUMN_NAME = 'custom_data'
+AND table_name = 'upload_token';
+
+set @query = IF(@exist <= 0, 'ALTER TABLE upload_token ADD custom_data TEXT AFTER object_id', 'select \'Column Exists\' status');
+
+prepare stmt from @query;
+
+EXECUTE stmt;
