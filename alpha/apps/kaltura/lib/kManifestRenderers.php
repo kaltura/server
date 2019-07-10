51fb2718dad65670809bb9c95dc690d33046dd8d
ravitshalem <ravit.shalem@kaltura.com>
2019-07-10 20:12:01 +0300

    PLAT-9959:Adding Timing-Allow-Origin to the response header

--- alpha/apps/kaltura/lib/kManifestRenderers.php
+++ alpha/apps/kaltura/lib/kManifestRenderers.php
@@ -303,6 +303,7 @@ abstract class kManifestRenderer
 		$headers = $this->getHeaders();
 		$headers[] = "Access-Control-Allow-Origin:*";
 		$headers[] = "Access-Control-Expose-Headers: Server,range,Content-Length,Content-Range";
+		$headers[] = 'Timing-Allow-Origin:*';
 		foreach ($headers as $header)
 		{
 			if ($this->deliveryCode)
