e037e7b9c05182321d8ee418cf97e773c186df5a
moshemaorkaltura <moshe.maor@kaltura.com>
2019-07-18 14:54:41 +0300

    PLAT-10043: set access control profile correct type for esearch ags

--- plugins/search/providers/elastic_search/lib/model/enum/ESearchEntryAggregationFieldName.php
+++ plugins/search/providers/elastic_search/lib/model/enum/ESearchEntryAggregationFieldName.php
@@ -9,5 +9,5 @@ interface ESearchEntryAggregationFieldName extends BaseEnum
 	const ENTRY_TYPE = 'entry_type';
 	const MEDIA_TYPE = 'media_type';
 	const TAGS = 'tags.raw';
-	const ACCESS_CONTROL_PROFILE = 'access_control_profile_id';
+	const ACCESS_CONTROL_PROFILE = 'access_control_id';
 }
