96fdb82c61551f6b8c913cfa99bed69216e98415
Noam Arad <noam.arad@kaltura.com>
2019-07-29 15:34:06 +0300

    add partner -7 permissions and group permissions to event notification list and dispatch

--- deployment/updates/scripts/add_permissions/2019_07_22_monitoring_proxy_add_partner.php
+++ deployment/updates/scripts/add_permissions/2019_07_22_monitoring_proxy_add_partner.php
@@ -15,6 +15,10 @@ passthru("php $script $config");
 
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+
 $config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/partner.-7.ini';
 passthru("php $script $config");
 
+
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.eventnotification.eventnotificationtemplate.ini';
+passthru("php $script $config");
