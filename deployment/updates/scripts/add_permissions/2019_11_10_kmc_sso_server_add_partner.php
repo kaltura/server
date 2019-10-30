901675051b0efe611ab9a31b6c9d7273ef4371a5
ravitshalem <ravit.shalem@kaltura.com>
2019-10-30 16:33:03 +0200

    PLAT-10251:adding new partner for kmc_sso_server, with sso->list permission

---
+++ deployment/updates/scripts/add_permissions/2019_11_10_kmc_sso_server_add_partner.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package deployment
+ * @subpackage orion.roles_and_permissions
+ */
+
+$script = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/insertDefaults.php';
+$config = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/init_data/01.Partner.ini';
+passthru("php $script $config");
+
+$script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+
+$config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/partner.-8.ini';
+passthru("php $script $config");
+
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.sso.sso.ini';
+passthru("php $script $config");
+
