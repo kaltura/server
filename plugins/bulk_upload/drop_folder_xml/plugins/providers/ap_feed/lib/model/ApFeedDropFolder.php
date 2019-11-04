d01122e5e9c8e616689a57086f05892237f8a01d
hilak <hila.karimov@kaltura.com>
2019-11-04 15:28:51 -0500

    PSVAMB-6815 Code review comments

--- plugins/bulk_upload/drop_folder_xml/plugins/providers/ap_feed/lib/model/ApFeedDropFolder.php
+++ plugins/bulk_upload/drop_folder_xml/plugins/providers/ap_feed/lib/model/ApFeedDropFolder.php
@@ -8,14 +8,16 @@ class ApFeedDropFolder extends FeedDropFolder
 	/**
 	 * @return the $apApiKey
 	 */
-	public function getApApiKey() {
+	public function getApApiKey()
+	{
 		return $this->getFromCustomData('apApiKey');
 	}
 	
 	/**
 	 * @param int $itemHandlingLimit
 	 */
-	public function setApApiKey($apApiKey) {
+	public function setApApiKey($apApiKey)
+	{
 		$this->putInCustomData('apApiKey', $apApiKey);
 	}
 }
