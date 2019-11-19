bd0194721fe1834620e3cafb6b113fc8e85d156c
yossipapi <yossi.papiashvili@kaltura.com>
2019-11-19 10:25:48 +0200

    PLAT-10313: If changing custom data value in preUpdate we need to call setCustomDataObj for it to take affect

--- plugins/content/caption/base/lib/model/CaptionAsset.php
+++ plugins/content/caption/base/lib/model/CaptionAsset.php
@@ -92,16 +92,20 @@ class CaptionAsset extends asset
 			foreach ($captionAssets as $captionAsset)
 			{
 				/* @var $captionAsset CaptionAsset */
-				if($captionAsset->getLanguage() != $this->getLanguage())
+				if($captionAsset->getLanguage() != $this->getLanguage() || $this->getId() == $captionAsset->getId())
 					continue;
 				
 				//If current captionAsset has no accuracy or has higher accuracy than an existing one consider it as the one that should be displayed
 				if( !$captionAsset->getAccuracy() || $this->getAccuracy() > $captionAsset->getAccuracy() )
 				{
-					$this->setDisplayOnPlayer(true);
-					//The setCustomDataObj is called in preSave so when changing custom data value we need to re-set it
-					$this->setCustomDataObj();
-					if($captionAsset->getDisplayOnPlayer())
+					if($this->getDisplayOnPlayer() == false)
+					{
+						$this->setDisplayOnPlayer(true);
+						//The setCustomDataObj is called in preSave so when changing custom data value we need to re-set it
+						$this->setCustomDataObj();
+					}
+					
+					if($captionAsset->getDisplayOnPlayer() == true)
 					{
 						$captionAsset->setDisplayOnPlayer(false);
 						$captionAsset->save();
