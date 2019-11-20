32c27b3a8a0879b90863d2ff09b3009a759ed64f
Amir Asaelov <amir.asaelov@Kaltura.com>
2019-11-20 14:59:09 +0200

    SUP-20245: add Price <> 0 when fetching all vendor task to avoid return unnecessary results which may exceed the php memory_limit

--- plugins/reach/lib/model/data/kVendorCredit.php
+++ plugins/reach/lib/model/data/kVendorCredit.php
@@ -153,6 +153,7 @@ class kVendorCredit
 		$c->add(EntryVendorTaskPeer::REACH_PROFILE_ID, $reachProfileId , Criteria::EQUAL);
 		$c->add(EntryVendorTaskPeer::STATUS, array(EntryVendorTaskStatus::PENDING, EntryVendorTaskStatus::PROCESSING, EntryVendorTaskStatus::READY), Criteria::IN);
 		$c->add(EntryVendorTaskPeer::QUEUE_TIME, $this->getSyncCreditStartDate(), Criteria::GREATER_EQUAL);
+		$c->add(EntryVendorTaskPeer::PRICE, 0, Criteria::NOT_EQUAL);
 		$c->add(EntryVendorTaskPeer::PARTNER_ID, $partnerId);
 		$this->addAdditionalCriteria($c);
 
