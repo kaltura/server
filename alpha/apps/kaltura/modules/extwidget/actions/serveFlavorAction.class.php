1ce83110de7f4c7108f78f30935ab1a92c1ca7d1
yossipapi <yossi.papiashvili@kaltura.com>
2019-12-01 15:15:32 +0200

    No-Plat: support multiple serve flavor path search replace

--- alpha/apps/kaltura/modules/extwidget/actions/serveFlavorAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/serveFlavorAction.class.php
@@ -27,7 +27,7 @@ class serveFlavorAction extends kalturaAction
 		$fullPath = $fileSync->getFullPath();
 		
 		$pathPrefix = kConf::get('serve_flavor_path_search_prefix', 'local', array());
-		 if($pathPrefix && !is_array($pathPrefix))
+		if($pathPrefix && !is_array($pathPrefix))
 		{
 			$pathPrefix = array($pathPrefix);
 		}
