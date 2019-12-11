c9be58157e136ce430bac1395c549379a432698b
gotlieb <gotlieb.erez@gmail.com>
2019-12-11 14:51:13 +0200

    Batches Crash Fix

--- batch/scheduler/KSchedulerConfig.class.php
+++ batch/scheduler/KSchedulerConfig.class.php
@@ -373,7 +373,7 @@ class KSchedulerConfig extends Zend_Config_Ini
 				}
 				else
 				{
-					KalturaLog::alert('Could not be decode batch configuration maps. Try to load from disc');
+					KalturaLog::alert('Could not be decode batch configuration maps');
 
 				}
 			}
