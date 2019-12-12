66d890f8556bf731d97f7ccd1ddbcb84b0d5f302
Orly Lampert <orly.lampert@kaltura.com>
2019-12-12 12:28:02 +0200

    add report for live funnel analysis

--- alpha/apps/kaltura/lib/reports/kKavaReportsMgr.class.php
+++ alpha/apps/kaltura/lib/reports/kKavaReportsMgr.class.php
@@ -283,6 +283,7 @@ class kKavaReportsMgr extends kKavaBase
 		self::EVENT_TYPE_FLAVOR_SWITCH,
 		self::EVENT_TYPE_BUFFER_START,
 		self::EVENT_TYPE_ERROR,
+		self::EVENT_TYPE_PLAY_REQUESTED,
 	);
 
 	protected static $media_type_count_aggrs = array(
