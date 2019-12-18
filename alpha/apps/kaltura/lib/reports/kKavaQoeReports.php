8f8db03609ad6acefc3ac79efef700fb24381615
Orly Lampert <orly.lampert@kaltura.com>
2019-12-18 12:00:47 +0200

    add ebvs metric

--- alpha/apps/kaltura/lib/reports/kKavaQoeReports.php
+++ alpha/apps/kaltura/lib/reports/kKavaQoeReports.php
@@ -255,11 +255,13 @@ class kKavaQoeReports extends kKavaReportsMgr
 		ReportType::QOE_ENGAGEMENT => array(
 			self::REPORT_METRICS => array(
 				self::METRIC_UNIQUE_SESSIONS,
-				self::METRIC_AVG_VIEW_PLAY_TIME_SEC
+				self::METRIC_AVG_VIEW_PLAY_TIME_SEC,
+				self::METRIC_COUNT_EBVS
 			),
 			self::REPORT_GRAPH_METRICS => array(
 				self::METRIC_UNIQUE_SESSIONS,
-				self::METRIC_AVG_VIEW_PLAY_TIME_SEC
+				self::METRIC_AVG_VIEW_PLAY_TIME_SEC,
+				self::METRIC_COUNT_EBVS
 			)
 		),
 
