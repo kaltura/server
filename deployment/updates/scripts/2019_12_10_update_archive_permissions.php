e03c998a3a3e1178b058acd41d1afff89703f53c
danielyag <daniel.yagodaev@kaltura.com>
2020-01-06 11:26:32 +0200

    WEBC-1574: add bootstrap to archive permissions script

--- deployment/updates/scripts/2019_12_10_update_archive_permissions.php
+++ deployment/updates/scripts/2019_12_10_update_archive_permissions.php
@@ -6,6 +6,9 @@
  * Add permissions to caption asset
  */
 
+define('DEPLOYMENT_DIR', realpath(__DIR__ . '/../..'));
+require_once (DEPLOYMENT_DIR . '/bootstrap.php');
+
 $script = realpath(dirname(__FILE__) . '/../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
 $config = realpath(dirname(__FILE__)) . '/../../permissions/service.pushnotification.pushnotificationtemplate.ini';
 passthru("php $script $config");
