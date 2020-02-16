3f6f5b6b1c75429ada14ca7e961e0f249ca4f4d4
yossipapi <yossi.papiashvili@kaltura.com>
2020-02-16 16:39:17 +0200

    No-Plat: Always trim last new line to avoid line jump in mysql update

--- alpha/apps/kaltura/lib/batch2/kBatchManager.php
+++ alpha/apps/kaltura/lib/batch2/kBatchManager.php
@@ -44,7 +44,7 @@ class kBatchManager
 		
 		if($flavorAsset)
 		{
-			$description = ltrim($flavorAsset->getDescription() . "\n" . $description);
+			$description = rtrim($flavorAsset->getDescription() . "\n" . $description);
 			$flavorAsset->setDescription($description);
 //			$flavorAsset->incrementVersion();
 		}	
