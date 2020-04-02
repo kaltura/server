b7554479cd78cdd30b52c0e18a8a012dbeccc172
avichainoach <avichai.noach@kaltura.com>
2020-04-02 14:59:19 +0300

    REACH2-835: Add business days enums

--- plugins/reach/lib/model/enums/VendorServiceTurnAroundTime.php
+++ plugins/reach/lib/model/enums/VendorServiceTurnAroundTime.php
@@ -14,7 +14,9 @@ interface VendorServiceTurnAroundTime extends BaseEnum
 	const EIGHT_HOURS           = 28800;
 	const TWELVE_HOURS          = 43200;
 	const TWENTY_FOUR_HOURS     = 86400;
+	const ONE_BUSINESS_DAY      = 86401;
 	const FORTY_EIGHT_HOURS     = 172800;
+	const TWO_BUSINESS_DAYS     = 172801;
 	const FOUR_DAYS             = 345600;
 	const FIVE_DAYS             = 432000;
 	const TEN_DAYS              = 864000;
