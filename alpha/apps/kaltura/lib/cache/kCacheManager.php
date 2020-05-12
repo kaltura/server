215b2c2553a172c99ba1cf1808c1af08c7dbcd6c
Inbal Ben David <inbal.bendavid@kaltura.com>
2020-05-12 11:11:16 +0300

    Revert "PLAT-10879: add new cache key layer name for storage keys"

--- alpha/apps/kaltura/lib/cache/kCacheManager.php
+++ alpha/apps/kaltura/lib/cache/kCacheManager.php
@@ -39,7 +39,6 @@ class kCacheManager
 	const CACHE_TYPE_AUTH_CACHED_DATA = 'authCachedData';
 	const CACHE_TYPE_DRUID_QUERIES = 'druidQueries';
 	const CACHE_TYPE_PLAYS_VIEWS = 'playsViews';
-	const CACHE_TYPE_STORAGE_CACHE_KEYS = 'storageCacheKeys';
 
 	protected static $caches = array();
 	
