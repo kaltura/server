016f9e506de5c1e4eae4f6a70ad6cff30c99a4e0
avichainoach <avichai.noach@kaltura.com>
2020-05-31 11:04:51 +0300

    PLAT-10916: add validateHashKayAction

--- deployment/updates/scripts/add_permissions/2020_05_31_user_validateHashKey.php
+++ deployment/updates/scripts/add_permissions/2020_05_31_user_validateHashKey.php
@@ -3,10 +3,8 @@
  * @package deployment
  * Add permissions to user validateHashKey
  */
-//require_once (__DIR__ . '/../../bootstrap.php');
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
-
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/service.user.ini';
 passthru("php $script $config");
 
