eb6648fd4ddc1441f8b9718c58277569728fb6d8
yossipapi <yossi.papiashvili@kaltura.com>
2020-06-04 10:17:29 +0300

    aws: add shared dc ids when trying to fetch local file syncs

--- alpha/apps/kaltura/lib/storage/kFileSyncUtils.class.php
+++ alpha/apps/kaltura/lib/storage/kFileSyncUtils.class.php
@@ -908,14 +908,21 @@ class kFileSyncUtils implements kObjectChangedEventConsumer, kObjectAddedEventCo
 	public static function getReadyFileSyncForKey ( FileSyncKey $key , $fetch_from_remote_if_no_local = false , $strict = true , $resolve = true )
 	{
 		KalturaLog::debug("key [$key], fetch_from_remote_if_no_local [$fetch_from_remote_if_no_local], strict [$strict]");
-		$dc = kDataCenterMgr::getCurrentDc();
-		$dc_id = $dc["id"];
+		
+		$currentDcId = kDataCenterMgr::getCurrentDcId();
+		$currentDcIds[] = $currentDcId;
+		$sharedStorageIds = kDataCenterMgr::getSharedStorageProfileIds();
+		foreach ($sharedStorageIds as $sharedStorageId)
+		{
+			$currentDcIds[] = $sharedStorageId;
+		}
+		
 		$c = new Criteria();
 		$c = FileSyncPeer::getCriteriaForFileSyncKey( $key );
 		if ( ! $fetch_from_remote_if_no_local )
 		{
 			// if $fetch_from_remote_if_no_local is true - don't restrict to the current DC - this will save an extra hit to the DB in case the file is not present
-			$c->addAnd ( FileSyncPeer::DC , $dc_id );
+			$c->addAnd ( FileSyncPeer::DC , $currentDcIds, Criteria::IN );
 		}
 		// search only for ready
 		$c->addAnd ( FileSyncPeer::STATUS , FileSync::FILE_SYNC_STATUS_READY );
@@ -937,11 +944,11 @@ class kFileSyncUtils implements kObjectChangedEventConsumer, kObjectAddedEventCo
 
 		$desired_file_sync = null;
 		$local = false;
-		$sortedFileSync = self::getSortedFileSyncs($file_sync_list, $fetch_from_remote_if_no_local, $resolve, $key->partner_id, $dc_id);
+		$sortedFileSync = self::getSortedFileSyncs($file_sync_list, $fetch_from_remote_if_no_local, $resolve, $key->partner_id, $currentDcId);
 		if($sortedFileSync)
 		{
 			$desired_file_sync = $sortedFileSync[0];
-			if($desired_file_sync->getDc() == $dc_id)
+			if(in_array( $desired_file_sync->getDc(), $currentDcIds))
 			{
 				$local = true;
 			}
