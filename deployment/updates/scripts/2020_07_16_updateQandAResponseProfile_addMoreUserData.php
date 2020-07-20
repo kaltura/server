bce759914547fcc7e2972d21ef09c40380ac6297
noam-arad <noam-arad@users.noreply.github.com>
2020-07-20 10:09:47 +0300

    WEBC-1925
    remove template from file path

--- deployment/updates/scripts/2020_07_16_updateQandAResponseProfile_addMoreUserData.php
+++ deployment/updates/scripts/2020_07_16_updateQandAResponseProfile_addMoreUserData.php
@@ -9,7 +9,7 @@ require_once (DEPLOYMENT_DIR . '/bootstrap.php');
 
 $scriptPath = realpath(DEPLOYMENT_DIR . '/../tests/standAloneClient/exec.php');
 
-$templateUpdateXmlPath = realpath(DEPLOYMENT_DIR . '/updates/scripts/xml/2020_07_16_updateQandAResponseProfile_addMoreUserData.template.xml');
+$templateUpdateXmlPath = realpath(DEPLOYMENT_DIR . '/updates/scripts/xml/2020_07_16_updateQandAResponseProfile_addMoreUserData.xml');
 
 if(!file_exists($templateUpdateXmlPath) || !file_exists($scriptPath))
 {
