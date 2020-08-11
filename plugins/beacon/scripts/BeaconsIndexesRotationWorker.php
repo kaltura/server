6a8b5e0a71d7616e529863b70b4467acad4c9d63
ZurKaltura <zur.magen@kaltura.com>
2020-08-11 14:53:42 +0300

    PLAT-11115 Updating beacon rotation index to create indexes for new machine (#9674)
    * PLAT-11115 Updating beacon rotation index to create indexes for new machine
    
    * Changing script to end in case new index already exists

--- plugins/beacon/scripts/BeaconsIndexesRotationWorker.php
+++ plugins/beacon/scripts/BeaconsIndexesRotationWorker.php
@@ -5,6 +5,8 @@
  */
 class BeaconsIndexesRotationWorker extends ElasticIndexRotationWorker
 {
+	const INDEX_NAME_DATE_DELTA = '-1 month';
+
 	/**
 	 * @param $currentSearchingIndices array
 	 * @param $aliasesToRemove array
@@ -26,5 +28,46 @@ class BeaconsIndexesRotationWorker extends ElasticIndexRotationWorker
 				$aliasesToAdd[]  = new ElasticIndexAlias($index, $this->indexPattern . kBeacon::ELASTIC_INDEX_OLD_POSTFIX . $count);
 			}
 		}
+	}
+
+	protected function getIndexesToCreate()
+	{
+		$indexesName = array();
+		$date = new DateTime();
+		for ($i = 1; $i <= $this->maxNumberOfIndices; $i++)
+		{
+			$yearMonth = $date->format($this->indexDateFormat);
+			array_unshift($indexesName, $this->indexPattern . '-' . $yearMonth);
+			$date->modify(self::INDEX_NAME_DATE_DELTA);
+		}
+
+		return $indexesName;
+	}
+
+	protected function createIndexFromScratch()
+	{
+		KalturaLog::info('Creating new index from scratch mode');
+		$indexesToCreate = $this->getIndexesToCreate();
+		$aliasesToRemove = array();
+		$aliasesToAdd = array();
+		foreach($indexesToCreate as $newIndex)
+		{
+			if ($this->dryRun)
+			{
+				KalturaLog::debug("Dry run - creating index $newIndex");
+			}
+			else
+			{
+				$this->createNewIndex($newIndex);
+			}
+
+			$aliasesToAdd[] = new ElasticIndexAlias($newIndex, $this->searchAlias);
+		}
+
+		$this->handleCurrentSearchIndices($indexesToCreate, $aliasesToRemove, $aliasesToAdd);
+		$aliasesToRemove = array();
+		$newIndex = end($indexesToCreate);
+		$aliasesToAdd[] = new ElasticIndexAlias($newIndex, $this->indexAlias);
+		$this->changeAliases($aliasesToAdd, $aliasesToRemove);
 	}
 }
