232b865a33d97e9ccc6fc9cec233922f52c844e6
erankor <eran.kornblau@kaltura.com>
2020-08-22 11:45:16 +0300

    export script cleanup
    remove sourceDc param, use original file sync instead
    use the flavor version instead of getting all version file syncs

--- alpha/scripts/utils/ExportByEntryListToDifferentStorage.php
+++ alpha/scripts/utils/ExportByEntryListToDifferentStorage.php
@@ -1,25 +1,17 @@
 <?php
 
-if (count($argv) != 5)
+if (count($argv) != 4)
 {
-	print("USAGE: <sourceDc> <storageId> <fileName> <realrun-dryrun> ");
+	print("USAGE: <storageId> <fileName> <realrun-dryrun> ");
 	exit(0);
 }
 
 define("BASE_DIR", dirname(__FILE__));
 require_once(BASE_DIR.'/../../../alpha/scripts/bootstrap.php');
 
-$sourceDc = $argv[1];
 $storageIdDest = $argv[2];
 $filePath = $argv[3];
 $dryRun = $argv[4] != 'realrun';
-
-if (!is_numeric($sourceDc))
-{
-	KalturaLog::warning("Source DC Id Destination id should be numeric");
-	exit(0);
-}
-
 
 if (!is_numeric($storageIdDest))
 {
@@ -43,18 +35,17 @@ else
 }
 KalturaStatement::setDryRun($dryRun);
 
-main($sourceDc, $storageIdDest, $filePath);
+main($storageIdDest, $filePath);
 
 /**
- * @param $sourceDc
  * @param $storageIdDest
  * @param $filePath
  * @throws PropelException
  */
-function main($sourceDc, $storageIdDest, $filePath)
+function main($storageIdDest, $filePath)
 {
-	KalturaLog::debug("Running for file [$filePath] and storageIdDest [$storageIdDest] and source dc $sourceDc");
-	$count = 0;
+	KalturaLog::debug("Running for file [$filePath] and storageIdDest [$storageIdDest]");
+
 	$entryIds = file($filePath);
 	if (empty($entryIds))
 	{
@@ -68,6 +59,8 @@ function main($sourceDc, $storageIdDest, $filePath)
 		KalturaLog::warning("Storage [$storageIdDest] does not exists");
 		exit(0);
 	}
+
+	$count = 0;
 
 	foreach ($entryIds as $entryId)
 	{
@@ -94,72 +87,66 @@ function main($sourceDc, $storageIdDest, $filePath)
 		foreach ($assets as /** @var flavorAsset $asset * */ $asset)
 		{
 			KalturaLog::debug('Handling asset with id ' . $asset->getId());
-			if ($externalStorage->shouldExportFlavorAsset($asset))
-			{
-				$criteria = new Criteria(FileSyncPeer::DATABASE_NAME);
-				$criteria->add(FileSyncPeer::OBJECT_ID, $asset->getId(), Criteria::EQUAL);
-				$criteria->add(FileSyncPeer::OBJECT_TYPE, FileSyncObjectType::ASSET);
-				$criteria->add(FileSyncPeer::OBJECT_SUB_TYPE, flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
-				$criteria->add(FileSyncPeer::DELETED_ID, 0, Criteria::EQUAL);
-				$criteria->add(FileSyncPeer::DC, array($sourceDc, $storageIdDest), Criteria::IN);
-				$criteria->add(FileSyncPeer::PARTNER_ID, $asset->getPartnerId(), Criteria::EQUAL);
-				$criteria->addDescendingOrderByColumn(FileSyncPeer::VERSION);
-				$criteria->addAscendingOrderByColumn(FileSyncPeer::DC);
-				$fileSyncs = FileSyncPeer::doSelect($criteria);
 
-				$remoteDcFileSyncFound = false;
-				$maxVersion = -1;
-				$fileSyncToHandle = null;
-				//Get the Max Version and Local fileSync to handle
-				foreach ($fileSyncs as /** @var FileSync $fileSync * */ $fileSync)
-				{
-					if ($fileSync->getDc() == $sourceDc && $fileSync->getFileSize() > 0 && $fileSync->getVersion() > $maxVersion)
-					{
-						$maxVersion = $fileSync->getVersion();
-						$fileSyncToHandle = $fileSync;
-					}
-				}
-				if (!$fileSyncToHandle)
-				{
-					KalturaLog::debug('No filesync to handle for flavor asset ' . $asset->getId());
-					continue;
-				}
-
-				// look for a sibling with same version on the remote storage
-				foreach ($fileSyncs as /** @var FileSync $fileSync * */ $fileSync)
-				{
-					if ($fileSync->getDc() == $storageIdDest && $fileSync->getVersion() == $fileSyncToHandle->getVersion())
-					{
-						$remoteDcFileSyncFound = true;
-					}
-				}
-
-				if ($remoteDcFileSyncFound)
-				{
-					KalturaLog::debug("Found file sync in remote dc [$storageIdDest] for assetId " . $asset->getId() . " . skipping exporting asset");
-					continue;
-				}
-
-				if ($fileSyncToHandle->getStatus() != flavorAsset::FLAVOR_ASSET_STATUS_READY)
-				{
-					KalturaLog::debug("Filesync " . $fileSyncToHandle->getId() . " in not ready " . $fileSyncToHandle->getStatus() . " - skipping asset");
-					continue;
-				}
-				try
-				{
-					KalturaLog::debug("Handling filesync " . $fileSyncToHandle->getId());
-					$newfileSync = $fileSyncToHandle->cloneToAnotherStorage($storageIdDest);
-					$newfileSync->save();
-					KalturaLog::debug('New FileSync created ' . $newfileSync->getId());
-				}
-				catch (Exception $e)
-				{
-					KalturaLog::warning("Could not create newFileSync for fileSync [" . $fileSync->getId() . "]" . $e->getMessage());
-				}
-			}
-			else
+			if (!$externalStorage->shouldExportFlavorAsset($asset))
 			{
 				KalturaLog::debug('Asset ' . $asset->getId() . ' should not be Exported to remote storge ' . $storageIdDest);
+				continue;
+			}
+
+			$criteria = new Criteria(FileSyncPeer::DATABASE_NAME);
+			$criteria->add(FileSyncPeer::OBJECT_ID, $asset->getId(), Criteria::EQUAL);
+			$criteria->add(FileSyncPeer::OBJECT_TYPE, FileSyncObjectType::ASSET);
+			$criteria->add(FileSyncPeer::VERSION, $asset->getVersion());
+			$criteria->add(FileSyncPeer::OBJECT_SUB_TYPE, flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
+			$criteria->add(FileSyncPeer::DELETED_ID, 0, Criteria::EQUAL);
+			$fileSyncs = FileSyncPeer::doSelect($criteria);
+
+			$remoteDcFileSyncFound = false;
+			$fileSyncToHandle = null;
+
+			//Get the Local fileSync to handle
+			foreach ($fileSyncs as /** @var FileSync $fileSync * */ $fileSync)
+			{
+				if ($fileSync->getOriginal() && $fileSync->getFileSize() > 0)
+				{
+					$fileSyncToHandle = $fileSync;
+				}
+
+				if ($fileSync->getDc() == $storageIdDest)
+				{
+					$remoteDcFileSyncFound = true;
+				}
+			}
+
+			if (!$fileSyncToHandle)
+			{
+				KalturaLog::debug('No filesync to handle for flavor asset ' . $asset->getId());
+				continue;
+			}
+
+			if ($fileSyncToHandle->getStatus() != flavorAsset::FLAVOR_ASSET_STATUS_READY)
+			{
+				KalturaLog::debug("Filesync " . $fileSyncToHandle->getId() . " in not ready " . $fileSyncToHandle->getStatus() . " - skipping asset");
+				continue;
+			}
+
+			if ($remoteDcFileSyncFound)
+			{
+				KalturaLog::debug("Found file sync in remote dc [$storageIdDest] for assetId " . $asset->getId() . " . skipping exporting asset");
+				continue;
+			}
+
+			try
+			{
+				KalturaLog::debug("Handling filesync " . $fileSyncToHandle->getId());
+				$newfileSync = $fileSyncToHandle->cloneToAnotherStorage($storageIdDest);
+				$newfileSync->save();
+				KalturaLog::debug('New FileSync created ' . $newfileSync->getId());
+			}
+			catch (Exception $e)
+			{
+				KalturaLog::warning("Could not create newFileSync for fileSync [" . $fileSync->getId() . "]" . $e->getMessage());
 			}
 		}
 
