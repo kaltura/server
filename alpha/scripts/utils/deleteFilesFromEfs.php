2b21c9980bc75d8e4241a53a1904206623687800
Zur <zur.magen@kaltura.com>
2020-09-16 13:59:01 +0300

    fixing code review comments

--- alpha/scripts/utils/deleteFilesFromEfs.php
+++ alpha/scripts/utils/deleteFilesFromEfs.php
@@ -8,24 +8,27 @@ class deleteFilesWorker
 {
     protected $con;
 	protected $dryRun;
+	protected $minFileSize;
+	protected $efsFilePath;
+	protected $minUpdatedAtTime;
 	const STOP_FILE_PATH = '/tmp/deleteScript.stop';
 	const FILE_SYNC_READY_STATUS = 2;
 	const FILE_SYNC_PURGE_STATUS = 4;
 	const MAP_NAME = 'cloud_storage';
-	const EFS_FILE_PATH_PARAM = 'efs_file_path';
 	const EFS_DC = 1;
 	const AWS_DC = 2;
-	const MIN_FILE_SIZE_PARAM = 'min_file_size';
 	const MAX_UPDATED_AT_PARAM = 'min_updated_at';
 	const ITERATION_FILESYNC_LIMIT = 'iteration_filesync_limit';
 	const SCRIPT_MS_RUNNING_TIME_THRESHOLD = 30000;
 	const SLEEP_BETWEEN_FAST_ITERATIONS = 120;
-	const TWO_DAYS_IN_SECONDS = 172800;
 	const MIN_FILE_SYNC_ID_TIME_WINDOW = 600;
 
-    public function __construct($dryRun)
+    public function __construct($minFileSize, $efsFilePath, $minUpdatedAtTime, $dryRun)
 	{
 		$this->con =myDbHelper::getConnection(myDbHelper::DB_HELPER_CONN_PROPEL2);
+		$this->minFileSize = $minFileSize;
+		$this->efsFilePath = $efsFilePath;
+		$this->minUpdatedAtTime = $minUpdatedAtTime;
 		$this->dryRun = $dryRun;
 	}
 
@@ -75,20 +78,17 @@ class deleteFilesWorker
 	protected function getCandidatesForDelete($minFileSyncId)
 	{
 		KalturaLog::info("Getting efs file syncs");
-		$efsFilePath = kConf::get(self::EFS_FILE_PATH_PARAM,self::MAP_NAME, '%r74v1%');
-		$minFileSize = kConf::get(self::MIN_FILE_SIZE_PARAM,self::MAP_NAME, 100*1024*1024);
 		$maxUpdatedAt = kConf::get(self::MAX_UPDATED_AT_PARAM,self::MAP_NAME, 12*3600);
 		$iterationFileSyncLimit = kConf::get(self::ITERATION_FILESYNC_LIMIT,self::MAP_NAME, 1000);
 		$criteria = new Criteria();
-		$criterion  = $criteria->getNewCriterion(FileSyncPeer::ID, $minFileSyncId, Criteria::GREATER_EQUAL);
-		$criteria->addAnd($criterion);
+		$criteria->add(FileSyncPeer::ID, $minFileSyncId, Criteria::GREATER_EQUAL);
 		$criteria->add(FileSyncPeer::OBJECT_TYPE, FileSyncObjectType::ASSET);
 		$criteria->add(FileSyncPeer::OBJECT_SUB_TYPE, flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ASSET);
 		$criteria->add(FileSyncPeer::STATUS, self::FILE_SYNC_READY_STATUS, Criteria::EQUAL);
-		$criteria->add(FileSyncPeer::FILE_PATH, $efsFilePath, Criteria::LIKE);
+		$criteria->add(FileSyncPeer::FILE_PATH, $this->efsFilePath, Criteria::LIKE);
 		$criteria->add(FileSyncPeer::DC, self::EFS_DC, Criteria::EQUAL);
-		$criteria->add(FileSyncPeer::FILE_SIZE, $minFileSize,Criteria::GREATER_THAN);
-		$criteria->add(FileSyncPeer::UPDATED_AT,time() - $maxUpdatedAt,Criteria::LESS_THAN);
+		$criteria->add(FileSyncPeer::FILE_SIZE, $this->minFileSize, Criteria::GREATER_THAN);
+		$criteria->add(FileSyncPeer::UPDATED_AT,time() - $maxUpdatedAt, Criteria::LESS_THAN);
 		$criteria->addAscendingOrderByColumn(FileSyncPeer::ID);
 		$criteria->setLimit($iterationFileSyncLimit);
 		$result = null;
@@ -117,19 +117,16 @@ class deleteFilesWorker
 
 	protected function getMinFileSyncIdQuery($minUpdatedAt)
 	{
-		$efsFilePath = kConf::get(self::EFS_FILE_PATH_PARAM,self::MAP_NAME, '%r74v1%');
-		$minFileSize = kConf::get(self::MIN_FILE_SIZE_PARAM,self::MAP_NAME, 100*1024*1024);
 		$criteria = new Criteria();
 		$criterion = $criteria->getNewCriterion(FileSyncPeer::UPDATED_AT, $minUpdatedAt,Criteria::GREATER_EQUAL);
-		$criterion2 = $criteria->getNewCriterion(FileSyncPeer::UPDATED_AT,$minUpdatedAt + self::MIN_FILE_SYNC_ID_TIME_WINDOW,Criteria::LESS_THAN);
+		$criterion->addAnd($criteria->getNewCriterion(FileSyncPeer::UPDATED_AT,$minUpdatedAt + self::MIN_FILE_SYNC_ID_TIME_WINDOW,Criteria::LESS_THAN));
 		$criteria->addAnd($criterion);
-		$criteria->addAnd($criterion2);
 		$criteria->add(FileSyncPeer::OBJECT_TYPE, FileSyncObjectType::ASSET);
 		$criteria->add(FileSyncPeer::OBJECT_SUB_TYPE, flavorAsset::FILE_SYNC_ASSET_SUB_TYPE_ASSET);
 		$criteria->add(FileSyncPeer::STATUS, self::FILE_SYNC_READY_STATUS, Criteria::EQUAL);
-		$criteria->add(FileSyncPeer::FILE_PATH, $efsFilePath, Criteria::LIKE);
+		$criteria->add(FileSyncPeer::FILE_PATH, $this->efsFilePath, Criteria::LIKE);
 		$criteria->add(FileSyncPeer::DC, self::EFS_DC, Criteria::EQUAL);
-		$criteria->add(FileSyncPeer::FILE_SIZE, $minFileSize,Criteria::GREATER_THAN);
+		$criteria->add(FileSyncPeer::FILE_SIZE, $this->minFileSize, Criteria::GREATER_THAN);
 		$criteria->addAscendingOrderByColumn(FileSyncPeer::UPDATED_AT);
 		return $criteria;
 	}
@@ -137,7 +134,7 @@ class deleteFilesWorker
 	protected function getMinFileSyncId()
 	{
 		KalturaLog::info('Looking for minimum file id');
-		$minTime = time() - self::TWO_DAYS_IN_SECONDS;
+		$minTime = time() - $this->minUpdatedAtTime;
 		$fileSync = null;
 		$maxUpdatedAt = kConf::get(self::MAX_UPDATED_AT_PARAM,self::MAP_NAME, 12*3600);
 		while($minTime < (time() - $maxUpdatedAt))
@@ -181,6 +178,11 @@ class deleteFilesWorker
 			$efsSize = kFile::fileSize($efsFilePath);
 			if ($efsSize == $S3Size)
 			{
+				if($this->dryRun)
+				{
+					KalturaLog::info("{$efsFilePath} size {$efsSize} and {$s3Location} size {$S3Size} match");
+				}
+
 				return true;
 			}
 		}
@@ -308,10 +310,23 @@ class deleteFilesWorker
 	}
 }
 
-$dryRun = true;
-if($argc > 1)
+if($argc < 4)
 {
-	$dryRun = $argv[1];
+	echo "Usage: php $argv[0] [efsFilePath] [minimum file size] [minimum updated time in seconds] <dryRun>." . PHP_EOL;
+	echo "example for looking at for %r74v1% pattern with files at minimum size of 100*1024*1024 and 2 days back use:" . PHP_EOL;
+	echo "php deleteFilesFromEfs.php '%r74v1%' 104857600 172800 1" . PHP_EOL;
+	die;
+}
+
+
+$minFileSize = $argv[1];
+$efsFilePath = $argv[2];
+$minUpdatedAtTime = $argv[3];
+
+$dryRun = true;
+if($argc > 4)
+{
+	$dryRun = $argv[4];
 }
 
 if ($dryRun)
@@ -324,5 +339,5 @@ else
 }
 
 KalturaStatement::setDryRun($dryRun);
-$deleteFilesWorker = new deleteFilesWorker($dryRun);
+$deleteFilesWorker = new deleteFilesWorker($minFileSize, $efsFilePath, $minUpdatedAtTime, $dryRun);
 $deleteFilesWorker->doWork();
