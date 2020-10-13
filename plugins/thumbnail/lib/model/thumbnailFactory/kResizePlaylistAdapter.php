e23ee8089bcffe53c2427fae9c4d8b8b2b4ace72
ZurKaltura <zur.magen@kaltura.com>
2020-10-13 09:56:31 +0300

    Cloudmgt 750 thumb adapter fix for playlist default vid sec (#9945)

--- plugins/thumbnail/lib/model/thumbnailFactory/kResizePlaylistAdapter.php
+++ plugins/thumbnail/lib/model/thumbnailFactory/kResizePlaylistAdapter.php
@@ -8,13 +8,14 @@ class kResizePlaylistAdapter extends kBaseResizeAdapter
 {
 	protected function preTransformationExtraActions()
 	{
-		parent::preTransformationExtraActions();
 		$entry = $this->getEntry();
 		$sourceEntry = myPlaylistUtils::getFirstEntryFromPlaylist($entry);
 		if ($sourceEntry)
 		{
 			$this->parameters->set(kThumbFactoryFieldName::SOURCE_ENTRY, $sourceEntry);
 		}
+
 		myPlaylistUtils::updatePlaylistStatistics($entry->getPartnerId(), $entry);
+		parent::preTransformationExtraActions();
 	}
 }
