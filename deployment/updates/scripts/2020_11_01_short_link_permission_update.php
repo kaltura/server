489e138a7856734d89e3f15e33a67d24acc6dd89
Moshe Maor <moshe.maor@kaltura.com>
2020-11-03 09:44:12 +0200

    PLAT-11249: remove widget permission from shortLink:list

---
+++ deployment/updates/scripts/2020_11_01_short_link_permission_update.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package deployment
+ * @subpackage falcon.roles_and_permissions
+ *
+ * Adds event notification plugin permissions
+ * No need to re-run after server code deploy
+ */
+
+$script = realpath(dirname(__FILE__) . '/../../../') . '/alpha/scripts/utils/permissions/removePermissionsAndItems.php';
+$config = realpath(dirname(__FILE__) . '/../../../') . '/deployment/updates/scripts/ini_files/2020_11_01_shortlink_update_permission.ini';
+print ($config);
+passthru("php $script $config");
