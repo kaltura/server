3cebf094ae6fd5a8d5ff3dd79fc91dadad442466
InbalBD <inbal.bendavid@kaltura.com>
2020-11-23 13:05:49 +0200

    PLAT-11239: add expiry to enforced serve requests

--- alpha/apps/kaltura/lib/storage/urlTokenizers/kKalturaUrlTokenizer.php
+++ alpha/apps/kaltura/lib/storage/urlTokenizers/kKalturaUrlTokenizer.php
@@ -13,8 +13,24 @@ class kKalturaUrlTokenizer extends kUrlTokenizer
 		$lastSlashPosition = strrpos($url, "/");
 		$path = substr($url, 0, $lastSlashPosition);
 		$fileName = substr($url, $lastSlashPosition + 1);
+
+		$expiry = $this->getExpiry();
+		$path .= '/exp/' . $expiry;
+
 		$signature = kDeliveryUtils::urlsafeB64Encode(hash_hmac('sha256', $path, $this->key, true));
+
 		return $path . '/sig/' . $signature . '/' . $fileName;
+	}
+
+	protected function getExpiry()
+	{
+		$expiry = time() + 86400;
+		$ksObj = kCurrentContext::$ks_object;
+		if($ksObj)
+		{
+			$expiry = $ksObj->valid_until;
+		}
+		return $expiry;
 	}
 
 }
