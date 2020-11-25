5a0790ef5c20575e41c574cd1d19c648e1c66659
ravitshalem <ravit.shalem@kaltura.com>
2020-11-25 17:43:52 +0200

    PLAT-22483:new script that will update tag on entries according to reference id

---
+++ alpha/scripts/utils/updateTagAccordingToRefenceId.php
@@ -0,0 +1,41 @@
+<?php
+require_once('/opt/kaltura/app/alpha/scripts/bootstrap.php');
+
+if ($argc < 3)
+{
+	die("Usage: php $argv[0] entryIdsFile referenceIdsTagFile.csv <realrun | dryrun> \n");
+}
+
+$entryIdsFile = $argv[1] ;
+$referenceIdsTagFile = $argv[2];
+$dryrun = true;
+if($argc == 4 && $argv[3] == 'realrun')
+	$dryrun = false;
+
+$entryIds = file ($entryIdsFile) or die ('Could not read file'."\n");
+$referenceIdsTagList = file ($referenceIdsTagFile) or die ('Could not read file'."\n");
+
+foreach ($entryIds as $entryId)
+{
+	$entryId = trim($entryId);
+	$entry = entryPeer::retrieveByPK($entryId);
+	if ($entry)
+	{
+		$entryReferenceId = $entry->getReferenceID();
+		foreach ($referenceIdsTagList as $referenceIdsTag)
+		{
+			$referenceIdsTag = trim($referenceIdsTag);
+			list($referenceId, $tag) = explode(',', $referenceIdsTag);
+			if ($referenceId && $tag && $entryReferenceId === $referenceId)
+			{
+				$entryTags = $entry->getTags();
+				$entry->setTags($entryTags . ',__' . $tag);
+				$entry->save();
+				print_r('Adding tag __' . $tag . ' to entryId: '. $entryId .
+					' with reference id: ' . $entryReferenceId . "\n");
+			}
+		}
+	}
+}
+print_r("Done! \n");
+
