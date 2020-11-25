09b6d94f7962bd6de87b286d2a580f1bbcf4af27
ravitshalem <ravit.shalem@kaltura.com>
2020-11-25 18:18:26 +0200

    PLAT-22482:fixing code review remarks

--- alpha/scripts/utils/updateEntriesRecordings.php
+++ alpha/scripts/utils/updateEntriesRecordings.php
@@ -1,14 +1,13 @@
 <?php
 require_once('/opt/kaltura/app/tests/lib/KalturaClient.php');
-if ($argc < 6)
+if ($argc < 5)
 {
-	die("Usage: php $argv[0] partnerId adminSecret serviceUrl simuliveEntryIdsFile vodEntryIdsFile \n");
+	die("Usage: php $argv[0] partnerId adminSecret serviceUrl simuliveVodFile \n");
 }
 $partnerId = $argv[1];
 $adminSecret = $argv[2];
 $url = $argv[3];
-$simuliveEntryIdsFile = $argv[4] ;
-$vodEntryIdsFile = $argv[5];
+$simuliveVodFile = $argv[4] ;
 
 $config = new KalturaConfiguration();
 $config->serviceUrl = $url;
@@ -22,42 +21,47 @@ $ks = $client->session->start(
 	'disableentitlement');
 $client->setKS($ks);
 
-$simuliveEntryIds = file ($simuliveEntryIdsFile) or die ('Could not read file'."\n");
-$vodEntryIds = file ($vodEntryIdsFile) or die ('Could not read file'."\n");
+$simuliveVodEntryIds = file ($simuliveVodFile) or die ('Could not read file'."\n");
 
-if (count($simuliveEntryIds) != count($vodEntryIds))
+foreach($simuliveVodEntryIds as $simuliveEntryIdVodEntryId)
 {
-	print_r("simuliveEntryIdsFile and vodEntryIdsFile should be on the same length \n");
-	exit;
-}
-
-for ($i = 0; $i < count($simuliveEntryIds); $i++)
-{
-	$simuliveEntryId = trim($simuliveEntryIds[$i]);
-	$vodEntryId = trim($vodEntryIds[$i]);
+	$simuliveEntryIdVodEntryId = trim($simuliveEntryIdVodEntryId);
+	list($simuliveEntryId, $vodEntryId) = explode(',', $simuliveEntryIdVodEntryId);
 	try
 	{
 		$simuliveEntry = $client->baseEntry->get($simuliveEntryId);
 	}
 	catch(Exception $e)
 	{
-		print_r($e->getMessage() . ' entryId: ' . $simuliveEntry ."\n");
+		print_r($e->getMessage() . ' entryId: ' . $simuliveEntryId ."\n");
 		continue;
 	}
 	if ($simuliveEntry)
 	{
 		$kLive = new KalturaLiveStreamEntry();
 		$kLive->recordedEntryId = $vodEntryId;
-		$kLive->displayInSearch = KalturaEntryDisplayInSearchType::SYSTEM;
 		try
 		{
 			$client->baseEntry->update($simuliveEntryId, $kLive);
-			print_r('EntryId: ' . $simuliveEntryId . ' was updated with recordedEntryId: '. $vodEntryId .
-				' displayInSearch: ' . KalturaEntryDisplayInSearchType::SYSTEM . "\n");
+			print_r('EntryId: ' . $simuliveEntryId . ' was updated with recordedEntryId: '. $vodEntryId . "\n");
 		}
 		catch(Exception $e)
 		{
 			print_r($e->getMessage() . ' entryId: ' . $simuliveEntryId ."\n");
+			continue;
+		}
+
+		$kLive = new KalturaLiveStreamEntry();
+		$kLive->displayInSearch = KalturaEntryDisplayInSearchType::SYSTEM;
+		try
+		{
+			$client->baseEntry->update($vodEntryId, $kLive);
+			print_r('EntryId: ' . $vodEntryId . ' was updated with displayInSearch: ' . KalturaEntryDisplayInSearchType::SYSTEM . "\n");
+		}
+		catch(Exception $e)
+		{
+			print_r($e->getMessage() . ' entryId: ' . $simuliveEntryId ."\n");
+			continue;
 		}
 		addCategoryEntryIds($client, $simuliveEntryId, $vodEntryId);
 	}
