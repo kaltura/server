71305cf7b9c04ff8931a0ff054f778e9a0fc2cf4
yossipapi <yossi.papiashvili@kaltura.com>
2020-12-09 11:48:55 +0200

    FOUN-61: Use fs wrapper methods when working with files + resolve file path when building ffmpeg command

--- batch/batches/Convert/Engines/KConversionEngine.class.php
+++ batch/batches/Convert/Engines/KConversionEngine.class.php
@@ -216,7 +216,7 @@ abstract class KConversionEngine
 			
 		try
 		{			
-			if ( file_exists ( $file ))
+			if ( kFile::checkFileExists(( $file )) )
 			{
 				$media_info = shell_exec("mediainfo ".realpath($file));
 				$this->addToLogFile ( $log_file ,$media_info ) ;
