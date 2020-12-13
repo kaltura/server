0ca46353fac2b6c233e0742b547936fd6cecb631
InbalBD <inbal.bendavid@kaltura.com>
2020-12-13 17:56:21 +0200

    PLAT-11239: add expiry to kaltura url tokenizer and recognizer

--- alpha/apps/kaltura/lib/storage/urlRecognizer/kKalturaUrlRecognizer.php
+++ alpha/apps/kaltura/lib/storage/urlRecognizer/kKalturaUrlRecognizer.php
@@ -8,6 +8,10 @@
 class kKalturaUrlRecognizer extends kUrlRecognizer
 {
 
+	const DOWNLOAD_NOT_RECOGNIZED = 0;
+	const DOWNLOAD_RECOGNIZED_OK = 1;
+	const DOWNLOAD_RECOGNIZED_NOT_OK = 2;
+
 	/**
 	 * @var string
 	 */
@@ -36,11 +40,34 @@ class kKalturaUrlRecognizer extends kUrlRecognizer
 		$matches = null;
 		if (!preg_match('#/sig/([^/]+)/#', $uri, $matches, PREG_OFFSET_CAPTURE))
 		{
-			return false;
+			return self::DOWNLOAD_NOT_RECOGNIZED;
 		}
 		$partToSign = substr($uri, 0, $matches[0][1]);
 		$requestSignature = $matches[1][0];
 
+		if (!preg_match('#/exp/([^/]+)/#', $uri, $expMatches, PREG_OFFSET_CAPTURE))
+		{
+			return self::DOWNLOAD_NOT_RECOGNIZED;
+		}
+		$expiry = $expMatches[1][0];
+		if($expiry && (time() > $expiry))
+		{
+			return self::DOWNLOAD_RECOGNIZED_NOT_OK;
+		}
+
+		$partToSign = $this->encodeFileName($partToSign);
+
+		$calculatedSignature = kDeliveryUtils::urlsafeB64Encode(hash_hmac('sha256', $partToSign, $this->key, true));
+		if($calculatedSignature !== $requestSignature)
+		{
+			return self::DOWNLOAD_RECOGNIZED_NOT_OK;
+		}
+
+		return self::DOWNLOAD_RECOGNIZED_OK;
+	}
+
+	protected function encodeFileName($partToSign)
+	{
 		if(preg_match('#/fileName/([^/]+)/#', $partToSign, $fileNameMatches, PREG_OFFSET_CAPTURE))
 		{
 			$prefix = substr($partToSign, 0, $fileNameMatches[1][1]);
@@ -49,13 +76,7 @@ class kKalturaUrlRecognizer extends kUrlRecognizer
 			$partToSign = $prefix . $newFileName . $suffix;
 		}
 
-		$calculatedSignature = kDeliveryUtils::urlsafeB64Encode(hash_hmac('sha256', $partToSign, $this->key, true));
-		if($calculatedSignature !== $requestSignature)
-		{
-			return false;
-		}
-
-		return true;
+		return $partToSign;
 	}
 
 }
