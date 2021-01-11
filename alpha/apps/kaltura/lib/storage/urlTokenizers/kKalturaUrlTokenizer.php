9e24e9971687c29680e68d178ce7c06ce09ef96e
InbalBD <inbal.bendavid@kaltura.com>
2021-01-11 02:58:42 +0200

    PLAT-11239: handle cases where file name is the final param

--- alpha/apps/kaltura/lib/storage/urlTokenizers/kKalturaUrlTokenizer.php
+++ alpha/apps/kaltura/lib/storage/urlTokenizers/kKalturaUrlTokenizer.php
@@ -15,22 +15,23 @@ class kKalturaUrlTokenizer extends kUrlTokenizer
 		$file = substr($url, $lastSlashPosition + 1);
 		$ending = '/' . $file;
 
-		if(preg_match('#/fileName/([^/]+)/#', $pathToSign, $matches, PREG_OFFSET_CAPTURE))
+		if(preg_match('#/fileName/([^/]+)#', $pathToSign, $matches, PREG_OFFSET_CAPTURE))
 		{
 			$fileNamePart = $matches[0][0];
 			$pathToSign = str_replace($fileNamePart, '/', $pathToSign);
-			$ending = $fileNamePart  . ltrim($ending, '/');
+			$ending = $fileNamePart  . $ending;
 		}
 
-		if(preg_match('#/dirFileName/([^/]+)/#', $pathToSign, $matches, PREG_OFFSET_CAPTURE))
+		if(preg_match('#/dirFileName/([^/]+)#', $pathToSign, $matches, PREG_OFFSET_CAPTURE))
 		{
 			$fileNamePart = $matches[0][0];
 			$pathToSign = str_replace($fileNamePart, '/', $pathToSign);
-			$ending = $fileNamePart . ltrim($ending, '/');
+			$ending = $fileNamePart . $ending;
 		}
 
 		$expiry = kApiCache::getTime() + $this->getWindow();
 		$pathToSign .= '/exp/' . $expiry;
+		$pathToSign = str_replace(array('///', '//'), array('/', '/'), $pathToSign);
 
 		$signature = kDeliveryUtils::urlsafeB64Encode(hash_hmac('sha256', $pathToSign, $this->key, true));
 		return $pathToSign . '/sig/' . $signature . $ending;
