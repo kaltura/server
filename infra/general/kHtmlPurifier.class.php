f6040da3eba0434cc21ddfad912620b3a41642b4
Moshe Maor <moshe.maor@kaltura.com>
2021-02-03 15:13:52 +0200

    PLAT-22455:html purifier fix caching issue

--- infra/general/kHtmlPurifier.class.php
+++ infra/general/kHtmlPurifier.class.php
@@ -97,13 +97,10 @@ class kHtmlPurifier
 		if ( ! self::$AllowedProperties )
 		{
 			$xssAllowedObjectProperties = kConf::get('xss_allowed_object_properties');
-			if (kCurrentContext::$HTMLPurifierBaseListOnlyUsage)
+            $AllowedProperties = $xssAllowedObjectProperties['base_list'];
+			if (!kCurrentContext::$HTMLPurifierBaseListOnlyUsage)
 			{
-				$AllowedProperties = $xssAllowedObjectProperties['base_list'];
-			}
-			else
-			{
-				$AllowedProperties = array_merge($xssAllowedObjectProperties['base_list'], $xssAllowedObjectProperties['extend_list']);
+                $AllowedProperties = array_merge($AllowedProperties, $xssAllowedObjectProperties['extend_list']);
 			}
 			// Convert values to keys (we don't care about the values) in order to test via array_key_exists.
 			self::$AllowedProperties = array_flip($AllowedProperties);
