332a274c29eb89ad72c046f3f8361138e59bc478
TalDubovKaltura <tal.dubov@kaltura.com>
2021-05-02 15:40:43 +0300

    PSVAMB-18194 -> CR fixes

--- plugins/reach/plugins/OpenCalais/batch/RuleEngine.php
+++ plugins/reach/plugins/OpenCalais/batch/RuleEngine.php
@@ -17,7 +17,8 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $kmsIntelliTagRules
      * @param string $textTimes
      */
-    public function __construct(array $kmsIntelliTagRules, $textTimes) {
+    public function __construct(array $kmsIntelliTagRules, $textTimes)
+    {
         $this->textTimes = json_decode($textTimes, true);
         $this->kmsIntelliTagRules = $this->setRulesWithKeys($kmsIntelliTagRules);
     }
@@ -25,9 +26,11 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $kmsIntelliTagRules
      * @return array
      */
-    protected function setRulesWithKeys(array $kmsIntelliTagRules) {
+    protected function setRulesWithKeys(array $kmsIntelliTagRules)
+    {
         $kmsIntelliTagRulesKeys = array();
-        foreach ($kmsIntelliTagRules as $kmsIntelliTagRule){
+        foreach ($kmsIntelliTagRules as $kmsIntelliTagRule)
+        {
             $kmsIntelliTagRulesKeys[$kmsIntelliTagRule[self::RULE_OCM_GROUPROP][self::RULE_ENTT_KEY_PROP]][] = $kmsIntelliTagRule;
         }
         return $kmsIntelliTagRulesKeys;
@@ -40,13 +43,17 @@ class RuleEngine extends OpenCalaisConstants
     {
         $responseArray = json_decode($response, true);
         $result = array();
-        foreach ($responseArray as $key => $apiItemDetails){
-            if(!$this->passedRuleConditions($apiItemDetails)){
+        foreach ($responseArray as $key => $apiItemDetails)
+        {
+            if(!$this->passedRuleConditions($apiItemDetails))
+            {
                 continue;
             }
             $rules = $this->getRulesForEntity($apiItemDetails);
-            foreach ($rules as $rule) {
-                if(isset($rule[self::RULE_OCM_GROUPROP][self::RULE_THRSHLD_PROP]) && !$this->passedRuleConditionThresholds($rule[self::RULE_OCM_GROUPROP][self::RULE_THRSHLD_PROP], $apiItemDetails)){
+            foreach ($rules as $rule)
+            {
+                if(isset($rule[self::RULE_OCM_GROUPROP][self::RULE_THRSHLD_PROP]) && !$this->passedRuleConditionThresholds($rule[self::RULE_OCM_GROUPROP][self::RULE_THRSHLD_PROP], $apiItemDetails))
+                {
                     continue;
                 }
                 $result = array_merge($result, $this->getValuesByRuleAndApiResponse($rule, $apiItemDetails));
@@ -59,15 +66,18 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $apiItemDetails
      * @return array[]
      */
-    protected function getValuesByRuleAndApiResponse(array $rule, array $apiItemDetails){
+    protected function getValuesByRuleAndApiResponse(array $rule, array $apiItemDetails)
+    {
         $cuePoints = $this->getValuesForCuePoints($apiItemDetails);
         $resolutions[] = $apiItemDetails;
-        if(isset($apiItemDetails['resolutions']) && !empty($apiItemDetails['resolutions'])){
+        if(isset($apiItemDetails['resolutions']) && !empty($apiItemDetails['resolutions']))
+        {
             $resolutions = $apiItemDetails['resolutions'];
         }
         $result = $this->getFieldsArrayUsingRule($resolutions, $rule, $apiItemDetails);
 
-        if(!empty($cuePoints)){
+        if(!empty($cuePoints))
+        {
             $result[0]['cuePoints_list'] = $cuePoints;
         }
         return $result;
@@ -75,17 +85,19 @@ class RuleEngine extends OpenCalaisConstants
     /**
      * @param array $items
      * @param array $rule
-     * @param string $systemName
+     * @param string $fullItem
      * @return array
      */
-    protected function getFieldsArrayUsingRule(array $items, array $rule, $fullItem){
+    protected function getFieldsArrayUsingRule(array $items, array $rule, $fullItem)
+    {
         $result = array();
-        foreach ($items as $item){
+        foreach ($items as $item)
+        {
             $result[] =  array(
-                'entryMetadataShowTaxonomy' => array(
+                self::ENTRY_METADATA_SHOW_TAXONOMY => array(
                     $rule[self::RULE_KALTURA_GROUPROP][self::RULE_SHO_TAX_ELE_PROP] => $item[$rule[self::RULE_OCM_GROUPROP][self::RULE_ENTT_ID_PROP]],
                 ),
-                'dynamicMetadata' => array(
+                self::DYNAMIC_METADATA => array(
                     'fieldName' => $rule[self::RULE_KALTURA_GROUPROP][self::RULE_SHO_TAX_ELE_PROP],
                     'objectId' => isset($item[$rule[self::RULE_OCM_GROUPROP][self::RULE_ENTT_ID_PROP]]) ? $item[$rule[self::RULE_OCM_GROUPROP][self::RULE_ENTT_ID_PROP]] : '',
                     'addIfNotExist' => isset($rule[self::RULE_KALTURA_GROUPROP][self::OP_OCM_ADD_DYNAMIC_OBJECT]),
@@ -99,8 +111,10 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $apiItemdetails
      * @return array
      */
-    protected function getValuesForCuePoints(array $apiItemdetails){
-        if(!isset($apiItemdetails[self::OPCAL_INSTANCES_PROP]) || count($apiItemdetails[self::OPCAL_INSTANCES_PROP]) < 1){
+    protected function getValuesForCuePoints(array $apiItemdetails)
+    {
+        if(!isset($apiItemdetails[self::OPCAL_INSTANCES_PROP]) || count($apiItemdetails[self::OPCAL_INSTANCES_PROP]) < 1)
+        {
             return array();
         }
         $name = $apiItemdetails[self::OPCAL_ENTT_NAME];
@@ -108,8 +122,10 @@ class RuleEngine extends OpenCalaisConstants
         $textTimes = $this->textTimes;
         $n = 0;
         $cuePoints = array();
-        foreach ($textTimes as $textWordTime){
-            if($textWordTime['w'] == $nameArray[0]){
+        foreach ($textTimes as $textWordTime)
+        {
+            if($textWordTime['w'] == $nameArray[0])
+            {
                 $this->handleCuePoints($cuePoints, $n, $nameArray);
             }
             $n++;
@@ -121,13 +137,16 @@ class RuleEngine extends OpenCalaisConstants
      * @param int $key
      * @param array $nameArray
      */
-    protected function handleCuePoints(array &$cuePoints, $key, array $nameArray) {
+    protected function handleCuePoints(array &$cuePoints, $key, array $nameArray)
+    {
         $textTimes = $this->textTimes;
         $fullName = '';
         $startFrom = $textTimes[$key]['s'];
         $endTo = $textTimes[$key]['e'];
-        for($i = 0; $i < count($nameArray); $i++){
-            if($textTimes[$key]['w'] == $nameArray[$i]){
+        for($i = 0; $i < count($nameArray); $i++)
+        {
+            if($textTimes[$key]['w'] == $nameArray[$i])
+            {
                 $endTo = $textTimes[$key]['e'];
                 $fullName .= $textTimes[$key++]['w'].' ';
             }
@@ -142,11 +161,14 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $apiItemDetails
      * @return bool
      */
-    protected function passedRuleConditions(array $apiItemDetails) {
-        if($this->getTypeFromItemInApi($apiItemDetails) == ''){ // no type property
+    protected function passedRuleConditions(array $apiItemDetails)
+    {
+        if($this->getTypeFromItemInApi($apiItemDetails) == '')
+        { // no type property
             return FALSE;
         }
-        if(empty($this->getRulesForEntity($apiItemDetails))){ // this type not exists on KMS Rules
+        if(empty($this->getRulesForEntity($apiItemDetails)))
+        { // this type not exists on KMS Rules
             return FALSE;
         }
         return TRUE;
@@ -155,9 +177,11 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $apiItemDetails
      * @return mixed|string
      */
-    protected function getRulesForEntity(array $apiItemDetails) {
+    protected function getRulesForEntity(array $apiItemDetails)
+    {
         $type = $this->getTypeFromItemInApi($apiItemDetails);
-        if(!isset($this->kmsIntelliTagRules[$type])){ // this type not exists on KMS Rules
+        if(!isset($this->kmsIntelliTagRules[$type]))
+        { // this type not exists on KMS Rules
             return array();
         }
         return $this->kmsIntelliTagRules[$type];
@@ -166,11 +190,14 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $apiItemDetails
      * @return mixed|string
      */
-    protected function getTypeFromItemInApi(array $apiItemDetails){
-        if(isset($apiItemDetails[self::OPCAL_TYPE_PROP])){
+    protected function getTypeFromItemInApi(array $apiItemDetails)
+    {
+        if(isset($apiItemDetails[self::OPCAL_TYPE_PROP]))
+        {
             return $apiItemDetails[self::OPCAL_TYPE_PROP];
         }
-        if(isset($apiItemDetails[self::OPCAL_TYPE_GROUP_PROP])){
+        if(isset($apiItemDetails[self::OPCAL_TYPE_GROUP_PROP]))
+        {
             return $apiItemDetails[self::OPCAL_TYPE_GROUP_PROP];
         }
         return '';
@@ -180,13 +207,17 @@ class RuleEngine extends OpenCalaisConstants
      * @param array $apiItemdetails
      * @return bool
      */
-    protected function passedRuleConditionThresholds(array $thresholds, array $apiItemdetails) {
+    protected function passedRuleConditionThresholds(array $thresholds, array $apiItemdetails)
+    {
         $thresholds = isset($thresholds[self::THRSHLD_GROUPPROP][self::THRSHLD_NAME_PROP]) ? $thresholds : $thresholds[self::THRSHLD_GROUPPROP];
-        foreach ($thresholds as $threshold){
-            if(!isset($apiItemdetails[$threshold[self::THRSHLD_NAME_PROP]])){  // no such item in the API response
+        foreach ($thresholds as $threshold)
+        {
+            if(!isset($apiItemdetails[$threshold[self::THRSHLD_NAME_PROP]]))
+            {  // no such item in the API response
                 return false;
             }
-            if(ThresholdComparisonFactory::verify($apiItemdetails[$threshold[self::THRSHLD_NAME_PROP]], $threshold[self::THRSHLD_VAL_PROP], $threshold[self::THRSHLD_COMPARISON_PROP]) === FALSE){
+            if(ThresholdComparisonFactory::verify($apiItemdetails[$threshold[self::THRSHLD_NAME_PROP]], $threshold[self::THRSHLD_VAL_PROP], $threshold[self::THRSHLD_COMPARISON_PROP]) === FALSE)
+            {
                 return false;
             }
         }
