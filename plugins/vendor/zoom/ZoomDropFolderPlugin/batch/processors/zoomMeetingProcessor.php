7e17c1fd417f735378c24a8a8089b543956ccd62
ravitshalem <ravit.shalem@kaltura.com>
2021-05-05 01:32:08 +0300

    PLAT-22490:code review remarks

--- plugins/vendor/zoom/ZoomDropFolderPlugin/batch/processors/zoomMeetingProcessor.php
+++ plugins/vendor/zoom/ZoomDropFolderPlugin/batch/processors/zoomMeetingProcessor.php
@@ -29,7 +29,7 @@ class zoomMeetingProcessor extends zoomRecordingProcessor
         }
 		if ($categories)
 		{
-			$entry -> categories = implode(',', $categories);
+			$entry->categories = implode(',', $categories);
 		}
 	}
 
