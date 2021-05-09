fad175ded6d5dd6160232b038daf87fcc5b12819
Nadav Harnik <nadav.harnik@kaltura.com>
2021-05-09 23:07:24 +0300

    add base dir

--- alpha/scripts/kava/kava_plays_views_sync.sh
+++ alpha/scripts/kava/kava_plays_views_sync.sh
@@ -4,7 +4,7 @@
 echo `date`
 
 echo "get plays/views"
-TEMP_FILE=/tmp/playsviewsDump.txt
+TEMP_FILE=$BASE_DIR/tmp/playsviewsDump.txt
 mysql -h$KAVA_DB_HOST -P$KAVA_DB_PORT -u$KAVA_DB_USER -p$KAVA_DB_PASS $KAVA_DB_NAME -BN -e 'select entry_id, UNIX_TIMESTAMP(last_played_at), plays, views, plays_30days, views_30days, plays_7days, views_7days, plays_1day, views_1day
 from kava_plays_views' | php $BASE_DIR/app/alpha/scripts/kava/copyRecordingPlaysviewsToLive.php $TEMP_FILE
 # copyRecordingPlaysviewsToLive.php writes the dump to $TEMP_FILE
