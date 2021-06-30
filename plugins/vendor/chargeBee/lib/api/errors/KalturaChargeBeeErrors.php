5f2a1a8d3e216344e01906ec59cec098a54b9703
drorsouhami <dror.souhami@kaltura.com>
2021-06-30 10:02:55 +0300

    PLAT-22883: add support in basic http user password authentication per action

--- plugins/vendor/chargeBee/lib/api/errors/KalturaChargeBeeErrors.php
+++ plugins/vendor/chargeBee/lib/api/errors/KalturaChargeBeeErrors.php
@@ -7,4 +7,7 @@ class KalturaChargeBeeErrors extends KalturaErrors
 {
 	const NO_VENDOR_CONFIGURATION  = 'NO_VENDOR_CONFIGURATION;;Vendor configuration file was not found!';
 	const CHARGE_BEE_VENDOR_INTEGRATION_NOT_FOUND = "CHARGE_BEE_VENDOR_INTEGRATION_NOT_FOUND;ID;ChargeBee vendor integration with id provided not found [@ID@]";
+	const UNAUTHORIZED_USER_PASSWORD = 'UNAUTHORIZED_USER_PASSWORD;;Unauthorized user or password';
+	const MISSING_USER_PASSWORD_CONFIGURATION = 'MISSING_USER_PASSWORD_CONFIGURATION;;Missing configured user or password';
+	const MISSING_EVENT_TYPE = 'MISSING_EVENT_TYPE;;Missing event type in the request';
 }
