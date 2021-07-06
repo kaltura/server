2c7af4039e1cd0cf32dcc5589163daa7d6b97186
carmelsahar <carmel.sahar@kaltura.com>
2021-07-06 15:43:53 +0300

    FOUN-66 remove isEqual

--- alpha/apps/kaltura/lib/rules/conditions/kSiteCondition.php
+++ alpha/apps/kaltura/lib/rules/conditions/kSiteCondition.php
@@ -65,7 +65,7 @@ class kSiteCondition extends kMatchCondition
 		$oldMatch = ($field === $value) || (strpos($field, ".".$value) !== false);
 		$newMatch = ($field === $value) || (kString::endsWith($field, ".".$value) !== false);
 		KalturaLog::debug("kSiteCondition checks matches condition with field [$field] - oldMatch ".($oldMatch? 'true' : 'false').
-			" - newMatch ".($newMatch? 'true' : 'false')." isEqual is ".(($oldMatch==$newMatch)? 'true' : 'false'));
+			" - newMatch ".($newMatch? 'true' : 'false'));
 		Return $oldMatch;
 	}
 
