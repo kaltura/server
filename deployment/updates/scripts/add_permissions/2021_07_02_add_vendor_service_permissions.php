4130cb4ee938a47205526d3d629b8baee07c15aa
hila.karimov <hila.karimov@kaltura.com>
2021-07-06 21:06:17 -0400

    Update service name

--- deployment/updates/scripts/add_permissions/2021_07_02_add_vendor_service_permissions.php
+++ deployment/updates/scripts/add_permissions/2021_07_02_add_vendor_service_permissions.php
@@ -6,5 +6,5 @@
 
 $addPermissionsAndItemsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
 
-$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.vendor.vendor.ini';
+$config = realpath(dirname(__FILE__)) . '/../../../permissions/service.vendor.vendorIntegration.ini';
 passthru("php $addPermissionsAndItemsScript $config");
