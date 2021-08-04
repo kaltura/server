e113a20cd117bc74b66192b24e9b6bc8c0c272f4
gotlieb <gotlieb.erez@gmail.com>
2021-08-04 20:02:21 +0300

    FOUN-174

--- deployment/updates/scripts/2021_07_26_deploy_capture_tools_content_conversion_data.php
+++ deployment/updates/scripts/2021_07_26_deploy_capture_tools_content_conversion_data.php
@@ -2,15 +2,15 @@
 /**
  * @package deployment
  *
- * Deploy static content Flavors + conversion Profile
+ * Deploy capture tools content Flavors + conversion Profile
  *
  * No need to re-run after server code deploy
  */
 require_once (__DIR__ . '/../../bootstrap.php');
 $script = realpath(dirname(__FILE__) . '/../../') . '/base/scripts/insertDefaults.php';
 
-$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_static_content.conversionProfile2.ini';
+$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_capture_tools_content.conversionProfile2.ini';
 passthru("php $script $config");
 
-$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_static_content.flavorParamsConversionProfile.ini';
+$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_capture_tools_content.flavorParamsConversionProfile.ini';
 passthru("php $script $config");
--- deployment/updates/scripts/2021_07_26_deploy_zoom_recordings_conversion_data.php
+++ deployment/updates/scripts/2021_07_26_deploy_zoom_recordings_conversion_data.php
@@ -2,15 +2,15 @@
 /**
  * @package deployment
  *
- * Deploy zoom content Flavors + conversion Profile
+ * Deploy zoom recordings Flavors + conversion Profile
  *
  * No need to re-run after server code deploy
  */
 require_once (__DIR__ . '/../../bootstrap.php');
 $script = realpath(dirname(__FILE__) . '/../../') . '/base/scripts/insertDefaults.php';
 
-$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_zoom_content.conversionProfile2.ini';
+$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_zoom_recodrings.conversionProfile2.ini';
 passthru("php $script $config");
 
-$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_zoom_content.flavorParamsConversionProfile.ini';
+$config = realpath(dirname(__FILE__)) . '/ini_files/2021_07_26_zoom_recordings.flavorParamsConversionProfile.ini';
 passthru("php $script $config");
