31f65d0504038b82ac8348ad3e3d20a081f9a934
gotlieb <gotlieb.erez@gmail.com>
2021-08-10 11:09:13 +0300

    FOUN-195

--- alpha/apps/kaltura/lib/batch2/kFlowHelper.php
+++ alpha/apps/kaltura/lib/batch2/kFlowHelper.php
@@ -3555,28 +3555,32 @@ class kFlowHelper
 	protected static function handleStaticContent(entry $entry)
 	{
 		$disableStaticContentSourceDeletionPartners = kConf::get('disableStaticContentSourceDeletionPartners', 'runtime_config', array());
-		if (!in_array($entry->getPartnerId(), $disableStaticContentSourceDeletionPartners) && kBusinessPreConvertDL::shouldCheckStaticContentFlow($entry))
+		if (in_array($entry->getPartnerId(), $disableStaticContentSourceDeletionPartners) || !kBusinessPreConvertDL::shouldCheckStaticContentFlow($entry))
 		{
-			$conversionProfileKey = kBusinessPreConvertDL::getConversionProfileKey($entry);
-			$staticContentConversionProfiles = kConf::get('staticContentConversionProfiles', 'runtime_config', array());
-			$profile = conversionProfile2Peer::retrieveByPartnerIdAndSystemName($entry->getPartnerId(), $staticContentConversionProfiles[$conversionProfileKey], ConversionProfileType::MEDIA, true);
-			if ($profile)
+			return;
+		}
+
+		$conversionProfileKey = kBusinessPreConvertDL::getConversionProfileKey($entry);
+		$staticContentConversionProfiles = kConf::get('staticContentConversionProfiles', 'runtime_config', array());
+		$profile = conversionProfile2Peer::retrieveByPartnerIdAndSystemName($entry->getPartnerId(), $staticContentConversionProfiles[$conversionProfileKey], ConversionProfileType::MEDIA, true);
+		if (!$profile)
+		{
+			return;
+		}
+
+		$nonSourceFlavors = assetPeer::retrieveFlavorsWithTagsFiltering($entry->getId(), flavorParams::TAG_MBR, flavorParams::TAG_SOURCE);
+		if (count($nonSourceFlavors) >= 2)
+		{
+			$sourceFlavor = assetPeer::retrieveOriginalByEntryId($entry->getId());
+			$highestBitrateFlavor = assetPeer::retrieveHighestBitrateByEntryId($entry->getId(), null, flavorParams::TAG_SOURCE);
+			//If source flavor is not part of mbr playback and it is not the only asset on the entry do the replacement
+			if ($sourceFlavor && !$sourceFlavor->hasTag(flavorParams::TAG_MBR) && $highestBitrateFlavor &&  $highestBitrateFlavor->getId() != $sourceFlavor->getId())
 			{
-				$nonSourceFlavors = assetPeer::retrieveFlavorsWithTagsFiltering($entry->getId(), flavorParams::TAG_MBR, flavorParams::TAG_SOURCE);
-				if (count($nonSourceFlavors) >= 2)
-				{
-					$sourceFlavor = assetPeer::retrieveOriginalByEntryId($entry->getId());
-					$highestBitrateFlavor = assetPeer::retrieveHighestBitrateByEntryId($entry->getId(), null, flavorParams::TAG_SOURCE);
-					//If source flavor is not part of mbr playback and it is not the only asset on the entry do the replacement
-					if (!$sourceFlavor->hasTag(flavorParams::TAG_MBR) && $highestBitrateFlavor && $sourceFlavor && $highestBitrateFlavor->getId() != $sourceFlavor->getId())
-					{
-						$sourceFlavor->setStatus(asset::ASSET_STATUS_DELETED);
-						$sourceFlavor->save();
-						$highestBitrateFlavor->setIsOriginal(true);
-						$highestBitrateFlavor->addTags(array(flavorParams::TAG_SOURCE));
-						$highestBitrateFlavor->save();
-					}
-				}
+				$sourceFlavor->setStatus(asset::ASSET_STATUS_DELETED);
+				$sourceFlavor->save();
+				$highestBitrateFlavor->setIsOriginal(true);
+				$highestBitrateFlavor->addTags(array(flavorParams::TAG_SOURCE));
+				$highestBitrateFlavor->save();
 			}
 		}
 	}
