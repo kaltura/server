af40d293d9fd789b3ed80b2f7d1d193815da6e33
danielbarak <daniel.barak@kaltura.com>
2021-09-12 16:24:11 +0300

    Adding Zoom Transcript accuracy default value

---
+++ plugins/vendor/zoom/lib/kZoomFlowManager.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package plugins.Vendor
+ * @subpackage zoom
+ */
+
+class kZoomFlowManager implements kObjectCreatedEventConsumer
+{
+	const CONFIGURATION_PARAM_NAME = 'Zoom';
+	
+	
+	/**
+	 * @param BaseObject $object
+	 * @return bool
+	 */
+	public function objectCreated (BaseObject $object)
+	{
+		/* @var $object CaptionAsset */
+		$zoomConfiguration = kConf::get(self::CONFIGURATION_PARAM_NAME, kConfMapNames::MAP_NAME);
+		$object->setAccuracy($zoomConfiguration['ZoomTranscriptionAccuracy']);
+		$object->save();
+		
+		return true;
+	}
+	
+	/**
+	 * @param BaseObject $object
+	 * @return bool
+	 */
+	public function shouldConsumeCreatedEvent (BaseObject $object)
+	{
+		if($object instanceof CaptionAsset && $object->getSource() === CaptionSource::ZOOM)
+		{
+			return true;
+		}
+	}
+}
