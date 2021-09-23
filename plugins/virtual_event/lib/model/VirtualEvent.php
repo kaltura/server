2fd1d976806f030f1c9aa87239a9448ac959541e
danielbarak <daniel.barak@kaltura.com>
2021-09-23 15:57:11 +0300

    Cont.

--- plugins/virtual_event/lib/model/VirtualEvent.php
+++ plugins/virtual_event/lib/model/VirtualEvent.php
@@ -1,1 +1,204 @@
 <?php
+
+/**
+ * Skeleton subclass for representing a row from the 'virtual_event' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package plugins.virtual_event
+ * @subpackage model
+ */
+abstract class VirtualEvent extends BaseVirtualEvent implements IRelatedObject, IIndexable
+{
+	const CUSTOM_DATA_FIELD_OWNER_ID   = 'ownerId';
+	
+	const RESOURCES_INDEXED_FIELD_PREFIX = 'pid';
+	
+	
+	public function __construct ()
+	{
+		parent::__construct();
+		$this->applyDefaultValues();
+	}
+	
+	/**
+	 * Applies default values to this object.
+	 * This method should be called from the object's constructor (or equivalent initialization method).
+	 * @see __construct()
+	 */
+	public function applyDefaultValues ()
+	{
+		$this->setSequence(1);
+	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseVirtualEvent::preInsert()
+	 */
+	public function preInsert (PropelPDO $con = null)
+	{
+		$this->setStatus(VirtualEventStatus::ACTIVE);
+		$this->setPartnerId(kCurrentContext::getCurrentPartnerId());
+		
+		$this->setCustomDataObj();
+		
+		return parent::preInsert($con);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see BaseVirtualEvent::postInsert()
+	 */
+	public function postInsert (PropelPDO $con = null)
+	{
+		parent::postInsert($con);
+		
+		if (!$this->alreadyInSave)
+		{
+			kEventsManager::raiseEvent(new kObjectAddedEvent($this));
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see BaseVirtualEvent::postUpdate()
+	 */
+	public function postUpdate (PropelPDO $con = null)
+	{
+		if ($this->alreadyInSave)
+		{
+			return parent::postUpdate($con);
+		}
+		
+		$objectUpdated = $this->isModified();
+		$objectDeleted = false;
+		if ($this->isColumnModified(VirtualEventPeer::STATUS) && $this->getStatus() == VirtualEventStatus::DELETED)
+		{
+			$objectDeleted = true;
+		}
+		
+		$ret = parent::postUpdate($con);
+		
+		if ($objectDeleted)
+		{
+			kEventsManager::raiseEvent(new kObjectDeletedEvent($this));
+		}
+		
+		if ($objectUpdated)
+		{
+			kEventsManager::raiseEvent(new kObjectUpdatedEvent($this));
+		}
+		
+		return $ret;
+	}
+	
+	
+	/* (non-PHPdoc)
+	 * @see BaseVirtualEvent::preSave()
+	 */
+	public function preSave (PropelPDO $con = null)
+	{
+		return parent::preSave($con);
+	}
+	
+	public function incrementSequence ()
+	{
+		$this->setSequence(kDataCenterMgr::incrementVersion($this->getSequence()));
+	}
+	
+	/**
+	 * @param string $v
+	 */
+	public function setOwnerId ($puserId)
+	{
+		$kuser = kuserPeer::createKuserForPartner(kCurrentContext::getCurrentPartnerId(), $puserId, kCurrentContext::$is_admin_session);
+		$this->setOwnerKuserId($kuser->getId());
+		$this->putInCustomData(self::CUSTOM_DATA_FIELD_OWNER_ID, $puserId);
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getOwnerId ()
+	{
+		return $this->getFromCustomData(self::CUSTOM_DATA_FIELD_OWNER_ID);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see IIndexable::getIntId()
+	 */
+	public function getIntId ()
+	{
+		return $this->getId();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see IIndexable::getEntryId()
+	 */
+	public function getEntryId ()
+	{
+		return null;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see IIndexable::getIndexObjectName()
+	 */
+	public function getIndexObjectName ()
+	{
+		return 'ScheduleEventIndex';
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see IIndexable::indexToSearchIndex()
+	 */
+	public function indexToSearchIndex ()
+	{
+		kEventsManager::raiseEventDeferred(new kObjectReadyForIndexEvent($this));
+	}
+	
+	public function getSphinxIndexName ()
+	{
+		return kSphinxSearchManager::getSphinxIndexName(ScheduleEventIndex::getObjectIndexName());
+	}
+	
+	public function getEntryIds ()
+	{
+		return '';
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see IIndexable::getTemplateEntryId()
+	 */
+	public function getTemplateEntryId ()
+	{
+		return null;
+	}
+	
+	public function getSummary ()
+	{
+		if (parent::getSummary())
+		{
+			return parent::getSummary();
+		}
+	}
+	
+	public function getCacheInvalidationKeys ()
+	{
+		return array("virtualEvent:id" . strtolower($this->getId()));
+	}
+	
+	public function getSphinxMatchOptimizations ()
+	{
+		$objectName = $this->getIndexObjectName();
+		
+		return $objectName::getSphinxMatchOptimizations($this);
+	}
+}
--- plugins/virtual_event/lib/model/VirtualEventPeer.php
+++ plugins/virtual_event/lib/model/VirtualEventPeer.php
@@ -2,7 +2,7 @@
 
 
 /**
- * Skeleton subclass for performing query and update operations on the 'schedule_event' table.
+ * Skeleton subclass for performing query and update operations on the 'virtual_event' table.
  *
  *
  *
@@ -83,7 +83,6 @@ class VirtualEventPeer extends BaseVirtualEventPeer implements IRelatedObjectPee
 	public static function deleteByParentId($parentId, array $exceptForIds = null)
 	{
 		$criteria = new Criteria();
-		$criteria->add(VirtualEventPeer::PARENT_ID, $parentId);
 		$criteria->add(VirtualEventPeer::PARTNER_ID, kCurrentContext::getCurrentPartnerId());
 		
 		if($exceptForIds)
@@ -104,41 +103,6 @@ class VirtualEventPeer extends BaseVirtualEventPeer implements IRelatedObjectPee
 	}
 	
 	/**
-	 * Updates the status of all occurrences to cancelled
-	 * @param int $parentId
-	 * @param array $exceptForDates
-	 */
-	public static function cancelByParentId($parentId, array $exceptForDates = null)
-	{
-		$criteria = new Criteria();
-		$criteria->add(VirtualEventPeer::PARENT_ID, $parentId);
-		$criteria->add(VirtualEventPeer::START_DATE, kApiCache::getTime(), Criteria::GREATER_THAN);
-		
-		if($exceptForDates)
-		{
-			$criteria->add(VirtualEventPeer::ORIGINAL_START_DATE, $exceptForDates, Criteria::NOT_IN);
-		}
-		
-		$now = time();
-		
-		$update = new Criteria();
-		$update->add(VirtualEventPeer::STATUS, VirtualEventStatus::CANCELLED);
-		$update->add(VirtualEventPeer::UPDATED_AT, $now);
-		
-		$con = Propel::getConnection(VirtualEventPeer::DATABASE_NAME, Propel::CONNECTION_WRITE);
-		BasePeer::doUpdate($criteria, $update, $con);
-		
-		$virtualEvents = VirtualEventPeer::doSelect($criteria);
-		foreach($virtualEvents as $virtualEvent)
-		{
-			/* @var $virtualEvent VirtualEvent */
-			$virtualEvent->setStatus(VirtualEventStatus::CANCELLED);
-			$virtualEvent->setUpdatedAt($now);
-			$virtualEvent->indexToSearchIndex();
-		}
-	}
-	
-	/**
 	 * @param int $pk
 	 * @return VirtualEvent
 	 */
@@ -149,168 +113,6 @@ class VirtualEventPeer extends BaseVirtualEventPeer implements IRelatedObjectPee
 		self::setUseCriteriaFilter(true);
 		
 		return $virtualEvent;
-	}
-	
-	/**
-	 * @param int $parentId
-	 * @return array<VirtualEvent>
-	 */
-	public static function retrieveByParentId($parentId)
-	{
-		$criteria = new Criteria();
-		$criteria->add(VirtualEventPeer::PARENT_ID, $parentId);
-		return VirtualEventPeer::doSelect($criteria);
-	}
-	
-	
-	
-	/**
-	 * @param int $parentId
-	 * @param array $dates
-	 * @return array<VirtualEvent>
-	 */
-	public static function retrieveByParentIdAndDates($parentId, array $dates)
-	{
-		$criteria = new Criteria();
-		$criteria->add(VirtualEventPeer::PARENT_ID, $parentId);
-		$criteria->add(VirtualEventPeer::ORIGINAL_START_DATE, $dates, Criteria::IN);
-		
-		return VirtualEventPeer::doSelect($criteria);
-	}
-	
-	/**
-	 * @param int $parentId
-	 * @param array $startDates
-	 * @param array $endDates
-	 * @return array<VirtualEvent>
-	 */
-	public static function retrieveByParentIdAndStartAndEndDates($parentId, $startDates, $endDates)
-	{
-		$criteria = new Criteria();
-		$criteria->add(VirtualEventPeer::PARENT_ID, $parentId);
-		$criteria->add(VirtualEventPeer::START_DATE, $startDates, Criteria::IN);
-		$criteria->add(VirtualEventPeer::END_DATE, $endDates, Criteria::IN);
-		
-		return VirtualEventPeer::doSelect($criteria);
-	}
-	
-	/**
-	 * @param string $templateEntryId
-	 * @return array<VirtualEvent>
-	 */
-	public static function retrieveByTemplateEntryId($templateEntryId)
-	{
-		$c = KalturaCriteria::create(VirtualEventPeer::OM_CLASS);
-		$filter = new VirtualEventFilter();
-		$filter->setTemplateEntryIdEqual($templateEntryId);
-		$filter->attachToCriteria($c);
-		
-		return self::doSelect($c);
-	}
-	
-	/**
-	 * @param string $templateEntryId
-	 * @param array $types
-	 * @param int $startTime
-	 * @param int $endTime
-	 * @return array<VirtualEvent>
-	 */
-	public static function retrieveByTemplateEntryIdAndTypes($templateEntryId, $types, $startTime = null, $endTime = null)
-	{
-		$c = KalturaCriteria::create(VirtualEventPeer::OM_CLASS);
-		$c->add(VirtualEventPeer::TYPE, $types, Criteria::IN);
-		if ($startTime) // if giving start time - ignore all the events that already finished
-		{
-			$c->add(VirtualEventPeer::END_DATE, $startTime - self::TIME_MARGIN, Criteria::GREATER_EQUAL);
-		}
-		if ($endTime) // if giving end time - ignore all future events after 6 hours margin.
-		{
-			$c->add(VirtualEventPeer::START_DATE, $endTime + self::TIME_MARGIN, Criteria::LESS_EQUAL);
-		}
-		$filter = new VirtualEventFilter();
-		$filter->setTemplateEntryIdEqual($templateEntryId);
-		$filter->attachToCriteria($c);
-		return self::doSelect($c);
-	}
-	
-	public static function retrieveByTemplateEntryIdAndTime($templateEntryId,
-	                                                        $time = null)
-	{
-		$time = $time ? $time : time();
-		
-		$c = KalturaCriteria::create(VirtualEventPeer::OM_CLASS);
-		$c->add(VirtualEventPeer::END_DATE, $time, Criteria::GREATER_EQUAL);
-		$c->add(VirtualEventPeer::START_DATE, $time, Criteria::LESS_EQUAL);
-		$filter = new VirtualEventFilter();
-		$filter->setTemplateEntryIdEqual($templateEntryId);
-		$filter->attachToCriteria($c);
-		return self::doSelect($c);
-	}
-	
-	/**
-	 * @param string $templateEntryId
-	 * @param int $startTime
-	 * @param int $endTime
-	 * @param array $types
-	 * @return array<VirtualEvent>
-	 */
-	public static function retrieveOtherEvents($templateEntryId, $startDate, $endDate, array $idsToIgnore)
-	{
-		$c = KalturaCriteria::create(VirtualEventPeer::OM_CLASS);
-		
-		$criterion1 = $c->getNewCriterion(VirtualEventPeer::START_DATE, $startDate, Criteria::LESS_THAN);
-		$criterion1->addAnd($c->getNewCriterion(VirtualEventPeer::END_DATE, $startDate, Criteria::GREATER_THAN));
-		
-		$criterion2 = $c->getNewCriterion(VirtualEventPeer::START_DATE, $endDate, Criteria::LESS_THAN);
-		$criterion2->addAnd($c->getNewCriterion(VirtualEventPeer::END_DATE, $endDate, Criteria::GREATER_THAN));
-		
-		$criterion3 = $c->getNewCriterion(VirtualEventPeer::START_DATE, $startDate, Criteria::GREATER_EQUAL);
-		$criterion3->addAnd($c->getNewCriterion(VirtualEventPeer::END_DATE, $endDate, Criteria::LESS_EQUAL));
-		
-		$c->addOr($criterion1);
-		$c->addOr($criterion2);
-		$c->addOr($criterion3);
-		
-		$filter = new VirtualEventFilter();
-		$filter->setTemplateEntryIdEqual($templateEntryId);
-		$filter->setIdsNotIn($idsToIgnore);
-		$filter->attachToCriteria($c);
-		
-		return self::doSelect($c);
-	}
-	
-	/**
-	 * @param string $resourceIds
-	 * @param date $startDate
-	 * @param date $endDate
-	 * @param string|null $scheduleEventIdToIgnore
-	 * @return array <VirtualEvent>
-	 */
-	public static function retrieveEventsByResourceIdsAndDateWindow($resourceIds, $startDate, $endDate, $scheduleEventIdToIgnore = null)
-	{
-		$c = self::getRetrieveEventsByDateWindowCriteria($startDate, $endDate, $scheduleEventIdToIgnore);
-		$filter = new VirtualEventFilter();
-		$filter->setResourceIdsIn($resourceIds);
-		$filter->attachToCriteria($c);
-		
-		return self::doSelect($c);
-	}
-	
-	
-	/**
-	 * @param date $startDate
-	 * @param date $endDate
-	 * @param string|null $scheduleEventIdToIgnore
-	 * @return KalturaCriteria
-	 */
-	protected static function getRetrieveEventsByDateWindowCriteria($startDate, $endDate, $scheduleEventIdToIgnore = null)
-	{
-		$c = KalturaCriteria::create(VirtualEventPeer::OM_CLASS);
-		$c->addAnd(VirtualEventPeer::START_DATE, $endDate, Criteria::LESS_THAN);
-		$c->addAnd(VirtualEventPeer::END_DATE, $startDate, Criteria::GREATER_THAN);
-		$c->addAnd(VirtualEventPeer::STATUS, VirtualEventStatus::ACTIVE, Criteria::EQUAL);
-		
-		return $c;
 	}
 	
 	/* (non-PHPdoc)
