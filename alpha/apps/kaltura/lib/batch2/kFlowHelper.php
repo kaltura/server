7413358d49cb80e03bb8e16640215fb8bd447739
InbalBD <inbal.bendavid@kaltura.com>
2021-09-26 11:31:39 +0300

    PLAT-23131: if report should be custom make sure we get base url and avoid appending id to url

--- alpha/apps/kaltura/lib/batch2/kFlowHelper.php
+++ alpha/apps/kaltura/lib/batch2/kFlowHelper.php
@@ -3160,7 +3160,7 @@ class kFlowHelper
 				$reportName = '';
 			}
 			$url = "$fileName|$reportName|$time";
-			$url = urldecode($baseUrl) . '?id=' . urlencode($url);
+			$url = urldecode($baseUrl) . urlencode($url);
 			return $url;
 		}
 		
