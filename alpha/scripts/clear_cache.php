d500ae9c79b0c562ec9c4db219e0ec0f7dd9bae8
Arnon Rotem-Gal-Oz <arnon.rotem-gal-oz@kaltura.com>
2021-10-10 16:10:47 +0300

    revert php8 change

--- alpha/scripts/clear_cache.php
+++ alpha/scripts/clear_cache.php
@@ -43,7 +43,7 @@ function fixPath($path)
 function askToDelete($path, $interactive)
 {	
 	$baseKalturaPath = realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'../..');
-	if (str_starts_with($path, (string)$baseKalturaPath))
+	if (strpos($path, (string) $baseKalturaPath) === 0)
 	{
 		if ($interactive)
 		{
