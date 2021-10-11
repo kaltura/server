aea6429d7875a421df5c71e2750b7e44597fb389
hilak <hila.karimov@kaltura.com>
2021-10-11 18:31:46 -0400

    PSVAMB-27566

--- plugins/content/view_history/lib/model/ViewHistoryUserEntry.php
+++ plugins/content/view_history/lib/model/ViewHistoryUserEntry.php
@@ -5,9 +5,10 @@
  */
 class ViewHistoryUserEntry extends UserEntry
 {
-	const CUSTOM_DATA_PLAYBACK_CONTEXT = "playbackContext";
-	const CUSTOM_DATA_LAST_TIME_REACHED = "lastTimeReached";
-	const CUSTOM_DATA_LAST_UPDATE_TIME = "lastUpdateTime";
+	const CUSTOM_DATA_PLAYBACK_CONTEXT = 'playbackContext';
+	const CUSTOM_DATA_LAST_TIME_REACHED = 'lastTimeReached';
+	const CUSTOM_DATA_LAST_UPDATE_TIME = 'lastUpdateTime';
+	const CUSTOM_DATA_LAST_ENTRY_ID = 'lastEntryId';
 	
 	public function __construct()
 	{
@@ -18,9 +19,10 @@ class ViewHistoryUserEntry extends UserEntry
 	public function getPlaybackContext ()					{return $this->getFromCustomData(self::CUSTOM_DATA_PLAYBACK_CONTEXT);}
 	public function getLastTimeReached ()					{return $this->getFromCustomData(self::CUSTOM_DATA_LAST_TIME_REACHED);}
 	public function getLastUpdateTime ()					{return $this->getFromCustomData(self::CUSTOM_DATA_LAST_UPDATE_TIME);}
+	public function getLastEntryId ()						{return $this->getFromCustomData(self::CUSTOM_DATA_LAST_ENTRY_ID);}
 	
 	public function setPlaybackContext ($v)					{return $this->putInCustomData(self::CUSTOM_DATA_PLAYBACK_CONTEXT, $v);}
 	public function setLastTimeReached ($v)					{return $this->putInCustomData(self::CUSTOM_DATA_LAST_TIME_REACHED, $v);}
-	public function setLastUpdateTime ($v)					{return $this->putInCustomData(self::CUSTOM_DATA_LAST_UPDATE_TIME, $v);}
+	public function setLastEntryId ($v)						{return $this->putInCustomData(self::CUSTOM_DATA_LAST_ENTRY_ID, $v);}
 	
 }
