31fdb4aead35a7a75e5e9ebb2af6b82b5c1ba4de
drorsouhami <dror.souhami@kaltura.com>
2021-10-26 14:21:21 +0300

    Changes after code review

--- alpha/apps/kaltura/lib/live/kLiveStreamConsumer.php
+++ alpha/apps/kaltura/lib/live/kLiveStreamConsumer.php
@@ -3,62 +3,78 @@
 
 class kLiveStreamConsumer implements kObjectChangedEventConsumer
 {
-    /**
-     * @inheritDoc
-     */
-    public function objectChanged(BaseObject $object, array $modifiedColumns)
-    {
-        if ($object instanceof LiveEntry)
-        {
-            $this->handleLiveEntryChanged($object, $modifiedColumns);
-        }
+	/**
+	* @inheritDoc
+	 */
+	public function objectChanged(BaseObject $object, array $modifiedColumns)
+	{
+		if ($object instanceof LiveEntry && $this->isLiveEntryCategoryChanged($object, $modifiedColumns))
+		{
+			$this->handleLiveEntryCategoryChanged($object, $modifiedColumns);
+		}
+		
+		return true;
+	}
 
-        return true;
-    }
-
-    protected function handleLiveEntryChanged(entry $object, array $modifiedColumns)
-    {
-    	if(!in_array(entryPeer::CATEGORIES, $modifiedColumns))
-        {
-            return false;
-        }
-
-        /* @var $object LiveEntry */
-        $recordedEntry = entryPeer::retrieveByPK($object->getRecordedEntryId());
-        if (!$recordedEntry)
-        {
-            KalturaLog::info("Recorded entry {$object->getRecordedEntryId()} could not be retrieved. Skipping.");
-            return true;
-        }
-        
-        $categories = $object->getCategories();
+	protected function handleLiveEntryCategoryChanged(entry $object, array $modifiedColumns)
+	{
+		/* @var $object LiveEntry */
+		if (!$object->getRecordedEntryId())
+		{
+			return false;
+		}
+		$recordedEntry = entryPeer::retrieveByPK($object->getRecordedEntryId());
+		if (!$recordedEntry)
+		{
+			KalturaLog::info("Recorded entry {$object->getRecordedEntryId()} could not be retrieved.");
+			return false;
+		}
+		
+		$categories = $object->getCategories();
 		if (is_null($categories))
 		{
-			KalturaLog::info("Categories for live entry {$object->getId()} could not be retrieved retrieved. Skipping.");
+			KalturaLog::info("Categories for live entry {$object->getId()} could not be retrieved retrieved.");
+			return false;
+		}
+		
+		$recordedEntry->setCategories($categories);
+		$recordedEntry->save();
+		
+		return true;
+	}
+
+	protected function isLiveEntryCategoryChanged(entry $object, array $modifiedColumns)
+	{
+		/* @var $object LiveEntry */
+		
+		if (!$object->getRecordedEntryId())
+		{
+			return false;
+		}
+		
+		if(!in_array(entryPeer::CATEGORIES, $modifiedColumns))
+		{
+			return false;
+		}
+		
+		if (!PermissionPeer::isValidForPartner(PermissionName::FEATURE_KALTURA_LIVE_SYNC_RECORDED_VOD_CATEGORY, kCurrentContext::getCurrentPartnerId()))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function shouldConsumeChangedEvent(BaseObject $object, array $modifiedColumns)
+	{
+		if ($object instanceof LiveEntry && $this->isLiveEntryCategoryChanged($object, $modifiedColumns))
+		{
 			return true;
 		}
-
-        $recordedEntry->setCategories($categories);
-		$recordedEntry->save();
-
-        return true;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function shouldConsumeChangedEvent(BaseObject $object, array $modifiedColumns)
-    {
-        if (!($object instanceof LiveEntry))
-        {
-            return false;
-        }
-        
-        if (!PermissionPeer::isValidForPartner(PermissionName::FEATURE_KALTURA_LIVE_SYNC_RECORDED_VOD_CATEGORY, kCurrentContext::getCurrentPartnerId()))
-        {
-            return false;
-        }
 		
-        return true;
-    }
+		return false;
+	}
 }
