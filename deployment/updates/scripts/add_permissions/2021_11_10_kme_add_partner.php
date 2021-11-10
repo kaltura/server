f1663709d5bf98e56b3c886b6db252ff8adcea75
ravitshalem <ravit.shalem@kaltura.com>
2021-11-10 15:04:02 +0200

    PLAT-23254:add new partner for KME

--- deployment/updates/scripts/add_permissions/2021_11_10_kme_add_partner.php
+++ deployment/updates/scripts/add_permissions/2021_11_10_kme_add_partner.php
@@ -3,15 +3,15 @@
  * @package deployment
  * @subpackage quasar.roles_and_permissions
  */
-/*
+
 $script = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/insertDefaults.php';
 $config = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/init_data/01.Partner.ini';
 passthru("php $script $config");
-*/
+
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
 
-//$config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/partner.-13.ini';
-//passthru("php $script $config");
+$config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/partner.-13.ini';
+passthru("php $script $config");
 
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/service.appToken.ini';
 passthru("php $script $config");
