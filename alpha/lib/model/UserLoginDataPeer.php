4ff46ccbd04572395f9b0712b61c59c546e197c6
danielbarak <daniel.barak@kaltura.com>
2021-12-16 12:34:40 +0200

    better option, thanks to Eran K's help

--- alpha/lib/model/UserLoginDataPeer.php
+++ alpha/lib/model/UserLoginDataPeer.php
@@ -255,17 +255,15 @@ class UserLoginDataPeer extends BaseUserLoginDataPeer implements IRelatedObjectP
 			throw new kUserException('', kUserException::LOGIN_DATA_NOT_FOUND);
 		}
 		$partnerId = $loginData->getConfigPartnerId();
-		if ($partnerId <> 0)
+		$roleNames = null;
+		if ($partnerId)
 		{
-			myPartnerUtils::resetPartnerFilter('kuser');
+			kuserPeer::setUseCriteriaFilter(false);
+			$user = kuserPeer::getByLoginDataAndPartner($loginData->getId(), $partnerId);
+			kuserPeer::setUseCriteriaFilter(true);
+			$roleNames = ($user) ? $user->getUserRoleNames() : null;
 		}
 		$partner = PartnerPeer::retrieveByPK($partnerId);
-		$user = kuserPeer::getKuserByEmail($email, $partnerId);
-		$roleNames = null;
-		if ($user)
-		{
-			$roleNames = $user->getUserRoleNames();
-		}
 		$dynamicTemplateUserRoleName = kEmails::getDynamicEmailUserRoleName($roleNames);
 		// If on the partner it's set not to reset the password - skip the email sending
 		if($partner->getEnabledService(PermissionName::FEATURE_DISABLE_RESET_PASSWORD_EMAIL)) {
