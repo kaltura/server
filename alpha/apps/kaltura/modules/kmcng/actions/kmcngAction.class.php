37ca0ea99bd3c46123af7d487a0453c43d14c2a4
drorsouhami <dror.souhami@kaltura.com>
2022-01-16 10:58:36 +0200

    Replace nonce inside html content

--- alpha/apps/kaltura/modules/kmcng/actions/kmcngAction.class.php
+++ alpha/apps/kaltura/modules/kmcng/actions/kmcngAction.class.php
@@ -66,14 +66,14 @@ class kmcngAction extends kalturaAction
 		$config = str_replace("\\/", '/', $config);
 		
 		$randNum = strval(rand(11111111, 99999999));
-		$config = str_replace("@NONCE_TOKEN@", $randNum, $config);
 		
 		if (isset($kmcngParams['kmcng_content_security_policy']))
 		{
-			$kmcngContentSecurityPolicy = str_replace("@NONCE_TOKEN@", $randNum, $kmcngParams['kmcng_content_security_policy']);
+			$kmcngContentSecurityPolicy = str_replace("%NONCE_TOKEN%", $randNum, $kmcngParams['kmcng_content_security_policy']);
 			header("Content-Security-Policy: " . $kmcngContentSecurityPolicy);
 		}
-
+		
+		$content = str_replace("%NONCE_TOKEN%", $randNum, $content);
 		$content = str_replace("<base href=\"/\">", "<base href=\"/index.php/kmcng/\">", $content);
 		$content = preg_replace("/src=\"(?!(http:)|(https:)|\/)/i", "src=\"{$deployUrl}", $content);
 		$content = preg_replace("/href=\"(?!(http:)|(https:)|\/)/i", "href=\"{$deployUrl}", $content);
