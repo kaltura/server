ec765e3e0d112b6d4c7e68212b4513d044cd265c
MosheMaorKaltura <moshe.maor@kaltura.com>
2022-03-06 09:25:48 +0200

    PLAT-23626: Block partner registration selfserved from list of countries

--- api_v3/services/PartnerService.php
+++ api_v3/services/PartnerService.php
@@ -109,7 +109,7 @@ class PartnerService extends KalturaBaseService
 		$blockedCountriesList = kConf::getArrayValue("blockedRegistration",partner::GLOBAL_ACCESS_LIMITATIONS, kConfMapNames::RUNTIME_CONFIG, "");
 		if(!myPartnerUtils::isRequestFromAllowedCountry($blockedCountriesList, null) && $partner->isSelfServe)
 		{
-			throw new KalturaAPIException( APIErrors::PARTNER_REGISTRATION_ERROR, "Action is blocked temporary for the current country");
+			throw new KalturaAPIException( APIErrors::PARTNER_REGISTRATION_ERROR, "Action is temporary blocked from this country");
 		}
 
 		try
