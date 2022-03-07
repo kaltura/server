987ea9fc1721b539898e1f9f13d14f08d90c59d6
Nadav Harnik <nadav.harnik@kaltura.com>
2022-03-07 11:17:52 +0200

    small fix

--- api_v3/web/analyticsSyncServe.php
+++ api_v3/web/analyticsSyncServe.php
@@ -167,7 +167,7 @@ function getUserUpdates($updatedAt)
 	foreach ($rows as $row)
 	{
 		$status = $row['STATUS'];
-		if (!in_array($status , array(KuserStatus::ACTIVE, KuserStatus::BLOCKED)))
+		if ($status == KuserStatus::DELETED)
 		{
 			continue;
 		}
