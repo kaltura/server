d40dc128c8b590df62fa014475c919e01b0c9094
drorsouhami <dror.souhami@kaltura.com>
2022-03-08 12:02:21 +0200

    Allow multiple hosts

--- plugins/game/GamePlugin.php
+++ plugins/game/GamePlugin.php
@@ -63,8 +63,17 @@ class GamePlugin extends KalturaPlugin implements IKalturaServices
 			return null;
 		}
 		
+		if (isset($redisConfig['scheme']) && $redisConfig['scheme'])
+		{
+			$scheme = array('verify_peer' => true);
+		}
+		else
+		{
+			$scheme = array('verify_peer' => false);
+		}
+		
 		$config = array('host' => $redisConfig['host'], 'port' => $redisConfig['port'], 'timeout' => floatval($redisConfig['timeout']),
-			'cluster' => $redisConfig['cluster'], 'persistent' => $redisConfig['persistent'], 'password' => $redisConfig['password']);
+			'cluster' => $redisConfig['cluster'], 'persistent' => $redisConfig['persistent'], 'password' => $redisConfig['password'], 'scheme' => $scheme);
 		
 		$redisWrapper->init($config);
 		return $redisWrapper;
