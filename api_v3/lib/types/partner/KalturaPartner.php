204e0cff800828034f67f7602feceab4c83443eb
danielbarak <daniel.barak@kaltura.com>
2022-03-24 12:42:56 +0200

    PLAT-23655: Allowed Domain and Excluded Admin Role Are Not Copied From Template To New Partner

--- api_v3/lib/types/partner/KalturaPartner.php
+++ api_v3/lib/types/partner/KalturaPartner.php
@@ -457,6 +457,12 @@ class KalturaPartner extends KalturaObject implements IFilterable
 	 */
 	public $allowedDomains;
 	
+	/**
+	 * @var string
+	 * @readonly
+	 */
+	public $excludedAdminRoleName;
+	
 	private static $map_between_objects = array
 	(
 		'id' , 'name', 'website' => 'url1' , 'notificationUrl' => 'url2' , 'appearInSearch' , 'createdAt' , 'adminName' , 'adminEmail' , 'blockDirectLogin',
@@ -468,7 +474,8 @@ class KalturaPartner extends KalturaObject implements IFilterable
 		'host', 'cdnHost', 'isFirstLogin', 'logoutUrl', 'partnerParentId','crmId', 'referenceId', 'timeAlignedRenditions','eSearchLanguages',
 		'publisherEnvironmentType', 'ovpEnvironmentUrl', 'ottEnvironmentUrl', 'authenticationType', 'extendedFreeTrailExpiryReason', 'extendedFreeTrailExpiryDate',
 		'extendedFreeTrail', 'extendedFreeTrailEndsWarning', 'eightyPercentWarning', 'usageLimitWarning', 'lastFreeTrialNotificationDay','monitorUsage', 'additionalParams',
-		'passwordStructureValidations', 'passReplaceFreq', 'maxLoginAttempts', 'loginBlockPeriod', 'numPrevPassToKeep', 'twoFactorAuthenticationMode', 'isSelfServe','allowedDomains'
+		'passwordStructureValidations', 'passReplaceFreq', 'maxLoginAttempts', 'loginBlockPeriod', 'numPrevPassToKeep', 'twoFactorAuthenticationMode', 'isSelfServe','allowedDomains',
+		'excludedAdminRoleName'
 	);
 	
 	public function getMapBetweenObjects ( )
