1d461f54e4fac5c9115abd2bc76cc814d4b3542a
yossipapi <yossi.papiashvili@kaltura.com>
2022-03-28 21:43:49 +0300

    No-Plat: Fix indent

--- vendor/ZendFramework/library/Zend/View/Helper/HeadLink.php
+++ vendor/ZendFramework/library/Zend/View/Helper/HeadLink.php
@@ -341,7 +341,7 @@ class Zend_View_Helper_HeadLink extends Zend_View_Helper_Placeholder_Container_S
         $media                 = 'screen';
         $conditionalStylesheet = false;
         $href                  = array_shift($args);
-	    $extras                = array();
+        $extras                = array();
 
         if ($this->_isDuplicateStylesheet($href)) {
             return false;
@@ -408,7 +408,7 @@ class Zend_View_Helper_HeadLink extends Zend_View_Helper_Placeholder_Container_S
         $href  = array_shift($args);
         $type  = array_shift($args);
         $title = array_shift($args);
-	    $extras                = array();
+        $extras = array();
 
         if(0 < count($args) && is_array($args[0])) {
             $extras = array_shift($args);
