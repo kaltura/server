f6732e2c488b8e845b6a81c26153aa60c0309686
Salma Nassar <salma.nassar@kaltura.com>
2022-04-11 18:01:24 +0300

    SUP-30803 add duration limitation on scheduled event update to make it consistent with insert/add

--- plugins/schedule/base/lib/api/KalturaScheduleEvent.php
+++ plugins/schedule/base/lib/api/KalturaScheduleEvent.php
@@ -459,6 +459,8 @@ abstract class KalturaScheduleEvent extends KalturaObject implements IRelatedFil
 			$sourceObject->incrementSequence();
 		}
 
+		$maxSingleEventDuration = $this->getSingleScheduleEventMaxDuration();
+
 		if (!$this->isNull('duration'))
 		{
 			if (!$this->isNull('endDate'))
@@ -479,8 +481,6 @@ abstract class KalturaScheduleEvent extends KalturaObject implements IRelatedFil
 				$this->duration = null;
 			}
 
-			$maxSingleEventDuration = SchedulePlugin::getSingleScheduleEventMaxDuration();
-
 			// validate single event duration in recurring event or in single event is 24 hours at most
 			if ($this->recurrenceType == KalturaScheduleEventRecurrenceType::RECURRING)
 			{
@@ -499,7 +499,6 @@ abstract class KalturaScheduleEvent extends KalturaObject implements IRelatedFil
 		}
 		else
 		{
-			$maxSingleEventDuration = $this->getSingleScheduleEventMaxDuration();
 			if ($endDate - $startDate > $maxSingleEventDuration)
 			{
 				throw new KalturaAPIException(KalturaScheduleErrors::MAX_SCHEDULE_DURATION_REACHED, $maxSingleEventDuration);
