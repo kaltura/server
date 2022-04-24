e26fbdaf7a57f65996d60c7ed2dc4f0d493cc2b6
Orel Ariel <orel.ariel@kaltura.com>
2022-04-24 18:34:40 +0300

    SUP-28743: Add child entry search option for MR

---
+++ alpha/lib/enums/EntryDisplayChildInSearchType.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package Core
+ * @subpackage model.enum
+ */
+interface EntryDisplayChildInSearchType extends BaseEnum
+{
+    const INCLUDE_CHILD = -1;
+    const PARTNER_ONLY = 1; // Not needed
+}
