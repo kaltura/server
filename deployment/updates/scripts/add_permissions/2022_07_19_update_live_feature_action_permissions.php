3323e545becaaa779654e9b7fd5a342f7baf5026
Nadav Ravid <nadav.ravid@Kaltura.com>
2022-07-21 10:23:57 +0300

    update script description

--- deployment/updates/scripts/add_permissions/2022_07_19_update_live_feature_action_permissions.php
+++ deployment/updates/scripts/add_permissions/2022_07_19_update_live_feature_action_permissions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package deployment
- * Add permission REACH_VENDOR_PARTNER to new prop externalTaskId (insert and update)
+ * Add permissions for updateLiveFeature action
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
