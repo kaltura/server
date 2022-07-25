3c824f5f0ca197e5253a4dbde2d610ab5518173a
David Winder <david.winder@kaltura.com>
2022-07-25 20:28:54 +0300

    Add script for userRole.ini

--- deployment/updates/scripts/add_permissions/2022_07_19_update_live_feature_action_permissions.php
+++ deployment/updates/scripts/add_permissions/2022_07_19_update_live_feature_action_permissions.php
@@ -4,10 +4,14 @@
  * Add permissions for updateLiveFeature action
  */
 
-$script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+$addPermissionsAndItemsScript = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
+$insertDefaultsScript = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/insertDefaults.php';
 
 $config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/partner.0.ini';
-passthru("php $script $config");
+passthru("php $addPermissionsAndItemsScript $config");
+
+$config = realpath(dirname(__FILE__)) . '/03.UserRole.ini';
+passthru("php $insertDefaultsScript $config");
 
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/service.schedule.scheduleEvent.ini';
-passthru("php $script $config");
+passthru("php $addPermissionsAndItemsScript $config");
