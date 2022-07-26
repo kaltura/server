2d64e8e1652e7537900de058b7762f1f6933e803
David Winder <david.winder@kaltura.com>
2022-07-26 15:05:05 +0300

    use the base ini userRole

--- deployment/updates/scripts/add_permissions/2022_07_19_update_live_feature_action_permissions.php
+++ deployment/updates/scripts/add_permissions/2022_07_19_update_live_feature_action_permissions.php
@@ -10,7 +10,7 @@ $insertDefaultsScript = realpath(dirname(__FILE__) . '/../../../') . '/base/scri
 $config = realpath(dirname(__FILE__) . '/../../../') . '/permissions/partner.0.ini';
 passthru("php $addPermissionsAndItemsScript $config");
 
-$config = realpath(dirname(__FILE__)) . '/03.UserRole.ini';
+$config = realpath(dirname(__FILE__) . '/../../../') . '/base/scripts/init_data/03.UserRole.ini';
 passthru("php $insertDefaultsScript $config");
 
 $config = realpath(dirname(__FILE__)) . '/../../../permissions/service.schedule.scheduleEvent.ini';
