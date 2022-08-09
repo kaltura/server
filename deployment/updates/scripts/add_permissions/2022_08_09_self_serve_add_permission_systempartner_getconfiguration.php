6c199d7cf03fcbd8308ab070daae2ff9bb7e1206
ravitshalem <ravit.shalem@kaltura.com>
2022-08-09 16:27:20 +0300

    Rigel-18.12.0-SSRV-710:Add systemPartner getConfiguration permission for self serve partner

--- deployment/updates/scripts/add_permissions/2022_08_09_self_serve_add_permission_systempartner_getconfiguration.php
+++ deployment/updates/scripts/add_permissions/2022_08_09_self_serve_add_permission_systempartner_getconfiguration.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package deployment
- * @subpackage quasar.roles_and_permissions
+ * @subpackage rigel.roles_and_permissions
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionsAndItems.php';
