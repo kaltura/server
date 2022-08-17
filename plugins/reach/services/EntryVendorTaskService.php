66b6849b9b7bcdf15b9246138a39fd00a7f248ea
Nadav Ravid <nadav.ravid@Kaltura.com>
2022-08-17 11:35:31 +0300

    PLAT-23850: move catalog validation into validateForInsert moved check if catalog item exists to avoid edge case exception in validateForInsert when catalog item does not exist

--- plugins/reach/services/EntryVendorTaskService.php
+++ plugins/reach/services/EntryVendorTaskService.php
@@ -48,10 +48,6 @@ class EntryVendorTaskService extends KalturaBaseService
 		if (!$dbReachProfile)
 			throw new KalturaAPIException(KalturaReachErrors::REACH_PROFILE_NOT_FOUND, $entryVendorTask->reachProfileId);
 		
-		$dbVendorCatalogItem = VendorCatalogItemPeer::retrieveByPK($entryVendorTask->catalogItemId);
-		if (!$dbVendorCatalogItem)
-			throw new KalturaAPIException(KalturaReachErrors::CATALOG_ITEM_NOT_FOUND, $entryVendorTask->catalogItemId);
-		
 		$partnerCatalogItem = PartnerCatalogItemPeer::retrieveByCatalogItemId($entryVendorTask->catalogItemId, kCurrentContext::getCurrentPartnerId());
 		if (!$partnerCatalogItem)
 			throw new KalturaAPIException(KalturaReachErrors::CATALOG_ITEM_NOT_ENABLED_FOR_ACCOUNT, $entryVendorTask->catalogItemId);
