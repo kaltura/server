b3fce04bef8dabf9d6f3976201767c32c5e0b475
drorsouhami <dror.souhami@kaltura.com>
2022-08-22 11:30:00 +0300

    PLAT-23852: Change enum names

--- api_v3/lib/types/filters/orderEnums/KalturaUiConfOrderBy.php
+++ api_v3/lib/types/filters/orderEnums/KalturaUiConfOrderBy.php
@@ -9,8 +9,8 @@ class KalturaUiConfOrderBy extends KalturaStringEnum
 	const CREATED_AT_DESC = "-createdAt";
 	const UPDATED_AT_ASC = "+updatedAt";
 	const UPDATED_AT_DESC = "-updatedAt";
-	const ID_AT_ASC = "+id";
-	const ID_AT_DESC = "-id";
-	const NAME_AT_ASC = "+name";
-	const NAME_AT_DESC = "-name";
+	const ID_ASC = "+id";
+	const ID_DESC = "-id";
+	const NAME_ASC = "+name";
+	const NAME_DESC = "-name";
 }
