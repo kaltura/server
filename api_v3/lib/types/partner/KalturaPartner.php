afc242fe0471c3651292d087eabb24b023ec8d43
danielbarak <daniel.barak@kaltura.com>
2022-10-27 13:46:14 +0300

    PLAT-23939 - Return the vertical on partner.get

--- api_v3/lib/types/partner/KalturaPartner.php
+++ api_v3/lib/types/partner/KalturaPartner.php
@@ -470,10 +470,10 @@ class KalturaPartner extends KalturaObject implements IFilterable
 	public $eventPlatformAllowedTemplates;
 	
 	/**
-	 * @var string
+	 * @var int
 	 * @readonly
 	 */
-	public $verticalClassificationName;
+	public $verticalClassificationId;
 	
 	private static $map_between_objects = array
 	(
@@ -487,7 +487,7 @@ class KalturaPartner extends KalturaObject implements IFilterable
 		'publisherEnvironmentType', 'ovpEnvironmentUrl', 'ottEnvironmentUrl', 'authenticationType', 'extendedFreeTrailExpiryReason', 'extendedFreeTrailExpiryDate',
 		'extendedFreeTrail', 'extendedFreeTrailEndsWarning', 'eightyPercentWarning', 'usageLimitWarning', 'lastFreeTrialNotificationDay','monitorUsage', 'additionalParams',
 		'passwordStructureValidations', 'passReplaceFreq', 'maxLoginAttempts', 'loginBlockPeriod', 'numPrevPassToKeep', 'twoFactorAuthenticationMode', 'isSelfServe', 'allowedDomains',
-		'excludedAdminRoleName', 'eventPlatformAllowedTemplates', 'verticalClassificationName'
+		'excludedAdminRoleName', 'eventPlatformAllowedTemplates', 'verticalClassificationId'
 	);
 	
 	public function getMapBetweenObjects ( )
