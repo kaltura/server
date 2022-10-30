1a9fc896c6f46c44a1188a052bff17cfca3ca783
danielbarak <daniel.barak@kaltura.com>
2022-10-30 09:57:32 +0200

    PLAT-23909

--- api_v3/lib/types/partner/KalturaPartner.php
+++ api_v3/lib/types/partner/KalturaPartner.php
@@ -479,7 +479,7 @@ class KalturaPartner extends KalturaObject implements IFilterable
 	 * @var int
 	 * @readonly
 	 */
-	public $verticalClassificationId;
+	public $verticalClassification;
 	
 	private static $map_between_objects = array
 	(
@@ -493,7 +493,7 @@ class KalturaPartner extends KalturaObject implements IFilterable
 		'publisherEnvironmentType', 'ovpEnvironmentUrl', 'ottEnvironmentUrl', 'authenticationType', 'extendedFreeTrailExpiryReason', 'extendedFreeTrailExpiryDate',
 		'extendedFreeTrail', 'extendedFreeTrailEndsWarning', 'eightyPercentWarning', 'usageLimitWarning', 'lastFreeTrialNotificationDay','monitorUsage', 'additionalParams',
 		'passwordStructureValidations', 'passReplaceFreq', 'maxLoginAttempts', 'loginBlockPeriod', 'numPrevPassToKeep', 'twoFactorAuthenticationMode', 'isSelfServe', 'allowedDomains',
-		'excludedAdminRoleName', 'eventPlatformAllowedTemplates', 'verticalClassificationId', 'allowDefaultPasswordRestrictions'
+		'excludedAdminRoleName', 'eventPlatformAllowedTemplates', 'verticalClassification', 'allowDefaultPasswordRestrictions'
 
 	);
 	
@@ -519,7 +519,7 @@ class KalturaPartner extends KalturaObject implements IFilterable
 		$this->adminName = kString::stripUtf8InvalidChars($this->adminName);
 		$this->describeYourself = kString::stripUtf8InvalidChars($this->describeYourself);
 		$this->additionalParams = KalturaKeyValueArray::fromKeyValueArray($partner->getAdditionalParams());
-		$this->verticalClassificationName = $partner->getVerticalClasiffication();
+		$this->verticalClassification = $partner->getVerticalClasiffication();
 		$this->allowDefaultPasswordRestrictions = !$partner->getEnabledService(PermissionName::FEATURE_DISABLE_PASSWORD_RESTRICTION);
 		if (!$this->host){
 			$this->host = null;
