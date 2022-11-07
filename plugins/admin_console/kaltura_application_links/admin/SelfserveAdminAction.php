e8718ac865aa646f9285bfbb2b7af84f975318f4
MosheMaorKaltura <moshe.maor@kaltura.com>
2022-11-07 12:04:23 +0200

    PLAT-23957: code review remarks

--- plugins/admin_console/kaltura_application_links/admin/SelfserveAdminAction.php
+++ plugins/admin_console/kaltura_application_links/admin/SelfserveAdminAction.php
@@ -10,13 +10,9 @@ class SelfserveAdminAction extends KalturaApplicationPlugin
 
     public function doAction(Zend_Controller_Action $action)
     {
-
-        //try login using KS
         $ks = Infra_ClientHelper::getKs();
-        //$appUrl = kConf::getArrayValue("appUrl" , "selfserve", "application_links");
         $appUrl =  Zend_Registry::get('config')->applicationLinks->selfserve->appUrl;
         $loginUrl = Zend_Registry::get('config')->applicationLinks->selfserve->loginUrl;
-        $url = $loginUrl.$ks;
 
         //load the application page
         $action->view->appUrl = $appUrl;
