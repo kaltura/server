abdb0ad1d4669482a06faed3b708c4ee612d40f5
Moshe Maor <moshe.maor@kaltura.com>
2022-11-07 18:14:58 +0200

    PLAT-23957:add support of external links in admin console  (#11872)
    * PLAT-23957: adding support in external links from admin console
    
    * PLAT-23957: Add external kaltura application from admin console
    
    * PLAT-23957:External links
    
    * PLAT-23957: code review remarks
    
    * PLAT-23957: code review remarks
    
    * Update SelfserveAdminAction.php
    
    * PLAT-23957: code review remarks

---
+++ plugins/admin_console/kaltura_application_links/KalturaApplicationLinksPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+class KalturaApplicationLinksPlugin extends KalturaPlugin implements IKalturaApplicationPartialView, IKalturaAdminConsolePages
+{
+    const PLUGIN_NAME = 'KalturaApplicationLinks';
+    const ROOT_LABEL = "App Links";
+
+    /* (non-PHPdoc)
+     * @see IKalturaPlugin::getPluginName()
+     */
+    public static function getPluginName()
+    {
+        return self::PLUGIN_NAME;
+    }
+    /* (non-PHPdoc)
+     * @see IKalturaApplicationPartialView::getApplicationPartialViews()
+     */
+    public static function getApplicationPartialViews($controller, $action)
+    {
+        return array();
+    }
+
+    /* (non-PHPdoc)
+     * @see IKalturaAdminConsolePages::getApplicationPages()
+     */
+    public static function getApplicationPages()
+    {
+        //todo take list of items from config
+        $links = array(
+            new SelfserveAdminAction(self::ROOT_LABEL),
+        );
+        return $links;
+    }
+}
