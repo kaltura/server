39250d24c5746447fd2ab3e8332cb39c08542c41
bw-kaltura <bryna.weissman@kaltura.com>
2022-12-05 11:04:28 +0200

    Rigel-18.6.0-SUP-31043

--- alpha/scripts/utils/copyConversionProfile.php
+++ alpha/scripts/utils/copyConversionProfile.php
@@ -3,9 +3,9 @@ require_once (dirname(__FILE__) . '/../bootstrap.php');
 
 if ($argc < 3)
 {
-	die (' Error execute script, Usage: php copyConversionProfile.php
+    die('Error execute script, Usage: php copyConversionProfile.php
 	 < conversionProfileId >
-	 < partnerId >
+	 < destPartnerId >
 	 < realrun / dryrun >' . PHP_EOL);
 }
 
@@ -19,57 +19,67 @@ KalturaStatement::setDryRun($dryRun);
 KalturaLog::info($dryRun ? 'DRY RUN' : 'REAL RUN');
 
 $conversionProfileId = $argv[1];
-$partnerId = $argv[2];
+$destPartnerId = $argv[2];
 
 $conversionProfile = conversionProfile2Peer::retrieveByPKAndPartnerId($conversionProfileId, 0);
 
-if (!is_null($conversionProfile) && $conversionProfile->getStatus() == ConversionProfileStatus::ENABLED)
+if (is_null($conversionProfile) || $conversionProfile->getStatus() == ConversionProfileStatus::DISABLED)
 {
-	KalturaLog::debug("Conversion profile id {$conversionProfileId} found on partner 0");
-
-	try
-	{
-		$newConversionProfile = new conversionProfile2();
-		$newConversionProfile->setPartnerId($partnerId);
-		$newConversionProfile->setSystemName($conversionProfile->getSystemName());
-		$newConversionProfile->setName($conversionProfile->getName());
-		$newConversionProfile->setDescription($conversionProfile->getDescription());
-		$newConversionProfile->setConditionalProfiles($conversionProfile->getConditionalProfiles());
-		$newConversionProfile->setStatus($conversionProfile->getStatus());
-		$newConversionProfile->setType($conversionProfile->getType());
-
-		$newConversionProfile->save();
-
-		try
-		{
-			$listOfFlavorParamsConversionProfile = $conversionProfile->getflavorParamsConversionProfiles();
-			foreach ($listOfFlavorParamsConversionProfile as $flavorParamsConversionProfile)
-			{
-				/** @var $flavorParamsConversionProfile flavorParamsConversionProfile */
-
-				$newFlavorParam = new flavorParamsConversionProfile();
-				$newFlavorParam = $flavorParamsConversionProfile->copy();
-				$newFlavorParam->setConversionProfileId($newConversionProfile->getId());
-				$newFlavorParam->setCreatedAt(null);
-				$newFlavorParam->setUpdatedAt(null);
-
-				$newFlavorParam->save();
-			}
-		}
-		catch (Exception $exception)
-		{
-			KalturaLog::debug("Count not copy flavor params from conversion profile {$conversionProfile->getId()}");
-			KalturaLog::debug($exception->getMessage());
-		}
-	}
-	catch (Exception $exception)
-	{
-		KalturaLog::debug("Could not create the conversion profile for Pid {$partnerId}");
-		KalturaLog::debug($exception->getMessage());
-	}
+    die("Conversion profile id {$conversionProfileId} could not be found or is disabled on partner 0");
 }
-else
+
+KalturaLog::debug("Conversion profile id {$conversionProfileId} found on partner 0");
+
+try
 {
-	KalturaLog::debug("Conversion profile id {$conversionProfileId} could not be found or is not enable on partner 0");
+    $newConversionProfile = copyConversionProfile($conversionProfile, $destPartnerId);
+
+    try
+    {
+        $listOfFlavorParamsConversionProfile = $conversionProfile->getflavorParamsConversionProfiles();
+        foreach ($listOfFlavorParamsConversionProfile as $flavorParamsConversionProfile)
+        {
+            /** @var $flavorParamsConversionProfile flavorParamsConversionProfile */
+            $newFlavorParam = copyFlavorParam($flavorParamsConversionProfile, $newConversionProfile->getId());
+        }
+    }
+    catch (Exception $exception)
+    {
+        KalturaLog::debug("Could not copy flavor params {$flavorParamsConversionProfile->getId()} from conversion profile {$conversionProfile->getId()}");
+        KalturaLog::debug($exception->getMessage());
+    }
 }
+catch (Exception $exception)
+{
+    KalturaLog::debug("Could not create the conversion profile for Pid {$destPartnerId}");
+    KalturaLog::debug($exception->getMessage());
+}
+
 KalturaLog::info("Script Done!");
+
+function copyConversionProfile($conversionProfile, $destPartnerId)
+{
+    $newConversionProfile = new conversionProfile2();
+    $newConversionProfile->setPartnerId($destPartnerId);
+    $newConversionProfile->setSystemName($conversionProfile->getSystemName());
+    $newConversionProfile->setName($conversionProfile->getName());
+    $newConversionProfile->setDescription($conversionProfile->getDescription());
+    $newConversionProfile->setConditionalProfiles($conversionProfile->getConditionalProfiles());
+    $newConversionProfile->setStatus($conversionProfile->getStatus());
+    $newConversionProfile->setType($conversionProfile->getType());
+
+    $newConversionProfile->save();
+    return $newConversionProfile;
+}
+
+function copyFlavorParam($flavorParamsConversionProfile, $newConversionProfileId )
+{
+    /** @var $flavorParamsConversionProfile flavorParamsConversionProfile */
+    $newFlavorParam = new flavorParamsConversionProfile();
+    $newFlavorParam = $flavorParamsConversionProfile->copy();
+    $newFlavorParam->setConversionProfileId($newConversionProfileId);
+    $newFlavorParam->setCreatedAt(null);
+    $newFlavorParam->setUpdatedAt(null);
+
+    $newFlavorParam->save();
+}
