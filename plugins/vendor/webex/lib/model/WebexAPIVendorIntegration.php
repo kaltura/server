9f5d2339e580970042e0d261ab3f2d71070c6913
drorsouhami <dror.souhami@kaltura.com>
2022-12-11 11:17:14 +0200

    PLAT-23887: Add groupParticipation fields to KalturaWebexAPIIntegrationSetting

--- plugins/vendor/webex/lib/model/WebexAPIVendorIntegration.php
+++ plugins/vendor/webex/lib/model/WebexAPIVendorIntegration.php
@@ -10,13 +10,16 @@ class WebexAPIVendorIntegration extends VendorIntegration
 	const USER_MATCHING_MODE = 'userMatchingMode';
 	const USER_POSTFIX = 'UserPostfix';
 	const WEBEX_ACCOUNT_DESCRIPTION = 'webexAccountDescription';
+	const OPT_OUT_GROUP_NAMES = 'optOutGroupNames';
+	const OPT_IN_GROUP_NAMES = 'optInGroupNames';
+	const GROUP_PARTICIPATION_TYPE = 'groupParticipationType';
 
 	public function setWebexCategory($v)	{ $this->putInCustomData ( self::WEBEX_CATEGORY, $v); }
 	public function getWebexCategory( )	{ return $this->getFromCustomData(self::WEBEX_CATEGORY); }
 	public function unsetCategory( ) 	{ return $this->removeFromCustomData(self::WEBEX_CATEGORY); }
 	
 	public function setUserMatchingMode($v)	{ $this->putInCustomData ( self::USER_MATCHING_MODE, $v); }
-	public function getUserMatchingMode( )	{ return $this->getFromCustomData ( self::USER_MATCHING_MODE,null, kZoomUsersMatching::DO_NOT_MODIFY); }
+	public function getUserMatchingMode( )	{ return $this->getFromCustomData ( self::USER_MATCHING_MODE,null, kWebexAPIUsersMatching::DO_NOT_MODIFY); }
 	
 	public function setUserPostfix($v)	{ $this->putInCustomData ( self::USER_POSTFIX, $v); }
 	public function getUserPostfix( )	{ return $this->getFromCustomData(self::USER_POSTFIX); }
@@ -27,6 +30,62 @@ class WebexAPIVendorIntegration extends VendorIntegration
 	public function setWebexAccountDescription ($v)	{ $this->putInCustomData ( self::WEBEX_ACCOUNT_DESCRIPTION, $v);	}
 	public function getWebexAccountDescription ( )	{ return $this->getFromCustomData(self::WEBEX_ACCOUNT_DESCRIPTION);	}
 	
+	public function getOptOutGroupNames()
+	{
+		return $this->getFromCustomData(self::OPT_OUT_GROUP_NAMES);
+	}
+	
+	public function getOptInGroupNames()
+	{
+		return $this->getFromCustomData(self::OPT_IN_GROUP_NAMES);
+	}
+	
+	public function setOptOutGroupNames($v)
+	{
+		if ($this->getGroupParticipationType() == kWebexAPIGroupParticipationType::OPT_OUT)
+		{
+			$this->putInCustomData(self::OPT_OUT_GROUP_NAMES, $v);
+		}
+	}
+	
+	public function addGroupNameToOptOutGroups($v)
+	{
+		if ($this->getGroupParticipationType() == kWebexAPIGroupParticipationType::OPT_OUT)
+		{
+			$this->putInCustomData(self::OPT_OUT_GROUP_NAMES, $this->getOptOutGroupNames() . "\r\n" . $v);
+		}
+	}
+	
+	public function setOptInGroupNames($v)
+	{
+		if ($this->getGroupParticipationType() == kWebexAPIGroupParticipationType::OPT_IN)
+		{
+			$this->putInCustomData(self::OPT_IN_GROUP_NAMES, $v);
+		}
+	}
+	
+	public function addGroupNameToOptInGroups($v)
+	{
+		if ($this->getGroupParticipationType() == kWebexAPIGroupParticipationType::OPT_IN)
+		{
+			$this->putInCustomData(self::OPT_IN_GROUP_NAMES, $this->getOptInGroupNames() . "\r\n" . $v);
+		}
+	}
+	
+	public function setGroupParticipationType($v)
+	{
+		if ($v != $this->getGroupParticipationType())
+		{
+			$this->putInCustomData(self::GROUP_PARTICIPATION_TYPE, $v);
+			$this->putInCustomData(self::OPT_IN_GROUP_NAMES, '');
+			$this->putInCustomData(self::OPT_OUT_GROUP_NAMES, '');
+		}
+	}
+	
+	public function getGroupParticipationType()
+	{
+		return $this->getFromCustomData(self::GROUP_PARTICIPATION_TYPE, null, kWebexAPIGroupParticipationType::NO_CLASSIFICATION);
+	}
 
 	public function setTokensData($tokensDataAsArray)
 	{
---
+++ plugins/vendor/webex/lib/model/enums/kWebexAPIGroupParticipationType.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package plugins.WebexAPIDropFolder
+ * @subpackage model.enum
+ */
+interface kWebexAPIGroupParticipationType extends BaseEnum
+{
+	const NO_CLASSIFICATION = 0;
+	const OPT_IN = 1;
+	const OPT_OUT = 2;
+}
