d24329e4ee391c03026639866034029cdc028978
drorsouhami <dror.souhami@kaltura.com>
2023-02-27 14:53:09 +0200

    PLAT-24131: Entry ESearch using displayInSearch recycled

--- plugins/search/providers/elastic_search/lib/model/search/ESearchEntryQueryFilterAttributes.php
+++ plugins/search/providers/elastic_search/lib/model/search/ESearchEntryQueryFilterAttributes.php
@@ -7,26 +7,30 @@ class ESearchEntryQueryFilterAttributes extends ESearchBaseQueryFilterAttributes
 {
 	public function getDisplayInSearchFilter()
 	{
-		$displayInSearchQuery = new kESearchTermQuery(ESearchEntryFieldName::DISPLAY_IN_SEARCH, EntryDisplayInSearchType::SYSTEM);
-		$mustNotDisplayInSearchBoolQuery = new kESearchBoolQuery();
-		$mustNotDisplayInSearchBoolQuery->addToMustNot($displayInSearchQuery);
-
+		$displayInSearchBoolQuery = new kESearchBoolQuery();
+		
+		$mustNotDisplayInSearchTypes = array(EntryDisplayInSearchType::RECYCLED, EntryDisplayInSearchType::SYSTEM);
+		foreach ($mustNotDisplayInSearchTypes as $type)
+		{
+			$displayInSearchQuery = new kESearchTermQuery(ESearchEntryFieldName::DISPLAY_IN_SEARCH, $type);
+			$mustNotDisplayInSearchBoolQuery = new kESearchBoolQuery();
+			$mustNotDisplayInSearchBoolQuery->addToMustNot($displayInSearchQuery);
+			$displayInSearchBoolQuery->addToShould($mustNotDisplayInSearchBoolQuery);
+		}
+		
 		$ignoreDisplayInSearchQueries = array();
-
 		foreach	($this->ignoreDisplayInSearchValues as $key => $value)
 		{
-			if($value)
+			if ($value)
+			{
 				$ignoreDisplayInSearchQueries[] = new kESearchTermsQuery($key, $value);
+			}
 		}
-
-		if(count($ignoreDisplayInSearchQueries))
+		if (count($ignoreDisplayInSearchQueries))
 		{
-			$displayInSearchBoolQuery = new kESearchBoolQuery();
 			$displayInSearchBoolQuery->addQueriesToShould($ignoreDisplayInSearchQueries);
-			$displayInSearchBoolQuery->addToShould($mustNotDisplayInSearchBoolQuery);
-			return $displayInSearchBoolQuery;
 		}
 
-		return $mustNotDisplayInSearchBoolQuery;
+		return $displayInSearchBoolQuery;
 	}
 }
