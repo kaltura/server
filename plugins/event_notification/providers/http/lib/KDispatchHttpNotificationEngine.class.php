73b5e93eb892e5e3ac5e4b36c4effb05c66e4eb4
Tal Dubov <50355200+TalDubovKaltura@users.noreply.github.com>
2023-03-15 11:28:55 +0200

    PSVAMB-42269 - Indentation changes

--- plugins/event_notification/providers/http/lib/KDispatchHttpNotificationEngine.class.php
+++ plugins/event_notification/providers/http/lib/KDispatchHttpNotificationEngine.class.php
@@ -88,8 +88,8 @@ class KDispatchHttpNotificationEngine extends KDispatchEventNotificationEngine
 		}
 
 		$curlWrapper = new KCurlWrapper();
-        $curlWrapper->setOpt(CURLOPT_RETURNTRANSFER, 1);
-        $curlWrapper->setOpt(CURLOPT_HEADER, 1);
+        	$curlWrapper->setOpt(CURLOPT_RETURNTRANSFER, 1);
+        	$curlWrapper->setOpt(CURLOPT_HEADER, 1);
 
 		if(count($headers))
 			$curlWrapper->setOpt(CURLOPT_HTTPHEADER, $headers);
@@ -184,16 +184,16 @@ class KDispatchHttpNotificationEngine extends KDispatchEventNotificationEngine
 		$errCode = $curlWrapper->getErrorNumber();
 		$errMessage = $curlWrapper->getError();
 
-        $header_size = curl_getinfo($curlWrapper->ch, CURLINFO_HEADER_SIZE);
-        $headers = substr($results, 0, $header_size);
-        $body = strip_tags(substr($results, $header_size));
+		$header_size = curl_getinfo($curlWrapper->ch, CURLINFO_HEADER_SIZE);
+		$headers = substr($results, 0, $header_size);
+		$body = strip_tags(substr($results, $header_size));
 
 		$curlWrapper->close();
 
-        KalturaLog::info("HTTP Request httpCode [" . $httpCode . "] Results [$results] Headers [$headers] Body [$body]");
+        	KalturaLog::info("HTTP Request httpCode [" . $httpCode . "] Results [$results] Headers [$headers] Body [$body]");
 		if(!$results || $httpCode != 200)
 		{
-            $body = substr($body, 0, 1000);
+            $		body = substr($body, 0, 1000);
 			throw new kTemporaryException("Sending HTTP request failed [$errCode] httpCode [$httpCode]
 			    			    \n url: [$url] \n $errMessage \n Headers:\n [$headers] \n Body:\n [$body]", $httpCode);
 		}
