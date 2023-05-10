12aa83a9d65a394d296926d5c4e78c5454b3d37f
drorsouhami <dror.souhami@kaltura.com>
2023-05-10 12:50:51 +0300

    FOUN-1122: Add index on last_execution

--- deployment/updates/sql/2023_05_10_scheduled_task_profile_engine_type_index.sql
+++ deployment/updates/sql/2023_05_10_scheduled_task_profile_engine_type_index.sql
@@ -1,1 +1,1 @@
-ALTER TABLE scheduled_task_profile ADD INDEX `status_object_filter_engine_type` (`status`,`object_filter_engine_type`);
+ALTER TABLE scheduled_task_profile ADD INDEX `status_object_filter_engine_type_last_execution` (`status`,`object_filter_engine_type`,`last_execution_started_at`);
