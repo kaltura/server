0f4a736227d54284b1668bf7fb0413894e3f21d8
Guy Elkayam <guy.elkayam@kaltura.com>
2023-05-22 17:55:51 +0300

    FOUN-1130 add errors and log

--- alpha/scripts/utils/validateTwigIni.php
+++ alpha/scripts/utils/validateTwigIni.php
@@ -3,9 +3,21 @@ ini_set("memory_limit","256M");
 
 require_once(__DIR__ . '/../bootstrap.php');
 
-$filename = $argv[1];
-$tmpFileName = kConf::get("cache_root_path") . '/twig/' . $filename;
-@unlink($tmpFileName);
-IniUtils::parseIniFile( $filename, true );
+for ($i=1; $i < $argc; $i++) { 
+    $filename = $argv[$i];
 
-print_r(file_get_contents($tmpFileName));
+    KalturaLog::info("Validating [$filename]");
+
+    $tmpFileName = kConf::get("cache_root_path") . '/twig/' . $filename;
+    @unlink($tmpFileName);
+    try {
+        IniUtils::parseIniFile( $filename, true );
+    } catch (\Throwable $th) {
+        KalturaLog::crit("Validation failed on [$filename] with error [$th]");
+        KalturaLog::info('Faulty ini content:' . PHP_EOL . file_get_contents($filename) . PHP_EOL);
+        exit(1);
+    }
+    
+    print_r(file_get_contents($tmpFileName) . PHP_EOL);
+}
+KalturaLog::info('File validation was successful');
