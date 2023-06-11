7e2f60ae8cda0574dbbcf9a83d179a8503e389c9
Guy Elkayam <guy.elkayam@kaltura.com>
2023-06-11 16:21:13 +0300

    VCP-13172 Change kvote entry_rank_index to rank

--- deployment/base/sql/01.kaltura_ce_tables.sql
+++ deployment/base/sql/01.kaltura_ce_tables.sql
@@ -1472,7 +1472,7 @@ CREATE TABLE IF NOT EXISTS `kvote` (
   KEY `kvote_FI_3` (`kuser_id`),
   KEY `entry_user_status_index` (`entry_id`,`kuser_id`,`status`),
   KEY `partner_created_at` (`partner_id`,`created_at`),
-  KEY `entry_rank_index` (`entry_id`,`status`)
+  KEY `entry_rank_index` (`entry_id`,`rank`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
