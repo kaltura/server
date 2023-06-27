f5289f7a76b655b95d11b0ec500bb619b9d3be89
drorsouhami <dror.souhami@kaltura.com>
2023-06-27 20:47:30 +0300

    PLAT-24312: Thumbnail parameter nearest_aspect_ratio does not maintain correct ratio

--- alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
@@ -337,6 +337,15 @@ class thumbnailAction extends sfAction
 				// The required width and height will serve as the final crop values
 				$src_w = $width;
 				$src_h = $height;
+				
+				$exif = exif_read_data($imageFilePath);
+				$orientationsToHandle = array(5, 6, 7, 8);
+				if (isset($exif["Orientation"]) && in_array($exif["Orientation"], $orientationsToHandle))
+				{
+					$tmpWidth = $thumbWidth;
+					$thumbWidth = $thumbHeight;
+					$thumbHeight = $tmpWidth;
+				}
 
 				// Base on the thumbnail's dimensions
 				kThumbnailUtils::scaleDimensions( $thumbWidth, $thumbHeight, $width, $height, kThumbnailUtils::SCALE_UNIFORM_SMALLER_DIM, $width, $height );
