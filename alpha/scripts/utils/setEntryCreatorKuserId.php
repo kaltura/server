365d7c72d31e41a36d20923a41653b6cc9d663ae
hilak <hila.karimov@kaltura.com>
2023-07-10 10:01:44 -0400

    Code review changes

--- alpha/scripts/utils/setEntryCreatorKuserId.php
+++ alpha/scripts/utils/setEntryCreatorKuserId.php
@@ -3,7 +3,7 @@
 if($argc < 2)
 {
 	echo "Arguments missing.\n\n";
-	echo "Usage: php " . __FILE__ . " {mapping} <realrun / dryrun> \n";
+	echo "Usage: php " . __FILE__ . " {mapping} <realrun / dryrun>" . PHP_EOL;
 	exit;
 }
 $mapping = $argv[1];
@@ -18,17 +18,22 @@ $entryMappings = file($mapping, FILE_IGNORE_NEW_LINES);
 $counter = 0;
 foreach ($entryMappings as $entryMapping)
 {
-	list ($entryId,$userId) = explode(",", $entryMapping);
+	list ($entryId, $userId) = explode(",", $entryMapping);
 
-    $entry = entryPeer::retrieveByPK($entryId);
-    $kuser = kuserPeer::getActiveKuserByPartnerAndUid($entry->getPartnerId(), $userId);
-    if(!$entry || !$kuser)
+	$entry = entryPeer::retrieveByPK($entryId);
+	if (!$entry)
 	{
-		echo "Entry id [$entryId] or kuser with puser ID [$userId] not found\n";
+		echo "Entry id [$entryId] not found" . PHP_EOL;
+		continue;
+	}
+	$kuser = kuserPeer::getActiveKuserByPartnerAndUid($entry->getPartnerId(), $userId);
+	if(!$kuser)
+	{
+		echo "Kuser with puser ID [$userId] not found" . PHP_EOL;
 		continue;
 	}
 
-    $entry->setCreatorKuserId($kuser->getId());
+	$entry->setCreatorKuserId($kuser->getId());
 	$entry->setCreatorPuserId($userId);
 	$entry->save();
 	
