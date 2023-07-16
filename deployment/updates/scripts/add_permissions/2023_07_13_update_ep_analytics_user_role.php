6c9f0a4a24bc9035d92c18fd5ebd5e60a1c3ddfa
danielbarak <daniel.barak@kaltura.com>
2023-07-16 09:42:36 +0300

    PLAT-24331: Add permission to EP analytics Role

--- deployment/updates/scripts/add_permissions/2023_07_13_update_ep_analytics_user_role.php
+++ deployment/updates/scripts/add_permissions/2023_07_13_update_ep_analytics_user_role.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package deployment
- * @subpackage Rigel.roles_and_permissions
+ * @subpackage Scorpius.roles_and_permissions
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionToRole.php';
