7fd23375920e79bfb21f1a5cf538e7cf0c52e20b
Amir Asaelov <amir.asaelov@kaltura.com>
2023-07-19 16:32:44 +0300

    modify new table index

--- deployment/updates/sql/2023_07_20_alter_kuser_to_user_role_table.sql
+++ deployment/updates/sql/2023_07_20_alter_kuser_to_user_role_table.sql
@@ -1,1 +1,1 @@
-ALTER TABLE kuser_to_user_role ADD app_guid VARCHAR(255) DEFAULT NULL, ADD INDEX app_guid(`app_guid`);
+ALTER TABLE kuser_to_user_role ADD app_guid VARCHAR(255) DEFAULT NULL, ADD INDEX kuser_id_app_guid(`kuser_id`,`app_guid`);
