ade574cc219cd2e65d738435b247b8230acb1a3b
Amir Asaelov <amir.asaelov@kaltura.com>
2023-07-20 13:10:47 +0300

    modify "kuser_to_user_role" index

--- deployment/base/sql/01.kaltura_ce_tables.sql
+++ deployment/base/sql/01.kaltura_ce_tables.sql
@@ -1452,7 +1452,7 @@ CREATE TABLE IF NOT EXISTS `kuser_to_user_role` (
   `app_guid` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `kuser_to_user_role_FI_1` (`kuser_id`),
-  KEY `app_guid` (`app_guid`)
+  KEY `kuser_id_app_guid` (`kuser_id`,`app_guid`);
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
