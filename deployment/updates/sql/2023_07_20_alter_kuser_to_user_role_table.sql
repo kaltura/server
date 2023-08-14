acb56262fb4bb5b133c9d0375180b26cf4135852
Amir Asaelov <amir.asaelov@kaltura.com>
2023-08-14 13:36:01 +0300

    Change 'app_guid' from varchar 255 to 25

--- deployment/updates/sql/2023_07_20_alter_kuser_to_user_role_table.sql
+++ deployment/updates/sql/2023_07_20_alter_kuser_to_user_role_table.sql
@@ -1,5 +1,4 @@
-ALTER TABLE kuser_to_user_role ADD app_guid VARCHAR(255) DEFAULT NULL, ADD INDEX kuser_id_app_guid(`kuser_id`,`app_guid`);
-
-TODO consider
-1. Chaing app_guid varchar(24) - mongo db objectId is 24 char long (hex) and 12 char long (binary)
-2. Setting index for 'app_guid' as we will fetch from this table by 'app_guid'
+ALTER TABLE kuser_to_user_role
+    ADD app_guid VARCHAR(25) DEFAULT NULL,
+    ADD INDEX app_guid(`app_guid`),
+    ADD INDEX kuser_id_app_guid(`kuser_id`,`app_guid`);
