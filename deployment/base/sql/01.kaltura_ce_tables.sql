c4db67d55f91d415712dd778ffd5b4ee25d66f5b
Amir Asaelov <amir.asaelov@kaltura.com>
2023-08-14 13:36:01 +0300

    Change 'app_guid' from varchar 255 to 25

--- deployment/base/sql/01.kaltura_ce_tables.sql
+++ deployment/base/sql/01.kaltura_ce_tables.sql
@@ -1449,9 +1449,10 @@ CREATE TABLE IF NOT EXISTS `kuser_to_user_role` (
   `user_role_id` int(11) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `app_guid` varchar(255) DEFAULT NULL,
+  `app_guid` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `kuser_to_user_role_FI_1` (`kuser_id`),
+  KEY `app_guid` (`app_guid`),
   KEY `kuser_id_app_guid` (`kuser_id`,`app_guid`);
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
