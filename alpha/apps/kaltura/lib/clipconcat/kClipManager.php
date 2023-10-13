a6e63c7d738a3b87fe99a996858703830e5f2a4c
Salma <salma.nassar@kaltura.com>
2023-10-13 13:43:59 +0300

    PLAT-24432 add white background for picture to video conversion to handle transparency

--- alpha/apps/kaltura/lib/clipconcat/kClipManager.php
+++ alpha/apps/kaltura/lib/clipconcat/kClipManager.php
@@ -1199,15 +1199,23 @@ class kClipManager implements kBatchJobStatusEventConsumer
 			$cmdStr.= " -r " . $conversionParams[self::FRAME_RATE];
 		}
 		$cmdStr .= " -c:v libx264 -pix_fmt yuv420p";
+		$whiteBackgroundFilter = "split=2[bg][fg];[bg]drawbox=c=white@1:replace=1:t=fill[bg];[bg][fg]overlay=format=auto";
 		if(isset($conversionParams[self::WIDTH]) && isset($conversionParams[self::HEIGHT]))
 		{
 			$width = $conversionParams[self::WIDTH];
 			$height = $conversionParams[self::HEIGHT];
 			$frameSize = "$width:$height";
 			$cmdStr .= " -s " . str_replace(':', 'x', $frameSize);
-			$cmdStr .= " -filter_complex '[0:v]scale=iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih)[vflt0];[vflt0]pad=$width:$height:(ow-iw)/2:(oh-ih)/2'";
+			$scalingFilter = "scale=iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih)";
+			$paddingFilter = "pad=$width:$height:(ow-iw)/2:(oh-ih)/2";
+			$filter = "[0:v]" . $scalingFilter . "[vflt0];[vflt0]" . $paddingFilter . "[out];[out]" . $whiteBackgroundFilter;
 			$cmdStr .= " -aspect $frameSize";
 		}
+		else
+		{
+			$filter ="[0]$whiteBackgroundFilter";
+		}
+		$cmdStr .= " -filter_complex '$filter'";
 		$cmdStr .= " -c:a libfdk_aac -b:a 192k";
 		$cmdStr .= " -f mpegts -vsync 1";
 		$cmdStr .= " -y __outFileName__";
