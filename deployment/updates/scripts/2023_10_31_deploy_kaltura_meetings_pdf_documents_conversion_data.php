e53295aaa9511ef6b4e1406a7257adedb9098c24
David Winder <david.winder@kaltura.com>
2023-11-05 11:05:46 +0200

    Set params on the flavorParam

--- deployment/updates/scripts/2023_10_31_deploy_kaltura_meetings_pdf_documents_conversion_data.php
+++ deployment/updates/scripts/2023_10_31_deploy_kaltura_meetings_pdf_documents_conversion_data.php
@@ -4,7 +4,6 @@
  *
  * Deploy Kaltura meetings Flavors + conversion Profile
  *
- * No need to re-run after server code deploy
  */
 require_once (__DIR__ . '/../../bootstrap.php');
 $script = realpath(dirname(__FILE__) . '/../../') . '/base/scripts/insertDefaults.php';
