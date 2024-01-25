f0eafa4057833b7bddc03bab6a69cd2569de49c0
shamama <yair.shamama@kaltura.com>
2024-01-25 11:49:09 +0200

    LIV-1239: check if schedule event is active

--- plugins/schedule/base/lib/model/ScheduleEventPeer.php
+++ plugins/schedule/base/lib/model/ScheduleEventPeer.php
@@ -283,6 +283,7 @@ class ScheduleEventPeer extends BaseScheduleEventPeer implements IRelatedObjectP
 		
 		$c = KalturaCriteria::create(ScheduleEventPeer::OM_CLASS);
 		$c->add(ScheduleEventPeer::TYPE, $types, Criteria::IN);
+		$c->add(ScheduleEventPeer::STATUS, ScheduleEventStatus::ACTIVE, Criteria::EQUAL);
 		$c->add(ScheduleEventPeer::END_DATE, $time, Criteria::GREATER_EQUAL);
 		$c->add(ScheduleEventPeer::START_DATE, $time, Criteria::LESS_EQUAL);
 		$filter = new ScheduleEventFilter();
