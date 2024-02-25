683ae0f383dbfcccab7e678b7873bf24264ef4e8
Tal Dubov <tal.dubov@kaltura.com>
2024-02-25 12:10:41 +0200

    PSVAMB-48998 -> Ordering by user full_name

--- plugins/search/providers/elastic_search/lib/model/enum/ESearchUserOrderByFieldName.php
+++ plugins/search/providers/elastic_search/lib/model/enum/ESearchUserOrderByFieldName.php
@@ -8,5 +8,6 @@ interface ESearchUserOrderByFieldName extends BaseEnum
 	const UPDATED_AT = 'updated_at';
 	const CREATED_AT = 'created_at';
 	const SCREEN_NAME = 'screen_name.raw';
+    const FULL_NAME = 'full_name.raw';
 	const USER_ID = 'puser_id.raw';
 }
