1571e7ea3f2f6005cbc7fc3a16381d5c02e67494
amirhalfon1 <amir.halfon@kaltura.com>
2024-03-05 14:30:59 +0200

    code review changes: remove un-needed print_r change "@TOPIC_NAME@" to "entry" remove "template" from filename in script use "strlen" for "array_filter" instead of default filter callback

--- deployment/updates/scripts/2024_03_03_deploy_kafka_room_events.php
+++ deployment/updates/scripts/2024_03_03_deploy_kafka_room_events.php
@@ -5,9 +5,8 @@
 require_once (__DIR__ . '/../../bootstrap.php');
 
 $script = realpath(dirname(__FILE__) . "/../../../tests/standAloneClient/exec.php");
-$newTemplateUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2024_03_03_add_kafka_room_entry_notifications.template.xml");
+$newTemplateUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2024_03_03_add_kafka_room_entry_notifications.xml");
 
-print_r($newTemplateUpdate);
 if(!file_exists($newTemplateUpdate) || !file_exists($script))
 {
     KalturaLog::err("Missing update script file");
--- release-notes.md
+++ release-notes.md
@@ -4,7 +4,7 @@
 - Issue ID: FOUN-1370
 
 ### Configuration ###
-    Replace all tokens (SERVICE_URL, ADMIN_CONSOLE_PARTNER_ADMIN_SECRET, TOPIC_NAME) from the template XML file below and remove ".template" from the file name:
+    Replace all tokens (SERVICE_URL, ADMIN_CONSOLE_PARTNER_ADMIN_SECRET) from the template XML file below and remove ".template" from the file name:
 	/opt/kaltura/app/deployment/updates/scripts/xml/notifications/2024_03_03_add_kafka_room_entry_notifications.template.xml
 
 ### Deployment scripts ###
