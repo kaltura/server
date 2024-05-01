29c94edcd4aebd950d8e73b4cefa400178a610b4
shahbaa123 <129391094+shahbaa123@users.noreply.github.com>
2024-05-01 17:36:40 +0300

    Update kPlaybackContextDataHelper.php
    fixed indentation

--- alpha/apps/kaltura/lib/kPlaybackContextDataHelper.php
+++ alpha/apps/kaltura/lib/kPlaybackContextDataHelper.php
@@ -197,7 +197,7 @@ class kPlaybackContextDataHelper
 			if ($action->getType() == RuleActionType::LIMIT_FLAVORS)
 			{
 				/* @var $action kAccessControlLimitFlavorsAction */
-                $flavorParamsIds = explode(',', $action->getFlavorParamsIds());
+                		$flavorParamsIds = explode(',', $action->getFlavorParamsIds());
 				$flavorsParamsNotIn = $action->getIsBlockedList();
 			}
 		}
