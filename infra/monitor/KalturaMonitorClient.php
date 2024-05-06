6888ab44c36f58338a6c6155b07227009123339d
yossipapi <yossi.papiashvili@kaltura.com>
2024-05-06 15:38:07 +0300

    PHP8: Fix php notice errors

--- infra/monitor/KalturaMonitorClient.php
+++ infra/monitor/KalturaMonitorClient.php
@@ -483,7 +483,7 @@ class KalturaMonitorClient
 			self::FIELD_TABLE			=> $indexName,
 			self::FIELD_QUERY_TYPE		=> $actionName,
 			self::FIELD_EXECUTION_TIME	=> $queryTook,
-			self::FIELD_LENGTH			=> $body ? strlen($body) : 0,
+			self::FIELD_LENGTH			=> strlen(strval($body)),
 		));
 
 		if ($errorCode)
