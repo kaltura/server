5a9292ba67b0706dcd2b59d2cad32bc369c4eb93
zoharsKaltura <zohar.samra@kaltura.com>
2024-05-27 10:05:21 +0300

    when there is a live redirect event ctegories wont be coppied

--- alpha/lib/model/LiveEntry.php
+++ alpha/lib/model/LiveEntry.php
@@ -255,9 +255,9 @@ abstract class LiveEntry extends entry
 		return array(array('bitrate' => 300, 'width' => 320, 'height' => 240));
 	}
 
-	public function getRecordedEntryId()
+	public function getRecordedEntryId($useDynamic=true)
 	{
-		if($this->pluginableGetter(__FUNCTION__, $output))
+		if($useDynamic && $this->pluginableGetter(__FUNCTION__, $output))
 		{
 			return $output;
 		}
