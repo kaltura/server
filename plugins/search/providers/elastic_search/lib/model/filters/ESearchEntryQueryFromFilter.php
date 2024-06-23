2e1433426c82ce7b248db16549c0668fc54c1641
daniel.barak <daniel.barak@kaltura.com>
2024-06-23 16:31:03 +0300

    PLAT-24812

--- plugins/search/providers/elastic_search/lib/model/filters/ESearchEntryQueryFromFilter.php
+++ plugins/search/providers/elastic_search/lib/model/filters/ESearchEntryQueryFromFilter.php
@@ -83,7 +83,7 @@ class ESearchEntryQueryFromFilter extends ESearchQueryFromFilter
         ESearchEntryFilterFields::DURATION_TYPE,
         ESearchEntryFilterFields::EXTERNAL_SOURCE_TYPE,
         ESearchEntryFilterFields::ROOM_TYPE,
-	    ESearchEntryFilterFields::BROADCAST_ENTRY_ID,
+		ESearchEntryFilterFields::BROADCAST_ENTRY_ID,
     );
 
     protected static $specialFields = array(
@@ -398,7 +398,7 @@ class ESearchEntryQueryFromFilter extends ESearchQueryFromFilter
             ESearchEntryFilterFields::EXTERNAL_SOURCE_TYPE => ESearchEntryFieldName::EXTERNAL_SOURCE_TYPE,
             ESearchEntryFilterFields::RECYCLED_AT => ESearchEntryOrderByFieldName::RECYCLED_AT,
             ESearchEntryFilterFields::ROOM_TYPE => ESearchEntryFieldName::ROOM_TYPE,
-	        ESearchEntryFilterFields::BROADCAST_ENTRY_ID => ESearchEntryFieldName::BROADCAST_ENTRY_ID,
+			ESearchEntryFilterFields::BROADCAST_ENTRY_ID => ESearchEntryFieldName::BROADCAST_ENTRY_ID,
         );
 
         if (array_key_exists($field, $fieldsMap)) {
