73ddfd53469ed5ae5abb1407da051a037f0bdb17
Joshua Navarro <navarro967@gmail.com>
2024-07-01 14:48:45 -0400

    fix replacement

--- alpha/lib/model/LiveEntry.php
+++ alpha/lib/model/LiveEntry.php
@@ -327,7 +327,7 @@ abstract class LiveEntry extends entry
 		if (isset($liveConfiguration[LiveEntry::STREAM_NAME_TEMPLATE]))
 			$streamNameTemplate = $liveConfiguration[LiveEntry::STREAM_NAME_TEMPLATE];
 
-		str_replace('{entryId}', $this->getId(), $streamNameTemplate);
+		$streamNameTemplate = str_replace('{entryId}', $this->getId(), $streamNameTemplate);
 
 		return $this->getFromCustomData( "streamName", null, $streamNameTemplate);
 	}
