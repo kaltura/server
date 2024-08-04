7c459c890964d13a77aa73e613f7e49f2e3f0448
Romulo Vieira da Silva <rgvsilva@users.noreply.github.com>
2024-08-04 10:27:43 +0300

    Fixing file to point to templated XL

--- deployment/updates/scripts/2022_01_05_admin_tags_equals_boolean_notification.php
+++ deployment/updates/scripts/2022_01_05_admin_tags_equals_boolean_notification.php
@@ -4,7 +4,7 @@
  */
 require_once (__DIR__ . '/../../bootstrap.php');
 $script = realpath(dirname(__FILE__) . "/../../../tests/standAloneClient/exec.php");
-$newTemplateUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2022_01_05_admin_tags_equals_boolean_notification.template.xml");
+$newTemplateUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2022_01_05_admin_tags_equals_boolean_notification.xml");
 print_r($newTemplateUpdate);
 if(!file_exists($newTemplateUpdate) || !file_exists($script))
 {
