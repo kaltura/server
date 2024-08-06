83840e2665b711299e5a6ebe20db9d2a124cd2d4
Salma <salma.nassar@kaltura.com>
2024-08-06 07:58:50 +0300

    multi clip refactor

--- infra/cdl/kdl/KDLMediaObjectData.php
+++ infra/cdl/kdl/KDLMediaObjectData.php
@@ -212,7 +212,7 @@
 									 *  2: letterbox mode
 									 *  3: force 16/9 for everything that is not 16:9
 									 *  4: force '-aspect' option
-									 *  5: force perciese source AR
+									 *  5: force precise source AR
 									 *  7: cropping
 	 								 *  8: inverted cropping
 									 */
