a17d7ac44877f55b78aeab94812490cbd2ab655c
shahbaa123 <129391094+shahbaa123@users.noreply.github.com>
2024-09-17 13:56:20 +0300

    Update PlaylistService.php

--- api_v3/services/PlaylistService.php
+++ api_v3/services/PlaylistService.php
@@ -387,21 +387,21 @@ class PlaylistService extends KalturaEntryService
 		return $this->executeFromContentLogic($playlistType, $playlistContent, $detailed, $pager);
 	}
 
-  function executeFromContentLogic($playlistType, $playlistContent, $detailed = false, $pager = null, $playlistId = null)
-  {
-      $partnerId = $this->getPartnerId() ? $this->getPartnerId() : kCurrentContext::getCurrentPartnerId();
-      myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_PROPEL3;
-      if ($this->getKs() && is_object($this->getKs()) && $this->getKs()->isAdmin())
-      {
-          myPlaylistUtils::setIsAdminKs(true);
-      }
-      list($entryFiltersViaEsearch,  $entryFiltersViaSphinx, $totalResults) = myPlaylistUtils::splitEntryFilters($playlistContent);
-      $pagerSeparateQueries = self::decideWhereHandlingPager($pager,$entryFiltersViaEsearch, $entryFiltersViaSphinx);
-      $entryList = self::handlePlaylistByType($playlistType, $entryFiltersViaEsearch, $entryFiltersViaSphinx, $partnerId, $pagerSeparateQueries, $pager, $totalResults, $playlistContent, $playlistId);
-      myEntryUtils::updatePuserIdsForEntries($entryList);
-      KalturaLog::debug("entry ids count: " . (is_array($entryList ? count($entryList) : 0)));
-      return KalturaBaseEntryArray::fromDbArray($entryList, $this->getResponseProfile());
-  }
+	function executeFromContentLogic($playlistType, $playlistContent, $detailed = false, $pager = null, $playlistId = null)
+	{
+		$partnerId = $this->getPartnerId() ? $this->getPartnerId() : kCurrentContext::getCurrentPartnerId();
+		myDbHelper::$use_alternative_con = myDbHelper::DB_HELPER_CONN_PROPEL3;
+		if ($this->getKs() && is_object($this->getKs()) && $this->getKs()->isAdmin())
+		{
+			myPlaylistUtils::setIsAdminKs(true);
+		}
+		list($entryFiltersViaEsearch,  $entryFiltersViaSphinx, $totalResults) = myPlaylistUtils::splitEntryFilters($playlistContent);
+		$pagerSeparateQueries = self::decideWhereHandlingPager($pager,$entryFiltersViaEsearch, $entryFiltersViaSphinx);
+		$entryList = self::handlePlaylistByType($playlistType, $entryFiltersViaEsearch, $entryFiltersViaSphinx, $partnerId, $pagerSeparateQueries, $pager, $totalResults, $playlistContent, $playlistId);
+		myEntryUtils::updatePuserIdsForEntries($entryList);
+		KalturaLog::debug("entry ids count: " . (is_array($entryList ? count($entryList) : 0)));
+		return KalturaBaseEntryArray::fromDbArray($entryList, $this->getResponseProfile());
+	}
   
 	protected static function handlePlaylistByType($playlistType, $entryFiltersViaEsearch, $entryFiltersViaSphinx, $partnerId, $pagerSeperateQueries, $pager, $totalResults, $playlistContent, $playlistId = null)
 	{
