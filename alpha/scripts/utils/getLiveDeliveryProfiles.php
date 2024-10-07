732b2ac9a2df93613df6475885fd5eebe38a2a90
Shahbaa Shalan <shahbaa.shalan@kaltura.com>
2024-10-08 00:10:28 +0300

    SUP-44927 Getting live delivery profiles script

---
+++ alpha/scripts/utils/getLiveDeliveryProfiles.php
@@ -0,0 +1,47 @@
+<?php
+require_once(__DIR__ . '/../bootstrap.php');
+
+$output_csv_file = '/output.csv';
+$output_handle = fopen($output_csv_file, 'w');
+fputcsv($output_handle, ['PID', 'Live Delivery Profile']);
+
+$partners_exists = true;
+$bulk_size = 500;
+$lowest_partner_id = 100;
+$count_pid = 0;
+$count_queries= 0;
+while($partners_exists)
+{
+    $c = new Criteria();
+    $c->addAnd(PartnerPeer::STATUS, Partner::PARTNER_STATUS_DELETED, Criteria::NOT_EQUAL);
+    $c->addAnd(PartnerPeer::ID, $lowest_partner_id, Criteria::GREATER_THAN);
+    $c->addAscendingOrderByColumn(PartnerPeer::ID);
+    $c->setLimit($bulk_size);
+    $partners = PartnerPeer::doSelect($c);
+    $count_queries++;
+    if (!$partners)
+    {
+        KalturaLog::debug( "No more partners." );
+        $partners_exists = false;
+    }
+    else
+    {
+        KalturaLog::debug( "Looping ". count($partners) ." partners" );
+        foreach($partners as $partner)
+        {
+            $count_pid++;
+            $pid = $partner->getId();
+            KalturaLog::debug("$count_pid Currently processing PID $pid");
+            $live_delivery_profile_ids = $partner->getFromCustomData('live_delivery_profile_ids');
+            $live_delivery_profile_ids_string = implode(',', $live_delivery_profile_ids);
+            fputcsv($output_handle, [$pid, $live_delivery_profile_ids_string]);
+        }
+    }
+    $partner = end($partners);
+    if($partner)
+    {
+        $lowest_partner_id = $partner->getId();
+    }
+    unset($partners);
+    PartnerPeer::clearInstancePool();
+}
