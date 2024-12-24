bea3574ea375c0454b27c46d7a8d000839343829
olampert <orlylampert@kaltura.com>
2024-12-25 00:04:25 +0200

    code review

--- alpha/scripts/kava/systemUtils.php
+++ alpha/scripts/kava/systemUtils.php
@@ -5,28 +5,32 @@ require_once (dirname(__FILE__).'/../bootstrap.php');
 class systemUtils
 {
 	const MEMCACHE = 'MEMCACHE';
-	const ELASTIC = "ELASTIC";
+	const ELASTIC = 'ELASTIC';
 	const ELASTIC_HOST = '127.0.0.1';
 	const ELASTIC_PORT = '9200';
 	const ELASTIC_HEALTH_CHECK = '/_cluster/health?pretty';
 
 	protected static $checkFunc = array(
-		self::MEMCACHE => "systemUtils::pingMemcache",
-		self::ELASTIC => "systemUtils::pingElastic",
+		self::MEMCACHE => 'systemUtils::pingMemcache',
+		self::ELASTIC => 'systemUtils::pingElastic',
 	);
 
 	public static function getHealthCheckInfo($checkedComponents)
 	{
 		$healthCheckArray = array();
-		foreach ($checkedComponents as $component) {
-			if (isset(self::$checkFunc[$component])) {
+		foreach ($checkedComponents as $component)
+		{
+			if (isset(self::$checkFunc[$component]))
+			{
 				$healthCheckArray[$component] = call_user_func(self::$checkFunc[$component]);
 			}
 		}
 
 		$result = '';
-		foreach ($healthCheckArray as $component => $res) {
-			if (!$res) {
+		foreach ($healthCheckArray as $component => $res)
+		{
+			if (!$res)
+			{
 				$result .= "health check failed for $component " . PHP_EOL;
 			}
 		}
@@ -37,12 +41,12 @@ class systemUtils
 	public static function pingElastic()
 	{
 		$elasticHost = kConf::get('elasticHost', 'elastic', null);
-		if(!$elasticHost)
+		if (!$elasticHost)
 		{
 			$elasticHost = self::ELASTIC_HOST;
 		}
 		$elasticPort = kConf::get('elasticPort', 'elastic', null);
-		if(!$elasticPort)
+		if (!$elasticPort)
 		{
 			$elasticPort = self::ELASTIC_PORT;
 		}
@@ -50,12 +54,12 @@ class systemUtils
 		{
 			$url = 'http://' . $elasticHost . ':' . $elasticPort . self::ELASTIC_HEALTH_CHECK;
 			$elasticHealth = json_decode(KCurlWrapper::getContent($url), true);
-			if(!isset($elasticHealth['status']) || $elasticHealth['status'] == 'red')
+			if (!isset($elasticHealth['status']) || $elasticHealth['status'] == 'red')
 			{
 				return 0;
 			}
 		}
-		catch(Exception $e)
+		catch (Exception $e)
 		{
 			return 0;
 		}
