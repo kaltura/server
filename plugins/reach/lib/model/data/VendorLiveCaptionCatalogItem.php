07342a213df1346eb93fc5b0502907dc4629d200
hilak <hila.karimov@kaltura.com>
2025-01-08 19:44:53 -0500

    PSVAMB-69071 - add support for automated reach flows on live schedule events

--- plugins/reach/lib/model/data/VendorLiveCaptionCatalogItem.php
+++ plugins/reach/lib/model/data/VendorLiveCaptionCatalogItem.php
@@ -12,19 +12,27 @@ class VendorLiveCaptionCatalogItem extends VendorLiveCatalogItem
 
 	public function getTaskJobData($object)
 	{
+		$latestEvent = null;
 		if($object instanceof LiveEntry && $object->hasCapability(LiveEntry::LIVE_SCHEDULE_CAPABILITY))
 		{
 			$events = ScheduleEventPeer::retrieveByTemplateEntryIdAndTypes($object->getId(), [ScheduleEventType::LIVE_STREAM]);
 
-			$data = new kScheduledVendorTaskData();
-			if(count($events))
-			{
-				$latestEvent = $events[count($events)-1];
-				$data->setEntryDuration($latestEvent->getDuration()*1000);
-				$data->setStartDate($latestEvent->getStartDate());
-				$data->setEndDate($latestEvent->getEndDate());
-				$data->setScheduledEventId($latestEvent->getId());
+			if (count($events)) {
+				$latestEvent = $events[count($events) - 1];
 			}
+		}
+		elseif ($object instanceof LiveStreamScheduleEvent)
+		{
+			$latestEvent = $object;
+		}
+
+		if($latestEvent)
+		{
+			$data = new kScheduledVendorTaskData();
+			$data->setEntryDuration($latestEvent->getDuration()*1000);
+			$data->setStartDate(intval($latestEvent->getStartDate()));
+			$data->setEndDate(intval($latestEvent->getEndDate()));
+			$data->setScheduledEventId($latestEvent->getId());
 
 			return $data;
 		}
