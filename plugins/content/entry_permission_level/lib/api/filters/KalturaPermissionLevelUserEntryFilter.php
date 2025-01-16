214b40040507e08d8f5ca53c201d7a8c90d3e953
drorsouhami <drorsu@gmail.com>
2025-01-16 15:11:39 +0200

    PLAT-25073: Filter by permission levels

--- plugins/content/entry_permission_level/lib/api/filters/KalturaPermissionLevelUserEntryFilter.php
+++ plugins/content/entry_permission_level/lib/api/filters/KalturaPermissionLevelUserEntryFilter.php
@@ -5,9 +5,57 @@
  */
 class KalturaPermissionLevelUserEntryFilter extends KalturaUserEntryFilter
 {
+	/**
+	 * @var KalturaPermissionLevelArray
+	 */
+	public $permissionLevels;
+
+	/**
+	 * @var int
+	 */
+	public $permissionLevelsBitmask;
+
+	static private $map_between_objects = array
+	(
+		'permissionLevelsBitmask' => '_bitor_extended_status',
+	);
+
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
+	}
+
 	public function getListResponse(KalturaFilterPager $pager, KalturaDetachedResponseProfile $responseProfile = null)
 	{
 		$this->typeEqual = EntryPermissionLevelPlugin::getApiValue(PermissionLevelUserEntryType::PERMISSION_LEVEL);
+
+		if ($this->permissionLevels)
+		{
+			$permissionBitMask = [
+				KalturaUserEntryPermissionLevel::SPEAKER => 1,
+				KalturaUserEntryPermissionLevel::ROOM_MODERATOR => 2,
+				KalturaUserEntryPermissionLevel::ATTENDEE => 4,
+				KalturaUserEntryPermissionLevel::ADMIN => 8,
+				KalturaUserEntryPermissionLevel::PREVIEW_ONLY => 16,
+				KalturaUserEntryPermissionLevel::CHAT_MODERATOR => 32,
+				KalturaUserEntryPermissionLevel::PANELIST => 64,
+			];
+
+			KalturaLog::debug(print_r($this->permissionLevels, true));
+			$extendedHistory = 0;
+			foreach ($this->permissionLevels as $permissionLevel)
+			{
+				/** @var KalturaPermissionLevel $permissionLevel */
+				$val = $permissionLevel->permissionLevel;
+				$extendedHistory += $permissionBitMask[intval($val)];
+			}
+
+			if ($extendedHistory)
+			{
+				$this->permissionLevelsBitmask = $extendedHistory;
+			}
+		}
+
 		$response = parent::getListResponse($pager, $responseProfile);
 		return $response;
 	}
