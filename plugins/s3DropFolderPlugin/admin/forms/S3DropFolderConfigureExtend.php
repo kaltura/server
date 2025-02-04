9d6a6ad89aa69ef3952f7cd91641cd74173eff6d
Amir Asaelov <amir.asaelov@kaltura.com>
2025-02-04 12:26:17 +0200

    add description to s3 drop folder S3 Settings

--- plugins/s3DropFolderPlugin/admin/forms/S3DropFolderConfigureExtend.php
+++ plugins/s3DropFolderPlugin/admin/forms/S3DropFolderConfigureExtend.php
@@ -9,6 +9,17 @@ class Form_S3DropFolderConfigureExtend_SubForm extends Form_DropFolderConfigureE
 	{
 		return 'S3 settings';
 	}
+	
+	public function getDescription()
+	{
+		return "Authentication precedence:<br>
+				1. User & Password (leave empty to use 'IAM Role')<br><br>
+				2. IAM Role:<br>
+				2.1. Set Role ARN - will be used to assume that role<br>
+				Note: role 'Trust Policy' must allow 'cloud_storage' s3Arn to assume it<br><br>
+				2.2. Leave empty - will use 'cloud_storage' s3Arn to access bucket<br>
+				Note: Bucket Policy must allow s3Arn to operate it";
+	}
 
 	public function init()
 	{
