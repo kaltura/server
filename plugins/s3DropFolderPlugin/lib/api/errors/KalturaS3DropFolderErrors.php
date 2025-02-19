8222c8ab5450f637fde3b2dbc546d34ec8d994b1
Amir Asaelov <amir.asaelov@kaltura.com>
2025-02-19 17:10:28 +0200

    change exception error

--- plugins/s3DropFolderPlugin/lib/api/errors/KalturaS3DropFolderErrors.php
+++ plugins/s3DropFolderPlugin/lib/api/errors/KalturaS3DropFolderErrors.php
@@ -6,5 +6,5 @@
  */
 class KalturaS3DropFolderErrors extends KalturaErrors
 {
-	const MISSING_S3ARN_CONFIG = "MISSING_S3_ARN;;'useS3Arn' ('Bucket Policy Allow Access') was set but 's3Arn' value under 's3_drop_folder' in 'runtime_config' map is missing";
+	const MISSING_S3ARN_CONFIG = "MISSING_S3_ARN;;'useS3Arn' ('Bucket Policy Allow Access') was set but 's3Arn' config is missing";
 }
