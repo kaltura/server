7a5b76616324d6c12d365bb90d55055bdb94e6cc
Moshe Maor <moshe.maor@kaltura.com>
2025-03-09 13:25:13 +0200

    Revert "SUP-42743: Update the display in search filter, remove the redundant …"

--- plugins/search/providers/elastic_search/lib/model/search/ESearchEntryQueryFilterAttributes.php
+++ plugins/search/providers/elastic_search/lib/model/search/ESearchEntryQueryFilterAttributes.php
@@ -7,7 +7,12 @@ class ESearchEntryQueryFilterAttributes extends ESearchBaseQueryFilterAttributes
 {
 	public function getDisplayInSearchFilter()
 	{
-		$recycleDisplayInSearch = false;
+		$excludedDisplayInSearchStatuses = array(EntryDisplayInSearchType::RECYCLED, EntryDisplayInSearchType::SYSTEM);
+		$displayInSearchQuery = new kESearchTermsQuery(ESearchEntryFieldName::DISPLAY_IN_SEARCH, $excludedDisplayInSearchStatuses);
+		$mustNotDisplayInSearchBoolQuery = new kESearchBoolQuery();
+		$mustNotDisplayInSearchBoolQuery->addToMustNot($displayInSearchQuery);
+		
+		$queryContainsDisplayInSearch = false;
 		$ignoreDisplayInSearchQueries = array();
 		foreach	($this->ignoreDisplayInSearchValues as $key => $value)
 		{
@@ -20,41 +25,22 @@ class ESearchEntryQueryFilterAttributes extends ESearchBaseQueryFilterAttributes
 				$ignoreDisplayInSearchQueries[] = new kESearchTermsQuery($key, $value);
 				if ($key == ESearchEntryFieldName::DISPLAY_IN_SEARCH)
 				{
-					if (in_array(EntryDisplayInSearchType::RECYCLED, $value))
-					{
-						$recycleDisplayInSearch = true;
-					}
+					$queryContainsDisplayInSearch = true;
 				}
 			}
 		}
 		
 		if (!count($ignoreDisplayInSearchQueries))
 		{
-			return $this->getMustNotDisplayInSearch();
+			return $mustNotDisplayInSearchBoolQuery;
 		}
-
-		if ($recycleDisplayInSearch)
+		
+		$displayInSearchBoolQuery = new kESearchBoolQuery();
+		$displayInSearchBoolQuery->addQueriesToShould($ignoreDisplayInSearchQueries);
+		if (!$queryContainsDisplayInSearch)
 		{
-			$excludedSystemDisplayInSearch = array(EntryDisplayInSearchType::SYSTEM);
-			$systemDisplayInSearchQuery = new kESearchTermsQuery(ESearchEntryFieldName::DISPLAY_IN_SEARCH, $excludedSystemDisplayInSearch);
-			$mustNotDisplayInSearchSystem = new kESearchBoolQuery();
-			$mustNotDisplayInSearchSystem->addToMustNot($systemDisplayInSearchQuery);
-			return $mustNotDisplayInSearchSystem;
+			$displayInSearchBoolQuery->addToShould($mustNotDisplayInSearchBoolQuery);
 		}
-		else
-		{
-			$displayInSearchBoolQuery = new kESearchBoolQuery();
-			$displayInSearchBoolQuery->addToShould($this->getMustNotDisplayInSearch());
-			return $displayInSearchBoolQuery;
-		}
-
-	}
-	protected function getMustNotDisplayInSearch()
-	{
-		$excludedDisplayInSearchStatuses = array(EntryDisplayInSearchType::RECYCLED, EntryDisplayInSearchType::SYSTEM);
-		$displayInSearchQuery = new kESearchTermsQuery(ESearchEntryFieldName::DISPLAY_IN_SEARCH, $excludedDisplayInSearchStatuses);
-		$mustNotDisplayInSearchBoolQuery = new kESearchBoolQuery();
-		$mustNotDisplayInSearchBoolQuery->addToMustNot($displayInSearchQuery);
-		return $mustNotDisplayInSearchBoolQuery;
+		return $displayInSearchBoolQuery;
 	}
 }
