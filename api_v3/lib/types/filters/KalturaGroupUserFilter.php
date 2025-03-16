27c08f085997cc8e82d17e4042b09da5637d794e
daniel.barak <daniel.barak@kaltura.com>
2025-03-16 14:24:44 +0200

    ?? expression

--- api_v3/lib/types/filters/KalturaGroupUserFilter.php
+++ api_v3/lib/types/filters/KalturaGroupUserFilter.php
@@ -123,7 +123,7 @@ class KalturaGroupUserFilter extends KalturaGroupUserBaseFilter
 		{
 			$groupIdIn = explode(',', $this->groupIdIn);
 			$partnerId = kCurrentContext::getCurrentPartnerId();
-			$groupType = $this->groupType ? $this->groupType : GroupType::GROUP;
+			$groupType = $this->groupType ?? GroupType::GROUP;
 
 			$c = new Criteria();
 			$c->add(kuserPeer::PARTNER_ID, $partnerId, Criteria::EQUAL);
