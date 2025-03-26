41a0715e3d8676b7949fd10b78b457bc5ddc585d
yossipapi <yossi.papiashvili@kaltura.com>
2025-03-26 21:36:09 +0200

    Update kInfraMemcacheCacheWrapper.php

--- infra/cache/kInfraMemcacheCacheWrapper.php
+++ infra/cache/kInfraMemcacheCacheWrapper.php
@@ -68,7 +68,6 @@ class kInfraMemcacheCacheWrapper extends kInfraBaseCacheWrapper
 		if ($this->memcache)
 		{
 			$this->memcache->close();
-			$this->connectAttempts = 0;
 		}
 		$this->memcache = null;
 	}
