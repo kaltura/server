02128542163ddcf0ede350e7b51f0f43a1ee58db
David Winder <david.winder@kaltura.com>
2025-04-14 14:23:09 +0300

    SUP-47636: Split url to prefix by path for caption flavor

--- plugins/content/caption/base/CaptionPlugin.php
+++ plugins/content/caption/base/CaptionPlugin.php
@@ -673,14 +673,18 @@ class CaptionPlugin extends KalturaPlugin implements IKalturaServices, IKalturaP
 		foreach ($webVTTStreams as $stream)
 		{
 			/* @var $stream kLiveStreamParams */
-            $streamLang = $stream->getLanguage() ?? 'Unknown';
+			$streamLang = $stream->getLanguage() ?? 'Unknown';
+			$fullUrl = $deliveryProfile->getPackagerUrl($liveEntryServerNodes[0]) . 'index-s' . $stream->getFlavorId() . '-t.m3u8';
+			$url = parse_url($fullUrl, PHP_URL_PATH);
+			$prefix = substr($fullUrl, 0, -strlen($url));
+
 			$caption = [
-                'tokenizer'=> $deliveryProfile->getTokenizer(),
-                'urlPrefix'=> $deliveryProfile->getPackagerUrl($liveEntryServerNodes[0]),
-                'url'=> 'index-s' . $stream->getFlavorId() . '-t.m3u8',
-                'label'=> languageCodeManager::getFullLanguageNameFromThreeCode($streamLang),
-                'default'=> 'NO',
-                'language'=> $streamLang
+				'tokenizer'=> $deliveryProfile->getTokenizer(),
+				'urlPrefix'=> $prefix,
+				'url'=> $url,
+				'label'=> languageCodeManager::getFullLanguageNameFromThreeCode($streamLang),
+				'default'=> 'NO',
+				'language'=> $streamLang
 			];
 
 			$liveCaptions[] = $caption;
