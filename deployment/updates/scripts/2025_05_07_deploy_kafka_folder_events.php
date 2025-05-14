24b690d12d1f9269147ea5d9efeaad85d1cf1a8c
daniel.barak <daniel.barak@kaltura.com>
2025-05-14 10:09:41 +0300

    fixed templates

--- deployment/updates/scripts/2025_05_07_deploy_kafka_folder_events.php
+++ deployment/updates/scripts/2025_05_07_deploy_kafka_folder_events.php
@@ -6,16 +6,16 @@ require_once (__DIR__ . '/../../bootstrap.php');
 
 $script = realpath(dirname(__FILE__) . "/../../../tests/standAloneClient/exec.php");
 $entryUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_05_07_add_kafka_entry_updated_notifications.xml");
-$userDelete = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2023_03_08_update_kafka_kuser_notifications.xml");
+$userDelete = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_05_07_update_kafka_kuser_notifications.xml");
 $groupUserAddUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_05_07_add_kafka_groupuser_added_notifications.xml");
-$groupAddUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_05_07_add_kafka_group_added_notifications.xml");
+$groupUpdateDelete = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_05_07_add_kafka_group_updated_notifications.xml");
 
-$user = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2022_07_27_add_kafka_user_events.xml"); //TODO remove
+
 
 if(!file_exists($entryUpdate) ||
 	!file_exists($userDelete) ||
 	!file_exists($groupUserAddUpdate) ||
-	!file_exists($groupAddUpdate) ||
+	!file_exists($groupUpdateDelete) ||
 	!file_exists($script))
 {
 	KalturaLog::err("Missing script file");
@@ -35,12 +35,13 @@ if (!isset($kafkaConfig['brokers']) && !(isset($kafkaConfig['host']) && isset($k
 	return;
 }
 
+$user = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_05_07_update_kafka_kuser_events.xml"); //TODO remove
 passthru("php $script $user"); //TODO remove
 
 passthru("php $script $entryUpdate");
 passthru("php $script $userDelete");
 passthru("php $script $groupUserAddUpdate");
-passthru("php $script $groupAddUpdate");
+passthru("php $script $groupUpdateDelete");
 
 
 
