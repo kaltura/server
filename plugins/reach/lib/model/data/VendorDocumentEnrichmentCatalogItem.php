ed756c51565f6f5f3bfb95b20b73e64fdc4d9f97
zoharsKaltura <zohar.samra@kaltura.com>
2025-05-22 15:28:35 +0300

    add isEntryTypeSupported function

--- plugins/reach/lib/model/data/VendorDocumentEnrichmentCatalogItem.php
+++ plugins/reach/lib/model/data/VendorDocumentEnrichmentCatalogItem.php
@@ -9,4 +9,10 @@ class VendorDocumentEnrichmentCatalogItem extends VendorCatalogItem
 	{
 		$this->setServiceFeature(VendorServiceFeature::DOCUMENT_ENRICHMENT);
 	}
+
+	public function isEntryTypeSupported($type, $mediaType = null): bool
+	{
+		$supportedMediaTypes = [entry::ENTRY_MEDIA_TYPE_DOCUMENT, entry::ENTRY_MEDIA_TYPE_PDF];
+		return $type === entryType::DOCUMENT && in_array($mediaType, $supportedMediaTypes);
+	}
 }
