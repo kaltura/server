fee5049731ad01674c568ec98865710bf9472285
daniel.barak <daniel.barak@kaltura.com>
2025-05-27 11:16:17 +0300

    Add AudioDescriptionAsset class and enhance KalturaAudioDescriptionFlavorAsset with order property

--- api_v3/lib/types/conversionProfile/KalturaAudioDescriptionFlavorAsset.php
+++ api_v3/lib/types/conversionProfile/KalturaAudioDescriptionFlavorAsset.php
@@ -6,5 +6,26 @@
  */
 class KalturaAudioDescriptionFlavorAsset extends KalturaFlavorAsset
 {
-    // You can add audio description specific properties or methods here if needed
+	/**
+	 * The desired order of the flavor
+	 *
+	 * @var int
+	 */
+	public $order;
+
+	private static $map_between_objects = array
+	(
+		"order"
+	);
+
+	public function getMapBetweenObjects ( )
+	{
+		return array_merge ( parent::getMapBetweenObjects() , self::$map_between_objects );
+	}
+
+	public function toInsertableObject($sourceObject = null, $propsToSkip = array())
+	{
+		$sourceObject = new AudioDescriptionAsset();
+		return parent::toInsertableObject ($sourceObject, $propsToSkip);
+	}
 }
