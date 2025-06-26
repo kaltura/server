abea69bece661f8c272841f5a1466287c873e7bc
rkreich <roman.kreitchman@gmail.com>
2025-06-26 15:51:23 +0300

    PLAT-25286 - Metadata enrichment apply settings

---
+++ plugins/reach/lib/api/enums/KalturaMetadataEnrichmentApplyMode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package plugins.reach
+ * @subpackage enum
+ */
+class KalturaMetadataEnrichmentApplyMode extends KalturaStringEnum
+{
+	const FILL_EMPTY = "FILL_EMPTY";
+	const OVERRIDE_ALL = "OVERRIDE_ALL";
+	const FILL_EMPTY_AND_OVERRIDE_LIST = "FILL_EMPTY_AND_OVERRIDE_LIST";
+
+}
