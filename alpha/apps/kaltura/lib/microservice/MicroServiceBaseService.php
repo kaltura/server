2db5b8e916f9a287e05b2afaab15a83853729c45
Guy Elkayam <GElkayam@users.noreply.github.com>
2025-07-22 21:40:35 +0300

    Refactor buildServiceUrl to include service name in override name generation

--- alpha/apps/kaltura/lib/microservice/MicroServiceBaseService.php
+++ alpha/apps/kaltura/lib/microservice/MicroServiceBaseService.php
@@ -49,9 +49,14 @@ abstract class MicroServiceBaseService
 	 */
 	public static function buildServiceUrl($hostName, $serviceName, $isApi = true)
 	{
-		$overrideName = 'override_' . str_replace('-', '_', $hostName) . '_microservice_name';
+		$overrideName = 'override_' . str_replace('-','_',$hostName) . '_microservice';
+		if (!empty($serviceName))
+		{
+			$overrideName = $overrideName . '_' . str_replace('-','_',$serviceName);
+		}
+	
 		$overrideUrl = kConf::get($overrideName,'local', null);
-		if ($overrideUrl)
+		if (!empty($overrideUrl))
 		{
 			// if override url is set, use it
 			return $overrideUrl;
