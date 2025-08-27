dddaf60fb578af83dc1b796aad17ebb851ae5df2
Amir Asaelov <amir.asaelov@kaltura.com>
2025-08-27 07:13:40 +0300

    Add input validation instead of silently converting

--- plugins/scheduled_task/lib/processors/KGenericProcessor.php
+++ plugins/scheduled_task/lib/processors/KGenericProcessor.php
@@ -93,7 +93,15 @@ class KGenericProcessor
 
 	protected static function getUpdateDay($waitDays = 0) {
 		$now = intval(time() / 86400);  // as num of sec in day to get day number
-		return $now - intval($waitDays);
+		
+		if (!is_numeric($waitDays))
+		{
+			// Handle cases where $waitDays is not a number
+			// This happen for scheduled_task_profile object that was save before input validation was added at: plugins/scheduled_task/admin/forms/MediaRepurposingTasksSubForm.php:30
+			$waitDays = 0;
+		}
+		
+		return $now - $waitDays;
 	}
 
 	/**
