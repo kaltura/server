315924aa4873ba90af648c1656e350e4b43dd967
Salma Nassar <75244699+salma2222@users.noreply.github.com>
2025-09-07 14:48:32 +0300

    Update 2025_09_07_update_category_entry_kafka_notifications.php

--- deployment/updates/scripts/2025_09_07_update_category_entry_kafka_notifications.php
+++ deployment/updates/scripts/2025_09_07_update_category_entry_kafka_notifications.php
@@ -5,12 +5,12 @@
 require_once (__DIR__ . '/../../bootstrap.php');
 
 $script = realpath(dirname(__FILE__) . '/../../../') . '/tests/standAloneClient/exec.php';
-$scheduleEventNotificationUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_09_07_update_category_entry_kafka_notifications.xml");
+$kafkaEventNotificationUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2025_09_07_update_category_entry_kafka_notifications.xml");
 
-if(!file_exists($scheduleEventNotificationUpdate))
+if(!file_exists($kafkaEventNotificationUpdate))
 {
 	KalturaLog::err("Missing update script file");
 	return;
 }
 
-passthru("php $script $scheduleEventNotificationUpdate");
+passthru("php $script $kafkaEventNotificationUpdate");
