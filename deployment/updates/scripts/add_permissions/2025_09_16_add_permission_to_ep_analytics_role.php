f6c1dceba16170bbb72a75f1c8a6cec1e1796fce
daniel.barak <daniel.barak@kaltura.com>
2025-09-21 14:42:32 +0300

    PLAT-25375: Refactor beacon permissions to standardize naming and enhance role definitions

--- deployment/updates/scripts/add_permissions/2025_09_16_add_permission_to_ep_analytics_role.php
+++ deployment/updates/scripts/add_permissions/2025_09_16_add_permission_to_ep_analytics_role.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package deployment
- * @subpackage Scorpius.roles_and_permissions
+ * @subpackage Venus.roles_and_permissions
  */
 
 $script = realpath(dirname(__FILE__) . '/../../../../') . '/alpha/scripts/utils/permissions/addPermissionToRole.php';
-passthru("php $script 0 'EP user analytics role' 'EP_BEACON_PERMISSION' realrun");
+passthru("php $script 0 'EP user analytics role' 'BEACON_BASE' realrun");
