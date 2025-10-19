74baa18a5402a157e76c925e1278b4aca51ca039
Ravit Shalem <ravitshalem@IL-RAVITSH-RND.local>
2025-10-19 14:39:24 +0300

    FEC-14668: new stand alone page

--- alpha/apps/kaltura/modules/extwidget/templates/previewSuccess.php
+++ alpha/apps/kaltura/modules/extwidget/templates/previewSuccess.php
@@ -1,61 +1,54 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="lt-ie10 lt-ie9"> <![endif]-->
-<!--[if lt IE 10]>     <html class="lt-ie10"> <![endif]-->
-<!--[if gt IE 8]><!--> <html> <!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<?php if( $entry_id ) { ?>
-		<meta property="og:url" content="<?php echo $pageURL; ?>" />
-		<meta property="og:title" content="<?php echo addslashes(strip_tags(html_entity_decode($entry_name))); ?>" />
-		<meta property="og:description" content="<?php echo addslashes(strip_tags(html_entity_decode($entry_description))); ?>" />
-		<meta property="og:type" content="video.other" />
-		<meta property="og:image" content="<?php echo $entry_thumbnail_url; ?>/width/<?php echo $uiConf->getWidth();?>" />
-		<meta property="og:image:secure_url" content="<?php echo $entry_thumbnail_secure_url; ?>/width/<?php echo $uiConf->getWidth();?>" />
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<?php if( $entry_id ) { ?>
+	<meta property="og:url" content="<?php echo $pageURL; ?>" />
+	<meta property="og:title" content="<?php echo addslashes(strip_tags(html_entity_decode($entry_name))); ?>" />
+	<meta property="og:description" content="<?php echo addslashes(strip_tags(html_entity_decode($entry_description))); ?>" />
+	<meta property="og:type" content="video.other" />
+	<meta property="og:image" content="<?php echo $entry_thumbnail_url; ?>/width/<?php echo $uiConf->getWidth();?>" />
+	<meta property="og:image:secure_url" content="<?php echo $entry_thumbnail_secure_url; ?>/width/<?php echo $uiConf->getWidth();?>" />
 
-		<?php if( isset($flavor_asset_id) ) { ?>
-			<meta property="og:video" content="<?php echo $flavorUrl; ?>" />
-			<meta property="og:video:type" content="video/mp4" />
-		<?php } ?>
-		<meta property="og:video:url" content="<?php echo $playerUrl; ?>">
-		<meta property="og:video:secure_url" content="<?php echo $playerUrl; ?>">
-		<meta property="og:video:type" content="text/html">
-		<meta property="og:video:width" content="<?php echo $uiConf->getWidth();?>" />
-		<meta property="og:video:height" content="<?php echo $uiConf->getHeight();?>" />
-
-		<meta name="twitter:card" content="player"/>
-		<meta name="twitter:site" content="@kaltura"/>
-		<meta name="twitter:creator" content="@kaltura"/>
-		<meta name="twitter:title" content="<?php echo htmlspecialchars($entry_name); ?>" />
-		<meta name="twitter:description" content="<?php echo htmlspecialchars($entry_description); ?>" />
-		<meta name="twitter:image" content="<?php echo $entry_thumbnail_secure_url; ?>/width/<?php echo $uiConf->getWidth();?>" />
-		<meta name="twitter:player" content="<?php echo $playerUrl; ?>" />
-		<?php if( isset($flavorUrl) ) { ?>
-			<meta name="twitter:player:stream" content="<?php echo $flavorUrl; ?>" />
-		<?php } ?>
-		<meta name="twitter:player:stream:content_type" content="video/mp4"/>
-		<meta name="twitter:player:height" content="<?php echo $uiConf->getHeight();?>" />
-		<meta name="twitter:player:width" content="<?php echo $uiConf->getWidth();?>" />
-
-		<meta property="og:site_name" content="Kaltura" />
+	<?php if( isset($flavor_asset_id) ) { ?>
+		<meta property="og:video" content="<?php echo $flavorUrl; ?>" />
+		<meta property="og:video:type" content="video/mp4" />
 	<?php } ?>
-	<title><?php echo htmlspecialchars($entry_name); ?></title>
-	<link type="text/css" rel="stylesheet" href="/lib/css/shortlink.css" />
-	<?php if($framed)  { ?>
-		<style>
-			html, body {margin: 0; padding: 0; width: 100%; height: 100%; }
-			body { background-color: #fff !important; }
-			#framePlayerContainer {margin: 0 auto; padding-top: 20px; text-align: center; }
-			object, div { margin: 0 auto; }
-		</style>
+	<meta property="og:video:url" content="<?php echo $playerUrl; ?>">
+	<meta property="og:video:secure_url" content="<?php echo $playerUrl; ?>">
+	<meta property="og:video:type" content="text/html">
+	<meta property="og:video:width" content="<?php echo $uiConf->getWidth();?>" />
+	<meta property="og:video:height" content="<?php echo $uiConf->getHeight();?>" />
+
+	<meta name="twitter:card" content="player"/>
+	<meta name="twitter:site" content="@kaltura"/>
+	<meta name="twitter:creator" content="@kaltura"/>
+	<meta name="twitter:title" content="<?php echo htmlspecialchars($entry_name); ?>" />
+	<meta name="twitter:description" content="<?php echo htmlspecialchars($entry_description); ?>" />
+	<meta name="twitter:image" content="<?php echo $entry_thumbnail_secure_url; ?>/width/<?php echo $uiConf->getWidth();?>" />
+	<meta name="twitter:player" content="<?php echo $playerUrl; ?>" />
+	<?php if( isset($flavorUrl) ) { ?>
+		<meta name="twitter:player:stream" content="<?php echo $flavorUrl; ?>" />
 	<?php } ?>
-	<!--[if lte IE 7]>
-	<script src="/lib/js/json2.min.js"></script>
-	<![endif]-->
-	<script src="/lib/js/jquery-1.8.3.min.js"></script>
-	<script src="/lib/js/KalturaEmbedCodeGenerator-1.0.6.min.js"></script>
+	<meta name="twitter:player:stream:content_type" content="video/mp4"/>
+	<meta name="twitter:player:height" content="<?php echo $uiConf->getHeight();?>" />
+	<meta name="twitter:player:width" content="<?php echo $uiConf->getWidth();?>" />
+
+	<meta property="og:site_name" content="Kaltura" />
+<?php } ?>
+<title><?php echo htmlspecialchars($entry_name); ?></title>
+<link type="text/css" rel="stylesheet" href="/lib/css/shortlink.css" />
+<?php if($framed)  { ?>
+	<style>
+		html, body {margin: 0; padding: 0; width: 100%; height: 100%; }
+		body { background-color: #fff !important; }
+		#framePlayerContainer {margin: 0 auto; padding-top: 20px; text-align: center; }
+		object, div { margin: 0 auto; }
+	</style>
+<?php } ?>
+<!--[if lte IE 7]>
+<script src="/lib/js/json2.min.js"></script>
+<![endif]-->
+<script src="/lib/js/jquery-1.8.3.min.js"></script>
+<script src="/lib/js/KalturaEmbedCodeGenerator-1.0.6.min.js"></script>
 </head>
 <body>
 <?php if(!$framed) { ?>
@@ -181,14 +174,7 @@
                 <scr`+`ipt type='text/javascript' src='${codeUrl}'></scr`+`ipt>
                 <scr`+`ipt src='https://static.kaltura.com/content/static/player-scripts/thumbnail-embed.js'></scr`+`ipt>
                 <scr`+`ipt>
-                        __thumbnailEmbed({
-                            config: {
-                                provider: {
-                                    partnerId: '${data.partnerId}',
-                                    uiConfId: '${data.uiConfId}'
-                                },
-                                targetId: 'kaltura_player_1'
-                            },
+	@@ -192,45 +192,45 @@ function getParameterByName(name, url) {
                             mediaInfo: {entryId: '${data.entryId}' }
                         });
                 </scr`+`ipt>`;
