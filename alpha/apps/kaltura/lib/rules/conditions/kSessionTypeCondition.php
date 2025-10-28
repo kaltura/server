25a89584ca4feb7032727e6b538d6c6319db9d83
yossipapi <yossi.papiashvili@kaltura.com>
2025-10-28 17:07:28 +0200

    PSVAMB-84627: Dont validate ACP if api from internal subnet or disable check was sent

--- alpha/apps/kaltura/lib/rules/conditions/kSessionTypeCondition.php
+++ alpha/apps/kaltura/lib/rules/conditions/kSessionTypeCondition.php
@@ -44,7 +44,31 @@ class kSessionTypeCondition extends kCondition
 	protected function internalFulfilled(kScope $scope)
 	{
 		KalturaLog::debug("Checking session type condition, needed: $this->sessionType, actual: " . $scope->getKs()->type . ", not: " . boolval($this->not));
+		
+		$isInternalRequest =  $this->isInternalRequest();
+		$disableConditionCheck = $scope->getKs()->hasPrivilege(kSessionBase::PRIVILEGE_DISABLE_ACP_SESSION_TYPE_CHECK);
+		//If request ip is from internal subnet or has the privilege to disable session type check, bypass the session type condition
+		if($disableConditionCheck || $isInternalRequest)
+		{
+			KalturaLog::debug("Bypassing session type condition check [$isInternalRequest] [$disableConditionCheck]");
+			return $this->not;
+		}
+		
 		return $this->sessionType == $scope->getKs()->type;
+	}
+	protected function isInternalRequest()
+	{
+		$requestIp = requestUtils::getRemoteAddress();
+		$internalIpList = kConf::get('internal_partner_access_allowed_ips', kConfMapNames::SECURITY, array());
+		foreach ($internalIpList as $curRange)
+		{
+			if (kIpAddressUtils::isIpInRange($requestIp, $curRange))
+			{
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 	/* (non-PHPdoc)
