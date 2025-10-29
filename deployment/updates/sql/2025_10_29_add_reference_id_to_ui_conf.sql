065f72f1322caecb57154e76823784368ba7f438
TalDubovKaltura <tal.dubov@kaltura.com>
2025-10-29 13:59:25 +0200

    PLAT-25545 -> Reference id field to ui_conf sql table

--- deployment/updates/sql/2025_10_29_add_reference_id_to_ui_conf.sql
+++ deployment/updates/sql/2025_10_29_add_reference_id_to_ui_conf.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `ui_conf`
 	ADD `reference_id` VARCHAR(512) AFTER `partner_tags`,
-	ADD KEY `partner_id_reference_id_index`(`partner_id`, `reference_id`);
+	ADD INDEX `partner_id_reference_id_index`(`partner_id`, `reference_id`);
