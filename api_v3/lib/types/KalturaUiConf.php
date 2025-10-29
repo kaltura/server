b65e0eb3a94996264cca2405791de35a2a9eee24
TalDubovKaltura <tal.dubov@kaltura.com>
2025-10-29 14:17:08 +0200

    PLAT-25545 -> Reference id field addition to the api objects

--- api_v3/lib/types/KalturaUiConf.php
+++ api_v3/lib/types/KalturaUiConf.php
@@ -162,6 +162,10 @@ class KalturaUiConf extends KalturaObject implements IFilterable
 	 */
 	public $v2Redirect;
 
+	/**
+	 * @var string
+	 */
+	public $referenceId;
 
 	private static $map_between_objects = array
 	(
@@ -189,7 +193,8 @@ class KalturaUiConf extends KalturaObject implements IFilterable
 		"config",
 		"version",
 		"partnerTags",
-		"v2Redirect"
+		"v2Redirect",
+		"referenceId"
 	);
 
 	public function getMapBetweenObjects ( )
