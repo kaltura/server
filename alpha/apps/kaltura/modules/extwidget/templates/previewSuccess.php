e7abef53e7363c3121b7d9471e7ebcba77f0fffd
Moshe Maor <moshe.maor@kaltura.com>
2025-11-18 15:17:15 +0200

    Update previewSuccess.php

--- alpha/apps/kaltura/modules/extwidget/templates/previewSuccess.php
+++ alpha/apps/kaltura/modules/extwidget/templates/previewSuccess.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php
-	$googleVerificationToken = kConf::getArrayValue('googleVerificationToken', 'playkit-js', 'local', "");
+	$googleVerificationToken = kConf::getArrayValue('googleVerificationToken', 'playkit-js', 'local', false);
 	if( $googleVerificationToken ) { ?>
 	<meta name="google-site-verification" content="<?php echo $googleVerificationToken; ?>"/>
 	<?php } ?>
