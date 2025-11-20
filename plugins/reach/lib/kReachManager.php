56a144fc3e89dc0590b13724ec8bc32642a7ec1e
Moshe Maor <moshe.maor@kaltura.com>
2025-11-20 18:05:02 +0200

    PLAT-25588: avoid exception when no fullfiled catalog items found (#13672)

--- plugins/reach/lib/kReachManager.php
+++ plugins/reach/lib/kReachManager.php
@@ -150,7 +150,7 @@ class kReachManager implements kObjectChangedEventConsumer, kObjectCreatedEventC
 			$allowedCatalogItemIds = PartnerCatalogItemPeer::retrieveActiveCatalogItemIds($fullFieldCatalogItemIdsArr, $partnerId);
 			if(!count($allowedCatalogItemIds))
 			{
-				KalturaLog::debug('None of the fulfilled catalog item ids are active on partner, [' . implode(',', $fullFieldCatalogItemIds) . ']');
+				KalturaLog::debug('None of the fulfilled catalog item ids are active on partner, [' . implode(',', $fullFieldCatalogItemIds ? $fullFieldCatalogItemIds : []) . ']');
 				continue;
 			}
 			$this->addingEntryVendorTaskByObjectIds($taskObjectType, $allowedCatalogItemIds, $profileId, $object);
