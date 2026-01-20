198173fb12800ef7af1d161fa8e4cf68680b81de
noah solomon <noah.solomon@kaltura.com>
2026-01-20 14:22:10 +0200

    PLAT-25260 - accept new header and ks logic in playmanifest

--- alpha/web/index.php
+++ alpha/web/index.php
@@ -2,7 +2,7 @@
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
 {
 	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Range, Cache-Control');
+	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Range, Cache-Control,X-Kaltura-Ks');
 	header('Access-Control-Allow-Methods: POST, GET, HEAD, OPTIONS');
 	header('Access-Control-Expose-Headers: Server, Content-Length, Content-Range, Date, Cache-Control, Content-Encoding');
 	exit;
@@ -268,4 +268,3 @@ define('SF_ENVIRONMENT',	'prod');
 define('SF_DEBUG',			false);
 
 require_once(__DIR__ . '/../bootstrap.php');
-
