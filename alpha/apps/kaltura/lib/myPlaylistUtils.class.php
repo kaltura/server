2d707d1972a71ea581d582da5df1b8e3b2cd1c12
Moshe Maor <moshe.maor@kaltura.com>
2026-02-11 22:59:44 +0200

    SUP-51255: fix dynamic playlist entitlement veirification (#13789)
    * SUP-51255: fix dynamic playlist entitlement veirification
    
    * SUP-51255: Fix playlist thumb enitlement
    
    * SUP-51255: adding remark

--- alpha/apps/kaltura/lib/myPlaylistUtils.class.php
+++ alpha/apps/kaltura/lib/myPlaylistUtils.class.php
@@ -787,7 +787,6 @@ class myPlaylistUtils
 				// set a smaller limit incase the filter's limit is to high
 				$entry_filter->setLimit ( $current_limit );
 			}
-
 			// read the _eq_display_in_search field but ignore it because it's part of a more complex criterion
 			$display_in_search = $entry_filter->get( "_eq_display_in_search");
 			if ( $display_in_search >= 2 )
@@ -795,6 +794,8 @@ class myPlaylistUtils
 				$entry_filter->set ( "_eq_display_in_search" , null );
 			}
 			
+			//Setting search scope to avoid override of display_in_search criteria
+			$entry_filter->setPartnerSearchScope ( baseObjectFilter::MATCH_KALTURA_NETWORK_AND_PRIVATE );
 			$entry_filter->attachToCriteria( $c );
 
 			// add some hard-coded criteria
@@ -823,7 +824,6 @@ class myPlaylistUtils
 			{
 				self::addSchedulingToCriteria($c, $entry_filter);
 			}
-			
 			self::addModerationToCriteria($c);
 			$c = entryPeer::prepareEntitlementCriteriaAndFilters( $c );
 			$entry_ids_list_for_filter = $c->getFetchedIds();
