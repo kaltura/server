76080459c3fa4459561c580af4b4869c07eea6cb
shahbaa123 <shahbaa.shalan@kaltura.com>
2026-02-22 21:38:02 +0200

    Fix form ID to maintain compatibility with existing JavaScript
    Changed form ID from 'frmDeliveryProfileFilter' to 'frmPartnerIdFilter' to ensure
    the existing JavaScript code in the view template can properly target the form.

--- admin_console/forms/Delivery/DeliveryProfileFilter.php
+++ admin_console/forms/Delivery/DeliveryProfileFilter.php
@@ -8,7 +8,7 @@ class Form_Delivery_DeliveryProfileFilter extends Form_PartnerIdFilter
 	public function init()
 	{
 		parent::init();
-		$this->setAttrib('id', 'frmDeliveryProfileFilter');
+		$this->setAttrib('id', 'frmPartnerIdFilter');
 		$filterType = $this->getElement('filter_type');
 		$filterType->setMultiOptions(array(
 			'none' => 'None',
